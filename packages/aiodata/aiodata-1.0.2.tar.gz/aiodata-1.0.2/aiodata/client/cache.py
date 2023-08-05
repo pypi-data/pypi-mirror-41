3b978a3ee2ce97dc798088d5b42caed2b62cd860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:18 +0000

    Add package aiodata/1.0.2/aiodata-1.0.2.tar.gz

---
+++ packages/aiodata/aiodata-1.0.2.tar.gz/aiodata-1.0.2/aiodata/client/cache.py
@@ -0,0 +1,96 @@
+
+from . import helpers
+
+
+__all__ = ()
+
+
+class Entry:
+
+    __slots__ = ('__dict__',)
+
+    __init__ = helpers.update_value
+
+    def __getitem__(self, key):
+
+        try:
+
+            value = getattr(self, key)
+
+        except AttributeError:
+
+            raise KeyError(key) from None
+
+        return value
+
+
+class Entries(dict):
+
+    __slots__ = ('_build', '_primaries')
+
+    def __init__(self, build, keys):
+
+        self._build = build
+
+        self._primaries = keys
+
+    @property
+    def primaries(self):
+
+        return self._primaries
+
+    def get(self, *keys):
+
+        value = self
+
+        allowed = len(self._primaries)
+
+        for key in keys[:allowed]:
+
+            value = value[key]
+
+        return value
+
+    def query(self, data):
+
+        return (data[key] for key in self._primaries)
+
+    def create(self, data):
+
+        query = tuple(self.query(data))
+
+        (*start, final) = query
+
+        value = self
+
+        for (index, key) in enumerate(start):
+
+            try:
+
+                value = value[key]
+
+            except KeyError:
+
+                pass
+
+            else:
+
+                continue
+
+            keys = query[index:]
+
+            value[key] = value = self.__class__(self._build, self._primaries)
+
+        value[final] = entry = self._build(data)
+
+        return entry
+
+    def destroy(self, *keys):
+
+        (*start, final) = keys
+
+        values = self.get(*start)
+
+        value = values.pop(final)
+
+        return value
