25662a474cdc73f12df14a210a2cb21f1193f7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:03 +0000

    Add package doraemon-hub/0.2/doraemon-hub-0.2.tar.gz

---
+++ packages/doraemon-hub/doraemon-hub-0.2.tar.gz/doraemon-hub-0.2/rfhub/__main__.py
@@ -0,0 +1,5 @@
+from . import app
+
+app.hub = app.RobotHub()
+app.hub.start()
+
---
+++ packages/doraemon-hub/doraemon-hub-0.2.tar.gz/doraemon-hub-0.2/setup.py
@@ -0,0 +1,44 @@
+# N.B. to push a new version to PyPi, update the version number
+# in rfhub/version.py and then run 'python setup.py sdist upload'
+from setuptools import setup
+
+filename = 'rfhub/version.py'
+exec(open(filename).read())
+
+setup(
+    name             = 'doraemon-hub',
+    version          = __version__,
+    author           = 'Bryan Oakley',
+    author_email     = 'bryan.oakley@gmail.com',
+    url              = 'https://github.com/boakley/robotframework-hub/',
+    keywords         = 'doraemon',
+    license          = 'Apache License 2.0',
+    description      = 'Webserver for robot framework assets',
+    long_description = open('README.md').read(),
+    zip_safe         = True,
+    include_package_data = True,
+    install_requires = ['Flask', 'watchdog', 'robotframework', 'tornado'],
+    classifiers      = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Framework :: Robot Framework",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Intended Audience :: Developers",
+        ],
+    packages         =[
+        'rfhub',
+        'rfhub.blueprints',
+        'rfhub.blueprints.api',
+        'rfhub.blueprints.doc',
+        'rfhub.blueprints.dashboard',
+        ],
+    scripts          =[], 
+    entry_points={
+        'console_scripts': [
+            "rfhub = rfhub.__main__:main"
+        ]
+    }
+)
