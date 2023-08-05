e86aa30133653c69099d368aa9283c43f1b46378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:48 +0000

    Add package unicon/3.4.6/unicon-3.4.6-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/unicon/unicon-3.4.6-cp35-cp35m-manylinux1_x86_64.whl/unicon/plugins/junos/connection_provider.py
@@ -0,0 +1,37 @@
+"""
+Module:
+    unicon.plugins.junos
+
+Authors:
+    PyATS TEAM (pyats-support@cisco.com)
+
+Description:
+    This module imports connection provider class which has
+    exposes two methods named connect and disconnect. These
+    methods are implemented in such a way so that they can
+    handle majority of platforms and subclassing is seldom
+    required.
+"""
+from unicon.bases.routers.connection_provider import BaseSingleRpConnectionProvider
+from unicon.eal.dialogs import Dialog
+from .statements import connection_statement_list
+
+
+class JunosSingleRpConnectionProvider(BaseSingleRpConnectionProvider):
+    """ Implements Junos singleRP Connection Provider,
+        This class overrides the base class with the
+        additional dialogs and steps required for
+        connecting to any device via generic implementation
+    """
+    def __init__(self, *args, **kwargs):
+
+        """ Initializes the generic connection provider
+        """
+        super().__init__(*args, **kwargs)
+
+    def get_connection_dialog(self):
+        """ creates and returns a Dialog to handle all device prompts
+            appearing during initial connection to the device.
+            See statements.py for connnection statement lists
+        """
+        return Dialog(connection_statement_list)
