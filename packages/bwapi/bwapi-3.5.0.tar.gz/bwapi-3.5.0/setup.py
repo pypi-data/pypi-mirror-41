64a495d9b8e22f25373f73e1e06d2a4bfea0feb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:34 +0000

    Add package bwapi/3.5.0/bwapi-3.5.0.tar.gz

---
+++ packages/bwapi/bwapi-3.5.0.tar.gz/bwapi-3.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='bwapi',
+    version='3.5.0',
+    description='A software development kit for the Brandwatch API',
+    url='https://github.com/BrandwatchLtd/api_sdk',
+    author='Amy Barker, Jamie Lebovics, Paul Siegel and Jessica Bowden',
+    author_email=
+    'amyb@brandwatch.com, paul@brandwatch.com, jess@brandwatch.com',
+    license='License :: OSI Approved :: MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    py_modules=[
+        'bwproject', 'bwresources', 'bwdata', 'credentials', 'filters'
+    ],
+    scripts=['authenticate.py'],
+    install_requires=['requests'],
+    tests_require=['responses'])
