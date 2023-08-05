78600b35eb7fc9132ced7ada4c37e38577b8d5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package tornado-requests/0.0.2/tornado-requests-0.0.2.tar.gz

---
+++ packages/tornado-requests/tornado-requests-0.0.2.tar.gz/tornado-requests-0.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+
+setup(
+	name='tornado-requests',
+    version='0.0.2',
+    description='对tornado中AsyncHTTPClient的一个简单封装',
+    long_description_content_type="text/markdown",
+    url='https://github.com/hfldqwe/tornado-requests.git',
+    author='hfldqwe',
+    author_email='1941066681@qq.com',
+    license='MIT',
+    packages=setuptools.find_packages())
