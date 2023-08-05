1f37e0f2609b0815e8befcef5db4679d38e54da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package nesterstudy/1.0.0/nesterstudy-1.0.0.tar.gz

---
+++ packages/nesterstudy/nesterstudy-1.0.0.tar.gz/nesterstudy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+		name='nesterstudy',
+		version='1.0.0',
+		py_moudules=['nesterstudy'],
+		author='daggerpy',
+		author_email='749965808@qq.com',
+		url='https://www.cnblogs.com/Fatty-carrots/',
+		description='A simple printer of nested lists',
+
+		)
