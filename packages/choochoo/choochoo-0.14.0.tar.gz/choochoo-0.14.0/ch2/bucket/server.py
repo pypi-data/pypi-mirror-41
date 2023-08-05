066d95033c4839c2fea46154cae2f58e53bc294a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:16 +0000

    Add package choochoo/0.14.0/choochoo-0.14.0.tar.gz

---
+++ packages/choochoo/choochoo-0.14.0.tar.gz/choochoo-0.14.0/ch2/bucket/server.py
@@ -0,0 +1,106 @@
+
+from abc import abstractmethod, ABC
+from threading import Thread
+
+from bokeh.server.server import Server
+from tornado.ioloop import IOLoop
+
+SINGLETON = None
+
+TEMPLATE = '''
+{% block css_resources %}
+  {{ super() }}
+  <style>
+body {
+  background-color: white;
+}
+.centre {
+  text-align: center
+}
+.centre > div {
+  display: inline-block;
+}
+table {
+  margin: 20px;
+  border-spacing: 10px;
+}
+  </style>
+{% endblock %}
+{% block inner_body %}
+  <div class='centre'>
+  <h1>{{ header }}</h1>
+  {{ super() }}
+  </div>
+{% endblock %}
+'''
+
+
+class ServerThread(Server):
+
+    def __init__(self, log, app_map):
+        self._log = log
+        super().__init__(app_map, io_loop=IOLoop.instance())
+        Thread(target=self.io_loop.start).start()
+
+    def stop(self):
+
+        def callback():
+            self._log.info('Shutting down server')
+            self.io_loop.stop()
+            super(ServerThread, self).stop(wait=True)
+            self.unlisten()
+            self._log.debug('Server shut down')
+
+        self.io_loop.add_callback(callback)
+
+
+def singleton_server(log, app_map):
+
+    global SINGLETON
+
+    if SINGLETON is None:
+        log.info('Starting new server')
+        # tornado is weird about ioloops and threads
+        # https://github.com/universe-proton/universe-topology/issues/17
+        SINGLETON = ServerThread(log, app_map)
+
+    return SINGLETON
+
+
+class Page(ABC):
+
+    def __init__(self, log, db, template=TEMPLATE, **vars):
+        self._log = log
+        self._db = db
+        self._template = template
+        self._vars = vars
+
+    def __call__(self, doc):
+        self._log.debug('Page called with %s' % doc)
+        with self._db.session_context() as s:
+            params = doc.session_context.request.arguments
+            self._log.debug('Request params: %s' % params)
+            vars, root = self.create(s, **params)
+            vars = vars if vars is not None else {}
+            vars.update(self._vars)
+            doc.add_root(root)
+            doc.template = self._template
+            doc.template_variables.update(vars)
+            doc.title = vars['title'] if 'title' in vars else 'Choochoo'
+
+    __ERROR = object()
+
+    def single_int_param(self, name, values, deflt=__ERROR):
+        try:
+            return int(values[0])
+        except Exception as e:
+            msg = 'Could not parse "%s" for %s' % (values, name)
+            self._log.warn(msg)
+            if deflt != self.__ERROR:
+                return deflt
+            else:
+                raise Exception(msg)
+
+    @abstractmethod
+    def create(self, s, **kargs):
+        raise NotImplementedError()
---
+++ packages/choochoo/choochoo-0.14.0.tar.gz/choochoo-0.14.0/ch2/lib/date.py
@@ -0,0 +1,123 @@
+
+import pendulum as p
+import datetime as dt
+import time as t
+from calendar import monthrange
+
+
+def format_date(date):
+    return date.strftime('%Y-%m-%d')
+
+
+def format_time(time):
+    return time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def to_date(value, none=False):
+    if none and value is None:
+        return None
+    if isinstance(value, dt.datetime):
+        raise Exception('Use tz-aware conversion')
+    elif isinstance(value, dt.date):
+        return value
+    elif isinstance(value, int):
+        return dt.date.fromordinal(value)
+    else:
+        return dt.date(*t.strptime(value, '%Y-%m-%d')[:3])
+
+
+def to_time(value, none=False):
+    if none and value is None:
+        return None
+    if isinstance(value, dt.datetime):
+        return value.replace(tzinfo=dt.timezone.utc)
+    elif isinstance(value, dt.date):
+        raise Exception('Use tz-aware conversion')
+    elif isinstance(value, int):
+        return to_time(to_date(value))
+    elif isinstance(value, float):
+        return dt.datetime.fromtimestamp(value, dt.timezone.utc)
+    else:
+        for format in ('%Y-%m-%dT%H:%M:%S.%f', '%Y-%m-%d %H:%M:%S.%f',
+                       '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M:%S',
+                       '%Y-%m-%dT%H:%M', '%Y-%m-%d %H:%M', '%Y-%m-%d', '%Y'):
+            try:
+                return dt.datetime.strptime(value, format).replace(tzinfo=dt.timezone.utc)
+            except ValueError:
+                pass
+        raise ValueError('Cannot parse "%s" as a datetime' % value)
+
+
+DAY = 'd'
+WEEK = 'w'
+MONTH = 'm'
+YEAR = 'y'
+
+
+def add_date(date, duration):
+    # this only works with dates
+    # before, working with datetime, we had confusion between dates and datetimes and unnecessary conversions
+    # now, for times, use timedelta.
+    date, n, unit = to_date(date), duration[0], duration[1].lower()
+    if unit == DAY:
+        return date + dt.timedelta(days=n)
+    if unit == WEEK:
+        return date + dt.timedelta(days=n * 7)
+    if unit == MONTH:
+        year, month = date.year, date.month + n
+        while month > 12:
+            month -= 12
+            year += 1
+        while month < 1:
+            month += 12
+            year -= 1
+        return dt.date(year, month, min(date.day, monthrange(year, month)[1]))
+    if unit == YEAR:
+        year = date.year + n
+        return dt.date(year, date.month, min(date.day, monthrange(year, date.month)[1]))
+    raise Exception('Unexpected unit "%s" (need one of %s, %s, %s, %s)' % (unit, DAY, WEEK, MONTH, YEAR))
+
+
+def format_seconds(seconds):
+    if seconds >= 60:
+        minutes, seconds = seconds // 60, seconds % 60
+        if minutes >= 60:
+            hours, minutes = minutes // 60, minutes % 60
+            return '%dh%02dm%02ds' % (hours, minutes, seconds)
+        else:
+            return '%dm%02ds' % (minutes, seconds)
+    else:
+        return '%ds' % seconds
+
+
+def local_date_to_time(date):
+    date = to_date(date)
+    ptime = p.DateTime(year=date.year, month=date.month, day=date.day,
+                       tzinfo=p.tz.get_local_timezone()).in_timezone(dt.timezone.utc)
+    return dt.datetime(*ptime.timetuple()[:6], tzinfo=dt.timezone.utc)
+
+
+def time_to_local_date(time):
+    time = to_time(time)
+    ptime = p.DateTime(*time.timetuple()[:6], tzinfo=dt.timezone.utc).in_timezone(p.tz.get_local_timezone())
+    return dt.date(year=ptime.year, month=ptime.month, day=ptime.day)
+
+
+def min_time(a, b):
+    if a is None:
+        return b
+    if b is None:
+        return a
+    return min(a, b)
+
+
+def max_time(a, b):
+    if a is None:
+        return b
+    if b is None:
+        return a
+    return max(a, b)
+
+
+def extend_range(start, finish, time):
+    return min_time(start, time), max_time(finish, time)
