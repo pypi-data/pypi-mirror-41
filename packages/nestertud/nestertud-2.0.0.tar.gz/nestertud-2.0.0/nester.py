60ea9f27c982f014fb74553dfb8625c7265314fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:40 +0000

    Add package nestertud/2.0.0/nestertud-2.0.0.tar.gz

---
+++ packages/nestertud/nestertud-2.0.0.tar.gz/nestertud-2.0.0/nester.py
@@ -0,0 +1,11 @@
+'''This is an example funciton for trial.
+It prints all netsted list.'''
+def print_lol(the_list,indent=False,level=1):
+    for item in the_list:
+        if isinstance(item,list):
+            print_lol(item,indent,level+1)
+        else:
+            if(indent):
+                for i in range(level):
+                    print("\t",end='')
+            print(item)
