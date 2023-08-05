8b96dc6ac74e1916e4c3e914dca0a1a25bf252b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package yuu/0.1.2/yuu-0.1.2.tar.gz

---
+++ packages/yuu/yuu-0.1.2.tar.gz/yuu-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from yuu.common import __version__
+
+with open('README.md', 'r') as f:
+    desc = f.read()
+
+setuptools.setup(name = 'yuu',
+version = __version__,
+description = 'Yuu - A simple AbemaTV video downloader',
+long_description = desc,
+long_description_content_type = "text/markdown",
+author = 'noaione',
+author_email = 'noaione0809@gmail.com',
+keywords = ['ripping', 'downloader', 'parser'],
+license = 'GNU GPLv3',
+url = 'https://github.com/noaione/yuu',
+packages = setuptools.find_packages(),
+install_requires = ['requests[socks]', 'm3u8', 'tqdm', 'pycryptodome', 'beautifulsoup4'],
+classifiers = ['Development Status :: 5 - Production/Stable', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7'],
+entry_points = {
+    'console_scripts': ['yuu=yuu.command:main']
+})
