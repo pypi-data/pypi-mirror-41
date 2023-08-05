32fc96ab0bf78053cd96b1d54936707eb407b73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package hapic/0.71/hapic-0.71.tar.gz

---
+++ packages/hapic/hapic-0.71.tar.gz/hapic-0.71/hapic/exception.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+
+class HapicException(Exception):
+    pass
+
+
+class ConfigurationException(HapicException):
+    pass
+
+
+class WorkflowException(HapicException):
+    pass
+
+
+class DecorationException(HapicException):
+    pass
+
+
+class AlreadyDecoratedException(DecorationException):
+    pass
+
+
+class ProcessException(HapicException):
+    pass
+
+
+class ValidationException(ProcessException):
+    pass
+
+
+class InputWorkflowException(WorkflowException):
+    pass
+
+
+class OutputWorkflowException(WorkflowException):
+    pass
+
+
+class InputValidationException(InputWorkflowException, ValidationException):
+    pass
+
+
+class OutputValidationException(OutputWorkflowException, ValidationException):
+    pass
+
+
+class DocumentationException(HapicException):
+    pass
+
+
+class NoRoutesException(DocumentationException):
+    pass
+
+
+class RouteNotFound(DocumentationException):
+    pass
