ab3270cd0ddba55993553ebccfd16b9cfbd7634f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:23 +0000

    Add package sanic-prometheus/0.1.6/sanic-prometheus-0.1.6.tar.gz

---
+++ packages/sanic-prometheus/sanic-prometheus-0.1.6.tar.gz/sanic-prometheus-0.1.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='sanic-prometheus',
+    version='0.1.6',
+    description='Exposes various prometheus monitoring metrics of Sanic-based apps.',
+    url='http://github.com/dkruchinin/sanic-prometheus',
+    author='Dan Kruchinin',
+    author_email='dan.kruchinin@gmail.com',
+    license='MIT',
+    packages=['sanic_prometheus'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'sanic>=0.5.0',
+        'prometheus-client~=0.4.2',
+        'psutil>=5.2.0'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Topic :: System :: Monitoring',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sanic prometheus monitoring'
+)
