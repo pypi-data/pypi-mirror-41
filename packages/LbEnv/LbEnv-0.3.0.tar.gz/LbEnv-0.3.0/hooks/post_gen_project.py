d301bf033c35666df281c0e76c9334416c911030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package LbEnv/0.3.0/LbEnv-0.3.0.tar.gz

---
+++ packages/LbEnv/LbEnv-0.3.0.tar.gz/LbEnv-0.3.0/hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+###############################################################################
+# (c) Copyright 2018 CERN                                                     #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+import shutil
+
+if __name__ == '__main__':
+    if '{{cookiecutter.scripts_flavour}}' == 'prod':
+        shutil.copy('LbEnv-prod', 'LbEnv')
