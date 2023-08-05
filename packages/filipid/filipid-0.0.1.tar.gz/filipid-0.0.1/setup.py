a2ffdd4fb34d595dd15b8089941cf14e5939d064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package filipid/0.0.1/filipid-0.0.1.tar.gz

---
+++ packages/filipid/filipid-0.0.1.tar.gz/filipid-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="filipid",
+    version="0.0.1",
+    author="Gavrilo Andric",
+    author_email="gavriloandric@gmail.com",
+    description="UMAK utils module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    include_package_data=True,
+)
