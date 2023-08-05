7aab0f23904682383a05ab53df8c7c3c34af9f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package lbmessaging/0.0.5/lbmessaging-0.0.5.tar.gz

---
+++ packages/lbmessaging/lbmessaging-0.0.5.tar.gz/lbmessaging-0.0.5/lbmessaging/CvmfsDevExchange.py
@@ -0,0 +1,31 @@
+###############################################################################
+# (c) Copyright 2017 CERN                                                     #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+'''
+Module in charge of dealing with sending and receiving commands for the
+/cvmfs/lhcbdev.cern.ch
+'''
+
+from lbmessaging.CommandExchange import CommandExchange
+
+__author__ = 'Ben Couturier <ben.couturier@cern.ch>'
+
+
+CVMFSDEV_EXCHANGE = "cvmfsdev.action"
+CVMFSDEV_ERRORS_EXCHANGE = "cvmfsdev.action.error"
+
+class CvmfsDevExchange(CommandExchange):
+    """ Customized exchange for the cvmfsdev startum-0 server """
+
+    def __init__(self, channel):
+        """ Constructor that fixes the names of the exchanges """
+        super(CvmfsDevExchange, self).__init__(channel,
+                                               CVMFSDEV_EXCHANGE,
+                                               CVMFSDEV_ERRORS_EXCHANGE)
