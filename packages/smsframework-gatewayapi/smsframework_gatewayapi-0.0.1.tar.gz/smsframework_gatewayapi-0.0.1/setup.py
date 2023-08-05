e16d774e967caadd7a3fd0d5273e88a45cede7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:26 +0000

    Add package smsframework-gatewayapi/0.0.1/smsframework_gatewayapi-0.0.1.tar.gz

---
+++ packages/smsframework-gatewayapi/smsframework_gatewayapi-0.0.1.tar.gz/smsframework_gatewayapi-0.0.1/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+""" SMS framework: GatewayApi provider """
+
+from setuptools import setup, find_packages
+
+setup(
+    # http://pythonhosted.org/setuptools/setuptools.html
+    name='smsframework_gatewayapi',
+    version='0.0.1',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-smsframework-gatewayapi',
+    license='BSD',
+    description=__doc__,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['sms', 'message', 'notification', 'receive', 'send', 'gatewayapi'],
+
+    packages=find_packages(),
+    scripts=[],
+    entry_points={},
+
+    install_requires=[
+        'smsframework >= 0.0.9',
+        'requests_oauthlib >= 1.2.0',
+    ],
+    extras_require={
+        'receiver': ['flask >= 0.10', 'pyjwt >= 1.7.1'],  # sms receiving
+    },
+    test_suite='nose.collector',
+    include_package_data=True,
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
