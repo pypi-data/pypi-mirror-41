c4ee872272ebb390a84bce7f4d9ffdafefa66fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:47 +0000

    Add package tensorflowcv/0.0.18/tensorflowcv-0.0.18.tar.gz

---
+++ packages/tensorflowcv/tensorflowcv-0.0.18.tar.gz/tensorflowcv-0.0.18/README.md
@@ -0,0 +1,27 @@
+# Large-scale image classification networks
+
+Collection of large-scale image classification models on TensorFlow, pretrained on the ImageNet-1k dataset.
+
+## Installation
+
+To install, use:
+```
+pip install tensorflowcv tensorflow-gpu>=1.11.0
+```
+To enable/disable different hardware supports, check out TensorFlow installation [instructions](https://www.tensorflow.org/).
+
+Note that the models use NCHW data format. The current version of TensorFlow cannot work with them on CPU.
+
+## Usage
+
+Example of using the pretrained ResNet-18 model:
+```
+from tensorflowcv.model_provider import get_model as tfcv_get_model
+from tensorflowcv.model_provider import init_variables_from_state_dict as tfcv_init_variables_from_state_dict
+import tensorflow as tf
+net = tfcv_get_model("resnet18", pretrained=True)
+x = tf.placeholder(dtype=tf.float32, shape=(None, 3, 224, 224), name='xx')
+y_net = net(x)
+with tf.Session() as sess:
+    tfcv_init_variables_from_state_dict(sess=sess, state_dict=net.state_dict)
+```
---
+++ packages/tensorflowcv/tensorflowcv-0.0.18.tar.gz/tensorflowcv-0.0.18/tensorflowcv/models/densenet.py
@@ -0,0 +1,404 @@
+"""
+    DenseNet, implemented in TensorFlow.
+    Original paper: 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+"""
+
+__all__ = ['DenseNet', 'densenet121', 'densenet161', 'densenet169', 'densenet201']
+
+import os
+import tensorflow as tf
+from .common import pre_conv1x1_block, pre_conv3x3_block
+from .preresnet import preres_init_block, preres_activation
+
+
+def dense_unit(x,
+               in_channels,
+               out_channels,
+               dropout_rate,
+               training,
+               name="dense_unit"):
+    """
+    DenseNet unit.
+
+    Parameters:
+    ----------
+    x : Tensor
+        Input tensor.
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    dropout_rate : bool
+        Parameter of Dropout layer. Faction of the input units to drop.
+    training : bool, or a TensorFlow boolean scalar tensor
+      Whether to return the output in training mode or in inference mode.
+    name : str, default 'dense_unit'
+        Unit name.
+
+    Returns
+    -------
+    Tensor
+        Resulted tensor.
+    """
+    bn_size = 4
+    inc_channels = out_channels - in_channels
+    mid_channels = inc_channels * bn_size
+
+    identity = x
+
+    x = pre_conv1x1_block(
+        x=x,
+        in_channels=in_channels,
+        out_channels=mid_channels,
+        training=training,
+        name=name + "/conv1")
+    x = pre_conv3x3_block(
+        x=x,
+        in_channels=mid_channels,
+        out_channels=inc_channels,
+        training=training,
+        name=name + "/conv2")
+
+    use_dropout = (dropout_rate != 0.0)
+    if use_dropout:
+        x = tf.layers.dropout(
+            inputs=x,
+            rate=dropout_rate,
+            training=training,
+            name=name + "dropout")
+
+    x = tf.concat([identity, x], axis=1, name=name + "/concat")
+    return x
+
+
+def transition_block(x,
+                     in_channels,
+                     out_channels,
+                     training,
+                     name="transition_block"):
+    """
+    DenseNet's auxiliary block, which can be treated as the initial part of the DenseNet unit, triggered only in the
+    first unit of each stage.
+
+    Parameters:
+    ----------
+    x : Tensor
+        Input tensor.
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    training : bool, or a TensorFlow boolean scalar tensor
+      Whether to return the output in training mode or in inference mode.
+    name : str, default 'transition_block'
+        Unit name.
+
+    Returns
+    -------
+    Tensor
+        Resulted tensor.
+    """
+    x = pre_conv1x1_block(
+        x=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        training=training,
+        name=name + "/conv")
+    x = tf.layers.average_pooling2d(
+        inputs=x,
+        pool_size=2,
+        strides=2,
+        data_format='channels_first',
+        name=name + "/pool")
+    return x
+
+
+class DenseNet(object):
+    """
+    DenseNet model from 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    dropout_rate : float, default 0.0
+        Parameter of Dropout layer. Faction of the input units to drop.
+    in_channels : int, default 3
+        Number of input channels.
+    in_size : tuple of two ints, default (224, 224)
+        Spatial size of the expected input image.
+    classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 dropout_rate=0.0,
+                 in_channels=3,
+                 in_size=(224, 224),
+                 classes=1000,
+                 **kwargs):
+        super(DenseNet, self).__init__(**kwargs)
+        self.channels = channels
+        self.init_block_channels = init_block_channels
+        self.dropout_rate = dropout_rate
+        self.in_channels = in_channels
+        self.in_size = in_size
+        self.classes = classes
+
+    def __call__(self,
+                 x,
+                 training=False):
+        """
+        Build a model graph.
+
+        Parameters:
+        ----------
+        x : Tensor
+            Input tensor.
+        training : bool, or a TensorFlow boolean scalar tensor, default False
+          Whether to return the output in training mode or in inference mode.
+
+        Returns
+        -------
+        Tensor
+            Resulted tensor.
+        """
+        in_channels = self.in_channels
+        x = preres_init_block(
+            x=x,
+            in_channels=in_channels,
+            out_channels=self.init_block_channels,
+            training=training,
+            name="features/init_block")
+        in_channels = self.init_block_channels
+        for i, channels_per_stage in enumerate(self.channels):
+            if i != 0:
+                x = transition_block(
+                    x=x,
+                    in_channels=in_channels,
+                    out_channels=(in_channels // 2),
+                    training=training,
+                    name="features/stage{}/trans{}".format(i + 1, i + 1))
+                in_channels = in_channels // 2
+            for j, out_channels in enumerate(channels_per_stage):
+                x = dense_unit(
+                    x=x,
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    dropout_rate=self.dropout_rate,
+                    training=training,
+                    name="features/stage{}/unit{}".format(i + 1, j + 1))
+                in_channels = out_channels
+        x = preres_activation(
+            x=x,
+            training=training,
+            name="features/post_activ")
+        x = tf.layers.average_pooling2d(
+            inputs=x,
+            pool_size=7,
+            strides=1,
+            data_format="channels_first",
+            name="features/final_pool")
+
+        x = tf.layers.flatten(x)
+        x = tf.layers.dense(
+            inputs=x,
+            units=self.classes,
+            name="output")
+
+        return x
+
+
+def get_densenet(num_layers,
+                 model_name=None,
+                 pretrained=False,
+                 root=os.path.join('~', '.tensorflow', 'models'),
+                 **kwargs):
+    """
+    Create DenseNet model with specific parameters.
+
+    Parameters:
+    ----------
+    num_layers : int
+        Number of layers.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.tensorflow/models'
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    functor
+        Functor for model graph creation with extra fields.
+    """
+
+    if num_layers == 121:
+        init_block_channels = 64
+        growth_rate = 32
+        layers = [6, 12, 24, 16]
+    elif num_layers == 161:
+        init_block_channels = 96
+        growth_rate = 48
+        layers = [6, 12, 36, 24]
+    elif num_layers == 169:
+        init_block_channels = 64
+        growth_rate = 32
+        layers = [6, 12, 32, 32]
+    elif num_layers == 201:
+        init_block_channels = 64
+        growth_rate = 32
+        layers = [6, 12, 48, 32]
+    else:
+        raise ValueError("Unsupported DenseNet version with number of layers {}".format(num_layers))
+
+    from functools import reduce
+    channels = reduce(lambda xi, yi:
+                      xi + [reduce(lambda xj, yj:
+                                   xj + [xj[-1] + yj],
+                                   [growth_rate] * yi,
+                                   [xi[-1][-1] // 2])[1:]],
+                      layers,
+                      [[init_block_channels * 2]])[1:]
+
+    net = DenseNet(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        from .model_store import download_state_dict
+        net.state_dict, net.file_path = download_state_dict(
+            model_name=model_name,
+            local_model_store_dir_path=root)
+    else:
+        net.state_dict = None
+        net.file_path = None
+
+    return net
+
+
+def densenet121(**kwargs):
+    """
+    DenseNet-121 model from 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.tensorflow/models'
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    functor
+        Functor for model graph creation with extra fields.
+    """
+    return get_densenet(num_layers=121, model_name="densenet121", **kwargs)
+
+
+def densenet161(**kwargs):
+    """
+    DenseNet-161 model from 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.tensorflow/models'
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    functor
+        Functor for model graph creation with extra fields.
+    """
+    return get_densenet(num_layers=161, model_name="densenet161", **kwargs)
+
+
+def densenet169(**kwargs):
+    """
+    DenseNet-169 model from 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.tensorflow/models'
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    functor
+        Functor for model graph creation with extra fields.
+    """
+    return get_densenet(num_layers=169, model_name="densenet169", **kwargs)
+
+
+def densenet201(**kwargs):
+    """
+    DenseNet-201 model from 'Densely Connected Convolutional Networks,' https://arxiv.org/abs/1608.06993.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.tensorflow/models'
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    functor
+        Functor for model graph creation with extra fields.
+    """
+    return get_densenet(num_layers=201, model_name="densenet201", **kwargs)
+
+
+def _test():
+    import numpy as np
+    from .model_store import init_variables_from_state_dict
+
+    pretrained = False
+
+    models = [
+        densenet121,
+        densenet161,
+        densenet169,
+        densenet201,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+        x = tf.placeholder(
+            dtype=tf.float32,
+            shape=(None, 3, 224, 224),
+            name='xx')
+        y_net = net(x)
+
+        weight_count = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != densenet121 or weight_count == 7978856)
+        assert (model != densenet161 or weight_count == 28681000)
+        assert (model != densenet169 or weight_count == 14149480)
+        assert (model != densenet201 or weight_count == 20013928)
+
+        with tf.Session() as sess:
+            if pretrained:
+                init_variables_from_state_dict(sess=sess, state_dict=net.state_dict)
+            else:
+                sess.run(tf.global_variables_initializer())
+            x_value = np.zeros((1, 3, 224, 224), np.float32)
+            y = sess.run(y_net, feed_dict={x: x_value})
+            assert (y.shape == (1, 1000))
+        tf.reset_default_graph()
+
+
+if __name__ == "__main__":
+    _test()
