3354700aac0c0f9714194b4cabe75915f3f71fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:57 +0000

    Add package wemake-python-styleguide/0.7.0/wemake_python_styleguide-0.7.0-py3-none-any.whl

---
+++ packages/wemake-python-styleguide/wemake_python_styleguide-0.7.0-py3-none-any.whl/wemake_python_styleguide/presets/general.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from wemake_python_styleguide.visitors.ast import (
+    annotations,
+    attributes,
+    builtins,
+    classes,
+    comparisons,
+    conditions,
+    functions,
+    keywords,
+    loops,
+    naming,
+    statements,
+)
+from wemake_python_styleguide.visitors.ast.imports import WrongImportVisitor
+from wemake_python_styleguide.visitors.ast.modules import (
+    EmptyModuleContentsVisitor,
+)
+from wemake_python_styleguide.visitors.filenames.module import (
+    WrongModuleNameVisitor,
+)
+
+#: Used to store all general visitors to be later passed to checker:
+GENERAL_PRESET = (
+    # General:
+    statements.StatementsWithBodiesVisitor,
+    statements.WrongParametersIndentationVisitor,
+
+    keywords.WrongRaiseVisitor,
+    keywords.WrongKeywordVisitor,
+    keywords.WrongTryExceptVisitor,
+    keywords.WrongContextManagerVisitor,
+    keywords.ConsistentReturningVisitor,
+
+    loops.WrongComprehensionVisitor,
+    loops.WrongLoopVisitor,
+
+    attributes.WrongAttributeVisitor,
+    annotations.WrongAnnotationVisitor,
+
+    functions.WrongFunctionCallVisitor,
+    functions.FunctionDefinitionVisitor,
+
+    WrongImportVisitor,
+
+    naming.WrongNameVisitor,
+    naming.WrongModuleMetadataVisitor,
+    naming.WrongVariableAssignmentVisitor,
+
+    builtins.MagicNumberVisitor,
+    builtins.WrongStringVisitor,
+    builtins.WrongAssignmentVisitor,
+    builtins.WrongCollectionVisitor,
+
+    comparisons.WrongConditionalVisitor,
+    comparisons.ComparisonSanityVisitor,
+    comparisons.WrongComparisionOrderVisitor,
+
+    conditions.IfStatementVisitor,
+
+    # Classes:
+    classes.WrongClassVisitor,
+    classes.WrongMethodVisitor,
+    classes.WrongSlotsVisitor,
+
+    # Modules:
+    WrongModuleNameVisitor,
+    EmptyModuleContentsVisitor,
+)
