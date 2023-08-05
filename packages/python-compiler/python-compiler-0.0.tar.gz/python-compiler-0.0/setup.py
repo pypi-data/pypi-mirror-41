1a361f7a876d786cdd386e9bef630c7d1295f445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package python-compiler/0.0/python-compiler-0.0.tar.gz

---
+++ packages/python-compiler/python-compiler-0.0.tar.gz/python-compiler-0.0/setup.py
@@ -0,0 +1,18 @@
+import sys
+from setuptools import setup
+
+
+if sys.version_info < (3, 0):
+    sys.stderr.write("Sorry, Python < 3.0 is not supported\n")
+    sys.exit(1)
+
+
+setup(name='python-compiler',
+      version='0.0',
+      description="""Python bytecode compiler written in Python""",
+#      long_description=open('README.rst').read(),
+      url='https://github.com/pfalcon/python-compiler',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='Python',
+      packages=['compiler'])
