4ea9cfbac33ab38966a1916b9b85e9c3977f1db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:09 +0000

    Add package sedatatools/0.4.5/sedatatools-0.4.5-py2.py3-none-any.whl

---
+++ packages/sedatatools/sedatatools-0.4.5-py2.py3-none-any.whl/sedatatools/tools/connectors/prime.py
@@ -0,0 +1,16 @@
+import configparser
+from sys import platform
+
+
+class Prime:
+    def __init__(self):
+        if platform == 'linux':
+            print('platform linux detected')
+            self.secrets_path = '/home/vedad/prime_secrets'
+        else:
+            self.secrets_path = r'C:\Users\Datasoft\prime_secrets'
+
+        self.config = configparser.ConfigParser()
+        self.config.read(self.secrets_path)
+        self.user_name = self.config.get("prime", "user_name")
+        self.password = self.config.get("prime", "password")
