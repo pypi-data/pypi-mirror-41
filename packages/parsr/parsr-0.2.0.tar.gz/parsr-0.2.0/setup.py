d26c42e905166f11583408e08d4e5cb5b3785675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package parsr/0.2.0/parsr-0.2.0.tar.gz

---
+++ packages/parsr/parsr-0.2.0.tar.gz/parsr-0.2.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+develop = set([
+    'ipython',
+    'pytest',
+    'setuptools',
+    'twine',
+    'wheel',
+])
+
+
+if __name__ == "__main__":
+    setup(
+        name="parsr",
+        version="0.2.0",
+        description="Parsr is a simple parser combinator library in pure python.",
+        long_description=open("README.md").read(),
+        long_description_content_type='text/markdown',
+        url="https://github.com/csams/parsr",
+        author="Chris Sams",
+        author_email="cwsams@gmail.com",
+        packages=find_packages(),
+        package_data={'': ['LICENSE']},
+        license='Apache 2.0',
+        extras_require={
+            'develop': list(develop),
+        },
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.6'
+        ],
+        include_package_data=True
+    )
