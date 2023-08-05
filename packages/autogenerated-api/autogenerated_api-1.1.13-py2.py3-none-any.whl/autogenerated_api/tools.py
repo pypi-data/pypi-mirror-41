d93a4ca3a5368e9586727592cad0264315d095e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:16 +0000

    Add package autogenerated-api/1.1.13/autogenerated_api-1.1.13-py2.py3-none-any.whl

---
+++ packages/autogenerated-api/autogenerated_api-1.1.13-py2.py3-none-any.whl/autogenerated_api/tools.py
@@ -0,0 +1,10 @@
+__author__="RituRaj"
+
+
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 40
+    page_size_query_param = 'page_size'
+    max_page_size = 80
+
