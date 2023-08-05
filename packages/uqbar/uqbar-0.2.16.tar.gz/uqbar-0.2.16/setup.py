eaea1a286f6613132936487d8a1d56a7c8d9b2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package uqbar/0.2.16/uqbar-0.2.16.tar.gz

---
+++ packages/uqbar/uqbar-0.2.16.tar.gz/uqbar-0.2.16/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import pathlib
+
+import setuptools
+
+package_name = "uqbar"
+
+
+def read_version():
+    root_path = pathlib.Path(__file__).parent
+    version_path = root_path / package_name / "_version.py"
+    with version_path.open() as file_pointer:
+        file_contents = file_pointer.read()
+    local_dict = {}
+    exec(file_contents, None, local_dict)
+    return local_dict["__version__"]
+
+
+description = "Tools for building documentation with Sphinx, Graphviz and LaTeX"
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: MacOS",
+    "Operating System :: POSIX",
+    "Programming Language :: Python :: 3.6",
+]
+
+install_requires = [
+    "Sphinx>=1.7.0",
+    "Unidecode>=0.4.21",
+    "black",
+    "flake8",
+    "isort",
+    "pytest-cov",
+    "sphinx-autodoc-typehints>=1.3.0",
+    "sphinx-rtd-theme>=0.4.0",
+]
+
+keywords = ["sphinx", "graphviz", "latex", "documentation"]
+
+with open("README.md", "r") as file_pointer:
+    long_description = file_pointer.read()
+
+version = read_version()
+
+if __name__ == "__main__":
+    setuptools.setup(
+        author="Josiah Wolf Oberholtzer",
+        author_email="josiah.oberholtzer@gmail.com",
+        classifiers=classifiers,
+        description=description,
+        include_package_data=True,
+        install_requires=install_requires,
+        keywords=keywords,
+        long_description=description,
+        name=package_name,
+        packages=[package_name],
+        url="https://github.com/josiah-wolf-oberholtzer/uqbar",
+        version=version,
+        zip_safe=False,
+    )
---
+++ packages/uqbar/uqbar-0.2.16.tar.gz/uqbar-0.2.16/uqbar/sphinx/api.py
@@ -0,0 +1,112 @@
+"""
+Uqbar Sphinx API generation extension.
+
+Install by adding ``'uqbar.sphinx.api'`` to the ``extensions`` list in your
+Sphinx configuration.
+
+This extension provides the following configuration values which correspond to
+the initialization arguments to the :py:class:`uqbar.apis.APIBuilder` class.
+
+- ``uqbar_api_directory_name`` (default: ``api``)
+- ``uqbar_api_document_empty_modules`` (default: ``False``)
+- ``uqbar_api_document_private_members`` (default: ``False``)
+- ``uqbar_api_document_private_modules`` (default: ``False``)
+- ``uqbar_api_member_documenter_classes`` (default: ``None``)
+- ``uqbar_api_module_documenter_class`` (default: ``None``)
+- ``uqbar_api_root_documenter_class`` (default: ``None``)
+- ``uqbar_api_source_paths`` (default: ``[]``)
+- ``uqbar_api_title`` (default: ``API``)
+
+reStructuredText source files will be generated for the modules given by
+``uqbar_api_source_paths`` in the directory ``uqbar_api_directory_name``
+relative to your Sphinx source directory.
+"""
+import importlib
+import pathlib
+import types
+from typing import List  # noqa
+
+import sphinx.application  # type: ignore
+
+import uqbar.apis
+
+
+def on_builder_inited(app: sphinx.application.Sphinx):
+    """
+    Hooks into Sphinx's ``builder-inited`` event.
+
+    Builds out the ReST API source.
+    """
+    config = app.builder.config
+
+    target_directory = (
+        pathlib.Path(app.builder.env.srcdir) / config.uqbar_api_directory_name
+    )
+
+    initial_source_paths: List[str] = []
+    source_paths = config.uqbar_api_source_paths
+    for source_path in source_paths:
+        if isinstance(source_path, types.ModuleType):
+            if hasattr(source_path, "__path__"):
+                initial_source_paths.extend(getattr(source_path, "__path__"))
+            else:
+                initial_source_paths.extend(source_path.__file__)
+            continue
+        try:
+            module = importlib.import_module(source_path)
+            if hasattr(module, "__path__"):
+                initial_source_paths.extend(getattr(module, "__path__"))
+            else:
+                initial_source_paths.append(module.__file__)
+        except ImportError:
+            initial_source_paths.append(source_path)
+
+    root_documenter_class = config.uqbar_api_root_documenter_class
+    if isinstance(root_documenter_class, str):
+        module_name, _, class_name = root_documenter_class.rpartition(".")
+        module = importlib.import_module(module_name)
+        root_documenter_class = getattr(module, class_name)
+
+    module_documenter_class = config.uqbar_api_module_documenter_class
+    if isinstance(module_documenter_class, str):
+        module_name, _, class_name = module_documenter_class.rpartition(".")
+        module = importlib.import_module(module_name)
+        module_documenter_class = getattr(module, class_name)
+
+    member_documenter_classes = config.uqbar_api_member_documenter_classes or []
+    for i, member_documenter_class in enumerate(member_documenter_classes):
+        if isinstance(member_documenter_class, str):
+            module_name, _, class_name = member_documenter_class.rpartition(".")
+            module = importlib.import_module(module_name)
+            member_documenter_classes[i] = getattr(module, class_name)
+    if not member_documenter_classes:
+        member_documenter_classes = None
+
+    api_builder = uqbar.apis.APIBuilder(
+        initial_source_paths=initial_source_paths,
+        target_directory=target_directory,
+        document_empty_modules=config.uqbar_api_document_empty_modules,
+        document_private_members=config.uqbar_api_document_private_members,
+        document_private_modules=config.uqbar_api_document_private_modules,
+        member_documenter_classes=member_documenter_classes,
+        module_documenter_class=module_documenter_class,
+        root_documenter_class=root_documenter_class,
+        title=config.uqbar_api_title,
+    )
+    api_builder()
+
+
+def setup(app: sphinx.application.Sphinx):
+    """
+    Sets up Sphinx extension.
+    """
+    app.connect("builder-inited", on_builder_inited)
+    app.add_config_value("uqbar_api_directory_name", "api", "env")
+    app.add_config_value("uqbar_api_document_empty_modules", False, "env")
+    app.add_config_value("uqbar_api_document_private_members", False, "env")
+    app.add_config_value("uqbar_api_document_private_modules", False, "env")
+    app.add_config_value("uqbar_api_member_documenter_classes", None, "env")
+    app.add_config_value("uqbar_api_module_documenter_class", None, "env")
+    app.add_config_value("uqbar_api_root_documenter_class", None, "env")
+    app.add_config_value("uqbar_api_source_paths", [], "env")
+    app.add_config_value("uqbar_api_title", "API", "env")
