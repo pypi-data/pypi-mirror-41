ca16d01bffcaf0415de6c1cfa51b4532bec9be10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:56 +0000

    Add package guillotina-elasticsearch/3.2.1/guillotina_elasticsearch-3.2.1.tar.gz

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-3.2.1.tar.gz/guillotina_elasticsearch-3.2.1/guillotina_elasticsearch/schema.py
@@ -0,0 +1,89 @@
+from guillotina.component import get_utilities_for
+from guillotina.content import get_all_possible_schemas_for_type
+from guillotina.content import IResourceFactory
+
+import guillotina.directives
+
+
+CATALOG_TYPES = {
+    'searchabletext': {
+        'type': 'text',
+        'index': True
+    },
+    'text': {
+        'type': 'text',
+        'index': True
+    },
+    'keyword': {
+        'type': 'keyword',
+        'index': True
+    },
+    'textkeyword': {
+        'type': 'text',
+        'fields': {
+            'keyword': {
+                'type': 'keyword',
+                'ignore_above': 256
+            }
+        }
+    },
+    'int': {'type': 'integer'},
+    'date': {'type': 'date'},
+    'boolean': {'type': 'boolean'},
+    'binary': {'type': 'binary'},
+    'long': {'type': 'long'},
+    'float': {'type': 'float'},
+    'nested': {'type': 'nested'},
+    'object': {'type': 'object'},
+    'completion': {'type': 'completion'},
+    'path': {
+        'type': 'text',
+        'analyzer': 'path_analyzer'
+    }
+}
+
+
+def get_mappings(schemas=None, schema_info=False):
+
+    if schemas is None:
+        schemas = []
+        for name, _ in get_utilities_for(IResourceFactory):
+            # For each type
+            for schema in get_all_possible_schemas_for_type(name):
+                schemas.append(schema)
+        schemas = set(schemas)
+
+    mappings = {}
+    for schema in schemas:
+        index_fields = guillotina.directives.merged_tagged_value_dict(
+            schema, guillotina.directives.index.key)
+        for field_name, catalog_info in index_fields.items():
+            index_name = catalog_info.get('index_name', field_name)
+            catalog_type = catalog_info.get('type', 'text')
+            field_mapping = catalog_info.get('field_mapping', None)
+            if field_mapping is None:
+                field_mapping = CATALOG_TYPES[catalog_type].copy()
+            if 'store' in catalog_info:
+                field_mapping['store'] = catalog_info['store']
+            if schema_info:
+                if '_schemas' not in field_mapping:
+                    field_mapping['_schemas'] = []
+                if schema.__identifier__ not in field_mapping['_schemas']:
+                    field_mapping['_schemas'].append(schema.__identifier__)
+
+            if index_name in mappings and mappings[index_name] != field_mapping:
+                raise Exception(
+                    f'Index "{index_name}" of schema "{schema}" is trying to '
+                    f'overwrite existing mapping "{mappings[index_name]}" '
+                    f'with "{field_mapping}"'
+                )
+
+            mappings[index_name] = field_mapping
+
+    return {
+        'properties': mappings,
+        'dynamic': False,
+        '_all': {
+            'enabled': False
+        }
+    }
