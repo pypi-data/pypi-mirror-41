0a7c318a0fcc19e6952291dc6ef4a04c35ae0bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package BloomFilterPy/1.0.2/BloomFilterPy-1.0.2-py3-none-any.whl

---
+++ packages/BloomFilterPy/BloomFilterPy-1.0.2-py3-none-any.whl/pybloom/__init__.py
@@ -0,0 +1,14 @@
+import logging
+from logging.handlers import SysLogHandler
+
+name = 'BloomFilterPy'
+__version__ = '1.0.2'
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+handler = SysLogHandler(address='/dev/log')
+formatter = logging.Formatter('%(module)s.py: [%(levelname)s] => %(message)s')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
