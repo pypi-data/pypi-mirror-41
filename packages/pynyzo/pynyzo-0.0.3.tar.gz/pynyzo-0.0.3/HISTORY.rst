9b6d4e6a5008768768061141be66f5c3bbb28db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:33 +0000

    Add package pynyzo/0.0.3/pynyzo-0.0.3.tar.gz

---
+++ packages/pynyzo/pynyzo-0.0.3.tar.gz/pynyzo-0.0.3/HISTORY.rst
@@ -0,0 +1,20 @@
+=======
+History
+=======
+
+0.0.3 (2019-01-26)
+------------------
+
+* Various fixes and more transcriptions.
+
+0.0.2 (2019-01-26)
+------------------
+
+* First working message, StatusRequest17.
+
+
+0.0.1 (2019-01-25)
+------------------
+
+* First release on PyPI.
+
---
+++ packages/pynyzo/pynyzo-0.0.3.tar.gz/pynyzo-0.0.3/pynyzo/messageobject.py
@@ -0,0 +1,36 @@
+"""
+Transcripted from https://github.com/n-y-z-o/nyzoVerifier/blob/master/src/main/java/co/nyzo/verifier/MessageObject.java
+"""
+
+from abc import ABC, abstractmethod
+from pynyzo.helpers import base_app_log
+import json
+
+class MessageObject(ABC):
+
+    __slots__ = ('app_log', )
+
+    def __init__(self, app_log: object=None):
+        self.app_log = base_app_log(app_log)
+
+    @abstractmethod
+    def get_byte_size(self) -> int:
+        # return len(self._bytes)
+        pass
+
+    @abstractmethod
+    def get_bytes(self) -> bytes:
+        #return self._bytes
+        pass
+
+    def to_json(self) -> str:
+        return json.dumps({"Error": "Not implemented"})
+
+
+class EmptyMessageObject(MessageObject):
+
+    def get_byte_size(self) -> int:
+        return 0
+
+    def get_bytes(self) -> bytes:
+        return b''
