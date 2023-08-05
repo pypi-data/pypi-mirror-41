8dd9b1fa63c0b0360df68a77779a20871e8d78ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:35 +0000

    Add package helm-template-generator/2.0/helm-template-generator-2.0.tar.gz

---
+++ packages/helm-template-generator/helm-template-generator-2.0.tar.gz/helm-template-generator-2.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='helm-template-generator',
+    version='2.0',
+    description='Generate helm template files for aether',
+    url='http://github.com/ehealthafrica/ehealh-deployment',
+    author='Oluwafemi Olofintuyi',
+    author_email='devops@ehealthafrica.org',
+    license='MIT',
+    install_requires=['mako'],
+    packages=['helm_template_generator'],
+    package_data={
+        'helm_template_generator': [
+            'templates/*.tmpl.yaml',
+            'templates/*.tmpl.txt',
+            'templates/*.tmpl.tpl']},
+    entry_points={
+                'console_scripts': ['helm-template-generator=helm_template_generator.generator:main'],
+                },
+    zip_safe=False)
