432db4d93ba56ec011c74b9bf3e8957b59194a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:13 +0000

    Add package mtlib/0.0.9/mtlib-0.0.9.tar.gz

---
+++ packages/mtlib/mtlib-0.0.9.tar.gz/mtlib-0.0.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name='mtlib',
+    version='0.0.9',
+    packages=['mtlib',],
+    license='MIT License',
+    author="Eric Wilkison",
+    author_email="ericw@wilkison.com",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://bitbucket.org/ericwilkison/mtlib',
+    install_requires=[
+            "pycoap == 0.0.7",
+            "zeroconf >= 0.21.3"
+        ],
+    extras_require={
+        'test':  [ "asynctest >= 0.12.2" ],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
