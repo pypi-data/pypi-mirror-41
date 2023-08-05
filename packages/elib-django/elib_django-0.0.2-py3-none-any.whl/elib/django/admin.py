941d24bcdb8f9010b40b051fd6c487e831f773ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package elib-django/0.0.2/elib_django-0.0.2-py3-none-any.whl

---
+++ packages/elib-django/elib_django-0.0.2-py3-none-any.whl/elib/django/admin.py
@@ -0,0 +1,25 @@
+#
+# copyright (c) 2019 east301
+#
+# This software is released under the Apache License, Version 2.0.
+# https://opensource.org/licenses/Apache-2.0
+#
+
+from django.contrib import admin
+
+
+def reregister_model_admin(model):
+    def apply(model_admin):
+        admin.site.unregister(model)
+        admin.site.register(model, model_admin)
+        return model_admin
+
+    return apply
+
+
+def short_description(description):
+    def apply(func):
+        func.short_description = description
+        return func
+
+    return apply
