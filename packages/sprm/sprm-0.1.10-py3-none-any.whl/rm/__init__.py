b83ec4c0942033e1b0000f56954331e2bf509536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:05 +0000

    Add package sprm/0.1.10/sprm-0.1.10-py3-none-any.whl

---
+++ packages/sprm/sprm-0.1.10-py3-none-any.whl/rm/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Sven Serneels, Ponalytics
+"""
+
+__name__ = "rm"
+__author__ = "Sven Serneels"
+__license__ = "GNU"
+__date__ = "2019-01-26"
+
---
+++ packages/sprm/sprm-0.1.10-py3-none-any.whl/sprm/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 12:17:17 2018
+
+@author: Sven Serneels, Ponalytics
+"""
+
+__name__ = "sprm"
+__author__ = "Sven Serneels"
+__license__ = "GNU"
+__version__ = "0.1.10"
+__date__ = "2019-01-26"
+
+
