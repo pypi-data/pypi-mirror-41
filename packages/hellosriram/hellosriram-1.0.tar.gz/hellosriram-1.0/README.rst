2bc95fc21eac7f43b9a1b10e225b08710c778d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package hellosriram/1.0/hellosriram-1.0.tar.gz

---
+++ packages/hellosriram/hellosriram-1.0.tar.gz/hellosriram-1.0/README.rst
@@ -0,0 +1,20 @@
+
+Hello Sriram
+------------
+
+* GitHub: https://github.com/sriramsreedhar/hellosriram.git
+* PyPI: https://pypi.org/project/hellosriram
+* Free and open source software: `MIT License`
+
+
+Features
+--------
+
+* Cross-platform: Windows, Mac, and Linux are officially supported.
+
+* Works with Python 2.7, 3.4, 3.5, 3.6, and PyPy. 
+
+
+
+
+
---
+++ packages/hellosriram/hellosriram-1.0.tar.gz/hellosriram-1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='hellosriram',  
+     version='1.0',
+     scripts=['hellosriram'] ,
+     author="Sriram Sreedhar",
+     author_email="sriramsreedhar003@gmail.com",
+     description="Hello Sriram",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/sriramsreedhar/hellosriram.git",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 2.7",
+         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
