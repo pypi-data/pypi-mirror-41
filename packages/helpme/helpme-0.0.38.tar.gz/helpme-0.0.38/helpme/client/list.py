ab4e2664d3d8ab6c2dce93d2d21ae20e72929ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package helpme/0.0.38/helpme-0.0.38.tar.gz

---
+++ packages/helpme/helpme-0.0.38.tar.gz/helpme-0.0.38/helpme/client/list.py
@@ -0,0 +1,33 @@
+'''
+
+Copyright (C) 2018-2019 Vanessa Sochat.
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
+from helpme.logger import bot
+from helpme.defaults import HELPME_HELPERS
+import sys
+import pwd
+import os
+
+
+def main(args, extra):
+    '''print the listing of helpers installed, defined by the HELPME_HELPERS
+       variable in the default settings flie, and then exit.
+    '''    
+    bot.info('Helpers Installed:')
+    print('\n'.join(HELPME_HELPERS))
+    
