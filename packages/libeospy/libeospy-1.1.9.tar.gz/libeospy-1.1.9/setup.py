85832010dc42da1f822c7674dccd511402e51185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:11 +0000

    Add package libeospy/1.1.9/libeospy-1.1.9.tar.gz

---
+++ packages/libeospy/libeospy-1.1.9.tar.gz/libeospy-1.1.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='libeospy',
+      version=os.getenv('BUILD_VERSION', '0.0.1'),
+      description='Python library for the eos.io REST API',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='deck',
+      author_email='deck@eosnewyork.io',
+      url='https://github.com/eosnewyork/eospy',
+      packages=find_packages(),
+      test_suite = 'nose.collector',
+      install_requires=[
+          'requests',
+          'base58',
+          'ecdsa',
+          'colander',
+          'pytz',
+          'six',
+          'pyyaml',
+      ],
+      entry_points = {
+          'console_scripts' :[
+              'validate_chain = eospy.command_line:validate_chain',
+              'pycleos = eospy.command_line:cleos',
+              'pytesteos = eospy.command_line:testeos',
+          ],
+      }
+)
