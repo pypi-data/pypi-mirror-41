c1292720c0c51533e994b9dbde90db3b78aa68bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package play-dynamodb/0.0.2/play_dynamodb-0.0.2-py2.py3-none-any.whl

---
+++ packages/play-dynamodb/play_dynamodb-0.0.2-py2.py3-none-any.whl/play_dynamodb/providers.py
@@ -0,0 +1,71 @@
+import logging
+import boto3
+from pytest_play.providers import BaseProvider
+
+
+class DynamoDBProvider(BaseProvider):
+    """ DynamoDB provider """
+
+    ALLOWED_METHODS = [
+        'get_item',
+        'delete_item',
+        'put_item',
+        'query',
+        'scan',
+        'update_item',
+    ]
+
+    def __init__(self, engine):
+        super(DynamoDBProvider, self).__init__(engine)
+        self.logger = logging.getLogger()
+
+    def command_dynamodb(self, command, **kwargs):
+        method = command['method']
+        if method in self.ALLOWED_METHODS:
+            dynamodb = boto3.resource(
+                'dynamodb',
+                **command['connection'])
+            table_name = command['TableName']
+            table = dynamodb.Table(table_name)
+            method = getattr(table, method)
+            results = method(**command['parameters'])
+            try:
+                self._make_variable(command, results=results)
+                self._make_assertion(command, results=results)
+            except Exception as e:
+                self.logger.exception(
+                    'Exception for command %r',
+                    command,
+                    e)
+                raise e
+        else:
+            raise ValueError("method not allowed", method)
+
+    def _make_assertion(self, command, **kwargs):
+        """ Make an assertion based on python
+            expression against kwargs
+        """
+        assertion = command.get('assertion', None)
+        if assertion:
+            self.engine.execute_command(
+                {'provider': 'python',
+                 'type': 'assert',
+                 'expression': assertion
+                 },
+                **kwargs,
+            )
+
+    def _make_variable(self, command, **kwargs):
+        """ Make a variable based on python
+            expression against kwargs
+        """
+        expression = command.get('variable_expression', None)
+        if expression:
+            self.engine.execute_command(
+                {'provider': 'python',
+                 'type': 'store_variable',
+                 'name': command['variable'],
+                 'expression': expression
+                 },
+                **kwargs,
+            )
