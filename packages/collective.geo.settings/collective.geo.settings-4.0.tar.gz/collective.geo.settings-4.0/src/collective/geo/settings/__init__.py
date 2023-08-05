5effb990494929253ca1c032b0fc0965cdcb41b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package collective.geo.settings/4.0/collective.geo.settings-4.0.tar.gz

---
+++ packages/collective.geo.settings/collective.geo.settings-4.0.tar.gz/collective.geo.settings-4.0/src/collective/geo/settings/__init__.py
@@ -0,0 +1,33 @@
+from pkg_resources import get_distribution
+from zope.i18nmessageid import MessageFactory
+import config
+
+
+GeoSettingsMessageFactory = MessageFactory(config.PROJECTNAME)
+
+_ = GeoSettingsMessageFactory
+
+DISPLAY_PROPERTIES = [('id', _(u'ID')),
+                      ('Title', _(u'Title')),
+                      ('Description', _(u'Description')),
+                      ('Type', _(u'Type')),
+                      ('Subject', _(u'Subject')),
+                      ('getLocation', _(u'Content Location')),
+                      ('ModificationDate', _(u'Last Modified Date')),
+                      ('CreationDate', _(u'Creation Date')),
+                      ('EffectiveDate', _(u'Effective Date')),
+                      ('ExpirationDate', _(u'Expiration Date')),
+                      ('start', _('Start Date')),
+                      ('end', _('End Date')),
+                      ('listCreators', _(u'Creators')),
+                      ('Contributors', _(u'Contributors')),
+                      ('Rights', _(u'Rights Statement'))]
+
+DISPLAY_PROPERTIES_DATES = ('ModificationDate',
+                         'CreationDate',
+                         'EffectiveDate',
+                         'ExpirationDate',
+                         'start', 'end')
+
+
+IS_PLONE_5 = get_distribution('Plone').version >= '5'
