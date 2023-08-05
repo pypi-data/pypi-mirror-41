7543efbe9488440ba3a275a4de3ce831034a37f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:07 +0000

    Add package hwtypes/0.0.1/hwtypes-0.0.1.tar.gz

---
+++ packages/hwtypes/hwtypes-0.0.1.tar.gz/hwtypes-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='hwtypes',
+    version='0.0.1',
+    description='Python implementations of hardware types such as bit vectors',
+    scripts=[],
+    packages=[
+        "hwtypes",
+    ],
+    license='BSD License',
+    url='https://github.com/leonardt/hwtypes',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    python_requires='>=3.6'
+)
