827d1f35bcb5ce8353ab8fab891cf0a231258863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:04 +0000

    Add package django-choices/1.6.2/django-choices-1.6.2.tar.gz

---
+++ packages/django-choices/django-choices-1.6.2.tar.gz/django-choices-1.6.2/setup.py
@@ -0,0 +1,41 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+with open(path.join(path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name="django-choices",
+    version='1.6.2',
+    license="MIT",
+    description="Sanity for the django choices functionality.",
+    long_description=readme,
+    install_requires=['Django>=1.8'],
+    test_suite='runtests.get_suite',
+    url="https://github.com/bigjason/django-choices",
+    author="Jason Webb",
+    author_email="bigjasonwebb@gmail.com,sergeimaertens@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ]
+)
