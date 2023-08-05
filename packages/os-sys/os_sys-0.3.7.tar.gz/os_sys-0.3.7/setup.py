ae812610d97bdb696dee54e183b8809b2cdfa1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package os-sys/0.3.7/os_sys-0.3.7.tar.gz

---
+++ packages/os-sys/os_sys-0.3.7.tar.gz/os_sys-0.3.7/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+import setuptools as s
+try:
+    import ready
+except Exception:
+    pass
+long_description = 'if you try import os_sys and it not work you can try import\
+pack that while work then i know the problem and i working on it\
+but this is my solution for now thanks\n\n\n'
+with open("README.md", "r") as fh:
+    long_description.join(fh.read())
+
+
+long_description = long_description.replace('evry', 'every')
+setuptools.setup(
+    name="os_sys",
+    version="0.3.7",
+    author="Matthijs labots",
+    
+    author_email="libs.python@gmail.com",
+    description="a big plus lib for more functions to use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://python-libs-com.webnode.nl/",
+    python_requires='>=3',
+    include_package_data=True,
+    packages=['os_sys', 'os_sys.test', 'os_sys.programs', 'os_sys.data_files',
+              'pack', 'pack.test', 'pack.programs', 'pack.data_files',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    project_urls={
+        'all files': 'https://github.com/Matthijs990/os_sys',
+        'Downloads': 'https://python-libs-com.webnode.nl/downloads/',
+        'become a member': 'https://python-libs-com.webnode.nl/user-registration/',
+        'download all files': 'https://github.com/Matthijs990/os_sys.git',
+        'want to help': 'https://github.com/Matthijs990/os_sys/tree/master/do%20you%20want%20to%20help',
+        'startpage': 'https://pypi.org/project/os-sys/',
+    },
+)
+
+print('you need to typ:\n\
+    from os_sys import os_sys or:\n\
+    from os_sys import fail or:\n\
+    from os_sys import system or:\n\
+    from os_sys import modules or:\n\
+    from os_sys import wifi or:\n\
+    from os_sys import *')
