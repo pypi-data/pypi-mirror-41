7b1ffce16c3b880a255fe395a20e0942812b6d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:48 +0000

    Add package lbCVMFSchecker/0.0.2/lbCVMFSchecker-0.0.2.tar.gz

---
+++ packages/lbCVMFSchecker/lbCVMFSchecker-0.0.2.tar.gz/lbCVMFSchecker-0.0.2/lbCVMFSchecker/LbCvmfsChecker.py
@@ -0,0 +1,28 @@
+###############################################################################
+# (c) Copyright 2016 CERN                                                     #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+'''
+
+Module that allows to load all the checker Plugins
+
+:author: Stefan-Gabriel CHITIC
+'''
+import logging
+from PluginManager import LbPluginManager
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format="[%(asctime)s] "
+                               "%(levelname)-8s:"
+                               "%(message)s")
+    logging.getLogger().setLevel(logging.INFO)
+
+    l = LbPluginManager()
+    l.executeAll()
