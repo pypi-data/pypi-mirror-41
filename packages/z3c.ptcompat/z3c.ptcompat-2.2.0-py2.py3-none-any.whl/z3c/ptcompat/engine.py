9fd34f2858d20a9da5e67bdf0d5ec604aa6c4c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:02 +0000

    Add package z3c.ptcompat/2.2.0/z3c.ptcompat-2.2.0-py2.py3-none-any.whl

---
+++ packages/z3c.ptcompat/z3c.ptcompat-2.2.0-py2.py3-none-any.whl/z3c/ptcompat/engine.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Engine
+"""
+from zope.interface import implementer
+from zope.interface import provider
+from zope.pagetemplate.interfaces import IPageTemplateEngine
+from zope.pagetemplate.interfaces import IPageTemplateProgram
+
+from z3c.pt.pagetemplate import PageTemplate as ChameleonPageTemplate
+from chameleon.tal import RepeatDict
+
+
+# Py3: Fix Chameleon's RepeatDict, which cannot be adapted. Sigh.
+class TraversableRepeatDict(RepeatDict):
+    __providedBy__ = None
+    __provides__ = None
+
+    def __conform__(self, iface):
+        return None
+
+
+@implementer(IPageTemplateProgram)
+@provider(IPageTemplateEngine)
+class Program(object):
+    def __init__(self, template):
+        self.template = template
+
+    def __call__(self, context, macros, tal=True, **options):
+        if not tal:
+            return self.template.body
+
+        context.vars["repeat"] = TraversableRepeatDict(context.repeat_vars)
+
+        return self.template.render(**context.vars)
+
+    @classmethod
+    def cook(cls, source_file, text, engine, content_type):
+        # Chameleon doesn't like to have a 'filename' of None;
+        # see https://github.com/zopefoundation/z3c.ptcompat/issues/2
+        template = ChameleonPageTemplate(
+            text, filename=source_file or "<string>", keep_body=True
+        )
+
+        return cls(template), template.macros
---
+++ packages/z3c.ptcompat/z3c.ptcompat-2.2.0-py2.py3-none-any.whl/z3c/ptcompat/tests.py
@@ -0,0 +1,31 @@
+import unittest
+
+from zope.pagetemplate.tests import test_htmltests as reference
+
+
+class HTMLTests(reference.HTMLTests):
+    def setUp(self):
+        import z3c.ptcompat
+        import zope.component.testing
+        import zope.configuration.xmlconfig
+
+        zope.component.testing.setUp(self)
+        zope.configuration.xmlconfig.XMLConfig(
+            "configure.zcml", z3c.ptcompat
+        )()
+
+        super(HTMLTests, self).setUp()
+
+
+class TestProgram(unittest.TestCase):
+    def _makeOne(self, *args):
+        from z3c.ptcompat.engine import Program
+
+        return Program.cook(*args)
+
+    def test_call_with_no_tal_returns_template_body(self):
+        body = "<html />"
+        program, _ = self._makeOne(None, body, None, None)
+
+        result = program(None, None, tal=False)
+        self.assertEqual(result, body)
