e79e746ecfe6dc0cd29b54c0999607746a94a16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package FinanceScraper/0.1.1/FinanceScraper-0.1.1.tar.gz

---
+++ packages/FinanceScraper/FinanceScraper-0.1.1.tar.gz/FinanceScraper-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setup(
+    name='FinanceScraper',
+    version='0.1.1',
+    author='Lukas Budach',
+    author_email='lukas.budach@student.hpi.de',
+    description='An up-to-date web scraper providing financial data from various sources',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['financescraper'],
+    scripts=['bin/example.py'],
+    url="https://github.com/LukasBudach/FinanceScraper",
+    include_package_data=True,
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
