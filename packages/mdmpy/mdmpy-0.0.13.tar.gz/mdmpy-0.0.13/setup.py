9179c1629fde60e12b98bf84bacf14ca61400a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package mdmpy/0.0.13/mdmpy-0.0.13.tar.gz

---
+++ packages/mdmpy/mdmpy-0.0.13.tar.gz/mdmpy-0.0.13/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mdmpy",
+    version="0.0.13",
+    author="MDM Py Authors",
+    author_email="3600019+justanothergithubber@users.noreply.github.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justanothergithubber/mdmpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+   'numpy',
+   'scipy',
+   'pyomo'
+    ],
+)
