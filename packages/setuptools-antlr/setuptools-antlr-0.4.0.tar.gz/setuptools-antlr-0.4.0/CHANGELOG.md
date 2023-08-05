456b4dd70ab3236a9c187a9e1e9c556f5f878551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:51 +0000

    Add package setuptools-antlr/0.4.0/setuptools-antlr-0.4.0.tar.gz

---
+++ packages/setuptools-antlr/setuptools-antlr-0.4.0.tar.gz/setuptools-antlr-0.4.0/CHANGELOG.md
@@ -0,0 +1,39 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project
+adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.4.0] - 2019-01-27
+### Added
+- Python 3.7 support.
+- Equality comparison and hashing of grammars.
+### Removed
+- Generation of parent Python packages.
+
+## [0.3.0] - 2018-07-29
+### Changed
+- Folder structure of project.
+### Removed
+- Filtering of root-level grammars.
+
+## [0.2.2] - 2018-07-25
+### Fixed
+- Faulty detection of grammar rule as import statement.
+
+## [0.2.1] - 2018-06-15
+### Fixed
+- Faulty detection of import statements.
+
+## [0.2.0] - 2018-06-02
+### Changed
+- Use package source directory as default output directory.
+
+[Unreleased]: https://github.com/ferraith/setuptools-antlr/compare/0.4.0...HEAD
+[0.4.0]: https://github.com/ferraith/setuptools-antlr/compare/0.3.0...0.4.0
+[0.3.0]: https://github.com/ferraith/setuptools-antlr/compare/0.2.2...0.3.0
+[0.2.2]: https://github.com/ferraith/setuptools-antlr/compare/0.2.1...0.2.2
+[0.2.1]: https://github.com/ferraith/setuptools-antlr/compare/0.2.0...0.2.1
+[0.2.0]: https://github.com/ferraith/setuptools-antlr/compare/0.1.2...0.2.0
---
+++ packages/setuptools-antlr/setuptools-antlr-0.4.0.tar.gz/setuptools-antlr-0.4.0/setup.py
@@ -0,0 +1,48 @@
+"""Setup script for setuptools-antlr."""
+import sys
+
+import setuptools
+
+# need to guard script here due to reentrance while testing multiprocessing:
+if __name__ == '__main__':
+    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+    pytest_runner_opt = ['pytest-runner>=2.0,<3dev'] if needs_pytest else []
+
+    needs_pylint = {'lint'}.intersection(sys.argv)
+    setuptools_lint_opt = ['setuptools-lint>=0.4,<0.5dev'] if needs_pylint else []
+
+    needs_flake8 = {'flake8'}.intersection(sys.argv)
+    flake8_opt = ['flake8>=2.0,<3dev', 'flake8-import-order>=0.9,<0.10dev',
+                  'flake8-todo>=0.5,<0.6dev', 'pep8-naming>=0.3,<0.4dev'] if needs_flake8 else []
+
+    setuptools.setup(
+        name='setuptools-antlr',
+        version='0.4.0',
+        packages=setuptools.find_packages(),
+        package_data={'setuptools_antlr': ['lib/antlr-4.7.1-complete.jar', 'lib/LICENSE.txt']},
+        entry_points={
+            'distutils.commands': [
+                'antlr = setuptools_antlr.command:AntlrCommand'
+            ]
+        },
+        python_requires='>=3.5',
+        tests_require=['pytest>=3.6'],
+        setup_requires=pytest_runner_opt + setuptools_lint_opt + flake8_opt,
+        url='https://github.com/ferraith/setuptools-antlr',
+        license='MIT',
+        author='Andreas Schmidl',
+        author_email='Andreas.Schmidl@gmail.com',
+        description='Setuptools command for generating ANTLR based parsers.',
+        long_description=open('README.rst').read(),
+        platforms=['any'],
+        keywords='antlr setuptools dsl',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ]
+    )
