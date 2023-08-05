1424707a8636010e222c24480b4897ca2c340773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package mylibfahad/0.1/mylibfahad-0.1.tar.gz

---
+++ packages/mylibfahad/mylibfahad-0.1.tar.gz/mylibfahad-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mylibfahad',
+      version='0.1',
+      description='Testing to creat a library',
+      url='http://dont-have-link.com/no-link',
+      author='fahad.shawal',
+      author_email='fahadshawal@gmail.com',
+      license='MIT',
+      packages=['mylib'],
+      zip_safe=False)
