8622542e424d127af2e9f8c4627e067a669b7407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package blotto/0.0.1/blotto-0.0.1.tar.gz

---
+++ packages/blotto/blotto-0.0.1.tar.gz/blotto-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blotto",
+    version="0.0.1",
+    author="Jack Treanor",
+    author_email="jftreanor@gmail.com",
+    description="Computational framework for generating successful blotto entries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jftreanor/blotto",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
