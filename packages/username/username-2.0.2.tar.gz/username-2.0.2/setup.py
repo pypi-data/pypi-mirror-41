f02e7f2393ce8ff7e94108dfd2ef8a437c99d1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package username/2.0.2/username-2.0.2.tar.gz

---
+++ packages/username/username-2.0.2.tar.gz/username-2.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import sys
+
+with open("readme.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name = "username",
+    packages = ["username"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "2.0.2",
+    description = "Get the current user name",
+    author = "Carlos Abraham",
+    author_email = "abraham@abranhe.com",
+    url = "https://projects.abranhe.com/username",
+    classifiers=(
+			'Programming Language :: Python',
+			'Natural Language :: English',
+			'Environment :: Plugins',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.2',
+			'Programming Language :: Python :: 3.3',
+			'Programming Language :: Python :: 3.4',
+			'Operating System :: OS Independent',
+			'Operating System :: MacOS',
+			'Operating System :: Unix',
+    ),
+    project_urls={
+        'Source': 'https://github.com/abranhe/username',
+    },
+    keywords='cli command-line-interface python bash-tool username',
+    include_package_data=True
+)
