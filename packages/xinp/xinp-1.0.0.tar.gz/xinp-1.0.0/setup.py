ed09906e0407c7094f5265466b7d205e44e5e74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:46 +0000

    Add package xinp/1.0.0/xinp-1.0.0.tar.gz

---
+++ packages/xinp/xinp-1.0.0.tar.gz/xinp-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='xinp',
+    version='1.0.0',
+    py_modules=['xinp'],
+    author='wuxuexin',
+    author_email='wuxuexinlwx@163.com',
+    description='LOVE'
+    )
---
+++ packages/xinp/xinp-1.0.0.tar.gz/xinp-1.0.0/xinp.py
@@ -0,0 +1,5 @@
+def print_a(wuxuexin):
+    if wuxuexin=='gun':
+        print('I hate you!!!')
+    else:
+        print('I love you.')
