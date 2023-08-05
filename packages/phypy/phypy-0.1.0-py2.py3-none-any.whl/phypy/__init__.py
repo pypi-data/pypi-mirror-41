d20764a7276cd6c24dd44641cefa25a37b5ce321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:55 +0000

    Add package phypy/0.1.0/phypy-0.1.0-py2.py3-none-any.whl

---
+++ packages/phypy/phypy-0.1.0-py2.py3-none-any.whl/phypy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for phypy."""
+
+__author__ = """Chance Tarver"""
+__email__ = 'tarver.chance@gmail.com'
+__version__ = '0.1.0'
+
+from . import analog
+
+__all__ = ['analog']
