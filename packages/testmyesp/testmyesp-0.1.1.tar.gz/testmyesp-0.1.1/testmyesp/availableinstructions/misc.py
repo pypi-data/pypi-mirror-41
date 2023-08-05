c16acb6e8d92427493d95d5c9c400faa95472b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:31 +0000

    Add package testmyesp/0.1.1/testmyesp-0.1.1.tar.gz

---
+++ packages/testmyesp/testmyesp-0.1.1.tar.gz/testmyesp-0.1.1/testmyesp/availableinstructions/misc.py
@@ -0,0 +1,26 @@
+
+import time
+
+from colibris import schemas
+from colibris import settings
+
+from testmyesp.instructions import BaseInstruction, register_instruction
+
+
+@register_instruction
+class Sleep(BaseInstruction):
+    NAME = 'sleep'
+
+    def __init__(self, duration):
+        self.duration = duration
+
+        super().__init__()
+
+    def execute(self):
+        self.logger.debug('going to sleep for %d milliseconds', self.duration)
+        time.sleep(self.duration / 1000.0)
+        self.logger.debug('woke up')
+
+    class Schema(schemas.Schema):
+        duration = schemas.fields.Integer(required=True,
+                                          validate=schemas.validate.Range(1, settings.MAX_JOB_TIME * 1000))
