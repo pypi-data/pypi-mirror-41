97e17101e1373bb60bca129231d7d896c255e1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:01 +0000

    Add package LbEnv/0.1.5/LbEnv-0.1.5.tar.gz

---
+++ packages/LbEnv/LbEnv-0.1.5.tar.gz/LbEnv-0.1.5/{{cookiecutter.dest_dir}}/LbEnv-{{cookiecutter.scripts_flavour}}.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+###############################################################################
+# (c) Copyright 2018 CERN                                                     #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+if [ -z "$LBENV_SOURCED" ] ; then
+  _host_os=$({{cookiecutter.target_dir}}/host_os)
+
+  if [ -e {{cookiecutter.lbenv_root}}/$_host_os/bin/activate ] ; then
+    source {{cookiecutter.lbenv_root}}/$_host_os/bin/activate
+    eval $(python -m LbEnv --sh --siteroot {{cookiecutter.siteroot}} "$@")
+  else
+    echo "Platform not supported ($_host_os)"
+  fi
+  unset _host_os
+else
+  echo "LHCb environment already set up"
+fi
