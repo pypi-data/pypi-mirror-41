8abb58f395087c785a145fda87fb8e2b0c8ec608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:26 +0000

    Add package Transcrypt/3.7.16/Transcrypt-3.7.16.tar.gz

---
+++ packages/Transcrypt/Transcrypt-3.7.16.tar.gz/Transcrypt-3.7.16/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+sys.path.append ('transcrypt/modules/org/transcrypt')
+
+from setuptools import setup
+
+def read (*paths):
+	with open (os.path.join (*paths), 'r') as aFile:
+		return aFile.read()
+
+setup (
+	name = 'Transcrypt',
+	version = '3.7.16',
+	description = 'Python to JavaScript transpiler, supporting multiple inheritance and generating lean, highly readable code',
+	long_description = (
+		read ('README.rst')
+	),
+	keywords = ['python', 'javascript', 'transpiler', 'compiler', 'browser', 'web', 'multiple inheritance', 'transcrypt', 'django'],
+	url = 'http://www.transcrypt.org',	
+	license = 'Apache 2.0',
+	author = 'Jacques de Hooge',
+	author_email = 'jacques.de.hooge@qquick.org',
+	packages = ['transcrypt'],
+	install_requires = [
+		'mypy'
+	],
+	entry_points = {
+		'console_scripts': [
+			'transcrypt = transcrypt.__main__:main'
+		]
+	},
+	include_package_data = True,
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: Apache Software License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.7',
+	],
+)
