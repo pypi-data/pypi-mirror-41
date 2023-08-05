7fea1386b6f848d217b99358da650020925d62d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:16 +0000

    Add package microcache/1.0/microcache-1.0-py2.py3-none-any.whl

---
+++ packages/microcache/microcache-1.0-py2.py3-none-any.whl/microcache/_version.py
@@ -0,0 +1,5 @@
+__version__ = '1.0'
+__version_info__ = '.'.split(__version__)
+for i in range(len(__version_info__)):
+    if __version_info__[i].isdigit():
+        __version_info__[i] = int(__version_info__[i])
