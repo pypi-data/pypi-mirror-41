2400a7d6e721d9ba641105bdd0a94ac746227e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:11 +0000

    Add package epimetheus/0.3.0/epimetheus-0.3.0.tar.gz

---
+++ packages/epimetheus/epimetheus-0.3.0.tar.gz/epimetheus-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['epimetheus']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=18.2,<19.0', 'cached-property>=1.5,<2.0', 'sortedcontainers>=2.1,<3.0']
+
+setup_kwargs = {
+    'name': 'epimetheus',
+    'version': '0.3.0',
+    'description': 'Simplified prometheus client library',
+    'long_description': None,
+    'author': 'neumond',
+    'author_email': 'knifeslaughter@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
