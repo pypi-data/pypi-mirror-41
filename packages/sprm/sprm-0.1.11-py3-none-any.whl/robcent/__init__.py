a2e77b1caa4ec548fb53bd133d31ca372b530089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package sprm/0.1.11/sprm-0.1.11-py3-none-any.whl

---
+++ packages/sprm/sprm-0.1.11-py3-none-any.whl/robcent/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Sven Serneels, Ponalytics
+"""
+
+__name__ = "robcent"
+__author__ = "Sven Serneels"
+__license__ = "GNU"
+__version__ = "0.1.11"
+__date__ = "2019-01-26"
+
