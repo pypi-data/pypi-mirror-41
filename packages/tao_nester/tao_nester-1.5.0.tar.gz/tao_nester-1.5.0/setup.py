04c8f63a2f3f1f83d74b80ce6b66b228b6de3d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:50 +0000

    Add package tao_nester/1.5.0/tao_nester-1.5.0.tar.gz

---
+++ packages/tao_nester/tao_nester-1.5.0.tar.gz/tao_nester-1.5.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='tao_nester',
+    version      ='1.5.0',
+    py_modules   =['nester123'],
+    author_email ='dyu0104@163.com',
+    url          ='http://www.headfirstlabs.com',
+    description  ='A simple printer of nested lists',
+)
