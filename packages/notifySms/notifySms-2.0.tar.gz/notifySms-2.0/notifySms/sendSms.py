9265fdcbb33dc9dc7b38ebf2a08a3fe8d26ba53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package notifySms/2.0/notifySms-2.0.tar.gz

---
+++ packages/notifySms/notifySms-2.0.tar.gz/notifySms-2.0/notifySms/sendSms.py
@@ -0,0 +1,110 @@
+'''
+    This program helps to send sms notification to specified mobile.
+
+    usage :
+    1. Using Program to send sms
+        python sendSms.py [<Message needs to be sent>]
+    2. Using module in other script to nofiy via sms
+       from sendSms import send_sms
+       send_sms(<msg>,[<To Number>])
+'''
+import sys
+import json
+import logging
+import base64
+import platform
+import os
+from requests.exceptions import ConnectionError
+import requests
+import json
+
+way2sms = False
+logging.basicConfig(level=logging.ERROR, format=" [%(asctime)s][%(levelname)s] %(message)s")
+
+# get request
+def send_way2sms(phoneNo, textMessage):
+  reqUrl = 'http://www.way2sms.com/api/v1/sendCampaign'
+  req_params = {
+  'apikey':os.environ['apikey'],
+  'secret':os.environ['secret'],
+  'usetype': "stage",
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':phoneNo
+  }
+  return requests.post(reqUrl, req_params).text
+
+CFGFILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+logging.debug("Loading configuration file {} ".format(CFGFILE))
+
+try:
+    CONFIG = json.loads(open(CFGFILE).read())
+    logging.debug(CONFIG)
+except IOError:
+    logging.critical("Configuration file not found! \
+    Please make sure config.json file is in same directory")
+    sys.exit(1)
+
+def send_twilio(to,body):
+    from twilio.rest import Client
+    from twilio.base.exceptions import TwilioRestException
+    from twilio.http.http_client import TwilioHttpClient
+    client = "" #Client(base64.b64decode(CONFIG['account']), base64.b64decode(CONFIG['token']))
+    try:
+        proxy_client = TwilioHttpClient()
+        # assuming your proxy is available via the standard env var https_proxy:
+        ## (this is the case on pythonanywhere)
+        proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+        client = Client(base64.b64decode(CONFIG['account']), base64.b64decode(CONFIG['token']), http_client=proxy_client)
+    except KeyError:
+        try:
+            client = Client(base64.b64decode(CONFIG['account']), base64.b64decode(CONFIG['token']))
+            return client.messages.create(to=to, from_=CONFIG['from'], body=body)
+        except TypeError:
+            logging.error("Please check your config file as properly base64 encoded content")
+        except ConnectionError:
+            logging.error("Opps !! Check you Internet connection. \n{}".format(sys.exc_info()))
+        except TwilioRestException:
+            logging.error("Opps!! Check your credentials in config file : \
+            {} or server issue \n{}".format(CFGFILE, sys.exc_info()))
+        except TypeError:
+            logging.error("Please check your config file as properly base64 encoded content")
+
+
+def send_sms(msg_body, to_num=base64.b64decode(CONFIG['to_default'])):
+    '''
+    This function is used to send message with give mobile number and message
+    '''
+    try:
+        logging.debug("To : {}, From : {}, Message is : {}".format(to_num, CONFIG['from'], msg_body))
+        if way2sms == True:
+            response = send_way2sms(to_num[3:],msg_body)
+        else:
+            response = send_twilio(to_num,msg_body)
+        logging.info("Response: " + str(response))
+    except:
+        logging.error("Something unexpected happend! Check error \n{}".format(sys.exc_info()))
+    else:
+        logging.info("Message has been sent sucessfully")
+
+def usage():
+    ''' This is the help function to print usage
+
+    This program helps to send sms notification to specified mobile.
+
+    usage :
+    1. Using Program to send sms
+        python sendSms.py [<Message needs to be sent>]
+    2. Using module in other script to nofiy via sms
+       from sendSms import send_sms
+       send_sms(<msg>,[<To Number>])
+    '''
+
+if __name__ == '__main__':
+    logging.debug("Inside main function")
+    msg = "Default msg : " + str(platform.uname())
+    if len(sys.argv) > 1:
+        msg = ' '.join(sys.argv[1:])
+    else:
+        usage()
+    send_sms(msg)
---
+++ packages/notifySms/notifySms-2.0.tar.gz/notifySms-2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="notifySms",
+    version="2.0",
+    author="Kamal Mansata",
+    author_email="kamalmansata@gmail.com",
+    description="Package to send sms notification to mobile",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kamalmansata/notfiy",
+    download_url="https://github.com/kamalmansata/notfiy/archive/2.0.tar.gz",
+    install_requires=['requests','twilio'],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
