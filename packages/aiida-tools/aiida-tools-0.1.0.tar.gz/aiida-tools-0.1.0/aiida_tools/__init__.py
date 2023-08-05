550c93c53e652de9f221909d059a956453fd8c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package aiida-tools/0.1.0/aiida-tools-0.1.0.tar.gz

---
+++ packages/aiida-tools/aiida-tools-0.1.0.tar.gz/aiida-tools-0.1.0/aiida_tools/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+
+"""
+This module contains general helper functions to aid in developing AiiDA calculations and workchains.
+"""
+
+__version__ = '0.1.0'
+
+from ._input_validator import get_input_validator
+from ._check_workchain_step import *
+from . import workchain_inputs
+
+__all__ = ['workchain_inputs'] + _input_validator.__all__ + _check_workchain_step.__all__
