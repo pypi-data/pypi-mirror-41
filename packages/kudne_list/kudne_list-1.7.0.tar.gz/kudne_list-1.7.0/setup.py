831e918b43b79fb42833b83cd8b7db8ee0d4b1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package kudne_list/1.7.0/kudne_list-1.7.0.tar.gz

---
+++ packages/kudne_list/kudne_list-1.7.0.tar.gz/kudne_list-1.7.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name = 'kudne_list',
+      version = '1.7.0',
+      py_modules = ['kudne_list'],
+      author = 'subba',
+      email = 'anupam.subba27@gmail.com',
+      description = 'A simple tabulation of lists',
+     )
