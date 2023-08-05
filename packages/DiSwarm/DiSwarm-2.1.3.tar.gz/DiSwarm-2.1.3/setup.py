a155097e6ee6d4c164cbea6bd3560ddf2fbe67cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package DiSwarm/2.1.3/DiSwarm-2.1.3.tar.gz

---
+++ packages/DiSwarm/DiSwarm-2.1.3.tar.gz/DiSwarm-2.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DiSwarm",
+    version="2.1.3",
+    author="iTecX",
+    author_email="matteovh@gmail.com",
+    description="Discord-based Swarm Protocol Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iTecAI/DiSwarm",
+    py_modules=['keydet'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['discord','cryptography'],
+    include_package_data = True,
+    package_data={'\\': ['keydet.pyc']},
+)
