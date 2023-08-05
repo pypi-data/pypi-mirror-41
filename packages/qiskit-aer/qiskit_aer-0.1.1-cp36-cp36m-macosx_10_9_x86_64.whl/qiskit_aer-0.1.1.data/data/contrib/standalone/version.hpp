63cdf1dcff2dd2d1bf404f8e17c2ab241b2176bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:10 +0000

    Add package qiskit-aer/0.1.1/qiskit_aer-0.1.1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/qiskit-aer/qiskit_aer-0.1.1-cp36-cp36m-macosx_10_9_x86_64.whl/qiskit_aer-0.1.1.data/data/contrib/standalone/version.hpp
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2018, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 1
+#define PATCH_VERSION 1
