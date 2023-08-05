7afeeee9df8f19247529fe85447b6a1faf4429bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package spacy-nightly/2.1.0a6/spacy_nightly-2.1.0a6-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/spacy-nightly/spacy_nightly-2.1.0a6-cp27-cp27mu-manylinux1_x86_64.whl/spacy/lang/ar/punctuation.py
@@ -0,0 +1,20 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from ..char_classes import LIST_PUNCT, LIST_ELLIPSES, LIST_QUOTES, CURRENCY
+from ..char_classes import UNITS, ALPHA_UPPER
+
+_suffixes = (
+    LIST_PUNCT
+    + LIST_ELLIPSES
+    + LIST_QUOTES
+    + [
+        r"(?<=[0-9])\+",
+        # Arabic is written from Right-To-Left
+        r"(?<=[0-9])(?:{})".format(CURRENCY),
+        r"(?<=[0-9])(?:{})".format(UNITS),
+        r"(?<=[{au}][{au}])\.".format(au=ALPHA_UPPER),
+    ]
+)
+
+TOKENIZER_SUFFIXES = _suffixes
