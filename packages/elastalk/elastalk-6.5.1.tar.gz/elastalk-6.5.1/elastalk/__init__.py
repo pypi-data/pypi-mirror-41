04b5633091375620a08fd96008b99d003e350b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:02 +0000

    Add package elastalk/6.5.1/elastalk-6.5.1.tar.gz

---
+++ packages/elastalk/elastalk-6.5.1.tar.gz/elastalk-6.5.1/elastalk/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: elastalk
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Simple Conveniences for Talking to Elasticsearch
+"""
+from .version import __version__, __release__
