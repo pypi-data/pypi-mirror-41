525b261c9dc30e4c485c39d74931b86bc3c9d289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package Rechord/1.2.0/Rechord-1.2.0.tar.gz

---
+++ packages/Rechord/Rechord-1.2.0.tar.gz/Rechord-1.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Zach Davis",
+    author_email="zfdavis21@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    description="A simple python script to generate chord charts.",
+    entry_points='''
+        [console_scripts]
+        rechord=rechord.cli:cli
+    ''',
+    install_requires=["Click"],
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="Rechord",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/zwebthings/rechord",
+    version="1.2.0",
+)
