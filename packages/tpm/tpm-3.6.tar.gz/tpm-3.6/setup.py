22d8256cd7aea9f5d188f53a0acc184f3732c3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:42 +0000

    Add package tpm/3.6/tpm-3.6.tar.gz

---
+++ packages/tpm/tpm-3.6.tar.gz/tpm-3.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Setup to install TeamPasswordManager API Python Module."""
+from distutils.core import setup
+setup(name='tpm',
+      version='3.6',
+      py_modules=['tpm'],
+      install_requires=['requests', 'future'],
+      description='Provides functions to work with TeamPasswordManager API.',
+      url='https://github.com/peshay/tpm',
+      author='Andreas Hubert',
+      author_email='anhubert@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries :: Application '
+                   'Frameworks',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7'],
+      keywords='TeamPasswordManager json api',
+
+      )
