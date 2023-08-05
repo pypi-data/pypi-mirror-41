313ca46a3c811207c57ebda6c00114d5bd8a35b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package pybuff/0.1.4/pybuff-0.1.4-py3-none-any.whl

---
+++ packages/pybuff/pybuff-0.1.4-py3-none-any.whl/pybuff/__init__.py
@@ -0,0 +1,14 @@
+"""
+Python Overbuff Scraper
+-----------------------
+
+By: BigHeadGeorge :)
+"""
+
+__title__ = 'pybuff'
+__author__ = 'BigHeadGeorge'
+__copyright__ = 'Copyright 2018 BigHeadGeorge'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+from .grabber import get_player, BadBattletag
