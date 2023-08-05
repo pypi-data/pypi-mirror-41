4dbc3d6c50d330eceaab896a11fdea117405d466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package webweb/0.0.3/webweb-0.0.3.tar.gz

---
+++ packages/webweb/webweb-0.0.3.tar.gz/webweb-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="webweb",
+    version="0.0.3",
+    author="Dan Larremore; Hunter Wapman",
+    author_email="hunter.wapman@gmail.com",
+    description="webweb: an easy-to-use network visualization tool",
+    long_description="webweb: an easy-to-use network visualization tool",
+    long_description_content_type="text/markdown",
+    url="https://webwebpage.github.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
