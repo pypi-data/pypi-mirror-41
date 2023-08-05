83fe5128d80b80091a039b35927fe2c76f0a01ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package nmlu/0.7/nmlu-0.7.tar.gz

---
+++ packages/nmlu/nmlu-0.7.tar.gz/nmlu-0.7/nmlu/qinspect/common.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sparklines import sparklines
+
+
+def sparklines_str(col, bins=10):
+    bins = np.histogram(col[col.notnull()], bins=bins)[0]
+    return "".join(sparklines(bins))
+
+
+def df_types_and_stats(df: pd.DataFrame) -> pd.DataFrame:
+    missing = df.isnull().sum().sort_index()
+    missing_pc = (missing / len(df) * 100).round(2)
+    types_and_missing = pd.DataFrame(
+        {
+            'type': df.sort_index().dtypes,
+            '#nas': missing,
+            '%nas': missing_pc,
+            'card': df.agg('nunique').sort_index(),
+
+        },
+        index=df.columns.sort_values()
+    )
+    dist = df.agg([sparklines_str]).T.sort_index()
+    desc = df.describe(include='all').T.sort_index()
+    return pd.concat([dist, types_and_missing, desc], axis=1, sort=True)
