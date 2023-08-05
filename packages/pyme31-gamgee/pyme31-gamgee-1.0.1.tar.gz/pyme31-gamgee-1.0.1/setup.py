e6e50b48a67ab5c21f5c8ed6ebfba5741d9521e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:24 +0000

    Add package pyme31-gamgee/1.0.1/pyme31-gamgee-1.0.1.tar.gz

---
+++ packages/pyme31-gamgee/pyme31-gamgee-1.0.1.tar.gz/pyme31-gamgee-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyme31-gamgee",
+    version="1.0.1",
+    author="Johannes Müller",
+    author_email="mail@gamgee.de",
+    description="Reads data from Digital Multimeter ME-31 via serial port",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
