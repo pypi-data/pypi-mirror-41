b115949cae5dffd4f89b830e9b91b88aee09c72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package dredging/0.1/dredging-0.1.tar.gz

---
+++ packages/dredging/dredging-0.1.tar.gz/dredging-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dredging',
+      version='0.1',
+      description='Collection of helper functions for dredging math',
+      url='https://github.com/Flawless/dredging',
+      author='Alexander Ushanov',
+      author_email='alushanov92@gmail.com',
+      license='MIT',
+      packages=['dredging'],
+      zip_safe=False)
