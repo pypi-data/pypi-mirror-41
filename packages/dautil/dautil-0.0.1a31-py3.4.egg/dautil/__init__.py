56e3fb38e964e9270b9100adc0cb345b827f1146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package dautil/0.0.1a31/dautil-0.0.1a31-py3.4.egg

---
+++ packages/dautil/dautil-0.0.1a31-py3.4.egg/dautil/__init__.py
@@ -0,0 +1,39 @@
+# Temporary I think
+import warnings
+warnings.filterwarnings(action='ignore',
+                        message='.*IPython widgets are experimental.*')
+import logging
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+
+from .nb import *
+from .options import *
+from .plotting import *
+
+try:
+    from .report import *
+except ImportError as e:
+    LOGGER.warning('Could not import report module %s', e)
+
+from .stats import *
+
+try:
+    from .web import *
+except ImportError as e:
+    LOGGER.warning('Could not import web module %s', e)
+
+try:
+    from .db import *
+except ImportError as e:
+    LOGGER.warning('Could not import db module %s', e)
+
+try:
+    from .perf import *
+except ImportError as e:
+    LOGGER.warning('Could not import perf module %s', e)
+
+try:
+    from .nlp import *
+except ImportError as e:
+    LOGGER.warning('Could not import nlp module %s', e)
