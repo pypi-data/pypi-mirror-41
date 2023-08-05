b5d471175ba6dd839aeb1ecfa157d666575c2c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package carta-ach/0.4.4/carta-ach-0.4.4.tar.gz

---
+++ packages/carta-ach/carta-ach-0.4.4.tar.gz/carta-ach-0.4.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='carta-ach',
+    author='Carta, Inc.',
+    author_email='jared.hobbs@carta.com',
+    version='0.4.4',
+    packages=[
+        'ach',
+    ],
+    url='https://github.com/carta/python-ach',
+    license='MIT License',
+    description='Library to create and parse ACH files (NACHA)',
+    long_description=open('README.rst').read(),
+)
