95e6bf26e69f8d1b06f7b4a5bce8d8e9e40cc7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:16 +0000

    Add package pysumo/0.12/pysumo-0.12.tar.gz

---
+++ packages/pysumo/pysumo-0.12.tar.gz/pysumo-0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(name='pysumo',
+      version='0.12',
+      description='Control your parrot jumping Sumo!',
+      author='DEFRETIERE Clement',
+      author_email='pysumo@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'python-socketio',
+            'urllib3'
+          ]
+      )
