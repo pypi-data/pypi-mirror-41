1950aedc0109afe8a020be67056b38b669c55672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package ZypUtility/0.1.3/ZypUtility-0.1.3.tar.gz

---
+++ packages/ZypUtility/ZypUtility-0.1.3.tar.gz/ZypUtility-0.1.3/ZypUtility/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/22 9:31
+# @Author  : Zhangyp
+# @File    : __init__.py
+# @Software: PyCharm
+# @license : Copyright(C), eWord Technology Co., Ltd.
+# @Contact : yeahcheung213@163.com
+# import (from .extractor import Document)
