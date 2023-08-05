802f54003368856e206a80fe5a5bc3f6c5eb19cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package blackbook/0.0.1/blackbook-0.0.1.tar.gz

---
+++ packages/blackbook/blackbook-0.0.1.tar.gz/blackbook-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import unittest
+import doctest
+import os
+
+requirements = ["black>=18.9b0"]
+
+exec(open("src/blackbook/version.py", "r").read())
+
+setup(
+    name="blackbook",
+    version=__version__,
+    install_requires=requirements,
+    author="Nikoleta Glynatsi, Vince Knight, Henry Wilde",
+    author_email=("glynatsine@cardiff.ac.uk"),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="",
+    license="The MIT License (MIT)",
+    description="`Black` for Jupyter notebooks.",
+    entry_points={"console_scripts": "blackbook=blackbook.__main__:main"},
+)
