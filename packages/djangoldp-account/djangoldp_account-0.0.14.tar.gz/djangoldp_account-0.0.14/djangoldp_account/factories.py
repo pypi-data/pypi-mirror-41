ac2c7dfcf6ba79bb2acb783353d65d8a255a3e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:08 +0000

    Add package djangoldp-account/0.0.14/djangoldp_account-0.0.14.tar.gz

---
+++ packages/djangoldp-account/djangoldp_account-0.0.14.tar.gz/djangoldp_account-0.0.14/djangoldp_account/factories.py
@@ -0,0 +1,37 @@
+import factory
+import hashlib
+from .models import Account, ChatConfig, ChatProfile
+from django.db.models.signals import post_save
+from djangoldp.factories import UserFactory
+
+@factory.django.mute_signals(post_save)
+class AccountFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Account
+
+    user = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def members(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        emailMd5 = hashlib.md5(self.user.email.encode('utf-8')).hexdigest()
+        self.picture = "https://www.gravatar.com/avatar/%s" % emailMd5
+        self.save()
+
+@factory.django.mute_signals(post_save)
+class ChatConfigFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ChatConfig
+
+    jabberID = factory.Faker('uuid4')
+    jabberRoom = factory.Faker('boolean')
+
+@factory.django.mute_signals(post_save)
+class ChatProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ChatProfile
+
+    user = factory.SubFactory(UserFactory)
+    config = factory.SubFactory(ChatConfigFactory)
