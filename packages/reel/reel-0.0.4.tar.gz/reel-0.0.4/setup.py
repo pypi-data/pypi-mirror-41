9c98d65e3fa76a8eff2d8648c252e6c26f3a85b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package reel/0.0.4/reel-0.0.4.tar.gz

---
+++ packages/reel/reel-0.0.4.tar.gz/reel-0.0.4/setup.py
@@ -0,0 +1,29 @@
+"""Install the reel package."""
+from setuptools import find_packages, setup
+
+setup(
+    license='MIT',
+    name='reel',
+    version='0.0.4',
+    author='Zach Thompson',
+    author_email='zach@allotropic.com',
+    url='http://github.com/zthompson47/reel',
+    description='An async subprocess manager',
+    long_description_content_type='text/x-rst',
+    long_description=open('README.rst', 'r').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['reel=reel.cli:main'],
+    },
+    install_requires=[
+        'trio>=0.10.0',
+        'trio-click>=7.0.2',
+        'ansicolortags>=0.4',
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+)
---
+++ packages/reel/reel-0.0.4.tar.gz/reel-0.0.4/tests/test_cli.py
@@ -0,0 +1,53 @@
+"""Test suite for the reel command line interface."""
+from trio_click.testing import CliRunner
+
+import reel
+from reel import cli
+from reel.proc import Source
+
+from tests.fixtures import ENV_COVERAGE
+
+
+async def test_no_args():
+    """Run cli with no arguments."""
+    runner = CliRunner()
+    result = await runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert result.output == ''
+
+
+async def test_version():
+    """Show the version number."""
+    runner = CliRunner()
+    result = await runner.invoke(cli.main, ['--version'])
+    assert result.exit_code == 0
+    assert reel.__version__ in result.output
+
+
+async def test_no_args_external():
+    """Run the CLI externally with no arguments."""
+    command = 'python -m reel.cli'
+    noargs = Source(command, ENV_COVERAGE)
+    output = await noargs.run()
+    assert noargs.status == 0
+    assert output == ''
+
+
+async def test_version_external():
+    """Show the version number by running the actual CLI."""
+    command = 'python -m reel.cli -v'
+    version = Source(command, ENV_COVERAGE)
+    output = await version.read_text()
+    assert version.status == 0
+    assert output == reel.__version__
+
+
+async def test_dump_config():
+    """Show the current configuration."""
+    command = 'python -m reel.cli --config'
+    config = Source(command, ENV_COVERAGE)
+    dump = await config.read_list()
+    assert len(dump) == 3
+    assert config.status == 0
+    for line in dump:
+        assert line.startswith('REEL_')
