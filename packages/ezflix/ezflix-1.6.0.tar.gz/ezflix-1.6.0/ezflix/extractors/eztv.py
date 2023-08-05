dbba6ab8984f7899c05a724ac19db6cd4bb37258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:00 +0000

    Add package ezflix/1.6.0/ezflix-1.6.0.tar.gz

---
+++ packages/ezflix/ezflix-1.6.0.tar.gz/ezflix-1.6.0/ezflix/extractors/eztv.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+from tmdbv3api import TMDb, TV
+
+
+def eztv(q, limit, page=1, quality=None):
+    tmdb = TMDb()
+    tmdb.api_key = 'e1076b74406e0a7d0efb5318f1b662d0'
+    tv = TV()
+    sr = tv.search(q)
+    if not sr:
+        return
+    url = 'https://eztv.ag/api/get-torrents'
+    details = tv.external_ids(sr[0].id)
+    overview = sr[0].overview
+    req = requests.get('%s?imdb_id=%s&page=%s&limit=%s' % (url, details['imdb_id'][2:], page, limit))
+    if not req.ok:
+        return
+    results, count = [], 1
+    search_results = req.json()
+    if 'torrents' not in search_results:
+        return
+    for result in search_results['torrents']:
+        obj = {
+            'id': count,
+            'title': result['title'],
+            'magnet': result['magnet_url'],
+            'seeds': result['seeds'],
+            'peers': result['peers'],
+            'overview': overview,
+            'release_date': datetime.fromtimestamp(int(result['date_released_unix'])).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        if quality is not None:
+            if quality.lower() in result['title']:
+                results.append(obj)
+                count += 1
+        else:
+            results.append(obj)
+            count += 1
+    return results
---
+++ packages/ezflix/ezflix-1.6.0.tar.gz/ezflix-1.6.0/ezflix/extractors/yts.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+
+
+def yts(query_term, quality=None, limit=20, minimum_rating=4, sort_by='date_added', sort_order='asc', page=1):
+    params = {
+        'query_term': query_term,
+        'sort_by': sort_by,
+        'sort_order': sort_order,
+        'limit': limit,
+        'quality': quality,
+        'minimum_rating': minimum_rating,
+        'page': page
+
+    }
+    req = requests.get('https://yts.ag/api/v2/list_movies.json', params=params)
+    req = req.json()
+    if 'movies' not in req['data'] or not req['status'] == 'ok' or not req['data']['movie_count'] > 0:
+        return
+    arr, count = [], 1
+    for r in req['data']['movies']:
+        if 'torrents' in r:
+            for torrent in r['torrents']:
+                title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
+                obj = {'id': count,
+                       'title': title,
+                       'magnet': torrent['url'],
+                       'seeds': torrent['seeds'],
+                       'peers': torrent['peers'],
+                       'overview': r['synopsis'],
+                       'trailer': r['yt_trailer_code'],
+                       'release_date': datetime.strptime(torrent['date_uploaded'], '%Y-%m-%d %H:%M:%S').date()}
+                if quality is not None:
+                    if quality == torrent['quality']:
+                        arr.append(obj)
+                        count += 1
+                else:
+                    arr.append(obj)
+                    count += 1
+    return arr[0:limit]
