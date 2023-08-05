6f1cbf665cba5053727e655abf571ff1fa6cecb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:48 +0000

    Add package qcelemental/0.2.0/qcelemental-0.2.0.tar.gz

---
+++ packages/qcelemental/qcelemental-0.2.0.tar.gz/qcelemental-0.2.0/qcelemental/__init__.py
@@ -0,0 +1,23 @@
+"""
+Main init for QCElemental
+"""
+
+from .datum import Datum
+from .exceptions import (NotAnElementError, ValidationError, MoleculeFormatError, ChoicesError, DataUnavailableError)
+from . import molparse
+from . import models
+
+# Handle singletons, not their classes or modules
+from .periodic_table import periodictable
+from .physical_constants import constants, PhysicalConstantsContext
+from .covalent_radii import covalentradii, CovalentRadii
+del periodic_table
+del physical_constants
+del covalent_radii
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
