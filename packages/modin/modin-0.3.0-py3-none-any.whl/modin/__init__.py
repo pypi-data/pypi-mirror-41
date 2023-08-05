0fc4c3a44ecb74330519ce16931c2ec01893d31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:54 +0000

    Add package modin/0.3.0/modin-0.3.0-py3-none-any.whl

---
+++ packages/modin/modin-0.3.0-py3-none-any.whl/modin/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+
+def get_execution_engine():
+    # In the future, when there are multiple engines and different ways of
+    # backing the DataFrame, there will have to be some changed logic here to
+    # decide these things. In the meantime, we will use the currently supported
+    # execution engine + backing (Pandas + Ray).
+    if "MODIN_ENGINE" in os.environ:
+        # .title allows variants like ray, RAY, Ray
+        engine = os.environ["MODIN_ENGINE"].title()
+    else:
+        engine = "Ray" if "MODIN_DEBUG" not in os.environ else "Python"
+    return engine
+
+
+def get_partition_format():
+    # See note above about engine + backing.
+    return "Pandas"
+
+
+__version__ = "0.3.0"
+__execution_engine__ = get_execution_engine()
+__partition_format__ = get_partition_format()
+
+# We don't want these used outside of this file.
+del get_execution_engine
+del get_partition_format
