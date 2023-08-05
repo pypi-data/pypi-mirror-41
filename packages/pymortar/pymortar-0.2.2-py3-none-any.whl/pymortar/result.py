be35cbe682fdcd71d20ac018b7f7f76023e49e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:13 +0000

    Add package pymortar/0.2.2/pymortar-0.2.2-py3-none-any.whl

---
+++ packages/pymortar/pymortar-0.2.2-py3-none-any.whl/pymortar/result.py
@@ -0,0 +1,7 @@
+"""
+The result object helps pymortar build from streaming responses to a query,
+and provides an interface to look at both metadata and timeseries data that
+is the output of a call to Fetch(...)
+"""
+class Result:
+    pass
