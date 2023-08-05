9979e22884b33a895c3dfbfea08a6e1bd23842fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:45 +0000

    Add package eansearch/1.0.0/eansearch-1.0.0.tar.gz

---
+++ packages/eansearch/eansearch-1.0.0.tar.gz/eansearch-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='eansearch',
+      description='A Python class for EAN and ISBN name lookup and validation using the API on ean-search.org',
+      long_description=long_description,
+      version='1.0.0',
+      url='https://github.com/eansearch/python-ean-search',
+      author='Jan Willamowius',
+      author_email='info@relaxedcommunications.com',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent'
+      ],
+      packages=setuptools.find_packages()
+)
+
