64062baf318b7bb399a5aca398991a140bf5227a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:36 +0000

    Add package annif/0.38.2/annif-0.38.2.tar.gz

---
+++ packages/annif/annif-0.38.2.tar.gz/annif-0.38.2/annif/corpus/__init__.py
@@ -0,0 +1,8 @@
+"""Annif corpus operations"""
+
+
+from .document import DocumentDirectory, DocumentFile
+from .subject import Subject, SubjectDirectory, SubjectFileTSV
+from .subject import SubjectIndex, SubjectSet
+from .skos import SubjectFileSKOS
+from .combine import CombinedCorpus
