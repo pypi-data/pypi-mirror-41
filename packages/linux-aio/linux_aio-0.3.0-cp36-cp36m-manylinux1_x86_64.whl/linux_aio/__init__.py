ebf8af878e65650e0361f6db220aae784a224439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:21 +0000

    Add package linux-aio/0.3.0/linux_aio-0.3.0-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/linux-aio/linux_aio-0.3.0-cp36-cp36m-manylinux1_x86_64.whl/linux_aio/__init__.py
@@ -0,0 +1,6 @@
+# coding: UTF-8
+
+# noinspection PyUnresolvedReferences
+from .aio_context import AIOContext
+# noinspection PyUnresolvedReferences
+from .block import FDsyncBlock, FsyncBlock, PollBlock, ReadBlock, ReadVBlock, WriteBlock, WriteVBlock
---
+++ packages/linux-aio/linux_aio-0.3.0-cp36-cp36m-manylinux1_x86_64.whl/linux_aio/aio_event.py
@@ -0,0 +1,49 @@
+# coding: UTF-8
+
+from ctypes import py_object
+from typing import Tuple, Union
+
+from .block import AIOBlock, NonVectorBlock, ReadBlock, ReadVBlock, VectorBlock, WriteBlock, WriteVBlock
+from .raw import IOEvent
+
+
+class AIOEvent:
+    """
+    .. versionadded:: 0.2.0
+    """
+    __slots__ = ('_event', '_aio_block')
+
+    def __init__(self, event: IOEvent) -> None:
+        self._event = event  # type: IOEvent
+        self._aio_block = py_object.from_address(self._event.data).value  # type: AIOBlock
+
+    @property
+    def aio_block(self) -> AIOBlock:
+        return self._aio_block
+
+    @property
+    def buffer(self) -> Union[None, VectorBlock.BUF_TYPE, NonVectorBlock.BUF_TYPE]:
+        if isinstance(self._aio_block, (ReadBlock, WriteBlock, ReadVBlock, WriteVBlock)):
+            return self._aio_block.buffer
+        else:
+            return None
+
+    def stripped_buffer(self) -> Union[None, VectorBlock.BUF_TYPE, NonVectorBlock.BUF_TYPE]:
+        """`\0` pads removed buffer"""
+        buffer = self.buffer
+
+        if isinstance(buffer, (bytearray, bytes)):
+            return buffer.rstrip(b'\0')
+        elif isinstance(buffer, Tuple):
+            # TODO: implement me
+            raise NotImplementedError('Stripping of buffer vector is not implemented yet.')
+        else:
+            return None
+
+    @property
+    def response(self) -> int:
+        return self._event.res
+
+    @property
+    def response2(self) -> int:
+        return self._event.res2
