be1662443c060f8c91eb032f56c26b40149758a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package r7insight-python/1.0.0/r7insight_python-1.0.0.tar.gz

---
+++ packages/r7insight-python/r7insight_python-1.0.0.tar.gz/r7insight_python-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='r7insight_python',
+    version='1.0.0',
+    author='Rapid7',
+    author_email='InsightOpsTeam@rapid7.com',
+    packages=['r7insight'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/R7Insight/',
+    license='LICENSE.txt',
+    description='Python Logger plugin to send logs to Rapid7 Insight',
+    long_description=long_description,
+    install_requires=[
+        "certifi",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ])
