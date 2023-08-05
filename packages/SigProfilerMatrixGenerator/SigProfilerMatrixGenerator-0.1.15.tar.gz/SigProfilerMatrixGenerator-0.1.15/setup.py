115c373057cedccd125169e7efb9092d6c6a699d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:16 +0000

    Add package SigProfilerMatrixGenerator/0.1.15/SigProfilerMatrixGenerator-0.1.15.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-0.1.15.tar.gz/SigProfilerMatrixGenerator-0.1.15/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+def readme():
+	with open('README.rst') as f:
+		return(f.read())
+
+
+setup(name='SigProfilerMatrixGenerator',
+		version='0.1.15',
+		description='SigProfiler matrix generator tool',
+		url='',
+		author='Erik Bergstrom',
+		author_email='ebergstr@eng.ucsd.edu',
+		license='UCSD',
+		packages=find_packages(),#['SigProfilerMatrixGenerator'],
+		install_requires =[
+			"matplotlib",
+			"sigProfilerPlotting",
+			"statsmodels"],
+		include_package_data=True,
+		zip_safe=False)
