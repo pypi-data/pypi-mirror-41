d2c511c05bea7b038ee9e33699c3f5d4b37d3b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package squidward/0.0.19/squidward-0.0.19.tar.gz

---
+++ packages/squidward/squidward-0.0.19.tar.gz/squidward-0.0.19/squidward/kernels/__init__.py
@@ -0,0 +1,8 @@
+"""
+This script contains code for creating kernels for
+gaussian processes modeling.
+"""
+
+import squidward.kernels.kernel_base
+import squidward.kernels.kernel_base_multiprocessing
+import squidward.kernels.distance
