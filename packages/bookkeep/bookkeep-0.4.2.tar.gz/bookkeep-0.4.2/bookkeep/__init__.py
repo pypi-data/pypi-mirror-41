d90bf246e33b92caccd446a46a63d82a15643dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:01 +0000

    Add package bookkeep/0.4.2/bookkeep-0.4.2.tar.gz

---
+++ packages/bookkeep/bookkeep-0.4.2.tar.gz/bookkeep-0.4.2/bookkeep/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 08:23:05 2018
+
+@author: yoelr
+"""
+
+from .unit_manager import UnitManager
+from .smart_book import SmartBook
+from .read_only_book import ReadOnlyBook
+from .unit_registry import Quantity, Q_
+
+name = 'bookkeep'
+__all__ = ['UnitManager', 'SmartBook', 'ReadOnlyBook','Quantity', 'Q_']
