687e91e059f41901f8686feac8c4899da931dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package ac-ptk/0.0.4/ac-ptk-0.0.4.tar.gz

---
+++ packages/ac-ptk/ac-ptk-0.0.4.tar.gz/ac-ptk-0.0.4/performance/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+VERSION = [0, 0, 4]
+
+DEBUG = False
+
+# dump时间间隔(s)
+DUMP_INTERVAL = 1
+
+# 表单名称
+SHEET_NAME = "memory"
+
+
+COLORS = ['red', 'yellow', 'gray', 'blue', 'black', 'pink', '#ff34b3', '#fff8dc']
