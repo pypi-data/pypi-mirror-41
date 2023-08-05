81c605a004facdd9149a569b71684b86d3bd7777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package tibia.py/1.1.2/tibia.py-1.1.2-py3-none-any.whl

---
+++ packages/tibia.py/tibia.py-1.1.2-py3-none-any.whl/tibiapy/__init__.py
@@ -0,0 +1,10 @@
+from tibiapy import abc, enums, utils
+from tibiapy.character import *
+from tibiapy.enums import *
+from tibiapy.errors import *
+from tibiapy.guild import *
+from tibiapy.highscores import *
+from tibiapy.house import *
+from tibiapy.world import *
+
+__version__ = '1.1.2'
