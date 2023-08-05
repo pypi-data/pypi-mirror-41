f60a633caa8a6cfb6e38abb5592581c3430af271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:50 +0000

    Add package UnladenSwallow/0.2.1/UnladenSwallow-0.2.1.tar.gz

---
+++ packages/UnladenSwallow/UnladenSwallow-0.2.1.tar.gz/UnladenSwallow-0.2.1/setup.py
@@ -0,0 +1,24 @@
+"""Setup unladen_swallow."""
+
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="UnladenSwallow",
+    version="0.2.1",
+    packages=['unladen_swallow'],
+    install_requires=[
+        "Sphinx"
+    ],
+    author="Karl Saint Lucy",
+    author_email="karl@karlsaintlucy.com",
+    description=(
+        "This package returns the airspeed velocity of an unladen swallow."
+    ),
+    long_description=long_description,
+    license="BSD",
+    keywords="monty python unladen swallow airspeed velocity",
+    url="https://unladen-swallow.readthedocs.io/en/latest/",
+)
