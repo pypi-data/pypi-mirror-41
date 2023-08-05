a0d3952c527c2adfab44c47b100d2394a0ce574a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:25 +0000

    Add package essex/1.0.0/essex-1.0.0.tar.gz

---
+++ packages/essex/essex-1.0.0.tar.gz/essex-1.0.0/mkbin.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+# To run this container unprivileged, as a non-root user "myuser":
+# ----------------------------------------------------------------
+# /etc/subuid should contain something like:
+# myuser:100000:65536
+#
+# /etc/subgid should contain something like:
+# myuser:100000:65536
+#
+# To enable for the current session, run:
+# sudo sysctl kernel.unprivileged_userns_clone=1
+#
+# To enable for subsequent sessions,
+# /etc/sysctl.d/96-userns.conf should contain:
+# kernel.unprivileged_userns_clone = 1
+
+version=1.0.0
+ctnr=`buildah from docker://inn0kenty/pyinstaller-alpine:3.7`
+buildah run -v "$PWD/essex:/src" $ctnr -- /pyinstaller/pyinstaller.sh -F essex.py
+buildah run -v "$PWD/essex:/src" $ctnr -- /pyinstaller/pyinstaller.sh -F essex_complete.py -n _essex
+buildah rm $ctnr
+rm -rf essex/__pycache__ essex/build essex/*.spec
+cd essex/dist
+tar cfJ essex-$version-x86_64.tar.xz essex _essex
