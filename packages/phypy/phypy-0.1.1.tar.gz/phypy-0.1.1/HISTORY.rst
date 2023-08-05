310244dc9777c4b68119357688985faa55c253ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:29 +0000

    Add package phypy/0.1.1/phypy-0.1.1.tar.gz

---
+++ packages/phypy/phypy-0.1.1.tar.gz/phypy-0.1.1/HISTORY.rst
@@ -0,0 +1,14 @@
+=======
+History
+=======
+
+0.1.0 (2019-01-22)
+------------------
+
+* First release on PyPI.
+
+
+0.1.1 (2019-01-23)
+---------------------
+* Adds a working PA and OFDM modulator.
+* Begins working on basic DPD
---
+++ packages/phypy/phypy-0.1.1.tar.gz/phypy-0.1.1/phypy/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for phypy."""
+
+__author__ = """Chance Tarver"""
+__email__ = 'tarver.chance@gmail.com'
+__version__ = '0.1.1'
+
+from . import analog
+from . import corrections
+from . import modulators
+
+
+__all__ = ['analog', 'corrections', 'modulators']
