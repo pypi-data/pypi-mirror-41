f306485f851de56099c4549bf5e47c351b76c174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package napkin/0.5.3/napkin-0.5.3-py3-none-any.whl

---
+++ packages/napkin/napkin-0.5.3-py3-none-any.whl/napkin/__init__.py
@@ -0,0 +1,57 @@
+import os
+import importlib
+from . import sd
+
+__version__ = '0.5.3'
+
+# Name and description.
+# Note that the name should match to the module name, gen_<name>.
+SUPPORTED_FORMATS = {
+    'plantuml': 'PlantUML script',
+    'plantuml_png': 'PlantUML script and PNG image'
+}
+DEFAULT_FORAMT = 'plantuml'
+
+_collected_seq_diagrams = []
+
+
+class seq_diagram:
+    """
+    Decorator to mark the sequence  function.
+
+    'diagram_name' is the file name for the generated . The name of
+    decorated function will be used if not specified.
+
+    ex:
+    @napkin.seq_diagram()
+    def sd_simple(c):
+       ...
+
+    @napkin.seq_diagram('sd_another')
+    def foo(c):
+       ...
+    """
+    def __init__(self, name=None):
+        self.name = name
+
+    def __call__(self, wrapped_func):
+        if not self.name:
+            self.name = wrapped_func.__name__
+        self.sd_func = wrapped_func
+        _collected_seq_diagrams.append(self)
+
+
+def generate(output_format=DEFAULT_FORAMT, output_dir='.'):
+    """
+    Generate sequence diagrams from all the decorated functions.
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    module_name = '.gen_' + output_format
+    gen_module = importlib.import_module(module_name, 'napkin')
+
+    for d in _collected_seq_diagrams:
+        context = sd.parse(d.sd_func)
+        generated_files = gen_module.generate(d.name, output_dir, context)
+        print('File generated : {}'.format(', '.join(generated_files)))
