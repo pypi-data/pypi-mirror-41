467d56c6bd7e7ced045e810d1501c1e620167f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package nester-ur/0.0.3/nester_ur-0.0.3.tar.gz

---
+++ packages/nester-ur/nester_ur-0.0.3.tar.gz/nester_ur-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nester_ur',
+	version			= '0.0.3',
+	py_modules		= ['nester'],
+	author			= 'uendelrocha',
+	author_email	= 'uendelrocha@gmail.com',
+	url				= 'http://www.uendelrocha.com.br',
+	description		= 'A simple printer of nested lists',
+	)
