a33d0c46da1ac421a82a780ed40984b370829f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:47 +0000

    Add package simplest-notebook/0.0.2/simplest-notebook-0.0.2.tar.gz

---
+++ packages/simplest-notebook/simplest-notebook-0.0.2.tar.gz/simplest-notebook-0.0.2/simplest_notebook/build/commands.d.ts
@@ -0,0 +1,25 @@
+/**
+ * The map of command ids used by the notebook.
+ */
+export declare const CmdIds: {
+    invoke: string;
+    select: string;
+    invokeNotebook: string;
+    selectNotebook: string;
+    save: string;
+    interrupt: string;
+    restart: string;
+    switchKernel: string;
+    runAndAdvance: string;
+    deleteCell: string;
+    selectAbove: string;
+    selectBelow: string;
+    extendAbove: string;
+    extendBelow: string;
+    editMode: string;
+    merge: string;
+    split: string;
+    commandMode: string;
+    undo: string;
+    redo: string;
+};
