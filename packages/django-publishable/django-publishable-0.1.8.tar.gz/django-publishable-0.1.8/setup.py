2c1ff3537987066ab40f1a4e8320adab933915fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:55 +0000

    Add package django-publishable/0.1.8/django-publishable-0.1.8.tar.gz

---
+++ packages/django-publishable/django-publishable-0.1.8.tar.gz/django-publishable-0.1.8/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-publishable',
+    version=os.environ['TAG'],
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache Software License',
+    description='A simple Django app to add draft/publish capabilities to your models.',
+    long_description=README,
+    url='https://github.com/Ilyes-Hammadi/django-publishable',
+    author='Ilyes Hammadi',
+    author_email='hammadiilyesahmed@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
