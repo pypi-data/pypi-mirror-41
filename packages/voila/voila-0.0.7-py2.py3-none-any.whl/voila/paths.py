f8154b23168fa267d76fcd6ca52d01e395086892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:00 +0000

    Add package voila/0.0.7/voila-0.0.7-py2.py3-none-any.whl

---
+++ packages/voila/voila-0.0.7-py2.py3-none-any.whl/voila/paths.py
@@ -0,0 +1,13 @@
+#############################################################################
+# Copyright (c) 2018, Voila Contributors                                    #
+#                                                                           #
+# Distributed under the terms of the BSD 3-Clause License.                  #
+#                                                                           #
+# The full license is in the file LICENSE, distributed with this software.  #
+#############################################################################
+
+import os
+
+ROOT = os.path.dirname(__file__)
+STATIC_ROOT = os.path.join(ROOT, 'static')
+
