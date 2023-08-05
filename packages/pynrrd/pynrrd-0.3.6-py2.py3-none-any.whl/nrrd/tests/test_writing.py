c4c816d5ec5412502226e4eb8e76ff7953240cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:39 +0000

    Add package pynrrd/0.3.6/pynrrd-0.3.6-py2.py3-none-any.whl

---
+++ packages/pynrrd/pynrrd-0.3.6-py2.py3-none-any.whl/nrrd/tests/test_writing.py
@@ -0,0 +1,273 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+import tempfile
+import numpy as np
+from nrrd.tests.util import *
+import nrrd
+
+
+class TestWritingFunctions(unittest.TestCase):
+    def setUp(self):
+        self.temp_write_dir = tempfile.mkdtemp('nrrdtest')
+        self.data_input, _ = nrrd.read(RAW_NRRD_FILE_PATH)
+
+        with open(RAW_DATA_FILE_PATH, 'rb') as fh:
+            self.expected_data = fh.read()
+
+    def write_and_read_back_with_encoding(self, encoding, level=9):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_{}_{}.nrrd'.format(encoding, str(level)))
+        nrrd.write(output_filename, self.data_input, {u'encoding': encoding}, compression_level=level)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], encoding)
+
+        return output_filename
+
+    def test_write_raw(self):
+        self.write_and_read_back_with_encoding(u'raw')
+
+    def test_write_gz(self):
+        self.write_and_read_back_with_encoding(u'gzip')
+
+    def test_write_bzip2(self):
+        self.write_and_read_back_with_encoding(u'bzip2')
+
+    def test_write_gz_level1(self):
+        filename = self.write_and_read_back_with_encoding(u'gzip', level=1)
+
+        self.assertLess(os.path.getsize(GZ_NRRD_FILE_PATH), os.path.getsize(filename))
+
+    def test_write_bzip2_level1(self):
+        _ = self.write_and_read_back_with_encoding(u'bzip2', level=1)
+
+        # note: we don't currently assert reduction here, because with the binary ball test data,
+        #       the output size does not change at different bz2 levels.
+        # self.assertLess(os.path.getsize(BZ2_NRRD_FILE_PATH), os.path.getsize(fn))
+
+    def test_write_ascii_1d(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_ascii_1d.nrrd')
+
+        x = np.arange(1, 28)
+        nrrd.write(output_filename, x, {u'encoding': 'ascii'})
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(header['encoding'], 'ascii')
+        np.testing.assert_equal(data, x)
+
+    def test_write_ascii_2d(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_ascii_2d.nrrd')
+
+        x = np.arange(1, 28).reshape(3, 9, order='F')
+        nrrd.write(output_filename, x, {u'encoding': 'ascii'})
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(header['encoding'], 'ascii')
+        np.testing.assert_equal(data, x)
+
+    def test_write_ascii_3d(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_ascii_3d.nrrd')
+
+        x = np.arange(1, 28).reshape(3, 3, 3, order='F')
+        nrrd.write(output_filename, x, {u'encoding': 'ascii'})
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(header['encoding'], 'ascii')
+        np.testing.assert_equal(x, data)
+
+    def test_write_custom_fields_without_custom_field_map(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_custom_fields.nrrd')
+
+        data, header = nrrd.read(ASCII_1D_CUSTOM_FIELDS_FILE_PATH)
+        nrrd.write(output_filename, data, header)
+
+        with open(output_filename, 'r') as fh:
+            lines = fh.readlines()
+
+            # Strip newline from end of line
+            lines = [line.rstrip() for line in lines]
+
+            self.assertEqual(lines[5], 'type: uint8')
+            self.assertEqual(lines[6], 'dimension: 1')
+            self.assertEqual(lines[7], 'sizes: 27')
+            self.assertEqual(lines[8], 'kinds: domain')
+            self.assertEqual(lines[9], 'encoding: ASCII')
+            self.assertEqual(lines[10], 'spacings: 1.0458000000000001')
+            self.assertEqual(lines[11], 'int:=24')
+            self.assertEqual(lines[12], 'double:=25.5566')
+            self.assertEqual(lines[13], 'string:=This is a long string of information that is important.')
+            self.assertEqual(lines[14], 'int list:=1 2 3 4 5 100')
+            self.assertEqual(lines[15], 'double list:=0.2 0.502 0.8')
+            self.assertEqual(lines[16], 'string list:=words are split by space in list')
+            self.assertEqual(lines[17], 'int vector:=(100, 200, -300)')
+            self.assertEqual(lines[18], 'double vector:=(100.5,200.3,-300.99)')
+            self.assertEqual(lines[19], 'int matrix:=(1,0,0) (0,1,0) (0,0,1)')
+            self.assertEqual(lines[20], 'double matrix:=(1.2,0.3,0) (0,1.5,0) (0,-0.55,1.6)')
+
+    def test_write_custom_fields_with_custom_field_map(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_custom_fields.nrrd')
+
+        custom_field_map = {'int': 'int',
+                            'double': 'double',
+                            'string': 'string',
+                            'int list': 'int list',
+                            'double list': 'double list',
+                            'string list': 'string list',
+                            'int vector': 'int vector',
+                            'double vector': 'double vector',
+                            'int matrix': 'int matrix',
+                            'double matrix': 'double matrix'}
+
+        data, header = nrrd.read(ASCII_1D_CUSTOM_FIELDS_FILE_PATH, custom_field_map)
+        nrrd.write(output_filename, data, header, custom_field_map=custom_field_map)
+
+        with open(output_filename, 'r') as fh:
+            lines = fh.readlines()
+
+            # Strip newline from end of line
+            lines = [line.rstrip() for line in lines]
+
+            self.assertEqual(lines[5], 'type: uint8')
+            self.assertEqual(lines[6], 'dimension: 1')
+            self.assertEqual(lines[7], 'sizes: 27')
+            self.assertEqual(lines[8], 'kinds: domain')
+            self.assertEqual(lines[9], 'encoding: ASCII')
+            self.assertEqual(lines[10], 'spacings: 1.0458000000000001')
+            self.assertEqual(lines[11], 'int:=24')
+            self.assertEqual(lines[12], 'double:=25.5566')
+            self.assertEqual(lines[13], 'string:=This is a long string of information that is important.')
+            self.assertEqual(lines[14], 'int list:=1 2 3 4 5 100')
+            self.assertEqual(lines[15], 'double list:=0.20000000000000001 0.502 0.80000000000000004')
+            self.assertEqual(lines[16], 'string list:=words are split by space in list')
+            self.assertEqual(lines[17], 'int vector:=(100,200,-300)')
+            self.assertEqual(lines[18], 'double vector:=(100.5,200.30000000000001,-300.99000000000001)')
+            self.assertEqual(lines[19], 'int matrix:=(1,0,0) (0,1,0) (0,0,1)')
+            self.assertEqual(lines[20], 'double matrix:=(1.2,0.29999999999999999,0) (0,1.5,0) (0,-0.55000000000000004,'
+                                        '1.6000000000000001)')
+
+    def test_write_detached_raw_as_nrrd(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+        output_data_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nrrd')
+
+        nrrd.write(output_data_filename, self.data_input, {u'encoding': 'raw'}, detached_header=True,
+                   relative_data_path=False)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'raw')
+        self.assertEqual(header['data file'], output_data_filename)
+
+    def test_write_detached_raw_odd_extension(self):
+        output_data_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nrrd2')
+
+        nrrd.write(output_data_filename, self.data_input, {u'encoding': 'raw'}, detached_header=True)
+
+        # Read back the same file
+        data, header = nrrd.read(output_data_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'raw')
+        self.assertEqual('data file' in header, False)
+
+    def test_write_fake_encoding(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+
+        with self.assertRaisesRegex(nrrd.NRRDError, 'Invalid encoding specification while writing NRRD file: fake'):
+            nrrd.write(output_filename, self.data_input, {u'encoding': 'fake'})
+
+    def test_write_detached_raw(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+
+        # Data & header are still detached even though detached_header is False because the filename is .nhdr
+        # Test also checks detached data filename that it is relative (default value)
+        nrrd.write(output_filename, self.data_input, {u'encoding': 'raw'}, detached_header=False)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'raw')
+        self.assertEqual(header['data file'], 'testfile_detached_raw.raw')
+
+    def test_write_detached_gz(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+        output_data_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.raw.gz')
+
+        # Data & header are still detached even though detached_header is False because the filename is .nhdr
+        # Test also checks detached data filename that it is absolute
+        nrrd.write(output_filename, self.data_input, {u'encoding': 'gz'}, detached_header=False,
+                   relative_data_path=False)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'gz')
+        self.assertEqual(header['data file'], output_data_filename)
+
+    def test_write_detached_bz2(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+
+        # Data & header are still detached even though detached_header is False because the filename is .nhdr
+        # Test also checks detached data filename that it is relative (default value)
+        nrrd.write(output_filename, self.data_input, {u'encoding': 'bz2'}, detached_header=False)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'bz2')
+        self.assertEqual(header['data file'], 'testfile_detached_raw.raw.bz2')
+
+    def test_write_detached_ascii(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
+
+        # Data & header are still detached even though detached_header is False because the filename is .nhdr
+        # Test also checks detached data filename that it is relative (default value)
+        nrrd.write(output_filename, self.data_input, {u'encoding': 'txt'}, detached_header=False)
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(self.expected_data, data.tostring(order='F'))
+        self.assertEqual(header['encoding'], 'txt')
+        self.assertEqual(header['data file'], 'testfile_detached_raw.txt')
+
+    def test_invalid_custom_field(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_invalid_custom_field.nrrd')
+        header = {'int': 12}
+        custom_field_map = {'int': 'fake'}
+
+        with self.assertRaisesRegex(nrrd.NRRDError, 'Invalid field type given: fake'):
+            nrrd.write(output_filename, np.zeros((3, 9)), header, custom_field_map=custom_field_map)
+
+    def test_remove_endianness(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_remove_endianness.nrrd')
+
+        x = np.arange(1, 28)
+        nrrd.write(output_filename, x, {u'encoding': 'ascii', u'endian': 'little', 'space': 'right-anterior-superior',
+                                        'space dimension': 3})
+
+        # Read back the same file
+        data, header = nrrd.read(output_filename)
+        self.assertEqual(header['encoding'], 'ascii')
+
+        # Check for endian and space dimension, both of these should have been removed from the header
+        # Endian because it is an ASCII encoded file and space dimension because space is specified
+        self.assertFalse('endian' in header)
+        self.assertFalse('space dimension' in header)
+        np.testing.assert_equal(data, x)
+
+    def test_unsupported_encoding(self):
+        output_filename = os.path.join(self.temp_write_dir, 'testfile_unsupported_encoding.nrrd')
+        header = {'encoding': 'fake'}
+
+        with self.assertRaisesRegex(nrrd.NRRDError, 'Unsupported encoding: "fake"'):
+            nrrd.write(output_filename, np.zeros((3, 9)), header)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/pynrrd/pynrrd-0.3.6-py2.py3-none-any.whl/nrrd/writer.py
@@ -0,0 +1,302 @@
+import bz2
+import os
+import zlib
+from collections import OrderedDict
+from datetime import datetime
+
+from nrrd.errors import NRRDError
+from nrrd.formatters import *
+from nrrd.reader import _get_field_type
+
+# Reading and writing gzipped data directly gives problems when the uncompressed
+# data is larger than 4GB (2^32). Therefore we'll read and write the data in
+# chunks. How this affects speed and/or memory usage is something to be analyzed
+# further. The following two values define the size of the chunks.
+_WRITE_CHUNKSIZE = 2 ** 20
+
+_NRRD_FIELD_ORDER = [
+    'type',
+    'dimension',
+    'space dimension',
+    'space',
+    'sizes',
+    'space directions',
+    'kinds',
+    'endian',
+    'encoding',
+    'min',
+    'max',
+    'oldmin',
+    'old min',
+    'oldmax',
+    'old max',
+    'content',
+    'sample units',
+    'spacings',
+    'thicknesses',
+    'axis mins',
+    'axismins',
+    'axis maxs',
+    'axismaxs',
+    'centerings',
+    'labels',
+    'units',
+    'space units',
+    'space origin',
+    'measurement frame',
+    'data file']
+
+_TYPEMAP_NUMPY2NRRD = {
+    'i1': 'int8',
+    'u1': 'uint8',
+    'i2': 'int16',
+    'u2': 'uint16',
+    'i4': 'int32',
+    'u4': 'uint32',
+    'i8': 'int64',
+    'u8': 'uint64',
+    'f4': 'float',
+    'f8': 'double',
+    'V': 'block'
+}
+
+_NUMPY2NRRD_ENDIAN_MAP = {
+    '<': 'little',
+    'L': 'little',
+    '>': 'big',
+    'B': 'big'
+}
+
+def _format_field_value(value, field_type):
+    if field_type == 'int':
+        return format_number(value)
+    elif field_type == 'double':
+        return format_number(value)
+    elif field_type == 'string':
+        return str(value)
+    elif field_type == 'int list':
+        return format_number_list(value)
+    elif field_type == 'double list':
+        return format_number_list(value)
+    elif field_type == 'string list':
+        # TODO Handle cases where the user wants quotation marks around the items
+        return ' '.join(value)
+    elif field_type == 'int vector':
+        return format_vector(value)
+    elif field_type == 'double vector':
+        return format_optional_vector(value)
+    elif field_type == 'int matrix':
+        return format_matrix(value)
+    elif field_type == 'double matrix':
+        return format_optional_matrix(value)
+    else:
+        raise NRRDError('Invalid field type given: %s' % field_type)
+
+
+def write(filename, data, header={}, detached_header=False, relative_data_path=True, custom_field_map=None,
+                          compression_level=9):
+    """Write :class:`numpy.ndarray` to NRRD file
+
+    The :obj:`filename` parameter specifies the absolute or relative filename to write the NRRD file to. If the
+    :obj:`filename` extension is .nhdr, then the :obj:`detached_header` parameter is set to true automatically. If the
+    :obj:`detached_header` parameter is set to :obj:`True` and the :obj:`filename` ends in .nrrd, then the header file
+    will have the same path and base name as the :obj:`filename` but with an extension of .nhdr. In all other cases,
+    the header and data are saved in the same file.
+
+    :obj:`header` is an optional parameter containing the fields and values to be added to the NRRD header.
+
+    .. note::
+            The following fields are automatically generated based on the :obj:`data` parameter ignoring these values
+            in the :obj:`header`: 'type', 'endian', 'dimension', 'sizes'.
+
+    .. note::
+            The default encoding field used if not specified in :obj:`header` is 'gzip'.
+
+    See :ref:`user-guide:Writing NRRD files` for more information on writing NRRD files.
+
+    Parameters
+    ----------
+    filename : :class:`str`
+        Filename of the NRRD file
+    data : :class:`numpy.ndarray`
+        Data to save to the NRRD file
+    detached_header : :obj:`bool`, optional
+        Whether the header and data should be saved in separate files. Defaults to :obj:`False`
+    relative_data_path : :class:`bool`
+        Whether the data filename in detached header is saved with a relative path or absolute path.
+        This parameter is ignored if there is no detached header. Defaults to :obj:`True`
+    custom_field_map : :class:`dict` (:class:`str`, :class:`str`), optional
+        Dictionary used for parsing custom field types where the key is the custom field name and the value is a
+        string identifying datatype for the custom field.
+    compression_level : :class:`int`
+        Int specifying compression level, when using a compressed encoding (.gz, .bz2).
+        - For zlib (.gz): 1-9 set low to high compression; 0 disables; -1 uses zlib default.
+        - For bzip2 (.bz2): 1-9 set low to high compression.
+
+    See Also
+    --------
+    :meth:`read`, :meth:`read_header`, :meth:`read_data`
+    """
+
+    # Infer a number of fields from the NumPy array and overwrite values in the header dictionary.
+    # Get type string identifier from the NumPy datatype
+    header['type'] = _TYPEMAP_NUMPY2NRRD[data.dtype.str[1:]]
+
+    # If the datatype contains more than one byte and the encoding is not ASCII, then set the endian header value
+    # based on the datatype's endianness. Otherwise, delete the endian field from the header if present
+    if data.dtype.itemsize > 1 and header.get('encoding', '').lower() not in ['ascii', 'text', 'txt']:
+        header['endian'] = _NUMPY2NRRD_ENDIAN_MAP[data.dtype.str[:1]]
+    elif 'endian' in header:
+        del header['endian']
+
+    # If space is specified in the header, then space dimension can not. See
+    # http://teem.sourceforge.net/nrrd/format.html#space
+    if 'space' in header.keys() and 'space dimension' in header.keys():
+        del header['space dimension']
+
+    # Update the dimension and sizes fields in the header based on the data
+    header['dimension'] = data.ndim
+    header['sizes'] = list(data.shape)
+
+    # The default encoding is 'gzip'
+    if 'encoding' not in header:
+        header['encoding'] = 'gzip'
+
+    # A bit of magic in handling options here.
+    # If *.nhdr filename provided, this overrides `detached_header=False`
+    # If *.nrrd filename provided AND detached_header=True, separate header and data files written.
+    # If detached_header=True and data file is present, then write the files separately
+    # For all other cases, header & data written to same file.
+    if filename.endswith('.nhdr'):
+        detached_header = True
+
+        if 'data file' not in header:
+            # Get the base filename without the extension
+            base_filename = os.path.splitext(filename)[0]
+
+            # Get the appropriate data filename based on encoding, see here for information on the standard detached
+            # filename: http://teem.sourceforge.net/nrrd/format.html#encoding
+            if header['encoding'] == 'raw':
+                data_filename = '%s.raw' % base_filename
+            elif header['encoding'] in ['ASCII', 'ascii', 'text', 'txt']:
+                data_filename = '%s.txt' % base_filename
+            elif header['encoding'] in ['gzip', 'gz']:
+                data_filename = '%s.raw.gz' % base_filename
+            elif header['encoding'] in ['bzip2', 'bz2']:
+                data_filename = '%s.raw.bz2' % base_filename
+            else:
+                raise NRRDError('Invalid encoding specification while writing NRRD file: %s' % header['encoding'])
+
+            header['data file'] = os.path.basename(data_filename) \
+                if relative_data_path else os.path.abspath(data_filename)
+        else:
+            # TODO This will cause issues for relative data files because it will not save in the correct spot
+            data_filename = header['data file']
+    elif filename.endswith('.nrrd') and detached_header:
+        data_filename = filename
+        header['data file'] = os.path.basename(data_filename) \
+            if relative_data_path else os.path.abspath(data_filename)
+        filename = '%s.nhdr' % os.path.splitext(filename)[0]
+    else:
+        # Write header & data as one file
+        data_filename = filename
+        detached_header = False
+
+    with open(filename, 'wb') as fh:
+        fh.write(b'NRRD0005\n')
+        fh.write(b'# This NRRD file was generated by pynrrd\n')
+        fh.write(b'# on ' + datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S').encode('ascii') + b'(GMT).\n')
+        fh.write(b'# Complete NRRD file format specification at:\n')
+        fh.write(b'# http://teem.sourceforge.net/nrrd/format.html\n')
+
+        # Copy the options since dictionaries are mutable when passed as an argument
+        # Thus, to prevent changes to the actual options, a copy is made
+        # Empty ordered_options list is made (will be converted into dictionary)
+        local_options = header.copy()
+        ordered_options = []
+
+        # Loop through field order and add the key/value if present
+        # Remove the key/value from the local options so that we know not to add it again
+        for field in _NRRD_FIELD_ORDER:
+            if field in local_options:
+                ordered_options.append((field, local_options[field]))
+                del local_options[field]
+
+        # Leftover items are assumed to be the custom field/value options
+        # So get current size and any items past this index will be a custom value
+        custom_field_start_index = len(ordered_options)
+
+        # Add the leftover items to the end of the list and convert the options into a dictionary
+        ordered_options.extend(local_options.items())
+        ordered_options = OrderedDict(ordered_options)
+
+        for x, (field, value) in enumerate(ordered_options.items()):
+            # Get the field_type based on field and then get corresponding
+            # value as a str using _format_field_value
+            field_type = _get_field_type(field, custom_field_map)
+            value_str = _format_field_value(value, field_type)
+
+            # Custom fields are written as key/value pairs with a := instead of : delimeter
+            if x >= custom_field_start_index:
+                fh.write(('%s:=%s\n' % (field, value_str)).encode('ascii'))
+            else:
+                fh.write(('%s: %s\n' % (field, value_str)).encode('ascii'))
+
+        # Write the closing extra newline
+        fh.write(b'\n')
+
+        # If header & data in the same file is desired, write data in the file
+        if not detached_header:
+            _write_data(data, fh, header, compression_level=compression_level)
+
+    # If detached header desired, write data to different file
+    if detached_header:
+        with open(data_filename, 'wb') as data_fh:
+            _write_data(data, data_fh, header, compression_level=compression_level)
+
+
+def _write_data(data, fh, header, compression_level=None):
+    if header['encoding'] == 'raw':
+        # Convert the data into a string
+        raw_data = data.tostring(order='F')
+
+        # Write the raw data directly to the file
+        fh.write(raw_data)
+    elif header['encoding'].lower() in ['ascii', 'text', 'txt']:
+        # savetxt only works for 1D and 2D arrays, so reshape any > 2 dim arrays into one long 1D array
+        if data.ndim > 2:
+            np.savetxt(fh, data.ravel(order='F'), '%.17g')
+        else:
+            np.savetxt(fh, data.T, '%.17g')
+    else:
+        # Convert the data into a string
+        raw_data = data.tostring(order='F')
+
+        # Construct the compressor object based on encoding
+        if header['encoding'] in ['gzip', 'gz']:
+            compressobj = zlib.compressobj(compression_level, zlib.DEFLATED, zlib.MAX_WBITS | 16)
+        elif header['encoding'] in ['bzip2', 'bz2']:
+            compressobj = bz2.BZ2Compressor(compression_level)
+        else:
+            raise NRRDError('Unsupported encoding: "%s"' % header['encoding'])
+
+        # Write the data in chunks (see _WRITE_CHUNKSIZE declaration for more information why)
+        start_index = 0
+
+        # Loop through the data and write it by chunk
+        while start_index < len(raw_data):
+            # End index is start index plus the chunk size
+            end_index = start_index + _WRITE_CHUNKSIZE
+
+            # If the end index is past the data size, then clamp it to the data size
+            if end_index > len(raw_data):
+                end_index = len(raw_data)
+
+            # Write the compressed data
+            fh.write(compressobj.compress(raw_data[start_index:end_index]))
+
+            start_index = end_index
+
+        # Finish writing the data
+        fh.write(compressobj.flush())
+        fh.flush()
