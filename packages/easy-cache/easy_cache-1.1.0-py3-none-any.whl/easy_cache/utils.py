0d24193420111ef70ff22fd022cda4e075c09c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package easy-cache/1.1.0/easy_cache-1.1.0-py3-none-any.whl

---
+++ packages/easy-cache/easy_cache-1.1.0-py3-none-any.whl/easy_cache/utils.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import six
+
+
+def get_function_path(function, bound_to=None):
+    """Get received function path (as string), to import function later
+    with `import_string`.
+    """
+    if isinstance(function, six.string_types):
+        return function
+
+    # static and class methods
+    if hasattr(function, '__func__'):
+        real_function = function.__func__
+    elif callable(function):
+        real_function = function
+    else:
+        return function
+
+    func_path = []
+
+    module = getattr(real_function, '__module__', '__main__')
+    if module:
+        func_path.append(module)
+
+    if not bound_to:
+        try:
+            bound_to = six.get_method_self(function)
+        except AttributeError:
+            pass
+
+    if bound_to:
+        if isinstance(bound_to, six.class_types):
+            func_path.append(bound_to.__name__)
+        else:
+            func_path.append(bound_to.__class__.__name__)
+        func_path.append(real_function.__name__)
+    else:
+        # qualname is available in Python 3 only
+        func_path.append(getattr(real_function, '__qualname__', real_function.__name__))
+
+    return '.'.join(func_path)
+
+
+class cached_property(object):
+    """A property that is only computed once per instance and then replaces
+       itself with an ordinary attribute. Deleting the attribute resets the
+       property.
+
+       Source: https://github.com/bottlepy/bottle/blob/0.11.5/bottle.py#L175
+    """
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            # We're being accessed from the class itself, not from an object
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
