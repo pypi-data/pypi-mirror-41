224ff3b8fac5ee5c06bfcdc9362dfd0555263181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:49 +0000

    Add package sim-db/0.2.6/sim_db-0.2.6-py3-none-any.whl

---
+++ packages/sim-db/sim_db-0.2.6-py3-none-any.whl/sim_db/src_command_line_tool/commands/add_package_root_to_path.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2018 Håkon Austlid Taskén <hakon.tasken@gmail.com>
+# Licenced under the MIT License.
+
+import os
+import sys
+sys.path.insert(
+        0,
+        os.path.dirname(
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
