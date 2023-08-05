5cf620a3838af9f5624662a413e169c691930eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package followthemoney/1.5.1/followthemoney-1.5.1.tar.gz

---
+++ packages/followthemoney/followthemoney-1.5.1.tar.gz/followthemoney-1.5.1/followthemoney/types/entity.py
@@ -0,0 +1,22 @@
+from rdflib import URIRef
+from banal import is_mapping
+
+from followthemoney.types.common import PropertyType
+
+
+class EntityType(PropertyType):
+    name = 'entity'
+    group = 'entities'
+
+    def clean(self, text, **kwargs):
+        if is_mapping(text):
+            text = text.get('id')
+        if hasattr(text, 'id'):
+            text = text.id
+        return super(EntityType, self).clean(text, **kwargs)
+
+    def specificity(self, value):
+        return 1
+
+    def rdf(self, value):
+        return URIRef('urn:entity:%s' % value)
---
+++ packages/followthemoney/followthemoney-1.5.1.tar.gz/followthemoney-1.5.1/followthemoney/types/phone.py
@@ -0,0 +1,56 @@
+from rdflib import URIRef
+from banal import ensure_list
+from phonenumbers import geocoder
+from phonenumbers import parse as parse_number
+from phonenumbers import is_possible_number, is_valid_number, format_number
+from phonenumbers import PhoneNumberFormat
+from phonenumbers.phonenumberutil import NumberParseException
+
+from followthemoney.types.common import PropertyType
+
+
+class PhoneType(PropertyType):
+    name = 'phone'
+    group = 'phones'
+    strong = False
+
+    def _clean_countries(self, countries, country):
+        result = set([None])
+        countries = ensure_list(countries)
+        countries.extend(ensure_list(country))
+        for country in countries:
+            if isinstance(country, str):
+                country = country.strip().upper()
+                result.add(country)
+        return result
+
+    def clean_text(self, number, countries=None, country=None, **kwargs):
+        """Parse a phone number and return in international format.
+
+        If no valid phone number can be detected, None is returned. If
+        a country code is supplied, this will be used to infer the
+        prefix.
+
+        https://github.com/daviddrysdale/python-phonenumbers
+        """
+        for code in self._clean_countries(countries, country):
+            try:
+                num = parse_number(number, code)
+                if is_possible_number(num):
+                    if is_valid_number(num):
+                        return format_number(num, PhoneNumberFormat.E164)
+            except NumberParseException:
+                pass
+
+    def specificity(self, value):
+        return 1
+
+    def country_hint(self, value):
+        try:
+            number = parse_number(value)
+            return geocoder.region_code_for_number(number).lower()
+        except NumberParseException:
+                pass
+
+    def rdf(self, value):
+        return URIRef('tel:%s' % value)
