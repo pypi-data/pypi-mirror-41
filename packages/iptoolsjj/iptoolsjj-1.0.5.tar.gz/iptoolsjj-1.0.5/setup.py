520df040b5f93181b3f488811d290ee58c0edfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package iptoolsjj/1.0.5/iptoolsjj-1.0.5.tar.gz

---
+++ packages/iptoolsjj/iptoolsjj-1.0.5.tar.gz/iptoolsjj-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='iptoolsjj',
+      version='1.0.5',
+      description='IP Tools',
+      long_description="""see https://github.com/jarekj9/iptoolsjj""",
+      long_description_content_type='text/markdown',	
+      url='https://github.com/jarekj9/iptoolsjj',
+      author='Jaroslaw Jankun',
+      author_email='jaroslaw.jankun@gmail.com',
+      license='MIT',
+      packages=['iptoolsjj'],
+      zip_safe=False)
