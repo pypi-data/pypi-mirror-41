0539ff1740ffa090b3cd9729a118b4f622f68fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package jellyfin/0.0.0/jellyfin-0.0.0.tar.gz

---
+++ packages/jellyfin/jellyfin-0.0.0.tar.gz/jellyfin-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='jellyfin',
+    version='0.0.0',
+    author='Andrew Rabert',
+    author_email='ar@nullsum.net',
+    url='https://github.com/nvllsvm/python-jellyfin',
+    license='MIT',
+)
