a57ed9377b3a36061a5c8f54509653887ba4c70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package django-jackal/1.6.4/django_jackal-1.6.4.tar.gz

---
+++ packages/django-jackal/django_jackal-1.6.4.tar.gz/django_jackal-1.6.4/jackal/__init__.py
@@ -0,0 +1,16 @@
+"""
+ ______             __            __
+|_  __|            | |           | |
+  \ \   _____  ____| | __  _____ | |
+   \ \ / _` | / __/| |/ / / _` | | |
+/\_/ /| (_| || <__ |   < | (_| | | |___
+\___/ \__,_| \___/ |_|\_|\__,_|  \_____|
+
+"""
+
+from .__version__ import __version__
+
+__title__ = 'Jackal'
+__author__ = 'Yongjin Jo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018~ Yongjin Jo'
