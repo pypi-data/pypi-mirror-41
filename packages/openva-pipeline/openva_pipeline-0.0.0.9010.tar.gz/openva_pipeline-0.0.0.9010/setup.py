1eb13ea6657675f0e26b50beeb9d34b65ade1c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package openva-pipeline/0.0.0.9010/openva_pipeline-0.0.0.9010.tar.gz

---
+++ packages/openva-pipeline/openva_pipeline-0.0.0.9010.tar.gz/openva_pipeline-0.0.0.9010/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openva_pipeline",
+    version="0.0.0.9010",
+    author="Jason Thomas, Samuel J. Clark, & Martin Bratschi",
+    author_email="jarthomas@gmail.com",
+    description="Automates the processing of verbal autopsy data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/verbal-autopsy-software/openva_pipeline",
+    license="GPLv3",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'pysqlcipher3',
+        'requests',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+
