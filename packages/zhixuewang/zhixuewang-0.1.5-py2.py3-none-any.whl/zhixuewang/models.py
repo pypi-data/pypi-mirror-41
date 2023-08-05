1a81489cee0afb722e2a8cde3943c246de07dea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:11 +0000

    Add package zhixuewang/0.1.5/zhixuewang-0.1.5-py2.py3-none-any.whl

---
+++ packages/zhixuewang/zhixuewang-0.1.5-py2.py3-none-any.whl/zhixuewang/models.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+subjectDataModel = namedtuple("scoreDataModel", [
+    "score",
+    "classRank",
+    "gradeRank",
+    "subjectName",
+    "standardScore",
+    "examName",
+    "examId"
+])
+classDataModel = namedtuple("classDataModel", [
+    "avgScore",
+    "highScore",
+    "rank",
+    "lowScore"
+])
+gradeDataModel = namedtuple("gradeDataModel", [
+    "avgScore",
+    "highScore",
+    "lowScore"
+])
