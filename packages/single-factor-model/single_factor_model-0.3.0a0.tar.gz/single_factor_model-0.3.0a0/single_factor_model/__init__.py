55363cc657c2365913e095578370e75cecfb1ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:37 +0000

    Add package single-factor-model/0.3.0a0/single_factor_model-0.3.0a0.tar.gz

---
+++ packages/single-factor-model/single_factor_model-0.3.0a0.tar.gz/single_factor_model-0.3.0a0/single_factor_model/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 13:30:09 2018
+
+@author: yili.peng
+"""
+
+from .backtest import run_back_test
+from .bt_summary import summary,run_plot,run_plot_turnover,calc_return
