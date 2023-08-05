97cc58d6c6ab9f3b77e58e3cc4cbc1b1e8bfdb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package bands-inspect/0.2.1/bands-inspect-0.2.1.tar.gz

---
+++ packages/bands-inspect/bands-inspect-0.2.1.tar.gz/bands-inspect-0.2.1/bands_inspect/compare/__init__.py
@@ -0,0 +1,7 @@
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+"""
+Defines functions to compare different bandstructures.
+"""
+
+from . import difference
