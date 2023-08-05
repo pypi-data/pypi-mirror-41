b2eb210601975ef2083c149132eed7b5fc1e503b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:51 +0000

    Add package slimish-jinja/1.1.0/slimish_jinja-1.1.0.tar.gz

---
+++ packages/slimish-jinja/slimish_jinja-1.1.0.tar.gz/slimish_jinja-1.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='slimish_jinja',
+    version='1.1.0',
+    packages=['slimish_jinja'],
+    scripts=['slim_to_jinja.py'],
+    install_requires=['jinja2'],
+    license='BSD',
+    description='Slim templates syntax for Jinja2 templates',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Rahul Kumar',
+    url='https://github.com/thoughtnirvana/slimish-jinja2',
+    author_email='rahul@thoughtnirvana.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
