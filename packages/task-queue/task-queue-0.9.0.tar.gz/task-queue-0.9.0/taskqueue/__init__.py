68aaaaf0d625d410b70722ae0c5d1647328cadc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package task-queue/0.9.0/task-queue-0.9.0.tar.gz

---
+++ packages/task-queue/task-queue-0.9.0.tar.gz/task-queue-0.9.0/taskqueue/__init__.py
@@ -0,0 +1,8 @@
+from .registered_task import RegisteredTask, MockTask, PrintTask
+from .taskqueue import TaskQueue, MockTaskQueue, LocalTaskQueue
+from .secrets import (
+  QUEUE_NAME, TEST_QUEUE_NAME, QUEUE_TYPE, 
+  PROJECT_NAME, AWS_DEFAULT_REGION
+)
+
+__version__ = 0.9.0
