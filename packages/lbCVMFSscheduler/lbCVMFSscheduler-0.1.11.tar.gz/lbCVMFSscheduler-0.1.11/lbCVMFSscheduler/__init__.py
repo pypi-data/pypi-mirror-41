49a4345dd06d39b159e01ac35db7f0c2eafbee15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:48 +0000

    Add package lbCVMFSscheduler/0.1.11/lbCVMFSscheduler-0.1.11.tar.gz

---
+++ packages/lbCVMFSscheduler/lbCVMFSscheduler-0.1.11.tar.gz/lbCVMFSscheduler-0.1.11/lbCVMFSscheduler/__init__.py
@@ -0,0 +1,10 @@
+from lbmessaging.CvmfsDevExchange import CvmfsDevExchange
+from lbmessaging.CvmfsConDBExchange import CvmfsConDBExchange
+from lbmessaging.CvmfsProdExchange import CvmfsProdExchange
+
+
+exchangeManager = {
+    'CvmfsDevExchange': CvmfsDevExchange,
+    'CvmfsConDBExchange': CvmfsConDBExchange,
+    'CvmfsProdExchange': CvmfsProdExchange
+}
