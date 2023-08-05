ab697840a3ae1691cbce6cca2ad65ffcf101c872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package kudneHaru/1.1.0/kudneHaru-1.1.0.tar.gz

---
+++ packages/kudneHaru/kudneHaru-1.1.0.tar.gz/kudneHaru-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name = 'kudneHaru',
+      version = '1.1.0',
+      py_modules = ['kudne_haru'],
+      author = 'subba',
+      email = 'anupam.subba@gmail.com',
+      description = 'A simple tabulation of lists',
+     )
