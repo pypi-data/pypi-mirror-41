d99017822492fa8499c402bcdb2212082c3d5163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package keras-bert/0.25.0/keras-bert-0.25.0.tar.gz

---
+++ packages/keras-bert/keras-bert-0.25.0.tar.gz/keras-bert-0.25.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keras-bert',
+    version='0.25.0',
+    packages=find_packages(),
+    url='https://github.com/CyberZHG/keras-bert',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='BERT implemented in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+        'keras-pos-embd==0.6.0',
+        'keras-transformer==0.11.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
