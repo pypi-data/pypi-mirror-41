4bf453ee599cd7c4560e083d17d3a640d1292b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:21 +0000

    Add package mathtastic/1.0.0/mathtastic-1.0.0.tar.gz

---
+++ packages/mathtastic/mathtastic-1.0.0.tar.gz/mathtastic-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+# Should match git tag
+VERSION = '1.0.0'
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+with open("requirements.txt") as file:
+    REQUIRED_MODULES = [line.strip() for line in file]
+
+with open("requirements-dev.txt") as file:
+    DEVELOPMENT_MODULES = [line.strip() for line in file]
+
+
+setup(
+    name="mathtastic",
+    version=VERSION,
+    description="making math fantastic",
+    long_description=readme(),
+    keywords="math",
+    url="https://github.com/justindujardin/mathtastic",
+    author="Justin DuJardin",
+    author_email="justin@dujardinconsulting.com",
+    packages=["src"],
+    install_requires=REQUIRED_MODULES,
+    extras_require={"dev": DEVELOPMENT_MODULES},
+    entry_points={"console_scripts": ["command=src.command_line:main"]},
+    include_package_data=True,
+)
