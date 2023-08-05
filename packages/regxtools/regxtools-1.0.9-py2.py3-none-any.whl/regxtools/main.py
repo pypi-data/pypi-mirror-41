d9c7c2ae2e55f6155e9205646ed158076e609ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package regxtools/1.0.9/regxtools-1.0.9-py2.py3-none-any.whl

---
+++ packages/regxtools/regxtools-1.0.9-py2.py3-none-any.whl/regxtools/main.py
@@ -0,0 +1,20 @@
+from .regxgen import RegxGen
+import re
+
+class RegTool:
+    regTable={}
+
+    def __init__(self):
+        self.regTable=RegGen.table()
+
+    def eval(self, ref, str):
+        x = re.search(self.regTable[ref], str)
+        print(x.groupdict())
+
+
+if __name__ == "__main__":
+    RegGen.setConfig("regex.yaml").apply()
+    regFormater=RegTool()
+    print(regFormater.regTable)
+    regFormater.eval("ADM", "adm-54")
+    regFormater.eval("T3", "t3-54-5")
