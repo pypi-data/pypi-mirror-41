4ffa322702aa949b03b37bf3fa5491a86d7178b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package dautil/0.0.1a22/dautil-0.0.1a22-py3.4.egg

---
+++ packages/dautil/dautil-0.0.1a22-py3.4.egg/dautil/db.py
@@ -0,0 +1,69 @@
+''' Database related utilities. '''
+from dautil import log_api
+
+
+def count_where(session, column, search_val):
+    ''' Gets the count of a query as in the following SQL \
+        `SELECT count(column) from atable where atable = 'search_val'`\
+
+        .. note:: **DO NOT USE** when high-performance is required.
+
+    :param session: A `SQLAlchemy` session.
+    :param column: A table column as attribute in a Python class.
+    :param search_val: Value to search for.
+
+    :returns: The count of the query.
+    '''
+    return session.query(column).filter(column == search_val).count()
+
+
+def entity_from_column(column):
+    ''' Utility function which returns the entity of a column.
+
+    :param column: A table column as attribute in a Python class.
+
+    :returns: The entity.
+    '''
+    return column.parent.entity
+
+
+def where_first(session, column, search_val):
+    ''' Gets the first row as in the following SQL \
+        `SELECT * from atable where atable = 'search_val'`
+
+    :param session: A `SQLAlchemy` session.
+    :param column: A table column as attribute in a Python class.
+    :param search_val: Value to search for.
+
+    :returns: The first row of the query.
+    '''
+    logger = log_api.env_logger()
+    result = session.query(entity_from_column(column)).\
+        filter(column == search_val).first()
+    logger.debug('search_val=%s result=%s', search_val, result)
+
+    return result
+
+
+def map_to_id(session, column):
+    ''' Assuming that a table has a primary key column 'id'. \
+        This function creates dictionary with column value as key \
+        and id as value.
+
+    :param session: A `SQLAlchemy` session.
+    :param column: A table column as attribute in a Python class.
+
+    :returns: A dictionary with column values mapped to id.
+    '''
+    id_map = {}
+    logger = log_api.env_logger()
+    table = column.parent.entity
+    query = session.query(table.id, column)
+    logger.debug('Query %s', query)
+
+    for id, val in query.all():
+        id_map[val] = id
+
+    logger.debug('Id map %s', id_map)
+
+    return id_map
