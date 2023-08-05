922c4f80acec5844338555cc32c480b258edc26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package sensebook/0.1.2/sensebook-0.1.2.tar.gz

---
+++ packages/sensebook/sensebook-0.1.2.tar.gz/sensebook-0.1.2/sensebook/__init__.py
@@ -0,0 +1,9 @@
+"""Making sense of Facebooks undocumented API."""
+
+__title__ = "SenseBook"
+__version__ = "0.1.2"
+
+__all__ = ()
+
+
+from .sansio import State, LoginError, ProtocolError, Listener
