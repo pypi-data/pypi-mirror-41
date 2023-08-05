f3c81017185950994e16e6e1b5c3882cdc0fe80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package django-crypto-fields/0.1.18/django_crypto_fields-0.1.18-py3-none-any.whl

---
+++ packages/django-crypto-fields/django_crypto_fields-0.1.18-py3-none-any.whl/django_crypto_fields/constants.py
@@ -0,0 +1,18 @@
+from django.core.management.color import color_style
+
+style = color_style()
+
+AES = "aes"
+CIPHER_BUFFER_SIZE = 10
+CIPHER_PREFIX = "enc2:::"
+ENCODING = "utf-8"
+HASH_ALGORITHM = "sha256"
+HASH_PREFIX = "enc1:::"
+HASH_ROUNDS = 100000
+LOCAL_MODE = "local"
+PRIVATE = "private"
+PUBLIC = "public"
+RSA = "rsa"
+RSA_KEY_SIZE = 2048
+SALT = "salt"
+RESTRICTED_MODE = "restricted"
