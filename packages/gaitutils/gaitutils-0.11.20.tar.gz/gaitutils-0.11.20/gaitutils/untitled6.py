4b9cc7e20536743ab1942df630e6114276032d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package gaitutils/0.11.20/gaitutils-0.11.20.tar.gz

---
+++ packages/gaitutils/gaitutils-0.11.20.tar.gz/gaitutils-0.11.20/gaitutils/untitled6.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 13:47:01 2019
+
+@author: hus20664877
+"""
+
+import gaitutils
+import logging
+
+fn = r"C:\Users\hus20664877\Dropbox\File requests\trondheim_gait_data\Tobias Goihl - 4-511_P3_Tardieu02.c3d"
+gaitutils.Trial(fn)
