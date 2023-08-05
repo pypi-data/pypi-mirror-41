a737cf0ec99304e810dfb81d7ff214b8796e94c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package clodsa/1.2.24/clodsa-1.2.24.tar.gz

---
+++ packages/clodsa/clodsa-1.2.24.tar.gz/clodsa-1.2.24/clodsa/augmentors/augmentorsList.py
@@ -0,0 +1,100 @@
+from __future__ import absolute_import
+from .folderKerasLinearClassificationAugmentor import FolderKerasLinearClassificationAugmentor
+from .folderKerasSemanticSegmentationAugmentor import FolderKerasSemanticSegmentationAugmentor
+from .folderLinearClassificationAugmentor import FolderLinearClassificationAugmentor
+from .folderLinearSemanticSegmentationAugmentor import FolderLinearSemanticSegmentationAugmentor
+from .hdf5LinearClassificationAugmentor import HDF5LinearClassificationAugmentor
+from .hdf5LinearLocalizationAugmentor import  HDF5LinearLocalizationAugmentor
+from .hdf5LinearSegmentationAugmentor import HDF5LinearSegmentationAugmentor
+from .hdf5PowerClassificationAugmentor import HDF5PowerClassificationAugmentor
+from .hdf5PowerSegmentationAugmentor import HDF5PowerSegmentationAugmentor
+from .pascalVOCLinearDetectionAugmentor import PascalVOCLinearDetectionAugmentor
+from .pascalVOCLinearLocalizationAugmentor import PascalVOCLinearLocalizationAugmentor
+from .yoloLinearDetectionAugmentor import yoloLinearDetectionAugmentor
+from .cocoLinearInstanceSegmentationAugmentor import COCOLinearInstanceSegmentationAugmentor
+from .folderVideoLinearClassificationAugmentor import FolderVideoLinearClassificationAugmentor
+from .youtubeVideoLinearDetectionAugmentor import YoutubeVideoLinearDetectionAugmentor
+
+# List of augmentors organized by problem, then annotation mode, then output mode, and
+# finally generation
+Augmentors = {
+    "classification" : {
+        "folders": {
+            "folders": {
+                "linear" : FolderLinearClassificationAugmentor,
+                },
+            "hdf5": {
+                "linear" : HDF5LinearClassificationAugmentor,
+                "power" : HDF5PowerClassificationAugmentor,
+                },
+            "keras": {
+                "linear" : FolderKerasLinearClassificationAugmentor,
+                },
+            },
+        },
+
+    "localization" : {
+        "pascalvoc": {
+            "pascalvoc" : {
+                "linear" :PascalVOCLinearLocalizationAugmentor
+                },
+            "hdf5": {
+                "linear": HDF5LinearLocalizationAugmentor,
+            },
+
+
+            },
+        },
+
+    "detection" : {
+        "pascalvoc": {
+            "pascalvoc" : {
+                "linear" : PascalVOCLinearDetectionAugmentor
+                },
+
+            },
+        "yolo": {
+            "yolo": {
+                "linear": yoloLinearDetectionAugmentor
+            },
+
+        },
+        },
+
+    "semantic_segmentation" : {
+        "folders": {
+            "folders": {
+                "linear": FolderLinearSemanticSegmentationAugmentor,
+            },
+            "hdf5": {
+                "linear": HDF5LinearSegmentationAugmentor,
+                "power": HDF5PowerSegmentationAugmentor,
+            },
+            "keras": {
+                "linear": FolderKerasSemanticSegmentationAugmentor,
+            },
+        },
+    },
+    "instance_segmentation" : {
+        "coco": {
+            "coco": {
+                "linear": COCOLinearInstanceSegmentationAugmentor,
+            },
+        },
+    },
+
+   "stackclassification" : {
+        "videofolders": {
+            "videofolders": {
+                "linear" : FolderVideoLinearClassificationAugmentor,
+                },
+            },
+        },
+   "stackdetection" : {
+        "youtubevideo": {
+            "youtubevideo": {
+                "linear" : YoutubeVideoLinearDetectionAugmentor,
+                },
+            },
+        },
+}
---
+++ packages/clodsa/clodsa-1.2.24.tar.gz/clodsa-1.2.24/clodsa/transformers/transformerList.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from .transformerForImageClassification import TransformerForImageClassification
+from .transformerForImageDetection import TransformerForImageDetection
+from .transformerForImageInstanceSegmentation import TransformerForImageInstanceSegmentation
+from .transformerForImageLocalization import TransformerForImageLocalization
+from .transformerForImageSegmentation import TransformerForImageSegmentation
+from .transformerForImageStackClassification import TransformerForImageStackClassification
+from .transformerForImageStackDetection import TransformerForImageStackDetection
+
+
+Transformers = {
+    "classification" : TransformerForImageClassification,
+    "localization" : TransformerForImageLocalization,
+    "detection" : TransformerForImageDetection,
+    "semantic_segmentation" : TransformerForImageSegmentation,
+    "instance_segmentation" : TransformerForImageInstanceSegmentation,
+    "stackclassification" : TransformerForImageStackClassification,
+    "stackdetection": TransformerForImageStackDetection,
+}
