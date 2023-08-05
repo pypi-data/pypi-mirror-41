c99a8551cbeddc31e58335a88c35439259397743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package nestertud/1.0.0/nestertud-1.0.0.tar.gz

---
+++ packages/nestertud/nestertud-1.0.0.tar.gz/nestertud-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='nestertud',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='friendtu',
+    author_email='friendtu@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
