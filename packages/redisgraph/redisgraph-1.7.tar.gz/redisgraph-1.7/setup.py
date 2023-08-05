defbb4c841260e1529546794fa7d9508c227b502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:49 +0000

    Add package redisgraph/1.7/redisgraph-1.7.tar.gz

---
+++ packages/redisgraph/redisgraph-1.7.tar.gz/redisgraph-1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='redisgraph',
+    version='1.7',
+
+    description='RedisGraph Python Client',
+    url='https://github.com/redislabs/redisgraph-py',
+    packages=find_packages(),
+    install_requires=['redis', 'PTable'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database'
+    ]
+)
