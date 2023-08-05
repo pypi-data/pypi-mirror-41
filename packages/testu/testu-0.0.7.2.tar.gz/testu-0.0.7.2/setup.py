f990bcd43103f57055662f54c50bad4e1aadc9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:55 +0000

    Add package testu/0.0.7.2/testu-0.0.7.2.tar.gz

---
+++ packages/testu/testu-0.0.7.2.tar.gz/testu-0.0.7.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    author='dairoot',
+    author_email='623815825@qq.com',
+    description='A PyPI package with a Markdown README',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    name='testu',
+    url='http://github.com/di/markdown-description-example',
+    version='0.0.7.2',
+    data_files=["README.md"],
+)
