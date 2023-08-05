be3617ac3c82a6aa4a8157ca78232756d736b0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:47 +0000

    Add package deepctr/0.2.3/deepctr-0.2.3-py3-none-any.whl

---
+++ packages/deepctr/deepctr-0.2.3-py3-none-any.whl/deepctr/__init__.py
@@ -0,0 +1,7 @@
+from .import activations
+from .import layers
+from .import sequence
+from . import models
+from .utils import check_version, SingleFeat, VarLenFeat
+__version__ = '0.2.3'
+check_version(__version__)
