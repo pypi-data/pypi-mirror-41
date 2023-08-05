e4b39dc7c8651fe7c8ab1d19f8a741a04bd8aa73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package Elise/0.1.5/Elise-0.1.5.tar.gz

---
+++ packages/Elise/Elise-0.1.5.tar.gz/Elise-0.1.5/setup.py
@@ -0,0 +1,20 @@
+# encoding=utf-8
+# date: 2019/1/22
+__author__ = "Masako"
+
+from distutils.core import setup
+
+setup(
+    name='Elise',
+    version='0.1.5',
+    author='Masako',
+    author_email='Masako@example.com',
+    url='https://github.com/TitledPythonFile',
+    py_modules=['spider', 'crawler', 'test'],
+    license='GPL',
+    description='Simple multithreading spider',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests==2.19.1",
+    ],
+)
