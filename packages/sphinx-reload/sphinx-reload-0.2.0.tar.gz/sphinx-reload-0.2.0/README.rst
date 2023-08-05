25b6e47de57ff5c44f5f729f03fa49fc565f5410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package sphinx-reload/0.2.0/sphinx-reload-0.2.0.tar.gz

---
+++ packages/sphinx-reload/sphinx-reload-0.2.0.tar.gz/sphinx-reload-0.2.0/README.rst
@@ -0,0 +1,53 @@
+Sphinx-Reload
+*************
+
+|PyPI Version| |Build Status|
+
+.. image:: demo.gif
+
+
+Installation
+============
+
+To install, use ``pip`` (or ``easy_install``):
+
+::
+
+    $ pip install sphinx-reload
+
+
+The package installs the command-line program ``sphinx-reload``:
+
+::
+
+    $ sphinx-reload --version
+    v0.2.0
+
+Getting Started
+===============
+
+To begin live previewing your documentation, simply pass the path of your
+documentation's root as a command-line argument with ``sphinx-reload``.
+Here's an example assuming your documentation's root is under the current
+directory and named ``docs``:
+
+::
+
+    $ sphinx-reload docs/
+
+``sphinx-reload`` will open a preview in a new tab of your favorite browser
+and watch for changes in your documentation's source
+files (e.g., any `reStructuredText
+<http://docutils.sourceforge.net/rst.html>`__ files under the documentation's
+root).
+
+To view further usage details, use the script's ``--help`` option:
+
+::
+
+    $ sphinx-reload --help
+
+.. |Build Status| image:: https://travis-ci.org/prkumar/sphinx-reload.svg?branch=master
+   :target: https://travis-ci.org/prkumar/sphinx-reload
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/sphinx-reload.svg
+   :target: https://pypi.python.org/pypi/sphinx-reload
---
+++ packages/sphinx-reload/sphinx-reload-0.2.0.tar.gz/sphinx-reload-0.2.0/setup.py
@@ -0,0 +1,50 @@
+# Standard library imports
+from setuptools import setup, find_packages
+
+# Local imports
+import sphinx_reload
+
+
+def read(filename):
+    with open(filename) as stream:
+        return stream.read()
+
+
+metadata = dict({
+    "name": "sphinx-reload",
+    "author": "P. Raj Kumar",
+    "author_email": "raj.pritvi.kumar@gmail.com",
+    "version": sphinx_reload.__version__,
+    "url": "https://github.com/prkumar/sphinx-reload",
+    "license": "MIT",
+    "description": "Live preview your Sphinx documentation",
+    "long_description": read("README.rst"),
+    "classifiers": [
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Documentation"
+    ],
+    "keywords": "sphinx live preview sync reload documentation",
+    "install_requires": [
+        "livereload >= 2.5.1",
+    ],
+    "py_modules": ["sphinx_reload"],
+    "entry_points": {
+        "console_scripts": [
+            "sphinx-reload = sphinx_reload:main"
+        ]
+    }
+})
+
+if __name__ == "__main__":
+    setup(**metadata)
