9d0990c53ca9ddcf87c769de1d22b741846c45c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:26 +0000

    Add package django-tex/1.1.2.post4/django-tex-1.1.2.post4.tar.gz

---
+++ packages/django-tex/django-tex-1.1.2.post4.tar.gz/django-tex-1.1.2.post4/tests/settings.py
@@ -0,0 +1,44 @@
+
+"""
+Django settings for running tests
+"""
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SECRET_KEY = 'secret'
+
+INSTALLED_APPS = [
+    'django_tex',
+    'tests',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+TEMPLATES = [
+    {
+        'NAME': 'tex',
+        'BACKEND': 'django_tex.engine.TeXEngine', 
+        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'custom_templates'),
+        ],
+    },
+]
+
+LANGUAGE_CODE = 'de-de'
+
+TIME_ZONE = 'Europe/Berlin'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+DEBUG = True
