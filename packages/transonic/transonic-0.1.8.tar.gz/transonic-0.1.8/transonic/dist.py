2eb1769b499fb2fe6bef95e74f376cbdc5cdc0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package transonic/0.1.8/transonic-0.1.8.tar.gz

---
+++ packages/transonic/transonic-0.1.8.tar.gz/transonic-0.1.8/transonic/dist.py
@@ -0,0 +1,57 @@
+"""Utilities for the setup.py files
+===================================
+
+User API
+--------
+
+Provides the classes PythranBuildExt and PythranExtension to be used in the
+setup.py.
+
+.. autofunction:: detect_pythran_extensions
+
+"""
+
+import os
+from pathlib import Path
+from distutils.command.build_ext import build_ext
+
+try:
+    from pythran.dist import PythranBuildExt, PythranExtension
+
+    can_import_pythran = True
+except ImportError:
+    can_import_pythran = False
+    PythranBuildExt = build_ext
+    PythranExtension = None
+
+from .transpiler import make_backend_files
+from .util import modification_date
+
+__all__ = [
+    "make_backend_files",
+    "PythranBuildExt",
+    "PythranExtension",
+    "can_import_pythran",
+    "detect_pythran_extensions",
+    "modification_date",
+]
+
+
+def detect_pythran_extensions(name_package):
+
+    if not can_import_pythran:
+        return []
+    ext_names = []
+    for root, dirs, files in os.walk(str(name_package)):
+        path_dir = Path(root)
+        for name in files:
+            if (
+                name.endswith("_pythran.py")
+                or path_dir.name == "__pythran__"
+                and name.endswith(".py")
+            ):
+                path = path_dir / name
+                ext_names.append(
+                    str(path).replace(os.path.sep, ".").split(".py")[0]
+                )
+    return ext_names
---
+++ packages/transonic/transonic-0.1.8.tar.gz/transonic-0.1.8/transonic/mpi.py
@@ -0,0 +1,111 @@
+"""Minimalist MPI module
+========================
+
+"""
+
+import os
+from pathlib import Path
+
+if "TRANSONIC_NO_MPI" in os.environ:
+    nb_proc = 1
+    rank = 0
+else:
+    try:
+        from fluiddyn.util import mpi as _mpi
+    except ImportError:
+        try:
+            from mpi4py import MPI
+        except ImportError:
+            nb_proc = 1
+            rank = 0
+        else:
+            comm = MPI.COMM_WORLD
+            nb_proc = comm.size
+            rank = comm.Get_rank()
+    else:
+        rank = _mpi.rank
+        nb_proc = _mpi.nb_proc
+        if nb_proc > 1:
+            comm = _mpi.comm
+
+
+def barrier():
+    if nb_proc > 1:
+        comm.barrier()
+
+
+def bcast(value, root=0):
+    if nb_proc > 1:
+        value = comm.bcast(value, root=root)
+    return value
+
+
+class ShellProcessMPI:
+    def __init__(self, process, root=0):
+
+        if rank != root:
+            assert process is None
+
+        self.process = process
+        self.root = root
+
+    def is_alive(self):
+        if rank == self.root:
+            is_alive = self.is_alive_root()
+        else:
+            is_alive = None
+        return bcast(is_alive, self.root)
+
+    def is_alive_root(self):
+        return self.process.poll() is None
+
+
+PathSeq = Path
+
+if nb_proc > 1:
+
+    class PathMPI(type(Path())):
+        def exists(self):
+            answer = None
+            if rank == 0:
+                answer = super().exists()
+
+            ret = comm.bcast(("exists", answer))
+
+            if len(ret) != 2 and ret[0] != "exists":
+                print(rank, "bug!!!!", ret, flush=1)
+                raise RuntimeError
+
+            kind, answer = ret
+
+            return answer
+
+        def unlink(self):
+            if rank == 0:
+                super().unlink()
+            comm.barrier()
+
+    Path = PathMPI
+
+    def has_to_build(output_file: Path, input_file: Path):
+        from . import util
+
+        ret = None
+        if rank == 0:
+            ret = util.has_to_build(output_file, input_file)
+        return comm.bcast(ret)
+
+    def modification_date(filename):
+        from . import util
+
+        ret = None
+        if rank == 0:
+            ret = util.modification_date(filename)
+        return comm.bcast(ret)
+
+
+if __name__ == "__main__":
+    p = Path.home() / "Dev"
+    print(p.exists(), type(p))
+    p = PathSeq(p)
+    print(p, type(p))
