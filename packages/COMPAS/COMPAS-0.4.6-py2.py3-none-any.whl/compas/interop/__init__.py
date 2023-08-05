2562d54956dbdd3baf4972da4db14a2d5fc84ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package COMPAS/0.4.6/COMPAS-0.4.6-py2.py3-none-any.whl

---
+++ packages/COMPAS/COMPAS-0.4.6-py2.py3-none-any.whl/compas/interop/__init__.py
@@ -0,0 +1,47 @@
+"""
+********************************************************************************
+compas.interop
+********************************************************************************
+
+.. currentmodule:: compas.interop
+
+.. warning::
+
+    The functionality of this package is experimental and subject to frequent change.
+    For now, don't use it for anything important :)
+
+
+C++
+===
+
+Functions
+---------
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    cpp.shape
+    cpp.zeros
+    cpp.ones
+    cpp.eye
+
+
+Classes
+-------
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    cpp.Array1D
+    cpp.Array2D
+    cpp.Array3D
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__all__ = []
