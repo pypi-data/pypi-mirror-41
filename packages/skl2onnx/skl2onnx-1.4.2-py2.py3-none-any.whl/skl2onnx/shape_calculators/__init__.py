439b09f29f3762fe7f29916ecfcc201689e95cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package skl2onnx/1.4.2/skl2onnx-1.4.2-py2.py3-none-any.whl

---
+++ packages/skl2onnx/skl2onnx-1.4.2-py2.py3-none-any.whl/skl2onnx/shape_calculators/__init__.py
@@ -0,0 +1,24 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+# To register shape calculators for scikit-learn operators, import associated modules here.
+from . import ArrayFeatureExtractor
+from . import Concat
+from . import DictVectorizer
+from . import Imputer
+from . import KMeans
+from . import LabelBinariser
+from . import LabelEncoder
+from . import LinearClassifier
+from . import LinearRegressor
+from . import NearestNeighbours
+from . import OneHotEncoder
+from . import Scaler
+from . import SVM
+from . import SVD
+from . import TextVectorizer
+from . import TfidfTransformer
+from . import ZipMap 
