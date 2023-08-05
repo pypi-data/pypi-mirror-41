3ae2aaab2fb1591d253de28eec63dab0f302d1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package crossix/2019.0.0/crossix-2019.0.0-py2.py3-none-any.whl

---
+++ packages/crossix/crossix-2019.0.0-py2.py3-none-any.whl/crossix/static/crossix/js/app.js
@@ -0,0 +1,2 @@
+!function r(e,n,t){function u(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(o)return o(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var q=n[i]={exports:{}};e[i][0].call(q.exports,function(r){var n=e[i][1][r];return u(n?n:r)},q,q.exports,r,e,n,t)}return n[i].exports}for(var o="function"==typeof require&&require,i=0;i<t.length;i++)u(t[i]);return u}({1:[function(r,e,n){window.jQuery=$=r("jquery")},{jquery:"jquery"}]},{},[1]);
+//# sourceMappingURL=app.js.map
