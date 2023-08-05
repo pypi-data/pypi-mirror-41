f7f371a602ea902f2459a67868d3f03127abb6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package zenpy/2.0.8/zenpy-2.0.8.tar.gz

---
+++ packages/zenpy/zenpy-2.0.8.tar.gz/zenpy-2.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='zenpy',
+    packages=setuptools.find_packages(),
+    version='2.0.8',
+    description='Python wrapper for the Zendesk API',
+    license='GPLv3',
+    author='Face Toe',
+    author_email='facetoe@facetoe.com.au',
+    url='https://github.com/facetoe/zenpy',
+    download_url='https://github.com/facetoe/zenpy/releases/tag/2.0.8',
+    install_requires=[
+        'requests>=2.7.0',
+        'python-dateutil>=2.4.0',
+        'cachetools>=1.0.3',
+        'pytz>=2017.3',
+        'future>=0.16.0'
+    ],
+    keywords=['zendesk', 'api', 'wrapper'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+)
