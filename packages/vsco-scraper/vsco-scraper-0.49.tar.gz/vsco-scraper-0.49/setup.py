4a4a61132c5a9907e3ac0ca77257bb6a3184fbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package vsco-scraper/0.49/vsco-scraper-0.49.tar.gz

---
+++ packages/vsco-scraper/vsco-scraper-0.49.tar.gz/vsco-scraper-0.49/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vsco-scraper",
+    version='0.49',
+    description='Allows for a user to scrape users VSCOs',
+    author='Mustafa Abdi',
+    author_email='mustafabyabdi@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/mvabdi/vsco-scraper',
+    install_requires=[
+        'tqdm',
+        'requests',
+        'beautifulsoup4',
+    ],
+    entry_points='''
+        [console_scripts]
+        vsco-scraper=vscoscrape:main
+    ''',
+    keywords='vsco scrape image images download',
+)
