59b099dba578d075f06de1c261f1476473a24eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package runway-python/0.0.26/runway_python-0.0.26-py3-none-any.whl

---
+++ packages/runway-python/runway_python-0.0.26-py3-none-any.whl/runway/exceptions.py
@@ -0,0 +1,36 @@
+class RunwayError(Exception):
+    def __init__(self):
+        super(RunwayError, self).__init__()
+        self.message = 'An unknown error occurred.'
+        self.code = 500
+
+    def to_response(self):
+        return {'error': self.message}
+
+
+class MissingOptionException(RunwayError):
+    def __init__(self, name):
+        super(MissingOptionException, self).__init__()
+        self.message = 'Missing option: %s.' % name
+        self.code = 400
+
+
+class MissingInputException(RunwayError):
+    def __init__(self, name):
+        super(MissingInputException, self).__init__()
+        self.message = 'Missing input: %s.' % name
+        self.code = 400
+
+
+class InferenceError(RunwayError):
+    def __init__(self, message):
+        super(InferenceError, self).__init__()
+        self.message = 'Inference error: %s' % message
+        self.code = 500
+
+
+class UnknownCommandError(RunwayError):
+    def __init__(self, name):
+        super(UnknownCommandError, self).__init__()
+        self.message = 'Unknown command: %s.' % name
+        self.code = 404
