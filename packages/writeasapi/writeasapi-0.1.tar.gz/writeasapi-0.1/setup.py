7e62a276e3becddcfce58b9c34084f809b9663a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package writeasapi/0.1/writeasapi-0.1.tar.gz

---
+++ packages/writeasapi/writeasapi-0.1.tar.gz/writeasapi-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='writeasapi',
+    version='0.1',
+    author="CJ Eller",
+    author_email="cjeller1592@gmail.com",
+    description="An API client library for Write.as",
+    license="MIT",
+    py_modules=["writeas", "uri"],
+    url="https://github.com/cjeller1592/Writeas.py",
+ )
