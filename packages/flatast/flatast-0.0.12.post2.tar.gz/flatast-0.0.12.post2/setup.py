a951e912745f90f93766468d6e34b5f71c65e4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:13 +0000

    Add package flatast/0.0.12.post2/flatast-0.0.12.post2.tar.gz

---
+++ packages/flatast/flatast-0.0.12.post2.tar.gz/flatast-0.0.12.post2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='flatast',
+    version='0.0.12-2',
+    license='Apache 2.0',
+    author='Yijun Yu',
+    author_email='y.yu@open.ac.uk',
+    url='https://bitbucket.org/yijunyu/fast',
+    long_description=('Python runtime library for flat AST'
+                      'serialization format.'),
+    packages=['fast_', 'fast_.Data_', 'fast_.Graph_', 'fast_.Data_', 'flatast'],
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'jira_crawler=flatast.jira_crawler:main', 
+        'ggnn=flatast.flatast2ggnn:main' 
+        ]},
+    install_requires=['flatbuffers', 'protobuf', 'urllib3', 'bs4', 'lxml'],
+    requires=['flatbuffers', 'protobuf', 'urllib3', 'bs4', 'lxml'],
+    description='The FlatAST serialization format for Python',
+)
