5ae29ede785a9f81f93a118a50defdacbdfb1547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package nevolution-risk/82/nevolution_risk-82-py3-none-any.whl

---
+++ packages/nevolution-risk/nevolution_risk-82-py3-none-any.whl/nevolution_risk/v2/logic/graph.py
@@ -0,0 +1,53 @@
+import os
+
+from nevolution_risk.constants.colors import green, blue, deepskyblue
+from nevolution_risk.v2.logic.graphlogic import GraphLoader, RiskGraph
+from nevolution_risk.v2.logic.node import Node
+from nevolution_risk.v2.logic.player import Player
+
+
+class Graph(object):
+
+    def __init__(self, player_positions):
+        self.player1 = Player('player_one', 120, green)
+        self.player2 = Player("player_two", 120, deepskyblue)
+        loader = GraphLoader()
+        dir_name = os.path.dirname(os.path.realpath(__file__))
+        source_graph = loader.load_graph(os.path.join(dir_name, '../../res', 'small.txt'))
+        self.graph = RiskGraph(graph=source_graph[0], coord=source_graph[1])
+        self.nodes = []
+        for n in range(0, self.graph.node_count):
+            self.add_node(Node(n, self.graph.get_attributes(n)))
+
+        for n in range(0, self.graph.node_count):
+            for m in self.graph.get_adjlist()[n]:
+                self.nodes[n].add_node_to_list(self.nodes[m])
+                self.nodes[m].add_node_to_list(self.nodes[n])
+
+        self.nodes[player_positions[0]].player = self.player1
+        self.nodes[player_positions[1]].player = self.player2
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def attack(self, v0, v1, attacker):
+        attack = self.nodes[v0]
+        defend = self.nodes[v1]
+        if defend in attack.adj_list:
+            if attack.player == attacker:
+                if defend.player.name == 'default':
+                    defend.player = attacker
+                    return True
+        return False
+
+    def is_conquered(self):
+        for v in self.nodes:
+            if v.player.name == 'default':
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    graph = Graph((1, 8))
+    for node in graph.nodes:
+        print(node.x, node.y)
