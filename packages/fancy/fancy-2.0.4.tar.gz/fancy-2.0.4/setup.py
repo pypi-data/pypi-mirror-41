7c714c43d343e6d3316eb852cafed184883a8f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:56 +0000

    Add package fancy/2.0.4/fancy-2.0.4.tar.gz

---
+++ packages/fancy/fancy-2.0.4.tar.gz/fancy-2.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+
+    name = 'fancy',
+    packages = setuptools.find_packages(), 
+    version = 'v2.0.4',
+    description = 'Framework for the Analysis of Cosmic raYs',
+    author = 'Francesca Capel',
+    author_email = 'capel.francesca@gmail.com',
+    url = 'https://github.com/cescalara/fancy',
+    download_url = 'https://github.com/cescalara/fancy/archive/v0.9.tar.gz',
+    keywords = ['UHECR', 'analysis'], 
+    classifiers = [],
+
+)
