a55aad050d1bb54c3f0de855e486e7999a6051bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package tghs/0.0.0/tghs-0.0.0.tar.gz

---
+++ packages/tghs/tghs-0.0.0.tar.gz/tghs-0.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Carles Sala",
+    author_email='carles@pythiac.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    license="MIT license",
+    include_package_data=True,
+    name='tghs',
+    url='https://github.com/csala/tghs',
+    version='0.0.0',
+    zip_safe=False,
+)
