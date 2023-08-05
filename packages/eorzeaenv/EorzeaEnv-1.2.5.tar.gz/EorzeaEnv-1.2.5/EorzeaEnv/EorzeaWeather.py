fb62f9ea6086c56fc08c9a554b711620274bebff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:36 +0000

    Add package eorzeaenv/1.2.5/EorzeaEnv-1.2.5.tar.gz

---
+++ packages/eorzeaenv/EorzeaEnv-1.2.5.tar.gz/EorzeaEnv-1.2.5/EorzeaEnv/EorzeaWeather.py
@@ -0,0 +1,64 @@
+import re
+
+from numpy import uint32
+from .Data.TerritoryWeather import territory as _territory
+from .Data.WeatherRate import weather_rate as _weather_rate
+from .Data.Weather import weather as _weather
+
+
+class EorzeaWeather:
+    """About XIV Weather"""
+
+    @staticmethod
+    def forecast_weather(placename, timestamp, lang='en'):
+        """Genrate forecast result.
+
+        Parameters
+        ----------
+        field : str
+            The placename of FFXIV.
+        timestamp : float
+            The timestamp of weather changing.
+        lang: Optional str
+            The language of result.
+
+        Returns:
+        ----------
+        string
+            The result of forecast.
+
+        Raises:
+        ----------
+        KeyError
+            When placename invalid.
+
+        """
+        weather_rate = None
+        placename = placename.lower()
+        target = _calculate_forecast_target(timestamp)
+        for p, r in _territory:
+            if re.search(p, placename):
+                weather_rate = r
+
+        if not weather_rate:
+            raise KeyError('Valid Eorzea placename required')
+
+        for r, w in _weather_rate[weather_rate]:
+            if target < r:
+                return _weather[w][lang]
+
+
+def _calculate_forecast_target(lt):
+    """Thanks to Rogueadyn's SaintCoinach library for this calculation.
+        1. Do the magic 'cause for calculations
+        2. 16:00 is 0, 00:00 is 8 and 08:00 is 16.
+        3. Take Eorzea days since unix epoch.
+    """
+
+    bell = lt / 175
+    increment = uint32(bell + 8 - (bell % 8)) % 24
+    total_days = uint32(lt / 4200)
+    calc_base = total_days * 0x64 + increment
+    step1 = uint32(calc_base << 0xB) ^ calc_base
+    step2 = (step1 >> 8) ^ step1
+    return int(step2 % 100)
