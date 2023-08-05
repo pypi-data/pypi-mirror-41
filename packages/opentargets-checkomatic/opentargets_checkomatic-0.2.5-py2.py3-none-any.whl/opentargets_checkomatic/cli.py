13fd23617d1d9cce3d62c7a51d8f33d365328d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package opentargets-checkomatic/0.2.5/opentargets_checkomatic-0.2.5-py2.py3-none-any.whl

---
+++ packages/opentargets-checkomatic/opentargets_checkomatic-0.2.5-py2.py3-none-any.whl/opentargets_checkomatic/cli.py
@@ -0,0 +1,112 @@
+from __future__ import absolute_import, print_function
+import sys
+import yaml
+import addict
+import functional as fn
+import jsonpath_rw as jp
+from docopt import docopt
+
+from opentargets_checkomatic.helpers import make_client, uri_open_and_read
+from opentargets_checkomatic.helpers import to_vlist, to_vset
+import opentargets_checkomatic.evaluators as evaluators
+
+ARGS = """OpenTargets Check-O-Matic.
+
+Usage:
+  checkomatic --file=<filename.yml> eval
+  checkomatic --file=<filename.yml> eval rule <rule-name>
+  checkomatic (-h | --help)
+  checkomatic --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -f --file=<filename.yml>  config filename.
+
+"""
+
+
+def main():
+    arguments = docopt(ARGS, version='OT Check-O-Matic v0.0.1')
+
+    if arguments['eval']:
+        conf_content = uri_open_and_read(arguments['--file'])
+        c = addict.Dict(yaml.safe_load(conf_content))
+        ot_client = make_client(c.checkomatic.client.host, c.checkomatic.client.port)
+
+        _g = globals()
+        _g['c'] = ot_client
+
+        results = []
+
+        p = jp.parse('asdf')
+        p.find
+
+        # remove all rules except <rule-name>
+        if arguments['rule']:
+            rule_name = arguments['<rule-name>']
+            rules = c.checkomatic.rules.keys()
+            rules_to_remove = list(set(rules) - set([rule_name]))
+            for r in rules_to_remove:
+                c.checkomatic.rules.pop(r, None)
+
+        if c.checkomatic.rules.targets:
+            results += evaluators.eval_targets(ot_client, c.checkomatic.rules.targets.to_dict(), _g)
+
+        if c.checkomatic.rules.diseases:
+            results += evaluators.eval_diseases(ot_client, c.checkomatic.rules.diseases.to_dict(), _g)
+
+        if c.checkomatic.rules.stats:
+            results += evaluators.eval_stats(ot_client, c.checkomatic.rules.stats, _g)
+
+        if c.checkomatic.rules.metrics:
+            results += evaluators.eval_metrics(ot_client, c.checkomatic.rules.metrics, _g)
+
+        if c.checkomatic.rules.associations.targets:
+            results += evaluators.eval_associations(ot_client,
+                                                    'target',
+                                                    c.checkomatic.rules.associations.targets.to_dict(),
+                                                    _g)
+
+        if c.checkomatic.rules.associations.diseases:
+            results += evaluators.eval_associations(ot_client,
+                                                    'disease',
+                                                    c.checkomatic.rules.associations.diseases.to_dict(),
+                                                    _g)
+
+        if c.checkomatic.rules.evidences:
+            results += evaluators.eval_evidences(ot_client,
+                                                 c.checkomatic.rules.evidences.to_dict(),
+                                                 _g,
+                                                 c.checkomatic.client.size)
+
+        if c.checkomatic.rules.searches.targets:
+            results += evaluators.eval_searches(ot_client,
+                                                'target',
+                                                c.checkomatic.rules.searches.targets.to_dict(),
+                                                _g,
+                                                c.checkomatic.client.size)
+
+        if c.checkomatic.rules.searches.diseases:
+            results += evaluators.eval_searches(ot_client,
+                                                'disease',
+                                                c.checkomatic.rules.searches.diseases.to_dict(),
+                                                _g,
+                                                c.checkomatic.client.size)
+
+        oks = fn.seq(results).filter(lambda e: e == True).count_by_value().to_list()
+        fails = fn.seq(results).filter(lambda e: e == False).count_by_value().to_list()
+        print("oks", oks, "fails", fails)
+
+        if len(fails) > 0 or len(oks) == 0:
+            return 1
+        else:
+            return 0
+
+
+def cli():
+    sys.exit(main())
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/opentargets-checkomatic/opentargets_checkomatic-0.2.5-py2.py3-none-any.whl/opentargets_checkomatic/evaluators.py
@@ -0,0 +1,151 @@
+import addict
+import itertools as iters
+import functional as fn
+
+from opentargets_checkomatic.helpers import failed_at, make_local_env
+
+
+def eval_section(assertion_list, global_dict, local_env):
+    def _eval_elem(elem, global_scope, local_scope):
+        if '\n' in elem:
+            exec(elem, global_scope, local_scope)
+            r = local_scope['output']
+            if not r:
+                failed_at(elem)
+            return r
+        else:
+            r = eval(elem, global_scope, local_scope)
+            if not r:
+                failed_at(elem)
+            return r
+
+    return list(iters.imap(lambda e: _eval_elem(e, global_dict, local_env),
+                           assertion_list if assertion_list is not None else []))
+
+
+def eval_stats(ot_client, assertion_list, global_env):
+    print('eval stats', 'all')
+    data = ot_client.get_stats()
+    if data.total > 0:
+        local_env = make_local_env(input=None, output=False)
+        stats_o = addict.Dict(fn.seq(data.to_object()).take(1).head())
+        local_env['d'] = stats_o.to_dict()
+        local_env['o'] = stats_o
+        asserted = eval_section(assertion_list, global_env, local_env)
+        return asserted
+    else:
+        return []
+
+
+def eval_metrics(ot_client, assertion_list, global_env):
+    print('eval metrics', 'all')
+    data = ot_client.get_metrics()
+    if data.total > 0:
+        local_env = make_local_env(input=None, output=False)
+        stats_o = addict.Dict(fn.seq(data.to_object()).take(1).head())
+        local_env['d'] = stats_o.to_dict()
+        local_env['o'] = stats_o
+        asserted = eval_section(assertion_list, global_env, local_env)
+        return asserted
+    else:
+        return []
+
+
+def eval_targets(ot_client, section_dict, global_env):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        data = ot_client.search(k, size=1, filter="target")
+        data = ot_client.get_target(k)
+        print('eval target', k)
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            target_o = data.to_object().next()
+            local_env['d'] = target_o.to_dict()
+            local_env['o'] = target_o
+            asserted_k = eval_section(v, global_env, local_env)
+            asserted += asserted_k
+        else:
+            asserted += [False]
+
+    return asserted
+
+
+def eval_diseases(ot_client, section_dict, global_env):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        data = ot_client.get_disease(k)
+        print('eval disease', k)
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            disease_o = data.to_object().next()
+            local_env['d'] = disease_o.to_dict()
+            local_env['o'] = disease_o
+            asserted_k = eval_section(v, global_env, local_env)
+            asserted += asserted_k
+        else:
+            asserted += [False]
+
+    return asserted
+
+
+def eval_associations(ot_client, by_type, section_dict, global_env):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        if by_type == 'target':
+            data = ot_client.get_associations_for_target(k)
+        else:
+            data = ot_client.get_associations_for_disease(k)
+
+        print('eval association', k)
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            data_d = {'data': fn.seq(data.to_object()).to_list()}
+            local_env['d'] = data_d
+            local_env['o'] = addict.Dict(data_d)
+            asserted_k = eval_section(v, global_env, local_env)
+            asserted += asserted_k
+        else:
+            asserted += [False]
+
+    return asserted
+
+
+def eval_evidences(ot_client, section_dict, global_env, max_retrieve_n=10000):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        (t, d) = k.split('-')
+        data = ot_client.filter_evidence(target=t, disease=d, size=max_retrieve_n)
+        print('eval evidence', k)
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            data_d = {'data': fn.seq(data.to_object()).to_list()}
+            local_env['d'] = data_d
+            local_env['o'] = addict.Dict(data_d)
+            asserted_k = eval_section(v, global_env, local_env)
+            asserted += asserted_k
+        else:
+            asserted += [False]
+
+    return asserted
+
+
+def eval_searches(ot_client, by_type, section_dict, global_env, max_retrieve_n=10000):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        if by_type == 'target':
+            data = ot_client.search(k, filter=by_type, size=max_retrieve_n)
+        else:
+            data = ot_client.search(k, filter=by_type, size=max_retrieve_n)
+
+        print('eval search', k)
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            data_d = {'data': fn.seq(data.to_object()).to_list()}
+            local_env['d'] = data_d
+            local_env['o'] = addict.Dict(data_d)
+            asserted_k = eval_section(v, global_env, local_env)
+            asserted += asserted_k
+        else:
+            asserted += [False]
+
+    return asserted
