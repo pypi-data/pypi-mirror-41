67f3e4275bf05be98766630405f5a1d0295f8e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package shan-nester/1.1.0/shan-nester-1.1.0.tar.gz

---
+++ packages/shan-nester/shan-nester-1.1.0.tar.gz/shan-nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+	name  = 'shan-nester',
+	version = '1.1.0',
+	py_modules = ['shan-nester'],
+	author = 'shan',
+	author_email = 'starter@gmail.com',
+	url = 'https://dummy.com',
+	description = "A simple recursive print list",
+)
+
