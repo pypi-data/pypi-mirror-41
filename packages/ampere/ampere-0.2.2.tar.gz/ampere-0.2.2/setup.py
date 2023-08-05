f651bb6bcb70926bcb1a695d6da62bb7ad8ea11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:52 +0000

    Add package ampere/0.2.2/ampere-0.2.2.tar.gz

---
+++ packages/ampere/ampere-0.2.2.tar.gz/ampere-0.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ampere",
+    version="0.2.2",
+    author="Neal Dawson-Elli",
+    author_email="nealde@uw.edu",
+    description="A Python package for working with battery discharge data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nealde/Ampere",
+    packages=setuptools.find_packages(),
+    install_requires=['matplotlib', 'numpy', 'scipy'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ),
+)
