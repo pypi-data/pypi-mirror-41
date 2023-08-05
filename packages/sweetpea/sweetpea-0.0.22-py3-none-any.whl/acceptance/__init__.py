cd530a52be3dd896b1d6ced6f66e0b4912188f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:53 +0000

    Add package sweetpea/0.0.22/sweetpea-0.0.22-py3-none-any.whl

---
+++ packages/sweetpea/sweetpea-0.0.22-py3-none-any.whl/acceptance/__init__.py
@@ -0,0 +1,22 @@
+from typing import Tuple, List
+
+from itertools import repeat
+
+from sweetpea.primitives import Factor
+from sweetpea.constraints import AtMostKInARow
+from sweetpea.internal import get_all_level_names
+
+
+def __assert_atmostkinarow_pair(k: int, level: Tuple[str, str], experiments: List[dict]) -> None:
+    sublist = list(repeat(level[1], k + 1))
+    for e in experiments:
+        assert sublist not in [e[level[0]][i:i+k+1] for i in range(len(e[level[0]]) - (k + 1))]
+
+
+def assert_atmostkinarow(c: AtMostKInARow, experiments: List[dict]) -> None:
+    if isinstance(c.levels, Factor):
+        level_tuples = get_all_level_names([c.levels])
+        for t in level_tuples:
+            __assert_atmostkinarow_pair(c.k, t, experiments)
+    else:
+        __assert_atmostkinarow_pair(c.k, c.levels, experiments)
