a809914f2f0eb08ba55fdb8f015079a7ec8faeb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package onepanel/2.12.12b2/onepanel-2.12.12b2.tar.gz

---
+++ packages/onepanel/onepanel-2.12.12b2.tar.gz/onepanel-2.12.12b2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from onepanel.constants import *
+
+setup(
+    name="onepanel",
+    version=CLI_VERSION,
+    packages = ['onepanel', 'onepanel.commands', 'onepanel.types', 'onepanel.models', 'onepanel.utilities','onepanel.git_hooks'],
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    install_requires=[
+        'prettytable',
+        'requests',
+        'click',
+        'PTable',
+        'configobj',
+        'websocket-client',
+        'humanize',
+        'configparser',
+        'awscli',
+        'watchdog',
+        'iso8601'
+    ],
+    entry_points='''
+        [console_scripts]
+        onepanel=onepanel.cli:main
+    ''',
+)
