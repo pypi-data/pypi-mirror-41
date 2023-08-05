72bf4b551f2007679a1ca974457aa144f930d5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:34 +0000

    Add package blinkpy/0.11.2/blinkpy-0.11.2-py3-none-any.whl

---
+++ packages/blinkpy/blinkpy-0.11.2-py3-none-any.whl/blinkpy/blinkpy.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+blinkpy by Kevin Fronczak - A Blink camera Python library.
+
+https://github.com/fronzbot/blinkpy
+Original protocol hacking by MattTW :
+https://github.com/MattTW/BlinkMonitorProtocol
+Published under the MIT license - See LICENSE file for more details.
+"Blink Wire-Free HS Home Monitoring & Alert Systems" is a trademark
+owned by Immedia Inc., see www.blinkforhome.com for more information.
+I am in no way affiliated with Blink, nor Immedia Inc.
+"""
+
+import time
+import getpass
+import logging
+from requests.structures import CaseInsensitiveDict
+import blinkpy.helpers.errors as ERROR
+from blinkpy import api
+from blinkpy.sync_module import BlinkSyncModule
+from blinkpy.helpers.util import (
+    create_session, merge_dicts, BlinkURLHandler,
+    BlinkAuthenticationException)
+from blinkpy.helpers.constants import (
+    BLINK_URL, LOGIN_URL, LOGIN_BACKUP_URL)
+
+REFRESH_RATE = 30
+
+_LOGGER = logging.getLogger('blinkpy')
+
+
+class Blink():
+    """Class to initialize communication."""
+
+    def __init__(self, username=None, password=None,
+                 refresh_rate=REFRESH_RATE):
+        """
+        Initialize Blink system.
+
+        :param username: Blink username (usually email address)
+        :param password: Blink password
+        :param refresh_rate: Refresh rate of blink information.
+                             Defaults to 15 (seconds)
+        """
+        self._username = username
+        self._password = password
+        self._token = None
+        self._auth_header = None
+        self._host = None
+        self.account_id = None
+        self.network_ids = []
+        self.urls = None
+        self.sync = CaseInsensitiveDict({})
+        self.region = None
+        self.region_id = None
+        self.last_refresh = None
+        self.refresh_rate = refresh_rate
+        self.session = None
+        self.networks = []
+        self.cameras = CaseInsensitiveDict({})
+        self._login_url = LOGIN_URL
+
+    @property
+    def auth_header(self):
+        """Return the authentication header."""
+        return self._auth_header
+
+    def start(self):
+        """
+        Perform full system setup.
+
+        Method logs in and sets auth token, urls, and ids for future requests.
+        Essentially this is just a wrapper function for ease of use.
+        """
+        if self._username is None or self._password is None:
+            self.login()
+        else:
+            self.get_auth_token()
+
+        networks = self.get_ids()
+        for network_name, network_id in networks.items():
+            sync_module = BlinkSyncModule(self, network_name, network_id)
+            sync_module.start()
+            self.sync[network_name] = sync_module
+        self.cameras = self.merge_cameras()
+
+    def login(self):
+        """Prompt user for username and password."""
+        self._username = input("Username:")
+        self._password = getpass.getpass("Password:")
+        if self.get_auth_token():
+            _LOGGER.info("Login successful!")
+            return True
+        _LOGGER.warning("Unable to login with %s.", self._username)
+        return False
+
+    def get_auth_token(self):
+        """Retrieve the authentication token from Blink."""
+        if not isinstance(self._username, str):
+            raise BlinkAuthenticationException(ERROR.USERNAME)
+        if not isinstance(self._password, str):
+            raise BlinkAuthenticationException(ERROR.PASSWORD)
+
+        login_url = LOGIN_URL
+        self.session = create_session()
+        response = api.request_login(self,
+                                     login_url,
+                                     self._username,
+                                     self._password)
+
+        if response.status_code == 200:
+            response = response.json()
+            (self.region_id, self.region), = response['region'].items()
+        else:
+            _LOGGER.debug(
+                ("Received response code %s "
+                 "when authenticating, "
+                 "trying new url"), response.status_code
+            )
+            login_url = LOGIN_BACKUP_URL
+            response = api.request_login(self,
+                                         login_url,
+                                         self._username,
+                                         self._password)
+            self.region_id = 'piri'
+            self.region = "UNKNOWN"
+
+        self._host = "{}.{}".format(self.region_id, BLINK_URL)
+        self._token = response['authtoken']['authtoken']
+        self._auth_header = {'Host': self._host,
+                             'TOKEN_AUTH': self._token}
+        self.networks = response['networks']
+        self.urls = BlinkURLHandler(self.region_id)
+        self._login_url = login_url
+
+        return self._auth_header
+
+    def get_ids(self):
+        """Set the network ID and Account ID."""
+        response = api.request_networks(self)
+        # Look for only onboarded network, flag warning if multiple
+        # since it's unexpected
+        all_networks = []
+        network_dict = {}
+        for network, status in self.networks.items():
+            if status['onboarded']:
+                all_networks.append('{}'.format(network))
+                network_dict[status['name']] = network
+
+        # For the first onboarded network we find, grab the account id
+        for resp in response['networks']:
+            if str(resp['id']) in all_networks:
+                self.account_id = resp['account_id']
+                break
+
+        self.network_ids = all_networks
+        return network_dict
+
+    def refresh(self, force_cache=False):
+        """
+        Perform a system refresh.
+
+        :param force_cache: Force an update of the camera cache
+        """
+        if self.check_if_ok_to_update() or force_cache:
+            for sync_name, sync_module in self.sync.items():
+                _LOGGER.debug("Attempting refresh of sync %s", sync_name)
+                sync_module.refresh(force_cache=force_cache)
+
+    def check_if_ok_to_update(self):
+        """Check if it is ok to perform an http request."""
+        current_time = int(time.time())
+        last_refresh = self.last_refresh
+        if last_refresh is None:
+            last_refresh = 0
+        if current_time >= (last_refresh + self.refresh_rate):
+            self.last_refresh = current_time
+            return True
+        return False
+
+    def merge_cameras(self):
+        """Merge all sync camera dicts into one."""
+        combined = CaseInsensitiveDict({})
+        for sync in self.sync:
+            combined = merge_dicts(combined, self.sync[sync].cameras)
+        return combined
---
+++ packages/blinkpy/blinkpy-0.11.2-py3-none-any.whl/blinkpy/sync_module.py
@@ -0,0 +1,187 @@
+"""Defines a sync module for Blink."""
+
+import logging
+
+from requests.structures import CaseInsensitiveDict
+from blinkpy import api
+from blinkpy.camera import BlinkCamera
+from blinkpy.helpers.constants import ONLINE
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BlinkSyncModule():
+    """Class to initialize sync module."""
+
+    def __init__(self, blink, network_name, network_id):
+        """
+        Initialize Blink sync module.
+
+        :param blink: Blink class instantiation
+        """
+        self.blink = blink
+        self._auth_header = blink.auth_header
+        self.network_id = network_id
+        self.region = blink.region
+        self.region_id = blink.region_id
+        self.name = network_name
+        self.serial = None
+        self.status = None
+        self.sync_id = None
+        self.host = None
+        self.summary = None
+        self.homescreen = None
+        self.network_info = None
+        self.record_dates = {}
+        self.videos = {}
+        self.events = []
+        self.cameras = CaseInsensitiveDict({})
+        self.all_clips = {}
+
+    @property
+    def attributes(self):
+        """Return sync attributes."""
+        attr = {
+            'name': self.name,
+            'id': self.sync_id,
+            'network_id': self.network_id,
+            'serial': self.serial,
+            'status': self.status,
+            'region': self.region,
+            'region_id': self.region_id,
+        }
+        return attr
+
+    @property
+    def urls(self):
+        """Return device urls."""
+        return self.blink.urls
+
+    @property
+    def online(self):
+        """Return boolean system online status."""
+        return ONLINE[self.status]
+
+    @property
+    def arm(self):
+        """Return status of sync module: armed/disarmed."""
+        return self.network_info['network']['armed']
+
+    @arm.setter
+    def arm(self, value):
+        """Arm or disarm system."""
+        if value:
+            return api.request_system_arm(self.blink, self.network_id)
+
+        return api.request_system_disarm(self.blink, self.network_id)
+
+    def start(self):
+        """Initialize the system."""
+        response = api.request_syncmodule(self.blink, self.network_id)
+        self.summary = response['syncmodule']
+        self.sync_id = self.summary['id']
+        self.network_id = self.summary['network_id']
+        self.serial = self.summary['serial']
+        self.status = self.summary['status']
+
+        self.events = self.get_events()
+
+        self.homescreen = api.request_homescreen(self.blink)
+
+        self.network_info = api.request_network_status(self.blink,
+                                                       self.network_id)
+
+        camera_info = self.get_camera_info()
+        for camera_config in camera_info:
+            name = camera_config['name']
+            self.cameras[name] = BlinkCamera(self)
+
+        self.videos = self.get_videos()
+        for camera_config in camera_info:
+            name = camera_config['name']
+            if name in self.cameras:
+                self.cameras[name].update(camera_config, force_cache=True)
+
+    def get_events(self):
+        """Retrieve events from server."""
+        response = api.request_sync_events(self.blink, self.network_id)
+        return response['event']
+
+    def get_camera_info(self):
+        """Retrieve camera information."""
+        response = api.request_cameras(self.blink, self.network_id)
+        return response['devicestatus']
+
+    def refresh(self, force_cache=False):
+        """Get all blink cameras and pulls their most recent status."""
+        self.events = self.get_events()
+        self.videos = self.get_videos()
+        self.homescreen = api.request_homescreen(self.blink)
+        self.network_info = api.request_network_status(self.blink,
+                                                       self.network_id)
+        camera_info = self.get_camera_info()
+        for camera_config in camera_info:
+            name = camera_config['name']
+            self.cameras[name].update(camera_config, force_cache=force_cache)
+
+    def get_videos(self, start_page=0, end_page=1):
+        """
+        Retrieve last recorded videos per camera.
+
+        :param start_page: Page to start reading from on blink servers
+                           (defaults to 0)
+        :param end_page: Page to stop reading from (defaults to 1)
+        """
+        videos = list()
+        all_dates = dict()
+
+        for page_num in range(start_page, end_page + 1):
+            this_page = api.request_videos(self.blink, page=page_num)
+            if not this_page:
+                break
+            elif 'message' in this_page:
+                _LOGGER.warning("Could not retrieve videos. Message: %s",
+                                this_page['message'])
+                break
+
+            videos.append(this_page)
+        _LOGGER.debug("Getting videos from page %s through %s",
+                      start_page,
+                      end_page)
+        for page in videos:
+            for entry in page:
+                try:
+                    camera_name = entry['camera_name']
+                    clip_addr = entry['address']
+                    thumb_addr = entry['thumbnail']
+                except TypeError:
+                    _LOGGER.warning("Could not extract video information.")
+                    break
+                clip_date = clip_addr.split('_')[-6:]
+                clip_date = '_'.join(clip_date)
+                clip_date = clip_date.split('.')[0]
+                try:
+                    self.all_clips[camera_name][clip_date] = clip_addr
+                except KeyError:
+                    self.all_clips[camera_name] = {clip_date: clip_addr}
+
+                if camera_name not in all_dates:
+                    all_dates[camera_name] = list()
+                all_dates[camera_name].append(clip_date)
+                try:
+                    self.videos[camera_name].append(
+                        {
+                            'clip': clip_addr,
+                            'thumb': thumb_addr,
+                        }
+                    )
+                except KeyError:
+                    self.videos[camera_name] = [
+                        {
+                            'clip': clip_addr,
+                            'thumb': thumb_addr,
+                        }
+                    ]
+        self.record_dates = all_dates
+        _LOGGER.debug("Retrieved a total of %s records", len(all_dates))
+        return self.videos
