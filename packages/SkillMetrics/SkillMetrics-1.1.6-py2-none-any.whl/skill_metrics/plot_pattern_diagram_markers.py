2cf7c9eb41a82ebd2471cd61edfa3724a5d791d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package SkillMetrics/1.1.6/SkillMetrics-1.1.6-py2-none-any.whl

---
+++ packages/SkillMetrics/SkillMetrics-1.1.6-py2-none-any.whl/skill_metrics/plot_pattern_diagram_markers.py
@@ -0,0 +1,145 @@
+import math
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+import matplotlib
+import warnings
+
+def plot_pattern_diagram_markers(X,Y,option):
+    '''
+    Plots color markers on a pattern diagram.
+    
+    Plots color markers on a target diagram according their (X,Y) 
+    locations. The symbols and colors are chosen automatically with a 
+    limit of 70 symbol & color combinations.
+    
+    The color bar is titled using the content of option['titleColorBar'] 
+    (if non-empty string).
+    
+    INPUTS:
+    x : x-coordinates of markers
+    y : y-coordinates of markers
+    z : z-coordinates of markers (used for color shading)
+    option : dictionary containing option values. (Refer to 
+        GET_TARGET_DIAGRAM_OPTIONS function for more information.)
+    option['axismax'] : maximum for the X & Y values. Used to limit
+        maximum distance from origin to display markers
+    option['markerlabel'] : labels for markers
+    
+    OUTPUTS:
+    None
+
+    Created on Nov 30, 2016
+    Revised on Jan 6, 2019
+    
+    Author: Peter A. Rochford
+        Symplectic, LLC
+        www.thesymplectic.com
+        prochford@thesymplectic.com
+    '''
+
+    # Set face color transparency
+    alpha = option['alpha']
+    
+    # Set font and marker size
+    fontSize = matplotlib.rcParams.get('font.size') - 2
+    markerSize = option['markersize']
+    
+    if option['markerlegend'] == 'on':
+        # Check that marker labels have been provided
+        if option['markerlabel'] == '':
+            raise ValueError('No marker labels provided.')
+
+        # Plot markers of different color and shapes with labels 
+        # displayed in a legend
+        
+        # Define markers
+        kind = ['+','o','x','s','d','^','v','p','h','*']
+        colorm = ['b','r','g','c','m','y','k']
+        if len(X) > 70:
+            _disp('You must introduce new markers to plot more than 70 cases.')
+            _disp('The ''marker'' character array need to be extended inside the code.')
+        
+        if len(X) <= len(kind):
+            # Define markers with specified color
+            marker = []
+            markercolor = []
+            for color in colorm:
+                for symbol in kind:
+                    marker.append(symbol + option['markercolor'])
+                    rgba = clr.to_rgb(option['markercolor']) + (alpha,)
+                    markercolor.append(rgba)
+        else:
+            # Define markers and colors using predefined list
+            marker = []
+            markercolor = [] #Bug Fix: missing array initialization
+            for color in colorm:
+                for symbol in kind:
+                    marker.append(symbol + color)
+                    rgba = clr.to_rgb(color) + (alpha,)
+                    markercolor.append(rgba)
+        
+        # Plot markers at data points
+        limit = option['axismax']
+        hp = ()
+        markerlabel = []
+        for i, xval in enumerate(X):
+            if abs(X[i]) <= limit and abs(Y[i]) <= limit:
+                h = plt.plot(X[i],Y[i],marker[i], markersize = markerSize, 
+                     markerfacecolor = markercolor[i],
+                     markeredgecolor = marker[i][1],
+                     markeredgewidth = 2)
+                hp += tuple(h)
+                markerlabel.append(option['markerlabel'][i])
+        
+        # Add legend
+        maxLabelLength = len(max(markerlabel, key=len))
+        if len(markerlabel) == 0:
+            warnings.warn('No markers within axis limit ranges.')
+        elif len(markerlabel) <= 6 and maxLabelLength <= 6:
+            # Put legend in a default location
+            markerlabel = tuple(markerlabel)
+            hLegend = plt.legend(hp, markerlabel, loc = 'upper right',
+                                 fontsize = fontSize, numpoints=1,
+                                 bbox_to_anchor=(1.2,1.0))
+
+        else:
+            # Put legend to right of the plot in multiple columns as needed
+
+            nmarkers = len(markerlabel)
+            ncol = int(math.ceil(nmarkers / 15.0))
+            markerlabel = tuple(markerlabel)
+
+            # Shift figure to include legend
+            plt.gcf().subplots_adjust(right=0.6)
+
+            # Plot legend of multi-column markers
+            # Note: do not use bbox_to_anchor as this cuts off the legend
+            hLegend = plt.legend(hp, markerlabel, loc = (1.1, 0.25),
+                        fontsize = fontSize, numpoints=1, ncol = ncol)
+    else:
+        # Plot markers as dots of a single color with accompanying labels
+        # and no legend
+        
+        # Plot markers at data points
+        limit = option['axismax']
+        rgba = clr.to_rgb(option['markercolor']) + (alpha,) 
+        for i,xval in enumerate(X):
+            if abs(X[i]) <= limit and abs(Y[i]) <= limit:
+                # Plot marker
+                plt.plot(X[i],Y[i],'.', markersize = markerSize, 
+                     markerfacecolor = rgba,
+                     markeredgecolor = option['markercolor'])
+                
+                # Check if marker labels provided
+                if len(option['markerlabel']) > 0:
+                    # Label marker
+                    xtextpos = X[i] #ToDo: convert to double?
+                    ytextpos = Y[i]
+                    plt.text(xtextpos,ytextpos,option['markerlabel'][i], 
+                             color = option['markerlabelcolor'],
+                             verticalalignment = 'bottom',
+                             horizontalalignment = 'right',
+                             fontsize = fontSize)
+
+def _disp(text):
+    print(text)
