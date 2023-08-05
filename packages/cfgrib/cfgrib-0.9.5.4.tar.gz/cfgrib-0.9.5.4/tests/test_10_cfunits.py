8187582a7b2ec9a698a24aee2e182fb25ec30342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package cfgrib/0.9.5.4/cfgrib-0.9.5.4.tar.gz

---
+++ packages/cfgrib/cfgrib-0.9.5.4.tar.gz/cfgrib-0.9.5.4/tests/test_10_cfunits.py
@@ -0,0 +1,13 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+xr = pytest.importorskip('xarray')  # noqa
+
+from cf2cdm import cfunits
+
+
+def test_are_convertible():
+    assert cfunits.are_convertible('m', 'm')
+    assert cfunits.are_convertible('hPa', 'Pa')
+    assert not cfunits.are_convertible('m', 'Pa')
