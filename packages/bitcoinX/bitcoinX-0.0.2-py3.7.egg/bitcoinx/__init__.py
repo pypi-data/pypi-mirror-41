d25db31a329a28a85f903726617ab4ac03e262cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package bitcoinX/0.0.2/bitcoinX-0.0.2-py3.7.egg

---
+++ packages/bitcoinX/bitcoinX-0.0.2-py3.7.egg/bitcoinx/__init__.py
@@ -0,0 +1,16 @@
+from .chain import *
+from .coin import *
+from .hashes import *
+from .packing import *
+from .work import *
+
+_version_str = '0.0.2'
+_version = tuple(int(part) for part in _version_str.split('.'))
+
+__all__ = sum((
+    chain.__all__,
+    coin.__all__,
+    hashes.__all__,
+    packing.__all__,
+    work.__all__,
+), ())
