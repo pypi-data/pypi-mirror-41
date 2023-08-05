321ca8cc9e9659f675d535b0b69b9b47277b686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:44 +0000

    Add package sense-data/0.2.5/sense-data-0.2.5.tar.gz

---
+++ packages/sense-data/sense-data-0.2.5.tar.gz/sense-data-0.2.5/test/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''                                                          
+Copyright (C)2018 SenseDeal AI, Inc. All Rights Reserved                                                      
+File: {name}.py
+Author: xuwei                                        
+Email: weix@sensedeal.ai                                 
+Last modified: 2018.12.23
+Description:                                            
+'''
+
+import sense_core as sd
+sd.log_init_config('error_log', sd.config('log_path'))
+from sense_data import *
+
+xw =SenseDataService()
+
+# res = xw.get_stock_price_day('000001', '2019-1-18')
+res = xw.get_company_alias([])
+
+print(res)
