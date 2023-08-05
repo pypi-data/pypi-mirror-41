37e741a809a5d2c79d556a28c593f092b9deb4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package sprm/0.1.19/sprm-0.1.19-py3-none-any.whl

---
+++ packages/sprm/sprm-0.1.19-py3-none-any.whl/sprm/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 12:17:17 2018
+
+@author: Sven Serneels, Ponalytics
+"""
+
+__name__ = "sprm"
+__author__ = "Sven Serneels"
+__license__ = "GNU"
+__version__ = "0.1.19"
+__date__ = "2019-01-26"
+
+from .robcent import robcent
+from .sprm import snipls,sprm
+from .rm import rm
+
+
+
+
