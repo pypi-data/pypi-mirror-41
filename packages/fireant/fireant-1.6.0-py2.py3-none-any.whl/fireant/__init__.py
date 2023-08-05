3db9033e965119241814ac566b5e3d297fb1a75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package fireant/1.6.0/fireant-1.6.0-py2.py3-none-any.whl

---
+++ packages/fireant/fireant-1.6.0-py2.py3-none-any.whl/fireant/__init__.py
@@ -0,0 +1,8 @@
+# noinspection PyUnresolvedReferences
+from .database import *
+# noinspection PyUnresolvedReferences
+from .slicer import *
+# noinspection PyUnresolvedReferences
+from .slicer.widgets import *
+
+__version__ = '1.6.0'
