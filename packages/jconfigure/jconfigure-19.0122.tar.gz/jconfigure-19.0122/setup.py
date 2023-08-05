3cec432a1c8997015c47f957faa5e9671b2051d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:24 +0000

    Add package jconfigure/19.0122/jconfigure-19.0122.tar.gz

---
+++ packages/jconfigure/jconfigure-19.0122.tar.gz/jconfigure-19.0122/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="jconfigure",
+    packages=["jconfigure"],
+    version="19.0122",
+    description="A python configuration management library",
+    author="John Norwood",
+    author_email="norwood.john.m@gmail.com",
+    url="https://github.com/norwoodj/jconfigure",
+    download_url="https://github.com/norwoodj/jconfigure/archive/19.0122.tar.gz",
+    keywords=["configuration"],
+    classifiers=[],
+    install_requires=[
+        "PyYAML==3.12",
+    ]
+)
