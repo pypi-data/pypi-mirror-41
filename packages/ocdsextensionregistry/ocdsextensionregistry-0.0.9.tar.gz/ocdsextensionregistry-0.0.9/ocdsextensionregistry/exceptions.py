21fb5e3671e098d94cb210eae90d4ffe97a97b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:30 +0000

    Add package ocdsextensionregistry/0.0.9/ocdsextensionregistry-0.0.9.tar.gz

---
+++ packages/ocdsextensionregistry/ocdsextensionregistry-0.0.9.tar.gz/ocdsextensionregistry-0.0.9/ocdsextensionregistry/exceptions.py
@@ -0,0 +1,14 @@
+class OCDSExtensionRegistryError(Exception):
+    """Base class for exceptions from within this package"""
+
+
+class DoesNotExist(OCDSExtensionRegistryError):
+    """Raised if an object wasn't found for the given parameters"""
+
+
+class MissingExtensionMetadata(OCDSExtensionRegistryError):
+    """Raised if a method call requires extensions metadata, with which the extension registry was not initialized"""
+
+
+class CommandError(OCDSExtensionRegistryError):
+    """Errors from within this package's CLI"""
