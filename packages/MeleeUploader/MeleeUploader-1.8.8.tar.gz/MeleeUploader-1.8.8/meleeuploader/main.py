554f016e557908c2932a172a25b3d21c786129c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:49 +0000

    Add package MeleeUploader/1.8.8/MeleeUploader-1.8.8.tar.gz

---
+++ packages/MeleeUploader/MeleeUploader-1.8.8.tar.gz/MeleeUploader-1.8.8/meleeuploader/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+from . import form
+from . import consts
+from . import youtube as yt
+
+import pyforms_lite
+
+
+def main():
+    if "linux" in sys.platform:  # root needed for writing files
+        if os.geteuid() != 0:
+            print("Need sudo for writing files")
+            subprocess.call(['sudo', 'python3', sys.argv[0]])
+    # Always get the initial YT credentials outside of a thread. Threads break the setup process.
+    yt.get_youtube_service()
+    pyforms_lite.start_app(form.MeleeUploader, geometry=(200, 200, 1, 1))
+
+
+def ult():
+    consts.melee = False
+    main()
+
+
+if __name__ == "__main__":
+    main()
