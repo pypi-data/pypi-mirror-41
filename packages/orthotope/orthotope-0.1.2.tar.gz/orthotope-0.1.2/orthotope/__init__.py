6b9533a8a3c2ce734b71db023d5d6a6ca3e8cd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package orthotope/0.1.2/orthotope-0.1.2.tar.gz

---
+++ packages/orthotope/orthotope-0.1.2.tar.gz/orthotope-0.1.2/orthotope/__init__.py
@@ -0,0 +1,1 @@
+from .orthotope import *
---
+++ packages/orthotope/orthotope-0.1.2.tar.gz/orthotope-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="orthotope",
+    version="0.1.2",
+    packages=["orthotope"],
+    description="tensor with named axes and indexes",
+
+    author="xieyuheng",
+    author_email="xyheme@gmail.com",
+
+    python_requires=">=3",
+
+    license="GPLv3",
+)
