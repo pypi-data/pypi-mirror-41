3d32ce8e7986bff471b5788bbb3dad6d06dc7be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package molecule-galaxy-wrapper/0.0.3/molecule-galaxy-wrapper-0.0.3.tar.gz

---
+++ packages/molecule-galaxy-wrapper/molecule-galaxy-wrapper-0.0.3.tar.gz/molecule-galaxy-wrapper-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="molecule-galaxy-wrapper",
+    version="0.0.3",
+    author="Bernardo Vale",
+    author_email="bernardosilveiravale@gmail.com",
+    description="A wrapper for ansible-galaxy that allows you to define role dependencies inside a requirements.yml",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bernardoVale/molecule-galaxy-wrapper",
+    keywords='molecule ansible ansible-galaxy galaxy dependency',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'ruamel.yaml>=0.15.87',
+    ],
+    entry_points={
+        'console_scripts': ['galaxywrapper=molecule_wrapper.cli:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
