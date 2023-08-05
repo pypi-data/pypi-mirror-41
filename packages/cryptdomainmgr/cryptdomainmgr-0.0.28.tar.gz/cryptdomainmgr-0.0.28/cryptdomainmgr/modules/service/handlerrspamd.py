479dbfcfbfd2122a506fa84cabaf7d208e412fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package cryptdomainmgr/0.0.28/cryptdomainmgr-0.0.28.tar.gz

---
+++ packages/cryptdomainmgr/cryptdomainmgr-0.0.28.tar.gz/cryptdomainmgr-0.0.28/cryptdomainmgr/modules/service/handlerrspamd.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: UTF8 -*-
+
+#######################################################################
+#
+#    Copyright (c) 2018 Stefan Helmert <stefan.helmert@t-online.de>
+#
+#######################################################################
+
+from simpleloggerplus import simpleloggerplus as log
+from subprocess import check_output, CalledProcessError
+from cryptdomainmgr.modules.common.cdmstatehelper import isReady
+
+def prepare(serviceConfig, serviceState, state):
+    serviceState.setOpStateDone()
+
+def rollover(serviceConfig, serviceState, state):
+    serviceState.setOpStateWaiting()
+    if not isReady(serviceConfig, state, 'dkim'):
+        return
+    serviceState.setOpStateRunning()
+    log.info('  -> Rspamd reload')
+    try:
+        rv = check_output(('systemctl', 'start', 'rspamd'))
+        #rv = check_output(('systemctl', 'reload', 'rspamd')) # this is not working with rspamd
+        rv = check_output(('rspamadm', 'control', 'reload'))
+    except CalledProcessError as e:
+        log.error(e.output)
+        raise(e)
+    serviceState.setOpStateDone()
+
+def cleanup(serviceConfig, serviceState, state):
+    serviceState.setOpStateDone()
+
