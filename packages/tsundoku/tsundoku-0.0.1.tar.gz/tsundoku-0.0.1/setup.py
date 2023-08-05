37342936099ff228a7db001cec39c7282f0a46c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package tsundoku/0.0.1/tsundoku-0.0.1.tar.gz

---
+++ packages/tsundoku/tsundoku-0.0.1.tar.gz/tsundoku-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-17 alvations
+# URL:
+# For license information, see LICENSE.md
+
+from distutils.core import setup
+
+setup(
+    name='tsundoku',
+    version='0.0.1',
+    packages=['tsundoku',],
+    url = 'https://github.com/alvations/tsundonku',
+    description='Coursework for Text Processing using Machine Learning at NUS-ISS',
+    license="MIT",
+)
