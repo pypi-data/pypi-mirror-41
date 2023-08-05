c513a48b35b75ea9e0215cc40f1e11c614ca08f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package win-bash-aliases/0.0.1/win-bash-aliases-0.0.1.tar.gz

---
+++ packages/win-bash-aliases/win-bash-aliases-0.0.1.tar.gz/win-bash-aliases-0.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+from win_aliases import aliases
+
+
+def console_scripts():
+    scripts = []
+    for alias in aliases:
+        s = f'{alias} = win_aliases.__init__:{alias}'
+        scripts.append(s)
+    return scripts
+
+
+setup(
+    name='win-bash-aliases',
+    version='0.0.1',
+    url='https://github.com/spyoungtech/win-bash-aliases/',
+    license='MIT',
+    author='Spencer Young',
+    author_email='spencer.young@spyoung.com',
+    description='Windows WSL bash aliases for cmd and powershell',
+    packages=['win_aliases'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': console_scripts()
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
