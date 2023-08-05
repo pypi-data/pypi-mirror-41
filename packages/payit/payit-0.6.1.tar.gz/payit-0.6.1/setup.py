ea017fdf0f8530e1d508bf4dc2a326257a9c64ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package payit/0.6.1/payit-0.6.1.tar.gz

---
+++ packages/payit/payit-0.6.1.tar.gz/payit-0.6.1/setup.py
@@ -0,0 +1,48 @@
+import re
+import os.path
+import sys
+
+from setuptools import setup, find_packages
+
+package_name = 'payit'
+py_version = sys.version_info[:2]
+
+# reading package's version (same way sqlalchemy does)
+with open(os.path.join(os.path.dirname(__file__), package_name, '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+dependencies = [
+    'zeep',
+    'requests',
+    'py3rijndael >= 0.3.0'
+]
+
+if py_version < (3, 5):
+    dependencies.append('typing')
+
+setup(
+    name=package_name,
+    version=package_version,
+    author='Mahdi Ghanea.g',
+    description='Online payment gateways wrapper library.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/meyt/payit',
+    packages=find_packages(),
+    install_requires=dependencies,
+    license='MIT License',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
