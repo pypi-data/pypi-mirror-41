8314b500b187ed76b8412475a594f318c2ea9cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package batquant/0.0.1/batquant-0.0.1.tar.gz

---
+++ packages/batquant/batquant-0.0.1.tar.gz/batquant-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="batquant",
+    version="0.0.1",
+    author="Ninh Chu",
+    author_email="chulucninh09@gmail.com",
+    description="A package to get Vietnamese stock data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://chulucninh09.visualstudio.com/QuantTrading/_git/batquant",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
