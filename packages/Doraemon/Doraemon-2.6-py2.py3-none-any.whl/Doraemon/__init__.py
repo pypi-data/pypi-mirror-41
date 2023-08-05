ff9a1a083ea51a2027b7b3bf6effa07ef80bc62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:42 +0000

    Add package Doraemon/2.6/Doraemon-2.6-py2.py3-none-any.whl

---
+++ packages/Doraemon/Doraemon-2.6-py2.py3-none-any.whl/Doraemon/__init__.py
@@ -0,0 +1,13 @@
+# Requests
+from Doraemon.Requests import chrome_dora
+from Doraemon.Requests import requests_dora
+from Doraemon.Requests import proxies_dora
+
+# OnlineSeach
+from Doraemon.OnlineSearch import google_KG
+from Doraemon.OnlineSearch import google_translator
+from Doraemon.OnlineSearch import dianping
+from Doraemon.OnlineSearch import whois
+
+# Others
+from Doraemon.Others import logging_dora
