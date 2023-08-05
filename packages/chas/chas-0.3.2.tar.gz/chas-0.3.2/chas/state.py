72e3273c02ccac40fd7ad3ee03b051efa38159d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:11 +0000

    Add package chas/0.3.2/chas-0.3.2.tar.gz

---
+++ packages/chas/chas-0.3.2.tar.gz/chas-0.3.2/chas/state.py
@@ -0,0 +1,28 @@
+import datetime
+
+class State:
+    def __init__(self):
+        self.status = "N/A"
+        self.detail = None
+        self.timestamp_created = int(datetime.datetime.now().timestamp())
+        self.timestamp_finished = None
+            
+    def success(self, detail=None):
+        self.status = "Succeeded"
+        if detail is not None:
+            self.detail = detail
+        return self
+    
+    def failed(self):
+        self.status = "Failed"
+        return self
+    
+    def running(self):
+        self.status = "Running"
+        return self
+    
+    def finished(self):
+        if self.status not in ["Succeeded", "Failed"]:
+            self.status = "Finished"
+        return self
+
