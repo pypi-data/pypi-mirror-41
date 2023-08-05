5b918bf4b4d0771cdd80723180eb060087bbfc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:17 +0000

    Add package logicbit/1.2.6/logicbit-1.2.6-py2-none-any.whl

---
+++ packages/logicbit/logicbit-1.2.6-py2-none-any.whl/logicbit/__init__.py
@@ -0,0 +1,9 @@
+from logicbit import logic
+from logicbit import clock
+from logicbit import keyboard
+from logicbit import utils
+from logicbit import quine_mccluskey
+
+__version__ = "1.2.6"
+
+print("logicbit version "+__version__)
