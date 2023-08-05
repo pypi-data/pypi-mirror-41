21ac822d37fff5f41dd7d9c9e23bb986bec864f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package pyparsing-highlighting/0.2.2/pyparsing_highlighting-0.2.2-py3-none-any.whl

---
+++ packages/pyparsing-highlighting/pyparsing_highlighting-0.2.2-py3-none-any.whl/pp_highlighting/__init__.py
@@ -0,0 +1,11 @@
+"""Syntax highlighting for prompt_toolkit and HTML with pyparsing."""
+
+from .pp_highlighter import DummyStyler, PPHighlighter, Styler
+from .pp_validator import PPValidator
+
+dummy_styler = DummyStyler()
+
+__all__ = ['dummy_styler', 'DummyStyler', 'PPHighlighter', 'PPValidator',
+           'Styler']
+
+__version__ = '0.2.2'
