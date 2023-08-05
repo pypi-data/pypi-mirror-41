f9806f52950b61a06379d1696a5e7deec0a3b1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:28 +0000

    Add package synopy/0.0.0.post0/synopy-0.0.0.post0.tar.gz

---
+++ packages/synopy/synopy-0.0.0.post0.tar.gz/synopy-0.0.0.post0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='synopy',
+    version='0.0.0.post0',
+    packages=['synopy'],
+    url='https://github.com/graingert/synopy',
+    license='MIT',
+    author='George Spanos',
+    author_email='spanosgeorge@gmail.com',
+    maintainer='Thomas Grainger',
+    maintainer_email='synopy@graingert.co.uk',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Python library for the Synology DiskStation APIs',
+    install_requires=[
+        'requests>=2, <3.0.0',
+        'six>=1, <2.0.0',
+    ],
+)
