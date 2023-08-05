c78dbab0cca801287afd974b0cfdfa72100ff54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:31 +0000

    Add package sqlite-utils/0.7/sqlite_utils-0.7-py3-none-any.whl

---
+++ packages/sqlite-utils/sqlite_utils-0.7-py3-none-any.whl/sqlite_utils/cli.py
@@ -0,0 +1,94 @@
+import click
+import sqlite_utils
+import json
+
+
+@click.group()
+@click.version_option()
+def cli():
+    "Commands for interacting with a SQLite database"
+    pass
+
+
+@cli.command()
+@click.argument(
+    "path",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+@click.option(
+    "--fts4", help="Just show FTS4 enabled tables", default=False, is_flag=True
+)
+@click.option(
+    "--fts5", help="Just show FTS5 enabled tables", default=False, is_flag=True
+)
+def tables(path, fts4, fts5):
+    """List the tables in the database"""
+    db = sqlite_utils.Database(path)
+    for name in db.table_names(fts4=fts4, fts5=fts5):
+        print(name)
+
+
+@cli.command()
+@click.argument(
+    "path",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+def vacuum(path):
+    """Run VACUUM against the database"""
+    sqlite_utils.Database(path).vacuum()
+
+
+@cli.command()
+@click.argument(
+    "path",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+@click.option("--no-vacuum", help="Don't run VACUUM", default=False, is_flag=True)
+def optimize(path, no_vacuum):
+    """Optimize all FTS tables and then run VACUUM - should shrink the database file"""
+    db = sqlite_utils.Database(path)
+    tables = db.table_names(fts4=True) + db.table_names(fts5=True)
+    with db.conn:
+        for table in tables:
+            db[table].optimize()
+    if not no_vacuum:
+        db.vacuum()
+
+
+@cli.command()
+@click.argument(
+    "path",
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+@click.argument("table")
+@click.argument("json_file", type=click.File(), required=True)
+@click.option("--pk", help="Column to use as the primary key, e.g. id")
+def insert(path, table, json_file, pk):
+    "Insert records from JSON file into the table, create table if it is missing"
+    db = sqlite_utils.Database(path)
+    docs = json.load(json_file)
+    if isinstance(docs, dict):
+        docs = [docs]
+    db[table].insert_all(docs, pk=pk)
+
+
+@cli.command()
+@click.argument(
+    "path",
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+@click.argument("table")
+@click.argument("json_file", type=click.File(), required=True)
+@click.option("--pk", help="Column to use as the primary key, e.g. id")
+def upsert(path, table, json_file, pk):
+    "Upsert records based on their primary key"
+    db = sqlite_utils.Database(path)
+    docs = json.load(json_file)
+    if isinstance(docs, dict):
+        docs = [docs]
+    db[table].upsert_all(docs, pk=pk)
---
+++ packages/sqlite-utils/sqlite_utils-0.7-py3-none-any.whl/tests/test_cli.py
@@ -0,0 +1,132 @@
+from sqlite_utils import cli, Database
+from click.testing import CliRunner
+import json
+import os
+import pytest
+import sqlite3
+
+
+CREATE_TABLES = """
+create table Gosh (c1 text, c2 text, c3 text);
+create table Gosh2 (c1 text, c2 text, c3 text);
+"""
+
+
+@pytest.fixture
+def db_path(tmpdir):
+    path = str(tmpdir / "test.db")
+    db = sqlite3.connect(path)
+    db.executescript(CREATE_TABLES)
+    return path
+
+
+def test_tables(db_path):
+    result = CliRunner().invoke(cli.cli, ["tables", db_path])
+    assert "Gosh\nGosh2" == result.output.strip()
+
+
+def test_tables_fts4(db_path):
+    Database(db_path)["Gosh"].enable_fts(["c2"], fts_version="FTS4")
+    result = CliRunner().invoke(cli.cli, ["tables", "--fts4", db_path])
+    assert "Gosh_fts" == result.output.strip()
+
+
+def test_tables_fts5(db_path):
+    Database(db_path)["Gosh"].enable_fts(["c2"], fts_version="FTS5")
+    result = CliRunner().invoke(cli.cli, ["tables", "--fts5", db_path])
+    assert "Gosh_fts" == result.output.strip()
+
+
+def test_vacuum(db_path):
+    result = CliRunner().invoke(cli.cli, ["vacuum", db_path])
+    assert 0 == result.exit_code
+
+
+def test_optimize(db_path):
+    db = Database(db_path)
+    with db.conn:
+        for table in ("Gosh", "Gosh2"):
+            db[table].insert_all(
+                [
+                    {
+                        "c1": "verb{}".format(i),
+                        "c2": "noun{}".format(i),
+                        "c3": "adjective{}".format(i),
+                    }
+                    for i in range(10000)
+                ]
+            )
+        db["Gosh"].enable_fts(["c1", "c2", "c3"], fts_version="FTS4")
+        db["Gosh2"].enable_fts(["c1", "c2", "c3"], fts_version="FTS5")
+    size_before_optimize = os.stat(db_path).st_size
+    result = CliRunner().invoke(cli.cli, ["optimize", db_path])
+    assert 0 == result.exit_code
+    size_after_optimize = os.stat(db_path).st_size
+    assert size_after_optimize < size_before_optimize
+    # Sanity check that --no-vacuum doesn't throw errors:
+    result = CliRunner().invoke(cli.cli, ["optimize", "--no-vacuum", db_path])
+    assert 0 == result.exit_code
+
+
+def test_insert_simple(tmpdir):
+    json_path = str(tmpdir / "dog.json")
+    db_path = str(tmpdir / "dogs.db")
+    open(json_path, "w").write(json.dumps({"name": "Cleo", "age": 4}))
+    result = CliRunner().invoke(cli.cli, ["insert", db_path, "dogs", json_path])
+    assert 0 == result.exit_code
+    assert [{"age": 4, "name": "Cleo"}] == Database(db_path).execute_returning_dicts(
+        "select * from dogs"
+    )
+    db = Database(db_path)
+    assert ["dogs"] == db.table_names()
+    assert [] == db["dogs"].indexes
+
+
+def test_insert_with_primary_key(db_path, tmpdir):
+    json_path = str(tmpdir / "dog.json")
+    open(json_path, "w").write(json.dumps({"id": 1, "name": "Cleo", "age": 4}))
+    result = CliRunner().invoke(
+        cli.cli, ["insert", db_path, "dogs", json_path, "--pk", "id"]
+    )
+    assert 0 == result.exit_code
+    assert [{"id": 1, "age": 4, "name": "Cleo"}] == Database(
+        db_path
+    ).execute_returning_dicts("select * from dogs")
+    db = Database(db_path)
+    assert ["id"] == db["dogs"].pks
+
+
+def test_insert_multiple_with_primary_key(db_path, tmpdir):
+    json_path = str(tmpdir / "dogs.json")
+    dogs = [{"id": i, "name": "Cleo {}".format(i), "age": i + 3} for i in range(1, 21)]
+    open(json_path, "w").write(json.dumps(dogs))
+    result = CliRunner().invoke(
+        cli.cli, ["insert", db_path, "dogs", json_path, "--pk", "id"]
+    )
+    assert 0 == result.exit_code
+    assert dogs == Database(db_path).execute_returning_dicts(
+        "select * from dogs order by id"
+    )
+    db = Database(db_path)
+    assert ["id"] == db["dogs"].pks
+
+
+def test_upsert(db_path, tmpdir):
+    test_insert_multiple_with_primary_key(db_path, tmpdir)
+    json_path = str(tmpdir / "upsert.json")
+    db = Database(db_path)
+    assert 20 == db["dogs"].count
+    upsert_dogs = [
+        {"id": 1, "name": "Upserted 1", "age": 4},
+        {"id": 2, "name": "Upserted 2", "age": 4},
+        {"id": 21, "name": "Fresh insert 21", "age": 6},
+    ]
+    open(json_path, "w").write(json.dumps(upsert_dogs))
+    result = CliRunner().invoke(
+        cli.cli, ["upsert", db_path, "dogs", json_path, "--pk", "id"]
+    )
+    assert 0 == result.exit_code
+    assert 21 == db["dogs"].count
+    assert upsert_dogs == db.execute_returning_dicts(
+        "select * from dogs where id in (1, 2, 21) order by id"
+    )
