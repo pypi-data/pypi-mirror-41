490d6c1b8ed79216551faacbfa06dfea3f5433b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:13 +0000

    Add package ac-ptk/0.0.12/ac-ptk-0.0.12.tar.gz

---
+++ packages/ac-ptk/ac-ptk-0.0.12.tar.gz/ac-ptk-0.0.12/performance/meminfo.py
@@ -0,0 +1,12 @@
+class meminfo:
+    def __init__(self):
+        self.java_heap = 0
+        self.native_heap = 0
+        self.code = 0
+        self.stack = 0
+        self.graphics = 0
+        self.other = 0
+        self.system = 0
+        self.total = 0
+
+
