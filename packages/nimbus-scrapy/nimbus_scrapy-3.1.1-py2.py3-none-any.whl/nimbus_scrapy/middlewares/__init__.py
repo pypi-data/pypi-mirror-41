b6e25a232d96a391598d36d5577ec7cd5e82c410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package nimbus-scrapy/3.1.1/nimbus_scrapy-3.1.1-py2.py3-none-any.whl

---
+++ packages/nimbus-scrapy/nimbus_scrapy-3.1.1-py2.py3-none-any.whl/nimbus_scrapy/middlewares/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, division, unicode_literals
+
+from .deltafetch import DeltaFetch
+
+from .httpproxy import HttpProxyMiddleware
+
+from .retry import RetryProxyMiddleware
+
+from .maxcount import MaxCountMiddleware
+
