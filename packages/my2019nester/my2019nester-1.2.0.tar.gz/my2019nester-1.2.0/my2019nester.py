ace25754ec98984569c4c3d753bdadc742b64d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package my2019nester/1.2.0/my2019nester-1.2.0.tar.gz

---
+++ packages/my2019nester/my2019nester-1.2.0.tar.gz/my2019nester-1.2.0/my2019nester.py
@@ -0,0 +1,14 @@
+""" Function to print recursive lists """
+def printList(myList, nested = False, level=0):
+	""" Check if is a list and recurse on the list """
+	if isinstance(myList, list):
+		for each_entry in myList:
+			if isinstance(each_entry, list):
+				printList(each_entry, level + 1)
+			else:
+				if nested:
+					for each_level in range(level):
+						print("\t", end='')
+				print(each_entry)
+	else :
+		print (myList);
---
+++ packages/my2019nester/my2019nester-1.2.0.tar.gz/my2019nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+	name  = 'my2019nester',
+	version = '1.2.0',
+	py_modules = ['my2019nester'],
+	author = 'shan',
+	author_email = 'starter@gmail.com',
+	url = 'https://dummy.com',
+	description = "A simple recursive print list",
+)
+
