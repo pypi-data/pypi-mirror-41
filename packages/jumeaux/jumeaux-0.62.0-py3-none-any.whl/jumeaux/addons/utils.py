2a26a9d66afa15ba0fade59f25da3d225990390b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:16 +0000

    Add package jumeaux/0.62.0/jumeaux-0.62.0-py3-none-any.whl

---
+++ packages/jumeaux/jumeaux-0.62.0-py3-none-any.whl/jumeaux/addons/utils.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+import ast
+import re
+from typing import Any
+
+import pydash as py_
+from jinja2 import Environment, BaseLoader
+from jinja2.exceptions import TemplateSyntaxError
+from owlmixin import TOption
+
+
+def exact_match(target: str, regexp: str):
+    return bool(re.search(f'^{regexp}$', target))
+
+
+def get_by_diff_key(dic: dict, diff_key: str) -> Any:
+    return py_.get(dic, diff_key
+                   .replace("root", "")
+                   .replace("><", ".")
+                   .replace(">", "")
+                   .replace("<", "")
+                   .replace("'", ""))
+
+
+ENV = Environment(loader=BaseLoader())
+ENV.filters['reg'] = exact_match
+
+
+def when_filter(when: str, data: dict) -> bool:
+    return ast.literal_eval(ENV.from_string('{{' + when + '}}').render(data))
+
+
+def when_optional_filter(when: TOption[str], data: dict) -> bool:
+    return when.map(lambda x: when_filter(x, data)).get_or(True)
+
+
+def jinja2_format(fmt: str, data: dict) -> str:
+    return ENV.from_string(fmt).render(data)
+
+
+def get_jinja2_format_error(fmt: str) -> TOption[str]:
+    try:
+        ENV.from_string(fmt)
+        return TOption(None)
+    except TemplateSyntaxError as err:
+        return TOption(err.message)
