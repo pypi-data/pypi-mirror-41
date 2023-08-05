ac8c1b2bbf8dafc59cc6ed3f77a463d5d4459a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package fish-prode/1.1.0.post2/fish_prode-1.1.0.post2.tar.gz

---
+++ packages/fish-prode/fish_prode-1.1.0.post2.tar.gz/fish_prode-1.1.0.post2/CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+
+## [1.1.0] - 2018-08-25
+### Changed
+- Refactored as Python package.
+
+
+
+## [0.0.1] - 2017-08-06 - First release
+
+
+
+[1.1.0] https://github.com/ggirelli/fish-prode/releases/tag/v1.1.0  
+[0.0.1] https://github.com/ggirelli/fish-prode/releases/tag/v0.0.1  
---
+++ packages/fish-prode/fish_prode-1.1.0.post2.tar.gz/fish_prode-1.1.0.post2/setup.py
@@ -0,0 +1,48 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open('README.md', "r") as f:
+	long_description = f.read()
+
+setup(name='fish_prode',
+	version='1.1.0.post2',
+	description='''A FISH probe design web interface.''',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/ggirelli/fish-prode',
+	author='Gabriele Girelli',
+	author_email='gabriele.girelli@scilifelab.se',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Bio-Informatics',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3 :: Only',
+	],
+	keywords='biology cell DNA RNA FISH fluorescence hybridization bioimaging genome',
+	packages=["fish_prode", "fish_prode.sections", "fish_prode.sections.probe_design"],
+	install_requires=[
+		"bottle>=0.12.13",
+		"matplotlib>=2.2.2",
+		"numpy>=1.14.2",
+		"pandas>=0.22.0",
+		"paste>=2.0.3",
+		"scipy>=1.0.0"
+	],
+	scripts=["bin/fprode_dbextract", "bin/fprode_dbquery", "bin/fprode_serve"],
+	package_data={
+		'fish_prode': ['css/bootstrap.css', 'css/*', 'fonts/*', 'js/*', 'views/*'],
+		'fish_prode.sections.probe_design': ['css/*', 'documents/*', 'views/*']
+	},
+	test_suite="nose.collector",
+	tests_require=["nose"],
+)
