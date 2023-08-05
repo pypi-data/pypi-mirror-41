7d87dcd483a995361d3872ec8c5d1d42c4a6bf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:38 +0000

    Add package docassemble.webapp/0.3.36/docassemble.webapp-0.3.36.tar.gz

---
+++ packages/docassemble.webapp/docassemble.webapp-0.3.36.tar.gz/docassemble.webapp-0.3.36/docassemble/webapp/watchdog.py
@@ -0,0 +1,33 @@
+import psutil
+import time
+import sys
+
+busy_pids = set()
+
+while True:
+    busy_now = set()
+    no_longer_busy = set()
+    for pid in psutil.pids():
+        try:
+            p = psutil.Process(pid)
+            if p.name() == 'apache2' and p.cpu_times().user > 90.0 and p.cpu_percent(interval=1.0) > 19.0:
+                busy_now.add(pid)
+        except:
+            continue
+    for pid in busy_pids:
+        if not pid in busy_now:
+            no_longer_busy.add(pid)
+    for pid in no_longer_busy:
+        busy_pids.discard(pid)
+    for pid in busy_now:
+        if pid in busy_pids:
+            try:
+                p = psutil.Process(pid)
+                p.kill()
+            except:
+                pass
+            sys.stderr.write("Killed " + unicode(pid))
+            busy_pids.discard(pid)
+        else:
+            busy_pids.add(pid)
+    time.sleep(30)
