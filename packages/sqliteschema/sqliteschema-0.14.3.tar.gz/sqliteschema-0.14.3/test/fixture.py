9c51deb37e54d5b90ebcad0ba9cafb3454f8e0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:05 +0000

    Add package sqliteschema/0.14.3/sqliteschema-0.14.3.tar.gz

---
+++ packages/sqliteschema/sqliteschema-0.14.3.tar.gz/sqliteschema-0.14.3/test/fixture.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+import simplesqlite as sqlite
+from tabledata import TableData
+
+
+@pytest.fixture
+def database_path(tmpdir):
+    p = tmpdir.join("tmp.db")
+    db_path = str(p)
+    con = sqlite.SimpleSQLite(db_path, "w")
+
+    con.create_table_from_tabledata(
+        TableData("testdb0", ["attr_a", "attr b"], [[1, 2], [3, 4]]), index_attr_list=["attr_a"]
+    )
+
+    con.create_table_from_tabledata(
+        TableData("testdb1", ["foo", "bar", "hoge"], [[1, 2.2, "aa"], [3, 4.4, "bb"]]),
+        index_attr_list=("foo", "hoge"),
+    )
+
+    con.create_table(
+        "constraints",
+        [
+            "primarykey_id INTEGER PRIMARY KEY",
+            "notnull_value REAL NOT NULL",
+            "unique_value INTEGER UNIQUE",
+        ],
+    )
+
+    return db_path
+
+
+@pytest.fixture
+def mb_database_path(tmpdir):
+    p = tmpdir.join("mb_database_path.db")
+    db_path = str(p)
+    con = sqlite.SimpleSQLite(db_path, "w")
+
+    con.create_table_from_tabledata(
+        TableData("テーブル", ["いち", "に"], [[1, 2], [3, 4]]), index_attr_list=["いち"]
+    )
+
+    return db_path
