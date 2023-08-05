d52f27f670f26571ec8dc7c31dc23cfd1bde9665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:22 +0000

    Add package awsme/0.2.1/awsme-0.2.1-py2.py3-none-any.whl

---
+++ packages/awsme/awsme-0.2.1-py2.py3-none-any.whl/awsme/cloud_watch.py
@@ -0,0 +1,52 @@
+import datetime
+import logging
+from typing import Optional, Dict, Callable
+
+import boto3
+
+from .metric import Metric
+
+
+logger = logging.getLogger(__name__)
+
+
+class CloudWatch:
+    """CloudWatch client, provides method log, that records metric
+    using recorder_class instance.
+    """
+
+    def __init__(self,
+                 namespace: str,
+                 default_dimensions: Dict[str, str],
+                 recorder_class: Callable,
+                 **kwargs) -> None:
+        client = boto3.client(
+            'cloudwatch',
+            **kwargs,
+        )
+        self.default_dimensions = default_dimensions
+        self._recorder = recorder_class(namespace, client)
+
+    def log(self,
+            name: str,
+            dimensions: Optional[Dict[str, str]] = None,
+            value: float = 1,
+            **kwargs) -> None:
+        """Record metric.
+
+        ``dimensions`` are merged with default_dimensions.
+        Keyword arguments are passed to Metric constructor.
+        """
+        complete_dimensions = self.default_dimensions.copy()
+        complete_dimensions.update(dimensions or {})
+        self._record_metric(Metric(
+            event_time=datetime.datetime.utcnow(),
+            name=name,
+            value=value,
+            dimensions=complete_dimensions,
+            **kwargs,
+        ))
+
+    def _record_metric(self, metric: Metric) -> None:
+        logger.debug('log: %r', metric)
+        self._recorder.put_metric(metric)
