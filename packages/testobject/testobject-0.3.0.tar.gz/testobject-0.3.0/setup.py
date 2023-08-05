088d1adfaf4532a5f15b69982ab2ecd5ae1bf36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package testobject/0.3.0/testobject-0.3.0.tar.gz

---
+++ packages/testobject/testobject-0.3.0.tar.gz/testobject-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+setup(
+    name = 'testobject',
+    packages = ['testobject'],
+    version = '0.3.0',
+    description = 'Python API wrapper for TestObject',
+    author = 'Enrique Gonzalez',
+    author_email = 'egonzalezh94@gmail.com',
+    license='MIT',
+    url = 'https://github.com/enriquegh/testobject-python-api',
+    install_requires=['requests', 'pyyaml'],
+    long_description=long_description,
+    keywords = ['wrapper-api', 'testobject'],
+    classifiers = [],
+)
