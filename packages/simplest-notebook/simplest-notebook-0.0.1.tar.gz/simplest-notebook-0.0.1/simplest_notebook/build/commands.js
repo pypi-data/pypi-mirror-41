921b1cf9a0b9d9dbe775934e0f889bdd6aed26be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package simplest-notebook/0.0.1/simplest-notebook-0.0.1.tar.gz

---
+++ packages/simplest-notebook/simplest-notebook-0.0.1.tar.gz/simplest-notebook-0.0.1/simplest_notebook/build/commands.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * The map of command ids used by the notebook.
+ */
+exports.CmdIds = {
+    invoke: 'completer:invoke',
+    select: 'completer:select',
+    invokeNotebook: 'completer:invoke-notebook',
+    selectNotebook: 'completer:select-notebook',
+    save: 'notebook:save',
+    interrupt: 'notebook:interrupt-kernel',
+    restart: 'notebook:restart-kernel',
+    switchKernel: 'notebook:switch-kernel',
+    runAndAdvance: 'notebook-cells:run-and-advance',
+    deleteCell: 'notebook-cells:delete',
+    selectAbove: 'notebook-cells:select-above',
+    selectBelow: 'notebook-cells:select-below',
+    extendAbove: 'notebook-cells:extend-above',
+    extendBelow: 'notebook-cells:extend-below',
+    editMode: 'notebook:edit-mode',
+    merge: 'notebook-cells:merge',
+    split: 'notebook-cells:split',
+    commandMode: 'notebook:command-mode',
+    undo: 'notebook-cells:undo',
+    redo: 'notebook-cells:redo'
+};
