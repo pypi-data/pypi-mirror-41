36b9c885da336c2e90a02f6ec1dc415776d2b120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:37 +0000

    Add package alephclient/0.10.3/alephclient-0.10.3-py2.py3-none-any.whl

---
+++ packages/alephclient/alephclient-0.10.3-py2.py3-none-any.whl/alephclient/errors.py
@@ -0,0 +1,15 @@
+
+
+class AlephException(Exception):
+
+    def __init__(self, response):
+        self.status = response.status_code
+        self.response = response
+        try:
+            data = response.json()
+            self.message = data.get('message')
+        except Exception:
+            self.message = response.text
+
+    def __str__(self):
+        return self.message
