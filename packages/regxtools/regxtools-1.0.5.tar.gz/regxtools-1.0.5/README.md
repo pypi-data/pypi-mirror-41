93079c7aa0a8994ca78f30dc871eacdd0aec6e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package regxtools/1.0.5/regxtools-1.0.5.tar.gz

---
+++ packages/regxtools/regxtools-1.0.5.tar.gz/regxtools-1.0.5/README.md
@@ -0,0 +1,16 @@
+# RegxTools
+**This is still in early development stage, stable version is not yet, but soon released**
+
+
+### Installation
+```sh
+pip install regxtools
+```
+
+### Tools
+- [Regx](https://github.com/JeanMGirard/RegxTools/wiki/Regx)
+- [RegxLib](https://github.com/JeanMGirard/RegxTools/wiki/RegxLib) Library of regex, templates, pre-defined generators, etc..
+- [RegxTool](https://github.com/JeanMGirard/RegxTools/wiki/RegxTool) A tool for using and managing Regx objects
+- [RegxGen](https://github.com/JeanMGirard/RegxTools/wiki/RegxGen) A tool for generating Regx matrixes and tables
+
+
---
+++ packages/regxtools/regxtools-1.0.5.tar.gz/regxtools-1.0.5/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+
+setup(
+    name="regxtools",
+    version="1.0.5",
+
+    author="Jean M. Girard",
+    author_email="Jean.M.Girard@Outlook.com",
+
+    description="Tools for regular expressions ",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeanMGirard/RegxTools",
+    keywords='code-listing regex development',
+    packages=[
+        'regxtools'
+    ],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/JeanMGirard/RegxTools/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Source': 'https://github.com/JeanMGirard/RegxTools',
+    },
+    install_requires=[
+    ],
+    classifiers=[
+        #   3 - Alpha               #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    #entry_points={  # Optional
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+)
