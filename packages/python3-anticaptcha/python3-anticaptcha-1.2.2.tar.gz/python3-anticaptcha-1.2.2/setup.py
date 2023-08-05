1b13c21246abcae6bbabc63f81ae9c864cb90473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:32 +0000

    Add package python3-anticaptcha/1.2.2/python3-anticaptcha-1.2.2.tar.gz

---
+++ packages/python3-anticaptcha/python3-anticaptcha-1.2.2.tar.gz/python3-anticaptcha-1.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name = 'python3-anticaptcha',
+    version = '1.2.2',
+    author = 'AndreiDrang, redV0ID',
+
+    packages = ['python3_anticaptcha'],
+    install_requires = [
+                        'requests==2.21.0',
+                        'aiohttp==3.5.4',
+                        'pika==0.13.0'
+    ],
+    description = 'Python 3 Anti-Captcha service library with AIO module.',
+    package_dir={'python3-anticaptcha': 'python3_anticaptcha'},
+    include_package_data=True,
+    url = 'https://github.com/AndreiDrang/python3-anticaptcha',
+    author_email = 'drang.andray@gmail.com',
+    license = 'MIT',
+    keywords = '''captcha 
+                  anticaptcha 
+				  python3
+				  recaptcha
+				  captcha
+				  security
+				  api
+				  python-library
+				  python-anticaptcha
+				  anticaptcha-client''',
+    python_requires = '>=3.6',
+    zip_safe=False
+)
