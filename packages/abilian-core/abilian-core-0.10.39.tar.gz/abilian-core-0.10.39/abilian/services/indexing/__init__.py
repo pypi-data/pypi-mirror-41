843ea7f46b3076c64fc0206e208526515398df8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package abilian-core/0.10.39/abilian-core-0.10.39.tar.gz

---
+++ packages/abilian-core/abilian-core-0.10.39.tar.gz/abilian-core-0.10.39/abilian/services/indexing/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+""""""
+from __future__ import absolute_import, division, print_function
+
+from .schema import indexable_role
+from .service import service
+
+__all__ = ["service", "indexable_role"]
---
+++ packages/abilian-core/abilian-core-0.10.39.tar.gz/abilian-core-0.10.39/abilian/web/http.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""View decorators for controlling some aspect of HTTP, mainly: Cache
+headers."""
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from functools import wraps
+
+from flask import make_response
+
+
+def nocache(view):
+    @wraps(view)
+    def _nocache(*args, **kwargs):
+        resp = make_response(view(*args, **kwargs))
+        resp.headers["Cache-Control"] = "no-cache"
+        return resp
+
+    return _nocache
