75a05070946d7d7a89760c8eac478cbb9c5beab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:18 +0000

    Add package nester-ziwei/1.0.0/nester-ziwei-1.0.0.tar.gz

---
+++ packages/nester-ziwei/nester-ziwei-1.0.0.tar.gz/nester-ziwei-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester-ziwei',
+    version = '1.0.0',
+    py_modules = ['nester-ziwei'],
+    author = 'Ziwei',
+    author_email = 'zhuang13@stevens.edu',
+    url = 'http://www.google.com',
+    description = 'Test',
+    )
