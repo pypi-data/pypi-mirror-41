4dc3007d66cca84917ecb4bd30885a1b58752d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:15 +0000

    Add package tablextract/1.0.3/tablextract-1.0.3.tar.gz

---
+++ packages/tablextract/tablextract-1.0.3.tar.gz/tablextract-1.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tablextract',
+    version='1.0.3',
+    author='Juan C. Roldan',
+    author_email='juancarlos@sevilla.es',
+    description='Extract the information represented in any HTML table',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'bs4',
+        'etk',
+        'nltk',
+        'requests',
+        'scikit-learn',
+        'wikipediaapi',
+        'selenium'
+    ],
+    url='https://github.com/juancroldan/tablextract',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
