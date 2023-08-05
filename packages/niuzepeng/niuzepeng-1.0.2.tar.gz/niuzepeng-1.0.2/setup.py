597f9f52a848df1c75670ef36e2b4a0a2b8b10d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:40 +0000

    Add package niuzepeng/1.0.2/niuzepeng-1.0.2.tar.gz

---
+++ packages/niuzepeng/niuzepeng-1.0.2.tar.gz/niuzepeng-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+      name='niuzepeng',
+      version='1.0.2',
+      description='This is my first create!',
+      author='Niuzepeng',
+      author_email='18217127859@163.com',
+      url="https://github.com/zepen",
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+)
