97d2f572ff54c1b0c10407a84ddf678e0aa29516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:52 +0000

    Add package py-ddd/0.1.0/py-ddd-0.1.0.tar.gz

---
+++ packages/py-ddd/py-ddd-0.1.0.tar.gz/py-ddd-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='py-ddd',
+    version='0.1.0',
+    author='Pavel V. Pristupa',
+    author_email='pristupa@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    scripts=[],
+    url='https://github.com/pristupa/ddd',
+    license='MIT',
+    description='Domain-Driven Design for Python',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'dataclasses == 0.6',
+    ],
+    python_requires=">=3.6",
+)
