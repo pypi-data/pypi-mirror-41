b71954c361f901351478a73fe1ec3140041bc7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:52 +0000

    Add package stashy/0.6/stashy-0.6.tar.gz

---
+++ packages/stashy/stashy-0.6.tar.gz/stashy-0.6/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='stashy',
+      version="0.6",
+      description='Python API client for the  Atlassian Bitbucket Server (formerly known as Stash) REST API',
+      long_description=read('README.md'),
+      url='http://github.com/cosmin/stashy',
+      download_url = 'https://github.com/cosmin/stashy',
+      author='Cosmin Stejerean',
+      author_email='cosmin@offbytwo.com',
+      license='Apache License 2.0',
+      packages=['stashy', 'stashy.admin'],
+      test_suite = 'tests',
+      #scripts=['bin/stash'],
+      #tests_require=open('test-requirements.txt').readlines(),
+      install_requires=open('requirements.txt').readlines(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ]
+     )
