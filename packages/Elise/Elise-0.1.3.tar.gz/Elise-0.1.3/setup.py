3df9eb4363dd69cd1b65dc3e56c6dc922891f196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package Elise/0.1.3/Elise-0.1.3.tar.gz

---
+++ packages/Elise/Elise-0.1.3.tar.gz/Elise-0.1.3/setup.py
@@ -0,0 +1,20 @@
+# encoding=utf-8
+# date: 2019/1/22
+__author__ = "Masako"
+
+from distutils.core import setup
+
+setup(
+    name='Elise',
+    version='0.1.3',
+    author='Masako',
+    author_email='Masako@example.com',
+    packages=['sdist'],
+    url='https://github.com/TitledPythonFile',
+    license='GPL',
+    description='Simple multithreading spider',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests==2.19.1",
+    ],
+)
