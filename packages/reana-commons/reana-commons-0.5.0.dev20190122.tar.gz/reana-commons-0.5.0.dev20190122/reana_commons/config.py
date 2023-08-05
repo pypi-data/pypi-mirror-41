07cd6f5635adc9f2aa75ea92134f894b3e76b002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package reana-commons/0.5.0.dev20190122/reana-commons-0.5.0.dev20190122.tar.gz

---
+++ packages/reana-commons/reana-commons-0.5.0.dev20190122.tar.gz/reana-commons-0.5.0.dev20190122/reana_commons/config.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""REANA Commons configuration."""
+
+import os
+
+MQ_URL = os.getenv('RABBIT_MQ_URL',
+                   'message-broker.default.svc.cluster.local')
+"""Message queue (RabbitMQ) server host name."""
+
+MQ_USER = os.getenv('RABBIT_MQ_USER', 'test')
+"""Message queue (RabbitMQ) user name."""
+
+MQ_PASS = os.getenv('RABBIT_MQ_PASS', '1234')
+"""Message queue (RabbitMQ) password."""
+
+MQ_PORT = os.getenv('RABBIT_MQ_PORT', 5672)
+"""Message queue (RabbitMQ) service port."""
+
+MQ_CONNECTION_STRING = os.getenv('RABBIT_MQ', 'amqp://{0}:{1}@{2}//'.format(
+    MQ_USER, MQ_PASS, MQ_URL))
+"""Message queue (RabbitMQ) connection string."""
+
+MQ_DEFAULT_FORMAT = 'json'
+"""Default serializing format (to consume/produce)."""
+
+MQ_DEFAULT_EXCHANGE = ''
+"""Message queue (RabbitMQ) exchange."""
+
+MQ_DEFAULT_QUEUES = {'jobs-status':
+                     {'routing_key': 'jobs-status',
+                      'exchange': MQ_DEFAULT_EXCHANGE,
+                      'durable': False},
+                     'workflow-submission':
+                     {'routing_key': 'workflow-submission',
+                      'exchange': MQ_DEFAULT_EXCHANGE,
+                      'durable': True}
+                     }
+"""Default message queues."""
+
+MQ_PRODUCER_MAX_RETRIES = 3
+"""Max retries to send a message."""
+
+OPENAPI_SPECS = {
+    'reana-workflow-controller': (
+        'http://{address}:{port}'.format(
+            address=os.getenv('WORKFLOW_CONTROLLER_SERVICE_HOST', '0.0.0.0'),
+            port=os.getenv('WORKFLOW_CONTROLLER_SERVICE_PORT_HTTP', '5000')),
+        'reana_workflow_controller.json'),
+    'reana-server': (
+        os.getenv('REANA_SERVER_URL'),
+        'reana_server.json'),
+    'reana-job-controller': (
+        'http://{address}:{port}'.format(
+            address=os.getenv('JOB_CONTROLLER_SERVICE_HOST', '0.0.0.0'),
+            port=os.getenv('JOB_CONTROLLER_SERVICE_PORT_HTTP', '5000')),
+        'reana_job_controller.json')
+}
+"""REANA Workflow Controller address."""
+
+K8S_MAXIMUM_CONCURRENT_JOBS = 10
+"""Upper limit on concurrent jobs running in the cluster."""
+
+REANA_READY_CONDITIONS = {'reana_commons.tasks':
+                          ['check_predefined_conditions',
+                           'check_running_job_count']}
