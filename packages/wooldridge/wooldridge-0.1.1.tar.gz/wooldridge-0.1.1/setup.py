94acf1250c3b5a12e854aa84602aa306adc56e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:24 +0000

    Add package wooldridge/0.1.1/wooldridge-0.1.1.tar.gz

---
+++ packages/wooldridge/wooldridge-0.1.1.tar.gz/wooldridge-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import glob
+import os
+import sys
+from setuptools import find_packages, setup
+
+
+additional_files = []
+for filename in glob.iglob('./wooldridge/**', recursive=True):
+    if '.csv.bz' in filename:
+        additional_files.append(filename.replace('./wooldridge/', ''))
+
+setup(
+    name='wooldridge',
+    version='0.1.1',
+    author='Tetsu Haruyama',
+    author_email='tetsu.yes@gmail.com',
+    packages=find_packages(),
+    package_dir={'wooldridge': './wooldridge'},
+    include_package_data=True,
+    package_data={'wooldridge': additional_files},
+    url='https://github.com/spring-haru/wooldridge',
+    license='LICENSE',
+    description='Data sets from Introductory Econometrics: A Modern Approach (6th ed, J.M. Wooldridge)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['data', 'wooldridge', 'econometrics']
+)
