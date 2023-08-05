ae50d82626769e13d4edd8c5c0f2f914a7655ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package smake-buildtools/0.1.0/smake_buildtools-0.1.0.tar.gz

---
+++ packages/smake-buildtools/smake_buildtools-0.1.0.tar.gz/smake_buildtools-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="smake_buildtools",
+    version="0.1.0",
+    packages=['smake_buildtools'],
+    entry_points={
+    },
+    install_requires=[],
+    description='Simple Make replacement',
+    author='Qetyzm',
+    author_email='qetyzm@gmail.com',
+    url='https://github.com/qetyzm/smake',
+    keywords=['packages', 'import', 'downloader', 'package-management'],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read()
+)
