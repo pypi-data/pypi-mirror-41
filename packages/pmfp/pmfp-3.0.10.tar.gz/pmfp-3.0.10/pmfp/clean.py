304c9bc34c2a31f45ef1a31d60f5b6da18cea8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:16 +0000

    Add package pmfp/3.0.10/pmfp-3.0.10.tar.gz

---
+++ packages/pmfp/pmfp-3.0.10.tar.gz/pmfp-3.0.10/pmfp/clean.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+from pmfp.const import PROJECT_HOME
+
+
+def remove_readonly(func, path, _):
+    """Clear the readonly bit and reattempt the removal."""
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
+def clean(total=False):
+    print("清理项目开始")
+    if total:
+        EXCEPT = ["pmfprc.json",".git",".gitignore"]
+        for p in PROJECT_HOME.iterdir():
+            if (p.name not in EXCEPT) and (not p.name.startswith(".")):
+                if p.is_file():
+                    try:
+                        os.remove(str(p))
+                    except Exception as e:
+                        print(f"因为错误{str(e)}跳过删除文件 {str(p)}")
+                        continue
+                else:
+                    try:
+                        shutil.rmtree(str(p), onerror=remove_readonly)
+                    except Exception as e:
+                        print(f"因为错误{str(e)}跳过删除目录 {str(p)}")
+                        continue
+            else:
+                continue
+    else:
+        rms = ["document", "env", "dockerfile","node_modules",
+               "__pycache__", "test", "test_package",
+               "CMakeFiles", "CMakeCache.txt"]
+        for p in PROJECT_HOME.iterdir():
+            if p.name in rms:
+                if p.is_file():
+                    try:
+                        os.remove(str(p))
+                    except Exception as e:
+                        print(f"因为错误{str(e)}跳过删除文件 {str(p)}")
+                        continue
+                else:
+                    try:
+                        shutil.rmtree(str(p), onerror=remove_readonly)
+                    except Exception as e:
+                        print(f"因为错误{str(e)}跳过删除目录 {str(p)}")
+                        continue
+    print("清理项目结束")
