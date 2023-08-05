a71ab8a0a7c629fe56b581a0cdaa2bcb15d7eed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package Docify/0.0.3/Docify-0.0.3-py3-none-any.whl

---
+++ packages/Docify/Docify-0.0.3-py3-none-any.whl/docify/formatters/markdown.py
@@ -0,0 +1,20 @@
+from docify import Document
+from docify.lib.formatter import Formatter
+
+
+DOC_TMPL = '''\
+{}
+
+
+This document is generated using [Docify](https://github.com/rapidstack/docify)
+'''
+
+
+class Markdown(Formatter):
+    '''Markdown formatter.
+    By default Document object becomes markdown text when casted into string.
+    So it's basically doing noting except adding a grumpy citation.
+    '''
+    handlers = {
+        Document: lambda x: DOC_TMPL.format(x)
+    }
---
+++ packages/Docify/Docify-0.0.3-py3-none-any.whl/docify/lib/document.py
@@ -0,0 +1,33 @@
+class Document(object):
+    '''The base document object.
+    It can be passed as input to separate formatters which can parse
+    it into various formats.
+
+    Example usage: ::
+
+        doc = Document(Span('im a span !'))
+        print(HTML(doc))
+        print(Markdown(doc))
+        print(Whatever(doc))
+
+    :param Component[] components: Components to be added in the document'''
+
+    def __init__(self, *components):
+        self.components = []
+        self.depth = 0
+        for c in components:
+            self.add(c)
+
+    def __str__(self):
+        return '\n\n'.join(map(str, self.components))
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self)
+
+    def add(self, component):
+        '''Adds a component in the doc.
+
+        :param Component component: Component to be added'''
+
+        component.setparent(self)
+        self.components.append(component)
