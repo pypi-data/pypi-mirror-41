bbc41d34446fb44c8cd08cb1e249cca80d794628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package rabbitforyou1/1.0.0/rabbitforyou1-1.0.0.tar.gz

---
+++ packages/rabbitforyou1/rabbitforyou1-1.0.0.tar.gz/rabbitforyou1-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name		= 'rabbitforyou1',
+	version		= '1.0.0',
+	py_modules	= ['rabbitforyou1'],
+	author		= 'rabbitforyou',
+	description	= 'useless',
+)
