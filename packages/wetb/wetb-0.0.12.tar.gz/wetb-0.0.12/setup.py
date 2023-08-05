58fc22ba96c62c55ff5838c58802b3d2ee378b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:52 +0000

    Add package wetb/0.0.12/wetb-0.0.12.tar.gz

---
+++ packages/wetb/wetb-0.0.12.tar.gz/wetb-0.0.12/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for wetb.
+"""
+
+import os
+import sys
+from setuptools import setup, find_packages
+import wetb
+__version__ = wetb.__version__
+
+try:
+    from pypandoc import convert_file
+    read_md = lambda f: convert_file(f, 'rst', format='md')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+import numpy as np
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+def setup_package():
+
+    ex_info = [('wetb.fatigue_tools.rainflowcounting', ['pair_range', 'peak_trough', 'rainflowcount_astm']),
+			   ('wetb.signal.filters', ['cy_filters'])]
+    extlist = [Extension('%s.%s' % (module, n),
+                         [os.path.join(module.replace(".","/"), n)+'.pyx'],
+                         include_dirs=[np.get_include()]) for module, names in ex_info for n in names]
+
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['six'] + sphinx,
+          cmdclass = {'build_ext': build_ext},
+          ext_modules = extlist,
+          long_description=read_md('README.md'),
+          version=__version__,
+          packages=find_packages(),
+          )
+
+
+if __name__ == "__main__":
+    setup_package()
---
+++ packages/wetb/wetb-0.0.12.tar.gz/wetb-0.0.12/wetb/utils/process_exec.py
@@ -0,0 +1,63 @@
+'''
+Created on 10/03/2014
+
+@author: MMPE
+'''
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import range
+from builtins import str
+from future import standard_library
+import glob
+import re
+standard_library.install_aliases()
+
+import os
+
+
+DEBUG = False
+def pexec(args, cwd=None):
+    """
+    usage: errorcode, stdout, stderr, cmd = pexec("MyProgram.exe arg1, arg2", r"c:\tmp\")
+
+    """
+    import subprocess
+    if not isinstance(args, (list, tuple)):
+        args = [args]
+    args = [str(arg) for arg in args]
+    for i in range(len(args)):
+        if os.path.exists(args[i]):
+            args[i] = str(args[i]).replace('/', os.path.sep).replace('\\', os.path.sep).replace('"', '')
+
+    cmd = "%s" % '{} /c "{}"'.format (os.environ.get("COMSPEC", "cmd.exe"), subprocess.list2cmdline(args))
+    if cwd and os.path.isfile(cwd):
+        cwd = os.path.dirname(cwd)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=cwd)
+    stdout, stderr = proc.communicate()
+    errorcode = proc.returncode
+
+    return errorcode, stdout.decode('cp1252'), stderr.decode('cp1252'), cmd
+
+
+def process(args, cwd=None):
+    import subprocess
+    if not isinstance(args, (list, tuple)):
+        args = [args]
+    args = [str(arg) for arg in args]
+    for i in range(len(args)):
+        if os.path.exists(args[i]):
+            args[i] = str(args[i]).replace('/', os.path.sep).replace('\\', os.path.sep).replace('"', '')
+
+    cmd = "%s" % '{} /c "{}"'.format (os.environ.get("COMSPEC", "cmd.exe"), subprocess.list2cmdline(args))
+    if cwd is not None and os.path.isfile(cwd):
+        cwd = os.path.dirname(cwd)
+    return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, cwd=cwd)
+
+def exec_process(process):
+    stdout, stderr = process.communicate()
+    errorcode = process.returncode
+
+    return errorcode, stdout.decode(), stderr.decode()
+
