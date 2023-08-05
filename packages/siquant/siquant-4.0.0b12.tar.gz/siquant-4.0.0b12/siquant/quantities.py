1fb79a17088a5d4fd612e67aa48b86ba6ec43692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package siquant/4.0.0b12/siquant-4.0.0b12.tar.gz

---
+++ packages/siquant/siquant-4.0.0b12.tar.gz/siquant-4.0.0b12/siquant/quantities.py
@@ -0,0 +1,326 @@
+try:
+    from operator import matmul
+except ImportError:
+    pass  # matmul not supported
+
+from copy import copy, deepcopy
+from functools import total_ordering
+
+from .exceptions import UnitMismatchError, unexpected_type_error
+from .util import immutable
+
+
+def make(quantity, units):
+    """Entry point for creating quantities consistently
+
+    :param quantity: The value to tag with specific units.
+    :type quantity: ``_T``
+    :param units: The units of this quantity.
+    :type units: :class:`~siquant.units.SIUnit`
+    :rtype: ``_Q`` = :class:`~Quantity`
+    """
+    return units.factory(quantity, units)
+
+
+def converter(units):
+    """Create a converter function which will return Quantities.
+
+    :param units: The units to convert a quantity to.
+    :type units: :class:`~siquant.units.SIUnit`
+    :rtype: ``Callable[[Any], _Q]``
+    """
+
+    def _converter(value):
+        if isinstance(value, Quantity):
+            return value.cvt_to(units)
+        return make(value, units)
+
+    return _converter
+
+
+def validator(dimensions):
+    """Create a validator function which checks if a value matches expected dimensions.
+
+    .. seealso::
+
+        Predefined :mod:`~siquant.dimensions`.
+
+        Creating new :func:`~siquant.dimensions.SIDimensions`.
+
+    :param dimensions: The expected dimensions.
+    :type dimensions: ``tuple``
+    :rtype: ``Callable[[Any, ...], bool]``
+    """
+
+    if not isinstance(dimensions, tuple):
+        raise unexpected_type_error("dimensions", tuple, dimensions)
+    if not len(dimensions) == 7:
+        raise ValueError("Dimensions tuple must have 7 elements.", dimensions)
+
+    def _validator(*values):
+        return are_of(dimensions, *values)
+
+    return _validator
+
+
+def are_of(dimensions, *quantities):
+    """Check if quantities all match dimensions.
+
+    :param dimensions: The expected dimensionality.
+    :type dimensions: ``tuple``
+    :param quantities: Variadic. The instances to check against.
+    :rtype: ``bool``
+    """
+    try:
+        return all(q.is_of(dimensions) for q in quantities)
+    except AttributeError:
+        return False
+
+
+@immutable
+@total_ordering
+class Quantity:
+    """Quantity wraps a value with units and provides arithmetic passthrough operations.
+
+    .. note::
+
+        Creation of Quantity directly is discouraged.
+
+        The preferred method are:
+
+        q = value * si.meters
+
+        q = make(value, si.meters)
+
+        Both of these methods delegate instantiation to
+        :attr:`~siquant.units.SIUnit.factory` in order to
+        more easily support clean extensibility.
+
+    :ivar quantity: The wrapped value. read only.
+    :vartype quantity: ``_T``
+    :ivar units: The units of this quantity. read only.
+    :vartype units: :class:`~siquant.units.SIUnit`
+
+    :param quantity: The quantity to be wrapped.
+    :type quantity: ``_T``
+    :param units: The units the quantity's value is expressed in.
+    :type units: :class:`~siquant.units.SIUnit`
+    """
+
+    __slots__ = ("quantity", "units")
+
+    def __init__(self, quantity, units):
+        if isinstance(quantity, Quantity):
+            units = quantity.units * units
+            quantity = quantity.quantity
+        super().__setattr__("quantity", quantity)
+        super().__setattr__("units", units)
+
+    def is_of(self, dimensions):
+        """
+
+        :param dimensions:
+        :return: ``bool``
+        """
+        return self.units.dimensions == dimensions
+
+    def get_as(self, units):
+        """
+
+        :param units: The units to express the underlying value in.
+        :type units: :class:`~siquant.units.SIUnit`
+        :rtype: ``_T``
+        """
+        if self.units == units:
+            return self.quantity
+        if not self.units.compatible(units):
+            raise UnitMismatchError(self.units, units)
+        return self.units.scale / units.scale * self.quantity
+
+    def cvt_to(quantity, units):
+        """Create an equivalent Quantity expressed in the provided units.
+
+        :param units: The units to express this quantity in.
+        :type units: :class:`~siquant.units.SIUnit`
+        :return: ``_Q`` = :class:`~siquant.quantities.Quantity`
+        """
+        return make(quantity.get_as(units), units)
+
+    def round_to(self, units, places=0):
+        """Create an equivalent Quantity rounded in provided units.
+
+        :param units: The units to express the quantity in.
+        :type units: :class:`~siquant.units.SIUnit`
+        :param places: The number of decimal places to round to.
+        :type places: ``int``
+        :rtype: ``_Q`` = :class:`~siquant.quantities.Quantity`
+        """
+        return make(round(self.get_as(units), places), units)
+
+    def compatible(self, other):
+        """
+
+        :param other: The quantity to check for dimensional compatibility.
+        :type other: ``_Q`` = :class:`~siquant.quantities.Quantity`
+        :rtype: bool
+        """
+        if not isinstance(other, Quantity):
+            raise unexpected_type_error("other", Quantity, other)
+        return self.units.compatible(other.units)
+
+    def __abs_epsilon(self, atol=1e-6):
+        if isinstance(atol, Quantity):
+            return atol
+        return atol * self.units
+
+    def __rel_epsilon(self, other, rtol=1e-9):
+        return rtol * max(abs(self), abs(other), 1 * self.units)
+
+    def abs_approx(self, other, atol=1e-6):
+        return self.approx(other, rtol=0, atol=atol)
+
+    def rel_approx(self, other, rtol=1e-9):
+        return self.approx(other, rtol=rtol, atol=0)
+
+    def approx(self, other, rtol=1e-9, atol=1e-6):
+        """
+
+        :raises: ``TypeError`` if other is not a Quantity
+
+        :param other:
+        :param rtol:
+        :type rtol:
+        :param atol:
+        :type atol:
+        :return:
+        """
+        if not self.compatible(other):
+            return False
+
+        epsilon = max(self.__rel_epsilon(other, rtol), self.__abs_epsilon(atol))
+        return abs(other - self) <= epsilon
+
+    def __add__(self, other):
+        if other == 0:
+            return self
+        if isinstance(other, Quantity):
+            return make(self.quantity + other.get_as(self.units), self.units)
+        return NotImplemented
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        return NotImplemented
+
+    __iadd__ = __add__
+
+    def __sub__(self, other):
+        if other == 0:
+            return self
+        if isinstance(other, Quantity):
+            return make(self.quantity - other.get_as(self.units), self.units)
+        return NotImplemented
+
+    def __rsub__(self, other):
+        if other == 0:
+            return -self
+        return NotImplemented
+
+    __isub__ = __sub__
+
+    def __neg__(self):
+        return make(-self.quantity, self.units)
+
+    def __bool__(self):
+        return bool(self.quantity)
+
+    def __eq__(self, other):
+        if isinstance(other, Quantity):
+            return self.units.compatible(other.units) and self.quantity == other.get_as(
+                self.units
+            )
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Quantity):
+            return not self.units.compatible(
+                other.units
+            ) or self.quantity != other.get_as(self.units)
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.quantity * self.units.scale, self.units.base_units()))
+
+    def __str__(self):
+        return "%s %s" % (self.quantity, self.units)
+
+    def __repr__(self):
+        return "%s(%r, %r)" % (self.__class__.__name__, self.quantity, self.units)
+
+    def __abs__(self):
+        return make(abs(self.quantity), self.units)
+
+    def __lt__(self, other):
+        if isinstance(other, Quantity):
+            return self.quantity < other.get_as(self.units)
+        return NotImplemented
+
+    def __mul__(self, rhs):
+        if isinstance(rhs, Quantity):
+            return make(self.quantity * rhs.quantity, self.units * rhs.units)
+        return make(self.quantity * rhs, self.units)
+
+    __imul__ = __mul__
+
+    def __rmul__(self, lhs):
+        return make(lhs * self.quantity, self.units)
+
+    def __matmul__(self, rhs):
+        if isinstance(rhs, Quantity):
+            return make(matmul(self.quantity, rhs.quantity), self.units * rhs.units)
+        return make(matmul(self.quantity, rhs), self.units)
+
+    __imatmul__ = __matmul__
+
+    def __rmatmul__(self, lhs):
+        return make(matmul(lhs, self.quantity), self.units)
+
+    def __truediv__(self, rhs):
+        if isinstance(rhs, Quantity):
+            return make(self.quantity / rhs.quantity, self.units / rhs.units)
+        return make(self.quantity / rhs, self.units)
+
+    __itruediv__ = __truediv__
+
+    def __rtruediv__(self, lhs):
+        return Quantity(lhs / self.quantity, ~self.units)
+
+    def __pow__(self, exponent):
+        try:
+            return make(self.quantity ** exponent, self.units ** exponent)
+        except TypeError:
+            return NotImplemented
+
+    def __invert__(self):
+        return make(1 / self.quantity, ~self.units)
+
+    def __float__(self):
+        return float(self.quantity)
+
+    def __int__(self):
+        return int(self.quantity)
+
+    def __len__(self):
+        return len(self.quantity)
+
+    def __iter__(self):
+        return (make(q, self.units) for q in self.quantity)
+
+    def __getitem__(self, idx):
+        return make(self.quantity[idx], self.units)
+
+    def __copy__(self):
+        return make(copy(self.quantity), self.units)
+
+    def __deepcopy__(self, memodict):
+        return make(deepcopy(self.quantity), self.units)
---
+++ packages/siquant/siquant-4.0.0b12.tar.gz/siquant-4.0.0b12/tests/test_quantities.py
@@ -0,0 +1,394 @@
+import sys
+import math
+
+from copy import copy, deepcopy
+
+import pytest
+import numpy as np
+
+import siquant.dimensions as dims
+
+from siquant.exceptions import UnitMismatchError, ImmutabilityError
+from siquant import make, converter, validator, are_of, si, SIUnit
+
+
+def test_q_copying():
+    a = 100 * si.meters
+    b = copy(a)
+    c = deepcopy(a)
+
+    assert a is not b
+    assert a is not c
+    assert a == b
+    assert a == c
+
+    class Ref:
+        def __init__(self, value):
+            self.value = value
+
+        def __eq__(self, other):
+            return self.value == other.value
+
+    ref = Ref(100)
+    ref_ref = Ref(Ref(100))
+
+    rq = make(ref, si.meters)
+    rc = copy(rq)
+
+    assert rq == rc
+    assert rq is not rc
+
+    rqq = make(ref_ref, si.meters)
+    rqc = copy(rqq)
+    rqd = deepcopy(rqq)
+
+    assert rqq is not rqc
+    assert rqq == rqc
+
+    ref_ref.value.value = 50
+    assert rqq == rqc
+    assert rqq != rqd
+
+
+def test_q_immutability():
+    mass = 100 * si.kilograms
+    with pytest.raises(ImmutabilityError):
+        mass.quantity = 10
+    with pytest.raises(ImmutabilityError):
+        del mass.quantity
+
+
+def test_q_np():
+    distances = make(np.array([1, 2, 3, 4, 5, 6, 7]), si.meters)
+    distances *= 2
+
+    value = distances.get_as(si.meters)
+    assert np.array_equal(value, np.array([2, 4, 6, 8, 10, 12, 14]))
+
+    assert distances[0].approx(2 * si.meters)
+    assert distances[6].approx(14 * si.meters)
+
+    assert min(distances).approx(2 * si.meters)
+    assert max(distances).approx(14 * si.meters)
+
+
+def test_q_round_to():
+    distance = 1.23456789 * si.meters
+
+    assert 1235 == distance.round_to(si.millimeters).quantity
+
+    assert 1234.6 == distance.round_to(si.millimeters, 1).quantity
+
+
+def test_q_approx():
+
+    dist1 = 1.234567890 * si.meters
+    dist2 = 1.234567 * si.meters
+
+    assert dist1.approx(dist2)
+    assert dist1.approx(dist2, atol=1 * si.millimeters)
+
+    assert dist1.abs_approx(dist2)
+
+    with pytest.raises(UnitMismatchError):
+        dist1.approx(dist2, atol=1 * si.milligrams)
+
+    assert not dist1.approx(1.23456789 * si.kilograms)
+
+    dist1 = 1234567890 * si.meters
+    dist2 = 1234567891 * si.meters
+
+    assert dist1.approx(dist2)
+    assert dist1.rel_approx(dist2)
+    assert not dist1.approx(dist2, rtol=0)
+    assert not dist1.abs_approx(dist2)
+
+    assert dist1.approx(dist2, rtol=0, atol=1 * si.meters)
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 5), reason="operators.matmul introduced in py35"
+)
+def test_q_matmul():
+    from operator import matmul
+
+    distances = make(np.array([1, 2, 3]), si.meters)
+    value = matmul(distances, distances)
+    assert value.get_as(si.meters ** 2) == 14
+
+    value = matmul(distances, np.array([1, 2, 3]))
+    assert value.get_as(si.meters ** 1) == 14
+
+    with pytest.raises(TypeError):
+        # numpy really should just return NotImplemented instead of throwing a TypeError
+        value = matmul(np.array([1, 2, 3]), distances)
+        assert value.get_as(si.meters ** 1) == 14
+
+
+def test_are_of_validator():
+    dim_check = validator(dims.speed_t)
+
+    assert dim_check(1 * si.meters / si.seconds)
+    assert dim_check(1 * si.meters / si.seconds, 2 * si.meters / si.seconds)
+    assert not dim_check(1 * si.meters)
+    assert not dim_check(1)
+    assert not dim_check(None)
+
+    with pytest.raises(TypeError):
+        validator([1, 2, 3, 4, 5, 6, 7])
+
+    with pytest.raises(ValueError):
+        validator((1, 2, 3))
+
+
+def test_are_of():
+    assert are_of(dims.distance_t, 1 * si.meters, 2 * si.millimeters, 3 * si.kilometers)
+    assert not are_of(dims.angle_t, 1, 2, 3)
+    assert not are_of(dims.distance_t, 1 * si.meters, 1 * si.kilograms)
+
+
+def test_create_and_extract():
+    mass = make(100, SIUnit.Unit(kg=1))
+    acceleration = make(1, SIUnit.Unit(m=1, s=-2))
+
+    force = 100 * si.newtons
+
+    assert force.quantity == 100
+    assert force.get_as(si.kilonewtons) == 0.1
+    assert force == mass * acceleration
+
+
+def test_hash():
+    angle1 = 2 * math.pi * si.radians
+    angle2 = 360 * si.degrees
+
+    assert hash(angle1) == hash(angle2)
+
+    circumference = angle1 * 1 * si.meters
+    assert hash(circumference) != hash(angle1)
+
+
+def test_q_compatible():
+    dist1 = 1 * si.meters
+    dist2 = 1 * si.millimeters
+    area = 1 * si.meters ** 2
+    assert dist1.compatible(dist2)
+    assert dist2.compatible(dist1)
+    assert not dist1.compatible(area)
+    assert not area.compatible(dist1)
+
+    with pytest.raises(TypeError):
+        dist = 1 * si.meters
+        dist.compatible(si.meters)
+
+
+def test_q_eq_ne():
+    dist1 = 1 * si.meters
+    dist2 = 1000 * si.millimeters
+    dist3 = 1 * si.kilometers
+    area = 1 * si.meters ** 2
+
+    assert dist1 == dist2
+    assert dist2 == dist1
+
+    assert dist1 != dist3
+    assert dist3 != dist1
+
+    assert not area == dist1
+    assert not dist1 == area
+
+    assert not dist1 == si.meters
+    assert not si.meters == dist1
+
+    assert area != dist1
+    assert dist1 != area
+
+    assert dist1 != si.meters
+    assert si.meters != dist1
+
+
+def test_q_ordering():
+    dist1 = 1 * si.meters
+    dist2 = 1000 * si.millimeters
+    dist3 = 1 * si.kilometers
+    dist4 = 1 * si.millimeters
+    area = 1 * si.meters ** 2
+
+    assert dist1 >= dist2
+    assert dist2 <= dist1
+    assert dist1 <= dist2
+    assert dist2 >= dist1
+
+    assert dist3 >= dist1
+    assert dist1 <= dist3
+    assert dist3 > dist1
+    assert dist1 < dist3
+
+    assert dist4 <= dist1
+    assert dist1 >= dist4
+    assert dist4 < dist1
+    assert dist1 > dist4
+
+    with pytest.raises(UnitMismatchError):
+        dist1 > area
+    with pytest.raises(UnitMismatchError):
+        dist1 >= area
+    with pytest.raises(UnitMismatchError):
+        dist1 < area
+    with pytest.raises(UnitMismatchError):
+        dist1 <= area
+
+    with pytest.raises(TypeError):
+        dist1 > 1
+    with pytest.raises(TypeError):
+        dist1 >= 1
+    with pytest.raises(TypeError):
+        dist1 < 1
+    with pytest.raises(TypeError):
+        dist1 <= 1
+
+
+def test_q_float():
+    assert pytest.approx(1) == math.cos(0 * si.radians)
+    assert pytest.approx(-1) == math.cos(math.pi * si.radians)
+    assert pytest.approx(0) == math.cos(math.pi / 2 * si.radians)
+    assert pytest.approx(0) == math.cos(math.pi * 3 / 2 * si.radians)
+
+
+def test_q_truth():
+    dist_true = 1 * si.meters
+    dist_false = 0 * si.meters
+    assert dist_true
+    assert not dist_false
+
+
+def test_q_abs():
+    assert abs(1 * si.meters) == 1 * si.meters
+    assert abs(-1 * si.meters) == 1 * si.meters
+
+
+def test_q_invert():
+    dist = 1000 * si.millimeters
+    assert ~dist == 1 / 1000 / si.millimeters
+
+
+def test_q_neg():
+    dist = 1000 * si.millimeters
+    assert -dist == -1000 * si.millimeters
+
+
+def test_q_pow():
+    dist = 1000 * si.millimeters
+    assert dist ** 2 == 1 * si.meters ** 2
+    assert dist ** -1 == 1 * si.meters ** -1
+
+    dist = 100 * si.millimeters ** 2
+    assert dist ** 0.5 == 10 * si.millimeters
+
+    with pytest.raises(TypeError):
+        dist ** dist
+    with pytest.raises(TypeError):
+        dist ** si.meters
+
+
+def test_q_mul():
+    dist = 1000 * si.millimeters
+    assert 2 * dist == 2000 * si.millimeters
+    assert dist * 2 == 2000 * si.millimeters
+    assert dist * dist == 1000 * 1000 * si.millimeters ** 2
+
+    dist *= 2
+    assert dist == 2000 * si.millimeters
+
+
+def test_q_div():
+    dist = 1000 * si.millimeters
+    assert 2 / dist == 1 / 500 / si.millimeters
+    assert dist / 2 == 500 * si.millimeters
+    assert dist / dist == 1 * si.unity
+
+    assert dist / si.millimeters == 1000 * si.unity
+    assert si.millimeters / dist == 1 / 1000 * si.unity
+
+    with pytest.raises(TypeError):
+        dist / (1, 2)
+    with pytest.raises(TypeError):
+        (1, 2) / dist
+
+
+def test_dimensionality():
+    dist = 1000 * si.millimeters
+    assert dist.is_of(dims.distance_t)
+    assert not dist.is_of(dims.area_t)
+
+
+def test_q_add():
+    dist1 = 1 * si.meters
+    dist2 = 1000 * si.millimeters
+    area = dist1 * dist2
+
+    diff1 = dist1 + dist2
+    diff2 = dist2 + dist1
+
+    assert dist1 == dist1 + 0
+    assert dist1 == 0 + dist1
+
+    assert diff1 == 2 * si.meters
+    assert diff1.units == si.meters
+
+    assert diff2 == 2 * si.meters
+    assert diff2.units == si.millimeters
+
+    with pytest.raises(TypeError):
+        dist1 + si.meters
+    with pytest.raises(TypeError):
+        si.meters + dist1
+
+    with pytest.raises(UnitMismatchError):
+        dist1 + area
+    with pytest.raises(UnitMismatchError):
+        area + dist1
+
+    dist1 += dist2
+    assert dist1 == 2 * si.meters
+
+
+def test_q_sub():
+    dist1 = 1 * si.meters
+    dist2 = 1000 * si.millimeters
+    area = dist1 * dist2
+
+    assert dist1 == dist1 - 0
+    assert -dist1 == 0 - dist1
+
+    diff1 = dist1 - dist2
+    diff2 = dist2 - dist1
+
+    assert diff1 == 0 * si.meters
+    assert diff1.units == si.meters
+
+    assert diff2 == 0 * si.meters
+    assert diff2.units == si.millimeters
+
+    with pytest.raises(TypeError):
+        dist1 - si.meters
+    with pytest.raises(TypeError):
+        si.meters - dist1
+
+    with pytest.raises(UnitMismatchError):
+        dist1 - area
+    with pytest.raises(UnitMismatchError):
+        area - dist1
+
+    dist1 -= dist2
+    assert dist1 == 0 * si.meters
+
+
+def test_q_converters():
+    cvtr = converter(si.meters)
+    assert cvtr(1000) == 1000 * si.meters
+    assert cvtr(1000 * si.meters) == 1000 * si.meters
+    assert cvtr(1000 * si.millimeters) == 1 * si.meters
+
+    with pytest.raises(UnitMismatchError):
+        cvtr(1000 * si.kilograms)
