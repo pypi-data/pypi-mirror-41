5260c3a3b554b247196e2c6c3850f449087c95a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package scatter3d/0.0/scatter3d-0.0.tar.gz

---
+++ packages/scatter3d/scatter3d-0.0.tar.gz/scatter3d-0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r" ,encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='scatter3d',
+    version='0.0',
+    package_dir={'scatter3d': 'src'},
+    packages=['scatter3d'],
+    author='wcy',
+    author_email='1208640961@qq.com',
+    description='结合flask生成3d散点模型,并在浏览器中浏览',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
---
+++ packages/scatter3d/scatter3d-0.0.tar.gz/scatter3d-0.0/src/scatter3d.py
@@ -0,0 +1,38 @@
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='./templates', static_url_path='')
+
+
+@app.route("/get_data", methods=['POST', 'GET'])
+def get_data():
+    data = scatter3d.get_data()
+    return data
+
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    return render_template('index.html')
+
+
+class Scatter3d(object):
+
+    def __init__(self, host="127.0.0.1", port=80):
+        self.host = host
+        self.port = port
+
+    def run(self):
+        app.run(host=self.host, port=self.port)
+
+    def set_data(self, x, y, z):
+        self.x = x.tolist()
+        self.y = y.tolist()
+        self.z = z.tolist()
+
+    def get_data(self):
+        data = {"x": self.x, "y": self.y, "z": self.z}
+        return json.dumps(data, ensure_ascii=False)
+
+
+scatter3d = Scatter3d()
