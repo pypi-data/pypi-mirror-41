0e7f1cedfb82912f5719c87cb279d4d872f7014f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:08 +0000

    Add package recordclass/0.9/recordclass-0.9-cp37-cp37m-win32.whl

---
+++ packages/recordclass/recordclass-0.9-cp37-cp37m-win32.whl/recordclass/test/typing/__init__.py
@@ -0,0 +1,8 @@
+# {{LICENCE}}
+
+from recordclass.test.typing.test_recordclass import *
+from recordclass.test.typing.test_structclass import *
+
+def test_all():
+    import unittest
+    unittest.main(verbosity=2)
