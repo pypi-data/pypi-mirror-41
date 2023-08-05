cdb1bf9981053b895ff77387ebee54e9f1217b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:52 +0000

    Add package OmicsIntegrator/2.3.8/OmicsIntegrator-2.3.8.tar.gz

---
+++ packages/OmicsIntegrator/OmicsIntegrator-2.3.8.tar.gz/OmicsIntegrator-2.3.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='OmicsIntegrator',
+    packages=['OmicsIntegrator'],
+    package_dir={'OmicsIntegrator': 'src'},
+    package_data={'OmicsIntegrator': ['annotation/final_annotation.pickle']},
+    version='2.3.8',
+    url='https://github.com/fraenkel-lab/OmicsIntegrator2',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
+    license='MIT',
+    author='zfrenchee',
+    author_email='alex@lenail.org',
+    description='',
+    install_requires=[
+        "numpy",
+        "pandas==0.23.4",
+        "networkx==2.1",
+        "pcst_fast==1.0.7",
+        "python-louvain",
+        "goenrich",
+        "sklearn",
+        "axial"
+    ],
+    entry_points={
+        'console_scripts': [
+            'OmicsIntegrator = src.__main__:main',
+        ]
+    },
+)
+
