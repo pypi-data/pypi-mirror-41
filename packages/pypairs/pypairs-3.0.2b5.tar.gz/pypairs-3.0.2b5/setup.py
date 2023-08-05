a9138713188d3b3bb5414499b058d892ca9a93db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:05 +0000

    Add package pypairs/3.0.2b5/pypairs-3.0.2b5.tar.gz

---
+++ packages/pypairs/pypairs-3.0.2b5.tar.gz/pypairs-3.0.2b5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+from io import open
+import versioneer
+
+with open('requirements.txt', encoding='utf-8') as requirements:
+    requires = [l.strip() for l in requirements]
+
+with open('README.rst', encoding='utf-8') as readme_f:
+    readme = readme_f.read()
+
+setup(
+    name='pypairs',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='A python scRNA-Seq classifier',
+    long_description=readme,
+    packages=find_packages(),
+    url='https://github.com/rfechtner/pypairs',
+    license='BSD',
+    author='Ron Fechtner',
+    author_email='ronfechtner@gmail.com',
+    python_requires='>=3.5',
+    install_requires=requires,
+    extras_require=dict(
+        plotting=['matplotlib', 'plotly'],
+        scanpy=['scanpy']
+    )
+)
