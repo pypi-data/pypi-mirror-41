004ca0e46e169c7286a0ca97367b67c953474937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:09 +0000

    Add package pykemen/0.1.1/pykemen-0.1.1.tar.gz

---
+++ packages/pykemen/pykemen-0.1.1.tar.gz/pykemen-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='pykemen',
+    version='0.1.1',
+    description='a pykemen pip-installable package',
+    license='Eiup',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'pykemen': ['google/*.py', '*']},
+    author='Ayyoub Marroun',
+    author_email='ayyoub@metriplica.com',
+    keywords=['google apis', 'utilities'],
+    url='https://github.com/ayyoubmarroun/pykemen',
+    download_url='https://github.com/ayyoubmarroun/pykemen/archive/0.0.8b.tar.gz',
+    install_requires=[
+        'httplib2',
+        'pandas',
+        'google_api_python_client',
+        'oauth2client',
+    ]
+)
