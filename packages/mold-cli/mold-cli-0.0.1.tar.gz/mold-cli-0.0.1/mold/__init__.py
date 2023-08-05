a528ac54b448677d7964f9503bd992f0d9ef90ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:02 +0000

    Add package mold-cli/0.0.1/mold-cli-0.0.1.tar.gz

---
+++ packages/mold-cli/mold-cli-0.0.1.tar.gz/mold-cli-0.0.1/mold/__init__.py
@@ -0,0 +1,12 @@
+"""
+mold - a CLI for system configuration and shell workflow.
+"""
+import mold.util as mold 
+
+__name__ = 'mold-cli'
+__licence__ = 'MIT'
+__version__ = '0.0.1'
+__author__ = 'Duncan Marsh'
+__email__ = 'slugbyte@slugbyte.com'
+__url__ = 'https://github.com/slugbyte/mold'
+__description__ = 'mold - A CLI for system configuration and shell workflow.'
