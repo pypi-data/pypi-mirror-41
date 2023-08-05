7f774a9cca6c7a1d0b4e93c1da2322d48a8a9b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package trex-stl-lib/0.2/trex_stl_lib-0.2.tar.gz

---
+++ packages/trex-stl-lib/trex_stl_lib-0.2.tar.gz/trex_stl_lib-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='trex_stl_lib',
+    version='0.2',
+    description='Trex Stateless library',
+
+    url='https://github.com/dedie/trex_stl_lib',
+    author='Daron Edie',
+    author_email='darby69@gmail.com',
+    license='Apache',
+    zip_safe=False,
+
+    packages=find_packages('src', exclude=['test']),
+    package_dir={'': 'src'},
+
+    install_requires=[
+        'scapy',
+        'simpy',
+        'pyzmq',
+        'texttable',
+        'pyyaml',
+        'jsonrpclib-pelix',
+    ],
+)
