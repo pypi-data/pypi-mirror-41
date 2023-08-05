8e56054c489d7142c8a906844e8391fd114eea1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package click-web/0.5.1/click-web-0.5.1.tar.gz

---
+++ packages/click-web/click-web-0.5.1.tar.gz/click-web-0.5.1/tests/fixtures/script/a_script.py
@@ -0,0 +1,50 @@
+import click
+
+
+@click.group()
+@click.option("--debug/--no-debug", help='Global debug flag')
+def cli(debug):
+    'the root command'
+    pass
+
+
+@cli.command()
+def simple_no_params_command():
+    'Help text'
+    click.echo(f"Simpel noparams command called")
+
+
+@cli.group()
+def sub_group():
+    'a sub group'
+    pass
+
+
+@sub_group.command()
+def a_sub_group_command():
+    'Help for sub_group.sub_group_command '
+    click.echo(f"Sub group command called")
+
+
+@cli.command()
+@click.option("--an-option", type=str, default="option_value", help='help for an option')
+@click.argument("an-argument", default=10, type=int)
+def command_with_option_and_argument(an_argument, an_option):
+    'Help text'
+    click.echo(f"Ran command with option: {an_option} argument {an_argument}")
+
+
+@cli.command()
+@click.argument('folder', type=click.Path(exists=True))
+def command_with_input_folder(folder):
+    click.echo(click.format_filename(folder))
+
+
+@cli.command()
+@click.argument('folder', type=click.Path())
+def command_with_output_folder(folder):
+    click.echo(click.format_filename(folder))
+
+
+if __name__ == '__main__':
+    cli()
