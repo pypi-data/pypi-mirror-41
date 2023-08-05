875960c0f16d6398e1d430afd7814e1a10a45d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:09 +0000

    Add package azure-kusto-data/0.0.21/azure_kusto_data-0.0.21-py2.py3-none-any.whl

---
+++ packages/azure-kusto-data/azure_kusto_data-0.0.21-py2.py3-none-any.whl/azure/kusto/data/helpers.py
@@ -0,0 +1,28 @@
+"""Kusto helper functions"""
+
+import pandas
+
+
+def dataframe_from_result_table(table):
+    """Converts Kusto tables into pandas DataFrame.
+    :param azure.kusto.data._models.KustoResultTable table: Table recieved from the response.
+    :return: pandas DataFrame.
+    :rtype: pandas.DataFrame
+    """
+
+    if not table.columns or not table.rows:
+        return pandas.DataFrame()
+
+    frame = pandas.DataFrame.from_records(
+        [row.to_list() for row in table.rows], columns=[col.column_name for col in table.columns]
+    )
+    bool_columns = [col.column_name for col in table.columns if col.column_type == "bool"]
+    for col in bool_columns:
+        frame[col] = frame[col].astype(bool)
+
+    for i in range(len(table.rows)):
+        seventh = table.rows[i]._seventh_digit
+        for name in seventh.keys():
+            frame.loc[i, name] += pandas.Timedelta(seventh[name] * 100, unit="ns")
+
+    return frame
