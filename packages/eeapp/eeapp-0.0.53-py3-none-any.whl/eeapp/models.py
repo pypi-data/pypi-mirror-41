c7ef602edfbd8b3fee394400b8fa06d111f04c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:52 +0000

    Add package eeapp/0.0.53/eeapp-0.0.53-py3-none-any.whl

---
+++ packages/eeapp/eeapp-0.0.53-py3-none-any.whl/eeapp/models.py
@@ -0,0 +1,144 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User,Group,UserManager,AbstractUser,AbstractBaseUser,PermissionsMixin
+from rest_framework.authtoken.models import Token
+
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from eeapp.base.fields.ImageSetField import ImageSetField
+
+# print(__name__)
+
+
+class UManager(UserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(VALID=True)
+
+class SafeManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(VALID=True)
+
+
+class Model(models.Model):
+    VALID = models.BooleanField(default=True)
+    objects = SafeManager()
+    class Meta:
+        abstract = True
+
+#AbstractUser
+class Account(AbstractBaseUser, PermissionsMixin):
+    display_name = models.CharField(max_length=255,default='',verbose_name='昵称')
+    VALID = models.BooleanField(default=True)
+
+    account_role = models.CharField(max_length=10,default='base')
+    wxopenid = models.CharField(max_length=64,default='')
+    objects = UManager()
+    # objects = UserManager()
+
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = True
+
+
+    def get_role(self):
+        return 'base'
+
+    def auth_token(self):
+        token,created = Token.objects.get_or_create(user=self)
+        return  token.key
+
+    class Meta(User.Meta):
+        abstract = True
+        permissions = (
+            ('base_account_create', 'base create new base'),
+            ('base_account_fix', 'base edit other base'),
+            ('base_account_delete', 'base drop other base')
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            if self.email:
+                self.username = self.email
+        self.account_role = self.get_role()
+        super(Account, self).save(*args, **kwargs)
+        token = Token.objects.get_or_create(user=self)
+
+
+
+#
+# class SuperAccount(Account):
+#     class Meta:
+#         abstract = False
+#         db_table = 'super_account'
+
+
+
+
+class OptionsModel(models.Model):
+    name = models.CharField(max_length=64,blank=False,verbose_name='名称')
+    value = models.CharField(max_length=20,blank=False,verbose_name='值')
+
+
+
+class TestModel(models.Model):
+    hobby_choice = (
+        ('music', '音乐'),
+        ('sport', '运动'),
+        ('film', '电影'),
+        ('tour', '旅游'),
+    )
+
+
+    email = models.EmailField(max_length=70,blank=True,verbose_name='邮箱')
+    name = models.CharField(max_length=20,blank=False,verbose_name='名称')
+    covers = ImageSetField(blank=True,verbose_name='封面')
+    sex = models.CharField(choices=(('boy','男'),('girl','女'),('unknown','未知')),verbose_name='性别',max_length=10,default='unknown')
+    hobbies = models.CharField(verbose_name='爱好',max_length=50,blank=True)
+    time = models.TimeField(verbose_name='时间')
+    birthday = models.DateField(verbose_name='生日')
+
+
+    # over_time = models.DateTimeField(blank=True,verbose_name='过期时间')
+
+
+
