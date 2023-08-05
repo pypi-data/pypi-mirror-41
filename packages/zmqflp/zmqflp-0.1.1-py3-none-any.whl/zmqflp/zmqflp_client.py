549f9a0ee77580b1b7acb9dae0ae0995f87a9fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:50 +0000

    Add package zmqflp/0.1.1/zmqflp-0.1.1-py3-none-any.whl

---
+++ packages/zmqflp/zmqflp-0.1.1-py3-none-any.whl/zmqflp/zmqflp_client.py
@@ -0,0 +1,21 @@
+import time
+import umsgpack
+import logging
+from .zmqflp_api import FreelanceClient
+
+class ZMQFLPClient(object):
+    def __init__(self, list_of_server_ips_with_ports_as_str):
+        self.client = FreelanceClient()
+        time.sleep(0.1)
+        for ip in list_of_server_ips_with_ports_as_str:
+            logging.info('client: connecting to server '+ip)
+            self.client.connect("tcp://"+ip)
+            logging.info('client: added server '+ip)
+
+    def send_and_receive(self, in_request):
+        reply = self.client.request(umsgpack.dumps(in_request))
+        if not reply:
+            logging.info("error, request "+str(in_request)+" unserviced")
+            return False
+        else:
+            return umsgpack.loads(reply[2], encoding="utf-8")
