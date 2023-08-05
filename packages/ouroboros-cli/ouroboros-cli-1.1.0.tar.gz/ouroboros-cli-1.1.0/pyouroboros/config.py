422d6e6f74b65e5bf42fa85803882adfdfef4284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:38 +0000

    Add package ouroboros-cli/1.1.0/ouroboros-cli-1.1.0.tar.gz

---
+++ packages/ouroboros-cli/ouroboros-cli-1.1.0.tar.gz/ouroboros-cli-1.1.0/pyouroboros/config.py
@@ -0,0 +1,123 @@
+from logging import getLogger
+from pyouroboros.logger import BlacklistFilter
+
+
+class Config(object):
+    options = ['INTERVAL', 'PROMETHEUS', 'DOCKER_SOCKETS', 'MONITOR', 'IGNORE', 'LOG_LEVEL', 'PROMETHEUS_ADDR',
+               'PROMETHEUS_PORT', 'NOTIFIERS', 'REPO_USER', 'REPO_PASS', 'CLEANUP', 'RUN_ONCE', 'LATEST',
+               'INFLUX_URL', 'INFLUX_PORT', 'INFLUX_USERNAME', 'INFLUX_PASSWORD', 'INFLUX_DATABASE', 'INFLUX_SSL',
+               'INFLUX_VERIFY_SSL', 'DATA_EXPORT', 'SELF_UPDATE', 'LABEL_ENABLE', 'DOCKER_TLS_VERIFY', 'LABELS_ONLY',
+               'DRY_RUN']
+
+    interval = 300
+    docker_sockets = 'unix://var/run/docker.sock'
+    docker_tls_verify = False
+    monitor = []
+    ignore = []
+    data_export = None
+    log_level = 'info'
+    latest = False
+    cleanup = False
+    run_once = False
+    dry_run = False
+    self_update = False
+    label_enable = False
+    labels_only = False
+
+    repo_user = None
+    repo_pass = None
+    auth_json = None
+
+    prometheus = False
+    prometheus_addr = '127.0.0.1'
+    prometheus_port = 8000
+
+    influx_url = '127.0.0.1'
+    influx_port = 8086
+    influx_ssl = False
+    influx_verify_ssl = False
+    influx_username = 'root'
+    influx_password = 'root'
+    influx_database = None
+
+    notifiers = []
+
+    def __init__(self, environment_vars, cli_args):
+        self.cli_args = cli_args
+        self.environment_vars = environment_vars
+        self.filtered_strings = None
+
+        self.logger = getLogger()
+        self.parse()
+
+    def config_blacklist(self):
+        filtered_strings = [getattr(self, key.lower()) for key in Config.options
+                            if key.lower() in BlacklistFilter.blacklisted_keys]
+        # Clear None values
+        self.filtered_strings = list(filter(None, filtered_strings))
+        # take lists inside of list and append to list
+        for index, value in enumerate(self.filtered_strings, 0):
+            if isinstance(value, list):
+                self.filtered_strings.extend(self.filtered_strings.pop(index))
+                self.filtered_strings.insert(index, self.filtered_strings[-1:][0])
+        # Added matching for ports
+        ports = [string.split(':')[0] for string in self.filtered_strings if ':' in string]
+        self.filtered_strings.extend(ports)
+        # Added matching for tcp sockets. ConnectionPool ignores the tcp://
+        tcp_sockets = [string.split('//')[1] for string in self.filtered_strings if '//' in string]
+        self.filtered_strings.extend(tcp_sockets)
+        # Get JUST hostname from tcp//unix
+        for socket in getattr(self, 'docker_sockets'):
+            self.filtered_strings.append(socket.split('//')[1].split(':')[0])
+
+        for handler in self.logger.handlers:
+            handler.addFilter(BlacklistFilter(set(self.filtered_strings)))
+
+    def parse(self):
+        for option in Config.options:
+            if self.environment_vars.get(option):
+                if option in ['INTERVAL', 'PROMETHEUS_PORT', 'INFLUX_PORT']:
+                    try:
+                        opt = int(self.environment_vars[option])
+                        setattr(self, option.lower(), opt)
+                    except ValueError as e:
+                        print(e)
+                elif option in ['LATEST', 'CLEANUP', 'RUN_ONCE', 'INFLUX_SSL', 'INFLUX_VERIFY_SSL', 'DRY_RUN',
+                                'SELF_UPDATE', 'LABEL_ENABLE', 'DOCKER_TLS_VERIFY', 'LABELS_ONLY']:
+                    if self.environment_vars[option].lower() in ['true', 'yes']:
+                        setattr(self, option.lower(), True)
+                    elif self.environment_vars[option].lower() in ['false', 'no']:
+                        setattr(self, option.lower(), False)
+                    else:
+                        self.logger.error('%s is not true/yes, nor false/no for %s. Assuming false',
+                                          self.environment_vars[option], option)
+                else:
+                    setattr(self, option.lower(), self.environment_vars[option])
+            elif vars(self.cli_args).get(option):
+                setattr(self, option.lower(), vars(self.cli_args).get(option))
+
+        # Specific var changes
+        if self.repo_user and self.repo_pass:
+            self.auth_json = {'Username': self.repo_user, 'Password': self.repo_pass}
+
+        if self.interval < 30:
+            self.interval = 30
+
+        for option in ['docker_sockets', 'notifiers', 'monitor', 'ignore']:
+            if isinstance(getattr(self, option), str):
+                string_list = getattr(self, option)
+                setattr(self, option, [string.strip(' ').strip('"') for string in string_list.split(' ')])
+
+        # Config sanity checks
+        if self.data_export == 'influxdb' and not self.influx_database:
+            self.logger.error("You need to specify an influx database if you want to export to influxdb. Disabling "
+                              "influxdb data export.")
+
+        if self.data_export == 'prometheus' and self.self_update:
+            self.logger.warning("If you bind a port to ouroboros, it will be lost when it updates itself.")
+
+        if self.dry_run and not self.run_once:
+            self.logger.warning("Dry run is designed to be ran with run once. Setting for you.")
+            self.run_once = True
+
+        self.config_blacklist()
---
+++ packages/ouroboros-cli/ouroboros-cli-1.1.0.tar.gz/ouroboros-cli-1.1.0/pyouroboros/ouroboros.py
@@ -0,0 +1,159 @@
+import schedule
+from time import sleep
+from os import environ
+
+from requests.exceptions import ConnectionError
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+from pyouroboros.config import Config
+from pyouroboros.dockerclient import Docker
+from pyouroboros.logger import OuroborosLogger
+from pyouroboros.dataexporters import DataManager
+from pyouroboros.notifiers import NotificationManager
+from pyouroboros import VERSION, BRANCH
+
+
+def main():
+    """Declare command line options"""
+    parser = ArgumentParser(description='ouroboros', formatter_class=RawTextHelpFormatter,
+                            epilog='EXAMPLE: ouroboros -d tcp://1.2.3.4:5678 -i 20 -m container1 container2 -l warn')
+
+    core_group = parser.add_argument_group("Core", "Configuration of core functionality")
+    core_group.add_argument('-v', '--version', action='version', version=VERSION)
+
+    core_group.add_argument('-d', '--docker-sockets', nargs='+', default=Config.docker_sockets, dest='DOCKER_SOCKETS',
+                            help='Sockets for docker management\n'
+                                 'DEFAULT: "unix://var/run/docker.sock"\n'
+                                 'EXAMPLE: -d unix://var/run/docker.sock tcp://192.168.1.100:2376')
+
+    core_group.add_argument('-t', '--docker-tls-verify', default=False, dest='DOCKER_TLS_VERIFY', action='store_true',
+                            help='Enable docker TLS\n'
+                                 'REQUIRES: docker cert mount')
+
+    core_group.add_argument('-i', '--interval', type=int, default=Config.interval, dest='INTERVAL',
+                            help='Interval in seconds between checking for updates\n'
+                                 'DEFAULT: 300')
+
+    core_group.add_argument('-l', '--log-level', choices=['debug', 'info', 'warn', 'error', 'critical'],
+                            dest='LOG_LEVEL', default=Config.log_level, help='Set logging level\n'
+                                                                             'DEFAULT: info')
+
+    core_group.add_argument('-u', '--self-update', default=False, dest='SELF_UPDATE', action='store_true',
+                            help='Let ouroboros update itself')
+
+    core_group.add_argument('-o', '--run-once', default=False, action='store_true', dest='RUN_ONCE', help='Single run')
+
+    core_group.add_argument('-A', '--dry-run', default=False, action='store_true', dest='DRY_RUN',
+                            help='Run without making changes. Best used with run-once')
+
+    core_group.add_argument('-N', '--notifiers', nargs='+', default=Config.notifiers, dest='NOTIFIERS',
+                            help='Apprise formatted notifiers\n'
+                                 'EXAMPLE: -N discord://1234123412341234/jasdfasdfasdfasddfasdf '
+                                 'mailto://user:pass@gmail.com')
+
+    docker_group = parser.add_argument_group("Docker", "Configuration of docker functionality")
+    docker_group.add_argument('-m', '--monitor', nargs='+', default=Config.monitor, dest='MONITOR',
+                              help='Which container(s) to monitor\n'
+                                   'DEFAULT: All')
+
+    docker_group.add_argument('-n', '--ignore', nargs='+', default=Config.ignore, dest='IGNORE',
+                              help='Container(s) to ignore\n'
+                                   'EXAMPLE: -n container1 container2')
+
+    docker_group.add_argument('-k', '--label-enable', default=False, dest='LABEL_ENABLE', action='store_true',
+                              help='Enable label monitoring for ouroboros label options\n'
+                                   'Note: labels take precedence'
+                                   'DEFAULT: False')
+
+    docker_group.add_argument('-M', '--labels-only', default=False, dest='LABELS_ONLY', action='store_true',
+                              help='Only watch containers that utilize labels\n'
+                                   'This allows a more strict compliance for environments'
+                                   'DEFAULT: False')
+
+    docker_group.add_argument('-c', '--cleanup', default=False, dest='CLEANUP', action='store_true',
+                              help='Remove old images after updating')
+
+    docker_group.add_argument('-L', '--latest', default=False, dest='LATEST', action='store_true',
+                              help='Check for latest image instead of pulling current tag')
+
+    docker_group.add_argument('-r', '--repo-user', default=None, dest='REPO_USER',
+                              help='Private docker registry username\n'
+                                   'EXAMPLE: foo@bar.baz')
+
+    docker_group.add_argument('-R', '--repo-pass', default=None, dest='REPO_PASS',
+                              help='Private docker registry password\n'
+                                   'EXAMPLE: MyPa$$w0rd')
+
+    data_group = parser.add_argument_group('Data Export', 'Configuration of data export functionality')
+    data_group.add_argument('-D', '--data-export', choices=['prometheus', 'influxdb'], default=None, dest='DATA_EXPORT',
+                            help='Enable exporting of data for chosen option')
+
+    data_group.add_argument('-a', '--prometheus-addr', default=Config.prometheus_addr,
+                            dest='PROMETHEUS_ADDR', help='Bind address to run Prometheus exporter on\n'
+                                                         'DEFAULT: 127.0.0.1')
+
+    data_group.add_argument('-p', '--prometheus-port', type=int, default=Config.prometheus_port,
+                            dest='PROMETHEUS_PORT', help='Port to run Prometheus exporter on\n'
+                                                         'DEFAULT: 8000')
+
+    data_group.add_argument('-I', '--influx-url', default=Config.influx_url, dest='INFLUX_URL',
+                            help='URL for influxdb\n'
+                                  'DEFAULT: 127.0.0.1')
+
+    data_group.add_argument('-P', '--influx-port', type=int, default=Config.influx_port, dest='INFLUX_PORT',
+                            help='PORT for influxdb\n'
+                                  'DEFAULT: 8086')
+
+    data_group.add_argument('-U', '--influx-username', default=Config.influx_username, dest='INFLUX_USERNAME',
+                            help='Username for influxdb\n'
+                                  'DEFAULT: root')
+
+    data_group.add_argument('-x', '--influx-password', default=Config.influx_password, dest='INFLUX_PASSWORD',
+                            help='Password for influxdb\n'
+                                  'DEFAULT: root')
+
+    data_group.add_argument('-X', '--influx-database', default=Config.influx_password, dest='INFLUX_DATABASE',
+                            help='Influx database name. Required if using influxdb')
+
+    data_group.add_argument('-s', '--influx-ssl', default=False, dest='INFLUX_SSL', action='store_true',
+                            help='Use SSL when connecting to influxdb')
+
+    data_group.add_argument('-V', '--influx-verify-ssl', default=False, dest='INFLUX_VERIFY_SSL', action='store_true',
+                            help='Verify SSL certificate when connecting to influxdb')
+
+    args = parser.parse_args()
+
+    if environ.get('LOG_LEVEL'):
+        log_level = environ.get('LOG_LEVEL')
+    else:
+        log_level = args.LOG_LEVEL
+    ol = OuroborosLogger(level=log_level)
+    ol.logger.info('Version: %s-%s', VERSION, BRANCH)
+    config = Config(environment_vars=environ, cli_args=args)
+    config_dict = {key: value for key, value in vars(config).items() if key.upper() in config.options}
+    ol.logger.debug("Ouroboros configuration: %s", config_dict)
+
+    data_manager = DataManager(config)
+    notification_manager = NotificationManager(config, data_manager)
+    notification_manager.send(kind='startup')
+
+    for socket in config.docker_sockets:
+        try:
+            docker = Docker(socket, config, data_manager, notification_manager)
+            schedule.every(config.interval).seconds.do(docker.update_containers).tag(f'update-containers-{socket}')
+        except ConnectionError:
+            ol.logger.error("Could not connect to socket %s. Check your config", socket)
+
+    schedule.run_all()
+
+    if config.run_once:
+        for socket in config.docker_sockets:
+            schedule.clear(f'update-containers-{socket}')
+
+    while schedule.jobs:
+        schedule.run_pending()
+        sleep(1)
+
+
+if __name__ == "__main__":
+    main()
