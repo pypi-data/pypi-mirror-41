907ca4ebc4dc30ed015edbe1398fc5ccd13031b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package tstoolbox/30.75.35.23/tstoolbox-30.75.35.23.tar.gz

---
+++ packages/tstoolbox/tstoolbox-30.75.35.23.tar.gz/tstoolbox-30.75.35.23/tstoolbox/functions/converttz.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""Collection of functions for the manipulation of time series."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import mando
+from mando.rst_text_formatter import RSTHelpFormatter
+
+from .. import tsutils
+
+
+@mando.command(formatter_class=RSTHelpFormatter, doctype='numpy')
+@tsutils.doc(tsutils.docstrings)
+def converttz(fromtz,
+              totz,
+              input_ts='-',
+              columns=None,
+              start_date=None,
+              end_date=None,
+              round_index=None,
+              dropna='no',
+              clean=False,
+              index_type='datetime',
+              names=None,
+              source_units=None,
+              target_units=None,
+              skiprows=None):
+    """Convert the time zone of the index.
+
+    Parameters
+    ----------
+    fromtz : str
+        The time zone of the original time-series.
+    totz : str
+        The time zone of the converted time-series.
+    {input_ts}
+    {start_date}
+    {end_date}
+    {columns}
+    {dropna}
+    {clean}
+    {skiprows}
+    {index_type}
+    {names}
+    {source_units}
+    {target_units}
+    {round_index}
+
+    """
+    tsd = tsutils.common_kwds(tsutils.read_iso_ts(input_ts,
+                                                  skiprows=skiprows,
+                                                  names=names,
+                                                  index_type=index_type),
+                              start_date=start_date,
+                              end_date=end_date,
+                              pick=columns,
+                              round_index=round_index,
+                              dropna=dropna,
+                              source_units=source_units,
+                              target_units=target_units,
+                              clean=clean)
+
+    # TODO Should test that 'fromtz' matches time zone that might be already
+    # set in tsd.
+
+    try:
+        tsd = tsd.tz_localize(fromtz).tz_convert(totz)
+    except TypeError:
+        tsd = tsd.tz_convert(totz)
+    return tsutils.printiso(tsd,
+                            showindex='always')
---
+++ packages/tstoolbox/tstoolbox-30.75.35.23.tar.gz/tstoolbox-30.75.35.23/tstoolbox/functions/date_slice.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""Collection of functions for the manipulation of time series."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+
+import mando
+from mando.rst_text_formatter import RSTHelpFormatter
+
+from .. import tsutils
+
+warnings.filterwarnings('ignore')
+
+
+@mando.command(formatter_class=RSTHelpFormatter, doctype='numpy')
+@tsutils.doc(tsutils.docstrings)
+def date_slice(input_ts='-',
+               columns=None,
+               start_date=None,
+               end_date=None,
+               dropna='no',
+               clean=False,
+               skiprows=None,
+               index_type='datetime',
+               names=None,
+               round_index=None,
+               source_units=None,
+               target_units=None,
+               float_format='%g'):
+    """Print out data to the screen between start_date and end_date.
+
+    This isn't really useful anymore because "start_date" and "end_date" are
+    available in all sub-commands.
+
+    Parameters
+    ----------
+    {input_ts}
+    {columns}
+    {start_date}
+    {end_date}
+    {dropna}
+    {clean}
+    {skiprows}
+    {index_type}
+    {names}
+    {float_format}
+    {source_units}
+    {target_units}
+    {round_index}
+
+    """
+    return tsutils.printiso(
+        tsutils.common_kwds(tsutils.read_iso_ts(input_ts,
+                                                skiprows=skiprows,
+                                                names=names,
+                                                index_type=index_type),
+                            start_date=start_date,
+                            end_date=end_date,
+                            pick=columns,
+                            round_index=round_index,
+                            dropna=dropna,
+                            source_units=source_units,
+                            target_units=target_units,
+                            clean=clean), float_format=float_format)
