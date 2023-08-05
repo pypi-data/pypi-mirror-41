7b97b12be45b8eccb1bc32204f80a5fbfe9e0b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package streamsx/1.11.10/streamsx-1.11.10.tar.gz

---
+++ packages/streamsx/streamsx-1.11.10.tar.gz/streamsx-1.11.10/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+setup(
+  name = 'streamsx',
+  packages = ['streamsx', 'streamsx.spl', 'streamsx.topology', 'streamsx.scripts', 'streamsx._streams'],
+  include_package_data=True,
+  version = '1.11.10',
+  description = 'IBM Streams Python Support',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'debrunne@us.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/pypi.streamsx',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/1.11.10',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics'],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['requests', 'future', 'dill', 'enum34'],
+  entry_points = {
+        'console_scripts': [
+            'streamsx-runner=streamsx.scripts.runner:main',
+            'streams-service=streamsx.scripts.service:main',
+            'streamsx-info=streamsx.scripts.info:main',
+            'spl-python-extract=streamsx.scripts.extract:main'
+        ],
+  },
+)
---
+++ packages/streamsx/streamsx-1.11.10.tar.gz/streamsx-1.11.10/streamsx/.toolkit/com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/toolkit.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2017
+
+"""
+SPL toolkit integration.
+
+********
+Overview
+********
+
+SPL operators are defined by an SPL toolkit. When a ``Topology`` 
+contains invocations of SPL operators, their defining toolkit must
+be made known using :py:func:`add_toolkit`.
+
+Toolkits shipped with the IBM Streams product under
+``$STREAMS_INSTALL/toolkits`` are implictly known and
+must not be added through ``add_toolkit``.
+
+"""
+from future.builtins import *
+
+import os
+
+def add_toolkit(topology, location):
+    """Add an SPL toolkit to a topology.
+
+    Args:
+        topology(Topology): Topology to include toolkit in.
+        location(str): Location of the toolkit directory.
+    """
+    import streamsx.topology.topology
+    assert isinstance(topology, streamsx.topology.topology.Topology)
+    tkinfo = dict()
+    tkinfo['root'] = os.path.abspath(location)
+    topology.graph._spl_toolkits.append(tkinfo)
+
