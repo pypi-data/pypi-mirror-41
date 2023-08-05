0fe94abbf35a0ff0e117b07ea251f6e5292439ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:33 +0000

    Add package pyre-check/0.0.19/pyre_check-0.0.19-py3-none-macosx_10_11_x86_64.whl

---
+++ packages/pyre-check/pyre_check-0.0.19-py3-none-macosx_10_11_x86_64.whl/pyre_check/commands/restart.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from .command import Command
+from .incremental import Incremental
+from .start import Start
+from .stop import Stop
+
+
+class Restart(Command):
+    NAME = "restart"
+
+    def __init__(self, arguments, configuration, analysis_directory) -> None:
+        super(Restart, self).__init__(arguments, configuration, analysis_directory)
+
+    def _run(self) -> None:
+        Stop(self._arguments, self._configuration, self._analysis_directory).run()
+        Incremental(
+            self._arguments, self._configuration, self._analysis_directory
+        ).run()
