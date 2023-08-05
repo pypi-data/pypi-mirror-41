033b919b0b03bfa10fce7261b47c5b3848540e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package regxtools/1.0.9/regxtools-1.0.9.tar.gz

---
+++ packages/regxtools/regxtools-1.0.9.tar.gz/regxtools-1.0.9/regxtools/regxgen/module.py
@@ -0,0 +1,45 @@
+import re, yaml, json, ast
+from pathlib import Path
+
+
+class RegxGen:
+    _config="regGen.yml"
+    _regList={}
+    _regTable={}
+
+    @classmethod
+    def setConfig(cls, file):
+        if (file is not None) and (Path(file).is_file()):
+            cls._config = file
+        return cls
+
+    @classmethod
+    def apply(cls):
+        with open(cls._config, "r") as file:
+            data = yaml.load(file.read())
+            cls._regList  = RegGen.__regList(data["params"])
+            cls._regTable = RegGen.__regTable(data["items"])
+
+    @classmethod
+    def convert(cls, doc):
+        '''better call it on a combined string of requests (document) '''
+        for str, reg in cls._regList.items():
+            doc = re.sub(str, reg, doc)
+        return doc
+
+    @classmethod
+    def table(cls):
+        return ast.literal_eval(
+            cls.convert(str(cls._regTable)))
+
+    @staticmethod
+    def __regList(params, regList={}):
+        for param in params:
+            regList[param["str"]]=param["reg"]
+        return regList
+
+    @staticmethod
+    def __regTable(items, regTable={}):
+        for item, str in items.items():
+            regTable[item]=str
+        return regTable
