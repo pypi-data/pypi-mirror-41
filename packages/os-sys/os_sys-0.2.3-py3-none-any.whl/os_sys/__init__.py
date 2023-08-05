0665ea778b9268190aa4df36293f21c5edc90bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package os-sys/0.2.3/os_sys-0.2.3-py3-none-any.whl

---
+++ packages/os-sys/os_sys-0.2.3-py3-none-any.whl/os_sys/__init__.py
@@ -0,0 +1,10 @@
+from os_sys import __all__ as os_sys_
+from wifi import __all__ as wifi_
+from system import __all__ as system_
+__all__ = ['os_sys', 'system', 'wifi']
+print(__all__)
+print(os_sys_)
+print('')
+print(system_)
+print('')
+print(wifi_)
