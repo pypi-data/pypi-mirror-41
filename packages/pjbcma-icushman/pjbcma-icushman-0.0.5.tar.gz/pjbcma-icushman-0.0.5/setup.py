c11df0ad5cf99348af86c03b719fb245b5d7ad18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package pjbcma-icushman/0.0.5/pjbcma-icushman-0.0.5.tar.gz

---
+++ packages/pjbcma-icushman/pjbcma-icushman-0.0.5.tar.gz/pjbcma-icushman-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pjbcma-icushman",
+    version="0.0.5",
+    author="Isaiah Cushman",
+    author_email="icushman@uci.edu",
+    description="An assistant for using pyjags",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/icushman/pjBCM",
+    packages=['pjbcma'],
+    install_requires=[
+	"pyjags",
+	"pandas",
+	"matplotlib",
+	"seaborn",
+	"scipy",
+	"numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+
