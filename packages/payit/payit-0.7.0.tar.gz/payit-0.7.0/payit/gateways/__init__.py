5b6c8aa1d03cba63f1f73bdfa2e6289fccb395f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package payit/0.7.0/payit-0.7.0.tar.gz

---
+++ packages/payit/payit-0.7.0.tar.gz/payit-0.7.0/payit/gateways/__init__.py
@@ -0,0 +1,18 @@
+from .mellat import MellatGateway
+from .payir import PayIrGateway
+from .zarinpal import ZarinpalGateway
+from .irankish import IrankishGateway
+from .asanpardakht import AsanPardakhtGateway
+from .parsian import ParsianGateway
+from .bahamta import BahamtaGateway
+
+
+__all__ = (
+    MellatGateway,
+    PayIrGateway,
+    ZarinpalGateway,
+    IrankishGateway,
+    AsanPardakhtGateway,
+    ParsianGateway,
+    BahamtaGateway
+)
