65581298d3a773a18fc7a0cd88ae621b228c7f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package eyes-core/3.15.4/eyes_core-3.15.4.tar.gz

---
+++ packages/eyes-core/eyes_core-3.15.4.tar.gz/eyes_core-3.15.4/applitools/core/capture.py
@@ -0,0 +1,74 @@
+import abc
+import typing as tp
+
+import attr
+
+from .geometry import Point, Region
+from .metadata import CoordinatesType
+from .utils import ABC, image_utils
+
+if tp.TYPE_CHECKING:
+    from PIL import Image
+
+__all__ = ('EyesScreenshot',)
+
+
+@attr.s
+class EyesScreenshot(ABC):
+    """
+     Base class for handling screenshots.
+     """
+
+    _image = attr.ib()  # type: Image.Image
+
+    @abc.abstractmethod
+    def sub_screenshot(self, region, throw_if_clipped=False):
+        # type: (Region, bool) -> Region
+        pass
+
+    @abc.abstractmethod
+    def convert_location(self, location, from_, to):
+        # type: (Point, CoordinatesType, CoordinatesType) -> Point
+        pass
+
+    @abc.abstractmethod
+    def location_in_screenshot(self, location, coordinates_type):
+        # type: (Point, CoordinatesType) -> Point
+        pass
+
+    @abc.abstractmethod
+    def intersected_region(self, region, result_coordinate_types):
+        # type: (Region, CoordinatesType) -> Region
+        pass
+
+    def convert_region_location(self, region, from_, to):
+        # type: (Region, CoordinatesType, CoordinatesType) -> Region
+        assert region is not None
+        assert isinstance(region, Region)
+        assert from_ is not None
+        assert to is not None
+
+        if region.is_empty:
+            return Region.create_empty_region()
+
+        updated_location = self.convert_location(region.location, from_, to)
+        return Region(updated_location.x, updated_location.y, region.width, region.height)
+
+    @property
+    def image_region(self):
+        # type: () -> Region
+        return Region(0, 0, self._image.width, self._image.height, CoordinatesType.SCREENSHOT_AS_IS)
+
+    @staticmethod
+    def from_region(region):
+        # type: (Region) -> Image.Image
+        return Image.new('RGBA', (region.width, region.height))
+
+    def get_bytes(self):
+        # type: () -> bytes
+        """
+        Returns the bytes of the screenshot.
+
+        :return: The bytes representation of the png.
+        """
+        return image_utils.get_bytes(self._image)
---
+++ packages/eyes-core/eyes_core-3.15.4.tar.gz/eyes_core-3.15.4/applitools/core/positioning/region_provider.py
@@ -0,0 +1,37 @@
+import typing as tp
+
+import attr
+
+from applitools.core.geometry import Region
+from applitools.core.utils import ABC
+
+if tp.TYPE_CHECKING:
+    from applitools.core.capture import EyesScreenshot
+
+__all__ = ('RegionProvider', 'NullRegionProvider', 'NULL_REGION_INSTANCE')
+
+
+@attr.s
+class RegionProvider(ABC):
+    """
+    Encapsulates a getRegion "callback" and how the region's coordinates should be used.
+    """
+    _region = attr.ib()
+
+    def get_region(self, eyes_screenshot=None):
+        # type: (tp.Optional[EyesScreenshot]) -> Region
+        """
+        :return: A region with "as is" viewport coordinates.
+        """
+        return self._region
+
+    def __str__(self):
+        return str(self._region)
+
+
+@attr.s
+class NullRegionProvider(RegionProvider):
+    _region = attr.ib(init=False, factory=Region.create_empty_region)
+
+
+NULL_REGION_INSTANCE = NullRegionProvider()
