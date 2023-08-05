423adc8ed474cb96ff90ac75518ac67f073ae9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:45 +0000

    Add package xml2pytorch/0.0.10/xml2pytorch-0.0.10.tar.gz

---
+++ packages/xml2pytorch/xml2pytorch-0.0.10.tar.gz/xml2pytorch-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xml2pytorch",
+    version="0.0.10",
+    author="Yifan Zhou",
+    author_email="yfzhou.cs@gmail.com",
+    description="Using xml to define pytorch neural networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yfzhoucs/xml2pytorch",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        "torch>=0.4.1",
+        "numpy>=1.15.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
