59f5322ae5f9b6c5f50e50b23463006b61f3c462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:45 +0000

    Add package natthaphon/0.0.2/natthaphon-0.0.2.tar.gz

---
+++ packages/natthaphon/natthaphon-0.0.2.tar.gz/natthaphon-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="natthaphon",
+    version="0.0.2",
+    author="Natthaphon Hogcharoen",
+    author_email="palm22180@gmail.com",
+    description="A small package for my usage",
+    long_description="No need for any description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DableUTeeF/pytorch_api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
