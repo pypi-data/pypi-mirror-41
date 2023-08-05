eb02c9f1c61da6ebf344753d66a58d24b8ae6e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:01 +0000

    Add package textexploration/0.2.1/textexploration-0.2.1.tar.gz

---
+++ packages/textexploration/textexploration-0.2.1.tar.gz/textexploration-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="textexploration",
+    version="0.2.1",
+    author="Matthijs Brouwer",
+    url="https://github.com/textexploration/python",
+    packages=find_packages(),
+    license="Apache License 2.0",
+    long_description=open("README.txt").read(),
+    install_requires=[
+        "requests >= 2.21.0",
+        "pandas",
+    ],
+    classifiers=[
+        "Framework :: Jupyter",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+)
+
