93c0452f5b75b10b76e0bafa0ebf0d7b8c45b811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package castero/0.4.0/castero-0.4.0.tar.gz

---
+++ packages/castero/castero-0.4.0.tar.gz/castero-0.4.0/setup.py
@@ -0,0 +1,70 @@
+import subprocess
+
+import setuptools
+
+import castero
+
+install_requires = [
+    'requests',
+    'python-vlc',
+    'cjkwrap'
+]
+
+tests_require = [
+    'pytest',
+    'coverage',
+    'codecov'
+]
+
+extras_require = {
+    'test': tests_require
+}
+
+mpv_exists = False
+try:
+    subprocess.check_output("mpv --version", shell=True)
+    mpv_exists = True
+except subprocess.CalledProcessError:
+    pass
+
+if mpv_exists:
+    install_requires.append('pympv')
+
+
+def long_description():
+    with open("README.md") as readme:
+        return readme.read()
+
+
+setuptools.setup(
+    name=castero.__title__,
+    version=castero.__version__,
+    description=castero.__description__,
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    keywords=castero.__keywords__,
+    url=castero.__url__,
+    author=castero.__author__,
+    author_email=castero.__author_email__,
+    license=castero.__license__,
+    packages=[
+        'castero', 'castero.perspectives', 'castero.players', 'castero.menus'
+    ],
+    package_data={
+        'castero': ['templates/*'],
+    },
+    python_requires='>=3',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    entry_points={'console_scripts': ['castero=castero.__main__:main']},
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Terminals'
+    ],
+)
