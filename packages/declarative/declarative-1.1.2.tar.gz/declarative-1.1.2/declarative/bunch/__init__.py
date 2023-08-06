473e6f75bd095bcb34969a1cae5071d2ee7e3ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:15 +0000

    Add package declarative/1.1.2/declarative-1.1.2.tar.gz

---
+++ packages/declarative/declarative-1.1.2.tar.gz/declarative-1.1.2/declarative/bunch/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from .bunch import (
+    Bunch,
+    FrozenBunch,
+    HookBunch,
+)
+
+from .depbunch import (
+    DepBunch,
+    depB_property,
+)
+
+from .deep_bunch import (
+    DeepBunch,
+    DeepBunchSingleAssign,
+)
+
+from .shadow_bunch import ShadowBunch
+
+from .tag_bunch import TagBunch
+
+
+
+
+
+
