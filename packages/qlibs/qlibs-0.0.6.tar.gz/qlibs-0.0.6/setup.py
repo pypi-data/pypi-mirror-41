77f9b92c0b6e9479db5ce28d804120267d497479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package qlibs/0.0.6/qlibs-0.0.6.tar.gz

---
+++ packages/qlibs/qlibs-0.0.6.tar.gz/qlibs-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+#print(setuptools.find_packages())
+
+setuptools.setup(
+    name="qlibs",
+    version="0.0.6",
+    author="IQuant",
+    author_email="quant3234@gmail.com",
+    description="Random things",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/IntQuant/qlibs/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data = True,
+    install_requires=["moderngl", "PySDL2"]
+)
