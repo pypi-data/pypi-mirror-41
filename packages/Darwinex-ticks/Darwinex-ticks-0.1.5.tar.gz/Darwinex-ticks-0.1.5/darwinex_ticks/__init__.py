0490bf2fb16b38291b71bdabf2e3bda8f0be8ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package Darwinex-ticks/0.1.5/Darwinex-ticks-0.1.5.tar.gz

---
+++ packages/Darwinex-ticks/Darwinex-ticks-0.1.5.tar.gz/Darwinex-ticks-0.1.5/darwinex_ticks/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from .core import *
+
+__version__ = "0.1.4"
+__author__ = "Antonio Rodriguez (Paduel)"
+
+
---
+++ packages/Darwinex-ticks/Darwinex-ticks-0.1.5.tar.gz/Darwinex-ticks-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'Darwinex tick data downloader Python API'
+
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name='Darwinex-ticks',
+    version='0.1.5',
+    packages=find_packages(),
+    url='https://github.com/paduel/darwinex_ticks',
+    license='MIT',
+    author='Paduel',
+    author_email='paduel@gmail.com',
+    install_requires=['pandas'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
