411c8a8f04d62bd6cd462d54fc3ed22e78fc44bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:19 +0000

    Add package jupyterlab-zip/1.0.0/jupyterlab-zip-1.0.0.tar.gz

---
+++ packages/jupyterlab-zip/jupyterlab-zip-1.0.0.tar.gz/jupyterlab-zip-1.0.0/README.md
@@ -0,0 +1,16 @@
+# jupyterlab-zip
+
+Zip and Download folders in Jupyterlab
+
+## Install
+
+```
+make conda-env
+```
+```
+make watch-src
+```
+```
+#In a new terminal window
+make watch-lab
+```
---
+++ packages/jupyterlab-zip/jupyterlab-zip-1.0.0.tar.gz/jupyterlab-zip-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterlab-zip',
+    version='1.0.0',
+    description='Zip and download folder contents',
+    url='https://git.corp.adobe.com/adobesensei/jupyterlab-zip',
+    author='SChatter',
+    install_requires=[
+        'tornado',
+        'requests',
+        'six',
+        'jupyterlab<0.35',
+        'notebook>=5.2,<6.0a',
+        'flake8',
+        'twine',
+        'nodejs'
+    ],
+    data_files=[
+        ("etc/jupyter/jupyter_notebook_config.d", [
+            "config/jupyterlab-zip.json"
+        ])
+    ],
+    packages=find_packages(),
+    license='MIT',
+    include_package_data=True,
+)
