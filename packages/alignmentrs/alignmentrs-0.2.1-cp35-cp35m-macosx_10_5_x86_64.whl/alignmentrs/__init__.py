45354e1cba0d960a77d9de8f8dd89bb550ba60e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package alignmentrs/0.2.1/alignmentrs-0.2.1-cp35-cp35m-macosx_10_5_x86_64.whl

---
+++ packages/alignmentrs/alignmentrs-0.2.1-cp35-cp35m-macosx_10_5_x86_64.whl/alignmentrs/__init__.py
@@ -0,0 +1,12 @@
+from libalignmentrs import sequence as libsequence
+from libalignmentrs import marker as libmarker
+from libalignmentrs.sequence import Sequence
+from libalignmentrs.marker import Marker
+from alignmentrs.alignment import AlignmentMatrix, Alignment
+from alignmentrs.alignment import fasta_file_to_list, fasta_file_to_alignment
+
+__author__ = 'Kent Kawashima'
+__version__ = '0.2.1'
+__all__ = ['libalignment', 'libsequence', 'Sequence', 'Marker',
+           'AlignmentMatrix', 'Alignment',
+           'fasta_file_to_list', 'fasta_file_to_alignment']
