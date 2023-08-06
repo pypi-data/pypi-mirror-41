25527e912bbca90aeb251042ca41307db3fe3dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:16 +0000

    Add package dataone.test-utilities/3.3.0/dataone.test_utilities-3.3.0-py3-none-any.whl

---
+++ packages/dataone.test-utilities/dataone.test_utilities-3.3.0-py3-none-any.whl/d1_test/instance_generator/tests/test_access_policy.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import d1_test.d1_test_case
+import d1_test.instance_generator.access_policy as access_policy
+
+# ===============================================================================
+
+
+@d1_test.d1_test_case.reproducible_random_decorator('TestAccessPolicy')
+class TestAccessPolicy(d1_test.d1_test_case.D1TestCase):
+    def test_1000(self):
+        """select_random_set_of_permissions()"""
+        permissions = access_policy.random_set_of_permissions()
+        self.sample.assert_equals(
+            permissions, 'inst_gen_select_random_set_of_permissions'
+        )
+
+    def test_1010(self):
+        """permissions_to_tag_string()"""
+        permissions = access_policy.random_set_of_permissions()
+        s = access_policy.permissions_to_tag_string(permissions)
+        self.sample.assert_equals(s, 'inst_gen_permissions_to_tag_string')
+
+    def test_1020(self):
+        """random_subject_with_permission_labels()"""
+        permissions = access_policy.random_set_of_permissions()
+        s = access_policy.random_subject_with_permission_labels(permissions)
+        self.sample.assert_equals(s, 'inst_gen_random_subject_with_permission_labels')
+
+    def test_1030(self):
+        """random_subjects_with_permission_labels()"""
+        permissions = access_policy.random_set_of_permissions()
+        subjects = access_policy.random_subject_list_with_permission_labels(permissions)
+        self.sample.assert_equals(
+            subjects, 'inst_gen_random_subjects_with_permission_labels'
+        )
+
+    def test_1040(self):
+        """generate()"""
+        access_policy_pyxb = access_policy.generate()
+        self.sample.assert_equals(access_policy_pyxb, 'inst_gen_generate')
+
+    def test_1050(self):
+        """random_subject_list()"""
+        subject_list = access_policy.random_subject_list()
+        self.sample.assert_equals(subject_list, 'inst_gen_random_subject_list')
---
+++ packages/dataone.test-utilities/dataone.test_utilities-3.3.0-py3-none-any.whl/d1_test/mock_api/tests/test_list_objects.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import responses
+
+import d1_common.const
+import d1_common.date_time
+import d1_common.types.dataoneTypes_v2_0
+import d1_common.types.exceptions
+import d1_common.util
+
+import d1_test.d1_test_case
+import d1_test.mock_api.list_objects as mock_object_list
+
+
+class TestMockListObjects(d1_test.d1_test_case.D1TestCase):
+    @responses.activate
+    def test_1000(self, mn_client_v1_v2):
+        """mock_api.listObjects() returns a DataONE ObjectList PyXB object."""
+        mock_object_list.add_callback(d1_test.d1_test_case.MOCK_MN_BASE_URL)
+        assert isinstance(
+            mn_client_v1_v2.listObjects(), mn_client_v1_v2.pyxb_binding.ObjectList
+        )
+
+    @responses.activate
+    def test_1010(self, mn_client_v1_v2):
+        """mock_api.listObjects() returns a populated ObjectList."""
+        mock_object_list.add_callback(d1_test.d1_test_case.MOCK_MN_BASE_URL)
+        object_list = mn_client_v1_v2.listObjects()
+        self.sample.assert_equals(object_list, 'populated')
+
+    @responses.activate
+    def test_1020(self, mn_client_v1_v2):
+        """mock_api.listObjects(): Passing a trigger header triggers a DataONEException"""
+        mock_object_list.add_callback(d1_test.d1_test_case.MOCK_MN_BASE_URL)
+        with pytest.raises(d1_common.types.exceptions.ServiceFailure):
+            mn_client_v1_v2.listObjects(vendorSpecific={'trigger': '500'})
+
+    # TODO: More tests
