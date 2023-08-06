a80e90e9dc3c48488e750274f4774afb1d8ee508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:17 +0000

    Add package bib2doc/0.1.8/bib2doc-0.1.8.tar.gz

---
+++ packages/bib2doc/bib2doc-0.1.8.tar.gz/bib2doc-0.1.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='bib2doc',    
+    version='0.1.8',                          
+    scripts=['bib2doc']                  
+)
+
+# Use "python setup.py sdist upload" to update pypi package.
+
