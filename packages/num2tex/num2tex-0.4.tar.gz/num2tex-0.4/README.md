dc117ce21183a23e6fdb7978de5cc13c64f2f6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:17 +0000

    Add package num2tex/0.4/num2tex-0.4.tar.gz

---
+++ packages/num2tex/num2tex-0.4.tar.gz/num2tex-0.4/README.md
@@ -0,0 +1,56 @@
+# num2tex
+Converts a float or int into a latex-formatted string.  Inspired by https://stackoverflow.com/questions/13490292/format-number-using-latex-notation-in-python/13490601#13490601
+
+## Installation
+Install with
+```bash
+pip install num2tex
+```
+## Usage
+`num2tex` inherits from `str`, and so can be used in a similar manner.
+
+```python
+from num2tex import num2tex
+print(num2tex(13.4e10))
+```
+```
+134000000000.0
+```
+which gives the same precision and float/scientific notation formatting as `print(str(1.34e10))` or `print(str(1.34e10))`.  We can customize the format as we would for a string:
+```python
+# Print in string with two significant figures
+print('$a = {:0.2g}$'.format(num2tex(13.6e10)))
+```
+```
+'1.4 \\times 10^{11}'
+```
+Or we can set the precision from the beginning so that it always displays the desired number of significant figures.
+```python
+a = num2tex(1.4e10,precision=2)
+print('$a = {}$'.format(a))
+```
+returns
+```
+'1.3 \\times 10^{10}'
+```
+but `precision` can still be overridden by a format specifier:
+```python
+print('$a = {:f}$'.format(num2tex(13.6e10,precision=2)))
+```
+```
+$a = 136000000000.000000$
+```
+## num2tex in Jupyter
+`num2tex` will produce Jupyter-friendly output:
+```python
+from num2tex import num2tex
+a = num2tex(13.4e10,precision=2)
+a
+```
+![Jupyter output](https://raw.githubusercontent.com/AndrewChap/num2tex/master/images/jp_samp_0.png)
+
+## Future
+ 1. Add format option i.e. `num2tex(13.6e10,format=':.2g')`
+ 2. Add support for Google Collaboratory
+ 3. Add testing
+ 4. Get user feedback
---
+++ packages/num2tex/num2tex-0.4.tar.gz/num2tex-0.4/num2tex/__init__.py
@@ -0,0 +1,46 @@
+class num2tex(str):
+    def __new__(self, num, precision=None):
+        self.num = num
+        self.precision = precision
+        return str.__new__(self,num)
+
+    # __format__() gets called, for example,
+    # when evaluating 'a={0.2g}'.format(num2tex(2.5456))
+    def __format__(self,f):
+        return self.__str__(f)
+
+    # method that converts the number into a string
+    # with precision given by f (of type string)
+    # or by self.precision (of type int)
+    def __str__(self,f=None):
+        if self.num is None:
+            return '\mathrm{None}'
+        elif self.num == float('inf'):
+            return '\infty'
+        elif self.num == -float('inf'):
+            return '-\infty'
+        elif self.num != self.num:
+            return '\mathrm{NaN}'
+        formatUnspecified = (f is None) or (f == '')
+        if formatUnspecified and self.precision is None:
+            num_string = str(self.num)
+        else:
+            if formatUnspecified:
+                f = '0.{}'.format(self.precision)
+            num_string = '{0:{1}}'.format(self.num,f)
+        if 'e' in num_string:
+            base, exponent = num_string.split('e')
+            return r'{0} \times 10^{{{1}}}'.format(base, int(exponent))
+        else:
+            return num_string
+
+    # unambiguous string representation of num2tex object
+    def __repr__(self):
+        if self.precision is None:
+            return 'num2tex(num={})'.format(self.num)
+        else:
+            return 'num2tex(num={}, precision={})'.format(self.num,self.precision)
+
+    # for pretty printing in a Jupyter Notebook
+    def _repr_latex_(self):
+        return '$$' + self.__str__() + '$$'
