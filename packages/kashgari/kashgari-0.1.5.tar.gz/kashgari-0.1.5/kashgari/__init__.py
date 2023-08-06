f5b594300aa36ede8d5395e6675596e34d807632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:25 +0000

    Add package kashgari/0.1.5/kashgari-0.1.5.tar.gz

---
+++ packages/kashgari/kashgari-0.1.5.tar.gz/kashgari-0.1.5/kashgari/__init__.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+"""
+@author: BrikerMan
+@contact: eliyar917@gmail.com
+@blog: https://eliyar.biz
+
+@version: 1.0
+@license: Apache Licence
+@file: __init__.py.py
+@time: 2019-01-19 13:42
+
+"""
+import kashgari.embeddings
+import kashgari.corpus
+import kashgari.tasks
+
+
+if __name__ == "__main__":
+    print("Hello world")
