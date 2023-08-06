e97493431b22ede07adbf87ba74f3687531f4579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package mycroft-ekylibre-utils/0.1/mycroft-ekylibre-utils-0.1.tar.gz

---
+++ packages/mycroft-ekylibre-utils/mycroft-ekylibre-utils-0.1.tar.gz/mycroft-ekylibre-utils-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycroft-ekylibre-utils',
+    version='0.1',
+    packages=find_packages(),
+    url='http://github.com/ekylibre',
+    license='Apache License 2.0',
+    author='Ekylibre',
+    author_email='rdechazelles@ekylibre.com',
+    description='Ekylibre set of tools for MycroftAI skills',
+    long_description=open('README.rst').read(),
+    install_requires=["requests"],
+    include_package_data=True,
+)
