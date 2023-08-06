330c41a277c32202d6795a45a61f76ea57e858cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:26 +0000

    Add package torchex/0.0.12/torchex-0.0.12.tar.gz

---
+++ packages/torchex/torchex-0.0.12.tar.gz/torchex-0.0.12/torchex/nn/functional/polar_transform.py
@@ -0,0 +1,16 @@
+'''
+Polar Transformer Network
+ref: https://arxiv.org/abs/1709.01889
+   : https://github.com/daniilidis-group/polar-transformer-networks
+'''
+
+
+def polar_transform(input):
+    ''' change input image to polar coordinate.
+    Args: 
+        input (B, H, W)
+    '''
+    pass
+    
+    
+    
