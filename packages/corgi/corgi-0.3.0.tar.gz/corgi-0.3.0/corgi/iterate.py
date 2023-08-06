306c63b5c7eaf0b4e23598dbcc764dae33477ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:34 +0000

    Add package corgi/0.3.0/corgi-0.3.0.tar.gz

---
+++ packages/corgi/corgi-0.3.0.tar.gz/corgi-0.3.0/corgi/iterate.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from itertools import groupby
+
+
+def groupdict(col, fn, value_fn=list):
+    "Performs a groupby on a collection and consumes the generator result into a python dict."
+    sorted_col = sorted(col, key=fn)
+    return defaultdict(list, {k: value_fn(v) for k, v in groupby(sorted_col, key=fn)})
+
+
+def flatten(lst):
+    "Flatten a list of lists. For full flattening, see compiler.ast.flatten."
+    return [item for sublist in lst for item in sublist]
+
+
+def unique(lst):
+    return list(set(lst))
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
---
+++ packages/corgi/corgi-0.3.0.tar.gz/corgi-0.3.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('VERSION', 'r') as f:
+    version = f.read()
+
+setup(name='corgi',
+      version=version,
+      description='Python data and analysis tools',
+      author='Paul English',
+      author_email='paulnglsh@gmail.com',
+      url='https://github.com/log0ymxm/corgi',
+      packages=['corgi'],
+      install_requires=[
+          'numpy',
+          'six',
+          'sqlalchemy',
+          'tqdm',
+      ],
+      )
