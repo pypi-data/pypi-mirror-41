21e0d9763d998e99054674ec7dd9222a1efe4298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package charm-tools/2.5.1/charm-tools-2.5.1.tar.gz

---
+++ packages/charm-tools/charm-tools-2.5.1.tar.gz/charm-tools-2.5.1/charmtools/build/fetchers.py
@@ -0,0 +1,151 @@
+import os
+import json
+import logging
+
+import requests
+from charmtools import fetchers
+from charmtools.fetchers import (git,  # noqa
+                                 Fetcher,
+                                 get_fetcher,
+                                 FetchError)
+
+from path import Path as path
+
+
+log = logging.getLogger(__name__)
+
+
+class RepoFetcher(fetchers.LocalFetcher):
+    @classmethod
+    def can_fetch(cls, url):
+        search_path = [os.getcwd(), os.environ.get("JUJU_REPOSITORY", ".")]
+        cp = os.environ.get("LAYER_PATH")
+        if cp:
+            search_path.extend(cp.split(":"))
+        for part in search_path:
+            p = (path(part) / url).normpath()
+            if p.exists():
+                return dict(path=p)
+        return {}
+
+
+fetchers.FETCHERS.insert(0, RepoFetcher)
+
+
+class LayerFetcher(fetchers.LocalFetcher):
+    LAYER_INDEX = "https://juju.github.io/layer-index/"
+    NO_LOCAL_LAYERS = False
+    NAMESPACE = "layer"
+    ENVIRON = "CHARM_LAYERS_DIR"
+    OLD_ENVIRON = "LAYER_PATH"
+    OPTIONAL_PREFIX = "juju-layer-"
+    ENDPOINT = "layers"
+
+    @classmethod
+    def can_fetch(cls, url):
+        # Search local path first, then the interface webservice
+        if url.startswith("{}:".format(cls.NAMESPACE)):
+            name = url[len(cls.NAMESPACE) + 1:]
+
+            if not cls.NO_LOCAL_LAYERS:
+                prefixed_name = '{}-{}'.format(cls.NAMESPACE, name)
+                search_path = []
+                if cls.ENVIRON in os.environ:
+                    search_path.append(os.environ[cls.ENVIRON])
+                elif cls.OLD_ENVIRON in os.environ:
+                    search_path.append(os.environ[cls.OLD_ENVIRON])
+                else:
+                    search_path.append(os.environ.get("JUJU_REPOSITORY", "."))
+                for part in search_path:
+                    basepath = path(part)
+                    for dirname in (name, prefixed_name):
+                        p = (basepath / dirname).normpath()
+                        if p.exists():
+                            return dict(path=p)
+
+            choices = [name]
+            if name.startswith(cls.OPTIONAL_PREFIX):
+                choices.append(name[len(cls.OPTIONAL_PREFIX):])
+            for choice in choices:
+                uri = "%s%s/%s.json" % (
+                    cls.LAYER_INDEX, cls.ENDPOINT, choice)
+                log.debug('Checking layer index: {}'.format(uri))
+                if uri.startswith('file://'):
+                    choice_path = path(uri[7:])
+                    if not choice_path.exists():
+                        continue
+                    result = json.loads(choice_path.text())
+                    if not result.get('repo'):
+                        continue
+                    log.debug('Found repo: {}'.format(result['repo']))
+                    return result
+                try:
+                    result = requests.get(uri)
+                except Exception:
+                    result = None
+                if result and result.ok:
+                    result = result.json()
+                    if result.get("repo"):
+                        log.debug('Found repo: {}'.format(result['repo']))
+                        return result
+            return {}
+
+    def target(self, dir_):
+        """Return a :class:`path` of the directory where the downloaded item
+        will be located.
+
+        :param str dir_: Directory into which the item will be downloaded.
+        :return: :class:`path`
+
+        """
+        if hasattr(self, "path"):
+            return self.path
+        elif hasattr(self, "repo"):
+            _, target = self._get_repo_fetcher_and_target(self.repo, dir_)
+            return target
+
+    def _get_repo_fetcher_and_target(self, repo, dir_):
+        """Returns a :class:`Fetcher` for ``repo``, and the destination dir
+        at which the downloaded repo will be created.
+
+        :param str repo: The repo url.
+        :param str dir_: Directory into which the repo will be downloaded.
+        :return: 2-tuple of (:class:`Fetcher`, :class:`path`)
+
+        """
+        u = self.url[len(self.NAMESPACE) + 1:]
+        f = get_fetcher(repo)
+        return f, path(dir_) / u
+
+    def fetch(self, dir_):
+        if hasattr(self, "path"):
+            return super(InterfaceFetcher, self).fetch(dir_)
+        elif hasattr(self, "repo"):
+            f, target = self._get_repo_fetcher_and_target(self.repo, dir_)
+            res = f.fetch(dir_)
+            # make sure we save the revision of the actual repo, before we
+            # start traversing subdirectories and moving contents around
+            self.revision = self.get_revision(res)
+            if res != target:
+                res = path(res)
+                if hasattr(self, 'subdir'):
+                    res = res / self.subdir
+                target.rmtree_p()
+                # call this way instead of `res.rename(target)`
+                # to make unit test easier
+                path.rename(res, target)
+            return target
+
+
+fetchers.FETCHERS.insert(0, LayerFetcher)
+
+
+class InterfaceFetcher(LayerFetcher):
+    NAMESPACE = "interface"
+    ENVIRON = "CHARM_INTERFACES_DIR"
+    OLD_ENVIRON = "INTERFACE_PATH"
+    OPTIONAL_PREFIX = "juju-relation-"
+    ENDPOINT = "interfaces"
+
+
+fetchers.FETCHERS.insert(0, InterfaceFetcher)
---
+++ packages/charm-tools/charm-tools-2.5.1.tar.gz/charm-tools-2.5.1/setup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# flake8: ignore=E501
+#
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU General Public License version 3 (see the file LICENSE).
+
+import os
+import subprocess
+import sys
+import json
+from setuptools import setup, find_packages
+
+
+curdir = os.path.dirname(__file__)
+version_cache = os.path.join(curdir, 'charmtools', 'VERSION')
+version_script = os.path.join(curdir, 'charmtools', 'git_version.py')
+version_raw = subprocess.check_output([sys.executable, version_script,
+                                       '--format=json']).strip()
+if sys.version_info >= (3, 0):
+    version_raw = version_raw.decode('UTF-8')
+version = json.loads(version_raw)['version']
+if version == 'unknown':
+    # during install; use cached VERSION
+    with open(version_cache, 'r') as fh:
+        version_raw = fh.read()
+    version = json.loads(version_raw)['version']
+else:
+    # during build; update cached VERSION
+    with open(version_cache, 'w') as fh:
+        fh.write(version_raw)
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fh:
+    readme = fh.read()
+
+
+setup(
+    name='charm-tools',
+    version=version,
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        'launchpadlib<1.11',
+        'cheetah3>=3.0.0',
+        'pyyaml==3.11',
+        'requests<=2.19.1',
+        'libcharmstore',
+        'blessings<=1.6',
+        'ruamel.yaml<=0.10.23',
+        'pathspec<=0.3.4',
+        'otherstuf<=1.1.0',
+        # 'path.py<=8.1.2',
+        'path.py>=10.5',
+        'pip>=1.5.4',
+        # 'jujubundlelib',
+        'virtualenv>=1.11.4',
+        'colander<=1.5.1',
+        'jsonschema<=2.5.1',
+        'secretstorage<2.4',
+        'dict2colander==0.2',
+    ],
+    include_package_data=True,
+    maintainer='Marco Ceppi',
+    maintainer_email='marco@ceppi.net',
+    description=('Tools for maintaining Juju charms'),
+    long_description=readme,
+    license='GPL v3',
+    url='https://github.com/juju/charm-tools',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'console_scripts': [
+            'charm-add = charmtools.generate:main',
+            'charm-build = charmtools.build.builder:main',
+            'charm-create = charmtools.create:main',
+            'charm-help = charmtools.cli:usage',
+            'charm-layers = charmtools.build.builder:inspect',
+            'charm-proof = charmtools.proof:main',
+            'charm-pull-source = charmtools.pullsource:main',
+            'charm-version = charmtools.version:main',
+        ],
+        'charmtools.templates': [
+            'bash = charmtools.templates.bash:BashCharmTemplate',
+            'reactive-python = charmtools.templates.reactive_python:ReactivePythonCharmTemplate',
+            'reactive-bash = charmtools.templates.reactive_bash:ReactiveBashCharmTemplate',
+            'python-basic = charmtools.templates.python:PythonCharmTemplate',
+            'python = charmtools.templates.python_services'
+            ':PythonServicesCharmTemplate',
+            'ansible = charmtools.templates.ansible:AnsibleCharmTemplate',
+            'chef = charmtools.templates.chef:ChefCharmTemplate',
+            'powershell = charmtools.templates.powershell:PowerShellCharmTemplate',
+        ]
+    },
+)
