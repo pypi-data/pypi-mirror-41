b040987b59befe5ebefd597e3115f55e8c5baf9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:55 +0000

    Add package valiot-worker/0.3/valiot_worker-0.3.tar.gz

---
+++ packages/valiot-worker/valiot_worker-0.3.tar.gz/valiot_worker-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='valiot_worker',
+      version='0.3',
+      description='The best worker ever.',
+      url='http://github.com/valiot/valiot-worker',
+      author='Mich B',
+      author_email='michel@valiot.io',
+      license='MIT',
+      packages=['valiot_worker'],
+      zip_safe=False)
