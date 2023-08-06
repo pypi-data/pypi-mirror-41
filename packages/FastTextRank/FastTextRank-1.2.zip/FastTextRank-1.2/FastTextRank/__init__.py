9d784a2a4d406d051c9da4efb430762aa7b441ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:23 +0000

    Add package FastTextRank/1.2/FastTextRank-1.2.zip

---
+++ packages/FastTextRank/FastTextRank-1.2.zip/FastTextRank-1.2/FastTextRank/__init__.py
@@ -0,0 +1,7 @@
+#-*- encoding:utf-8 -*-
+from __future__ import absolute_import
+from .FastTextRank4Sentence import FastTextRank4Sentence
+# from .TextRank4Sentence import TextRank4Sentence
+from . import util
+
+version = '0.2'
