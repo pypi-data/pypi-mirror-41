a655dbfd6de854c69bcd155671eee26d554a94b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package tol/0.0.5/tol-0.0.5-py3-none-any.whl

---
+++ packages/tol/tol-0.0.5-py3-none-any.whl/tol/__init__.py
@@ -0,0 +1,11 @@
+"""
+ 
+ Prepare execution helpers that are compatible with invoke/fabric and subprocess.run
+ 
+"""
+
+## this is only available from 3.5?
+#from subprocess import run
+
+## make optional dependency on invoke/fabric
+#from invoke import run
