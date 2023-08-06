7f6831b1c27cdeb7b9944df6234a51881a0c0982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package ac-ptk/0.0.17/ac-ptk-0.0.17.tar.gz

---
+++ packages/ac-ptk/ac-ptk-0.0.17.tar.gz/ac-ptk-0.0.17/performance/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+
+from performance import config
+
+VERSION = [0, 0, 17]
+
+DEBUG = config.DEBUG
+
+# dump时间间隔(s)
+DUMP_INTERVAL = config.dump_interval
+
+# 表单名称
+SHEET_NAME = "memory"
+
+TODAY = time.strftime("%Y-%m-%d")
+
+TIME = time.strftime("%H-%M-%S")
+
+COLORS = ['none', 'red', 'yellow', 'gray', 'blue', 'black', 'pink', '#ff34b3', '#fff8dc']
+
+
+DB_NAME = os.getcwd() + os.sep + 'ptk.db'
