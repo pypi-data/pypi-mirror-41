afa6a6e55ebcf25f6d47c2c806c681e2f0a73d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package growbot/0.2.3/growbot-0.2.3.tar.gz

---
+++ packages/growbot/growbot-0.2.3.tar.gz/growbot-0.2.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='growbot',
+    version='0.2.3',
+    author="Team xiv",
+    author_email="me@qaisjp.com",
+    description="Client library for GrowBot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/teamxiv/growbot-python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'websockets>=7.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
+
+# To Publish:
+# First, build a source distribution:
+# $ python setup.py sdist
+# Then upload this to PyPi (have ~/.pypirc exist)
+# $ twine upload dist/*
