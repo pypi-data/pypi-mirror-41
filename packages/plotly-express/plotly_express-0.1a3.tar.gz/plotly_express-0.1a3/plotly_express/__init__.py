13cb503ca9932ccbaf9f09bb294a291d1e99367e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:38 +0000

    Add package plotly-express/0.1a3/plotly_express-0.1a3.tar.gz

---
+++ packages/plotly-express/plotly_express-0.1a3.tar.gz/plotly_express-0.1a3/plotly_express/__init__.py
@@ -0,0 +1,28 @@
+from ._chart_types import (  # noqa: F401
+    scatter,
+    scatter_3d,
+    scatter_polar,
+    scatter_ternary,
+    scatter_mapbox,
+    scatter_geo,
+    line,
+    line_3d,
+    line_polar,
+    line_ternary,
+    line_mapbox,
+    line_geo,
+    bar,
+    bar_polar,
+    violin,
+    box,
+    histogram,
+    scatter_matrix,
+    parallel_coordinates,
+    parallel_categories,
+    choropleth,
+    density_contour,
+)
+
+from ._px import set_mapbox_access_token  # noqa: F401
+
+from . import data, colors  # noqa: F401
