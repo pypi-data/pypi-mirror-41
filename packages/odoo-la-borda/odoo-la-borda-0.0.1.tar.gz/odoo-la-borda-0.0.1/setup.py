747622f144f2010c86aee74390b1cf11dec72727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:39 +0000

    Add package odoo-la-borda/0.0.1/odoo-la-borda-0.0.1.tar.gz

---
+++ packages/odoo-la-borda/odoo-la-borda-0.0.1.tar.gz/odoo-la-borda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="odoo-la-borda",
+    version="0.0.1",
+    author="Coopdevs",
+    description="Odoo customizations for La Borda",
+    license="GPL",
+    packages=['la-borda'],
+    classifiers=[],
+)
