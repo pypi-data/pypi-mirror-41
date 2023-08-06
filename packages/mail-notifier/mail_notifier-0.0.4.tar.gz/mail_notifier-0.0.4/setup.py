9166da39ea7ae59b28255462fc50411db2589ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package mail-notifier/0.0.4/mail_notifier-0.0.4.tar.gz

---
+++ packages/mail-notifier/mail_notifier-0.0.4.tar.gz/mail_notifier-0.0.4/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='mail_notifier',
+    version='0.0.4',
+    url='http://git.shookai.com/team-black/shk-lib-notifier.git',
+    author_email='bartlomiej.tyniewicki@shookai.com',
+    description='Sending notification via SMTP',
+    packages=["mail_notifier"],
+    include_package_data=True,
+)
