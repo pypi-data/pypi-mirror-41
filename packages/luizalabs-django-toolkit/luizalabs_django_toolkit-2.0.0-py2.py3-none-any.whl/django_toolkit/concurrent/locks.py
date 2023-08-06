e18e4bfedbd0b35d88db1c7b54563eb92119922e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package luizalabs-django-toolkit/2.0.0/luizalabs_django_toolkit-2.0.0-py2.py3-none-any.whl

---
+++ packages/luizalabs-django-toolkit/luizalabs_django_toolkit-2.0.0-py2.py3-none-any.whl/django_toolkit/concurrent/locks.py
@@ -0,0 +1,70 @@
+from django.core.cache import caches
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+
+class LockActiveError(Exception):
+    pass
+
+
+class Lock(object):
+
+    def __init__(self):
+        self.active = False
+
+
+class LocalMemoryLock(Lock):
+    """
+    A context manager to handle a lock status in memory
+
+    The active status is True on __enter__ and False on __exit__
+    """
+
+    def __enter__(self):
+
+        if self.active:
+            raise LockActiveError('Lock is already active')
+
+        self.active = True
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.active = False
+
+
+class CacheLock(Lock):
+    """
+    A context manager to handle a lock status using Django cache
+
+    The active status is True on __enter__ and False on __exit__
+
+    The cache will be deleted on context __exit__
+    """
+
+    def __init__(
+        self,
+        key,
+        cache_alias='default',
+        expire=DEFAULT_TIMEOUT,
+        raise_exception=True,
+        delete_on_exit=True
+    ):
+        super(CacheLock, self).__init__()
+        self._key = key
+        self._expire = expire
+        self.cache = caches[cache_alias]
+        self.raise_exception = raise_exception
+        self.delete_on_exit = delete_on_exit
+
+    def __enter__(self):
+        self.active = self.cache.add(self._key, True, self._expire)
+
+        if not self.active and self.raise_exception:
+            raise LockActiveError('For key {key}'.format(key=self._key))
+
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        if self.active and self.delete_on_exit:
+            self.cache.delete(self._key)
+
+        self.active = False
