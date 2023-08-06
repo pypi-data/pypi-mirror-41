bdd7bce77a91dd5f69bd651d3325f9226e5750c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package SDUtils/2.0.0/SDUtils-2.0.0.tar.gz

---
+++ packages/SDUtils/SDUtils-2.0.0.tar.gz/SDUtils-2.0.0/setup.py
@@ -0,0 +1,37 @@
+"""
+StratoDem Analytics : setup.py
+Principal Author(s) : Michael Clawar
+Secondary Author(s) :
+Description :
+
+Notes :
+
+September 29, 2017
+"""
+from setuptools import setup
+
+
+setup(
+    name='SDUtils',
+    version='2.0.0',
+    packages=['sd_utils'],
+    license='(c) 2017- StratoDem Analytics. All rights reserved.',
+    description='StratoDem utilities',
+    long_description='General logging and QoL utilities',
+    author='StratoDem Analytics',
+    author_email='tech@stratodem.com',
+    url='https://github.com/StratoDem/SDUtils',
+    install_requires=[
+        'slackclient>=1.1.0',
+        'numpy>=1.14.0',
+        'pandas>=0.22',
+        'joblib',
+        'dask>=0.16.1',
+        'xarray>=0.10.0',
+        'geopandas>=0.3.0',
+        'simpledbf>=0.2.6',
+        'pyarrow==0.11',
+        'toolz',
+        'cloudpickle',
+    ],
+)
