fa934957edd895490398e2ada96a1be0e5e3b5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:09 +0000

    Add package streetscape/0.2.0/streetscape-0.2.0.tar.gz

---
+++ packages/streetscape/streetscape-0.2.0.tar.gz/streetscape-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='streetscape',
+      version='0.2.0',
+      description='help to collect panoramic street view in a city',
+      url='http://github.com/yonghah/streetscape',
+      download_url='http://github.com/yonghah/streetscape/tarball/0.2.0',
+      author='Yongha Hwang',
+      author_email='yongha.hwang@gmail.com',
+      license='MIT',
+      packages=['streetscape'],
+      zip_safe=False)
