acf5f3a759f40261d4cadc71db6a875a52d11831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:35 +0000

    Add package rowingphysics/0.4.9/rowingphysics-0.4.9.tar.gz

---
+++ packages/rowingphysics/rowingphysics-0.4.9.tar.gz/rowingphysics-0.4.9/setup.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+#from setuptools import setup, find_packages
+from distutils.core import setup
+import setuptools
+
+import re
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='rowingphysics',
+
+      version=re.search(
+
+          '^__version__\s*=\s*"(.*)"',
+          open('rowingphysics/rowingphysics.py').read(),
+          re.M
+
+          ).group(1),
+
+      description='Rowing Physics calculations',
+      
+      long_description=readme(),
+
+      url='http://sanderroosendaal.wordpress.com',
+
+      author='Sander Roosendaal',
+
+      author_email='roosendaalsander@gmail.com',
+
+      license='MIT',
+
+#      py_modules = ['rowingphysics.crew',
+#                   'rowingphysics.rowingphysics',
+#                   'rowingphysics.rigging'],
+#
+#      packages=find_packages(),
+
+      packages = ['rowingphysics'],
+      
+      keywords = 'rowing ergometer concept2',
+      
+      install_requires=[
+          'numpy',
+          'matplotlib',
+          'pandas',
+          ],
+
+      zip_safe=False,
+#      include_package_data=True,
+
+
+      )
