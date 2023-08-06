4bed6e6473720522e560f14eee022aa9af4a000b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package privacyscanner/0.3.2/privacyscanner-0.3.2.tar.gz

---
+++ packages/privacyscanner/privacyscanner-0.3.2.tar.gz/privacyscanner-0.3.2/privacyscanner/scanmodules/chromedevtools/extractors/javascriptlibs.py
@@ -0,0 +1,27 @@
+from privacyscanner.scanmodules.chromedevtools.extractors.base import Extractor
+from privacyscanner.scanmodules.chromedevtools.utils import javascript_evaluate, JavaScriptError
+
+
+LIBRARY_JS = """
+(function() {
+    return {
+        'jQuery': typeof(jQuery) !== 'undefined' ? jQuery.fn.jquery : null,
+        'React': typeof(React) !== 'undefined' ? React.version : null,
+        'AngularJS': typeof(angular) !== 'undefined' ? angular.version.full : null
+    };
+})()
+"""
+
+
+class JavaScriptLibsExtractor(Extractor):
+    def extract_information(self):
+        versions = {
+            'jQuery': None,
+            'React': None,
+            'AngularJS': None
+        }
+        try:
+            versions.update(javascript_evaluate(self.page.tab, LIBRARY_JS))
+        except JavaScriptError:
+            pass
+        self.result['javascript_libraries'] = versions
