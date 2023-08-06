55487eade56b7e0b7ecf20341ebd1aacc54fa137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:07 +0000

    Add package phrydy/1.2.3/phrydy-1.2.3.tar.gz

---
+++ packages/phrydy/phrydy-1.2.3.tar.gz/phrydy-1.2.3/phrydy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from phrydy import doc
+from phrydy import mediafile
+from phrydy import utils
+from phrydy._version import get_versions
+from phrydy.mediafile import MediaFile
+
+__version__ = get_versions()['version']
+del get_versions
