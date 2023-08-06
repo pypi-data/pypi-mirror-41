fc839cbc57d35c11d6dcc25c0a43487ebcd61867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:33 +0000

    Add package dictknife/0.9.0/dictknife-0.9.0-py2.py3-none-any.whl

---
+++ packages/dictknife/dictknife-0.9.0-py2.py3-none-any.whl/dictknife/commands/jsonknife.py
@@ -0,0 +1,125 @@
+import logging
+import contextlib
+import warnings
+from dictknife.langhelpers import make_dict
+from dictknife import loading
+from dictknife import deepmerge
+from dictknife.cliutils import traceback_shortly
+from dictknife.accessing import Accessor
+from dictknife.jsonknife import Expander
+from dictknife.jsonknife import Bundler
+from dictknife.jsonknife import extract_example
+from dictknife.jsonknife.resolver import get_resolver_from_filename
+from dictknife.jsonknife.accessor import assign_by_json_pointer, access_by_json_pointer
+
+logger = logging.getLogger(__name__)
+
+
+def cut(*, src, dst, refs):
+    d = loading.loadfile(src)
+    accessor = Accessor(make_dict)
+    for ref in refs:
+        if ref.startswith("#/"):
+            ref = ref[2:]
+        accessor.maybe_remove(d, ref.split("/"))
+    loading.dumpfile(d, dst)
+
+
+def deref(*, src, dst, refs, unwrap, wrap):
+    resolver = get_resolver_from_filename(src)
+    expander = Expander(resolver)
+    if unwrap and not refs:
+        refs = list(refs)
+        refs.append(unwrap)
+
+    if not refs:
+        d = expander.expand()
+    else:
+        d = make_dict()
+        for ref in refs:
+            ref_wrap = wrap
+            if "@" in ref:
+                ref, ref_wrap = ref.split("@", 1)
+            extracted = expander.expand_subpart(expander.access(ref))
+            if ref_wrap:
+                assign_by_json_pointer(d, ref_wrap, extracted)
+            else:
+                d = deepmerge(d, extracted)
+    loading.dumpfile(d, dst)
+
+
+def bundle(*, src, dst):
+    resolver = get_resolver_from_filename(src)
+    bundler = Bundler(resolver)
+    d = bundler.bundle()
+    loading.dumpfile(d, dst)
+
+
+def examples(*, src, ref, format):
+    """output sample value from swagger's spec"""
+    data = loading.loadfile(src)
+    if ref is not None:
+        data = access_by_json_pointer(data, ref)
+    d = extract_example(data)
+    loading.dumpfile(d, format=format)
+
+
+def main():
+    import argparse
+    formats = loading.get_formats()
+
+    parser = argparse.ArgumentParser()
+    parser.print_usage = parser.print_help  # hack
+    parser.add_argument(
+        "--log", choices=list(logging._nameToLevel.keys()), default="INFO", dest="log_level"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+
+    subparsers = parser.add_subparsers(dest="subcommand")
+    subparsers.required = True
+
+    # cut
+    fn = cut
+    sparser = subparsers.add_parser(fn.__name__, description=fn.__doc__)
+    sparser.set_defaults(subcommand=fn)
+    sparser.add_argument("--src", default=None)
+    sparser.add_argument("--dst", default=None)
+    sparser.add_argument("--ref", dest="refs", action="append")
+
+    # deref
+    fn = deref
+    sparser = subparsers.add_parser(fn.__name__, description=fn.__doc__)
+    sparser.set_defaults(subcommand=fn)
+    sparser.add_argument("--src", default=None)
+    sparser.add_argument("--dst", default=None)
+    sparser.add_argument("--ref", dest="refs", action="append")
+    sparser.add_argument("--unwrap", default=None)
+    sparser.add_argument("--wrap", default=None)
+
+    # bundle
+    fn = bundle
+    sparser = subparsers.add_parser(fn.__name__, description=fn.__doc__)
+    sparser.set_defaults(subcommand=fn)
+    sparser.add_argument("--src", default=None)
+    sparser.add_argument("--dst", default=None)
+
+    # examples
+    fn = examples
+    sparser = subparsers.add_parser(fn.__name__, description=fn.__doc__)
+    sparser.set_defaults(subcommand=fn)
+    sparser.add_argument("src", nargs="?", default=None)
+    sparser.add_argument("--ref", dest="ref", default=None)
+    sparser.add_argument("-f", "--format", default="json", choices=formats)
+
+    args = parser.parse_args()
+
+    with contextlib.ExitStack() as s:
+        params = vars(args)
+        if params.pop("quiet"):
+            args.log_level = logging._levelToName[logging.WARNING]
+            s.enter_context(warnings.catch_warnings())
+            warnings.simplefilter("ignore")
+        logging.basicConfig(level=getattr(logging, params.pop("log_level")))
+        with traceback_shortly(params.pop("debug")):
+            return params.pop("subcommand")(**params)
---
+++ packages/dictknife/dictknife-0.9.0-py2.py3-none-any.whl/dictknife/jsonknife/resolver.py
@@ -0,0 +1,151 @@
+import sys
+import logging
+import os.path
+from .. import loading
+from ..langhelpers import reify, pairrsplit
+from .relpath import normpath
+from ..accessing import Accessor
+from .accessor import (
+    access_by_json_pointer,
+    assign_by_json_pointer,
+)
+
+logger = logging.getLogger("jsonknife.resolver")
+
+
+class AccessorMixin:
+    # need self.doc
+    def assign(self, path, value, *, doc=None, a=Accessor()):
+        return a.assign(doc or self.doc, path, value)
+
+    def access(self, path, *, doc=None, a=Accessor()):
+        return a.access(doc or self.doc, path)
+
+    def access_by_json_pointer(self, jsonref, *, doc=None):
+        return access_by_json_pointer(doc or self.doc, jsonref)
+
+    def assign_by_json_pointer(self, jsonref, value, *, doc=None):
+        return assign_by_json_pointer(doc or self.doc, jsonref, value)
+
+
+class OneDocResolver(AccessorMixin):
+    def __init__(self, doc, name="*root*", onload=None):
+        self.doc = doc
+        self.name = name
+        self.onload = onload
+        if self.onload is not None:
+            self.onload(self.doc, self)
+
+    def resolve(self, query, format=None):
+        # not support external file
+        if not query.startswith("#/"):
+            raise ValueError("invalid query {!r}".format(query))
+        return self, query[1:]
+
+
+class ExternalFileResolver(AccessorMixin):
+    def __init__(
+        self,
+        filename,
+        cache=None,
+        loader=None,
+        history=None,
+        doc=None,
+        rawfilename=None,
+        onload=None,
+        format=None
+    ):
+        self.rawfilename = rawfilename or filename
+        self.filename = os.path.normpath(os.path.abspath(filename))
+        self.cache = cache or {}  # filename -> resolver
+        self.loader = loader or loading
+        self.history = history or [ROOT]
+        self.onload = onload
+        self.format = format
+        if doc is not None:
+            self.doc = doc
+            if self.onload is not None:
+                self.onload(doc, self)
+
+    def __repr__(self):
+        return "<FileResolver {!r}>".format(self.filename)
+
+    @property
+    def name(self):
+        return self.filename
+
+    @reify
+    def doc(self):
+        logger.debug(
+            "load file[%s]: %r (where=%r)", len(self.history), self.rawfilename,
+            self.history[-1].filename
+        )
+        with open(self.filename) as rf:
+            self.doc = self.loader.load(rf, format=self.format)
+        if self.onload is not None:
+            self.onload(self.doc, self)
+        return self.doc
+
+    def new(self, filename, doc=None, rawfilename=None, format=None):
+        rawfilename = rawfilename or filename
+        history = self.history[:]
+        history.append(self)
+        return self.__class__(
+            filename,
+            cache=self.cache,
+            loader=self.loader,
+            history=history,
+            doc=doc,
+            rawfilename=rawfilename,
+            onload=self.onload,
+            format=format,
+        )
+
+    def resolve_pathset(self, query):  # todo: refactoring
+        filepath, query = pairrsplit(query, "#")
+        if filepath == "":
+            return self.filename, self.filename, query
+        fullpath = normpath(filepath, where=os.path.dirname(self.filename))
+        return fullpath, filepath, query
+
+    def resolve(self, query, format=None):
+        if query.startswith("#"):
+            return self, query[1:]
+        if "#" not in query:
+            query = query + "#"
+
+        fullpath, filepath, query = self.resolve_pathset(query)
+        return self.resolve_subresolver(fullpath, rawfilename=filepath, format=format), query
+
+    def resolve_subresolver(self, filename, rawfilename=None, format=None):
+        if filename in self.cache:
+            cached = self.cache[filename]
+            if cached.history[-1].filename == self.filename:
+                return cached
+            else:
+                return self.new(filename, doc=cached.doc, rawfilename=rawfilename, format=format)
+        subresolver = self.cache[filename] = self.new(
+            filename, rawfilename=rawfilename, format=format
+        )
+        return subresolver
+
+
+class ROOT:
+    filename = "*root*"
+    rawfilename = "*root*"
+    history = []
+
+
+def get_resolver(filename, loader=loading, doc=None, onload=None):
+    if filename is None:
+        doc = doc or loading.load(sys.stdin)
+        return OneDocResolver(doc, onload=onload)
+    else:
+        resolver = ExternalFileResolver(filename, loader=loader, onload=onload)
+        if doc:
+            resolver.doc = doc
+        return resolver
+
+
+# for backward compatibility
+get_resolver_from_filename = get_resolver
