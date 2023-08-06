5ed995f823410fb9dae9b806b465b7e7b8ad72b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package luchunhua748/1.0.0/luchunhua748-1.0.0.tar.gz

---
+++ packages/luchunhua748/luchunhua748-1.0.0.tar.gz/luchunhua748-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='luchunhua748',
+    version='1.0.0',
+    py_modules=['luchunhua748'],
+    author='gzd',
+    author_email='2208839329@qq.com',
+    url='',
+    description='A Simple print of nested lists',
+)
