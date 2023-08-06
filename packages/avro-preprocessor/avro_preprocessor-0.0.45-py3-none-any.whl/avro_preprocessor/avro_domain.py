5e52873b800f8dea5a4a7f64c2d7ea96c85c9739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:41 +0000

    Add package avro-preprocessor/0.0.45/avro_preprocessor-0.0.45-py3-none-any.whl

---
+++ packages/avro-preprocessor/avro_preprocessor-0.0.45-py3-none-any.whl/avro_preprocessor/avro_domain.py
@@ -0,0 +1,45 @@
+"""
+Module providing a custom order for json-based Avro schemas.
+"""
+from collections import OrderedDict
+from typing import Union
+
+__author__ = "Nicola Bova"
+__copyright__ = "Copyright 2018, Jaumo GmbH"
+__email__ = "nicola.bova@jaumo.com"
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Avro:
+    """
+    Helper class to avoid plain strings as dictionary keys.
+    """
+    Name = 'name'
+    Namespace = 'namespace'
+    Type = 'type'
+    Fields = 'fields'
+    Items = 'items'
+    Doc = 'doc'
+    Record = 'record'
+    Error = 'error'
+    Enum = 'enum'
+    Default = 'default'
+    LogicalType = 'logicalType'
+
+    Protocol = 'protocol'
+    Messages = 'messages'
+    Request = 'request'
+    Response = 'response'
+    Types = 'types'
+    Errors = 'errors'
+
+    Optional = 'nullable_optional'
+    PartitionKey = 'partition-field'
+
+    Null = 'null'
+    Value = 'value'
+
+    Node = Union[str, list, OrderedDict]
+    Key = Union[slice, int, None]
