f421657aab0e1aa65bbb23abb64266ff10d5fe9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:23 +0000

    Add package sumfrom/1.1/sumfrom-1.1.tar.gz

---
+++ packages/sumfrom/sumfrom-1.1.tar.gz/sumfrom-1.1/README.md
@@ -0,0 +1,7 @@
+# sum from 1 to x
+
+Whatever.
+
+![Build Status](https://img.shields.io/travis/outloudvi/sum_from_1_to_x.svg)
+
+This is a simple project which has an aim of illustrating how can a package be freakingly overwrapped.
---
+++ packages/sumfrom/sumfrom-1.1.tar.gz/sumfrom-1.1/setup.py
@@ -0,0 +1,15 @@
+"""
+ This is a simple project which has an aim of illustrating how can a package be
+   freakingly overwrapped.
+"""
+
+from distutils.core import setup
+
+setup(name='sumfrom',
+      version='1.1',
+      description='Sum helper',
+      author='Outvi V',
+      author_email='git@outv.im',
+      url='https://github.com/outloudvi/sum_from_1_to_x',
+      py_modules=['sumfrom'],
+     )
