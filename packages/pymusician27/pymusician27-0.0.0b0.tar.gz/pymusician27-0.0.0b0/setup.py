d8604e6531ed76e44d897c09be78c5ec394c1659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:21 +0000

    Add package pymusician27/0.0.0b0/pymusician27-0.0.0b0.tar.gz

---
+++ packages/pymusician27/pymusician27-0.0.0b0.tar.gz/pymusician27-0.0.0b0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name="pymusician27",
+      version="0.0.0-b",
+      description="A python package for music composition and analysis.",
+      long_description="""
+      Read the README here:
+      https://github.com/ScottMorse/PyMusician27
+      This is the PyMusician package ported to Python 2.7
+      """,
+      url="https://github.com/ScottMorse/PyMusicia27",
+      author="Scott Morse",
+      author_email="scottmorsedev@gmail.com",
+      license="Apache",
+      packages=["pymusician27"],
+      include_package_data=True,
+      install_requires=[
+          'numpy',
+      ],
+      python_requires='>2.7',
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False
+)
