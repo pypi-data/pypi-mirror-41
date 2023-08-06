ff99be1db0100c62eed79ad32eb765c2bb36e9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package randomheaders/0.1/randomheaders-0.1.tar.gz

---
+++ packages/randomheaders/randomheaders-0.1.tar.gz/randomheaders-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='randomheaders',
+      version='0.1',
+      description='Generating Random Headers/User Agents with a locally saved file',
+      url='https://github.com/theriley106/randomheaders',
+      author='theriley106',
+      author_email='christopherlambert106@gmail.com',
+      license='MIT',
+      packages=['randomheaders'],
+      zip_safe=False)
