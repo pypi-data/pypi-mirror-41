8ffa03a1c9c03c966738dae94082dfb3abb910f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:24 +0000

    Add package phylopypruner/0.4.7/phylopypruner-0.4.7.tar.gz

---
+++ packages/phylopypruner/phylopypruner-0.4.7.tar.gz/phylopypruner-0.4.7/phylopypruner/phylopypruner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"Module for running PhyloPyPruner by just invoking this script."
+
+import os
+import sys
+
+arguments = []
+for argument in sys.argv[1:]:
+    if os.path.exists(argument):
+        argument = os.path.abspath(argument)
+    arguments.append(argument)
+
+os.chdir("..")
+os.system("{} -m phylopypruner {}".format(sys.executable, " ".join(arguments)))
