6974b5de6fb85e142906a5aa56151928196b5cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:21 +0000

    Add package BIDSHandler/0.2.1/BIDSHandler-0.2.1-py3-none-any.whl

---
+++ packages/BIDSHandler/BIDSHandler-0.2.1-py3-none-any.whl/bidshandler/__init__.py
@@ -0,0 +1,13 @@
+"""API for handling folders containing BIDS formatted data."""
+
+__version__ = '0.2.1'
+name = "BIDSHandler"  # noqa
+
+from .bidstree import BIDSTree  # noqa
+from .project import Project  # noqa
+from .subject import Subject  # noqa
+from .session import Session  # noqa
+from .scan import Scan  # noqa
+from .bidserrors import (NoProjectError, NoSubjectError, NoSessionError,  # noqa
+                         NoScanError, IDError, MappingError, AssociationError)
+from .utils import download_test_data  # noqa
