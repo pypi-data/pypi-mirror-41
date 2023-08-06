20c401f7413be983059db9a908e0e57e7520db9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:19 +0000

    Add package deepclas4bio/0.0.11/deepclas4bio-0.0.11.tar.gz

---
+++ packages/deepclas4bio/deepclas4bio-0.0.11.tar.gz/deepclas4bio-0.0.11/deepclas4bio/__init__.py
@@ -0,0 +1,5 @@
+from deepclas4bio.listMethods import *
+from deepclas4bio.predict import *
+from deepclas4bio.evaluate import *
+
+__all__=[predict,predictBatch,listFrameworks,listModels,listMeasures,evaluate]
