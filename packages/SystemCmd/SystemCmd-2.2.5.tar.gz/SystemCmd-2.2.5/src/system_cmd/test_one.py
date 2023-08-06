19f3207b0eb00eca6a72241a2bd9bf64fb529bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:26 +0000

    Add package SystemCmd/2.2.5/SystemCmd-2.2.5.tar.gz

---
+++ packages/SystemCmd/SystemCmd-2.2.5.tar.gz/SystemCmd-2.2.5/src/system_cmd/test_one.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from system_cmd import system_cmd_result
+
+
+def test_one():
+    system_cmd_result('.', 'ls -a')
+
+
+
+def test_false():
+    res = system_cmd_result('.', 'cp not-existing done')
+    print(res)
