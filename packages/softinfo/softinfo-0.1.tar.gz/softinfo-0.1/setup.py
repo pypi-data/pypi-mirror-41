4a56ffd4d6bc48627d6b13bcdc681f6ebd66a505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:02 +0000

    Add package softinfo/0.1/softinfo-0.1.tar.gz

---
+++ packages/softinfo/softinfo-0.1.tar.gz/softinfo-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'softinfo',
+    version = '0.1',
+    packages = find_packages(),
+    author="523144419",
+    author_email="523144419@qq.com",
+    description="For scan software info package",
+    url="https://github.com/523144419/autoScan4CMDB",
+)
