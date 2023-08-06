9aeb543d4baa185995da721c1b1a7df650368d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:49 +0000

    Add package flamyngo/0.9.12/flamyngo-0.9.12.tar.gz

---
+++ packages/flamyngo/flamyngo-0.9.12.tar.gz/flamyngo-0.9.12/flamyngo/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Copyright (c) Materials Virtual Lab.
+# Distributed under the terms of the BSD License.
+
+__author__ = "Shyue Ping Ong"
+__email__ ="shyuep@gmail.com"
+__version__ = "0.9.12"
+
