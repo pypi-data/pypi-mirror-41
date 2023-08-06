cbf5fa160d7aa46ef0eb1ddd748b3c34ca99a134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:10 +0000

    Add package sdhpy/0.3/sdhpy-0.3.tar.gz

---
+++ packages/sdhpy/sdhpy-0.3.tar.gz/sdhpy-0.3/sdhpy/sdhstore/ckanhelper.py
@@ -0,0 +1,43 @@
+import ckanapi
+
+from sdhpy.errors import CkanAuthError
+
+
+def get_store_user(apikey, url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    current_user = ckan.action.current_user_show()
+    if not current_user["user"]:
+        raise CkanAuthError("Invalid Store API key provided")
+    return current_user
+
+def get_dataset(apikey, name_or_id, url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    dataset_object = ckan.action.package_show(id=name_or_id)
+    return dataset_object
+
+def get_resource(apikey, id, url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    resource_object = ckan.action.resource_show(id=id)
+    return resource_object
+
+
+def get_ckan(apikey, url="https://store.smartdatahub.io"):
+    return ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+
+
+def search_resource_by_tbl_and_db(apikey, db, tbl, url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    resource_object = ckan.action.resource_search(query=["table:"+tbl.lower(), "database:" + db.lower()])
+    return resource_object
+
+
+def search_resources_by_db(apikey, db, url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    resource_object = ckan.action.resource_search(query=["database:" + db.lower()])
+    return resource_object
+
+
+def serve_resource(apikey, resource_id, delivery_channel="http", delivery_format="csv_headers", url="https://store.smartdatahub.io"):
+    ckan = ckanapi.RemoteCKAN(url, apikey, "Smart Data Hub Python Client")
+    resource_object = ckan.action.package_serve(resource_id=resource_id, delivery_channel=delivery_channel, delivery_format=delivery_format)
+    return resource_object
---
+++ packages/sdhpy/sdhpy-0.3.tar.gz/sdhpy-0.3/sdhpy/tests/pandas_tests.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from sdhpy.pandas import get_pandas_dataframe, SdhPandas
+import os
+
+
+class TestPandas(TestCase):
+    def setUp(self):
+        self.api_key = os.environ["SDH_API"]
+        self.sdh = SdhPandas(store_apikey=self.api_key)
+    def tearDown(self):
+        pass
+
+    def test_pandas_url(self):
+        dataset = self.sdh.url(
+            "http://store.smartdatahub.io/dataset/fo_hagstova_foroya_tb01010_current_accounts_summary_1998_2015/resource/2a451036-8a7f-4506-853e-26b5a000dc4a")
+        self.assertIsNotNone(dataset)
+        self.assertIsNotNone(dataset.data)
+
+    def test_pandas_dynamic(self):
+        dataset = self.sdh.fo_hagstova_foroya.tb01010_current_accounts_summary_1998_2015__fact_all_piped
+        self.assertIsNotNone(dataset)
+        self.assertIsNotNone(dataset.data)
+
+    def test_show_tables(self):
+        self.assertIsNotNone(self.sdh.fi_vuosien_2012_2014_data_vuoden_tarkkuudella.show_tables())
+
