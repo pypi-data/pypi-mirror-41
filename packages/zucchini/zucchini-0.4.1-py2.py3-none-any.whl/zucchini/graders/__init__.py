d3384dd64ddbf9736aadb42ce4c8a0d3269d5b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:53 +0000

    Add package zucchini/0.4.1/zucchini-0.4.1-py2.py3-none-any.whl

---
+++ packages/zucchini/zucchini-0.4.1-py2.py3-none-any.whl/zucchini/graders/__init__.py
@@ -0,0 +1,30 @@
+from .exceptions import InvalidGraderConfigError
+from .grader_interface import GraderInterface, Part
+from .threaded_grader import ThreadedGrader
+from .prompt_grader import PromptGrader
+from .open_file_grader import OpenFileGrader
+from .command_grader import CommandGrader
+from .libcheck_grader import LibcheckGrader
+from .junit_json_grader import JUnitJSONGrader
+from .junit_xml_grader import JUnitXMLGrader
+from .bitwise_json_grader import BitwiseJSONGrader
+from .circuitsim_grader import CircuitSimGrader
+from .pylc3_grader import PyLC3Grader
+
+__all__ = ['InvalidGraderConfigError', 'GraderInterface', 'Part',
+           'ThreadedGrader', 'PromptGrader', 'OpenFileGrader', 'CommandGrader',
+           'LibcheckGrader', 'JUnitJSONGrader', 'JUnitXMLGrader',
+           'BitwiseJSONGrader', 'CircuitSimGrader', 'PyLC3Grader']
+
+_GRADERS = (
+    PromptGrader,
+    OpenFileGrader,
+    CommandGrader,
+    LibcheckGrader,
+    JUnitJSONGrader,
+    JUnitXMLGrader,
+    BitwiseJSONGrader,
+    CircuitSimGrader,
+    PyLC3Grader,
+)
+AVAILABLE_GRADERS = {cls.__name__: cls for cls in _GRADERS}
