04bdbc7ecdb5ee3a3ada697fd5bc6b126de1159e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:04 +0000

    Add package waynevanpython/0.0.1/waynevanpython-0.0.1.tar.gz

---
+++ packages/waynevanpython/waynevanpython-0.0.1.tar.gz/waynevanpython-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waynevanpython",
+    version="0.0.1",
+    author="Wayne Van Son",
+    author_email="waynevspersonal@gmail.com",
+    description="An example, but mainly shortcuts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/waynevanson/waynevanpython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
