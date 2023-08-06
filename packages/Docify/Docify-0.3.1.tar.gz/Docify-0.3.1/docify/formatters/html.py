a2850e92909a6bf0c7bca01f61e9a114a4984863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package Docify/0.3.1/Docify-0.3.1.tar.gz

---
+++ packages/Docify/Docify-0.3.1.tar.gz/Docify-0.3.1/docify/formatters/html.py
@@ -0,0 +1,181 @@
+import cgi
+
+from docify.lib.formatter import Formatter
+from docify import Document, components as c
+
+
+DOC_TMPL = '''\
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Docify Document</title>
+    </head>
+    <body>
+        <div id="container">
+            {}
+        </div>
+    </body>
+</html>
+'''
+
+
+class HTML(Formatter):
+    '''HTML formatter to format document into plain HTML.'''
+
+    def __init__(self, *args, **kwargs):
+        self.tmpl = DOC_TMPL
+        self.indent = 4
+        self._spacing = 4
+        self.initial_spacing = 12
+        super(HTML, self).__init__(*args, **kwargs)
+
+    def tag(self, tag, components=[], properties={}):
+        attrs = ''
+        if len(properties) > 0:
+            attrs += ' '
+            for k in properties:
+                v = properties[k]
+                attrs += '{}="{}"'.format(k, v.replace('"', '\\"'))
+
+        if len(components) == 0:
+            return '<{0}{1} />'.format(tag, attrs)
+
+        return '<{0}{1}>{2}</{0}>'.format(
+            tag, attrs, ''.join([self.f(c) for c in components]))
+
+    def update_handlers(self):
+        '''Overriding parent method'''
+
+        super(HTML, self).update_handlers()
+
+        @self.handle(Document)
+        def handle_doc(self, obj):
+            self._spacing
+            return self.tmpl.format(('\n' + (' ' * self._spacing)).join(
+                [self.f(c) for c in obj.components]))
+
+        @self.handle(c.Text)
+        def handle_text(self, obj):
+            return cgi.escape(obj.value)
+
+        @self.handle(c.Nbsp)
+        def handle_nbsp(self, obj):
+            return '&nbsp;'
+
+        @self.handle(c.Break)
+        def handle_br(self, obj):
+            return '<br />'
+
+        @self.handle(c.HorizontalRule)
+        def handle_hr(self, obj):
+            return '<hr />'
+
+        @self.handle(c.Anchor)
+        def handle_a(self, obj):
+            return self.tag('a', [obj.value], obj.props)
+
+        @self.handle(c.Image)
+        def handle_img(self, obj):
+            return self.tag('img', [], obj.props)
+
+        @self.handle(c.Header1)
+        def handle_h1(self, obj):
+            return self.tag('h1', obj.components, obj.props)
+
+        @self.handle(c.Header2)
+        def handle_h2(self, obj):
+            return self.tag('h2', obj.components, obj.props)
+
+        @self.handle(c.Header3)
+        def handle_h3(self, obj):
+            return self.tag('h3', obj.components, obj.props)
+
+        @self.handle(c.Header4)
+        def handle_h4(self, obj):
+            return self.tag('h4', obj.components, obj.props)
+
+        @self.handle(c.Header5)
+        def handle_h5(self, obj):
+            return self.tag('h5', obj.components, obj.props)
+
+        @self.handle(c.Header6)
+        def handle_h6(self, obj):
+            return self.tag('h6', obj.components, obj.props)
+
+        @self.handle(c.Footer)
+        def handle_footer(self, obj):
+            return self.tag('footer', obj.components, obj.props)
+
+        @self.handle(c.Small)
+        def handle_small(self, obj):
+            return self.tag('small', obj.components, obj.props)
+
+        @self.handle(c.Cite)
+        def handle_cite(self, obj):
+            return self.tag('cite', obj.components, obj.props)
+
+        @self.handle(c.Italic)
+        def handle_i(self, obj):
+            return self.tag('i', obj.components, obj.props)
+
+        @self.handle(c.Bold)
+        def handle_b(self, obj):
+            return self.tag('b', obj.components, obj.props)
+
+        @self.handle(c.Blockquote)
+        def handle_blockquote(self, obj):
+            return self.tag('blockquote', obj.components, obj.props)
+
+        @self.handle(c.Pre)
+        def handle_pre(self, obj):
+            return self.tag('pre', obj.components, obj.props)
+
+        @self.handle(c.Code)
+        def handle_code(self, obj):
+            return self.tag('code', obj.components, obj.props)
+
+        @self.handle(c.Del)
+        def handle_del(self, obj):
+            return self.tag('del', obj.components, obj.props)
+
+        @self.handle(c.Section)
+        def handle_section(self, obj):
+            return self.tag('section', obj.components, obj.props)
+
+        @self.handle(c.Paragraph)
+        def handle_p(self, obj):
+            return self.tag('p', obj.components, obj.props)
+
+        @self.handle(c.Span)
+        def handle_span(self, obj):
+            return self.tag('span', obj.components, obj.props)
+
+        @self.handle(c.OrderedList)
+        def handle_ol(self, obj):
+            return self.tag('ol', obj.components, obj.props)
+
+        @self.handle(c.UnorderedList)
+        def handle_ul(self, obj):
+            return self.tag('ul', obj.components, obj.props)
+
+        @self.handle(c.ListItem)
+        def handle_li(self, obj):
+            return self.tag('li', obj.components, obj.props)
+
+        @self.handle(c.Table)
+        def handle_table(self, obj):
+            return self.tag('table', obj.components, obj.props)
+
+        @self.handle(c.TableHeader)
+        def handle_th(self, obj):
+            return self.tag('th', obj.components, obj.props)
+
+        @self.handle(c.TableRow)
+        def handle_tr(self, obj):
+            return self.tag('tr', obj.components, obj.props)
+
+        @self.handle(c.TableData)
+        def handle_td(self, obj):
+            return self.tag('td', obj.components, obj.props)
---
+++ packages/Docify/Docify-0.3.1.tar.gz/Docify-0.3.1/docify/lib/formatter.py
@@ -0,0 +1,60 @@
+import re
+from copy import deepcopy
+
+from docify import Document, components as c
+
+
+class Formatter(object):
+    '''Abstract class for formatter. Do not use it directly.
+
+    :param Document document: Document to format.
+    :param bool cite: Whether to add the grumpy citation. Default is True.
+
+    Example usage: ::
+
+        class MyFormatter(Formatter):
+            @self.handle(Span, Paragraph)
+            def handle_span_and_paragraph(self, obj):
+                return str(obj)
+    '''
+
+    def __init__(self, document, cite=True):
+        self.handlers = {}
+        raw_doc = deepcopy(document)
+        if cite:
+            raw_doc.add(c.Hr())
+            raw_doc.add(c.Footer(c.P(c.Small(c.Cite(
+                'This document was generated with ',
+                c.A('Docify', 'https://github.com/rapidstack/Docify'),
+                '.')))))
+        self.update_handlers()
+        self.doc = self.format(raw_doc)
+
+    def handle(self, *items):
+        '''Handle decorator. Register a handler for given items.
+        
+        :param list items: List of items to handle.
+        '''
+        return lambda func: [self.handlers.update({i: func}) for i in items]
+    
+    def update_handlers(self):
+        '''Update handlers. Called by __init__ before performing format operation.'''
+        pass
+
+    def format(self, obj):
+        '''Parse and format an object using appropriate handler.
+        If handler is not found, it will use `str`.
+
+        :param Component|Document obj: Object to format.
+        '''
+        otype = type(obj)
+        if otype not in self.handlers:
+            return str(obj)
+        return self.handlers[otype](self, obj)
+
+    def __repr__(self):
+        return self.doc
+
+
+# Alias for format method
+Formatter.f = Formatter.format
