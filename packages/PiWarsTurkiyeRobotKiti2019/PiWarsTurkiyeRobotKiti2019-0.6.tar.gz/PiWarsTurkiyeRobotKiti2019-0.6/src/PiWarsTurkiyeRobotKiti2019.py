af24eeeaca020b4e3f20bb12ca7636291e7eb22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package PiWarsTurkiyeRobotKiti2019/0.6/PiWarsTurkiyeRobotKiti2019-0.6.tar.gz

---
+++ packages/PiWarsTurkiyeRobotKiti2019/PiWarsTurkiyeRobotKiti2019-0.6.tar.gz/PiWarsTurkiyeRobotKiti2019-0.6/src/PiWarsTurkiyeRobotKiti2019.py
@@ -0,0 +1,10 @@
+import ServoKontrol
+import UltrasonikSensoru
+import MotorKontrol
+import Kumanda
+import HizlandirilmisPiKamera
+
+class piwarsturkiyerobotkiti2019:
+
+    def __init__(self):
+        self.__version__ = "0.6"
