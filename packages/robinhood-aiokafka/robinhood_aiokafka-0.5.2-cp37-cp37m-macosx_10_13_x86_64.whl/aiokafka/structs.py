14658dfc9527b0657ff5a7081e8da305d6a253e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:37 +0000

    Add package robinhood-aiokafka/0.5.2/robinhood_aiokafka-0.5.2-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/robinhood-aiokafka/robinhood_aiokafka-0.5.2-cp37-cp37m-macosx_10_13_x86_64.whl/aiokafka/structs.py
@@ -0,0 +1,19 @@
+import collections
+from rhkafka.common import OffsetAndMetadata, TopicPartition, BrokerMetadata
+
+__all__ = [
+    "OffsetAndMetadata", "TopicPartition", "RecordMetadata", "ConsumerRecord",
+    "BrokerMetadata"
+]
+
+RecordMetadata = collections.namedtuple(
+    'RecordMetadata', ['topic', 'partition', 'topic_partition', 'offset',
+                       'timestamp', 'timestamp_type'])
+
+ConsumerRecord = collections.namedtuple(
+    "ConsumerRecord", ["topic", "partition", "offset", "timestamp",
+                       "timestamp_type", "key", "value", "checksum",
+                       "serialized_key_size", "serialized_value_size"])
+
+OffsetAndTimestamp = collections.namedtuple(
+    "OffsetAndTimestamp", ["offset", "timestamp"])
