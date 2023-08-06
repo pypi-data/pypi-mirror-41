a06c394ec69200669bc0eda21bd203691a3ea2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package agithub/2.2.0/agithub-2.2.0.tar.gz

---
+++ packages/agithub/agithub-2.2.0.tar.gz/agithub-2.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '2.2.0'
+
+setup(name='agithub',
+      version=version,
+      description="A lightweight, transparent syntax for REST clients",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
+      ],
+      keywords=['api', 'REST', 'GitHub', 'Facebook', 'SalesForce'],
+      author='Jonathan Paugh',
+      author_email='jpaugh@gmx.us',
+      url='https://github.com/jpaugh/agithub',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      )
