c3b431e4831495274a15165707d345e5c6ea9adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package api-shop/1.6.1/api-shop-1.6.1.tar.gz

---
+++ packages/api-shop/api-shop-1.6.1.tar.gz/api-shop-1.6.1/api_shop/data_format.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from datetime import datetime as dt
+
+class datetime():
+    '''将str转换成datetime格式'''
+    
+    def __repr__(self):
+        return "<class 'datetime'>"
+
+    def __new__(self, string):
+        if ':' in string:
+            return dt.strptime(string, '%Y-%m-%d %H:%M:%S')
+        else:
+            return dt.strptime(string, '%Y-%m-%d')
