d8308905aeb28d033599f23dd00fac3c14923355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:11 +0000

    Add package portainer-py/0.7.5/portainer-py-0.7.5.tar.gz

---
+++ packages/portainer-py/portainer-py-0.7.5.tar.gz/portainer-py-0.7.5/portainer_py/portainer.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+from urllib.parse import urljoin
+
+import requests
+
+from . import api_versions
+from .api_versions import Portainer
+
+
+def portainer_for_host(host) -> Portainer:
+    """
+    Finds and returns the right Portainer class for the version
+    running on this host
+    """
+    url = urljoin(host, "api/status")
+
+    try:
+        r = requests.request("GET", url).json()
+    except requests.exceptions.RequestException as err:
+        raise ConnectionError(err)
+
+    return api_versions.get_closest_api_version(r.get("Version"))(host)
