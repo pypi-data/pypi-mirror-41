d58735ea24877530018849bdf656df3665e9a1bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:26 +0000

    Add package j5/0.0.2/j5-0.0.2-py3-none-any.whl

---
+++ packages/j5/j5-0.0.2-py3-none-any.whl/j5/__init__.py
@@ -0,0 +1,5 @@
+"""j5 Robotics API."""
+
+from .boards import BoardGroup
+
+__all__ = ["BoardGroup"]
---
+++ packages/j5/j5-0.0.2-py3-none-any.whl/tests/test_smoke.py
@@ -0,0 +1,6 @@
+"""Bare basic tests."""
+
+
+def test_if_we_can_make_a_robot():
+    """Test if we can import the library."""
+    import j5  # noqa: W391, F401
