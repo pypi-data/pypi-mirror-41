d0a12b6a79168e0d0db68e76b842d56ff06cdb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:31 +0000

    Add package pyipma/1.2.1/pyipma-1.2.1.tar.gz

---
+++ packages/pyipma/pyipma-1.2.1.tar.gz/pyipma-1.2.1/pyipma/consts.py
@@ -0,0 +1,36 @@
+API_DISTRITS = "http://api.ipma.pt/open-data/distrits-islands.json"
+API_FORECAST = "http://api.ipma.pt/open-data/forecast/meteorology/cities/daily/"
+NEW_API_FORECAST = "http://api.ipma.pt/public-data/forecast/aggregate/"
+API_OBSERVATION_STATIONS = "http://api.ipma.pt/public-data/observation/surface-stations/stations.json"
+API_OBSERVATION_OBSERVATIONS = "http://api.ipma.pt/public-data/observation/surface-stations/observations.json"
+API_WEATHER_TYPE = "http://api.ipma.pt/open-data/weather-type-classe.json"
+API_WIND_TYPE = "http://api.ipma.pt/open-data/wind-speed-daily-classe.json"
+
+#deprecated
+#API_OBSERVATION = "http://pda.ipma.pt/observacao.jsp"
+#API_XML_OBSERVATION = "http://www.ipma.pt/resources.www/internal.user/pw_hh_pt.xml"
+
+WIND_DIRECTION = {
+    'N': "Norte",
+    'S': "Sul",
+    'E': "Este",
+    'W': "Oeste",
+    'NE': "Nordeste",
+    'SE': "Sudeste",
+    'SW': "Sudoeste",
+    'NW': "Noroeste",
+    '': "",
+}
+
+WIND_DIRECTION_ID = {
+    0: '',
+    1: 'N',
+    2: 'NE',
+    3: 'E',
+    4: 'SE',
+    5: 'S',
+    6: 'SW',
+    7: 'W',
+    8: 'NW',
+    9: 'N'
+}
