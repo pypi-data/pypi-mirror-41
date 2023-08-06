f7200cb58ebbde279078a71b0e28f7c45c924b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:51 +0000

    Add package html-telegraph-poster/0.1.40/html_telegraph_poster-0.1.40.tar.gz

---
+++ packages/html-telegraph-poster/html_telegraph_poster-0.1.40.tar.gz/html_telegraph_poster-0.1.40/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='html_telegraph_poster',
+      version='0.1.40',
+      description='Posts your html to telegra.ph blogging service',
+      keywords='telegra.ph post html telegram',
+      url='https://github.com/mercuree/html-telegraph-poster',
+      author='Garry G',
+      author_email='mercuree.lab@gmail.com',
+      license='MIT',
+      packages=['html_telegraph_poster'],
+      install_requires=['lxml', 'requests', 'requests_toolbelt'],
+      zip_safe=False)
