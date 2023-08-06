533a6c1023b2ee64b1aea771bfc2fa036ee1cf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:20 +0000

    Add package crs-debuger/0.0.6a1/crs_debuger-0.0.6a1.tar.gz

---
+++ packages/crs-debuger/crs_debuger-0.0.6a1.tar.gz/crs_debuger-0.0.6a1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crs_debuger",
+    version="0.0.6.alpha1",
+    author="HuangXiaojun",
+    author_email="huangxiaojun@sightp.com",
+    description="crs debuger",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ywyt738/crs_debuger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 1 - Planning",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
