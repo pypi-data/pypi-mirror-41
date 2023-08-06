6c2c443f7e703892cfb270d741da7eaa8d6de286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package argus-toolbelt/1.6.12/argus_toolbelt-1.6.12-py3-none-any.whl

---
+++ packages/argus-toolbelt/argus_toolbelt-1.6.12-py3-none-any.whl/argus_cli/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Loader for argus_cli."""
+from argus_cli.settings import settings
+from argus_cli.helpers.log import log
+
+from argus_cli.cli import main
+
+if __name__ == "__main__":
+    main()
