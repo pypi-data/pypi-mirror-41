3af5a6756f1f35b7c3be066087284ee250e1de5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:04 +0000

    Add package luchunhua/1.0.0/luchunhua-1.0.0.tar.gz

---
+++ packages/luchunhua/luchunhua-1.0.0.tar.gz/luchunhua-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='luchunhua',
+    version='1.0.0',
+    py_modules=['luchunhua'],
+    author='gzd',
+    author_email='2208839329@qq.com',
+    description='A simple printer of nested ',
+)
