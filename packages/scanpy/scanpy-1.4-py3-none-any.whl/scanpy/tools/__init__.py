dc18d4b6e6fa0dad70bc8fd891635f89d0687381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:22 +0000

    Add package scanpy/1.4/scanpy-1.4-py3-none-any.whl

---
+++ packages/scanpy/scanpy-1.4-py3-none-any.whl/scanpy/tools/__init__.py
@@ -0,0 +1,13 @@
+from ..preprocessing._simple import pca
+from ._tsne import tsne
+from ._umap import umap
+from ._diffmap import diffmap
+from ._draw_graph import draw_graph
+
+from ._paga import paga, paga_degrees, paga_expression_entropies, paga_compare_paths
+from ._rank_genes_groups import rank_genes_groups
+from ._dpt import dpt
+from ._leiden import leiden
+from ._louvain import louvain
+from ._sim import sim
+from ._score_genes import score_genes, score_genes_cell_cycle
