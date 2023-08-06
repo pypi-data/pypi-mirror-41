38e04c3840184cda61423337c24f74673dca0f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package pymarketstore2/0.17/pymarketstore2-0.17.tar.gz

---
+++ packages/pymarketstore2/pymarketstore2-0.17.tar.gz/pymarketstore2-0.17/tests/test_jsonrpc.py
@@ -0,0 +1,31 @@
+from pymarketstore import jsonrpc
+import pytest
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+import imp
+imp.reload(jsonrpc)
+
+
+@patch.object(jsonrpc, 'requests')
+def test_jsonrpc(requests):
+    requests.Session().post.return_value = 'dummy_data'
+
+    cli = jsonrpc.MsgpackRpcClient('http://localhost:5993/rcp')
+    result = cli.call('DataService.Query', a=1)
+    assert result == 'dummy_data'
+    resp = {
+        'jsonrpc': '2.0',
+        'id': 1,
+        'result': {'ok': True},
+    }
+    assert cli.response(resp)['ok']
+
+    del resp['result']
+    resp['error'] = {
+        'message': 'Error',
+        'data': 'something',
+    }
+    with pytest.raises(Exception):
+        cli.response(resp)
