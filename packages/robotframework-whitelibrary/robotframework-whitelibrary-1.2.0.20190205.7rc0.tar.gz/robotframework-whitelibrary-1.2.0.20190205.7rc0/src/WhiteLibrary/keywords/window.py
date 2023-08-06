d097791500e4ccea944dd774433f78fa73a9ebad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:40 +0000

    Add package robotframework-whitelibrary/1.2.0.20190205.7rc0/robotframework-whitelibrary-1.2.0.20190205.7rc0.tar.gz

---
+++ packages/robotframework-whitelibrary/robotframework-whitelibrary-1.2.0.20190205.7rc0.tar.gz/robotframework-whitelibrary-1.2.0.20190205.7rc0/src/WhiteLibrary/keywords/window.py
@@ -0,0 +1,41 @@
+from WhiteLibrary.keywords.librarycomponent import LibraryComponent
+from WhiteLibrary.keywords.robotlibcore import keyword
+from TestStack.White.UIItems.WindowItems import Window   # noqa: F401
+from robot.api import logger    # noqa: F401
+
+
+class WindowKeywords(LibraryComponent):
+    @keyword
+    def attach_window(self, window_title):
+        """Attach WhiteLibrary to a window of already attached application.
+
+        See `Attach Application By Id`, `Attach Application By Name` or
+        `Launch application` for more details.
+
+        ``window_title`` is the title of the window.
+        """
+        self.state.window = self.state.app.GetWindow(window_title)
+
+    @keyword
+    def select_modal_window(self, window_title):
+        """Select modal window.
+
+        ``window_title`` is the title of the window.
+        """
+        self.state.window = self.state.window.ModalWindow(window_title)
+
+    @keyword
+    def close_window(self, window_title=None):
+        """Close window with window title.
+
+        If title is not given already attached window is closed.
+        See `Attach Application By Id`, `Attach Application By Name` or
+        `Launch application` for more details.
+
+        ``window_title`` is the title of the window.
+        """
+        if window_title is not None:
+            self.state.window = self.state.app.GetWindow(window_title)
+
+        self.state.window.Close()
+
