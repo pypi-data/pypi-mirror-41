1f97553fd1e18b7a7917d1ef7ea016ec189afa2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:34 +0000

    Add package movies-organizer/1.0.3/movies_organizer-1.0.3.tar.gz

---
+++ packages/movies-organizer/movies_organizer-1.0.3.tar.gz/movies_organizer-1.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name='movies_organizer',  
+
+    version='1.0.3',
+
+    scripts=['movies_organizer'] ,
+
+    author="Ahmed Alrifai",
+
+    author_email="ahmed.alrifai.97@gmail.com",
+
+    description="Tool for organizing movies by IMDB rating.",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/ahmedalrifai/movie-organizer",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        'omdb',
+    ],
+
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ]
+ )
