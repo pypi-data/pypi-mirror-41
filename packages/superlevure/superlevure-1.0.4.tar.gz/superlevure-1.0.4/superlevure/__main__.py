1a7fbb7eaa0118e9be48f3a298aa666517de14bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:49 +0000

    Add package superlevure/1.0.4/superlevure-1.0.4.tar.gz

---
+++ packages/superlevure/superlevure-1.0.4.tar.gz/superlevure-1.0.4/superlevure/__main__.py
@@ -0,0 +1,32 @@
+import sys
+import json
+
+
+def print_c(string, message_type="info", endline=True, tqdm_=False):
+    """
+        print_colored
+        """
+    COMMANDS = {
+        "info": (33, "[!] "),
+        "que": (34, "[?] "),
+        "bad": (31, "[-] "),
+        "good": (32, "[+] "),
+        "run": (97, "[~] "),
+    }
+    message = "\033[{}m{}\033[0m{}".format(
+        COMMANDS[message_type][0], COMMANDS[message_type][1], string
+    )
+
+    if not tqdm_:
+        print(message, end=("\n" if endline else ""))
+    else:
+        if "tqdm" in sys.modules:
+            tqdm.write(message)
+
+
+def init_conf_file(name="conf.json"):
+
+    conf = open(name, "r")
+    conf = json.load(conf)
+
+    return conf
