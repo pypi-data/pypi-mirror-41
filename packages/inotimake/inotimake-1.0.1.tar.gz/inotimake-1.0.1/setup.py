5edb8e08233ed6643d3ef71b9ee29ffede1e27df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package inotimake/1.0.1/inotimake-1.0.1.tar.gz

---
+++ packages/inotimake/inotimake-1.0.1.tar.gz/inotimake-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup 
+  
+
+# Package meta-data.
+NAME = 'inotimake'
+DESCRIPTION = ''
+URL = 'https://github.com/a77763/inoti-make'
+EMAIL = 'pachedo@gmail.com'
+AUTHOR = 'A77763'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '1.0.0'
+
+
+  
+# specify requirements of your package here 
+REQUIREMENTS = ['json', 'os', 'subprocess', 'shlex', 'sys', 'nltk', 're'] 
+  
+# some more details 
+CLASSIFIERS = [ 
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: Implementation :: PyPy'
+    ] 
+  
+# calling the setup function  
+setup(
+    name=NAME,
+    version='1.0.1',
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=['inotimake'], 
+    classifiers=CLASSIFIERS, 
+    install_requires=REQUIREMENTS, 
+      ) 
