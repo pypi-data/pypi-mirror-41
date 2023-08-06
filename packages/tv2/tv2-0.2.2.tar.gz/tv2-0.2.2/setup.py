9b66413352dc603359b25ee1d8f56bb4bf62954f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:19 +0000

    Add package tv2/0.2.2/tv2-0.2.2.tar.gz

---
+++ packages/tv2/tv2-0.2.2.tar.gz/tv2-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="tv2",
+    version="0.2.2",
+    author="Sean Hammond, Vincent Perricone, Madison Scott-Clary",
+    packages=["tv2"],
+    scripts=["bin/tv2"],
+    url="http://github.com/makyo/tv2/",
+    license="GNU General Public License, Version 3",
+    description="A fast note-taking app for the UNIX terminal",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "urwid==2.0.1",
+        "chardet==3.0.4",
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        ],
+)
