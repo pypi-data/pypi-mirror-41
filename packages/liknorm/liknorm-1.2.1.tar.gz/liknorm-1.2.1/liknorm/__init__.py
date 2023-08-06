2552c1fecde12e330db460b60967554a6e9fd615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:03 +0000

    Add package liknorm/1.2.1/liknorm-1.2.1.tar.gz

---
+++ packages/liknorm/liknorm-1.2.1.tar.gz/liknorm-1.2.1/liknorm/__init__.py
@@ -0,0 +1,18 @@
+"""
+LikNorm
+=======
+
+Python wrapper around the C library LikNorm for fast integration involving
+a normal distribution and an exponential-family distribution.
+
+It exports the ``LikNormMachine`` class and the function ``test`` for testing
+the package.
+"""
+from __future__ import absolute_import as _
+
+from ._machine import LikNormMachine
+from ._testit import test
+
+__version__ = "1.2.1"
+
+__all__ = ["__version__", "test", "LikNormMachine"]
