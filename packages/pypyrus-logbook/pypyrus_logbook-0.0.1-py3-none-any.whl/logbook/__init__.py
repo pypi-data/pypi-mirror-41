d25d0e09b0f7d59a9c2c447fe8427bf96238dcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:17 +0000

    Add package pypyrus-logbook/0.0.1/pypyrus_logbook-0.0.1-py3-none-any.whl

---
+++ packages/pypyrus-logbook/pypyrus_logbook-0.0.1-py3-none-any.whl/logbook/__init__.py
@@ -0,0 +1,12 @@
+from .log import Log
+from .header import Header
+from .output import Output
+from .record import Record
+from .formatter import Formatter
+
+__author__ = 'Timur Faradzhov'
+__email__ = 'timurfaradzhov@gmail.com'
+__copyright__ = 'Copyright 2019, Timur Faradzhov.'
+__doc__ = 'Module for extended logging in Python applications.'
+__version__ = '0.0.1'
+__license__ = 'MIT'
