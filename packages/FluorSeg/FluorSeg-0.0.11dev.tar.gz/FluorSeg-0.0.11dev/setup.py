05eb576b638eb62ef25e04abddbf73a74234e10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:16 +0000

    Add package FluorSeg/0.0.11dev/FluorSeg-0.0.11dev.tar.gz

---
+++ packages/FluorSeg/FluorSeg-0.0.11dev.tar.gz/FluorSeg-0.0.11dev/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='FluorSeg',
+    version='0.0.11dev',
+    packages=['fluorseg', 'fluorseg.test'],
+    author='Dan MacLean',
+    author_email='dan.maclean@tsl.ac.uk',
+    url="https://github.com/TeamMacLean/fluorseg",
+    license='LICENSE.txt',
+    description="Segment regions from fluorescence images",
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "javabridge >= 1.0.14",
+        "python-bioformats >= 1.5.2",
+        "matplotlib >= 3.0.1",
+        "numpy >= 1.5.3",
+        "read-roi >= 1.4.2",
+        "pillow >= 5.3.0",
+        "scipy >= 1.1.0",
+        "scikit-image >= 0.14.1",
+    ],
+)
