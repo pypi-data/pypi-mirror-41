2b5453fecebae0e03172f7da69db023a51fd908a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package o2-docparser/1.6/o2_docparser-1.6.tar.gz

---
+++ packages/o2-docparser/o2_docparser-1.6.tar.gz/o2_docparser-1.6/o2_docparser.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+from pathlib import Path
+import requests
+import json
+import pandas as pd
+import re
+from time import sleep
+
+
+# In[41]:
+
+
+def make_json_file(filename, json_filename, download_url, parsed_response_id, content):
+    data = {
+        "filename": "{0}.pdf".format(filename),
+        "parsed_response_id": parsed_response_id,
+        "download_url": download_url,
+        "content": content,
+    }
+    with open(json_filename, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+    return data
+
+
+def get_content(download_url, api_key):
+    res = requests.get(download_url, auth=(api_key, ""), timeout=5)
+    try:
+        content = res.json()[0]
+    except:
+        content = {}
+    return content
+
+
+def get_response(download_url):
+    count = 0
+    content = {}
+    while content == {}:
+        content = get_content(download_url, api_key)
+        if content == {}:
+            sleep(10)
+            count += 1
+            print(count * 10)
+    return content
+
+
+def convert_invoice(filename, api_key, parser_id):
+    upload_url = f"https://api.docparser.com/v1/document/upload/{parser_id}"
+    j = Path("json_data")
+    json_filename = j / f"{filename}.json"
+    if json_filename.exists():
+        print("Exists")
+        with open(json_filename) as json_data:
+            data = json.load(json_data)
+    else:
+        with open("./invoices/{}.pdf".format(filename), "rb") as f:
+            req = requests.post(upload_url, files={"file": f}, auth=(api_key, ""))
+        parsed_response_id = req.json()["id"]
+        download_url = (
+            f"https://api.docparser.com/v1/results/{parser_id}/{parsed_response_id}"
+        )
+        content = get_response(download_url)
+        data = make_json_file(
+            filename, json_filename, download_url, parsed_response_id, content
+        )
+    return data
+
+
+# In[ ]:
---
+++ packages/o2-docparser/o2_docparser-1.6.tar.gz/o2_docparser-1.6/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="o2_docparser",
+    version="1.6",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["o2_docparser"],
+    install_requires=["o2_docparser"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
