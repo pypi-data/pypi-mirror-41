0c3318fbd374c9738833c316696c9f7a9f7fb073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:03 +0000

    Add package dict.cc.py/3.0.0/dict.cc.py-3.0.0.tar.gz

---
+++ packages/dict.cc.py/dict.cc.py-3.0.0.tar.gz/dict.cc.py-3.0.0/README.md
@@ -0,0 +1,59 @@
+dict.cc.py
+=========
+
+![dict.cc.py usage](https://i.imgur.com/83XCU53.gif)
+
+Simple unofficial command line interface for ![dict.cc](https://www.dict.cc) written in Python 2. It supports translations between the most common languages available on the website.
+
+Installation
+------------
+
+dict.cc.py works with python2 and python3 and is available at pypi. All you have to do to install it is:
+
+```bash
+pip install dict.cc.py
+```
+
+Usage
+-----
+
+It's super easy! Here's a quick example of using it to translate the word `beer` between english (`en`) and swedish (`sv`):
+
+```bash
+$ dict.cc.py en sv beer
+Showing 3 of 3 result(s)
+
+  English                                                     Swedish
+  =======                                                     =======
+  beer........................................................öl
+  beer glass..................................................ölglas
+  wheat beer..................................................veteöl
+```
+
+You can also search for phrases by using quotation marks:
+
+```bash
+$ dict.cc.py en sv "free beer"
+Showing 10 of 50 result(s)
+
+  English                                                     German
+  =======                                                     =======
+  free beer...................................................Freibier
+  alcohol-free beer...........................................alkoholfreies Bier
+  free as in beer.............................................frei wie in Freibier
+  Beer-Lambert law............................................Lambert-Beer-Gesetz
+  free agent..................................................Free Agent
+  lead-free labels............................................Bleifrei-Aufkleber
+  Free Birds..................................................Free Birds – Esst uns an einem anderen Tag
+  beer........................................................Bier
+  beer........................................................Gerstenkaltschale
+  beer........................................................Gerstenlimonade
+  beer........................................................Gerstensaft
+```
+
+Available languages include: `en`, `de`, `sv`, `pt`, `it`, `fr`, `ro`, `nl`.
+
+License
+-------
+
+Public domain.
---
+++ packages/dict.cc.py/dict.cc.py-3.0.0.tar.gz/dict.cc.py-3.0.0/scripts/dict.cc.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from dictcc import Dict, AVAILABLE_LANGUAGES
+
+
+LINE_LENGTH = 60
+
+
+def ensure_unicode(string):
+    if hasattr(string, "decode"):
+        return string.decode("utf-8")
+    return string
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Unoficial CLI for dict.cc. "\
+        "It supports translations between the following languages: {}".format(
+        ", ".join(AVAILABLE_LANGUAGES)))
+    parser.add_argument("input_language", type=str, help="Input language",
+        choices=AVAILABLE_LANGUAGES.keys())
+    parser.add_argument("output_language", type=str, help="Output language",
+        choices=AVAILABLE_LANGUAGES.keys())
+    parser.add_argument("word",
+                         type=ensure_unicode,
+                         help=("Word to translate (use quotation marks for "
+                               "phrases like \"free beer\")."))
+    parser.add_argument("--max-results", type=int, default=10)
+    args = parser.parse_args()
+
+    if args.input_language == args.output_language:
+        raise ValueError("Please choose different languages")
+
+    return args
+
+
+def print_header(from_lang, to_lang):
+    print(u"{}{}{}\n{}{}{}".format(
+        from_lang,
+        " "*(LINE_LENGTH-len(from_lang)),
+        to_lang,
+        "="*len(from_lang),
+        " "*(LINE_LENGTH-len(from_lang)),
+        "="*len(to_lang),
+    ))
+
+
+def print_translation(input_word, output_word):
+    # Two points less for the spaces, so tables don't break
+    print(u"{} {} {}".format(input_word,
+                          "."*(LINE_LENGTH-len(input_word)-2),
+                          output_word))
+
+
+
+def run():
+    args = parse_args()
+
+    result = Dict.translate(args.word,
+                            args.input_language,
+                            args.output_language)
+
+    if not result.n_results:
+        print("No results found for {} ({} <-> {}).".format(
+            args.word, args.input_language, args.output_language))
+        return
+    else:
+        print("Showing {} of {} result(s)\n".format(
+            min(args.max_results, result.n_results), result.n_results))
+
+    print_header(result.from_lang, result.to_lang)
+
+    for i, (input_word, output_word) in enumerate(result.translation_tuples):
+        print_translation(input_word, output_word)
+        if i == args.max_results:
+            break
+
+
+if __name__ == "__main__":
+    run()
+
+
