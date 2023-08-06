5f2a4e05665dd84c90fbd4fc32a4f308141ea015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package jmorman-authenticator/0.0.4/jmorman-authenticator-0.0.4.tar.gz

---
+++ packages/jmorman-authenticator/jmorman-authenticator-0.0.4.tar.gz/jmorman-authenticator-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmorman-authenticator',
+    version='0.0.4',
+    description='Custom authenticator',
+    author='John Morman',
+    author_email='jmorman@microsoft.com',
+    license='3 Clause BSD',
+    packages=['customoauth']
+)
