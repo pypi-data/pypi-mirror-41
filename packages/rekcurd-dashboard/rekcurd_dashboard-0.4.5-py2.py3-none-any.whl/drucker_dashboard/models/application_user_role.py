9818c838196216edead9f9d38d37c4e8f698c5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package rekcurd-dashboard/0.4.5/rekcurd_dashboard-0.4.5-py2.py3-none-any.whl

---
+++ packages/rekcurd-dashboard/rekcurd_dashboard-0.4.5-py2.py3-none-any.whl/drucker_dashboard/models/application_user_role.py
@@ -0,0 +1,38 @@
+import enum
+from .dao import db
+from sqlalchemy import (
+    Column, Integer,
+    Enum,
+    ForeignKey,
+    UniqueConstraint
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
+
+
+class Role(enum.Enum):
+    viewer = 1
+    editor = 2
+    owner = 3
+
+
+class ApplicationUserRole(db.Model):
+    """
+    Application-User Role
+    """
+    __tablename__ = 'application_user_roles'
+    __table_args__ = (
+        UniqueConstraint('application_id', 'user_id'),
+        {'mysql_engine': 'InnoDB'}
+    )
+
+    application_id = Column(Integer, ForeignKey('applications.application_id', ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), primary_key=True)
+    role = Column(Enum(Role), nullable=False, default=Role.viewer)
+
+    application = relationship('Application', lazy='joined', innerjoin=True,
+                               backref=backref("application_user_roles",
+                                               cascade="all, delete-orphan", passive_deletes=True))
+    user = relationship('User',
+                        backref=backref("application_user_roles",
+                                        cascade="all, delete-orphan", passive_deletes=True))
