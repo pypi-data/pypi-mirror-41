24725b766748b2aee07a9672e8df81646c7a1181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:23 +0000

    Add package renew/0.1.1/renew-0.1.1.tar.gz

---
+++ packages/renew/renew-0.1.1.tar.gz/renew-0.1.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='renew',
+    description='Creates reproducible repr of arbitrary classes with a nice layout.',
+    version='0.1.1',
+    author='Michał Kaczmarczyk',
+    author_email='michal.s.kaczmarczyk@gmail.com',
+    maintainer='Michał Kaczmarczyk',
+    maintainer_email='michal.s.kaczmarczyk@gmail.com',
+    license='MIT license',
+    url='https://gitlab.com/kamichal/renew',
+    long_description=long_description,
+    long_description_content_type='text/x-rst; charset=UTF-8',
+    packages=['renew'],
+    requires=[],
+    install_requires=[],
+    keywords='',
+    classifiers=[
+        # https://pypi.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Code Generators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
