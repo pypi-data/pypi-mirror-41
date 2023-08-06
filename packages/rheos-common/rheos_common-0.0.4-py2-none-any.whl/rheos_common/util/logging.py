630700212d73f857a4f5d838e5f741225813a782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:57 +0000

    Add package rheos-common/0.0.4/rheos_common-0.0.4-py2-none-any.whl

---
+++ packages/rheos-common/rheos_common-0.0.4-py2-none-any.whl/rheos_common/util/logging.py
@@ -0,0 +1,21 @@
+import os
+import logging
+import time
+from logging.handlers import RotatingFileHandler
+
+
+def setup_root_logger(name_prefix):
+    if not os.path.exists("log"):
+        os.makedirs("log")
+
+    recfmt = logging.Formatter('%(asctime)s.%(msecs)03d %(levelname)s %(message)s')
+
+    handler = RotatingFileHandler(time.strftime(f"log/{name_prefix}.log"),maxBytes=0, backupCount=0)
+    handler.setFormatter(recfmt)
+    handler.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+
+    return logger
+
