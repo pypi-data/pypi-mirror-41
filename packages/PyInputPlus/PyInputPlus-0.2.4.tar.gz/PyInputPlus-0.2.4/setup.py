3dcaac0354ec57de4df180bda64ceb6c218dc999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:05 +0000

    Add package PyInputPlus/0.2.4/PyInputPlus-0.2.4.tar.gz

---
+++ packages/PyInputPlus/PyInputPlus-0.2.4.tar.gz/PyInputPlus-0.2.4/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+import re
+
+# Load version from module (without loading the whole module)
+with open('src/pyinputplus/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
+setup(
+    name='PyInputPlus',
+    version=version,
+    url='https://github.com/asweigart/pyinputplus',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=('Provides more featureful versions of input() and raw_input().'),
+    license='BSD',
+    long_description=long_description,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    test_suite='tests',
+    install_requires=['pysimplevalidate>=0.2.6'],
+    keywords="input validation text gui message box",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
