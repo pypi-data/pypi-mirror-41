34c9ff501a77c96a86085ab4c8f417243c4ff665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package japtkpy/0.0.3/japtkpy-0.0.3.tar.gz

---
+++ packages/japtkpy/japtkpy-0.0.3.tar.gz/japtkpy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="japtkpy",
+    version="0.0.3",
+    author="Sheharyar Naseem",
+    author_email="sherryfbi1994@gmal.com",
+    description="A simple Japanese Language Tokenizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sherryt400/japtkpy",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
