088f2b2d518dc4e7ee4693677039f6663e59a845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:31 +0000

    Add package luo/0.0.1/luo-0.0.1.tar.gz

---
+++ packages/luo/luo-0.0.1.tar.gz/luo-0.0.1/setup.py
@@ -0,0 +1,42 @@
+import codecs
+import os
+import sys
+
+try:
+	from setuptools import setup, find_packages
+except:
+	from distutils.core import setup
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "luo",
+    version = "0.0.1",
+    description = "tools for luo",
+    long_description = read("README.md"),
+    classifiers =
+	[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+		'Topic :: Scientific/Engineering :: Astronomy',
+		'Topic :: Scientific/Engineering :: GIS',
+		'Topic :: Scientific/Engineering :: Mathematics',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+    ],
+    install_requires=
+	[
+
+    ],
+    keywords = "tools",
+    author = "yucheng",
+    author_email = "yuchengluo@outlook.com",
+    url ="https://github.com/guruL/luo",
+    license = "MIT",
+    packages = find_packages(),
+    include_package_data= True,
+    zip_safe= True,
+)
