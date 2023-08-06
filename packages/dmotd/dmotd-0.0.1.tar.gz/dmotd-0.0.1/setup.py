3530ee8882ab5a280f3fd292b9c9a25976a887c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package dmotd/0.0.1/dmotd-0.0.1.tar.gz

---
+++ packages/dmotd/dmotd-0.0.1.tar.gz/dmotd-0.0.1/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2019 emanuele
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from setuptools import setup
+
+def get_file_conts(file):
+    with open(file, "r") as f:
+        contents = f.read()
+    
+    return contents
+
+setup(
+    name="dmotd",
+    version="0.0.1",
+    description="Dynamic Message Of The Day.",
+    long_description=get_file_conts("README.md"),
+    long_description_content_type="text/markdown",
+    url="http://github.com/monolix/dmotd",
+    author="Monolix",
+    author_email="monolix.team@gmail.com",
+    license="MIT",
+    packages=["dmotd"],
+    install_requires=get_file_conts("requirements.txt").split("\n"),
+    zip_safe=False
+)
