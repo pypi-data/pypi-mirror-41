eb06ddaf83a58611289afa040d7606aa57f71560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:30 +0000

    Add package psv/0.2.1/psv-0.2.1.tar.gz

---
+++ packages/psv/psv-0.2.1.tar.gz/psv-0.2.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2016, 2017, 2018 Joshua Walters
+"""
+
+
+version = '0.2.1'
+
+from setuptools import setup
+setup(name='psv',
+      install_requires=["tabulate==0.8.2"],
+      version=version,
+      description='CSV Parser',
+      author='Joshua W',
+      author_email='DolphDevgithub@gmail.com',
+      url='https://github.com/Dolphman/PSV',
+      packages=['psv', 'psv.core', 'psv.core.objects', 'psv.core.parsing', 'psv.core.output', "psv.core.utils", "psv.core.exceptions"],
+      package_data={'': ['LICENSE.txt', "readme.md"]},
+      keywords=['csv', "Comma Separated Values", "Python Separated Values", "psv"],
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Topic :: Utilities",
+                   "Programming Language :: Python :: 3.2",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"]
+                    )
