78e83871c81ef117b498ed828a4020536b44c514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:09 +0000

    Add package eo-learn-geometry/0.4.2/eo-learn-geometry-0.4.2.tar.gz

---
+++ packages/eo-learn-geometry/eo-learn-geometry-0.4.2.tar.gz/eo-learn-geometry-0.4.2/eolearn/geometry/__init__.py
@@ -0,0 +1,8 @@
+"""
+Subpackage containing EOTasks for geometrical transformations
+"""
+
+from .utilities import ErosionTask, VectorToRaster, RasterToVector
+from .sampling import PointSamplingTask, PointSampler, PointRasterSampler
+
+__version__ = '0.4.2'
