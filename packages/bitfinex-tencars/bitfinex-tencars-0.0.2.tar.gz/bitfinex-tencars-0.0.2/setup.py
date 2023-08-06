552ccf285e58856fc89fce5e6d251fa9ec27b7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:52 +0000

    Add package bitfinex-tencars/0.0.2/bitfinex-tencars-0.0.2.tar.gz

---
+++ packages/bitfinex-tencars/bitfinex-tencars-0.0.2.tar.gz/bitfinex-tencars-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bitfinex-tencars",
+    version="0.0.2",
+    url="https://github.com/akcarsten/bitfinex_api",
+    author="Carsten Klein",
+    description="Bitfinex REST API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
