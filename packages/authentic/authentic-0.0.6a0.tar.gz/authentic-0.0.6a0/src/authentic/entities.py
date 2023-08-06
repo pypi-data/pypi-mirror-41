af18f8a3eb3f47becfda30f26c79d13b0d8938d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:21 +0000

    Add package authentic/0.0.6a0/authentic-0.0.6a0.tar.gz

---
+++ packages/authentic/authentic-0.0.6a0.tar.gz/authentic-0.0.6a0/src/authentic/entities.py
@@ -0,0 +1,62 @@
+"""Account and Authentication Entities"""
+from enum import Enum
+
+from protean.core import field
+from protean.core.entity import Entity
+
+from .utils import get_account_roles
+
+
+class DefaultRolesEnum(Enum):
+    """ Enumerator of roles for an account """
+    ADMIN = 'ADMIN'
+
+
+class Account(Entity):
+    """
+    This class initializes an Account Entity.
+    """
+    # username, email and password for auth
+    username = field.StringMedium(required=True, unique=True)
+    email = field.StringLong(required=True, unique=True)
+    password = field.StringLong(required=True)
+
+    # List of roles for the Account
+    roles = field.List(choices=get_account_roles())
+
+    # personal information of the account
+    title = field.StringMedium()
+    name = field.StringLong()
+    phone = field.StringMedium()
+    timezone = field.StringMedium()
+
+    # Flag indicates if account has been locked
+    is_locked = field.Boolean(default=False)
+    is_active = field.Boolean(default=True)
+
+    # Flag indicates if the email has been verified
+    is_verified = field.Boolean(default=False)
+    is_idp = field.Boolean(default=False)
+
+    # Counter to save failed login attempts
+    login_attempts = field.Integer(default=0)
+    password_history = field.List(default=[])
+
+    # Multi factor authentication settings
+    mfa_key = field.StringLong()
+    mfa_enabled = field.Boolean(default=False)
+
+    verification_token = field.StringLong()
+    token_timestamp = field.DateTime()
+
+
+class Session(Entity):
+    """ This class initializes a Session entity for storing session data. """
+    # The key of this session
+    session_key = field.StringLong(identifier=True)
+
+    # Optional data stored for this session
+    session_data = field.Dict()
+
+    # Expiry date for the session
+    expire_date = field.DateTime()
---
+++ packages/authentic/authentic-0.0.6a0.tar.gz/authentic-0.0.6a0/tests/conftest.py
@@ -0,0 +1,33 @@
+"""Module to setup Factories and other required artifacts for tests"""
+import os
+
+from protean.core.repository import repo
+from protean.impl.repository.dict_repo import DictSchema
+
+from authentic.entities import Account
+from authentic.entities import Session
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+# Setup the schemas used by the test cases
+class AccountSchema(DictSchema):
+    """ Schema for the Account Entity"""
+
+    class Meta:
+        """ Meta class for schema options"""
+        entity = Account
+        schema_name = 'accounts'
+
+
+class SessionSchema(DictSchema):
+    """ Schema for the Session Entity"""
+
+    class Meta:
+        """ Meta class for schema options"""
+        entity = Session
+        schema_name = 'sessions'
+
+
+repo.register(AccountSchema)
+repo.register(SessionSchema)
