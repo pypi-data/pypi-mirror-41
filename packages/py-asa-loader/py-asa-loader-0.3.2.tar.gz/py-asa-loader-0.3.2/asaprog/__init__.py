0d0fa2ed5ae3d8fc177ab5913e5d11432276b673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:40 +0000

    Add package py-asa-loader/0.3.2/py-asa-loader-0.3.2.tar.gz

---
+++ packages/py-asa-loader/py-asa-loader-0.3.2.tar.gz/py-asa-loader-0.3.2/asaprog/__init__.py
@@ -0,0 +1,5 @@
+from asaprog.pac_decoder import PacketDecoder
+from asaprog.pac_encoder import pac_encode
+from asaprog.loader import Loader
+from asaprog.simulator import Simulator
+from asaprog.util import *
---
+++ packages/py-asa-loader/py-asa-loader-0.3.2.tar.gz/py-asa-loader-0.3.2/test/test_encoder.py
@@ -0,0 +1,13 @@
+import conftest
+from asaprog import pac_encode
+from asaprog.util import *
+
+if __name__ == "__main__":
+    pac = {
+        'command': asaProgCommand.CHK_DEVICE.value,
+        'data': b'test'
+    }
+    res = pac_encode(pac)
+    print(res)
+    print(res[-1])
+
