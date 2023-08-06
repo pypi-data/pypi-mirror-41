214e4af6f3e0f02fee3b87dead055bac4600cdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:27 +0000

    Add package MLHelper/0.2.5/MLHelper-0.2.5-py3-none-any.whl

---
+++ packages/MLHelper/MLHelper-0.2.5-py3-none-any.whl/MLHelper/__init__.py
@@ -0,0 +1,22 @@
+from ._helper_functions import GAI as GAI
+from ._helper_functions import PAI as PAI
+from ._image_label_reader.ImageLabelReader import DataObject as ImgLblReader
+from ._image_label_reader.ImageReader import Reader as ImgReader
+
+from . import heatmap
+
+
+# check for OpenCV 3
+try:
+    import cv2 as _cv2
+    _s: str = _cv2.__version__
+    if not _s.startswith("3"):
+        raise Exception("MLHelper needs OpenCV 3 to work properly")
+    del _cv2
+    del _s
+except Exception as e:
+    print(e)
+    print("please check your OpenCV installation")
+
+
+
