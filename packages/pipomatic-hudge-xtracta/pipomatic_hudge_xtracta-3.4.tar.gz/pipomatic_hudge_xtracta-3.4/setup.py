757723bc3e23f5a9451f0041d2f8f11a3a7cb502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:06 +0000

    Add package pipomatic-hudge-xtracta/3.4/pipomatic_hudge_xtracta-3.4.tar.gz

---
+++ packages/pipomatic-hudge-xtracta/pipomatic_hudge_xtracta-3.4.tar.gz/pipomatic_hudge_xtracta-3.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pipomatic_hudge_xtracta",
+    version="3.4",
+    author="Hudge",
+    author_email="doug@mybusinessautomated.com",
+    description="Xtracta helper file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pipomatic_hudge_xtracta",
+    py_modules = ['xtracta'],
+    install_requires=[
+        'requests',
+        'xmltodict',
+        'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
