f33ad0a47f060494b1893519c1bdb4e04aa684ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:58 +0000

    Add package qibullet/0.0.6/qibullet-0.0.6.tar.gz

---
+++ packages/qibullet/qibullet-0.0.6.tar.gz/qibullet-0.0.6/qibullet/__init__.py
@@ -0,0 +1,13 @@
+from qibullet.link import Link
+from qibullet.joint import Joint
+from qibullet.pepper_virtual import PepperVirtual
+from qibullet.simulation_manager import SimulationManager
+from qibullet.camera import Camera, CameraRgb, CameraDepth, CameraResolution
+
+try:
+    from qibullet.ros_wrapper import PepperRosWrapper
+
+except ImportError:
+    pass
+
+name = 'qibullet'
