fa84ee12a0d9d0f683a43f20e63c61f6018a50c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:51 +0000

    Add package ezconfig/1.3.1/ezconfig-1.3.1.tar.gz

---
+++ packages/ezconfig/ezconfig-1.3.1.tar.gz/ezconfig-1.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="ezconfig",
+      version="1.3.1",
+      description="Utility class for reading configuration files.",
+      author="Nathan Addy",
+      author_email="nathan.addy@windfalltechnical.com",
+      url="http://bitbucket.org/nathanaddy/ezconfig",
+      license="MIT",
+      packages=["ezconfig"],
+      install_requires = ["dateutils"],
+      zip_safe=False
+)
