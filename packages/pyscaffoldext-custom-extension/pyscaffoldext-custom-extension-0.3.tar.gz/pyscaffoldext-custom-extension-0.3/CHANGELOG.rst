ff305655b5148d3d5ac7b8e71a965a76c11f6823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:49 +0000

    Add package pyscaffoldext-custom-extension/0.3/pyscaffoldext-custom-extension-0.3.tar.gz

---
+++ packages/pyscaffoldext-custom-extension/pyscaffoldext-custom-extension-0.3.tar.gz/pyscaffoldext-custom-extension-0.3/CHANGELOG.rst
@@ -0,0 +1,26 @@
+=========
+Changelog
+=========
+
+Version 0.3
+===========
+
+- Docstrings in Google Format
+- Added conftest.py as template
+- Reworked README.rst template
+- Added default unit test for extension
+- Few fixes
+
+Version 0.2
+===========
+
+- Check for pyscaffoldext naming convention
+- Added new README.rst
+- Check with flake8
+- Added testing requirements
+- Usage of tox
+
+Version 0.1
+===========
+
+- First release
---
+++ packages/pyscaffoldext-custom-extension/pyscaffoldext-custom-extension-0.3.tar.gz/pyscaffoldext-custom-extension-0.3/tests/test_custom_extension.py
@@ -0,0 +1,46 @@
+from os.path import exists as path_exists
+
+from pyscaffold.api import create_project
+from pyscaffold.cli import parse_args
+
+from pyscaffoldext.custom_extension.extension import CustomExtension
+
+
+def test_add_custom_extension(tmpfolder):
+    opts = {"package": "my_extension", "project": "pyscaffoldext-my_project",
+            "extensions": [CustomExtension("custom_ext")]}
+    create_project(opts)
+    assert path_exists("pyscaffoldext-my_project/src/pyscaffoldext/"
+                       "my_extension/extension.py")
+
+
+def test_add_custom_extension_with_namespace(tmpfolder):
+    args = ["--namespace", "test", "--custom-extension",
+            "pyscaffoldext-some_extension"]
+
+    opts = parse_args(args)
+    create_project(opts)
+    assert path_exists("pyscaffoldext-some_extension/src/pyscaffoldext/"
+                       "test/some_extension/extension.py")
+
+
+def test_add_custom_extension_with_namespace_2(tmpfolder):
+    args = ["--namespace", "test.second_level",
+            "--custom-extension", "pyscaffoldext-some_extension"]
+
+    opts = parse_args(args)
+    create_project(opts)
+    assert path_exists(
+            "pyscaffoldext-some_extension/src/pyscaffoldext/test/second_level/"
+            "some_extension/extension.py")
+
+
+def test_add_custom_extension_with_namespace_3(tmpfolder):
+    args = ["--namespace", "test.second_level.third_level",
+            "--custom-extension", "pyscaffoldext-some_extension"]
+
+    opts = parse_args(args)
+    create_project(opts)
+    assert path_exists(
+            "pyscaffoldext-some_extension/src/pyscaffoldext/test/second_level/"
+            "third_level/some_extension/extension.py")
