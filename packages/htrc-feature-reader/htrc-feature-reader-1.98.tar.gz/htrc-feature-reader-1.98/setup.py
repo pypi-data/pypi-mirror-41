466b0ae1c9a9d05a5d238772f06177d5c2f687d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:38 +0000

    Add package htrc-feature-reader/1.98/htrc-feature-reader-1.98.tar.gz

---
+++ packages/htrc-feature-reader/htrc-feature-reader-1.98.tar.gz/htrc-feature-reader-1.98/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='htrc-feature-reader',
+      version='1.98',
+      description='Library for working with the HTRC Extracted Features dataset',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Natural Language :: English',
+      ],
+      keywords='hathitrust text-mining text-analysis features',
+      url='https://github.com/organisciak/htrc-feature-reader',
+      author='Peter Organisciak',
+      author_email='organisciak@gmail.com',
+      license='NCSA',
+      packages=['htrc_features'],
+      install_requires=['six', 'pandas', 'numpy', 'requests', 'bz2file', 'ujson', 'pymarc'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      entry_points={
+          'console_scripts': [
+              'htid2rsync = htrc_features.utils:htid2rsync_cmd'
+              ]
+          },
+      zip_safe=False)
