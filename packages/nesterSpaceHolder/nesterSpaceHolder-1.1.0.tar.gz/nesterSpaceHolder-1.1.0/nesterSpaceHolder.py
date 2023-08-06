ecfadd95d1b8571886cd7108625c28f1e65c50ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:06 +0000

    Add package nesterSpaceHolder/1.1.0/nesterSpaceHolder-1.1.0.tar.gz

---
+++ packages/nesterSpaceHolder/nesterSpaceHolder-1.1.0.tar.gz/nesterSpaceHolder-1.1.0/nesterSpaceHolder.py
@@ -0,0 +1,17 @@
+"""This is the nesterSpaceHolder.py module, and it contains a function called print_lol()
+that prints each item contained in a nested list of any depth."""
+def print_lol(l,t):
+    """
+    This function takes a list as an argument and cycles through each item in the list,
+    calling itself on items in the list that are nested lists. All items contained in
+    the lists are printed.
+    A second argument called t is used to determine how many tab stops should be used
+    based on an item's depth in the list nesting.
+    """
+    for i in l:
+        if isinstance(i,list):
+            print_lol(i,t+1)
+        else:
+            for tab_stop in range(t):
+                print("\t", end='')
+            print(i)
