dede579607241d9d7a3dd762240939f3f1cd7507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:44 +0000

    Add package pyLAD/1.0.0/pyLAD-1.0.0.tar.gz

---
+++ packages/pyLAD/pyLAD-1.0.0.tar.gz/pyLAD-1.0.0/pyLAD/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyLAD SETUP.PY
+short_version = '1.0.0'
+version = '1.0.0'
+full_version = '1.0.0'
+git_revision = '64a44b9ae132223455cc12bea5e462c1a39e384f'
+release = True
+
+if not release:
+    version = full_version
