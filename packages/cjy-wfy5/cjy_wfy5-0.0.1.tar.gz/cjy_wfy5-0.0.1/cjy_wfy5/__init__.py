735c86eb0148a6d0cb7f84a6bce65d74c9c4fd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package cjy-wfy5/0.0.1/cjy_wfy5-0.0.1.tar.gz

---
+++ packages/cjy-wfy5/cjy_wfy5-0.0.1.tar.gz/cjy_wfy5-0.0.1/cjy_wfy5/__init__.py
@@ -0,0 +1,3 @@
+name = "cjy_wfy5"
+from cjy_wfy5 import main
+from cjy_wfy5 import *
---
+++ packages/cjy-wfy5/cjy_wfy5-0.0.1.tar.gz/cjy_wfy5-0.0.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cjy_wfy5",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="cjy",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    'pygame==1.9.4'
+    ]
+)
