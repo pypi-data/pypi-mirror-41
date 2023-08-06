4f12d11926f1a3e2ca1ea84f5198705588241e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:03 +0000

    Add package olapy/0.7.5/olapy-0.7.5.tar.gz

---
+++ packages/olapy/olapy-0.7.5.tar.gz/olapy-0.7.5/olapy/__main__.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import click
+from .cli import init
+from .core.services.xmla import runserver
+
+
+def main(arg):
+    """
+    Execute xmla provider.
+
+    :param arg: -c | --console :  show logs in server console
+    :return:
+    """
+    runserver()
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(runserver)
+cli.add_command(init)
+
+if __name__ == "__main__":
+    main(sys.argv)
