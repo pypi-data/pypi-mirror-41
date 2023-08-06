0e933fad533365c47cad7f7f0ead3fdbc56dafe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:21 +0000

    Add package sqliteschema/0.14.4/sqliteschema-0.14.4-py2.py3-none-any.whl

---
+++ packages/sqliteschema/sqliteschema-0.14.4-py2.py3-none-any.whl/sqliteschema/_const.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+
+MAX_VERBOSITY_LEVEL = 100
+
+# https://www.sqlite.org/fileformat2.html
+SQLITE_SYSTEM_TABLE_LIST = [
+    "sqlite_master",
+    "sqlite_sequence",
+    "sqlite_stat1",
+    "sqlite_stat2",
+    "sqlite_stat3",
+    "sqlite_stat4",
+]
+
+
+class SchemaHeader(object):
+    ATTR_NAME = "Attribute"
+    DATA_TYPE = "Type"
+    PRIMARY_KEY = "PRIMARY KEY"
+    NOT_NULL = "NOT NULL"
+    UNIQUE = "UNIQUE"
+    INDEX = "Index"
