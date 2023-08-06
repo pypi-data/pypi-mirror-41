16fbce4042e9909d7bb4ba4da474ea3b2fccdaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:22 +0000

    Add package py-dmenu/0.1/py-dmenu-0.1.tar.gz

---
+++ packages/py-dmenu/py-dmenu-0.1.tar.gz/py-dmenu-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='py-dmenu',
+    version='0.1',
+    description='A module to easly handle dmenu inputs with python',
+    url='http://github.com/manuelraa/py-dmenu',
+    author='Manuelraa',
+    author_email='manuel@bloodycrystals.de',
+)
