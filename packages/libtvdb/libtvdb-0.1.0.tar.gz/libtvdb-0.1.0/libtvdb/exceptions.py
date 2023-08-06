49d92ce5f1176ebbceec61dfe13881e795ec3b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:56 +0000

    Add package libtvdb/0.1.0/libtvdb-0.1.0.tar.gz

---
+++ packages/libtvdb/libtvdb-0.1.0.tar.gz/libtvdb-0.1.0/libtvdb/exceptions.py
@@ -0,0 +1,14 @@
+"""Custom exception types."""
+
+class TVDBException(Exception):
+    """Thrown when we can't get a more specific exception type."""
+
+
+class NotFoundException(TVDBException):
+    """Thrown when a show is not found after a search."""
+    pass
+
+
+class TVDBAuthenticationException(TVDBException):
+    """Thrown on authentication error."""
+    pass
