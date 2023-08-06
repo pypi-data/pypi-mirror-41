ec856841ee9d6258e7de64cbb9f4e4f53bc73122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package janis-pipelines/0.2.5/janis%20pipelines-0.2.5.tar.gz

---
+++ packages/janis-pipelines/janis%20pipelines-0.2.5.tar.gz/janis pipelines-0.2.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from janis import __version__
+
+DESCRIPTION = "Contains classes and helpers to build a workflow, and provide options to convert to CWL / WDL"
+
+######## SHOULDN'T NEED EDITS BELOW THIS LINE ########
+
+with open("./janis/README.md") as readme:
+    long_description = readme.read()
+
+setup(
+    name="janis pipelines",
+    version=__version__,
+    description=DESCRIPTION,
+    url="https://github.com/PMCC-BioinformaticsCore/janis",
+    author="Michael Franklin, Evan Thomas, Mohammad Bhuyan",
+    author_email="michael.franklin@petermac.org",
+    license="GNU",
+    packages=["janis"] + ["janis." + p for p in sorted(find_packages('./janis'))],
+    install_requires=["networkx>=2.1", "ruamel.yaml >= 0.12.4, <= 0.15.77", "six", "illusional.wdlgen",
+                      "illusional.cwlgen>=0.0.4"],
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    extras_require={
+        'bioinformatics': ['janis.bioinformatics']
+    }
+)
