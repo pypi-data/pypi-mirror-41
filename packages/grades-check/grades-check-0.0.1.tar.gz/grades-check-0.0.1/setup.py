0d6cd3ffef858571f362efe5e65ed4c1278c613e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:28 +0000

    Add package grades-check/0.0.1/grades-check-0.0.1.tar.gz

---
+++ packages/grades-check/grades-check-0.0.1.tar.gz/grades-check-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grades-check",
+    version="0.0.1",
+    author="Ailenei dan",
+    author_email="ailenei_d1996@yahoo.com",
+    description="A simple script that allows you to verify if a site changed or not",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'requests',
+      ],
+)
