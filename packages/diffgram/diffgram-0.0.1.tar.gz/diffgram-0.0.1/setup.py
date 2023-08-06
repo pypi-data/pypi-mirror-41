38b4de88d1c047af359bf95ece9c64c98b8fa3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package diffgram/0.0.1/diffgram-0.0.1.tar.gz

---
+++ packages/diffgram/diffgram-0.0.1.tar.gz/diffgram-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diffgram",
+    version="0.0.1",
+    author="Anthony Sarkis",
+    author_email="anthonysarkis@diffgram.com",
+    description="Deep learning, image annotation, training data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diffgram/diffgram",
+    packages=setuptools.find_packages(exclude=["task", "brain"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+)
