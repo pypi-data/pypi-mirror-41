196f4590f7b40f233dfea251ba65e14a07591a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:55 +0000

    Add package qot/0.0.0/qot-1.69.420.tar.gz

---
+++ packages/qot/qot-1.69.420.tar.gz/qot-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='qot',
+    description='qot',
+    long_description='qot',
+    author='qot',
+    license='qot',
+    packages=['qot'],
+    entry_points={'console_scripts': ['qot=qot:qot']},
+)
