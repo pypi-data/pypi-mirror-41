7ed6b71815208a69739e362557fcf5acbc086f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package koala2/0.0.29/koala2-0.0.29.tar.gz

---
+++ packages/koala2/koala2-0.0.29.tar.gz/koala2-0.0.29/setup.py
@@ -0,0 +1,30 @@
+"""Setup script."""
+
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup(
+        name="koala2",
+
+        version="0.0.29",
+
+        author="Ants, open innovation lab",
+        author_email="contact@weareants.fr",
+
+        packages=find_packages(),
+
+        include_package_data=True,
+
+        url="https://github.com/anthill/koala",
+
+        license="GNU GPL3",
+        description=(
+            "A python module to extract all the content of an Excel document "
+            "and enable calculation without Excel"
+        ),
+
+        long_description=open("README.md").read(),
+
+        install_requires=open('requirements.txt').readlines(),
+    )
