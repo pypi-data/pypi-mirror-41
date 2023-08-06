c391fed9b651af96e42ae2b8f7dc79aa797bf47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:56 +0000

    Add package cpctl/1.7.2/cpctl-1.7.2.tar.gz

---
+++ packages/cpctl/cpctl-1.7.2.tar.gz/cpctl-1.7.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+for name in ('README', 'README.md'):
+    if path.exists(path.join(this_directory, name)):
+        with open(path.join(this_directory, name), encoding='utf-8') as f:
+            long_description = f.read()
+
+setup(
+    name='cpctl',
+    packages=['cpctl'],
+    version='1.7.2',
+    description='Cooper Control Tool',
+    url='https://github.com/blavka/cpctl',
+    author='HARDWARIO s.r.o.',
+    author_email='ask@hardwario.com',
+    license='MIT',
+    keywords = ['cooper', 'cli', 'tool'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research'
+    ],
+    install_requires=[
+        'click==6.7', 'pyserial==3.4', 'pyzmq>=17.1.2'
+    ],
+    entry_points='''
+        [console_scripts]
+        cpctl=cpctl.cli:main
+    ''',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
