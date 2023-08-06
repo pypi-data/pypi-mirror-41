3e89b5e8059950e4b4b6c3e2756004e775322cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:02 +0000

    Add package financeager/0.17/financeager-0.17-py2.py3-none-any.whl

---
+++ packages/financeager/financeager-0.17-py2.py3-none-any.whl/financeager/localserver.py
@@ -0,0 +1,32 @@
+"""Local server proxy for direct communication (client and server reside in
+common process)."""
+
+from .server import Server
+from .exceptions import InvalidRequest
+
+
+class LocalServer(Server):
+    """Subclass mocking a locally running server. Convenient for testing"""
+
+    def run(self, command, **kwargs):
+        """Run command on local server, and shut it down immediately. Any
+        unexpected exceptions will be propagated upwards.
+
+        :raises: InvalidRequest on invalid request
+        """
+        response = super().run(command, **kwargs)
+
+        if command != "stop":
+            # don't shutdown twice
+            super().run("stop")
+
+        # stop-command is expected to return None
+        if response is not None and "error" in response:
+            raise InvalidRequest("Invalid request: {}".format(
+                response["error"]))
+
+        return response
+
+
+def proxy(**kwargs):
+    return LocalServer(**kwargs)
