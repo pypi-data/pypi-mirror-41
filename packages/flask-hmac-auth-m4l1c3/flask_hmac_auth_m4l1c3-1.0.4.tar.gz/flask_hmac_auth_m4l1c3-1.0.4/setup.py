32198c9325d7088f6453f5ecf772ba9b2c429bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package flask-hmac-auth-m4l1c3/1.0.4/flask_hmac_auth_m4l1c3-1.0.4.tar.gz

---
+++ packages/flask-hmac-auth-m4l1c3/flask_hmac_auth_m4l1c3-1.0.4.tar.gz/flask_hmac_auth_m4l1c3-1.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'flask_hmac_auth_m4l1c3',
+    version = '1.0.4',
+    description = 'A Flask middleware for HMAC request auth',
+    author = 'm4l1c3',
+    author_email = 'm4l1c3@tutanota.com',
+    url = 'https://github.com/m4l1c3/flask-hmac-auth',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
