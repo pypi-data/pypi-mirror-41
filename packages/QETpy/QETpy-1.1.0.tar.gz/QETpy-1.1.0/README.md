9a2a6a89c030f66ab6fa66e5b140dea09f3a5286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:47 +0000

    Add package QETpy/1.1.0/QETpy-1.1.0.tar.gz

---
+++ packages/QETpy/QETpy-1.1.0.tar.gz/QETpy-1.1.0/README.md
@@ -0,0 +1,25 @@
+# QETpy Package
+-------
+
+[![Build Status](https://travis-ci.com/ucbpylegroup/QETpy.svg?branch=master)](https://travis-ci.com/ucbpylegroup/QETpy) [![Documentation Status](https://readthedocs.org/projects/qetpy/badge/?version=latest)](https://qetpy.readthedocs.io/en/latest/?badge=latest) [![codecov](https://codecov.io/gh/ucbpylegroup/QETpy/branch/master/graph/badge.svg)](https://codecov.io/gh/ucbpylegroup/QETpy) 
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Python 3.6](https://img.shields.io/badge/python-3.6+-blue.svg)](https://www.python.org/downloads/release/python-360/)
+[![PyPI version](https://badge.fury.io/py/QETpy.svg)](https://badge.fury.io/py/QETpy)
+
+
+QETpy (Quasiparticle-trap-assisted Electrothermal-feedback Transition-edge sensors) provides tools for TES based detector calibration and analysis. It contains submodules for noise modeling, IV analysis, complex impedance fitting, non-linear optimum filter pulse fitting, and many other useful detector R&D analysis tools.
+
+The full documentation can be found at https://qetpy.readthedocs.io/en/latest/
+
+To install the current stable version of QETpy, from the command line type
+
+`pip install --upgrade qetpy`
+
+To install the most recent development version of QETpy, clone this repo, then from the top-level directory of the repo, type the following lines into your command line
+
+`python setup.py clean`  
+`python setup.py install --user`
+
+This package requires python 3.6 or greater. A current version of Anaconda3 should be sufficient, however a conda environment file as well as a list of dependencies is provided (condaenv.yml and requirements.txt)
+    
+Examples of how to use the package can be found in the `demos/` directory. This directory contains Jupyter notebooks with example code and testing data
---
+++ packages/QETpy/QETpy-1.1.0.tar.gz/QETpy-1.1.0/test/test_utils.py
@@ -0,0 +1,113 @@
+import pytest
+import numpy as np
+from qetpy import calc_psd
+from qetpy.cut import removeoutliers, iterstat
+from qetpy.utils import stdcomplex, lowpassfilter, align_traces, calc_offset, energy_absorbed, powertrace_simple
+
+def test_align_traces():
+    traces = np.random.randn(100, 32000)
+    res = align_traces(traces)
+    
+    assert len(res)>0
+
+def test_calc_offset():
+    traces = np.random.randn(100, 32000)
+    res = calc_offset(traces, is_didv=True)
+    
+    assert len(res)>0
+    
+def test_calc_psd():
+    traces = np.random.randn(100, 32000)
+    res = calc_psd(traces)
+
+    assert len(res)>0
+    
+def test_iterstat():
+    traces = np.random.randn(100, 32000)
+    offsets = traces.mean(axis=1)
+    res = iterstat(offsets)
+    
+    assert len(res)>0
+    
+def test_removeoutliers():
+    traces = np.random.randn(100, 32000)
+    offsets = traces.mean(axis=1)
+    res = removeoutliers(offsets)
+    
+    assert len(res)>0
+    
+def test_stdcomplex():
+    vals = np.array([3.0+3.0j, 0.0, 0.0])
+    res = stdcomplex(vals)
+    
+    assert res == np.sqrt(2)*(1.0+1.0j)
+
+def test_lowpassfilter():
+    traces = np.random.randn(100)
+    res = lowpassfilter(traces)
+    
+    assert res.shape == traces.shape
+    
+def test_powertrace_simple():
+    test_traces = 4*np.ones(shape = (10,10))
+    power_test = powertrace_simple(trace = test_traces, 
+                ioffset = 1, qetbias = 1, rload = 1, rsh = 1)
+    
+    assert np.all(power_test == -6)
+    
+def test_energy_absorbed():
+    test_traces = np.zeros(shape=100)
+    test_traces[50:75] = 2
+    energy = energy_absorbed(trace=test_traces, 
+                                time=np.arange(100)*1e-20,
+                                indbasepre=0,
+                                indbasepost=75, 
+                                ioffset=0, 
+                                qetbias=1, 
+                                rload=1, 
+                                rsh=1)
+    assert int(energy) == 3
+    
+    energy = energy_absorbed(trace=test_traces, 
+                                fs = 1e20,
+                                time=None,
+                                indbasepre=0,
+                                indbasepost=75, 
+                                ioffset=0, 
+                                qetbias=1, 
+                                rload=1, 
+                                rsh=1)
+    assert int(energy) == 3
+    
+    energy = energy_absorbed(trace=test_traces, 
+                                fs = 1e20,
+                                time=None,
+                                indbasepre=10,
+                                indbasepost=None, 
+                                ioffset=0, 
+                                qetbias=1, 
+                                rload=1, 
+                                rsh=1)
+    assert int(energy) == 3
+    
+    with pytest.raises(ValueError):
+        energy = energy_absorbed(trace=test_traces, 
+                                 fs=1e20,
+                                 time=None,
+                                 indbasepre=None,
+                                 indbasepost=None, 
+                                 ioffset=0, 
+                                 qetbias=1, 
+                                 rload=1, 
+                                 rsh=1)
+    
+    with pytest.raises(ValueError):
+        energy = energy_absorbed(trace=test_traces, 
+                                 fs=None,
+                                 time=None,
+                                 indbasepre=10,
+                                 indbasepost=None, 
+                                 ioffset=0, 
+                                 qetbias=1, 
+                                 rload=1, 
+                                 rsh=1)
