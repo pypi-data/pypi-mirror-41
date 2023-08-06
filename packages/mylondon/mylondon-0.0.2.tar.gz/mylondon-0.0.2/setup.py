bd1e8852135417735fadb534a0ce9eface22bbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:29 +0000

    Add package mylondon/0.0.2/mylondon-0.0.2.tar.gz

---
+++ packages/mylondon/mylondon-0.0.2.tar.gz/mylondon-0.0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="mylondon",
+    version='0.0.2',
+    description='A Small example package',
+    author='rakens2',
+    packages=setuptools.find_packages()
+)
