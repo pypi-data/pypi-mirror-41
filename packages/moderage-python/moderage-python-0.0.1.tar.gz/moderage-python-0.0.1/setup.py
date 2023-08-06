8081f64746e6baa03046ee40d8cb661bca8a19cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package moderage-python/0.0.1/moderage-python-0.0.1.tar.gz

---
+++ packages/moderage-python/moderage-python-0.0.1.tar.gz/moderage-python-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="moderage-python",
+    version="0.0.1",
+    author="Chris Bamford",
+    author_email="chrisbam4d@gmail.com",
+    description="ModeRage python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/chrisbam4d/moderage-python-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
