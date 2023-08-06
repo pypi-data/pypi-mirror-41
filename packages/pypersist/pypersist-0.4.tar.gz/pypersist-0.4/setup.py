576e0f6d693c7293ef68e900e73a60d1be28eb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:05 +0000

    Add package pypersist/0.4/pypersist-0.4.tar.gz

---
+++ packages/pypersist/pypersist-0.4.tar.gz/pypersist-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    return open(path).read().strip()
+
+setup(name='pypersist',
+      version=read("VERSION"),
+      description='Persistent memoisation framework for Python',
+      url='https://github.com/mtorpey/pypersist',
+      author='Michael Torpey',
+      author_email='mct25@st-andrews.ac.uk',
+      license='GPL',
+      packages=['pypersist'],
+      include_package_data=True,
+      long_description=read('README.md'),
+      zip_safe=False)
