0c76f169a7af6668cb01e8af7e6edf7bc3de0b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:06 +0000

    Add package amieci/0.0.5/amieci-0.0.5.tar.gz

---
+++ packages/amieci/amieci-0.0.5.tar.gz/amieci-0.0.5/README.md
@@ -0,0 +1,5 @@
+# amieci
+The amie python API
+
+Install with 
+`pip install git+ssh://git@github.com/disraptoer/amieci.git`
---
+++ packages/amieci/amieci-0.0.5.tar.gz/amieci-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='amieci',
+      version='0.0.5',
+      description='The python API for amie',
+      url='https://github.com/disraptoer/amieci',
+      author='johannes beil',
+      author_email='jb@amie.dk',
+      license='private',
+      packages=['amieci'],
+      zip_safe=False)
