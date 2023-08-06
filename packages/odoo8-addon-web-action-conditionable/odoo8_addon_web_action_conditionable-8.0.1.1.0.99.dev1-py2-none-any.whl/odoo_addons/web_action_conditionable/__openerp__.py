71593ed6d465715fc07d821a547639a87cba85d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:55 +0000

    Add package odoo8-addon-web-action-conditionable/8.0.1.1.0.99.dev1/odoo8_addon_web_action_conditionable-8.0.1.1.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo8-addon-web-action-conditionable/odoo8_addon_web_action_conditionable-8.0.1.1.0.99.dev1-py2-none-any.whl/odoo_addons/web_action_conditionable/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_action_conditionable',
+    "version": "8.0.1.1.0",
+    "category": "Web",
+    "website": "https://github.com/OCA/web",
+    "development_status": "Beta",
+    "depends": [
+        'base',
+        'web',
+    ],
+    "license": "AGPL-3",
+    'data': [
+        'views/view.xml'
+    ],
+    "author": "Cristian Salamea,Onestein,"
+              "Odoo Community Association (OCA)",
+    "installable": True,
+}
