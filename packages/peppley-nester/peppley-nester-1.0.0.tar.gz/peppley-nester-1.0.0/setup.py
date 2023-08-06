20f1df3e7944f619d37bead9cfe161ba33279548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:20 +0000

    Add package peppley-nester/1.0.0/peppley-nester-1.0.0.tar.gz

---
+++ packages/peppley-nester/peppley-nester-1.0.0.tar.gz/peppley-nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'peppley-nester',
+    version = '1.0.0',
+    py_modules = ['peppley-nester'],
+    author = 'dpeppley',
+    author_email = 'dpeppley@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'Simple nested list printer',
+)
