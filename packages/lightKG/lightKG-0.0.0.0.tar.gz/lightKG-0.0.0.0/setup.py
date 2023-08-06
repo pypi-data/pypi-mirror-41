39a655c089c02e8570799db34717004349ceebf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:03 +0000

    Add package lightKG/0.0.0.0/lightKG-0.0.0.0.tar.gz

---
+++ packages/lightKG/lightKG-0.0.0.0.tar.gz/lightKG-0.0.0.0/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Author: Liu Yang <mkliuyang@gmail.com>
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='lightKG',
+    version='0.0.0.0',
+    description= "lightsmile's kg library",
+    author='lightsmile',
+    author_email='iamlightsmile@gmail.com',
+    url='https://github.com/smilelight/lightKG',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'torchtext>=0.4.0',
+        'tqdm>=4.28.1',
+        'torch>=1.0.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
