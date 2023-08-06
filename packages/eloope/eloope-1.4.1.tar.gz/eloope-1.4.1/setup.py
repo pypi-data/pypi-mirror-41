52b87cf8a226dc2170ec9f10af47da08065d04fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:31 +0000

    Add package eloope/1.4.1/eloope-1.4.1.tar.gz

---
+++ packages/eloope/eloope-1.4.1.tar.gz/eloope-1.4.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='eloope',
+    version='1.4.1',
+    author='Czw_96',
+    author_email='459749926@qq.com',
+    description='Distributed event loop engine.',
+    url='https://github.com/Czw96/eloope',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': ['eloope = eloope.command:main']
+    },
+    install_requires=[
+        'gevent',
+        'redis',
+        'paramiko',
+        'requests',
+    ]
+)
