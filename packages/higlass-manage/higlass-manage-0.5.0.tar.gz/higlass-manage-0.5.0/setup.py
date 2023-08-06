c816c352153ac8eac0f8c8e7f54e4daf4cde8e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:41 +0000

    Add package higlass-manage/0.5.0/higlass-manage-0.5.0.tar.gz

---
+++ packages/higlass-manage/higlass-manage-0.5.0.tar.gz/higlass-manage-0.5.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='higlass-manage',
+    author='Peter Kerpedjiev',
+    author_email='pkerpedjiev@gmail.com',
+    url='https://github.com/pkerpedjiev/higlass-manage',
+    description='Wrappers for running the HiGlass Docker container',
+    version='0.5.0',
+    py_modules=['higlass_manage'],
+    install_requires=[
+        'Click',
+        'clodius==0.9.0',
+        'cooler>=0.7.10',
+        'pandas>=0.19',
+        'docker',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        higlass-manage=higlass_manage:cli
+    ''',
+)
