0a8478df25ad9168e3b37cbacc4eb97cc4559d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:39 +0000

    Add package kommando/1.1.3rc0/kommando-1.1.3rc0-py3-none-any.whl

---
+++ packages/kommando/kommando-1.1.3rc0-py3-none-any.whl/kommando/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'Kommando.py'
+__author__ = 'Andre Augusto'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Andre Augusto'
+__version__ = '1.1.3rc'
+
+from .base import KBot, Kontext
