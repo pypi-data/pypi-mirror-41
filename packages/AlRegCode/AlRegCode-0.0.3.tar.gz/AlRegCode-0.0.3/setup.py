dada30841891469a4843e66725d59ddb9a91f1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:42 +0000

    Add package AlRegCode/0.0.3/AlRegCode-0.0.3.tar.gz

---
+++ packages/AlRegCode/AlRegCode-0.0.3.tar.gz/AlRegCode-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AlRegCode",
+    version="0.0.3",
+    author="M Nasrul Alawy",
+    author_email="alphacsoft@gmail.com",
+    description="The Small Package For Generate Increment Registration Code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://alphacsoft.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
