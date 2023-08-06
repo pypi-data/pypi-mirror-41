5a7a072102ee5b25bac76ce4fe6f19ae4feebd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:10 +0000

    Add package nester-onbon/1.0.0/nester-onbon-1.0.0.tar.gz

---
+++ packages/nester-onbon/nester-onbon-1.0.0.tar.gz/nester-onbon-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = 'nester-onbon',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'onbon',
+    author_email = 'wangyq@onbonbx.com',
+    url = 'http://www.onbonbx.com',
+    description = 'This is a simple demo'
+)
