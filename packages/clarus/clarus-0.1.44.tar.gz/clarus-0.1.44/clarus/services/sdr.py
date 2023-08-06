06a927e94cf6803be5fed8fed7af69c14dfbe4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:56 +0000

    Add package clarus/0.1.44/clarus-0.1.44.tar.gz

---
+++ packages/clarus/clarus-0.1.44.tar.gz/clarus-0.1.44/clarus/services/sdr.py
@@ -0,0 +1,20 @@
+import clarus.services
+
+def lastprice(output=None, **params):
+    return clarus.services.api_request('SDR', 'LastPrice', output=output, **params)
+
+def lasttrade(output=None, **params):
+    return clarus.services.api_request('SDR', 'LastTrade', output=output, **params)
+
+def tradefilter(output=None, **params):
+    return clarus.services.api_request('SDR', 'TradeFilter', output=output, **params)
+
+def trades(output=None, **params):
+    return clarus.services.api_request('SDR', 'Trades', output=output, **params)
+
+def volume(output=None, **params):
+    return clarus.services.api_request('SDR', 'Volume', output=output, **params)
+
+def volumeadv(output=None, **params):
+    return clarus.services.api_request('SDR', 'VolumeADV', output=output, **params)
+
