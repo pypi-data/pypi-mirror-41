8f3d03505e87d4e27fed01134b36551cd0782891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:19 +0000

    Add package madness/0.1.0/madness-0.1.0.tar.gz

---
+++ packages/madness/madness-0.1.0.tar.gz/madness-0.1.0/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name = "madness",
+    version = "0.1.0",
+    author = "Forrest Button",
+    author_email = "forrest.button@gmail.com",
+    description = "",
+    long_description = Path("README.md").read_text(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Waffles32/madness",
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	include_package_data = True,
+	install_requires = Path('requirements.txt').read_text().splitlines(),
+	python_requires='>3.6',
+	zip_safe = False,
+)
