2674de712af3601a210fbd104e3842ca674f7aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package PiWarsTurkiyeRobotKiti2019/0.9.4/PiWarsTurkiyeRobotKiti2019-0.9.4.tar.gz

---
+++ packages/PiWarsTurkiyeRobotKiti2019/PiWarsTurkiyeRobotKiti2019-0.9.4.tar.gz/PiWarsTurkiyeRobotKiti2019-0.9.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+
+setup(
+      name = "PiWarsTurkiyeRobotKiti2019",
+      version = "0.9.4",
+      author = "Yaşar İdikut, Sarp Yoel Kastro",
+      author_email = "yasar.idikut@hisarschool.k12.tr, sarp.kastro@hisarschool.k12.tr",
+      description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
+      packages = ["PiWarsTurkiyeRobotKiti2019"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   ]
+
+)
