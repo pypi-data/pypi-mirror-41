1e94af82994d3f1c0274612d13da6de818c6769b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package keras-self-attention/0.33.0/keras-self-attention-0.33.0.tar.gz

---
+++ packages/keras-self-attention/keras-self-attention-0.33.0.tar.gz/keras-self-attention-0.33.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='keras-self-attention',
+    version='0.33.0',
+    packages=['keras_self_attention'],
+    url='https://github.com/CyberZHG/keras-self-attention',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Attention mechanism for processing sequential data that considers the context for each timestamp',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
