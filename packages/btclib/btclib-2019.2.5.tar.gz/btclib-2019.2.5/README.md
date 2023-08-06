9c02b5b5ef0ae52e14730a9e215a3a3b7b640e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:12 +0000

    Add package btclib/2019.2.5/btclib-2019.2.5.tar.gz

---
+++ packages/btclib/btclib-2019.2.5.tar.gz/btclib-2019.2.5/README.md
@@ -0,0 +1,47 @@
+# btclib: a python library for 'bitcoin cryptography'
+
+<http://github.com/dginst/btclib>
+
+[![Build Status](https://travis-ci.org/dginst/btclib.svg)](https://travis-ci.org/dginst/btclib)
+[![Coverage Status](https://coveralls.io/repos/github/dginst/btclib/badge.svg)](https://coveralls.io/github/dginst/btclib)
+[![PyPI status](https://img.shields.io/pypi/status/btclib.svg)](https://pypi.python.org/pypi/btclib/)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/btclib.svg)](https://pypi.python.org/pypi/btclib/)
+[![PyPI version](https://img.shields.io/pypi/v/btclib.svg)](https://pypi.python.org/pypi/btclib/)
+[![GitHub License](https://img.shields.io/github/license/dginst/btclib.svg)](https://github.com/dginst/btclib/blob/master/LICENSE)
+
+btclib is a python3 type annotated library intended for teaching and demonstration of the elliptic curve cryptography used in bitcoin.
+
+It does not have external requirements or dependencies; to install (and upgrade) it:
+
+```shell
+python3 -m pip install --upgrade btclib
+```
+
+Originally developed for the [_Bitcoin and Blockchain Technology Course_](https://www.ametrano.net/bbt/) at Milano Bicocca and Politecnico di Milano, its algorithms are not intended for production environments: they could be broken using side-channel attacks; moreover, they might be subjected to major refactoring without care for backward compatibility.
+
+The library includes:
+
+- modulo algebra functions (gcd, inverse, legendre symbol, square root)
+- octet / integer / point conversion functions
+- elliptic curve class
+  - fast algebra implemented using Jacobian coordinates
+  - double scalar multiplication (Straus's algorithm, also known as Shamir's trick)
+  - multi scalar multiplication (Bos-coster's algorithm)
+  - point simmetry solution: odd/even, high/low, and quadratic residue
+  - available curves: SEC 1 v1 and v2, NIST, Brainpool, and low cardinality test curves
+- DSA signature and DER encoding
+- Schnorr signature (according to bip-schnorr bitcoin standardization)
+  - batch validation
+  - threshold signature
+  - MuSig multi-signature
+- Borromean ring signature
+- RFC-6979 to make signature schemes deterministic
+- sign-to-contract notarization
+- Diffie-Hellman
+- Pedersen Committment
+- base58 encoding, addresses, WIFs
+- BIP32 hierarchical deterministic wallets
+- BIP39 mnemonic code for generating deterministic keys
+- [Electrum](https://electrum.org/#home) standard for mnemonic code
+
+A very extensive test suite reproduces results from major official sources and covers 100% of the library code base.
---
+++ packages/btclib/btclib-2019.2.5.tar.gz/btclib-2019.2.5/btclib/bip39.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2019 The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""BIP39 entropy / mnemonic / seed functions"""
+
+from hashlib import sha256, pbkdf2_hmac
+
+from btclib.entropy import Entropy, GenericEntropy, bytes_from_entropy, \
+    str_from_entropy
+from btclib.mnemonic import mnemonic_dict
+from btclib import bip32
+
+
+def _raw_entropy_checksum(raw_entr: GenericEntropy) -> Entropy:
+    raw_entr = bytes_from_entropy(raw_entr, _allowed_raw_entr_bits)
+    # raw_entr 256-bit checksum
+    byteschecksum = sha256(raw_entr).digest()  # 256 bits
+    # convert checksum to binary '01' string
+    intchecksum = int.from_bytes(
+        byteschecksum, 'big')                  # leading zeros are lost
+    checksum = bin(intchecksum)[2:]            # remove '0b'
+    checksum = checksum.zfill(256)             # pad with lost zeros
+    # rightmost bits
+    checksum_bits = len(raw_entr) // 4
+    return checksum[:checksum_bits]
+
+
+#  bits per word = bpw = 11
+#  CheckSum = raw ENTropy / 32
+#  MnemonicSentence (in words) = (ENT + CS) / bpw
+#
+# | ENT | CS | ENT+CS | MS |
+# +-----+----+--------+----+
+# | 128 |  4 |    132 | 12 |
+# | 160 |  5 |    165 | 15 |
+# | 192 |  6 |    198 | 18 |
+# | 224 |  7 |    231 | 21 |
+# | 256 |  8 |    264 | 24 |
+_allowed_raw_entr_bits = (128, 160, 192, 224, 256)
+
+# https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+#
+# input raw entropy can be expresses as binary string, bytes-like, or int
+# it must be 128, 160, 192, 224, or 256 bits
+# int is front-padded with zeros up to 128, 160, 192, 224, or 256 bits
+#
+# output entropy is returned as binary string
+
+
+def entropy_from_raw_entropy(raw_entropy: GenericEntropy) -> Entropy:
+    raw_entropy = str_from_entropy(raw_entropy, _allowed_raw_entr_bits)
+    checksum = _raw_entropy_checksum(raw_entropy)
+    return raw_entropy + checksum
+
+
+def mnemonic_from_raw_entropy(raw_entr: GenericEntropy,
+                                    lang: str) -> str:
+    entropy = entropy_from_raw_entropy(raw_entr)
+    indexes = mnemonic_dict.indexes_from_entropy(entropy, lang)
+    mnemonic = mnemonic_dict.mnemonic_from_indexes(indexes, lang)
+    return mnemonic
+
+
+def raw_entropy_from_mnemonic(mnemonic: str, lang: str) -> Entropy:
+    """output raw entropy is returned as binary string"""
+    indexes = mnemonic_dict.indexes_from_mnemonic(mnemonic, lang)
+    entropy = mnemonic_dict.entropy_from_indexes(indexes, lang)
+
+    # raw entropy is only the first part of entropy
+    raw_entr_bits = int(len(entropy)*32/33)
+    if raw_entr_bits not in _allowed_raw_entr_bits:
+        m = f"mnemonic with wrong number of bits ({raw_entr_bits}); "
+        m += f"expected: {_allowed_raw_entr_bits}"
+        raise ValueError(m)
+    raw_entr = entropy[:raw_entr_bits]
+
+    # the second one being the checksum, to be verified
+    bytes_raw_entr = int(raw_entr, 2).to_bytes(raw_entr_bits//8, 'big')
+    checksum = _raw_entropy_checksum(bytes_raw_entr)
+    if entropy[raw_entr_bits:] != checksum:
+        raise ValueError("invalid mnemonic checksum")
+
+    return raw_entr
+
+# TODO: re-evaluate style
+
+
+def seed_from_mnemonic(mnemonic: str, passphrase: str) -> bytes:
+    hash_name = 'sha512'
+    password = mnemonic.encode()
+    salt = ('mnemonic' + passphrase).encode()
+    iterations = 2048
+    dksize = 64
+    return pbkdf2_hmac(hash_name, password, salt, iterations, dksize)
+
+# TODO: re-evaluate style
+
+
+def mprv_from_mnemonic(mnemonic: str,
+                       passphrase: str,
+                       xversion: bytes) -> bytes:
+    seed = seed_from_mnemonic(mnemonic, passphrase)
+    return bip32.mprv_from_seed(seed, xversion)
+
+# TODO: move to wallet file
+
+
+def mprv_from_raw_entropy(raw_entr: GenericEntropy,
+                          passphrase: str,
+                          lang: str,
+                          xversion: bytes) -> bytes:
+    mnemonic = mnemonic_from_raw_entropy(raw_entr, lang)
+    mprv = mprv_from_mnemonic(mnemonic, passphrase, xversion)
+    return mprv
