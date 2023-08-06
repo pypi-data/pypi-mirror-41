d417476ae446174b5eae5e19713c5ce73597403f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:27 +0000

    Add package pulpcore/3.0.0b20/pulpcore-3.0.0b20.tar.gz

---
+++ packages/pulpcore/pulpcore-3.0.0b20.tar.gz/pulpcore-3.0.0b20/pulpcore/app/serializers/__init__.py
@@ -0,0 +1,40 @@
+# Load order: base, fields, all others.
+# - fields can import directly from base if needed
+# - all can import directly from base and fields if needed
+from .base import (  # noqa
+    DetailRelatedField,
+    ModelSerializer,
+    MasterModelSerializer,
+    DetailIdentityField,
+    IdentityField,
+    NestedIdentityField,
+    NestedRelatedField,
+    RelatedField,
+    validate_unknown_fields,
+    AsyncOperationResponseSerializer
+)
+from .fields import BaseURLField, ContentRelatedField, LatestVersionField  # noqa
+from .content import (  # noqa
+    ArtifactSerializer,
+    NoArtifactContentSerializer,
+    SingleArtifactContentSerializer,
+    MultipleArtifactContentSerializer,
+)
+from .progress import ProgressReportSerializer  # noqa
+from .publication import (  # noqa
+    ContentGuardSerializer,
+    DistributionSerializer,
+    PublicationSerializer,
+)
+from .repository import (  # noqa
+    ExporterSerializer,
+    RemoteSerializer,
+    PublisherSerializer,
+    RepositoryPublishURLSerializer,
+    RepositorySerializer,
+    RepositorySyncURLSerializer,
+    RepositoryVersionSerializer,
+    RepositoryVersionCreateSerializer
+)
+from .task import MinimalTaskSerializer, TaskSerializer, WorkerSerializer  # noqa
+from .user import UserSerializer  # noqa
