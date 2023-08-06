3022cf943291af86b1141e5d9deec30ed994a06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:08 +0000

    Add package anji-orm/0.10.0/anji_orm-0.10.0-py2.py3-none-any.whl

---
+++ packages/anji-orm/anji_orm-0.10.0-py2.py3-none-any.whl/anji_orm/rethinkdb/parser/transformation_parser.py
@@ -0,0 +1,22 @@
+# pylint: disable=no-self-use
+import rethinkdb as R
+
+from ...core import QueryRow, BaseTransformationQueryParser
+from .utils import _build_row
+
+__author__ = "Bogdan Gladyshev"
+__copyright__ = "Copyright 2017, Bogdan Gladyshev"
+__credits__ = ["Bogdan Gladyshev"]
+__license__ = "MIT"
+__version__ = "0.10.0"
+__maintainer__ = "Bogdan Gladyshev"
+__email__ = "siredvin.dark@gmail.com"
+__status__ = "Production"
+
+__all__ = ['RethinkDBTransformationQueryParser']
+
+
+class RethinkDBTransformationQueryParser(BaseTransformationQueryParser[R.RqlQuery]):
+
+    def process_group_statement(self, db_query: R.RqlQuery, group_row: QueryRow) -> R.RqlQuery:
+        return db_query.group(_build_row(group_row))
