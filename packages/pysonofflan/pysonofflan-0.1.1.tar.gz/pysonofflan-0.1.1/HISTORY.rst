f123dd85852843bce6ea3c95d2237f2292913e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package pysonofflan/0.1.1/pysonofflan-0.1.1.tar.gz

---
+++ packages/pysonofflan/pysonofflan-0.1.1.tar.gz/pysonofflan-0.1.1/HISTORY.rst
@@ -0,0 +1,11 @@
+=======
+History
+=======
+
+0.1.0 (2019-01-27)
+------------------
+
+* First release on PyPI.
+* Basic functional CLI client, to allow basic control (on, off, check state)
+* Added comprehensive logging with verbosity option to help debug new devices
+* Control of device is via async websocket, so should be usable in async code
---
+++ packages/pysonofflan/pysonofflan-0.1.1.tar.gz/pysonofflan-0.1.1/pysonofflan/discover.py
@@ -0,0 +1,44 @@
+import ipaddress
+import logging
+import socket
+from itertools import chain
+from typing import Dict
+
+
+class Discover:
+    @staticmethod
+    async def discover(logger=None) -> Dict[str, str]:
+        """
+        Attempts websocket connection on port 8081 to all IP addresses on
+        common home IP subnets: 192.168.0.X and 192.168.1.X, in the hope of
+        detecting  available supported devices in the local network.
+
+        :rtype: dict
+        :return: Array of devices {"ip": "device_id"}
+        """
+        if logger is None:
+            logger = logging.getLogger(__name__)
+
+        logger.debug("Attempting connection to all IPs on local network. "
+                     "This will take approximately 1 minute, please wait...")
+        devices = {}
+
+        try:
+            local_ip_ranges = chain(
+                ipaddress.IPv4Network('127.0.0.1/32'),
+                ipaddress.IPv4Network('192.168.0.0/24'),
+                ipaddress.IPv4Network('192.168.1.0/24')
+            )
+
+            for ip in local_ip_ranges:
+                logger.debug("Attempting connection to IP: %s" % ip)
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(0.1)
+                result = sock.connect_ex((str(ip), 8081))
+                if result == 0:
+                    logger.debug("Found open 8081 port at local IP: %s" % ip)
+                    devices[ip] = ip
+        except Exception as ex:
+            logger.error("Caught Exception: %s" % ex, exc_info=False)
+
+        return devices
