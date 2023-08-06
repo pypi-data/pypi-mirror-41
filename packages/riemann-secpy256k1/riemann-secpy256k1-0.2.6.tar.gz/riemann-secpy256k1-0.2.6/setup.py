233f8f6e31dd84fb2f31c37a0d86370f43769503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:09 +0000

    Add package riemann-secpy256k1/0.2.6/riemann-secpy256k1-0.2.6.tar.gz

---
+++ packages/riemann-secpy256k1/riemann-secpy256k1-0.2.6.tar.gz/riemann-secpy256k1-0.2.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name='riemann-secpy256k1',
+    version='0.2.6',
+    author=[
+        "RJ Rybarczyk",
+        "James Prestwich",
+        "Jarrett Wheatley"
+    ],
+    author_email="team@summa.one",
+    description="Python ffi bindings to secp256k1 bitcoin-core library.",
+    url="https://github.com/summa-tx/secpy256k1",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    package_dir={'secpy256k1': 'secpy256k1'},
+    package_data={'secpy256k1': ['py.typed']},
+    install_requires=['cffi>=1.11.5'],
+    cffi_modules=['secpy256k1/build_secp256k1/build.py:ffibuilder'],
+    setup_requires=['cffi>=1.11.5'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
