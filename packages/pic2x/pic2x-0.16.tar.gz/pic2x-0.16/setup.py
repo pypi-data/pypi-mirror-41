d0497c8336f4b6078607c2a6a362f4e872fe5be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:56 +0000

    Add package pic2x/0.16/pic2x-0.16.tar.gz

---
+++ packages/pic2x/pic2x-0.16.tar.gz/pic2x-0.16/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pic2x',
+    version='0.16',
+    description=(
+        'Transform your picture to anything.'
+    ),
+    long_description=open('README.rst').read(),
+    author='ZillyRex',
+    author_email='zillyrain@gmail.com',
+    maintainer='ZillyRex',
+    maintainer_email='zillyrain@gmail.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/ZillyRex/pic2x',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
