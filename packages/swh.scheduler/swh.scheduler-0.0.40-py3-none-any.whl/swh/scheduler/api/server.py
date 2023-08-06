8065d0840064ac99cf9a66269c8f9e3afda4c3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package swh.scheduler/0.0.40/swh.scheduler-0.0.40-py3-none-any.whl

---
+++ packages/swh.scheduler/swh.scheduler-0.0.40-py3-none-any.whl/swh/scheduler/api/server.py
@@ -0,0 +1,148 @@
+# Copyright (C) 2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import logging
+
+from flask import request
+
+from swh.core import config
+from swh.scheduler import get_scheduler as get_scheduler_from
+from swh.core.api import (SWHServerAPIApp, decode_request,
+                          error_handler,
+                          encode_data_server as encode_data)
+
+DEFAULT_CONFIG_PATH = 'backend/scheduler'
+DEFAULT_CONFIG = {
+    'scheduler': ('dict', {
+        'cls': 'local',
+        'args': {
+            'scheduling_db': 'dbname=softwareheritage-scheduler-dev',
+        },
+    })
+}
+
+
+app = SWHServerAPIApp(__name__)
+scheduler = None
+
+
+@app.errorhandler(Exception)
+def my_error_handler(exception):
+    return error_handler(exception, encode_data)
+
+
+def get_sched():
+    global scheduler
+    if not scheduler:
+        scheduler = get_scheduler_from(**app.config['scheduler'])
+    return scheduler
+
+
+@app.route('/')
+def index():
+    return 'SWH Scheduler API server'
+
+
+@app.route('/close_connection', methods=['POST'])
+def close_connection():
+    return encode_data(get_sched().close_connection())
+
+
+@app.route('/set_status_tasks', methods=['POST'])
+def set_status_tasks():
+    return encode_data(get_sched().set_status_tasks(**decode_request(request)))
+
+
+@app.route('/create_task_type', methods=['POST'])
+def create_task_type():
+    return encode_data(get_sched().create_task_type(**decode_request(request)))
+
+
+@app.route('/get_task_type', methods=['POST'])
+def get_task_type():
+    return encode_data(get_sched().get_task_type(**decode_request(request)))
+
+
+@app.route('/get_task_types', methods=['POST'])
+def get_task_types():
+    return encode_data(get_sched().get_task_types(**decode_request(request)))
+
+
+@app.route('/create_tasks', methods=['POST'])
+def create_tasks():
+    return encode_data(get_sched().create_tasks(**decode_request(request)))
+
+
+@app.route('/disable_tasks', methods=['POST'])
+def disable_tasks():
+    return encode_data(get_sched().disable_tasks(**decode_request(request)))
+
+
+@app.route('/get_tasks', methods=['POST'])
+def get_tasks():
+    return encode_data(get_sched().get_tasks(**decode_request(request)))
+
+
+@app.route('/search_tasks', methods=['POST'])
+def search_tasks():
+    return encode_data(get_sched().search_tasks(**decode_request(request)))
+
+
+@app.route('/peek_ready_tasks', methods=['POST'])
+def peek_ready_tasks():
+    return encode_data(get_sched().peek_ready_tasks(**decode_request(request)))
+
+
+@app.route('/grab_ready_tasks', methods=['POST'])
+def grab_ready_tasks():
+    return encode_data(get_sched().grab_ready_tasks(**decode_request(request)))
+
+
+@app.route('/schedule_task_run', methods=['POST'])
+def schedule_task_run():
+    return encode_data(get_sched().schedule_task_run(
+        **decode_request(request)))
+
+
+@app.route('/mass_schedule_task_runs', methods=['POST'])
+def mass_schedule_task_runs():
+    return encode_data(
+        get_sched().mass_schedule_task_runs(**decode_request(request)))
+
+
+@app.route('/start_task_run', methods=['POST'])
+def start_task_run():
+    return encode_data(get_sched().start_task_run(**decode_request(request)))
+
+
+@app.route('/end_task_run', methods=['POST'])
+def end_task_run():
+    return encode_data(get_sched().end_task_run(**decode_request(request)))
+
+
+@app.route('/filter_task_to_archive', methods=['POST'])
+def filter_task_to_archive():
+    return encode_data(
+        get_sched().filter_task_to_archive(**decode_request(request)))
+
+
+@app.route('/delete_archived_tasks', methods=['POST'])
+def delete_archived_tasks():
+    return encode_data(
+        get_sched().delete_archived_tasks(**decode_request(request)))
+
+
+def run_from_webserver(environ, start_response,
+                       config_path=DEFAULT_CONFIG_PATH):
+    """Run the WSGI app from the webserver, loading the configuration."""
+    cfg = config.load_named_config(config_path, DEFAULT_CONFIG)
+    app.config.update(cfg)
+    handler = logging.StreamHandler()
+    app.logger.addHandler(handler)
+    return app(environ, start_response)
+
+
+if __name__ == '__main__':
+    print('Please use the "swh-scheduler api-server" command')
---
+++ packages/swh.scheduler/swh.scheduler-0.0.40-py3-none-any.whl/swh/scheduler/task.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2015-2017 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import celery.app.task
+from celery.utils.log import get_task_logger
+
+
+class SWHTask(celery.app.task.Task):
+    """a schedulable task (abstract class)
+
+    Current implementation is based on Celery. See
+    http://docs.celeryproject.org/en/latest/reference/celery.app.task.html for
+    how to use tasks once instantiated
+
+    """
+
+    _log = None
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        self.send_event('task-result-exception')
+
+    def on_success(self, retval, task_id, args, kwargs):
+        self.send_event('task-result', result=retval)
+
+    @property
+    def log(self):
+        if self._log is None:
+            self._log = get_task_logger(self.name)
+        return self._log
+
+    def run(self, *args, **kwargs):
+        self.log.debug('%s: args=%s, kwargs=%s', self.name, args, kwargs)
+        ret = super().run(*args, **kwargs)
+        self.log.debug('%s: OK => %s', self.name, ret)
+        return ret
+
+
+class Task(SWHTask):
+    """a schedulable task (abstract class)
+
+    DEPRECATED! Please use SWHTask as base for decorated functions instead.
+
+    Sub-classes must implement the run_task() method.
+
+    Current implementation is based on Celery. See
+    http://docs.celeryproject.org/en/latest/reference/celery.app.task.html for
+    how to use tasks once instantiated
+
+    """
+
+    abstract = True
+
+    def run(self, *args, **kwargs):
+        """This method is called by the celery worker when a task is received.
+
+        Should not be overridden as we need our special events to be sent for
+        the reccurrent scheduler. Override run_task instead."""
+        return self.run_task(*args, **kwargs)
+
+    def run_task(self, *args, **kwargs):
+        """Perform the task.
+
+        Must return a json-serializable value as it is passed back to the task
+        scheduler using a celery event.
+        """
+        raise NotImplementedError('tasks must implement the run_task() method')
