88ad047a8cb8b7f1bb0f47ae2dd54d7958d544a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:48 +0000

    Add package gugu/0.1.3/gugu-0.1.3.tar.gz

---
+++ packages/gugu/gugu-0.1.3.tar.gz/gugu-0.1.3/gugu/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+__version__ = '0.1.3'
+__author__ = 'TabQ'
+
+
+from . import config
+
+from .base import Base
+
+from .billboard import BillBoard
+
+from .classify import Classify
+
+from .lowriskintarb import LowRiskIntArb
+
+from .macro import Macro
+
+from .marketdata import MarketData
+
+from .reference import Reference
+
+from .stockdata import StockData
+
+from .stockinfo import StockInfo
+
+from .utility import Utility
+
