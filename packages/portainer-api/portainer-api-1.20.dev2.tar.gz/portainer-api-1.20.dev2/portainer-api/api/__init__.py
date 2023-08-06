5989cee54dc1b03ae2d26a78f50f72412b3ef604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package portainer-api/1.20.dev2/portainer-api-1.20.dev2.tar.gz

---
+++ packages/portainer-api/portainer-api-1.20.dev2.tar.gz/portainer-api-1.20.dev2/portainer-api/api/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from portainer-api.api.auth_api import AuthApi
+from portainer-api.api.dockerhub_api import DockerhubApi
+from portainer-api.api.endpoint_groups_api import EndpointGroupsApi
+from portainer-api.api.endpoints_api import EndpointsApi
+from portainer-api.api.registries_api import RegistriesApi
+from portainer-api.api.resource_controls_api import ResourceControlsApi
+from portainer-api.api.settings_api import SettingsApi
+from portainer-api.api.stacks_api import StacksApi
+from portainer-api.api.status_api import StatusApi
+from portainer-api.api.tags_api import TagsApi
+from portainer-api.api.team_memberships_api import TeamMembershipsApi
+from portainer-api.api.teams_api import TeamsApi
+from portainer-api.api.templates_api import TemplatesApi
+from portainer-api.api.upload_api import UploadApi
+from portainer-api.api.users_api import UsersApi
