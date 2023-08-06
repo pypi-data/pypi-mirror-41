3ad668045c18093b4a9ed63b0c4db94b67699d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:06 +0000

    Add package chainpoint3/0.1.1/chainpoint3-0.1.1.tar.gz

---
+++ packages/chainpoint3/chainpoint3-0.1.1.tar.gz/chainpoint3-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+install_requires = [
+    "merkletools"
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='chainpoint3',
+    version='0.1.1',
+    description='Chainpoint proof of existence library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    url='https://github.com/GitTiago/pychainpoint',
+    author='Tiago Santos',
+    keywords='proof of existence, blockchain, merkle tree',
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=install_requires
+)
---
+++ packages/chainpoint3/chainpoint3-0.1.1.tar.gz/chainpoint3-0.1.1/tests/__init__.py
@@ -0,0 +1,1 @@
+name = "chainpoint3.tests"
