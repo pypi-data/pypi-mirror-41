fd12831302ed08a8293aaf710aeac0e7aed79993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package python-reapy/0.1.0.dev1/python-reapy-0.1.0.dev1.tar.gz

---
+++ packages/python-reapy/python-reapy-0.1.0.dev1.tar.gz/python-reapy-0.1.0.dev1/reapy/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+# Whether reapy is imported from inside REAPER.
+def is_inside_reaper():
+    inside = hasattr(sys.modules["__main__"], "obj")
+    return inside
+
+from . import reascript_api
+from .core.project import Project
+from .core.reaper import *
+
+CURRENT_PROJECT = Project(0)
