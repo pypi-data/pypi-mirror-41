f4c535524f21a502f6c514c65dd1fcd90ee6920e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:14 +0000

    Add package AlRegCode/0.0.2/AlRegCode-0.0.2.tar.gz

---
+++ packages/AlRegCode/AlRegCode-0.0.2.tar.gz/AlRegCode-0.0.2/alregcode/__init__.py
@@ -0,0 +1,33 @@
+name = "alregcode"
+
+import datetime
+
+def regGenerate(str_code, int_code, len_code):
+
+    str_code = str_code
+    now = datetime.date.today()
+    tahun = str(now.year)
+    bulan = str(now.month)
+    if len(bulan) < 2:
+        bulan = "0" + str(now.month)
+
+    tanggal = tahun + bulan
+
+    kode = int_code
+
+    if isinstance(kode, type(None)):
+        nomor = ""
+        for x in range(len_code):
+            nomor = nomor + "0"
+    else:
+        nomorr = str(int(kode) + 1)
+        nomors = "0"
+        for x in range((len_code-1)-len(nomorr)):
+            nomors = nomors + "0"
+
+        nomor = nomors + nomorr 
+
+
+    kode = str_code+tahun+bulan+nomor
+
+    return kode
