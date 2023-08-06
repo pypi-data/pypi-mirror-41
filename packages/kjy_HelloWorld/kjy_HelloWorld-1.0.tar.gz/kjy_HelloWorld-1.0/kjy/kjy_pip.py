c45aa67e2c6201ffc1882f2708670b62995c2113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:53 +0000

    Add package kjy_HelloWorld/1.0/kjy_HelloWorld-1.0.tar.gz

---
+++ packages/kjy_HelloWorld/kjy_HelloWorld-1.0.tar.gz/kjy_HelloWorld-1.0/kjy/kjy_pip.py
@@ -0,0 +1,8 @@
+import setuptools
+# setuptools.dist()
+class Dog_pip(object):
+	def __init__(self):
+		self.dog = "hello,world"
+
+new = Dog_pip()
+print(new.dog)
---
+++ packages/kjy_HelloWorld/kjy_HelloWorld-1.0.tar.gz/kjy_HelloWorld-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+        name='kjy_HelloWorld',     # 包名字
+        version='1.0',   # 包版本
+        description='This is a test of the setup',   # 简单描述
+        author='kjy',  # 作者
+        author_email='609363537@qq.com',  # 作者邮箱
+        url='https://github.com/kangjianyu/HelloWorld.git',      # 包的主页
+        packages=find_packages(),                 # 包
+)
