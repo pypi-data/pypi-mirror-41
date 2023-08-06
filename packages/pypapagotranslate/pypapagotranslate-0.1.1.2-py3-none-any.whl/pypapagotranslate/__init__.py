67c1f6520ddb48b1227b7077263a90b00c4016ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:22 +0000

    Add package pypapagotranslate/0.1.1.2/pypapagotranslate-0.1.1.2-py3-none-any.whl

---
+++ packages/pypapagotranslate/pypapagotranslate-0.1.1.2-py3-none-any.whl/pypapagotranslate/__init__.py
@@ -0,0 +1,13 @@
+"""
+Papago translate client for Python.
+-----------------------------------
+Made by SeolHa314
+"""
+
+from .translator import N2MT, SMT
+from .exceptions import *
+
+__title__ = "pypapagotranslate"
+__author__ = "SeolHa314"
+__license__ = "MIT"
+__version__ = "0.1.1.2"
