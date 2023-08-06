255a5bc70e7da05ec64f16d4f6277364657d3f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:50 +0000

    Add package pymltools/2019.1.29/pymltools-2019.1.29.tar.gz

---
+++ packages/pymltools/pymltools-2019.1.29.tar.gz/pymltools-2019.1.29/README.md
@@ -0,0 +1,7 @@
+# pymltools
+machine learning tools for python3.6.
+
+Links:
+
+* [Github](https://github.com/frkhit/pymltools)
+* [Pypi](https://github.com/frkhit/pymltools)
---
+++ packages/pymltools/pymltools-2019.1.29.tar.gz/pymltools-2019.1.29/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+# version info
+VERSION = (2019, 1, 29)
+VERSION_STATUS = ""
+VERSION_TEXT = ".".join(str(x) for x in VERSION) + VERSION_STATUS
+
+setup(name="pymltools",
+      version=VERSION_TEXT,
+      description="machine learning tools for python3.6",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Other Audience',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+      ],
+      install_requires=[
+          "tensorflow",
+          "pyxtools",
+          "h5py",
+      ],
+      author="frkhit",
+      url="https://github.com/frkhit/pymltools",
+      author_email="frkhit@gmail.com",
+      license="MIT",
+      packages=find_packages(),
+      package_data={'': ["LICENSE", "README.md", "MANIFEST.in"]},
+      include_package_data=True, )
