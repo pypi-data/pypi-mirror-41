c784e5d1db23359044ce7c622955aa2b68dc736b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:26 +0000

    Add package linkpred/0.4.1/linkpred-0.4.1.tar.gz

---
+++ packages/linkpred/linkpred-0.4.1.tar.gz/linkpred-0.4.1/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='linkpred',
+    version='0.4.1',
+    url='http://github.com/rafguns/linkpred/',
+    license='New BSD License',
+    author='Raf Guns',
+    tests_require=['nose'],
+    install_requires=[
+        'matplotlib>=2.1',
+        'networkx>=2.1',
+        'numpy>=1.14',
+        'pyyaml>=3.0',
+        'scipy>=1.0',
+        'smokesignal>=0.7',
+    ],
+    author_email='raf.guns@uantwerpen.be',
+    description='Python package for link prediction',
+    long_description=long_description,
+    packages=[
+        'linkpred',
+        'linkpred.evaluation',
+        'linkpred.network',
+        'linkpred.predictors',
+    ],
+    package_data={
+        'linkpred': ['tests/*.py'],
+        'linkpred.evaluation': ['tests/*.py'],
+        'linkpred.network': ['tests/*.py'],
+        'linkpred.predictors': ['tests/*.py'],
+    },
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': ['linkpred=linkpred.cli:main'],
+    },
+)
