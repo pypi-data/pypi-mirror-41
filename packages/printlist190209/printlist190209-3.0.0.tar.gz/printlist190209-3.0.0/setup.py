4a9eee931c449fdf342b87812c81f766fda2ff9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:31 +0000

    Add package printlist190209/3.0.0/printlist190209-3.0.0.tar.gz

---
+++ packages/printlist190209/printlist190209-3.0.0.tar.gz/printlist190209-3.0.0/setup.py
@@ -0,0 +1,12 @@
+﻿from distutils.core import setup        #从python发布工具导入setup函数
+
+setup(
+    name         = 'printlist190209',                    # 包名
+    version      = '3.0.0',                        # 版本号
+    py_modules   = ['printlist'],
+    author       = 'sonia',
+    author_email = 'sonia_du@163.com',
+    url          = 'http://www.devlve.top',
+    description  = 'printlist-190209模块，提供了一个名为printlist()的函数,这个函数的作用是显示列表，其中有可能包含（也可能不包含）嵌套列表。',
+    )
+    
