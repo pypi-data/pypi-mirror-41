9e11a40e7551c3532e41f081729dfd81137577be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:11 +0000

    Add package pyairly/1.0/pyairly-1.0.tar.gz

---
+++ packages/pyairly/pyairly-1.0.tar.gz/pyairly-1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(name='pyairly',
+      version='1.0',
+      license='Apache 2.0',
+      url='https://github.com/lemonov/PyAirly',
+      install_requires=[
+          'requests',
+      ],
+      py_modules=['pyairly'],
+      python_requires='>=3'
+      )
