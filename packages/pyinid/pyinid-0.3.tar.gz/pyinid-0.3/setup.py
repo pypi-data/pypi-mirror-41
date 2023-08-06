22ba4e77c58e3f3e7f3d727630e77fb72831c431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:49 +0000

    Add package pyinid/0.3/pyinid-0.3.tar.gz

---
+++ packages/pyinid/pyinid-0.3.tar.gz/pyinid-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+import io
+
+def read(*parts):
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
+
+    with io.open(filename, encoding='utf-8', mode='rt') as fp:
+        return fp.read()
+
+
+setup(name='pyinid',
+      version='0.3',
+      description='Validate And Generate Iran National ID',
+      long_description=read('README.md'),
+      url='https://github.com/RYNEQ/pyINID',
+      author='Ariyan Eghbal (RYN)',
+      author_email='ariyan.eghbal@gmail.com',
+      license='GPLv2',
+      packages=['inid'],
+      zip_safe=False)
+
