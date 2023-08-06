cd379c540647cfff7b2453e8ed3323d40d920e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:30 +0000

    Add package sigproSS/0.0.0.12/sigproSS-0.0.0.12.tar.gz

---
+++ packages/sigproSS/sigproSS-0.0.0.12.tar.gz/sigproSS-0.0.0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+
+setup(name='sigproSS',
+      version='0.0.0.12',
+      description=' Decomposes mutational catalogues to mutational signatures',
+      url="https://github.com/AlexandrovLab/SigProfilerExtractor.git",
+      author='S Mishu Ashiqul Islam',
+      author_email='m0islam@ucsd.edu',
+      license='UCSD',
+      packages=['sigproSS'],
+      install_requires=[
+          'matplotlib>=2.2.2',
+	  'scipy>=1.1.0', 
+	  'numpy>=1.14.4', 
+	  'pandas>=0.23.4', 
+	  'SigProfilerMatrixGenerator>=0.1.20', 
+	  'sigProfilerPlotting>=0.1.17', 
+	  'sigproextractor>=0.0.5.6', 
+	  'statsmodels>=0.9.0',
+	  'scikit-learn',
+      	   ],
+      include_package_data=True,      
+      zip_safe=False)
