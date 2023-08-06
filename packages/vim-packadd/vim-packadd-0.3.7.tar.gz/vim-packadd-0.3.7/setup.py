960338afa5b68e9f9bbf87de8d94c0f4611bf048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package vim-packadd/0.3.7/vim-packadd-0.3.7.tar.gz

---
+++ packages/vim-packadd/vim-packadd-0.3.7.tar.gz/vim-packadd-0.3.7/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from distutils.core import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('packadd/packadd.py').read(),
+    re.M
+    ).group(1)
+
+
+setup(
+    name = "vim-packadd",
+    packages = ["packadd"],
+    entry_points = {
+        "console_scripts": ['packadd = packadd.packadd:main']
+        },
+    version = version,
+    description = "Package manager for Vim8.",
+    author = "Antoine Dray",
+    author_email = "antoine.dray@epita.fr",
+    license='MIT',
+    install_requires=[
+          'gitpython',
+      ],
+    url = "https://github.com/cloudnodes/vim-packadd",
+    )
