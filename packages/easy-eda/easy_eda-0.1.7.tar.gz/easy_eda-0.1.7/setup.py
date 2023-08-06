d8de310e970cf0d2bddd74a8cf1dcd9215858df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package easy-eda/0.1.7/easy_eda-0.1.7.tar.gz

---
+++ packages/easy-eda/easy_eda-0.1.7.tar.gz/easy_eda-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+LONG_DESCRIPTION = """Easy Exploratory Data Analysis
+
+The functionality present in this package simplifies
+Exploratory Data Analysis for datasets provided as
+pandas DataFrames.
+"""
+
+setup(
+    name='easy_eda',
+    version='0.1.7',
+    description='Exploratory Data Analysis',
+    long_description=LONG_DESCRIPTION,
+    url='http://github.com/miskolc/easy_eda',
+    author='Dragos Calin',
+    author_email='dragos.calin.gm@mail.com',
+    license='MIT',
+    packages=['easy_eda'],
+    install_requires=[
+        'matplotlib >= 2.2.3',
+        'numpy >= 1.15.1',
+        'pandas >= 0.22.0',
+        'seaborn >= 0.9.0',
+    ],
+    zip_safe=False
+)
