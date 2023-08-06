2a7ccf2798a51d90d221b6056689f0847dcec3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:25 +0000

    Add package socketscpi/0.0.2/socketscpi-0.0.2.tar.gz

---
+++ packages/socketscpi/socketscpi-0.0.2.tar.gz/socketscpi-0.0.2/socketscpi/__init__.py
@@ -0,0 +1,6 @@
+__author__ = """Morgan Allison"""
+__email__ = 'morgan.j.allison@gmail.com'
+__version__ = '0.0.2'
+
+from socketscpi.socketscpi import SocketInstrument
+# from socketscpi.socketscpi import SockInstError
