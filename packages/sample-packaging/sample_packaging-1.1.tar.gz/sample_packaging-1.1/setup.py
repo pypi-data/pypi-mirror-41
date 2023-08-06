5a1c8abfe4e39bb41cf535c86cb807f54e07d665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package sample-packaging/1.1/sample_packaging-1.1.tar.gz

---
+++ packages/sample-packaging/sample_packaging-1.1.tar.gz/sample_packaging-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sample_packaging',
+      version='1.1',
+      description='Sample packaging',
+      url='https://github.com/kartikiyyer/sample-packaging.git',
+      author='Kartik',
+      author_email='sample@packaging.com',
+      license='MIT',
+      packages=['sample_packaging'],
+      zip_safe=False)
