36ffd4d9b9627f1e2e07153668951b438caa6ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:18 +0000

    Add package o2-docparser/1.1/o2_docparser-1.1.tar.gz

---
+++ packages/o2-docparser/o2_docparser-1.1.tar.gz/o2_docparser-1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="o2_docparser",
+    version="1.1",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=[""],
+    install_requires=["o2_docparser"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
