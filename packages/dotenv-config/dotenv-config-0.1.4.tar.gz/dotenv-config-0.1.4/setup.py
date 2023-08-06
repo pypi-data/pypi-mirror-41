46534b6ecc6e690605d24a44dd494a9febf3d03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:19 +0000

    Add package dotenv-config/0.1.4/dotenv-config-0.1.4.tar.gz

---
+++ packages/dotenv-config/dotenv-config-0.1.4.tar.gz/dotenv-config-0.1.4/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as _f:
+        return _f.read()
+
+
+setup(
+    author="Nikita Sivakov",
+    author_email="sivakov512@gmail.com",
+    description="Simple dotenv loader with the possibility of casting types",
+    install_requires=['python-dotenv'],
+    keywords="dotenv config env types cast",
+    license="MIT",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    name="dotenv-config",
+    py_modules=['dotenv_config'],
+    python_requires='>=3.6',
+    url="https://github.com/sivakov512/dotenv-config",
+    version="0.1.4",
+)
