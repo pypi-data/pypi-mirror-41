5ac44a9cc709a97c571f940e66c8af99d065eb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:04 +0000

    Add package tarball-deploy/0.0.0/tarball-deploy-0.0.0.tar.gz

---
+++ packages/tarball-deploy/tarball-deploy-0.0.0.tar.gz/tarball-deploy-0.0.0/tarball_deploy/__init__.py
@@ -0,0 +1,27 @@
+import argparse
+import pkg_resources
+import os
+import sys
+
+from tarball_deploy.workdir import Workdir
+
+
+__version__ = pkg_resources.get_distribution(__name__).version
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    parser.add_argument("--rollback", action="store_true")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    workdir = Workdir(os.getcwd())
+    if args.rollback:
+        workdir.rollback()
+    else:
+        workdir.deploy(sys.stdin)
