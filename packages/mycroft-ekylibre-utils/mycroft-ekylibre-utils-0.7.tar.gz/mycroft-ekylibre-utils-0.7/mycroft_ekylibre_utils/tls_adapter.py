46b2a7fd3eac9bd3bd2d3b631ef23bc8e5e7fcbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package mycroft-ekylibre-utils/0.7/mycroft-ekylibre-utils-0.7.tar.gz

---
+++ packages/mycroft-ekylibre-utils/mycroft-ekylibre-utils-0.7.tar.gz/mycroft-ekylibre-utils-0.7/mycroft_ekylibre_utils/tls_adapter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ssl
+from urllib3 import PoolManager
+from requests.adapters import HTTPAdapter
+
+
+class TlsAdapter(HTTPAdapter):
+    """"Transport adapter" that allows us to use SSLv3."""
+
+    def init_poolmanager(self, connections, maxsize, block=False, **kwargs):
+        self.poolmanager = PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+    # def init_poolmanager(self, *args, **kwargs):
+    #     ssl_context = ssl.create_default_context()
+    #     ssl_context.options &= ~ssl.PROTOCOL_TLSv1_2
+    #     ssl_context.check_hostname = False
+    #     kwargs['ssl_context'] = ssl_context
+    #     return super(TlsAdapter, self).init_poolmanager(*args, **kwargs)
