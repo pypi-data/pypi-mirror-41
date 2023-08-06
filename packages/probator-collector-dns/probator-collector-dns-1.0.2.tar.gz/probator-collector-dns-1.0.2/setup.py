7b424f74aa4c61933d2b35b303a081742eda5646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:19 +0000

    Add package probator-collector-dns/1.0.2/probator-collector-dns-1.0.2.tar.gz

---
+++ packages/probator-collector-dns/probator-collector-dns-1.0.2.tar.gz/probator-collector-dns-1.0.2/setup.py
@@ -0,0 +1,71 @@
+import os
+from codecs import open
+
+import setuptools
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(path, 'README.md')) as fd:
+    long_desc = fd.read()
+
+setuptools.setup(
+    name='probator-collector-dns',
+    use_scm_version=True,
+    python_requires='~=3.7',
+
+    entry_points={
+        'probator.plugins.collectors': [
+            'collector_dns = probator_collector_dns:DNSCollector'
+        ],
+    },
+
+    packages=setuptools.find_packages(),
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'probator~=1.0',
+        'dnspython~=1.16',
+    ],
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+
+    # Metadata for the project
+    description='DNS Collector',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bunjiboys/probator-collector-dns/',
+    author='Asbjorn Kjaer',
+    author_email='bunjiboys+probator@gmail.com',
+    license='License :: OSI Approved :: Apache Software License',
+    classifiers=[
+        # Current project status
+        'Development Status :: 4 - Beta',
+
+        # Audience
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+
+        # License information
+        'License :: OSI Approved :: Apache Software License',
+
+        # Supported python versions
+        'Programming Language :: Python :: 3.7',
+
+        # Frameworks used
+        'Framework :: Flask',
+        'Framework :: Sphinx',
+
+        # Supported OS's
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+
+        # Extra metadata
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Topic :: Security',
+        'Topic :: Utilities',
+    ],
+    keywords='cloud security',
+)
