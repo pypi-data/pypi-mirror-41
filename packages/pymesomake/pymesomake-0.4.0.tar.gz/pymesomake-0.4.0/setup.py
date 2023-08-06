dcb1b46a16b5f2f678f7e1170b6ce9640ff200b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package pymesomake/0.4.0/pymesomake-0.4.0.tar.gz

---
+++ packages/pymesomake/pymesomake-0.4.0.tar.gz/pymesomake-0.4.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymesomake",
+    version="0.4.0",
+    author="Eric Theise",
+    author_email="erictheise@gmail.com",
+    description="An implementation of the mesostic generation algorithm Andrew Culver developed for John Cage.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erictheise/pymesomake",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'pymesomake = pymesomake.__main__:main'
+        ]
+    },
+    install_requires=[
+        'Click',
+        'nltk'
+    ]
+)
