b7b2b28b974a7de25f76a529de1bd570df1ec8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:29 +0000

    Add package licensing/0.3/licensing-0.3.tar.gz

---
+++ packages/licensing/licensing-0.3.tar.gz/licensing-0.3/licensing/methods.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 08:06:39 2019
+
+@author: Artem Los
+"""
+
+from licensing.internal import Helpers
+from licensing.models import *
+
+class Key:
+    
+    """
+    License key related methods. More docs: https://app.cryptolens.io/docs/api/v3/Key.
+    """
+    
+    
+    def activate(token, rsa_pub_key, product_id, key, machine_code, fields_to_return = 0,\
+                 metadata = False, floating_time_interval = 0,\
+                 max_overdraft = 0):
+        
+        """
+        Calls the Activate method in Web API 3 and returns a tuple containing
+        (LicenseKey, Message). If an error occurs, LicenseKey will be None. If
+        everything went well, no message will be returned.
+        
+        More docs: https://app.cryptolens.io/docs/api/v3/Activate
+        """
+        
+        response = Response("","","","")
+        
+        try:
+            response = Response.from_string(Helpers.send_request("key/activate", {"token":token,\
+                                                  "ProductId":product_id,\
+                                                  "key":key,\
+                                                  "MachineCode":machine_code,\
+                                                  "FieldsToReturn":fields_to_return,\
+                                                  "metadata":metadata,\
+                                                  "FloatingTimeInterval": floating_time_interval,\
+                                                  "MaxOverdraft": max_overdraft,\
+                                                  "Sign":"True",\
+                                                  "SignMethod":1}))
+        except Exception:
+            return (None, "Could not contact the server.")
+        
+        pubkey = RSAPublicKey.from_string(rsa_pub_key)
+    
+        if response.result == "1":
+            return (None, response.message)
+        else:
+            try:
+                if Helpers.verify_signature(response, pubkey):
+                    return (LicenseKey.from_response(response), response.message)
+                else:
+                    return (None, "The signature check failed.")
+            except Exception:
+                return (None, "The signature check failed.")
