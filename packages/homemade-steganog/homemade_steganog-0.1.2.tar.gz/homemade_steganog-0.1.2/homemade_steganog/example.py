b10cab89ee0b8257ac4719d812195405bffbfc3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:32 +0000

    Add package homemade-steganog/0.1.2/homemade_steganog-0.1.2.tar.gz

---
+++ packages/homemade-steganog/homemade_steganog-0.1.2.tar.gz/homemade_steganog-0.1.2/homemade_steganog/example.py
@@ -0,0 +1,18 @@
+# Example usage
+import matplotlib.pyplot as plt
+# from homemade_steganog.steg import Steg
+from steg import Steg
+
+img_path = '../data/img.jpeg'
+data_path = '../data/text.txt'
+s = Steg()
+s.load_img(img_path)
+s.load_data(data_path)
+data_in_img = s.encrypt()
+
+# Send image here
+plt.imshow(data_in_img)
+plt.show()
+
+message = s.decrypt_img(data_in_img)
+print(message)
---
+++ packages/homemade-steganog/homemade_steganog-0.1.2.tar.gz/homemade_steganog-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='homemade_steganog',
+    version='0.1.2',
+    packages=['homemade_steganog',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
