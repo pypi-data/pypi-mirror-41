754867689951bb8967be9d6af9c99e6d96c9b57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package pesummary/0.1.3/pesummary-0.1.3.tar.gz

---
+++ packages/pesummary/pesummary-0.1.3.tar.gz/pesummary-0.1.3/setup.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2018  Charlie Hoy <charlie.hoy@ligo.org>
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(name='pesummary',
+      version='0.1.3',
+      description='Python package to produce summary pages for Parameter '
+                  'estimation codes',
+      author='Charlie Hoy',
+      author_email='charlie.hoy@ligo.org',
+      url='https://git.ligo.org/charlie.hoy/pesummary',
+      download_url='https://git.ligo.org/charlie.hoy/pesummary',
+      install_requires=[
+        'h5py',
+        'numpy',
+        'corner',
+        'matplotlib',
+        'deepdish',
+        'pandas',
+        'pygments',
+        'astropy',
+        'lalsuite',
+        'pytest'],
+      include_package_data=True,
+      packages=find_packages(),
+      package_dir={'pesummary': 'pesummary'},
+      package_data={'pesummary': ['js/*.js', 'css/*.css']},
+      scripts=['pesummary/bin/summarypages.py',
+               'pesummary/bin/summaryplots.py',
+               'pesummary/bin/inputs.py'],
+      classifiers=[
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"],
+      license='MIT',
+      long_description=readme,
+      long_description_content_type='text/markdown')
