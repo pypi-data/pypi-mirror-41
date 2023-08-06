07e33e676fb04960559675227bdc658855edab48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:15 +0000

    Add package PyEIS/1.0.7/PyEIS-1.0.7.tar.gz

---
+++ packages/PyEIS/PyEIS-1.0.7.tar.gz/PyEIS-1.0.7/bin/PyEIS_script.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+from PyEIS import PyEIS_Data_extraction
+from PyEIS import PyEIS_Lin_KK
+from PyEIS import PyEIS_Advanced_tools
+from PyEIS import PyEIS
+
+if __name__ == "__name__":
+    main.main()
---
+++ packages/PyEIS/PyEIS-1.0.7.tar.gz/PyEIS-1.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyEIS",
+    version="1.0.7",
+    author="Kristian B. Knudsen",
+    author_email="kknu@berkeley.edu",
+    description="A Python-based Electrochemical Impedance Spectroscopy simulator and analyzer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kbknudsen/PyEIS",
+    license='LICENSE',
+    packages=['PyEIS'],
+    scripts=['bin/PyEIS_script.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
