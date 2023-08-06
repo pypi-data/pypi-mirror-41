3c0bb6db060bb68e23895554c19c8d7cd36887f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package gym-push/0.0.3/gym-push-0.0.3.tar.gz

---
+++ packages/gym-push/gym-push-0.0.3.tar.gz/gym-push-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gym-push',
+      packages=['gym_push'],
+      version='0.0.3',
+      install_requires=['gym', 'numpy', 'pandas'],  # And any other dependencies foo needs,
+      author='Kieran Fraser',
+      author_email='kfraser@tcd.ie',
+      download_url='https://github.com/kieranfraser/gym-push/archive/0.0.3.tar.gz'
+) 
