236741f475a0d61d847f24aeceda501e1f385935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:15 +0000

    Add package example-pkg-ParkNamsun/0.0.2/example-pkg-ParkNamsun-0.0.2.tar.gz

---
+++ packages/example-pkg-ParkNamsun/example-pkg-ParkNamsun-0.0.2.tar.gz/example-pkg-ParkNamsun-0.0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="example-pkg-ParkNamsun",
+    version='0.0.2',
+    description='A Small example package',
+    author='rakens2',
+    packages=setuptools.find_packages()
+)
