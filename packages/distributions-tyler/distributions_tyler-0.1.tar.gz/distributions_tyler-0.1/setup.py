d85cd09077f46c42f070736d821e5fa5c849725a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:00 +0000

    Add package distributions-tyler/0.1/distributions_tyler-0.1.tar.gz

---
+++ packages/distributions-tyler/distributions_tyler-0.1.tar.gz/distributions_tyler-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_tyler',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tyler'],
+      author = 'Tyler Watkins',
+      author_email = 'tylerwatkins101@gmail.com',
+      zip_safe=False)
