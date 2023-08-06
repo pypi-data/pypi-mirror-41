1211e848043024dbaf982027b19aa7de05e43400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package cmml/1.1.dev0/cmml-1.1.dev0.tar.gz

---
+++ packages/cmml/cmml-1.1.dev0.tar.gz/cmml-1.1.dev0/cmml/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    flask
+    ~~~~~
+
+    A microframework based on Werkzeug.  It's extensively documented
+    and follows best practice patterns.
+
+    :copyright: © 2010 by the Pallets team.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = '1.1.dev'
+
