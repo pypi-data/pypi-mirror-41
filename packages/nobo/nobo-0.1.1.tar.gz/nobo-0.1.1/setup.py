934156d5a9c23ef2ad84bfb0f3657a22efa07e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:57 +0000

    Add package nobo/0.1.1/nobo-0.1.1.tar.gz

---
+++ packages/nobo/nobo-0.1.1.tar.gz/nobo-0.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nobo',
+      version='0.1.1',
+      description='A third-party Rits API',
+      long_description='A third-party spider that you could get your data from each service provides by Ritsumeikan University',
+      author='Zhou Fang',
+      license='GPLv3',
+      author_email='is0385rx@ed.ritsumei.ac.jp',
+      url='https://github.com/fang2hou/Nobo',
+      packages=['Nobo'],
+      keywords = ['ritsumei', 'manaba'],
+      python_requires='>=3.4',
+      install_requires=[
+        "beautifulsoup4",
+        "selenium",
+      ],
+     )
