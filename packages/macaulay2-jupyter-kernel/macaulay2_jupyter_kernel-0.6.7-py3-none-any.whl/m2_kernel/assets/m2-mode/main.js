c627a7944d55b9c98b6e0b7530afcf435f984f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:34 +0000

    Add package macaulay2-jupyter-kernel/0.6.7/macaulay2_jupyter_kernel-0.6.7-py3-none-any.whl

---
+++ packages/macaulay2-jupyter-kernel/macaulay2_jupyter_kernel-0.6.7-py3-none-any.whl/m2_kernel/assets/m2-mode/main.js
@@ -0,0 +1,13 @@
+define([
+    'base/js/namespace',
+    './macaulay2'
+  ], function () {
+    "use strict";
+
+    return {
+      load_ipython_extension: function () {
+        console.log('Loading Macaulay2 mode...');
+      }
+    };
+
+  });
