6d7618e6430d92aadbdb970d65d368b10f72f877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package forge-python-sdk/0.1.1/forge-python-sdk-0.1.1.tar.gz

---
+++ packages/forge-python-sdk/forge-python-sdk-0.1.1.tar.gz/forge-python-sdk-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forge-python-sdk",
+    version="0.1.1",
+    author="Riley Shu",
+    author_email="riley@arcblock.io",
+    description="A small example package",
+    data_files=[('config', ['forge/config/forge_default.toml'])],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArcBlock/forge-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'grpcio',
+        'grpcio-tools',
+        'toml',
+        'deepmerge',
+    ],
+)
