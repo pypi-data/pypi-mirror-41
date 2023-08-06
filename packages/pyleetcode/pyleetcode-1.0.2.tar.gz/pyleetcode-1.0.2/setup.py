8650180cc23b8ad86a4f32215a104db93f8b00fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package pyleetcode/1.0.2/pyleetcode-1.0.2.tar.gz

---
+++ packages/pyleetcode/pyleetcode-1.0.2.tar.gz/pyleetcode-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyleetcode',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'bs4',
+        'requests',
+        'colorama',
+    ],
+    entry_points='''
+        [console_scripts]
+        leet=src.commands:leet
+        submit=src.commands:submit
+        login=src.commands:login
+        logout=src.commands:logout
+        show=src.commands:show
+        detail=src.commands:detail
+        start=src.commands:start
+        lang=src.commands:lang
+    ''',
+)
