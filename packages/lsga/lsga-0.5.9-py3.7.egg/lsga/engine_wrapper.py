fb7bbeea53080c2249ea564e464b1cbcbd032bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package lsga/0.5.9/lsga-0.5.9-py3.7.egg

---
+++ packages/lsga/lsga-0.5.9-py3.7.egg/lsga/engine_wrapper.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lsga import GAEngine
+from lsga.components import *
+from lsga.operators import *
+from lsga.analysis.fitness_store import FitnessStore
+from lsga.analysis.console_output import ConsoleOutput
+
+import sys, inspect
+
+
+class EngineWrapper():
+
+    def create_engine(self, ranges, n_jobs=1, eps=0.001, population_size=50, encoding='binary', selection='tournament',
+                      crossover='uniform', mutation='flip_bit'):
+        """
+
+        :param ranges:
+        :param n_jobs:
+        :param eps:
+        :param population_size:
+        :param encoding:
+        :param selection:
+        :param crossover:
+        :param mutation:
+        """
+
+        assert encoding in ['binary', 'decimal']
+        assert selection in ['tournament', 'roulette_wheel', 'linear_ranking', 'exponential_ranking']
+        assert crossover in ['uniform']
+        assert mutation in ['flip_bit']
+
+        if encoding == 'binary':
+            indv_template = BinaryIndividual(ranges=ranges, eps=eps)
+        else:
+            indv_template = DecimalIndividual(ranges=ranges, eps=eps)
+
+        if selection == 'tournament':
+            lsga_selection = TournamentSelection()
+        elif selection == 'roulette_wheel':
+            lsga_selection = RouletteWheelSelection()
+        elif selection == 'linear_ranking':
+            lsga_selection = LinearRankingSelection()
+        else:
+            lsga_selection = ExponentialRankingSelection()
+
+        if crossover == 'uniform':
+            lsga_crossover = UniformCrossover(pc=0.8, pe=0.5)
+
+        if mutation == 'flip_bit':
+            lsga_mutation = FlipBitBigMutation(pm=0.1, pbm=0.55, alpha=0.6)
+
+        population = Population(indv_template=indv_template, size=population_size, n_jobs=n_jobs).init()
+
+        engine = GAEngine(population=population, selection=lsga_selection,
+                          crossover=lsga_crossover, mutation=lsga_mutation,
+                          analysis=[ConsoleOutput, FitnessStore])
+
+        return engine
+
+    def print_classes(self):
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj):
+                print(obj)
