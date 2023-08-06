dd75612b752a19b96ef06955b612aec2e85ea398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:08 +0000

    Add package nuntius/0.2.0/nuntius-0.2.0.tar.gz

---
+++ packages/nuntius/nuntius-0.2.0.tar.gz/nuntius-0.2.0/nuntius/actions.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+
+from nuntius.models import BaseSubscriber
+
+
+def update_subscriber(email, status):
+    model = settings.NUNTIUS_SUBSCRIBER_MODEL
+    model_class = ContentType.objects.get(
+        app_label=model.split(".")[0], model=model.split(".")[1].lower()
+    ).model_class()
+    model_class.objects.set_subscriber_status(email, status)
