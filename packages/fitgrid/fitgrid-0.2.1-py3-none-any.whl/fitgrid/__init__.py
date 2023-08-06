2c94915bddd39d08d30886c891f78ad4fbce62f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:41 +0000

    Add package fitgrid/0.2.1/fitgrid-0.2.1-py3-none-any.whl

---
+++ packages/fitgrid/fitgrid-0.2.1-py3-none-any.whl/fitgrid/__init__.py
@@ -0,0 +1,9 @@
+from .fake_data import generate
+from .io import (
+    epochs_from_hdf,
+    epochs_from_dataframe,
+    load_grid,
+    epochs_from_feather,
+)
+from .models import run_model, lm, lmer
+from . import utils, defaults
