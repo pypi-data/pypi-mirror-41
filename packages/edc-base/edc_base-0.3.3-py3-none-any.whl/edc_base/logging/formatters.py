908cbf2cfc0792f2844cb83067e3eb2b97f8c222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:43 +0000

    Add package edc-base/0.3.3/edc_base-0.3.3-py3-none-any.whl

---
+++ packages/edc-base/edc_base-0.3.3-py3-none-any.whl/edc_base/logging/formatters.py
@@ -0,0 +1,2 @@
+verbose_formatter = {
+    'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'}
---
+++ packages/edc-base/edc_base-0.3.3-py3-none-any.whl/edc_base/model_mixins/report_status_model_mixin.py
@@ -0,0 +1,31 @@
+from django.db import models
+# from edc_constants.constants import CLOSED, OPEN
+
+from ..choices import REPORT_STATUS
+from ..model_validators.date import datetime_not_future
+
+
+class ReportStatusModelMixin(models.Model):
+
+    report_status = models.CharField(
+        verbose_name='What is the status of this report?',
+        max_length=25,
+        choices=REPORT_STATUS)
+
+    report_closed_datetime = models.DateTimeField(
+        blank=True,
+        null=True,
+        validators=[datetime_not_future],
+        verbose_name=('Date and time report closed.'))
+
+#     @property
+#     def status(self):
+#         if self.report_status == CLOSED:
+#             return 'Closed'
+#         elif self.report_status == OPEN:
+#             return 'Open'
+#         else:
+#             return self.report_status
+
+    class Meta:
+        abstract = True
