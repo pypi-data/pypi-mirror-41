ca6fdf20b0f19f0f5309201477f3d9fd47ef005d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:57 +0000

    Add package sphinx-autoapi/0.7.1/sphinx-autoapi-0.7.1.tar.gz

---
+++ packages/sphinx-autoapi/sphinx-autoapi-0.7.1.tar.gz/sphinx-autoapi-0.7.1/autoapi/templates/python/function.rst
@@ -0,0 +1,9 @@
+{%- if obj.display %}
+
+.. function:: {{ obj.short_name }}({{ obj.args }})
+
+   {% if obj.docstring %}
+   {{ obj.docstring|prepare_docstring|indent(3) }}
+   {% endif %}
+
+{% endif %}
---
+++ packages/sphinx-autoapi/sphinx-autoapi-0.7.1.tar.gz/sphinx-autoapi-0.7.1/setup.py
@@ -0,0 +1,26 @@
+import codecs
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sphinx-autoapi',
+    version='0.7.1',
+    author='Eric Holscher',
+    author_email='eric@ericholscher.com',
+    url='http://github.com/rtfd/sphinx-autoapi',
+    license='BSD',
+    description='Sphinx auto API documentation generator',
+    packages=find_packages('.'),
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    include_package_data=True,
+    install_requires=[
+        'astroid;python_version>="3"',
+        'astroid<2;python_version<"3"',
+        'PyYAML',
+        'sphinx>=1.6',
+        'sphinxcontrib-golangdomain',
+        'sphinxcontrib-dotnetdomain',
+        'unidecode',
+    ],
+)
