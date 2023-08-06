981f04c3fbf96d97e68368100affe25e53292e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:13 +0000

    Add package nazk/1.0.0/nazk-1.0.0.tar.gz

---
+++ packages/nazk/nazk-1.0.0.tar.gz/nazk-1.0.0/nazk/exceptions.py
@@ -0,0 +1,17 @@
+# The exceptions
+
+
+class InvalidCurrency(Exception):
+    pass
+
+
+class InvalidSource(Exception):
+    pass
+
+
+class InvalidYearException(Exception):
+    pass
+
+
+class NoDataFoundException(Exception):
+    pass
