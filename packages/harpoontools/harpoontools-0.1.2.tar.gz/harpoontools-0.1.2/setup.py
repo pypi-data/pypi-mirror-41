3db19bf52e6aea2f5b25ca684395bd2cbdcec75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:28 +0000

    Add package harpoontools/0.1.2/harpoontools-0.1.2.tar.gz

---
+++ packages/harpoontools/harpoontools-0.1.2.tar.gz/harpoontools-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='harpoontools',
+    version='0.1.2',
+    description='CLI tools going with Harpoon',
+    url='https://github.com/Te-k/harpoontools',
+    author='Tek',
+    author_email='tek@randhome.io',
+    keywords='osint',
+    include_package_data=True,
+    install_requires=[
+        'harpoon==0.1.2'
+    ],
+    python_requires='>=3.5',
+    license='GPLv3',
+    packages=['harpoontools'],
+    entry_points= {
+        'console_scripts': [
+            'ipinfo=harpoontools.tools:ipinfo',
+            'asninfo=harpoontools.tools:asninfo',
+            'dns=harpoontools.tools:dns',
+            'asncount=harpoontools.tools:asncount',
+            'countrycount=harpoontools.tools:countrycount',
+            'htraceroute=harpoontools.tools:traceroute'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
+)
