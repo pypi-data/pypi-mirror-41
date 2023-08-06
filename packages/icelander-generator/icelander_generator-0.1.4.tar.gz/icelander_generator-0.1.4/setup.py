6fe5449fc9fa34a4f6d5bff19c8bb20fb6843221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:12 +0000

    Add package icelander-generator/0.1.4/icelander_generator-0.1.4.tar.gz

---
+++ packages/icelander-generator/icelander_generator-0.1.4.tar.gz/icelander_generator-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from os import path
+from setuptools import setup
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='icelander_generator',
+    packages=['icelander_generator'], # this must be the same as the name above
+    version='0.1.4',
+    description='A utility to generate random Icelanders',
+    author='7oi',
+    author_email='7oi@7oi.is',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/7oi/IcelanderGenerator',
+    install_requires=[
+        'lxml==4.3.0',
+        'requests==2.21.0',
+        'kennitala==0.1.4',
+        'future==0.17.1'
+    ],
+    include_package_data=True,
+    keywords=['tests', 'generator'],
+    classifiers = [],
+)
