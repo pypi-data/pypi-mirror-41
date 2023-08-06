8fd505dd12085c6f1b0ffb1e9ea85a1150212893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:37 +0000

    Add package aeropress/0.0.7/aeropress-0.0.7-py3-none-any.whl

---
+++ packages/aeropress/aeropress-0.0.7-py3-none-any.whl/aeropress/aws/cluster.py
@@ -0,0 +1,20 @@
+import boto3
+
+from aeropress import logger
+
+ecs_client = boto3.client('ecs', region_name='eu-west-1')
+
+
+def create(cluster_name: str) -> None:
+    response = ecs_client.create_cluster(
+        clusterName=cluster_name,
+    )
+
+    logger.info('Created cluster %s', cluster_name)
+    logger.debug('Created cluster details: %s', response)
+
+
+def describe(cluster: str) -> list:
+    return ecs_client.describe_clusters(
+        clusters=[cluster],
+    )
