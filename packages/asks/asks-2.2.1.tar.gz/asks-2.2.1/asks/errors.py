3e6edae0ea66f75ae1d2ffc655dd55ed522c5421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package asks/2.2.1/asks-2.2.1.tar.gz

---
+++ packages/asks/asks-2.2.1.tar.gz/asks-2.2.1/asks/errors.py
@@ -0,0 +1,37 @@
+'''
+Simple exceptions to be raised in case of errors.
+'''
+
+
+class AsksException(Exception):
+    '''
+    Base exception for all asks errors.
+    '''
+    pass
+
+
+class TooManyRedirects(AsksException):
+    pass
+
+
+class ConnectivityError(AsksException):
+    '''
+    Base exception for network failure errors.
+    '''
+    pass
+
+
+class BadHttpResponse(AsksException):
+    pass
+
+
+class BadStatus(AsksException):
+    pass
+
+
+class RequestTimeout(ConnectivityError):
+    pass
+
+
+class ServerClosedConnectionError(ConnectivityError):
+    pass
