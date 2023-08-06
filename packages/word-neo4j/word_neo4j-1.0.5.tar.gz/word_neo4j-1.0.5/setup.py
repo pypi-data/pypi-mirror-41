1aafce893c2ab406f40e35f99dc8e48bb627c4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:58 +0000

    Add package word-neo4j/1.0.5/word_neo4j-1.0.5.tar.gz

---
+++ packages/word-neo4j/word_neo4j-1.0.5.tar.gz/word_neo4j-1.0.5/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Kurt Bergen Ruegg",
+    author_email='kurtruegg92@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Word Neo4j contains modules relating to word components of Neo4j Cypher queries'",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='word_neo4j',
+    name='word_neo4j',
+    packages=find_packages(include=['word_neo4j']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/kruegg21/word_neo4j',
+    version='1.0.5',
+    zip_safe=False,
+)
---
+++ packages/word-neo4j/word_neo4j-1.0.5.tar.gz/word_neo4j-1.0.5/word_neo4j/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Word Neo4j."""
+
+__author__ = """Kurt Bergen Ruegg"""
+__email__ = 'kurtruegg92@gmail.com'
+__version__ = '1.0.5'
