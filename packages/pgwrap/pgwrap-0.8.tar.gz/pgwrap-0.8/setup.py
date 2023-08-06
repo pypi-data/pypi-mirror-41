08eed0e1106868601366b204d1696ff4de4fe6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:20 +0000

    Add package pgwrap/0.8/pgwrap-0.8.tar.gz

---
+++ packages/pgwrap/pgwrap-0.8.tar.gz/pgwrap-0.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distutils.core import Command,setup
+
+version = "0.8"
+description = 'Simple PostgreSQL database wrapper - provides wrapper over psycopg2 supporting a Python API for common sql functions (supports py2/py3'
+long_description = open("README").read()
+
+class GenerateReadme(Command):
+    description = "Generates README file"
+    user_options = []
+    def initialize_options(self): pass
+    def finalize_options(self): pass
+    def run(self):
+        import pgwrap,textwrap
+        long_description = textwrap.dedent(pgwrap.__doc__)
+        open("README","w").write(long_description)
+
+setup(name='pgwrap',
+      version = version,
+      description = description,
+      long_description = long_description,
+      author = 'Paul Chakravarti',
+      author_email = 'paul.chakravarti@gmail.com',
+      url = 'https://github.com/paulchakravarti/pgwrap',
+      cmdclass = { 'readme' : GenerateReadme },
+      packages = ['pgwrap'],
+      install_requires = ['psycopg2'],
+      license = 'BSD',
+      classifiers = [ "Topic :: Database",
+                      "Programming Language :: Python :: 2",
+                      "Programming Language :: Python :: 3",
+                    ]
+     )
