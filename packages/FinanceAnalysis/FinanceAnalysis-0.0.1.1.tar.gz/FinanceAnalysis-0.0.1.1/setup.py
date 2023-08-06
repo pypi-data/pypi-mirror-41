7a29b292cc85e305e0b11679df994cb8ad0e5383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:21 +0000

    Add package FinanceAnalysis/0.0.1.1/FinanceAnalysis-0.0.1.1.tar.gz

---
+++ packages/FinanceAnalysis/FinanceAnalysis-0.0.1.1.tar.gz/FinanceAnalysis-0.0.1.1/setup.py
@@ -0,0 +1,13 @@
+﻿
+from setuptools import find_packages, setup
+setup(
+    name='FinanceAnalysis',
+    version='0.0.1.1',
+    description='this is a Finance Analysis System',
+    author='fdsn',#作者
+    author_email='670428704@qq.com',
+    url="https://github.com/670428704/FinanceAnalysis.git",
+    # packages=find_packages(),
+    packages=['KLineAnalysis'],  #这里是所有代码所在的文件夹名称
+    install_requires=['tushare','numpy'],
+)
