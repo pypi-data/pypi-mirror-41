fc3009f6337bd0c9cfededa1b4c4e894681959c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:06 +0000

    Add package faux/0.0.7/faux-0.0.7-py2.py3-none-any.whl

---
+++ packages/faux/faux-0.0.7-py2.py3-none-any.whl/faux/client.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#
+# Simple REST server.
+#
+# @author <bprinty@gmail.com>
+# ---------------------------------------------------
+
+
+# imports
+# -------
+import os
+import requests
+from functools import wraps
+try:
+    from urllib.parse import urlsplit, urlunsplit, parse_qsl
+except ImportError:
+    from urlparse import urlsplit, urlunsplit, parse_qsl
+
+from .utils import request2path
+
+
+# config
+# ------
+CACHE = None
+
+
+def cache(path):
+    """
+    Method for setting options during request session.
+
+    Example:
+        >>> from faux import requests
+        >>> requests.cache('/path/to/local/cache')
+        >>> requests.get('http://localhost')
+    """
+    global CACHE
+    if not path:
+        raise AssertionError('Invalid path specified for cache directory ("{}")!'.format(path))
+    CACHE = path
+    return
+
+
+# decorators
+# ----------
+def cache_response(func):
+    """
+    Decorator for caching requests submitted with this module
+    into a file structure that can be served by a mock server.
+    """
+
+    @wraps(getattr(requests, func.__name__))
+    def _(*args, **kwargs):
+        global CACHE
+        response = func(*args, **kwargs)
+        if CACHE:
+
+            # generate local url
+            url = urlsplit(args[0])
+            rpath = request2path(
+                url=url.path,
+                args=dict(parse_qsl(url.query)),
+                payload=response.request.body
+            )
+            # for some reason, os.path.join(cache, method, rpath) doesn't work?
+            path = os.path.join(CACHE, response.request.method.upper()) + '/' + rpath
+            
+            # make location if it doesn't exist
+            directory, filename = os.path.dirname(path), os.path.basename(path)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            # write the data
+            with open(path, 'w') as fo:
+                fo.write(response.text)
+
+        return response
+    return _
+
+
+# functions
+# ---------
+@cache_response
+def get(*args, **kwargs):
+    return requests.get(*args, **kwargs)
+
+
+@cache_response
+def post(*args, **kwargs):
+    return requests.post(*args, **kwargs)
+
+
+@cache_response
+def put(*args, **kwargs):
+    return requests.put(*args, **kwargs)
+
+
+@cache_response
+def delete(*args, **kwargs):
+    return requests.delete(*args, **kwargs)
+
---
+++ packages/faux/faux-0.0.7-py2.py3-none-any.whl/faux/server.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+#
+# Classes for setting up server mock.
+#
+# -----------------------------------
+
+
+# imports
+# -------
+import os
+import re
+import six
+import json
+import time
+import logging
+import hashlib
+from functools import wraps
+from xml.etree import ElementTree
+from threading import Thread
+
+
+from .utils import format_data, request2path
+
+
+# classes
+# -------
+class Server(object):
+    """
+    Object mimicking flask server to allow for spinning
+    up server mock.
+    """
+
+    def __init__(self, *args, **kwargs):
+        from flask import Flask
+
+        self.cache = kwargs.pop('cache', None)
+        self.flask = Flask(*args, **kwargs)
+        # self.flask.logger.setLevel(logging.ERROR)
+        if self.cache:
+            self.init()
+        return
+
+    def init(self):
+        """
+        Method for decorating custom url handlers on server.
+        """
+        from flask import request, jsonify
+
+        # general-purpose request handler
+        @self.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+        def general(path):
+            # format path
+            path = request2path(path, args=request.args, payload=request.data)
+            filename = os.path.join(self.cache, path)
+
+            # if file doesn't exist, try to prepend the request method
+            if not os.path.exists(filename):
+                filename = os.path.join(self.cache, request.method.upper(), path)
+
+            # if file exists, read and return
+            if os.path.exists(filename) and not os.path.isdir(filename):
+                with open(filename, 'r') as fi:
+                    data = fi.read()
+                return data
+
+            return jsonify({
+                'error': 'Could not find local resource!',
+                'uri': filename
+            }), 404
+
+
+        # requests on root url
+        @self.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+        def root():
+            return general('.')
+
+
+        # requests on root url with uuid
+        @self.route('/<uuid:uuid>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+        def ident(uuid):
+            return general('_uuid')
+
+
+        # requests on nested path with uuid
+        @self.route('/<path:path>/<uuid:uuid>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+        def path_ident(path, uuid):
+            return general(path + '/_uuid')
+
+
+        # status url
+        @self.route('/_/status', methods=['GET'])
+        def status():
+            return {"status": "ok"}
+
+        return
+
+    @property
+    def logger(self):
+        """
+        Expose flask logger so user can change settings.
+
+        TODO: update this class to use __getattr__ for defaulting
+              to internal getattr(self.flask, item)
+        """
+        return self.flask.logger
+
+    def route(self, *args, **kwargs):
+        """
+        Override flask route decorator to provide easier
+        UX for return data. With these changes, users can
+        simply return a dictionary or xml Element object instead
+        of needing to craft a full response.
+        """
+        from flask import jsonify, Response
+
+        # inner decorator to make return values easier
+        def inner(method):
+            @wraps(method)
+            def _(*args, **kwargs):
+                ret = method(*args, **kwargs)
+                
+                # json
+                if isinstance(ret, dict):
+                    ret = format_data(json.dumps(ret))
+                    ret = json.loads(ret, strict=False)
+                    return jsonify(ret), 200
+                
+                # xml
+                elif isinstance(ret, ElementTree.Element):
+                    ret = ElementTree.tostring(ret, encoding='utf8', method='xml')
+                    ret = format_data(ret)
+                    return Response(ret, mimetype='text/xml')
+                
+                # string
+                elif isinstance(ret, six.string_types):
+                    ret = format_data(ret)
+                    return ret, 200
+
+                # other data
+                else:
+                    return ret
+            return _
+
+        # proxy for using normal flask route decorator
+        def decorator(func):
+            return self.flask.route(*args, **kwargs)(inner(func))
+
+        return decorator
+
+    def run(self, **kwargs):
+        # TODO: decorate all routes for reading from cached directory
+        return Instance(self.flask, **kwargs)
+
+    def __exit__(self, type, value, traceback):
+        return
+
+
+class Instance(object):
+    """
+    Contextmanager for running managing server mock.
+    """
+
+    def __init__(self, app, **kwargs):
+        self.app = app
+        kwargs['use_reloader'] = False
+        self.thread = Thread(target=self.app.run, kwargs=kwargs)
+        self.thread.daemon = True
+        return
+
+    def __enter__(self):
+        self.thread.start()
+        time.sleep(1)
+        return self.thread
+
+    def __exit__(self, type, value, traceback):
+        return
