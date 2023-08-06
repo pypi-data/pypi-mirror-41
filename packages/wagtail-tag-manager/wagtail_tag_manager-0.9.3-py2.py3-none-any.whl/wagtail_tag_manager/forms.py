eb4b312443ee3fa175c0024f2947348e385a755b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:31 +0000

    Add package wagtail-tag-manager/0.9.3/wagtail_tag_manager-0.9.3-py2.py3-none-any.whl

---
+++ packages/wagtail-tag-manager/wagtail_tag_manager-0.9.3-py2.py3-none-any.whl/wagtail_tag_manager/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from wagtail_tag_manager.settings import TagTypeSettings
+
+
+class ConsentForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for tag_type, config in TagTypeSettings.all().items():
+            initial = config == "initial" or config == "required"
+            if "initial" in kwargs:
+                initial = kwargs.get("initial")[tag_type]
+
+            self.fields[tag_type] = forms.BooleanField(
+                label=_(tag_type.title()),
+                required=config == "required",
+                disabled=config == "required",
+                initial=initial,
+            )
