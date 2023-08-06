218fdd1c96c85cf822a249e1c4ca9a6fbe70371a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package tpspy/0.8.3/tpspy-0.8.3.tar.gz

---
+++ packages/tpspy/tpspy-0.8.3.tar.gz/tpspy-0.8.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='tpspy',
+    version='0.8.3',
+    author="kangtian",
+    packages=setuptools.find_packages(),
+    license='All right reserved',
+    long_description="you know, enjoy it ...",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
