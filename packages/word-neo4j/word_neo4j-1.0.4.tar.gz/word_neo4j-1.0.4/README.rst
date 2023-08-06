d487115eb5cea56058fab2209e5250b6c94988cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:47 +0000

    Add package word-neo4j/1.0.4/word_neo4j-1.0.4.tar.gz

---
+++ packages/word-neo4j/word_neo4j-1.0.4.tar.gz/word_neo4j-1.0.4/README.rst
@@ -0,0 +1,37 @@
+==========
+Word Neo4j
+==========
+
+
+.. image:: https://img.shields.io/pypi/v/word_neo4j.svg
+        :target: https://pypi.python.org/pypi/word_neo4j
+
+.. image:: https://img.shields.io/travis/kruegg21/word_neo4j.svg
+        :target: https://travis-ci.org/kruegg21/word_neo4j
+
+.. image:: https://readthedocs.org/projects/word-neo4j/badge/?version=latest
+        :target: https://word-neo4j.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+Word Neo4j contains modules relating to word components of Neo4j Cypher queries'
+
+
+* Free software: MIT license
+* Documentation: https://word-neo4j.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/word-neo4j/word_neo4j-1.0.4.tar.gz/word_neo4j-1.0.4/tests/test_word_neo4j.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `word_neo4j` package."""
+import unittest.mock
+
+from word_neo4j import word_neo4j
+
+
+class TestKeyword(unittest.TestCase):
+    """Testing Keyword class."""
+    def test_default(self):
+        """Testing Keyword object creation."""
+        keyword = word_neo4j.Keyword('match')
+        assert keyword.word == 'MATCH'
+
+
+class TestNode(unittest.TestCase):
+    """Testing Node class."""
+    def test_default(self):
+        """Testing Node object creation with no kwargs."""
+        node = word_neo4j.Node('FooBar')
+        assert node.word == '(_foo_bar:FooBar)'
+
+    def test_empty(self):
+        """Testing Node object with None for value."""
+        node = word_neo4j.Node(None)
+        assert node.word == '()'
+
+    def test_explicitly_no_variable(self):
+        """Testing Node object creation with explicit variable=False."""
+        node = word_neo4j.Node('FooBar', variable=False)
+        assert node.word == '(:FooBar)'
+
+    def test_explicitly_add_variable(self):
+        """Testing Node object with variable=True'."""
+        node = word_neo4j.Node('FooBar', variable=True)
+        assert node.word == '(_foo_bar:FooBar)'
+
+    def test_provided_variable_name(self):
+        """Testing Node object with provided variable name."""
+        node = word_neo4j.Node('FooBar', variable_name='baz')
+        assert node.word == '(baz:FooBar)'
+
+    def test_provided_variable_name_explicitly_no_variable(self):
+        """Testing Node object
+
+        With provided variable name and explicit variable=False.
+        """
+        node = word_neo4j.Node('FooBar', variable_name='baz', variable=False)
+        assert node.word == '(:FooBar)'
+
+
+class TestRelationship(unittest.TestCase):
+    """Testing Relationship class."""
+    def test_default(self):
+        """Testing default relationship."""
+        relationship = word_neo4j.Relationship('foo_bar')
+        assert relationship.word == '-[:FOO_BAR]->'
+
+    def test_left(self):
+        """Testing left relationship."""
+        relationship = word_neo4j.Relationship('foo_bar', direction='left')
+        assert relationship.word == '<-[:FOO_BAR]-'
+
+    def test_empty(self):
+        """Testing empty relationship."""
+        relationship = word_neo4j.Relationship(None, direction='left')
+        assert relationship.word == '<-[]-'
+
+    def test_empty_right(self):
+        """Testing empty right relationship."""
+        relationship = word_neo4j.Relationship(None, direction='right')
+        assert relationship.word == '-[]->'
+
+
+class TestDynamicRelationship(unittest.TestCase):
+    def test_dynamic(self):
+        """Testing dynamic relationship."""
+        database = unittest.mock.Mock()
+        database.query = unittest.mock.Mock()
+        database.query.return_value = ('FOO_BAR', 'left')
+        relationship = word_neo4j.DynamicRelationship('foo', 'bar',
+                                                      database=database)
+        assert relationship.word == '<-[:FOO_BAR]-'
+        database.query.assert_called_with('foo', 'bar')
+
+
+if __name__ == '__main__':
+    pass
