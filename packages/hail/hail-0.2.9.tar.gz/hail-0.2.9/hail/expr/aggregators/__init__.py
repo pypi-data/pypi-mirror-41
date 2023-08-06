fa8e31f620fdcc4463facebe454bb036727fedca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package hail/0.2.9/hail-0.2.9.tar.gz

---
+++ packages/hail/hail-0.2.9.tar.gz/hail-0.2.9/hail/expr/aggregators/__init__.py
@@ -0,0 +1,34 @@
+from .aggregators import collect, collect_as_set, count, count_where, counter, \
+    any, all, take, min, max, sum, array_sum, mean, stats, product, fraction, \
+    hardy_weinberg_test, explode, filter, inbreeding, call_stats, info_score, \
+    hist, linreg, corr, group_by, downsample
+
+__all__ = [
+    'collect',
+    'collect_as_set',
+    'count',
+    'count_where',
+    'counter',
+    'any',
+    'all',
+    'take',
+    'min',
+    'max',
+    'sum',
+    'array_sum',
+    'mean',
+    'stats',
+    'product',
+    'fraction',
+    'hardy_weinberg_test',
+    'explode',
+    'filter',
+    'inbreeding',
+    'call_stats',
+    'info_score',
+    'hist',
+    'linreg',
+    'corr',
+    'group_by',
+    'downsample',
+]
