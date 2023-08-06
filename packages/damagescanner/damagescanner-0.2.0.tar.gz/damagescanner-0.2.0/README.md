d0b2254f79105bfd93f0e86e843a3686c4c69658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package damagescanner/0.2.0/damagescanner-0.2.0.tar.gz

---
+++ packages/damagescanner/damagescanner-0.2.0.tar.gz/damagescanner-0.2.0/README.md
@@ -0,0 +1,33 @@
+[![Build Status](https://travis-ci.com/ElcoK/DamageScanner.svg?branch=master)](https://travis-ci.com/ElcoK/DamageScanner) [![Documentation Status](https://readthedocs.org/projects/damagescanner/badge/?version=latest)](https://damagescanner.readthedocs.io/en/latest/?badge=latest) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.2551016.svg)](https://doi.org/10.5281/zenodo.2551016)
+
+
+# DamageScanner
+Python toolkit for direct damage assessments for natural disasters.
+
+Please refer to the [ReadTheDocs](http://damagescanner.readthedocs.io/) of this project for the full documentation of all functions. 
+
+**Requirements:** [NumPy](http://www.numpy.org/), [pandas](https://pandas.pydata.org/), [geopandas](http://geopandas.org/), [matplotlib](https://matplotlib.org/), [rasterio](https://github.com/mapbox/rasterio), [tqdm](https://github.com/tqdm/tqdm) 
+
+## Installation
+
+1. Open the python environment in your command prompt or bash in which you want to install this package.
+2. Type ``pip install damagescanner`` and it should install itself into your python environment.
+3. Now you can import the package like any other package!
+
+OR:
+
+1. Clone the repository or download the package on your computer and extract the folder.
+2. Go to the DamageScanner folder in your command prompt or bash.
+3. Type ``python setup.py install`` and it should install itself into your python environment.
+4. Now you can import the package like any other package!
+
+## To-do:
+* Improve ReadtheDocs.
+* Add plot capabilities.
+* Add examples.
+* Develop automated damage assessments using OpenStreetMap data.
+
+
+### License
+Copyright (C) 2019 Elco Koks. All versions released under the [MIT license](LICENSE).
+
---
+++ packages/damagescanner/damagescanner-0.2.0.tar.gz/damagescanner-0.2.0/src/damagescanner/vector.py
@@ -0,0 +1,104 @@
+import geopandas
+import pandas
+#import ogr
+import numpy 
+from tqdm import tqdm
+from shapely.wkt import loads
+
+def fetch_landuse(osm_path):
+    """
+    Function to extract land-use polygons from OpenStreetMap
+    
+    Arguments:
+        *osm_path* : file path to the .osm.pbf file of the region 
+        for which we want to do the analysis.
+        
+    Returns:
+        *GeoDataFrame* : a geopandas GeoDataFrame with all unique land-use polygons.
+    
+    """
+    
+    driver=ogr.GetDriverByName('OSM')
+    data = driver.Open(osm_path)
+                       
+    sql_lyr = data.ExecuteSQL("SELECT osm_id,landuse from multipolygons where landuse is not null")
+
+    osm_data = []
+    for feature in sql_lyr:
+            if feature.GetField('landuse') is not None:
+                shapely_geo = loads(feature.geometry().ExportToWkt()) 
+                if shapely_geo is None:
+                    continue
+                data_type=feature.GetField('landuse')
+                osm_id = feature.GetField('osm_id')
+
+                osm_data.append([osm_id,data_type,shapely_geo])
+                       
+    return geopandas.GeoDataFrame(osm_data,columns=['osm_id','landuse','geometry'],
+                            crs={'init': 'epsg:4326'})
+    
+def remove_overlap_openstreetmap(gdf):
+    """
+    Function to remove overlap in polygons in from OpenStreetMap.
+    
+    Arguments:
+        *gdf* : a geopandas GeoDataFrame with all unique land-use polygons
+        
+    Returns:
+        *GeoDataFrame* : a geopandas GeoDataFrame with (almost) non-overlapping polygons.
+    
+    """
+    
+    gdf['sq_area'] = gdf.area
+
+    new_landuse = []
+    for use in tqdm(gdf.itertuples(index=False),total=len(gdf),desc='Get unique shapes'):
+        use_geom = use.geometry
+        matches = gdf.loc[list(gdf.sindex.intersection(use.geometry.bounds))]
+        for match in matches.itertuples(index=False):
+            if use.sq_area > match.sq_area:
+                use_geom = use_geom.difference(match.geometry)
+        new_landuse.append([use.osm_id,use.landuse,use_geom])
+
+    new_gdf  =  geopandas.GeoDataFrame(pandas.DataFrame(new_landuse,columns=['osm_id','landuse','geometry'])) 
+    new_gdf.crs = {'init' : 'epsg:4326'}
+    return new_gdf
+
+
+def extract_value_other_gdf(x,gdf,col_name):
+    """
+    Function to extract value from column from other GeoDataFrame
+    
+    Arguments:
+        *x* : row of main GeoDataFrame.
+        
+        *gdf* : geopandas GeoDataFrame from which we want to extract values.
+        
+        *col_name* : the column name from which we want to get the value.
+        
+    
+    """
+    try:
+        return gdf.loc[list(gdf.sindex.intersection(x.geometry.bounds))][col_name].values[0]
+    except:
+        return None
+
+def get_losses(x,damage_curves,damage_values):
+    """
+    Function to estimate the damages.
+    
+    Arguments:
+        *x* : row of main GeoDataFrame
+        
+        *damage_curves*: pandas DataFrame of curves. Inundation depths should be the index.
+        
+        *damage_values*: dictionary with maximum damage values.
+        
+    Returns:
+        
+        Total damage for the given land-use object.
+    
+    """
+    
+    return numpy.interp(x.raster_val,list(damage_curves.index),list(damage_curves[x.landuse]))*damage_values[x.landuse]
+    
