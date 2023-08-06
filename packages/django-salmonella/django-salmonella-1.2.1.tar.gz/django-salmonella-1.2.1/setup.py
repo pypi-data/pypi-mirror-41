7e51cc8c37970d169fc488ecb78456062a2b6450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:24 +0000

    Add package django-salmonella/1.2.1/django-salmonella-1.2.1.tar.gz

---
+++ packages/django-salmonella/django-salmonella-1.2.1.tar.gz/django-salmonella-1.2.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='django-salmonella',
+    version='1.2.1',
+    author='Lincoln Loop: Seth Buntin, Yann Malet',
+    author_email='info@lincolnloop.com',
+    description=("django-salmonella was renamed to django-dynamic-raw-id! "
+                 "raw_id_fields widget replacement that handles display of an object's "
+                 "string value on change and can be overridden via a template."),
+    long_description=open('README.rst').read(),
+    url='https://pypi.org/project/django-dynamic-raw-id/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    packages=find_packages(),
+    package_data={
+        'salmonella': ['templates/*.*', 'static/*.*'],
+        'docs': ['*'],
+    },
+    include_package_data=True,
+    install_requires=[
+        'django>=1.8',
+    ],
+)
