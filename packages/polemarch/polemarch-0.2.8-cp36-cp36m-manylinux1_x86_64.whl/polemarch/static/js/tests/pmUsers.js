3188bde1b03a0266f3a0d9aec2fcef484836683d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package polemarch/0.2.8/polemarch-0.2.8-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/polemarch/polemarch-0.2.8-cp36-cp36m-manylinux1_x86_64.whl/polemarch/static/js/tests/pmUsers.js
@@ -0,0 +1,12 @@
+window.qunitTestsArray['guiPaths.profile/settings']={test:function()
+{let path="profile/settings"
+guiTests.openPage(path)
+guiTests.hasElement(1,".btn_save",path)
+guiTests.hasEditButton(false)
+guiTests.hasAddButton(0,path)
+guiTests.hasElement(1,".gui-field-chartLineSettings",path)
+guiTests.hasAddButton(0,path)
+$(".btn_save").trigger('click')}};window.qunitTestsArray['guiPaths.user.user_settings']={test:function()
+{let env={};let pk_obj={};let path='/user/new/';let password=rundomString(6);let fieldsData={username:{value:rundomString(6)},email:{value:rundomString(6)+"@gmail.com"},password:{value:password,do_not_compare:true},password2:{value:password,do_not_compare:true},};guiTests.openPage(path)
+guiTests.setValuesAndCreate(path,fieldsData,(data)=>{env.objectId=data.id;pk_obj.api_pk=data.id},true)
+guiTests.openPage("/user/{pk}/settings/",env,(env)=>{return vstMakeLocalApiUrl("/user/{pk}/settings/",pk_obj);});guiTests.updateObject("/user/{pk}/settings/",{autoupdateInterval:{value:5000}},true);guiTests.deleteObjByPath("/user/{pk}/",env,pk_obj);}};
