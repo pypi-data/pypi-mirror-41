de44708543bba8d075b9f6d922035de54ee398a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:30 +0000

    Add package zalgoify/1.0/zalgoify-1.0.tar.gz

---
+++ packages/zalgoify/zalgoify-1.0.tar.gz/zalgoify-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'zalgoify',
+    packages = ['zalgoify'], # this must be the same as the name above
+    version = '1.0',
+    description = 'A simple library for processing zalgo text',
+    author = 'Erik Boesen',
+    author_email = 'me@erikboesen.com',
+    url = 'https://github.com/ErikBoesen/zalgoify',
+    keywords = ['diacritic', 'filter', 'zalgo', 'he is coming', 'ahhh'],
+    classifiers = [],
+)
