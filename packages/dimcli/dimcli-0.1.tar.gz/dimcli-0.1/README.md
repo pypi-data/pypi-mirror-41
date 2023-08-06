0c04dc12ceddd4383917018ef6139183cf5c2de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package dimcli/0.1/dimcli-0.1.tar.gz

---
+++ packages/dimcli/dimcli-0.1.tar.gz/dimcli-0.1/README.md
@@ -0,0 +1,25 @@
+# dimcli
+
+> STILL IN DEVELOPMENT
+
+Python API for accessing dimensions.ai
+
+-   [https://app.dimensions.ai](https://app.dimensions.ai)
+-   [https://pypi.org/project/dimcli/](https://pypi.org/project/dimcli/)
+-   [https://app.dimensions.ai/dsl](https://app.dimensions.ai/dsl)
+
+### Install
+
+```
+pip install dimcli
+```
+
+### Develop
+
+Note: requires [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/).
+
+```
+$ mkvirtualenv dimcli
+$ pip install --editable .
+$ ./run-shell # iPython with library preloaded so you can play with it!
+```
---
+++ packages/dimcli/dimcli-0.1.tar.gz/dimcli-0.1/dimcli/cli.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import click
+from pprint import pprint
+from .core.lib import *
+from .core import repl
+from .core import credentials
+
+from .VERSION import *
+
+# HOW TO SET UP A CONFIGURATION FILE:
+
+# Add a '.dimcli.config.json' file to your home folder ('~').
+# The file should have the following structure:
+
+#     {
+#         "usr": "your-username",
+#         "psw" : "your-password",
+#         "service" : "https://app.dimensions.ai/api/"   # default
+#     }
+
+
+def help_interpret_args(args):
+    if len(args) == 1:
+        return args[0]
+    else:
+        return " ".join([x for x in args])
+
+
+@click.command()
+@click.argument("args", nargs=-1)
+@click.option(
+    "--register",
+    is_flag=True,
+    help="Store Dimensions user and password on this computer",
+)
+@click.pass_context
+def main_cli(ctx, args=None, register=False):
+    """
+    dimcli: client for the dimensions.ai
+    More info: https://docs.dimensions.ai/dsl/index.html
+    """
+
+    click.secho("dimcli " + VERSION, dim=True)
+    click.secho("------------", fg="white")
+
+    data = credentials.get_credentials()
+    if not data or register:
+        click.secho(
+            "Please set up a Dimensions account (this data will be stored in a hidden file in your home folder)",
+            fg="green",
+        )
+        credentials.register_credentials()
+        data = credentials.get_credentials()
+
+    # unique functionality > launch REPL
+    repl.main(data)
+
+
+if __name__ == "__main__":
+    main_cli()
