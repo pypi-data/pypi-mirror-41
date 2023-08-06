d5d5ae1250e64bac682675f9ce2f43001172812f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:35 +0000

    Add package typedargs/0.13.5/typedargs-0.13.5.tar.gz

---
+++ packages/typedargs/typedargs-0.13.5.tar.gz/typedargs-0.13.5/typedargs/types/bool.py
@@ -0,0 +1,36 @@
+# This file is adapted from python code released by WellDone International
+# under the terms of the LGPLv3.  WellDone International's contact information is
+# info@welldone.org
+# http://welldone.org
+#
+# Modifications to this file from the original created at WellDone International
+# are copyright Arch Systems Inc.
+
+# pylint: disable=unused-argument,missing-docstring
+
+# bool.py
+# Simple boolean type
+
+from builtins import str
+from past.builtins import basestring
+
+
+def convert(arg, **kwargs):
+    if arg is None:
+        return arg
+
+    if isinstance(arg, basestring):
+        comp = str(arg.lower())
+
+        if comp == u'true':
+            return True
+        if comp == u'false':
+            return False
+
+        raise ValueError("Unknown boolean value (should be true or false): %s" % arg)
+
+    return bool(arg)
+
+
+def default_formatter(arg, **kwargs):
+    return str(arg)
