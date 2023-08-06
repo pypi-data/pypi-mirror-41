f26179bd448604035b69f71497054d31da2f0c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:28 +0000

    Add package pulp-cookbook/0.0.4b1/pulp_cookbook-0.0.4b1-py3-none-any.whl

---
+++ packages/pulp-cookbook/pulp_cookbook-0.0.4b1-py3-none-any.whl/pulp_cookbook/app/serializers.py
@@ -0,0 +1,90 @@
+# (C) Copyright 2018 Simon Baatz <gmbnomis@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+from gettext import gettext as _
+
+from rest_framework import serializers
+
+from pulpcore.plugin.models import ContentArtifact
+
+from pulpcore.plugin.serializers import (
+    SingleArtifactContentSerializer, RemoteSerializer, PublisherSerializer
+)
+
+from .models import CookbookPackageContent, CookbookRemote, CookbookPublisher
+
+
+class CookbookPackageContentSerializer(SingleArtifactContentSerializer):
+    """Serializer for the cookbook content."""
+
+    name = serializers.CharField(
+        help_text=_("name of the cookbook")
+    )
+    version = serializers.CharField(
+        help_text=_("version of the cookbook"),
+        required=False
+    )
+    dependencies = serializers.JSONField(
+        help_text=_("dependencies of the cookbook"),
+        read_only=True
+    )
+    content_id_type = serializers.HiddenField(default=CookbookPackageContent.SHA256)
+    content_id = serializers.CharField(
+        help_text=_(
+            "content_id of the cookbook (UUID (lazy download)/SHA256 (immediate download/import)"
+        ),
+        read_only=True
+    )
+
+    def create(self, validated_data):
+        content_data = {k: v for k, v in validated_data.items() if k != '_artifact'}
+        content = super().create(content_data)
+        ContentArtifact.objects.create(
+            artifact=validated_data['_artifact'],
+            content=content,
+            relative_path=content.relative_path(),
+        )
+        return content
+
+    def update(self, instance, validated_data):
+        raise serializers.ValidationError("content is immutable")
+
+    class Meta:
+        fields = SingleArtifactContentSerializer.Meta.fields + (
+            'name', 'version', 'dependencies', 'content_id_type', 'content_id'
+        )
+        model = CookbookPackageContent
+
+
+class CookbookRemoteSerializer(RemoteSerializer):
+    """Serializer for the remote pointing to a universe repo."""
+
+    cookbooks = serializers.JSONField(
+        help_text=_('An optional JSON object in the format {"<cookbook name>":'
+                    ' "<version_string>" }. Used to limit the cookbooks to synchronize'
+                    ' from the remote'),
+        required=False
+    )
+
+    class Meta:
+        fields = RemoteSerializer.Meta.fields + ('cookbooks', )
+        model = CookbookRemote
+
+    # TODO: No support for version specifiers yet, only cookbook names
+    def validate_cookbooks(self, value):
+        if value == "":  # blank value
+            return value
+        if isinstance(value, dict):
+            if all(value.keys()):
+                return value
+        raise serializers.ValidationError(
+            _('Format must be {"<cookbook_name>" : "version_string" }')
+        )
+
+
+class CookbookPublisherSerializer(PublisherSerializer):
+    """Serializer for the publisher."""
+
+    class Meta:
+        fields = PublisherSerializer.Meta.fields
+        model = CookbookPublisher
