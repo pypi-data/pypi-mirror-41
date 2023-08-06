b3750bdb08a0ef96e58ad1e97c8cf118bd0b21fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:50 +0000

    Add package paicli/0.5.2/paicli-0.5.2.tar.gz

---
+++ packages/paicli/paicli-0.5.2.tar.gz/paicli-0.5.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paicli',
+    version="0.5.2",
+    description='Client for PAI',
+    author='Sotetsu KOYAMADA',
+    url='',
+    author_email='koyamada-s@sys.i.kyoto-u.ac.jp',
+    license='MIT',
+    install_requires=["requests",
+                      "pyyaml",
+                      "prompt_toolkit==1.0.9",
+                      "prettytable",
+                      "termcolor",
+                      "colorama",
+                      "click",
+                      ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': 'pai = paicli.main:main'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
