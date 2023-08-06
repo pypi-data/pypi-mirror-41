f7eec8da23e8d0b647c7379dcf5c9dc793a1f0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package DomainThesaurus/1.0.8/DomainThesaurus-1.0.8.tar.gz

---
+++ packages/DomainThesaurus/DomainThesaurus-1.0.8.tar.gz/DomainThesaurus-1.0.8/DST/__init__.py
@@ -0,0 +1,9 @@
+from . import datasets
+from . import domain_term
+from . import domain_thesaurus
+from . import phrase_detection
+from . import semantic_related_word
+from . import synonym_group
+from . import utils
+from . import word_classification
+
