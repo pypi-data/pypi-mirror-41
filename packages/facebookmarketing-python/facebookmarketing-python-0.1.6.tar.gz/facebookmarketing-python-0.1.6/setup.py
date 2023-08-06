5a6717a31dceebc10a225ad375f489b6d9dde4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:01 +0000

    Add package facebookmarketing-python/0.1.6/facebookmarketing-python-0.1.6.tar.gz

---
+++ packages/facebookmarketing-python/facebookmarketing-python-0.1.6.tar.gz/facebookmarketing-python-0.1.6/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='facebookmarketing-python',
+      version='0.1.6',
+      description='API wrapper for Facebook written in Python',
+      long_description=read('README.md'),
+      url='https://github.com/GearPlug/facebookmarketing-python',
+      author='Miguel Ferrer',
+      author_email='ingferrermiguel@gmail.com',
+      license='GPL',
+      packages=['facebookmarketing'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
