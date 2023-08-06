a06a4311cd9d12dcd1ef184a42a8fb80f66c2830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:14 +0000

    Add package kinto/13.0.1/kinto-13.0.1.tar.gz

---
+++ packages/kinto/kinto-13.0.1.tar.gz/kinto-13.0.1/kinto/plugins/accounts/authentication.py
@@ -0,0 +1,60 @@
+import bcrypt
+from pyramid import authentication as base_auth
+
+from kinto.core import utils
+from kinto.core.storage import exceptions as storage_exceptions
+
+from .utils import ACCOUNT_CACHE_KEY, ACCOUNT_POLICY_NAME
+
+
+def account_check(username, password, request):
+    settings = request.registry.settings
+    hmac_secret = settings["userid_hmac_secret"]
+    cache_key = utils.hmac_digest(hmac_secret, ACCOUNT_CACHE_KEY.format(username))
+    cache_ttl = int(settings.get("account_cache_ttl_seconds", 30))
+    hashed_password = utils.hmac_digest(cache_key, password)
+
+    # Check cache to see whether somebody has recently logged in with the same
+    # username and password.
+    cache = request.registry.cache
+    cache_result = cache.get(cache_key)
+
+    # Username and password have been verified previously. No need to compare hashes
+    if cache_result == hashed_password:
+        # Refresh the cache TTL.
+        cache.expire(cache_key, cache_ttl)
+        return True
+
+    # Back to standard procedure
+    parent_id = username
+    try:
+        existing = request.registry.storage.get(
+            parent_id=parent_id, resource_name="account", object_id=username
+        )
+    except storage_exceptions.ObjectNotFoundError:
+        return None
+
+    hashed = existing["password"].encode(encoding="utf-8")
+    pwd_str = password.encode(encoding="utf-8")
+    # Check if password is valid (it is a very expensive computation)
+    if bcrypt.checkpw(pwd_str, hashed):
+        cache.set(cache_key, hashed_password, ttl=cache_ttl)
+        return True
+
+
+class AccountsAuthenticationPolicy(base_auth.BasicAuthAuthenticationPolicy):
+    """Accounts authentication policy.
+
+    It will check that the credentials exist in the account resource.
+    """
+
+    name = ACCOUNT_POLICY_NAME
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(account_check, *args, **kwargs)
+
+    def effective_principals(self, request):
+        # Bypass default Pyramid construction of principals because
+        # Pyramid multiauth already adds userid, Authenticated and Everyone
+        # principals.
+        return []
---
+++ packages/kinto/kinto-13.0.1.tar.gz/kinto-13.0.1/kinto/plugins/admin/build/precache-manifest.854f1ca2e84cfb4bce3631ecec237d58.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "baf68a9cdc4486715fc3",
+    "url": "/v1/admin/static/js/runtime~main.baf68a9c.js"
+  },
+  {
+    "revision": "78ac4ef94718f2747516",
+    "url": "/v1/admin/static/js/main.78ac4ef9.chunk.js"
+  },
+  {
+    "revision": "9ebd0c891097825c2637",
+    "url": "/v1/admin/static/js/1.9ebd0c89.chunk.js"
+  },
+  {
+    "revision": "9ebd0c891097825c2637",
+    "url": "/v1/admin/static/css/1.66045a18.chunk.css"
+  },
+  {
+    "revision": "d444f5d091b5cdac1a4250d4d6af2107",
+    "url": "/v1/admin/index.html"
+  }
+];
