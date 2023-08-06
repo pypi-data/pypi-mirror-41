3208ba7a4a8acb21afb68e4cbd6a7ac769486f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package xbos-services-utils2/0.0.4.dev0/xbos-services-utils2-0.0.4.dev0.tar.gz

---
+++ packages/xbos-services-utils2/xbos-services-utils2-0.0.4.dev0.tar.gz/xbos-services-utils2-0.0.4.dev0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='xbos-services-utils2',
+    version='0.0.4dev',
+    packages=find_packages(),
+    include_package_data=True,
+    # license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='Daniel Lengyel',
+	author_email='daniel.lengyel@berkeley.edu',
+    description="Utility functions for xbos services.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+    	"pandas == 0.22.0",
+    	"numpy == 1.14.0",
+    	"pytz == 2018.3",
+        "PyYAML==3.12",
+    ],
+)
