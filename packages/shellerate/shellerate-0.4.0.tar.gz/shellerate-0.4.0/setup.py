213d27da8c85ecfcad8b2430dd5168729eab5023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:07 +0000

    Add package shellerate/0.4.0/shellerate-0.4.0.tar.gz

---
+++ packages/shellerate/shellerate-0.4.0.tar.gz/shellerate-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shellerate",
+    version="0.4.0",
+    python_requires='>=3.6.*',
+    author="Paolo Perego",
+    author_email="paolo@armoredcode.com",
+    description="A shellcode generator with encryption, encoding and polymorphism facilities built-in",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thesp0nge/shellerate",
+    packages=["shellerate"],
+    scripts=["bin/shellerate"],
+    #install_requires=["binutils"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
