1827d319649976ce55a8d8bb58c5f4dbb374f4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:22 +0000

    Add package odoo10-addon-donation-thanks/10.0.1.0.0.99.dev7/odoo10_addon_donation_thanks-10.0.1.0.0.99.dev7-py2-none-any.whl

---
+++ packages/odoo10-addon-donation-thanks/odoo10_addon_donation_thanks-10.0.1.0.0.99.dev7-py2-none-any.whl/odoo/addons/donation_thanks/models/donation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Barroux Abbey (http://www.barroux.org)
+# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class DonationDonation(models.Model):
+    _inherit = "donation.donation"
+
+    thanks_printed = fields.Boolean(
+        string='Thanks Printed',
+        help="This field automatically becomes active when "
+        "the thanks letter has been printed.")
+
+    def print_thanks(self):
+        self.ensure_one()
+        self.thanks_printed = True
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'donation.thanks',
+            'datas': {'model': 'donation.donation', 'ids': self.ids},
+            }
