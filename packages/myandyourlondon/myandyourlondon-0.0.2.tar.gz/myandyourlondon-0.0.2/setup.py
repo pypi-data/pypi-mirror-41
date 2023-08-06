3dc2f3283263b8e8af9180a0be67166297440d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package myandyourlondon/0.0.2/myandyourlondon-0.0.2.tar.gz

---
+++ packages/myandyourlondon/myandyourlondon-0.0.2.tar.gz/myandyourlondon-0.0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="myandyourlondon",
+    version='0.0.2',
+    description='A Small example package',
+    author='rakens2',
+    packages=setuptools.find_packages()
+)
