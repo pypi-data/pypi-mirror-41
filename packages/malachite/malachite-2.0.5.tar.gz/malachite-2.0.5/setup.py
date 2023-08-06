09603a104b1b78e6a714856ce082b81ffc7c6132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:33 +0000

    Add package malachite/2.0.5/malachite-2.0.5.tar.gz

---
+++ packages/malachite/malachite-2.0.5.tar.gz/malachite-2.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+# This call to setup() does all the work
+setup(
+    name="malachite",
+    version="2.0.5",
+    description="A Gene Enrichment Meta-Analysis (GEM) Tool for ToppGene",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ggersh/malachite-final",
+    author="Gregory R. Gershkowitz, Zachary B. Abrams, Kevin R. Coombes",
+    author_email="kevin.coombes@osumc.edu",
+    # license="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["bs4", "mechanize", "requests","lxml","xlrd"],
+    entry_points={
+        "console_scripts": [
+            "malachite=malachite.__main__:main",
+        ]
+    },
+)
