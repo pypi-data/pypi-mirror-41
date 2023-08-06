6eacdef192af104c3759beeeddbc39a5df1c2485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package dj-authentication/0.1.1/dj-authentication-0.1.1.tar.gz

---
+++ packages/dj-authentication/dj-authentication-0.1.1.tar.gz/dj-authentication-0.1.1/dj_authentication/methods/basic.py
@@ -0,0 +1,28 @@
+from base64 import b64decode
+
+from django.conf import settings
+from django.contrib.auth import authenticate
+
+
+def get_user(request):
+	params = dict(realm = settings.BASE_URL)
+
+	credentials = request.http_auth('Basic', params)
+	if credentials is not None:
+		uname, passwd = b64decode(credentials.encode('ascii')).decode('utf-8').split(':')
+		user = authenticate(request = request, username = uname, password = passwd)
+		if user:
+			return user
+
+
+ClientSecretBasicAuth = get_user
+
+
+def ClientSecretPostAuth(request):
+	client_id = request.POST.get('client_id')
+	client_secret = request.POST.get('client_secret')
+
+	if client_id and client_secret:
+		user = authenticate(request = request, username = client_id, password = client_secret)
+		if user:
+			return user
