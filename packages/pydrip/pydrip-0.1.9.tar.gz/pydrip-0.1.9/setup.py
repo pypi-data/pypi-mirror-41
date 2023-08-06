40ebdabab21385f3a783c09078808e6bbdc2607d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:59 +0000

    Add package pydrip/0.1.9/pydrip-0.1.9.tar.gz

---
+++ packages/pydrip/pydrip-0.1.9.tar.gz/pydrip-0.1.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+     name='pydrip',
+     version='0.1.9',
+     author="Matthew Clarkson",
+     author_email="mpclarkson@gmail.com",
+     description="A Python 3 client for the Drip API.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/flypaperplanes/pydrip",
+     packages=['pydrip'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=[
+          'requests',
+      ],
+ )
