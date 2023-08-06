2f88e1311a1eceab3c63b155853eb2a2d7dedc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package django-binary-database-files/1.0.3/django-binary-database-files-1.0.3.tar.gz

---
+++ packages/django-binary-database-files/django-binary-database-files-1.0.3.tar.gz/django-binary-database-files-1.0.3/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages, Command
+
+import binary_database_files
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def get_reqs(*fns):
+    lst = []
+    for fn in fns:
+        for package in open(os.path.join(CURRENT_DIR, fn)).readlines():
+            package = package.strip()
+            if not package:
+                continue
+            lst.append(package.strip())
+    return lst
+
+try:
+    long_description = read_md('README.md')
+except:
+    long_description = ''
+
+setup(
+    name='django-binary-database-files',
+    version=binary_database_files.__version__,
+    description='A storage system for Django that stores uploaded files in both the database and file system.',
+    long_description=long_description,
+    author='Johannes Wilm',
+    author_email='mail@johanneswilm.org',
+    url='http://github.com/johanneswilm/django-binary-database-files',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=get_reqs('pip-requirements-min-django.txt', 'pip-requirements.txt'),
+    tests_require=get_reqs('pip-requirements-test.txt'),
+)
