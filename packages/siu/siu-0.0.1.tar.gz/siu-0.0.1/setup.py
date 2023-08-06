89864e79145dbd5e70d56c78156aded3b9fdb3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:39 +0000

    Add package siu/0.0.1/siu-0.0.1.tar.gz

---
+++ packages/siu/siu-0.0.1.tar.gz/siu-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='siu',
+    packages=find_packages(),
+    version='0.0.1',
+    description='small, symbolic python expressions',
+    author='Michael Chow',
+    license='BSD',
+    author_email='mc_al_gh_siuba@fastmail.com',
+    url='https://github.com/machow/siu',
+    keywords=['package', ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
+
