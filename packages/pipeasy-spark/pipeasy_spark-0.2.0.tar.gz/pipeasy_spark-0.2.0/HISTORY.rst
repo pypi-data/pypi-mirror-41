3839a269ed8788a8d49504afbfaa3779c4efe589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:08 +0000

    Add package pipeasy-spark/0.2.0/pipeasy_spark-0.2.0.tar.gz

---
+++ packages/pipeasy-spark/pipeasy_spark-0.2.0.tar.gz/pipeasy_spark-0.2.0/HISTORY.rst
@@ -0,0 +1,13 @@
+=======
+History
+=======
+
+0.1.2 (2018-10-12)
+------------------
+
+* I am still learning how all these tools interact with each other
+
+0.1.1 (2018-10-12)
+------------------
+
+* First release on PyPI.
---
+++ packages/pipeasy-spark/pipeasy_spark-0.2.0.tar.gz/pipeasy_spark-0.2.0/pipeasy_spark/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pipeasy-spark.
+
+The pipeasy-spark package provides a set of convenience functions that
+make it easier to map each column of a Spark dataframe (or subsets of columns) to
+user-specified transformations.
+"""
+
+__author__ = """Quantmetry"""
+__email__ = 'bhabert@quantmetry.com'
+__version__ = '0.2.0'
+
+from .core import build_pipeline
+from .convenience import (
+    build_pipeline_by_dtypes,
+    build_default_pipeline,
+)
+
+
+__all__ = [
+    'build_pipeline',
+    'build_pipeline_by_dtypes',
+    'build_default_pipeline',
+]
