db4e8aa1e481f735f2112f344399125410233195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package hyphe-traph/1.1.0/hyphe_traph-1.1.0-py2-none-any.whl

---
+++ packages/hyphe-traph/hyphe_traph-1.1.0-py2-none-any.whl/traph/link_store/link_store.py
@@ -0,0 +1,176 @@
+# =============================================================================
+# Link Store Class
+# =============================================================================
+#
+# Class representing the structure storing the links as linked list of stubs.
+#
+from itertools import chain
+from traph.link_store.node import LinkStoreNode, LINK_STORE_FIRST_DATA_BLOCK
+from traph.link_store.header import LinkStoreHeader, LINK_STORE_HEADER_BLOCKS
+
+
+# Exceptions
+class LinkStoreTraversalException(Exception):
+    pass
+
+
+# Main class
+class LinkStore(object):
+
+    # =========================================================================
+    # Constructor
+    # =========================================================================
+    def __init__(self, storage):
+
+        # Properties
+        self.storage = storage
+
+        # Reading headers
+        self.header = LinkStoreHeader(storage)
+
+    # =========================================================================
+    # Read methods
+    # =========================================================================
+
+    # Method returning a node
+    def node(self, **kwargs):
+        return LinkStoreNode(self.storage, **kwargs)
+
+    # Method returning the root
+    def root(self):
+        return self.node(block=LINK_STORE_FIRST_DATA_BLOCK)
+
+    # =========================================================================
+    # Mutation methods
+    # =========================================================================
+
+    # TODO: not used for the time being
+    def add_link(self, source_node, target_block, out=True):
+
+        # If the node does not have outlinks yet
+        if not source_node.has_links(out=out):
+            link_node = self.node()
+            link_node.set_target(target_block)
+            link_node.write()
+
+            source_node.set_links(link_node.block, out=out)
+            source_node.write()
+
+            return
+
+        # Else:
+        link_node = self.node(block=source_node.links(out=out))
+
+        if not link_node.exists:
+            raise LinkStoreTraversalException('Block does not exist.')
+
+        # We go through the linked list to find matching link
+        while link_node.target() != target_block and link_node.has_next():
+            link_node.read_next()
+
+        # If we did not find a matching link, we add it
+        if link_node.target() != target_block:
+            sibling = self.node()
+            sibling.set_target(target_block)
+            sibling.write()
+            link_node.set_next(sibling.block)
+            link_node.write()
+
+        # Else we just increment the weight of the existing one
+        else:
+            link_node.increment_weight()
+            link_node.write()
+
+    def add_links(self, source_node, target_blocks, out=True):
+        target_blocks = iter(target_blocks)
+        links_block = source_node.links(out=out)
+
+        try:
+            first_target_block = next(target_blocks)
+        except StopIteration:
+            return
+
+        # If the node does not have outlinks yet
+        if not links_block:
+            link_node = self.node()
+            link_node.set_target(first_target_block)
+            link_node.write()
+
+            links_block = link_node.block
+            source_node.set_links(link_node.block, out=out)
+            source_node.write()
+
+            first_target_block = None
+
+        # Finding the current
+        link_nodes_index = {}
+        last_link_node = None
+
+        for link_node in self.link_nodes_iter(links_block):
+            link_nodes_index[link_node.target()] = link_node
+            last_link_node = link_node
+
+        if first_target_block:
+            target_blocks = chain([first_target_block], target_blocks)
+
+        # Adding new targets
+        # TODO: possible to virtualize chain and flush later to ease writes
+        for target_block in target_blocks:
+
+            if target_block in link_nodes_index:
+                link_node = link_nodes_index[target_block]
+                link_node.increment_weight()
+                link_node.write()
+            else:
+                link_node = self.node()
+                link_node.set_target(target_block)
+                link_node.write()
+
+                link_nodes_index[target_block] = link_node
+
+                last_link_node.set_next(link_node.block)
+                last_link_node.write()
+                last_link_node = link_node
+
+    def add_outlinks(self, source_node, target_blocks):
+        return self.add_links(source_node, target_blocks, out=True)
+
+    def add_inlinks(self, source_node, target_blocks):
+        return self.add_links(source_node, target_blocks, out=False)
+
+    # =========================================================================
+    # Iteration methods
+    # =========================================================================
+    def nodes_iter(self):
+        node = self.root()
+
+        while node.exists:
+            yield node
+            node.read(node.block + self.storage.block_size)
+
+    def link_nodes_iter(self, block):
+        node = self.node(block=block)
+
+        if not node.exists:
+            raise LinkStoreTraversalException('Block does not exist.')
+
+        yield node
+
+        while node.has_next():
+            node.read_next()
+            yield node
+
+    # =========================================================================
+    # Counting methods
+    # =========================================================================
+    def count_links(self):
+        blocks = self.storage.count_blocks() - LINK_STORE_HEADER_BLOCKS
+
+        return blocks / 2
+
+    def metrics(self):
+        stats = {
+            'nb_links': self.count_links()
+        }
+
+        return stats
---
+++ packages/hyphe-traph/hyphe_traph-1.1.0-py2-none-any.whl/traph/lru_trie/header.py
@@ -0,0 +1,102 @@
+# =============================================================================
+# LRU Trie Header
+# =============================================================================
+#
+# Class representing the header of the LRU Trie buffer. This header can be
+# used to store various metadata and/or state.
+#
+import struct
+
+# Binary format
+# -
+# NOTE: Since python mimics C struct, the block size should be respecting
+# some rules (namely have even addresses or addresses divisble by 4 on some
+# architecture).
+LRU_TRIE_HEADER_FORMAT = 'I124x'
+LRU_TRIE_HEADER_BLOCK_SIZE = struct.calcsize(LRU_TRIE_HEADER_FORMAT)
+
+# Header blocks
+# -
+# We are retaining at least one header block so we can keep the 0 block address
+# as a NULL pointer and be able to store some metadata about the structure.
+LRU_TRIE_HEADER_BLOCKS = 1
+
+# Positions
+LRU_TRIE_HEADER_LAST_WEBENTITY_ID = 0
+
+
+# Main class
+class LRUTrieHeader(object):
+
+    # =========================================================================
+    # Constructor
+    # =========================================================================
+    def __init__(self, storage):
+
+        # Properties
+        self.storage = storage
+        self.data = [
+            0  # Last webentity id
+        ]
+
+        self.__ensure()
+        self.read()
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+
+        return (
+            '<%(class_name)s'
+            ' last_webentity_id=%(last_webentity_id)s>'
+        ) % {
+            'class_name': class_name,
+            'last_webentity_id': self.last_webentity_id()
+        }
+
+    def __ensure(self):
+        block = 0
+
+        empty_data = struct.pack(LRU_TRIE_HEADER_FORMAT, 0)
+
+        while block < LRU_TRIE_HEADER_BLOCKS:
+            data = self.storage.read(block)
+
+            if not data:
+                self.storage.write(empty_data, block)
+
+            block += self.storage.block_size
+
+    # =========================================================================
+    # Utilities
+    # =========================================================================
+
+    # Method used to unpack data
+    def unpack(self, data):
+        return list(struct.unpack(LRU_TRIE_HEADER_FORMAT, data))
+
+    # Method used to set a switch to another block
+    def read(self):
+        self.data = self.unpack(self.storage.read(0))
+
+    # Method used to pack the node to binary form
+    def pack(self):
+        return struct.pack(LRU_TRIE_HEADER_FORMAT, *self.data)
+
+    # Method used to write the node's data to storage
+    def write(self):
+        self.storage.write(self.pack(), 0)
+
+    # =========================================================================
+    # Getters/Setters
+    # =========================================================================
+    def last_webentity_id(self):
+        return self.data[LRU_TRIE_HEADER_LAST_WEBENTITY_ID]
+
+    def set_last_webentity_id(self, last_id):
+        self.data[LRU_TRIE_HEADER_LAST_WEBENTITY_ID] = last_id
+
+    def increment_last_webentity_id_by(self, number):
+        self.data[LRU_TRIE_HEADER_LAST_WEBENTITY_ID] += number
+
+    def increment_last_webentity_id(self):
+        self.data[LRU_TRIE_HEADER_LAST_WEBENTITY_ID] += 1
