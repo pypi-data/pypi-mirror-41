e60a5b2a119d421832a7aa3250213dd970c7d1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:40 +0000

    Add package suntime/1.2.1/suntime-1.2.1.tar.gz

---
+++ packages/suntime/suntime-1.2.1.tar.gz/suntime-1.2.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import suntime
+
+setup(name='suntime',
+      version=suntime.__version__,
+      description='Simple sunset and sunrise time calculation python library',
+      author=suntime.__author__,
+      author_email=suntime.__email__,
+      url='https://github.com/SatAgro/suntime',
+      copyright='Copyright 2019 SatAgro',
+      packages=['suntime'], install_requires=['python-dateutil']
+      )
