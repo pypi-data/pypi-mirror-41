917f2bc9a945f0c54e776a357b5efe8114ecc7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package mob-assistant/0.2.0/mob-assistant-0.2.0.tar.gz

---
+++ packages/mob-assistant/mob-assistant-0.2.0.tar.gz/mob-assistant-0.2.0/README.md
@@ -0,0 +1,2 @@
+# Mob Assistant
+[![Build Status](https://travis-ci.org/ddrexl/mob-assistant.svg?branch=master)](https://travis-ci.org/ddrexl/mob-assistant)
---
+++ packages/mob-assistant/mob-assistant-0.2.0.tar.gz/mob-assistant-0.2.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['mob_assistant']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mob-assistant',
+    'version': '0.2.0',
+    'description': 'An assistant for mob programming.',
+    'long_description': '# Mob Assistant\n[![Build Status](https://travis-ci.org/ddrexl/mob-assistant.svg?branch=master)](https://travis-ci.org/ddrexl/mob-assistant)\n',
+    'author': 'Dominik Drexl',
+    'author_email': 'domdrexl@gmail.com',
+    'url': 'https://github.com/ddrexl/mob-assistant',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
