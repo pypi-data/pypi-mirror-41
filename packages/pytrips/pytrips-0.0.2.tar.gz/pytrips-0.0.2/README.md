8b3920d2ae3914a9bbdcbe0975af9af15bca9bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:33 +0000

    Add package pytrips/0.0.2/pytrips-0.0.2.tar.gz

---
+++ packages/pytrips/pytrips-0.0.2.tar.gz/pytrips-0.0.2/README.md
@@ -0,0 +1,16 @@
+# PyTrips
+
+[![Build Status](https://travis-ci.com/mrmechko/pytrips.svg?branch=master)](https://travis-ci.com/mrmechko/pytrips)
+
+PyTrips provides a python interface to interacting with the TRIPS ontology and parser.
+
+
+# Installation
+
+```
+git clone git://github.com/mrmechko/pytrips
+cd pytrips
+pip install -e .
+```
+
+Details pending.
---
+++ packages/pytrips/pytrips-0.0.2.tar.gz/pytrips-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class EnsureWordnet(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        import nltk
+        print("Now installing wordnet")
+        nltk.download("wordnet")
+        nltk.download("stopwords")
+        install.run(self)
+
+if __name__ == '__main__':
+    setup(
+        name="pytrips",
+        version="0.0.2",
+        author="Rik Bose",
+        author_email="rbose@cs.rochester.edu",
+        description="A simple python package for accessing the trips ontology and lexicon",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/mrmechko/pytrips",
+        packages=find_packages(exclude=["test"]),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=["nltk"],
+        cmdclass={
+            "install": EnsureWordnet,
+            "develop": EnsureWordnet
+        },
+    )
