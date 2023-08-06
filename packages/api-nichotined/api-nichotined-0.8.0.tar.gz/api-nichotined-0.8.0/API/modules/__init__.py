2e9a38816afc76427665278de3c3f285704e3133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package api-nichotined/0.8.0/api-nichotined-0.8.0.tar.gz

---
+++ packages/api-nichotined/api-nichotined-0.8.0.tar.gz/api-nichotined-0.8.0/API/modules/__init__.py
@@ -0,0 +1,15 @@
+from .baseApi import BaseApi
+from .delete import Delete
+from .get import Get
+from .patch import Patch
+from .post import Post
+from .put import Put
+
+__all__ = [
+    "BaseApi",
+    "Post",
+    "Get",
+    "Put",
+    "Delete",
+    "Patch"
+]
