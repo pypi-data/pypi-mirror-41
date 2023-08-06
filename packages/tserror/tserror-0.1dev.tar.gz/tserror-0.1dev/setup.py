9a0c924e9f497ef229673ba6015e75b091e8ddf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:21 +0000

    Add package tserror/0.1dev/tserror-0.1dev.tar.gz

---
+++ packages/tserror/tserror-0.1dev.tar.gz/tserror-0.1dev/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+REQUIREMENTS = [
+    'numpy',
+    'sklearn',
+    # test
+    'pytest',
+]
+
+setup(
+    name='tserror',
+    version='0.1dev',
+    author='Yuki Nagae',
+    author_email='yuki.nagae1130@gmail.com',
+    install_requires=REQUIREMENTS,
+    packages=['tserror'],
+    license='MIT',
+    description='A tiny python package analyzes time series prediction errors',
+    long_description=open('README.md').read(),
+)
