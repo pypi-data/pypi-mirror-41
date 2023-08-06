fc86471e7d36301cb0b1ff7b2ab51b575023d886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package cmdix/0.2.0/cmdix-0.2.0-py2.py3-none-any.whl

---
+++ packages/cmdix/cmdix-0.2.0-py2.py3-none-any.whl/cmdix/command/gzip.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, unicode_literals
+from ..compressor import compressor
+
+
+def parseargs(p):
+    '''
+    Add arguments and `func` to `p`.
+
+    :param p: ArgumentParser
+    :return:  ArgumentParser
+    '''
+    return compressor(p, 'gzip')
---
+++ packages/cmdix/cmdix-0.2.0-py2.py3-none-any.whl/cmdix/test/test_basename.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from . import BaseTestCase
+
+
+class TestCase(BaseTestCase):
+    def test_basename(self):
+        out = self.runcommandline('basename foo/bar/biz')[0]
+        assert out == 'biz\n'
