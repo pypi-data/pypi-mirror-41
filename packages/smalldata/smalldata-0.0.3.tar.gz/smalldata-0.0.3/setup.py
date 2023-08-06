1934876e6581ee0214fc570376e7beddf3ab41ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:36 +0000

    Add package smalldata/0.0.3/smalldata-0.0.3.tar.gz

---
+++ packages/smalldata/smalldata-0.0.3.tar.gz/smalldata-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smalldata",
+    version="0.0.3",
+    author="Martin Virtel",
+    author_email="martin.virtel@gmail.com",
+    description="Unix Pipe Fittings For Data Science",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mvtango/smalldata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+
+        'console_scripts' : [
+
+                             'sd_c=smalldata.counter:main',
+                             'sd_g=smalldata.groupby:main',
+                             'sd_e=smalldata.extract:main',
+
+                            ]
+
+    }
+)
