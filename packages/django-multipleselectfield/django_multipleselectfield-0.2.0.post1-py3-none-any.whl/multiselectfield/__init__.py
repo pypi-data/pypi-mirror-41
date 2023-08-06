2789c3e2ec343e9df0366badec8d3a448b347f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:53 +0000

    Add package django-multipleselectfield/0.2.0.post1/django_multipleselectfield-0.2.0.post1-py3-none-any.whl

---
+++ packages/django-multipleselectfield/django_multipleselectfield-0.2.0.post1-py3-none-any.whl/multiselectfield/__init__.py
@@ -0,0 +1,5 @@
+from multiselectfield.db.fields import MultiSelectField
+from multiselectfield.forms.fields import MultiSelectFormField
+
+
+default_app_config = "multiselectfield.apps.MultiSelectFieldConfig"
