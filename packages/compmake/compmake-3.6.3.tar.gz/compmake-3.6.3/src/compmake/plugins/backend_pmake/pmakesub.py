a1f7ee7176a0f77a00f5318ffd36ce469c53517a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:20 +0000

    Add package compmake/3.6.3/compmake-3.6.3.tar.gz

---
+++ packages/compmake/compmake-3.6.3.tar.gz/compmake-3.6.3/src/compmake/plugins/backend_pmake/pmakesub.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+import multiprocessing
+import signal
+import traceback
+from multiprocessing import TimeoutError
+
+from compmake.exceptions import CompmakeBug, HostFailed, JobFailed, JobInterrupted
+from compmake.jobs.manager import AsyncResultInterface
+from compmake.jobs.result_dict import result_dict_raise_if_error
+from contracts import check_isinstance, indent
+from future.moves.queue import Empty
+
+__all__ = [
+    'PmakeSub',
+]
+
+
+class PmakeSub(object):
+    EXIT_TOKEN = 'please-exit'
+
+    def __init__(self, name, signal_queue, signal_token, write_log=None):
+        self.name = name
+
+        self.job_queue = multiprocessing.Queue()
+        self.result_queue = multiprocessing.Queue()
+        print('starting process %s ' % name)
+        self.proc = multiprocessing.Process(target=pmake_worker,
+                                            args=(self.name,
+                                                  self.job_queue,
+                                                  self.result_queue,
+                                                  signal_queue,
+                                                  signal_token,
+                                                  write_log),
+                                            name=name)
+        self.proc.start()
+
+    def terminate(self):
+        self.job_queue.put(PmakeSub.EXIT_TOKEN)
+        self.job_queue.close()
+        self.result_queue.close()
+        self.job_queue = None
+        self.result_queue = None
+
+    def apply_async(self, function, arguments):
+        self.job_queue.put((function, arguments))
+        self.last = PmakeResult(self.result_queue)
+        return self.last
+
+
+def pmake_worker(name, job_queue, result_queue, signal_queue, signal_token,
+                 write_log=None):
+    if write_log:
+        f = open(write_log, 'w')
+
+        def log(s):
+            #print('%s: %s' % (name, s))
+            f.write('%s: ' % name)
+            f.write(s)
+            f.write('\n')
+            f.flush()
+    else:
+        def log(s):
+            print('%s: %s' % (name, s))
+            pass
+
+    log('started pmake_worker()')
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    def put_result(x):
+        log('putting result in result_queue..')
+        result_queue.put(x, block=True)
+        if signal_queue is not None:
+            log('putting result in signal_queue..')
+            signal_queue.put(signal_token, block=True)
+        log('(done)')
+
+    try:
+        while True:
+            log('Listening for job')
+            try:
+                job = job_queue.get(block=True, timeout=5)
+            except Empty:
+                log('Could not receive anything.')
+                continue
+            if job == PmakeSub.EXIT_TOKEN:
+                log('Received EXIT_TOKEN.')
+                break
+
+            log('got job: %s' % str(job))
+            function, arguments = job
+            try:
+                result = function(arguments)
+            except JobFailed as e:
+                log('Job failed, putting notice.')
+                log('result: %s' % str(e))  # debug
+                put_result(e.get_result_dict())
+            except JobInterrupted as e:
+                log('Job interrupted, putting notice.')
+                put_result(dict(abort=str(e)))  # XXX
+            except CompmakeBug as e:  # XXX :to finish
+                log('CompmakeBug')
+                put_result(e.get_result_dict())
+            else:
+                log('result: %s' % str(result))
+                put_result(result)
+
+            log('...done.')
+
+            # except KeyboardInterrupt: pass
+    except BaseException as e:
+        reason = 'aborted because of uncaptured:\n' + indent(
+                traceback.format_exc(), '| ')
+        mye = HostFailed(host="???", job_id="???",
+                         reason=reason, bt=traceback.format_exc())
+        log(str(mye))
+        put_result(mye.get_result_dict())
+    except:
+        mye = HostFailed(host="???", job_id="???",
+                         reason='Uknown exception (not BaseException)',
+                         bt="not available")
+        log(str(mye))
+        put_result(mye.get_result_dict())
+        log('(put)')
+
+
+    if signal_queue is not None:
+        signal_queue.close()
+    result_queue.close()
+    log('clean exit.')
+
+
+class PmakeResult(AsyncResultInterface):
+    """ Wrapper for the async result object obtained by pool.apply_async """
+
+    def __init__(self, result_queue):
+        self.result_queue = result_queue
+        self.result = None
+        # self.count = 0
+
+    def ready(self):
+        # self.count += 1
+        try:
+            self.result = self.result_queue.get(block=False)
+        except Empty:
+            # if self.count > 1000 and self.count % 100 == 0:
+            # print('ready()?  still waiting on %s' % str(self.job))
+            return False
+        else:
+            return True
+
+    def get(self, timeout=0):  # @UnusedVariable
+        if self.result is None:
+            try:
+                self.result = self.result_queue.get(block=True,
+                                                    timeout=timeout)
+            except Empty as e:
+                raise TimeoutError(e)
+
+        check_isinstance(self.result, dict)
+        result_dict_raise_if_error(self.result)
+        return self.result
---
+++ packages/compmake/compmake-3.6.3.tar.gz/compmake-3.6.3/src/compmake/storage/filesystem.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+import os
+import stat
+import traceback
+from glob import glob
+from os.path import basename
+
+from compmake import logger
+from compmake.exceptions import CompmakeBug, SerializationError
+from compmake.utils import (find_pickling_error, safe_pickle_dump,
+                            safe_pickle_load)
+from compmake.utils.safe_write import write_data_to_file
+
+if True:
+    track_time = lambda x: x
+else:
+    from ..utils import TimeTrack
+
+    track_time = TimeTrack.decorator
+
+trace_queries = False
+
+__all__ = [
+    'StorageFilesystem',
+]
+
+
+class StorageFilesystem(object):
+
+    def __init__(self, basepath, compress=False):
+        self.basepath = os.path.realpath(basepath)
+        self.checked_existence = False
+        if compress:
+            self.file_extension = '.pickle.gz'
+            others = list(self.keys0('.pickle'))
+        else:
+            self.file_extension = '.pickle'
+            others = list(self.keys0('.pickle.gz'))
+        if others:
+            msg = 'Extension is %s but found %s files with other extension.' % (self.file_extension, len(others))
+            raise Exception(msg)
+
+
+
+        # create a bunch of files that contain shortcuts
+        create_scripts(self.basepath)
+
+    def __repr__(self):
+        return "FilesystemDB(%r;%s)" % (self.basepath, self.file_extension)
+
+    @track_time
+    def sizeof(self, key):
+        filename = self.filename_for_key(key)
+        statinfo = os.stat(filename)
+        return statinfo.st_size
+
+    @track_time
+    def __getitem__(self, key):
+        if trace_queries:
+            logger.debug('R %s' % str(key))
+
+        self.check_existence()
+
+        filename = self.filename_for_key(key)
+
+        if not os.path.exists(filename):
+            msg = 'Could not find key %r.' % key
+            msg += '\n file: %s' % filename
+            raise CompmakeBug(msg)
+
+        try:
+            return safe_pickle_load(filename)
+        except Exception as e:
+            msg = ("Could not unpickle data for key %r. \n file: %s" %
+                   (key, filename))
+            logger.error(msg)
+            logger.exception(e)
+            msg += "\n" + traceback.format_exc()
+            raise CompmakeBug(msg)
+
+    def check_existence(self):
+        if not self.checked_existence:
+            self.checked_existence = True
+            if not os.path.exists(self.basepath):
+                # logger.info('Creating filesystem db %r' % self.basepath)
+                os.makedirs(self.basepath)
+
+    @track_time
+    def __setitem__(self, key, value):  # @ReservedAssignment
+        if trace_queries:
+            logger.debug('W %s' % str(key))
+
+        self.check_existence()
+
+        filename = self.filename_for_key(key)
+
+        try:
+            safe_pickle_dump(value, filename)
+            assert os.path.exists(filename)
+        except KeyboardInterrupt:
+            raise
+        except BaseException as e:
+            msg = ('Cannot set key %s: cannot pickle object '
+                   'of class %s: %s' % (key, value.__class__.__name__, e))
+            logger.error(msg)
+            logger.exception(e)
+            emsg = find_pickling_error(value)
+            logger.error(emsg)
+            raise SerializationError(msg + '\n' + emsg)
+
+    @track_time
+    def __delitem__(self, key):
+        filename = self.filename_for_key(key)
+        if not os.path.exists(filename):
+            msg = 'I expected path %s to exist before deleting' % filename
+            raise ValueError(msg)
+        os.remove(filename)
+
+    @track_time
+    def __contains__(self, key):
+        if trace_queries:
+            logger.debug('? %s' % str(key))
+
+        filename = self.filename_for_key(key)
+        ex = os.path.exists(filename)
+
+        # logger.debug('? %s %s %s' % (str(key), filename, ex))
+        return ex
+
+    @track_time
+    def keys0(self, extension=None):
+        if extension is None:
+            extension = self.file_extension
+        filename = self.filename_for_key('*', extension)
+        for x in glob(filename):
+            # b = splitext(basename(x))[0]
+            b = basename(x.replace(extension, ''))
+            key = self.basename2key(b)
+            yield key
+
+    @track_time
+    def keys(self):
+        # slow process
+        found = sorted(list(self.keys0()))
+        return found
+
+    def reopen_after_fork(self):
+        pass
+
+    dangerous_chars = {
+        '/': 'CMSLASH',
+        '..': 'CMDOT',
+        '~': 'CMHOME'
+    }
+
+    def key2basename(self, key):
+        """ turns a key into a reasonable filename"""
+        for char, replacement in self.dangerous_chars.items():
+            key = key.replace(char, replacement)
+        return key
+
+    def basename2key(self, key):
+        """ Undoes key2basename """
+        for char, replacement in StorageFilesystem.dangerous_chars.items():
+            key = key.replace(replacement, char)
+        return key
+
+    def filename_for_key(self, key, extension=None):
+        """ Returns the pickle storage filename corresponding to the job id """
+        if extension is None:
+            extension = self.file_extension
+        f = self.key2basename(key) + extension
+        return os.path.join(self.basepath, f)
+
+
+def chmod_plus_x(filename):
+    st = os.stat(filename)
+    os.chmod(filename, st.st_mode | stat.S_IEXEC)
+
+
+def create_scripts(basepath):
+    filename2cmd = \
+        {'ls_failed': 'ls failed',
+         'why_failed': 'why failed',
+         'make_failed': 'make failed',
+         'remake': 'remake',
+         'make': 'make',
+         'parmake': 'parmake',
+         'rparmake': 'rparmake',
+         'rmake': 'rmake',
+         'ls': 'ls',
+         'stats': 'stats',
+         'details': 'details',
+         }
+    for fn, cmd in filename2cmd.items():
+        s = "#!/bin/bash\ncompmake %s -c \"%s $*\"\n" % (basepath, cmd)
+        f = os.path.join(basepath, fn)
+        write_data_to_file(s, f, quiet=True)
+        chmod_plus_x(f)
+
+    s = "#!/bin/bash\ncompmake %s \n" % (basepath)
+    f = os.path.join(basepath, 'console')
+    write_data_to_file(s, f, quiet=True)
+    chmod_plus_x(f)
+
+    s = "#!/bin/bash\ncompmake %s -c \"$*\" \n" % (basepath)
+    f = os.path.join(basepath, 'run')
+    write_data_to_file(s, f, quiet=True)
+    chmod_plus_x(f)
