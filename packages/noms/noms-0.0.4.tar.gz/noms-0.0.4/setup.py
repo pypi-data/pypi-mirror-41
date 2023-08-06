30fbb5b1240e1a4f6b888a7ecbd08e6d06dbb714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:00 +0000

    Add package noms/0.0.4/noms-0.0.4.tar.gz

---
+++ packages/noms/noms-0.0.4.tar.gz/noms-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="noms",
+    version="0.0.4",
+    author="Noah Trenaman",
+    author_email="hello@noahtrenaman.com",
+    description="Nutrient Object Management System (noms) using the USDA's Standard Reference Database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/noahtren/noms",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+          'copy',
+          'operator',
+          'requests',
+          'json'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
