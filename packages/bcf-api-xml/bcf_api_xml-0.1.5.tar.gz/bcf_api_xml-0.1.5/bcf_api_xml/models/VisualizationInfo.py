4f51c8d4acf701a2e71b15e1ab9642333f268f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:05 +0000

    Add package bcf-api-xml/0.1.5/bcf_api_xml-0.1.5.tar.gz

---
+++ packages/bcf-api-xml/bcf_api_xml-0.1.5.tar.gz/bcf_api_xml-0.1.5/bcf_api_xml/models/VisualizationInfo.py
@@ -0,0 +1,57 @@
+from .models import JsonToXMLModel
+from .Component import Component
+from .OrthogonalCamera import OrthogonalCamera
+from .PerspectiveCamera import PerspectiveCamera
+from .Line import Line
+from .ClippingPlane import ClippingPlane
+from .ViewSetupHints import ViewSetupHints
+from .Visibility import Visibility
+
+
+class VisualizationInfo(JsonToXMLModel):
+    SCHEMA_NAME = "visinfo.xsd"
+
+    @property
+    def xml(self):
+        viewpoint = self.json
+        e = self.maker
+
+        children = []
+
+        components = viewpoint.get("components")
+        if components:
+            visibility = components["visibility"]
+
+            components_children = [ViewSetupHints(visibility["view_setup_hints"]).xml]
+
+            if xml_selections:
+                components_children.append(e.Selection(*xml_selections))
+
+            components_children.append(Visibility(visibility).xml)
+
+            if xml_colorings:
+                components_children.append(e.Coloring(*xml_colorings))
+
+            children.append(e.Components(*components_children))
+
+        if viewpoint.get("orthogonal_camera"):
+            xml_ortogonal_camera = OrthogonalCamera(viewpoint["orthogonal_camera"]).xml
+            children.append(xml_ortogonal_camera)
+
+        if viewpoint.get("perspective_camera"):
+            xml_perspective_camera = PerspectiveCamera(
+                viewpoint["perspective_camera"]
+            ).xml
+            children.append(xml_perspective_camera)
+
+        xml_lines = [Line(line).xml for line in viewpoint.get("lines", [])]
+        if xml_lines:
+            children.append(e.Lines(*xml_lines))
+
+        xml_planes = [
+            ClippingPlane(plane).xml for plane in viewpoint.get("clipping_planes", [])
+        ]
+        if xml_planes:
+            children.append(e.ClippingPlanes(*xml_planes))
+
+        return e.VisualizationInfo(*children, Guid=viewpoint["guid"])
