f2cfa23521ecb24ab16145c2ade0176ae03c7fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:55 +0000

    Add package vector-2d/1.5.2/vector_2d-1.5.2.tar.gz

---
+++ packages/vector-2d/vector_2d-1.5.2.tar.gz/vector_2d-1.5.2/vector_2d/__init__.py
@@ -0,0 +1,7 @@
+from vector_2d.vector import *
+from vector_2d.vectorPolar import *
+
+__all__ = ["vector", "vectorPolar", "VectorPolar", "Vector",  "round_vector", "angle", "distance_point_line",
+           "distance_point_segment"]
+
+
