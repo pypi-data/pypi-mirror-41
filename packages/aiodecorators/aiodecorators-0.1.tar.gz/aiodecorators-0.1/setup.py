dd39874df5b0405d900c29cf32b16e5dbed49ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package aiodecorators/0.1/aiodecorators-0.1.tar.gz

---
+++ packages/aiodecorators/aiodecorators-0.1.tar.gz/aiodecorators-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='aiodecorators',
+    version='0.1',
+    description='Function decorators for asyncio synchronization primitives',
+    keywords='asyncio aio lock semaphore boundedsemaphore decorator decorators synchronization',
+    url='http://github.com/dflupu/aiodecorators',
+    author='Daniel Lupu',
+    author_email='dflupu@gmail.com',
+    license='MIT',
+    packages=['aiodecorators'],
+    zip_safe=False
+)
