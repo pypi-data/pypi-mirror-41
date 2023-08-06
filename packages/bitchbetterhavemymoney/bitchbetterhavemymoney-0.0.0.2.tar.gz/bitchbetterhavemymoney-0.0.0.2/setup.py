9d7cdc30eec693d7bd278b28a7701146c797097d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:01 +0000

    Add package bitchbetterhavemymoney/0.0.0.2/bitchbetterhavemymoney-0.0.0.2.tar.gz

---
+++ packages/bitchbetterhavemymoney/bitchbetterhavemymoney-0.0.0.2.tar.gz/bitchbetterhavemymoney-0.0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+ 
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="bitchbetterhavemymoney",
+    version="0.0.0.2",
+    description="A Python CLI tool to check your Monizze maaltijdcheques.",
+    long_description=readme(),
+    long_description_content_type="text/plain",
+    url="https://github.com/lucassel/bitchbetterhavemymoney",
+    author="Lucas Selfslagh",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["bitchbetterhavemymoney"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "bitchbetterhavemymoney=bitchbetterhavemymoney.cli:main",
+        ]
+    },
+)
