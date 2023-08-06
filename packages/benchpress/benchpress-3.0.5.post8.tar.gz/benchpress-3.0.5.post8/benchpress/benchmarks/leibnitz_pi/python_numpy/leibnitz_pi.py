cf8c2d13b2294fa7a72c3c57c91da32f9b5bc119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package benchpress/3.0.5.post8/benchpress-3.0.5.post8.tar.gz

---
+++ packages/benchpress/benchpress-3.0.5.post8.tar.gz/benchpress-3.0.5.post8/benchpress/benchmarks/leibnitz_pi/python_numpy/leibnitz_pi.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from benchpress.benchmarks import util
+import numpy as np
+
+bench = util.Benchmark("Leibnitz Pi", "<size>*<niters>")
+
+
+def leibnitz_pi(N):
+    n = np.arange(0, N)
+    return np.sum(1.0 / (4 * n + 1) - 1.0 / (4 * n + 3))
+
+
+def main():
+    (size, niter) = bench.args.size
+
+    bench.start()
+    pi = 0.0
+    for _ in range(niter):
+        pi += 4.0 * leibnitz_pi(size)  # Execute benchmark
+        bench.flush()
+    bench.stop()
+    bench.pprint()
+
+
+if __name__ == "__main__":
+    main()
