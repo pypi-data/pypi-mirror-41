30d9f7c19de50ba9fea04fedbf60eaef409ea04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:48 +0000

    Add package arcana/0.3.1/arcana-0.3.1.tar.gz

---
+++ packages/arcana/arcana-0.3.1.tar.gz/arcana-0.3.1/arcana/data/__init__.py
@@ -0,0 +1,8 @@
+from .item import Fileset, Field
+from .collection import FilesetCollection, FieldCollection
+from .spec import (
+    FilesetSpec, FieldSpec, AcquiredFilesetSpec, AcquiredFieldSpec, BaseSpec,
+    BaseAcquiredSpec)
+from .base import BaseField, BaseFileset, BaseData
+from .selector import FilesetSelector, FieldSelector, BaseSelector
+from .file_format import FileFormat, Converter, IdentityConverter
---
+++ packages/arcana/arcana-0.3.1.tar.gz/arcana-0.3.1/arcana/environment/__init__.py
@@ -0,0 +1,5 @@
+from .static import StaticEnvironment
+from .modules import ModulesEnvironment
+from .requirement import (
+    BaseRequirement, CliRequirement, MatlabPackageRequirement,
+    PythonPackageRequirement, matlab_req)
