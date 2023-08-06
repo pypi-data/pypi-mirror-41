b55bab89e29d13b1e4a38e2c6e4c079255b42e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package mms-alice-testpackage/0.0.6/mms_alice_testpackage-0.0.6-py3-none-any.whl

---
+++ packages/mms-alice-testpackage/mms_alice_testpackage-0.0.6-py3-none-any.whl/jgTest_pkg/test.py
@@ -0,0 +1,15 @@
+
+
+class MMS_TestClass:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+
+	def add(self):
+		return self.a + self.b
+
+	def abs_diff(self, val1, val2):
+		return abs(val1-val2)
+
+	def output(self):
+		return 'Kack Fabi! Beeil dich! Wir ham Hunger!'
