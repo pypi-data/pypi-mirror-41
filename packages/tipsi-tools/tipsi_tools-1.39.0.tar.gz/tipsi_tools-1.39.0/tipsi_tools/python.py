d45e97f4113f7bf8021e2a258017eb6ce7fc6183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package tipsi-tools/1.39.0/tipsi_tools-1.39.0.tar.gz

---
+++ packages/tipsi-tools/tipsi_tools-1.39.0.tar.gz/tipsi_tools-1.39.0/tipsi_tools/python.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from decimal import ROUND_HALF_UP, Decimal
+
+
+def execfile(fname, _globals, _locals):
+    """
+    Usage: execfile('path/to/file.py', globals(), locals())
+    """
+    if os.path.exists(fname):
+        with open(fname) as f:
+            code = compile(f.read(), os.path.basename(fname), 'exec')
+            exec(code, _globals, _locals)
+            return True
+    else:
+        return False
+
+
+def rel_path(path, check=True, depth=1):
+    d = os.path.dirname(sys._getframe(depth).f_code.co_filename)
+    full = os.path.abspath(os.path.join(d, path))
+    if check and not os.path.exists(full):
+        raise Exception('No such path: {!r}'.format(full))
+    return full
+
+
+def usd_round(amount):
+    return amount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
