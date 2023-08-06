5088ec0a6ea6b19fe81e2221f77610967fefd686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package j5/0.0.2/j5-0.0.2.tar.gz

---
+++ packages/j5/j5-0.0.2.tar.gz/j5-0.0.2/j5/backends/base.py
@@ -0,0 +1,37 @@
+"""The base classes for backends."""
+
+from abc import ABCMeta, abstractmethod
+from typing import Any, List, Mapping
+
+
+class Backend(metaclass=ABCMeta):
+    """
+    The base class for a backend.
+
+    A backend is an implementation of a specific board for a specific environment.
+
+    """
+
+
+class BackendGroup(metaclass=ABCMeta):
+    """A collection of board implementations that can work together."""
+
+    @property
+    @abstractmethod
+    def board_backend_mapping(self) -> Mapping[Any, Backend]:
+        """A mapping of a supported board to a supported backend."""
+        raise NotImplementedError  # pragma: no cover
+
+    @property
+    def supported_boards(self) -> List[Any]:
+        """The boards that are supported by this backend group."""
+        return list(self.board_backend_mapping.keys())
+
+    def get_backend(self, board: Any) -> Backend:
+        """Get the backend for a board."""
+        if board not in self.supported_boards:
+            raise NotImplementedError(
+                "The {} does not support {}".format(str(self), str(board)),
+            )
+
+        return self.board_backend_mapping[board]()  # type: ignore
