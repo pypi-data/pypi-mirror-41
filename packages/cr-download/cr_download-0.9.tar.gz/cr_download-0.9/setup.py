047edd2513cdb4ada5a664d322061475db3fa674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package cr-download/0.9/cr_download-0.9.tar.gz

---
+++ packages/cr-download/cr_download-0.9.tar.gz/cr_download-0.9/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup_dir = path.abspath(path.dirname(__file__))
+with open(path.join(setup_dir, 'README.md'),
+          encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="cr_download",
+    version="0.9",
+    package_dir={'':'packages'},
+    packages=find_packages(),
+
+    scripts=["bin/critrole_download",
+             "bin/autocut_vod"],
+
+    package_data={
+        '':['share/config.yaml',
+            'share/sound_files/*']
+    },
+
+    install_requires=[
+        'pyacoustid',
+        'streamlink',
+        'ruamel.yaml>=0.15.0, <=0.15.87',
+        'requests',
+        'google-api-python-client',
+        'progressbar2'
+    ],
+
+    author="Teddy Weisman",
+    author_email="tjweisman@gmail.com",
+    license='MIT',
+    url="https://github.com/tjweisman/cr_download",
+
+    description="""Tool to download Critical Role episodes from
+    the Geek & Sundry Twitch channel, convert to audio, and recut.""",
+
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
