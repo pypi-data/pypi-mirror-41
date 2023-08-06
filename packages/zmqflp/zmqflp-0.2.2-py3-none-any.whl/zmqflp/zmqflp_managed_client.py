f38cd76925fe7350ceffa6bda8eaa4e4d429f465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:08 +0000

    Add package zmqflp/0.2.2/zmqflp-0.2.2-py3-none-any.whl

---
+++ packages/zmqflp/zmqflp-0.2.2-py3-none-any.whl/zmqflp/zmqflp_managed_client.py
@@ -0,0 +1,33 @@
+import time
+import msgpack
+import logging
+import zmqflp_api
+
+# Client usable with Context Managers
+# needed for containerized python jobs
+
+class ZMQFLPManagedClient(object):
+    def __init__(self, list_of_server_ips_with_ports_as_str):
+        self.client = zmqflp_api.FreelanceClient()
+        for ip in list_of_server_ips_with_ports_as_str:
+            logging.info('client: connecting to server '+ip)
+            self.client.connect("tcp://"+ip)
+            logging.info('client: added server '+ip)
+
+    def __enter__(self):
+        return self
+
+    def send_and_receive(self, in_request):
+        reply = self.client.request(msgpack.dumps(in_request, use_bin_type=True))
+        if not reply:
+            logging.info("error, request "+str(in_request)+" unserviced")
+            return False
+        else:
+            return msgpack.loads(reply[2], raw=False, encoding="utf-8")
+    
+    def __exit__(self, *args):
+        try:
+            print('stopping client...')
+            self.client.stop()
+        except Exception as e:
+            return
