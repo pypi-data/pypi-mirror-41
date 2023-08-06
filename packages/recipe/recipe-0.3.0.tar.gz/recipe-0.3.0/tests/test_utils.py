b7cb26f16af7b8b42b9e573362a4fe3dd5d1522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package recipe/0.3.0/recipe-0.3.0.tar.gz

---
+++ packages/recipe/recipe-0.3.0.tar.gz/recipe-0.3.0/tests/test_utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from recipe.utils import AttrDict, replace_whitespace_with_space
+
+
+class TestUtils(object):
+
+    def test_replace_whitespace_with_space(self):
+        assert replace_whitespace_with_space('fooo    moo') == 'fooo moo'
+        assert replace_whitespace_with_space('fooo\n\t moo') == 'fooo moo'
+
+
+class TestAttrDict(object):
+
+    def test_attr_dict(self):
+        d = AttrDict()
+        assert isinstance(d, dict)
+        d.foo = 2
+        assert d['foo'] == 2
+        d['bar'] = 3
+        assert d.bar == 3
