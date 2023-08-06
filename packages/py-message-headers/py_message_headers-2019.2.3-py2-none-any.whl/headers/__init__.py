6f66e5bf98ced2956396b9ec200210f70e2d6a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:19 +0000

    Add package py-message-headers/2019.2.3/py_message_headers-2019.2.3-py2-none-any.whl

---
+++ packages/py-message-headers/py_message_headers-2019.2.3-py2-none-any.whl/headers/__init__.py
@@ -0,0 +1,8 @@
+# Generated on 2019-02-03T13:03:06.509000
+
+from mail import *
+from http import *
+from mime import *
+from netnews import *
+
+VERSION = "2019.02.03"
---
+++ packages/py-message-headers/py_message_headers-2019.2.3-py2-none-any.whl/headers/netnews.py
@@ -0,0 +1,91 @@
+# Generated on 2019-02-03T13:03:06.509000
+
+# standard, [RFC5536]
+APPROVED = "Approved"
+
+# standard, [RFC5536]
+ARCHIVE = "Archive"
+
+# standard, [RFC5064]
+ARCHIVED_AT = "Archived-At"
+
+# standard, [RFC8315]
+CANCEL_KEY = "Cancel-Key"
+
+# standard, [RFC8315]
+CANCEL_LOCK = "Cancel-Lock"
+
+# standard, [RFC5536][RFC5322]
+COMMENTS = "Comments"
+
+# standard, [RFC5536]
+CONTROL = "Control"
+
+# standard, [RFC5536][RFC5322]
+DATE = "Date"
+
+# standard, [RFC5536]
+DISTRIBUTION = "Distribution"
+
+# standard, [RFC5536]
+EXPIRES = "Expires"
+
+# standard, [RFC5536]
+FOLLOWUP_TO = "Followup-To"
+
+# standard, [RFC5536][RFC5322]
+FROM = "From"
+
+# standard, [RFC5536]
+INJECTION_DATE = "Injection-Date"
+
+# standard, [RFC5536]
+INJECTION_INFO = "Injection-Info"
+
+# standard, [RFC5536][RFC5322]
+KEYWORDS = "Keywords"
+
+# standard, [RFC5536][RFC5322]
+MESSAGE_ID = "Message-ID"
+
+# standard, [RFC5536]
+NEWSGROUPS = "Newsgroups"
+
+# standard, [RFC5536]
+ORGANIZATION = "Organization"
+
+# standard, [RFC5537]
+ORIGINAL_SENDER = "Original-Sender"
+
+# standard, [RFC5536]
+PATH = "Path"
+
+# standard, [RFC5536][RFC5322]
+REFERENCES = "References"
+
+# standard, [RFC5536][RFC5322]
+REPLY_TO = "Reply-To"
+
+# standard, [RFC5536][RFC5322]
+SENDER = "Sender"
+
+# standard, [RFC5536][RFC5322]
+SUBJECT = "Subject"
+
+# standard, [RFC5536]
+SUMMARY = "Summary"
+
+# standard, [RFC5536][RFC2156]
+SUPERSEDES = "Supersedes"
+
+# standard, [RFC5536][RFC2616]
+USER_AGENT = "User-Agent"
+
+# standard, [RFC5536]
+XREF = "Xref"
+
+# standard, [RFC7259]
+JABBER_ID = "Jabber-ID"
+
+# standard, [ftp://ftp.isc.org/pub/pgpcontrol/FORMAT][https://ftp.isc.org/pub/pgpcontrol/FORMAT]
+X_PGP_SIG = "X-PGP-Sig"
