558926028d51462e9858e459f4dafd525d3147a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package gcmtools/0.3.0/gcmtools-0.3.0.tar.gz

---
+++ packages/gcmtools/gcmtools-0.3.0.tar.gz/gcmtools-0.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='gcmtools',
+    version='0.3.0',
+    packages=['gcmtools',],
+    author='Adiv Paradise',
+    author_email='paradise.astro@gmail.com',
+    license='GNU General Public License',
+    url='http://pypi.python.org/pypi/gcmtools/',
+    description='GCM Output Analysis Tools',
+    long_description=open('README.txt').read(),
+    )
