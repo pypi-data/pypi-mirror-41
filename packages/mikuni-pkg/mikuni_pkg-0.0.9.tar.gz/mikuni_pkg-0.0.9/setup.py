f822612e6da7395d0ac5c95b155c9ffc0b1cf0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:59 +0000

    Add package mikuni-pkg/0.0.9/mikuni_pkg-0.0.9.tar.gz

---
+++ packages/mikuni-pkg/mikuni_pkg-0.0.9.tar.gz/mikuni_pkg-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mikuni_pkg",
+    version="0.0.9",
+    author="Mikuni Motoi",
+    author_email="motoimikuni@egmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # packages=setuptools.find_packages(),pip 
+    install_requires=["numpy", "tensorflow", "mojimoji", "jaconv", "ipython", "keras", "pykakasi"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6"
+    ],
+)
