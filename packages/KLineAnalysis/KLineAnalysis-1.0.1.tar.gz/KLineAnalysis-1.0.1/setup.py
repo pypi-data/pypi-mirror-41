dbbc23e0a69088c18b1ea632c5c6262c10487e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package KLineAnalysis/1.0.1/KLineAnalysis-1.0.1.tar.gz

---
+++ packages/KLineAnalysis/KLineAnalysis-1.0.1.tar.gz/KLineAnalysis-1.0.1/setup.py
@@ -0,0 +1,23 @@
+
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "KLineAnalysis",      #这里是pip项目发布的名称
+    version = "1.0.1",  #版本号，数值大的会优先被pip
+    keywords = ("FinanceAnalysis","Finance","Analysis"),
+    description = "An feature extraction algorithm",
+    long_description = "An feature extraction algorithm, improve the FastICA",
+    license = "MIT Licence",
+
+    url = "https://github.com/670428704/FinanceAnalysis.git",     #项目相关文件地址，一般是github
+    author = "fdsn",
+    author_email = "670428704@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy","tushare","matplotlib","mpl_finance","datetime"]          #这个项目需要的第三方库
+)
+
+
