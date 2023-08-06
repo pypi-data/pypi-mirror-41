b7e014c9e91e11a61fd323e9ee77f4e69d4d08e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:33 +0000

    Add package ntelebot/0.2.0.1/ntelebot-0.2.0.1-py2.py3-none-any.whl

---
+++ packages/ntelebot/ntelebot-0.2.0.1-py2.py3-none-any.whl/ntelebot/__init__.py
@@ -0,0 +1,13 @@
+"""A simple implementation of https://core.telegram.org/bots/api."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# pylint: disable=cyclic-import
+from ntelebot import bot
+from ntelebot import delayqueue
+from ntelebot import deeplink
+from ntelebot import dispatch
+from ntelebot import errors
+from ntelebot import keyboardutil
+from ntelebot import loop
+from ntelebot import preprocess
