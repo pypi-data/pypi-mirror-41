f403269351f682860e108d20955951219120a30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package telegram-export/1.8.2/telegram-export-1.8.2.tar.gz

---
+++ packages/telegram-export/telegram-export-1.8.2.tar.gz/telegram-export-1.8.2/setup.py
@@ -0,0 +1,40 @@
+"""Setup for telegram-export"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open("README.rst", "r") as readme:
+    desc=readme.read()
+
+setup(
+    name='telegram-export',
+    license="MPL 2.0",
+    version='1.8.2',
+    description='A tool to download Telegram data (users, chats, messages, '
+                'and media) into a database (and display the saved data).',
+    long_description=desc,
+    url='https://github.com/expectocode/telegram-export',
+    author='expectocode and Lonami',
+    author_email='expectocode@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='Telegram messaging database',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm', 'telethon~=1.4.3', 'appdirs',
+        'async_generator'  # Python 3.5 async gen support
+    ],
+    scripts=['bin/telegram-export'],
+    test_suite='telegram_export.tests',
+    project_urls={
+        'Bug Reports': 'https://github.com/expectocode/telegram-export/issues',
+        'Source': 'https://github.com/expectocode/telegram-export'
+    }
+)
