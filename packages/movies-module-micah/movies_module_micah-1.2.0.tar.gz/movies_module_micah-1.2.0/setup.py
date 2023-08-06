45617d58d3508a99206c00f7509d637a23a4a81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:15 +0000

    Add package movies-module-micah/1.2.0/movies_module_micah-1.2.0.tar.gz

---
+++ packages/movies-module-micah/movies_module_micah-1.2.0.tar.gz/movies_module_micah-1.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="movies_module_micah",
+    version="1.2.0",
+    author="Vincent",
+    author_email="micahkabala@outlook.com",
+    description="Micah-specific exercise modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/micang404/movies_module_micah",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
