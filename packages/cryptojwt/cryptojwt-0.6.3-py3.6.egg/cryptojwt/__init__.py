482ff5c17091b8bf90fa67c524cc6c064353408a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:48 +0000

    Add package cryptojwt/0.6.3/cryptojwt-0.6.3-py3.6.egg

---
+++ packages/cryptojwt/cryptojwt-0.6.3-py3.6.egg/cryptojwt/__init__.py
@@ -0,0 +1,28 @@
+"""JSON Web Token"""
+import json
+import logging
+
+from .exception import BadSyntax
+from .utils import as_unicode
+from .utils import b64encode_item
+from .utils import b64d
+from .utils import split_token
+
+try:
+    from builtins import zip
+    from builtins import hex
+    from builtins import str
+except ImportError:
+    pass
+
+__version__ = '0.6.3'
+
+logger = logging.getLogger(__name__)
+
+JWT_TYPES = (u"JWT", u"application/jws", u"JWS", u"JWE")
+
+JWT_CLAIMS = {"iss": str, "sub": str, "aud": str, "exp": int, "nbf": int,
+              "iat": int, "jti": str, "typ": str}
+
+JWT_HEADERS = ["typ", "cty"]
+
