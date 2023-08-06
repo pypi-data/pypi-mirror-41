b0d5c1c6a4b76af780f70285e1f8f43c6e3cfe41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:13 +0000

    Add package modulegraph2/2.0a1/modulegraph2-2.0a1.tar.gz

---
+++ packages/modulegraph2/modulegraph2-2.0a1.tar.gz/modulegraph2-2.0a1/doc/changelog.rst
@@ -0,0 +1,9 @@
+Release history
+===============
+
+2.0a1
+-----
+
+Initial pre-release. This version should be
+functionally complete, but hasn't been used
+by other code yet.
---
+++ packages/modulegraph2/modulegraph2-2.0a1.tar.gz/modulegraph2-2.0a1/testsuite/nodebuilder-tree/setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from setuptools import setup, Extension, Command
+import py_compile
+import zipfile
+
+extension1 = Extension("extension", sources=["extension1.c"])
+extension2 = Extension("package.pkgext", sources=["pkgext.c"])
+extension3 = Extension("ext_package.__init__", sources=["init.c"])
+
+
+class build_bytecode(Command):
+    description = "build bytecode modules"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for dirname, dirs, files in os.walk("src"):
+            for fn in files:
+                if fn.endswith(".py"):
+                    srcpath = os.path.join(dirname, fn)
+                    dstpath = srcpath[4:] + "c"
+                    py_compile.compile(srcpath, dstpath, dstpath[:-1], True)
+
+
+class build_zipfile(Command):
+    description = "build zipfile with modules"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        with zipfile.ZipFile("packages.zip", "w") as zf:
+            for dirname, dirs, files in os.walk("zipsrc"):
+                for dn in dirs:
+                    # Explicitly add directories to the zipfile,
+                    # needed for implicit namespace packages in
+                    # zipfiles (see Python issue #14905)
+                    srcpath = os.path.join(dirname, dn)
+                    dstpath = srcpath[7:]
+                    zf.write(srcpath, dstpath)
+
+                for fn in files:
+                    srcpath = os.path.join(dirname, fn)
+                    dstpath = srcpath[7:]
+                    zf.write(srcpath, dstpath)
+
+
+setup(
+    ext_modules=[extension1, extension2, extension3],
+    cmdclass={"build_bytecode": build_bytecode, "build_zipfile": build_zipfile},
+)
