52dc233ae8bed83da22e7a1d41e30606c979c586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:32 +0000

    Add package kts/0.0.39/kts-0.0.39.tar.gz

---
+++ packages/kts/kts-0.0.39.tar.gz/kts-0.0.39/kts/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+# from . import cl_util
+
+print(sys.argv)
+
+if True or not sys.argv[0]:
+    from . import cl_util
+    # from cl_util import check_file_system
+    cl_util.check_file_system()
+    from .feature.decorators import preview, register, deregister, dropper, selector
+    from .feature.stl import *
+    from .feature import stl
+    from .feature.storage import feature_list as features
+    from .validation.experiment import experiment_list as experiments
+
+    __all__ = [
+        'feature',
+        'features',
+        'validation',
+        'experiments',
+        'stl',
+        'zoo',
+        'model',
+        'pipeline',
+        'storage',
+        'cli'
+    ]
+
+    with open("version.py", 'r') as f:
+        __version__ = f"0.0.{int(f.read().strip()) + 1}"
