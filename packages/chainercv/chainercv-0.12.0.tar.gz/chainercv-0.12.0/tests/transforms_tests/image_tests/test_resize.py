b0caa5a8b08f2954fe4219830836d83258b8daf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:49 +0000

    Add package chainercv/0.12.0/chainercv-0.12.0.tar.gz

---
+++ packages/chainercv/chainercv-0.12.0.tar.gz/chainercv-0.12.0/tests/transforms_tests/image_tests/test_resize.py
@@ -0,0 +1,31 @@
+import unittest
+
+import numpy as np
+import PIL
+
+import chainer
+from chainer import testing
+from chainercv.transforms import resize
+
+
+@testing.parameterize(*testing.product({
+    'interpolation': [PIL.Image.NEAREST, PIL.Image.BILINEAR,
+                      PIL.Image.BICUBIC, PIL.Image.LANCZOS],
+    'backend': ['cv2', 'PIL'],
+}))
+class TestResize(unittest.TestCase):
+
+    def test_resize_color(self):
+        img = np.random.uniform(size=(3, 24, 32))
+        chainer.config.cv_resize_backend = self.backend
+        out = resize(img, size=(32, 64), interpolation=self.interpolation)
+        self.assertEqual(out.shape, (3, 32, 64))
+
+    def test_resize_grayscale(self):
+        img = np.random.uniform(size=(1, 24, 32))
+        chainer.config.cv_resize_backend = self.backend
+        out = resize(img, size=(32, 64), interpolation=self.interpolation)
+        self.assertEqual(out.shape, (1, 32, 64))
+
+
+testing.run_module(__name__, __file__)
