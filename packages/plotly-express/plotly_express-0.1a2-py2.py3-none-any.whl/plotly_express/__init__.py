49ba32c3a22dd6dfce3fcb509f40f93fe538dc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:23 +0000

    Add package plotly-express/0.1a2/plotly_express-0.1a2-py2.py3-none-any.whl

---
+++ packages/plotly-express/plotly_express-0.1a2-py2.py3-none-any.whl/plotly_express/__init__.py
@@ -0,0 +1,28 @@
+from ._px import (  # noqa: F401
+    scatter,
+    scatter_3d,
+    scatter_polar,
+    scatter_ternary,
+    scatter_mapbox,
+    scatter_geo,
+    line,
+    line_3d,
+    line_polar,
+    line_ternary,
+    line_mapbox,
+    line_geo,
+    bar,
+    bar_polar,
+    violin,
+    box,
+    histogram,
+    scatter_matrix,
+    parallel_coordinates,
+    parallel_categories,
+    choropleth,
+    density_contour,
+    set_mapbox_access_token,
+)
+
+from . import data  # noqa: F401
+from . import colors  # noqa: F401
