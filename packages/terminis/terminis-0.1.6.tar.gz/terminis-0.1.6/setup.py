220bcee771f63f41769170740877df784b7017bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package terminis/0.1.6/terminis-0.1.6.tar.gz

---
+++ packages/terminis/terminis-0.1.6.tar.gz/terminis-0.1.6/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['terminis']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{':sys_platform == "win32"': ['windows-curses>=1.0,<2.0']}
+
+entry_points = \
+{'console_scripts': ['terminis = terminis.terminis:main']}
+
+setup_kwargs = {
+    'name': 'terminis',
+    'version': '0.1.6',
+    'description': 'Tetris clone for terminal. Ideal for servers without GUI!',
+    'long_description': '# Terminis\nTetris clone for terminal. Ideal for servers without GUI!\n\n## Usage\n```bash\n  terminis [level]\n```\n  level: integer between 1 and 15\n',
+    'author': 'adrienmalin',
+    'author_email': '41926238+adrienmalin@users.noreply.github.com',
+    'url': 'https://github.com/adrienmalin/Terminis',
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+}
+
+
+setup(**setup_kwargs)
