fb4930e4c514cb3af151dce246652d3cae26bd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package JapaneseVerbConjugator/1.0.0/JapaneseVerbConjugator-1.0.0-py3-none-any.whl

---
+++ packages/JapaneseVerbConjugator/JapaneseVerbConjugator-1.0.0-py3-none-any.whl/japaneseverbconjugator/src/driver.py
@@ -0,0 +1,9 @@
+import japaneseverbconjugator.src.JapaneseVerbFormGenerator
+from japaneseverbconjugator.src.constants.EnumeratedTypes import Polarity
+
+
+# bob = jg.JapaneseVerbFormGenerator()
+# result = bob.generate_plain_form("飲む", VerbClass.GODAN, Tense.PAST, Polarity.POSITIVE)
+# print(result)
+
+print("hello world")
