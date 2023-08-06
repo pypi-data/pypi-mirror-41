6c6accc537fd99944bf2a8f879487464927d68ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:30 +0000

    Add package llnl-scraper/0.6.0.dev0/llnl-scraper-0.6.0.dev0.tar.gz

---
+++ packages/llnl-scraper/llnl-scraper-0.6.0.dev0.tar.gz/llnl-scraper-0.6.0.dev0/setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setup(
+    name='llnl-scraper',
+    version='0.6.0-dev',
+    description='Package for extracting software repository metadata',
+    long_description=long_description,
+    author='Ian Lee',
+    author_email='lee1001@llnl.gov',
+    url='https://github.com/llnl/scraper',
+    packages=find_packages(),
+    install_requires=[
+        'github3.py==1.0.0a4',
+        'python-gitlab>=1.6.0',
+        'stashy>=0.3',
+        'python-dateutil>=2.7.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scraper = scraper.gen_code_gov_json:main',
+        ]
+    },
+    scripts=[
+        'scripts/codegov_compute_hours.py',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
