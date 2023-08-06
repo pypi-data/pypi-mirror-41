aa51e22d3b9a34d9ffdf4e9fb49898127b136e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:10 +0000

    Add package illusional.cwlgen/0.0.5/illusional.cwlgen-0.0.5.tar.gz

---
+++ packages/illusional.cwlgen/illusional.cwlgen-0.0.5.tar.gz/illusional.cwlgen-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+__version__ = "v0.0.5"
+
+setup(name="illusional.cwlgen",
+      version=__version__,
+      description='Fork of python-cwlgen | Generation of CWL programmatically. Avaible types: CommandLineTool, '
+                  'Workflow and Requirements',
+      long_description=open("./README.md").read(),
+      long_description_content_type="text/markdown",
+      author='Michael Franklin | Kenzo-Hugo Hillion and Herve Menager',
+      author_email='michael.franklin@petermac.org',
+      license='MIT',
+      keywords=['cwl'],
+      install_requires=['six', 'ruamel.yaml >= 0.12.4, <= 0.15.77'],
+      packages=["cwlgen"],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Topic :: Scientific/Engineering',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Environment :: Console',
+      ],
+)
