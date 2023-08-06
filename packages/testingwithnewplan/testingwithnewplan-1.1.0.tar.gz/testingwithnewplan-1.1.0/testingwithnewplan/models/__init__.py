5fbd67a35c14968884ef3e44f4132be313a97685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:49 +0000

    Add package testingwithnewplan/1.1.0/testingwithnewplan-1.1.0.tar.gz

---
+++ packages/testingwithnewplan/testingwithnewplan-1.1.0.tar.gz/testingwithnewplan-1.1.0/testingwithnewplan/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'order',
+    'user',
+    'category',
+    'tag',
+    'pet',
+    'api_response',
+    'status_enum',
+    'status_1_enum',
+    'status_2_enum',
+    'o_auth_scope_enum',
+]
