87d9f792138d135b821fff293fabe7432c7a6044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:24 +0000

    Add package introrl/0.0.1/introrl-0.0.1.tar.gz

---
+++ packages/introrl/introrl-0.0.1.tar.gz/introrl-0.0.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. commit signature, "date_str author_str sha_str"
+   Maintain spacing of "History" and "GitHub Log" titles
+
+History
+=======
+
+GitHub Log
+----------
+
+
+* Jan 29, 2019
+    - (by: sonofeft)
+        - First Created IntroRL with PyHatch
+
---
+++ packages/introrl/introrl-0.0.1.tar.gz/introrl-0.0.1/introrl/examples/example_1.py
@@ -0,0 +1,14 @@
+"""
+This example demonstrates the proper use of project: introrl
+"""
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../../"))  # needed to find introrl development version
+
+from introrl.env import Environment
+
+# TODO: create working example of project introrl
+
+my_class = Environment()
+
