a745bbd168ced2f84c40fca53903222dbb12ab6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:38 +0000

    Add package sprm/0.2.7/sprm-0.2.7-py3-none-any.whl

---
+++ packages/sprm/sprm-0.2.7-py3-none-any.whl/sprm/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 12:17:17 2018
+
+@author: Sven Serneels, Ponalytics
+"""
+
+__name__ = "sprm"
+__author__ = "Sven Serneels"
+__license__ = "GNU"
+__version__ = "0.2.7"
+__date__ = "2019-02-01"
+
+from .robcent import robcent
+from .sprm import snipls,sprm
+from .rm import rm
+from .sprm_plot import sprm_plot,sprm_plot_cv
+
+
+
+
