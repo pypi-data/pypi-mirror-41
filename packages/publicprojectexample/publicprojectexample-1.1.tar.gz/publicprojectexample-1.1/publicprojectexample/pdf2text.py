0fcae51940cb2749ef0cb7bdd44a7c67eadf07a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:24 +0000

    Add package publicprojectexample/1.1/publicprojectexample-1.1.tar.gz

---
+++ packages/publicprojectexample/publicprojectexample-1.1.tar.gz/publicprojectexample-1.1/publicprojectexample/pdf2text.py
@@ -0,0 +1,18 @@
+""" Header: This module provides functions to convert a PDF to text-format (DEMO!!!) 
+
+    A more detailed explanation.
+    ...
+"""
+
+
+def convert(path):
+    """
+        Convert the given pdf to text.
+
+        Parameters: 
+        path (str): The path to the pdf-file
+
+        Returns:
+        str: The pdf-file-content as text
+    """
+    print("pdf2text")
