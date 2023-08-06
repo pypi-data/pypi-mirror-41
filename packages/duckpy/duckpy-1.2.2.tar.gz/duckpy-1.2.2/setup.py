409b7ab97d5948569397f6110ffaeb5c5e0a28e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:51 +0000

    Add package duckpy/1.2.2/duckpy-1.2.2.tar.gz

---
+++ packages/duckpy/duckpy-1.2.2.tar.gz/duckpy-1.2.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='duckpy',
+    version='1.2.2',
+    packages=setuptools.find_packages(),
+    install_requires=['bs4', 'urllib3', 'certifi'],
+    url='https://github.com/AmanoTeam/duckpy',
+    author='Amano Team',
+    author_email='contact@amanoteam.ml',
+    description='A simple module that searches on DuckDuckGo',
+    long_description=readme,
+    long_description_content_type='text/markdown'
+)
