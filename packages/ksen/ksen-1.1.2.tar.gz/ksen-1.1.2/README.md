9f5873e4fec535671dfa049b34c267ca2a8b44f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package ksen/1.1.2/ksen-1.1.2.tar.gz

---
+++ packages/ksen/ksen-1.1.2.tar.gz/ksen-1.1.2/README.md
@@ -0,0 +1,7 @@
+# Caesar Encrypt Package
+
+## version1.1.2:
+solved a known bug
+
+## version1.1.0:
+first release
---
+++ packages/ksen/ksen-1.1.2.tar.gz/ksen-1.1.2/ks_encrypt/ks_encrypt_kb.py
@@ -0,0 +1,36 @@
+# data
+dictCap = 'Q W E R T Y U I O P A S D F G H J K L Z X C V B N M'.split(' ')
+dictLow = 'q w e r t y u i o p a s d f g h j k l z x c v b n m'.split(' ')
+
+
+# encrypt function
+def encrypt_kb(text, key):
+    encrypt_text = ''
+    for char in text:
+	char_encrypt = char
+        if char.isalpha():
+            char_encrypt = encrypt_alpha_by_alpha(char, key)
+        encrypt_text += char_encrypt
+    return encrypt_text
+
+
+# encrypt one alpha function
+def encrypt_alpha_by_alpha(alpha, key):
+    if alpha.islower():
+        pos = dictLow.index(alpha)
+        pos += key
+    elif alpha.isupper():
+        pos = dictCap.index(alpha)
+    if pos > 26:
+        pos -= 26
+    elif pos < 0:
+        pos += 26
+    if alpha.islower():
+        return dictLow[pos]
+    elif alpha.isupper():
+        return dictCap[pos]
+
+
+# main function
+if __name__ == '__main__':
+    print(encrypt_kb('sample', 2))
