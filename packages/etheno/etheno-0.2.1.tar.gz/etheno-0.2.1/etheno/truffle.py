bb973e95b9a96ebcb9b0fe73a810d57fdf98262b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:58 +0000

    Add package etheno/0.2.1/etheno-0.2.1.tar.gz

---
+++ packages/etheno/etheno-0.2.1.tar.gz/etheno-0.2.1/etheno/truffle.py
@@ -0,0 +1,42 @@
+from collections.abc import Sequence
+import time
+
+from .logger import EthenoLogger, PtyLogger
+
+class Truffle(object):
+    def __init__(self, parent_logger=None, log_level=None):
+        self._running = False
+        self.logger = EthenoLogger('Truffle', log_level=log_level, parent=parent_logger)
+
+    def terminate(self):
+        self._running = False
+
+    def run_tests(self):
+        return self.run('test')
+
+    def run_migrate(self):
+        return self.run('migrate')
+
+    def run(self, args):
+        self._running = True
+        if isinstance(args, Sequence) and not isinstance(args, str) and not isinstance(args, bytes):
+            if not isinstance(args, list):
+                args = list(args)
+        else:
+            args = [args]
+
+        p = PtyLogger(self.logger, ['/usr/bin/env', 'truffle'] + args)
+        p.start()
+
+        try:
+            while p.isalive():
+                if not self._running:
+                    self.logger.info("Etheno received a shutdown signal; terminating truffle %s" % ' '.join(args))
+                    break
+                time.sleep(1.0)
+        except KeyboardInterrupt as e:
+            self.logger.info("Caught keyboard interrupt; terminating truffle %s" % ' '.join(args))
+            raise e
+        finally:
+            p.close(force=True)
+        return p.wait()
