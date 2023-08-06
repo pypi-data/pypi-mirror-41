e42571708556022ed91680f990493a68a29b0c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package statick/0.1.10/statick-0.1.10.tar.gz

---
+++ packages/statick/statick-0.1.10.tar.gz/statick-0.1.10/setup.py
@@ -0,0 +1,32 @@
+"""Setup."""
+
+
+try:
+    from setuptools import setup
+except:  # pylint: disable=bare-except # noqa: E722
+    from distutils.core import setup  # pylint: disable=wrong-import-order
+
+import statick_tool
+
+with open('README.md') as f:
+    long_description = f.read()  # pylint: disable=invalid-name
+
+setup(
+    author='SSC Pacific',
+    name='statick',
+    description='Tool for running static analysis tools against packages of code.',
+    version=statick_tool.__version__,
+    packages=['statick_tool'],
+    package_data={'statick_tool': ['rsc/*', 'rsc/.clang-format',
+                                   'plugins/*.py', 'plugins/discovery/*',
+                                   'plugins/tool/*']},
+    scripts=['statick', 'statick_ws', 'statick_gauntlet'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['bandit', 'cmakelint', 'lizard', 'pycodestyle',
+                      'pydocstyle', 'pyflakes', 'yamllint', 'yapsy'],
+    url='https://github.com/sscpac/statick',
+    classifiers=[
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    ],
+)
