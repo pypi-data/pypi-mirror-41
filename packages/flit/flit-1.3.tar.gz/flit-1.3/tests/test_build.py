8abdc85a3c8e86d9383a60bd12cb676b5081359f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package flit/1.3/flit-1.3.tar.gz

---
+++ packages/flit/flit-1.3.tar.gz/flit-1.3/tests/test_build.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+import pytest
+import shutil
+import sys
+from tempfile import TemporaryDirectory
+from testpath import assert_isdir, MockCommand
+
+from flit import build, common
+
+samples_dir = Path(__file__).parent / 'samples'
+
+LIST_FILES_TEMPLATE = """\
+#!{python}
+import sys
+from os.path import join
+if '--deleted' not in sys.argv:
+    print('pyproject.toml')
+    print('{module}')
+    print('EG_README.rst')
+"""
+
+def test_build_main():
+    with TemporaryDirectory() as td:
+        pyproject = Path(td, 'pyproject.toml')
+        shutil.copy(str(samples_dir / 'module1-pkg.toml'), str(pyproject))
+        shutil.copy(str(samples_dir / 'module1.py'), td)
+        shutil.copy(str(samples_dir / 'EG_README.rst'), td)
+        Path(td, '.git').mkdir()   # Fake a git repo
+
+        with MockCommand('git', LIST_FILES_TEMPLATE.format(
+                python=sys.executable, module='module1.py')):
+            res = build.main(pyproject)
+        assert res.wheel.file.suffix == '.whl'
+        assert res.sdist.file.name.endswith('.tar.gz')
+
+        assert_isdir(Path(td, 'dist'))
+
+
+def test_build_module_no_docstring():
+    with TemporaryDirectory() as td:
+        pyproject = Path(td, 'pyproject.toml')
+        shutil.copy(str(samples_dir / 'no_docstring-pkg.toml'), str(pyproject))
+        shutil.copy(str(samples_dir / 'no_docstring.py'), td)
+        shutil.copy(str(samples_dir / 'EG_README.rst'), td)
+        Path(td, '.git').mkdir()   # Fake a git repo
+
+
+        with MockCommand('git', LIST_FILES_TEMPLATE.format(
+                python=sys.executable, module='no_docstring.py')):
+            with pytest.raises(common.NoDocstringError) as exc_info:
+                build.main(pyproject)
+            assert 'no_docstring.py' in str(exc_info.value)
---
+++ packages/flit/flit-1.3.tar.gz/flit-1.3/tests/test_wheel.py
@@ -0,0 +1,100 @@
+import configparser
+import os
+from pathlib import Path
+import shutil
+import tempfile
+from unittest import skipIf
+import zipfile
+
+import pytest
+from testpath import assert_isfile, assert_isdir
+
+from flit.wheel import wheel_main, WheelBuilder
+from flit.inifile import EntryPointsConflict
+
+samples_dir = Path(__file__).parent / 'samples'
+
+def clear_samples_dist():
+    try:
+        shutil.rmtree(str(samples_dir / 'dist'))
+    except FileNotFoundError:
+        pass
+
+def unpack(path):
+    z = zipfile.ZipFile(str(path))
+    t = tempfile.TemporaryDirectory()
+    z.extractall(t.name)
+    return t
+
+def test_wheel_module():
+    clear_samples_dist()
+    wheel_main(samples_dir / 'module1-pkg.ini')
+    assert_isfile(samples_dir / 'dist/module1-0.1-py2.py3-none-any.whl')
+
+def test_wheel_package():
+    clear_samples_dist()
+    wheel_main(samples_dir / 'package1-pkg.ini')
+    assert_isfile(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl')
+
+def test_dist_name():
+    clear_samples_dist()
+    wheel_main(samples_dir / 'altdistname.ini')
+    res = samples_dir / 'dist/package_dist1-0.1-py2.py3-none-any.whl'
+    assert_isfile(res)
+    with unpack(res) as td:
+        assert_isdir(Path(td, 'package_dist1-0.1.dist-info'))
+
+def test_entry_points():
+    clear_samples_dist()
+    wheel_main(samples_dir / 'entrypoints_valid.ini')
+    assert_isfile(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl')
+    with unpack(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl') as td:
+        entry_points = Path(td, 'package1-0.1.dist-info', 'entry_points.txt')
+        assert_isfile(entry_points)
+        cp = configparser.ConfigParser()
+        cp.read(str(entry_points))
+        assert 'console_scripts' in cp.sections()
+        assert 'myplugins' in cp.sections()
+
+def test_entry_points_conflict():
+    clear_samples_dist()
+    with pytest.raises(EntryPointsConflict):
+        wheel_main(samples_dir / 'entrypoints_conflict.ini')
+
+def test_wheel_builder():
+    # Slightly lower level interface
+    with tempfile.TemporaryDirectory() as td:
+        target = Path(td, 'sample.whl')
+        with target.open('wb') as f:
+            wb = WheelBuilder(samples_dir / 'package1-pkg.ini', f)
+            wb.build()
+
+        assert zipfile.is_zipfile(str(target))
+        assert wb.wheel_filename == 'package1-0.1-py2.py3-none-any.whl'
+
+@skipIf(os.name == 'nt', 'Windows does not preserve necessary permissions')
+def test_permissions_normed():
+    with tempfile.TemporaryDirectory() as td:
+        shutil.copy(str(samples_dir / 'module1.py'), td)
+        shutil.copy(str(samples_dir / 'module1-pkg.ini'), td)
+
+        Path(td, 'module1.py').chmod(0o620)
+        wheel_main(Path(td, 'module1-pkg.ini'))
+
+        whl = Path(td, 'dist', 'module1-0.1-py2.py3-none-any.whl')
+        assert_isfile(whl)
+        with zipfile.ZipFile(str(whl)) as zf:
+            info = zf.getinfo('module1.py')
+            perms = (info.external_attr >> 16) & 0o777
+            assert perms == 0o644, oct(perms)
+        whl.unlink()
+
+        # This time with executable bit set
+        Path(td, 'module1.py').chmod(0o720)
+        wheel_main(Path(td, 'module1-pkg.ini'))
+
+        assert_isfile(whl)
+        with zipfile.ZipFile(str(whl)) as zf:
+            info = zf.getinfo('module1.py')
+            perms = (info.external_attr >> 16) & 0o777
+            assert perms == 0o755, oct(perms)
