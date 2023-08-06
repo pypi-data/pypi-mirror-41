c04489cb597dc7e8f33705c24c9d85f0533c3f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:55 +0000

    Add package MetalScratch/0.0.2/metalScratch-0.0.2.tar.gz

---
+++ packages/MetalScratch/metalScratch-0.0.2.tar.gz/metalScratch-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+setup(name='metalScratch',
+      version='0.0.2',
+      description='metalScratch ',
+      url='https://github.com/yueiuieui/metal_sc_new',
+      author='metal_studio',
+      author_email='metal_studio@foxmail.com',
+      license='MIT',
+      packages=setuptools.find_packages())
