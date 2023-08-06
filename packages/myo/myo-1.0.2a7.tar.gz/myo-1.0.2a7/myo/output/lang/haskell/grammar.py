47f4665d4cc83925d0f2fdba441337ae05569f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package myo/1.0.2a7/myo-1.0.2a7.tar.gz

---
+++ packages/myo/myo-1.0.2a7.tar.gz/myo-1.0.2a7/myo/output/lang/haskell/grammar.py
@@ -0,0 +1,29 @@
+grammar = '''
+start: (specific | generic)+
+specific.5: dotted | undotted
+generic.1: any+
+dotted.5: dot (foundreq | notypeclass | notinscope | genericdot)
+undotted.1: notinscope
+
+foundreq.5: "Couldn't match" "expected"? "type" qname "with" "actual type"? qname
+notypeclass.5: "No instance for" parenstype "arising from" words qname
+notinscope.5: "Variable not in scope:" name "::" type+
+genericdot.1: any+
+dot: "•"
+qname: sql qnamedata sqr
+qnamedata: /[^’]+/
+parenstype: "(" name name ")"
+name: WORD
+words: WORD+
+type: /.+/
+sql: "‘"
+sqr: "’"
+any: (/[^•]+/ | NEWLINE)
+%import common.NEWLINE
+%import common.WS
+%import common.WORD
+%ignore WS
+%ignore NEWLINE
+'''
+
+__all__ = ('grammar',)
