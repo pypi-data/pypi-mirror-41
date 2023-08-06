98e751675c2c09ccbbff97528211e2ce1c73f0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package keepmenu/0.5.6/keepmenu-0.5.6.tar.gz

---
+++ packages/keepmenu/keepmenu-0.5.6.tar.gz/keepmenu-0.5.6/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python  # pylint: disable=missing-docstring
+
+from setuptools import setup
+
+setup(name="keepmenu",
+      version="0.5.6",
+      description="Dmenu frontend for Keepass databases",
+      long_description=open('README.rst', 'rb').read().decode('utf-8'),
+      author="Scott Hansen",
+      author_email="firecat4153@gmail.com",
+      url="https://github.com/firecat53/keepmenu",
+      download_url="https://github.com/firecat53/keepmenu/tarball/0.5.6",
+      scripts=['keepmenu'],
+      data_files=[('share/doc/keepmenu', ['README.rst', 'LICENSE.txt',
+                                          'config.ini.example']),
+                  ('share/man/man1', ['keepmenu.1'])],
+      install_requires=["PyUserInput", "pykeepass"],
+      license="MIT",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Utilities',
+      ],
+      keywords=("dmenu keepass keepassxc"),
+     )
