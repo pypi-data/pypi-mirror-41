ef844274b7ffc01502ccbb8ecd72f55b5f7e5dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:15 +0000

    Add package pandas-cub/0.0.1/pandas_cub-0.0.1.tar.gz

---
+++ packages/pandas-cub/pandas_cub-0.0.1.tar.gz/pandas_cub-0.0.1/README.md
@@ -0,0 +1,11 @@
+# Build your own Pandas Cub
+
+pandas_cub is a simple data analysis library. It is meant to be used as an instructional library for those looking to build their own data analysis libraries. You can complete the project at [this github repo][1].
+
+Install pandas_cub with:
+
+```
+pip install pandas_cub
+```
+
+[1]: https://github.com/tdpetrou/pandas_cub
---
+++ packages/pandas-cub/pandas_cub-0.0.1.tar.gz/pandas_cub-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pandas_cub",
+    version="0.0.1",
+    author="Ted Petrou",
+    author_email="ted@dunderdata.com",
+    description="A simple data analysis library similar to pandas",
+    url="https://github.com/dexplo/pandas_cub",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
