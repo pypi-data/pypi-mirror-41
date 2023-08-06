e6a29dd5114dd44f66692e42cc66dd4369b182a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package safe-parser/0.1.3/safe-parser-0.1.3.tar.gz

---
+++ packages/safe-parser/safe-parser-0.1.3.tar.gz/safe-parser-0.1.3/safeparser/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.3'
+
+from safeparser.parser import Parser, ParserException
---
+++ packages/safe-parser/safe-parser-0.1.3.tar.gz/safe-parser-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='safe-parser',
+    version='0.1.3',
+    author='João D. Ferreira',
+    author_email='jotomicron@gmail.com',
+    description='A simple parser of python-line code that can be safely executed',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jdferreira/safe-parser',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
