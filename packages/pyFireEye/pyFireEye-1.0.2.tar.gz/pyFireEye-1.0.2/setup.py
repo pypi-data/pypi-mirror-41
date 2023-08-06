cd51bedfa83f7f47810e878c9963ab4d9a264a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:29 +0000

    Add package pyFireEye/1.0.2/pyFireEye-1.0.2.tar.gz

---
+++ packages/pyFireEye/pyFireEye-1.0.2.tar.gz/pyFireEye-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+
+    with open("requirements.txt") as f:
+
+        requirements = [requirement.split("#")[0].strip() for requirement in f.read().split("\n") if requirement[0] != "#"]
+        return requirements
+
+
+setup(
+    version="1.0.2",
+    name="pyFireEye",
+    description="Python API bindings for FireEye Products",
+    long_description="",
+    url="https://gitlab.com/EmersonElectricCo/pyFireEye",
+    author="Isaiah Eichen, Grant Steiner, Timothy Lemm",
+    author_email="timothy.lemm@emerson.com",
+    packages=find_packages(),
+    install_requires=read_requirements()
+)
