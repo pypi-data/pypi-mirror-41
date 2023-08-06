f512a6e87b94c3c51779b3b919a1112686bc1dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package cpyc/0.0.1/cpyc-0.0.1.tar.gz

---
+++ packages/cpyc/cpyc-0.0.1.tar.gz/cpyc-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import argz
+
+
+author = 'bnbdr'
+setup(
+    name='cpyc',
+    version='0.0.1',
+    author=author,
+    author_email='bad.32@outlook.com',
+    description="cpyc",
+    long_description="cpyc",
+    license='MIT',
+    keywords='cpyc',
+    py_modules=['cpyc'],
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
