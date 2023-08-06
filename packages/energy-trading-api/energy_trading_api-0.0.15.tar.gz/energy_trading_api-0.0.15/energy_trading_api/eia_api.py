71dade4ea1eafd0f362e83fbe6fb5c32a48941e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:12 +0000

    Add package energy-trading-api/0.0.15/energy_trading_api-0.0.15.tar.gz

---
+++ packages/energy-trading-api/energy_trading_api-0.0.15.tar.gz/energy_trading_api-0.0.15/energy_trading_api/eia_api.py
@@ -0,0 +1,37 @@
+"""
+U.S. Energy Information Administration
+This is an API wrapper  for the eia-python API wrapper.
+
+Contains select calls to distinct Data Series, and returns pandas dataFrames
+
+"""
+import eia
+import pandas as pd
+from .common import commonData
+
+
+def __call_api(report,key):
+    api = eia.API(key)
+    series_search = api.data_by_series(series=commonData.dictEiaReports.get(report))
+    result = pd.DataFrame(series_search)
+    return result
+
+def eiaAPI(seriesID="",key=""):
+    return __call_api(seriesID,key)
+
+def coalAustraliaProduction(key=""):
+    return __call_api("australiaCoalProduction",key)
+
+def coalAustraliaConsumption(key=""):
+    return __call_api("australiaCoalConsumption",key)
+
+def ngAustraliaProduction(key=""):
+    return __call_api("australiaNGConsumption",key)
+
+def ngHenryHubSpotDaily(key=""):
+    return __call_api("henryHubNGSpotPriceDaily",key)
+
+def crudeCushingSpotDaily(key=""):
+    return __call_api("wtiCushingSpotPriceDaily",key)
+
+
