757e80754c8ef6efa046f14f0f15cb13c674313c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:18 +0000

    Add package supercommit/0.1.5/supercommit-0.1.5.tar.gz

---
+++ packages/supercommit/supercommit-0.1.5.tar.gz/supercommit-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='supercommit',
+    version='0.1.5',
+    author='zhangbinhui',
+    author_email="maru-zhang@foxmail.com",
+    packages=['supercommit'],
+    install_requires=['Click', 'inquirer', 'terminaltables', 'configparser', 'translate'],
+    entry_points='''
+        [console_scripts]
+        git-sc=supercommit.index:cli
+    '''
+)
