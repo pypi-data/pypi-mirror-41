31fd01b5fbc515a225ae1db0dac043a27e1351c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:58 +0000

    Add package mcmcAllotter/0.0.1/mcmcAllotter-0.0.1.tar.gz

---
+++ packages/mcmcAllotter/mcmcAllotter-0.0.1.tar.gz/mcmcAllotter-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mcmcAllotter",
+    version="0.0.1",
+    author="Saurabh Kumar",
+    author_email="saurabhkm@hotmail.com",
+    description="Solve allottment problems with MCMC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saurabhkm/mcmcAllotter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning',
+        'Natural Language :: English',
+        'Intended Audience :: Education',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
