d5fd990350c5f5a77d74b8d31d99d4019b97cada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:15 +0000

    Add package pep8-naming/0.8.1/pep8-naming-0.8.1.tar.gz

---
+++ packages/pep8-naming/pep8-naming-0.8.1.tar.gz/pep8-naming-0.8.1/testsuite/N804.py
@@ -0,0 +1,36 @@
+#: N804:7:13
+class Foo(object):
+    @classmethod
+    def mmm(cls, ads):
+        pass
+
+    @classmethod
+    def bad(self, ads):
+        pass
+
+    @calling()
+    def test(self, ads):
+        pass
+
+    def __init_subclass(self, ads):
+        pass
+#: N804:3:14(--classmethod-decorators=clazzy,cool)
+class NewClassIsRequired(object):
+    @cool
+    def test(self, sy):
+        pass
+#: N804
+class Meta(type):
+    def __new__(self, name, bases, attrs):
+        pass
+#: Okay
+class MetaMethod(type):
+    def test(cls):
+        pass
+#: Okay
+class NotMeta(object):
+    otherclass = Foo
+class AttributeParent(NotMeta.otherclass):
+    pass
+class CallParent(type('_tmp', (), {})):
+    pass
