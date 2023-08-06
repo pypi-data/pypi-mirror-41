8bc67941f57b802da1b21aec7a3507fdd25ba26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:07 +0000

    Add package pytorch-complex-tensor/0.0.11/pytorch-complex-tensor-0.0.11.tar.gz

---
+++ packages/pytorch-complex-tensor/pytorch-complex-tensor-0.0.11.tar.gz/pytorch-complex-tensor-0.0.11/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pytorch-complex-tensor',
+      version='0.0.11',
+      description='Pytorch complex tensor',
+      author='',
+      author_email='',
+      url='https://github.com/williamFalcon/pytorch-complex-tensor',
+      install_requires=[
+            'numpy>=1.15.4',
+            'torch>=1.0',
+            'torchvision>=0.2.1'
+      ],
+      packages=find_packages()
+      )
