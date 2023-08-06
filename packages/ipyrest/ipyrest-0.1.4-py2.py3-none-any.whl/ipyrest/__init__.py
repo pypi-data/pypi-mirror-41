076890d2ec744055d2777e23250c304a1a28016d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package ipyrest/0.1.4/ipyrest-0.1.4-py2.py3-none-any.whl

---
+++ packages/ipyrest/ipyrest-0.1.4-py2.py3-none-any.whl/ipyrest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Ipyrest."""
+
+__author__ = 'Dinu Gherman'
+__email__ = '@'.join(['gherman', 'darwin.in-berlin.de'])
+__version__ = '0.1.4'
+
+from .ipyrest import Api, recorder
+from .extendedtab import ExtendedTab
