5314f8f219acf6a14c539b30b064df87e230c209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:28 +0000

    Add package gtl-ville/2.0.2/gtl_ville-2.0.2-py3-none-any.whl

---
+++ packages/gtl-ville/gtl_ville-2.0.2-py3-none-any.whl/gv_services/geographer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from gv_services.storage.dbstorage.dbstorage import DbStorage
+
+
+class Geographer:
+
+    def __init__(self, logger):
+        super().__init__()
+        self.logger = logger
+        self.dbstorage = DbStorage()
+        self.network = None
+
+    async def async_init(self):
+        await self.dbstorage.async_init()
+
+    async def get_data_points(self, stream):
+        pass
+
+    async def get_roads(self, stream):
+        pass
+
+    async def get_zones(self, stream):
+        pass
+
+    async def get_mapping_roads_data_points(self, stream):
+        pass
+
+    async def get_mapping_zones_roads(self, stream):
+        pass
+
+    async def update_roads_freeflow_speed(self, stream):
+        pass
+
+    async def update_zones_freeflow_speed(self, stream):
+        pass
