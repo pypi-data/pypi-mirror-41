2c1c5e4c087d6782e0efec6983a2f8ea7a3ed29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:52 +0000

    Add package object-modeler/1.0.2/object_modeler-1.0.2.tar.gz

---
+++ packages/object-modeler/object_modeler-1.0.2.tar.gz/object_modeler-1.0.2/object_modeler/__init__.py
@@ -0,0 +1,6 @@
+from object_modeler.common import Field
+from object_modeler.dict_object_model import GenericDictObjectModel, ObjectModel
+from object_modeler.slots_object_model import GenericSlotsObjectModel, SlotsObjectModel, SlotsObjectModelKwargs
+
+__all__ = ['GenericSlotsObjectModel', 'GenericDictObjectModel', 'ObjectModel', 'Field',
+           'SlotsObjectModel', 'SlotsObjectModelKwargs']
