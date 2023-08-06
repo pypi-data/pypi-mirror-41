bf92d2971e12306ac8f3944f5f6992e471e02c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package marv-robotics/19.2.0/marv-robotics-19.2.0.tar.gz

---
+++ packages/marv-robotics/marv-robotics-19.2.0.tar.gz/marv-robotics-19.2.0/marv_robotics/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 - 2018  Ternaris.
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from __future__ import absolute_import, division, print_function
+
+__version__ = '19.02.0'
+
+from . import _monkeypatches
