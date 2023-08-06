caa550475260653e368d3487285fd125c22112e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:43 +0000

    Add package bioprinter/0.1.0/bioprinter-0.1.0.tar.gz

---
+++ packages/bioprinter/bioprinter-0.1.0.tar.gz/bioprinter-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+exec(open('bioprinter/version.py').read())  # loads __version__
+
+setup(name='bioprinter',
+      version=__version__,
+      author='Zulko',
+      description='',
+      long_description=open('README.rst').read(),
+      license='see LICENSE.txt',
+      keywords="bioprinter pointillism living art",
+      packages=find_packages(exclude='docs'),
+      install_requires=['numpy', 'Pillow', 'pytest'])
