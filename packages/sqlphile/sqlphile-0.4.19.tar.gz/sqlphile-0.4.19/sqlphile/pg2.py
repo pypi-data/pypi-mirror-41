af9961c04723f0255e945b33bfe9e1e9bc1431b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:11 +0000

    Add package sqlphile/0.4.19/sqlphile-0.4.19.tar.gz

---
+++ packages/sqlphile/sqlphile-0.4.19.tar.gz/sqlphile-0.4.19/sqlphile/pg2.py
@@ -0,0 +1,16 @@
+from . import db3
+import psycopg2
+from .dbtypes import DB_PGSQL, DB_SQLITE3
+
+class open (db3.open):
+	def __init__ (self, dbname, user, password, host = '127.0.0.1', port = 5432, dir = None, auto_reload = False):
+		if ":" in host:
+			host, port = host.split (":")
+			port = int (port)
+		self.conn = psycopg2.connect (host=host, dbname=dbname, user=user, password=password, port = port)
+		self.__init (dir, auto_reload, DB_PGSQL)		
+		
+	def field_names (self):
+		return [x.name for x in self.description]
+		
+		
