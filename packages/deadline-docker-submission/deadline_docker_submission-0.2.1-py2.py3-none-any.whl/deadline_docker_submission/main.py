b1965437bc45a2002a844296a88d985dc2844e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:29 +0000

    Add package deadline-docker-submission/0.2.1/deadline_docker_submission-0.2.1-py2.py3-none-any.whl

---
+++ packages/deadline-docker-submission/deadline_docker_submission-0.2.1-py2.py3-none-any.whl/deadline_docker_submission/main.py
@@ -0,0 +1,121 @@
+import argparse
+import logging
+import os
+import platform
+import sys
+from subprocess import Popen, PIPE
+
+logger = logging.getLogger()
+
+
+class ExeNotFound(Exception):
+    """ Custom exception for catching cases where an executable isn't found. """
+    pass
+
+
+class DeadlineDockerSubmitter(object):
+    """ Simple class for use in submitting docker jobs to the deadline render manager. """
+
+    def __init__(self):
+        """
+        Initializes an instance of this class.
+        """
+        logger.debug('instantiating DockerSubmitter instance')
+        self._exe = r'${DEADLINE_PATH}\deadlinecommand.exe'
+        self._command = None
+        self._pool = 'docker'
+        self._docker = r'C:\PROGRA~1\Docker Toolbox\docker.exe'
+
+    @property
+    def exe(self):
+        """
+        Attempts to return the path to the deadlinecommand.exe for use in submitting
+        a command line job to the deadline render farm manager.
+
+        Returns:
+            full path to the deadlinecommand.exe
+
+        Raises:
+            ExeNotFound error if the deadlinecommand.exe doesn't exist
+        """
+        exe = os.path.expandvars(self._exe)
+
+        if platform.system().lower() != 'windows':
+            exe = exe.split('.')[0]
+
+        if not os.path.exists(exe):
+            raise ExeNotFound('{} not found!. unable to submit.'.format(exe))
+        return exe
+
+    def __call__(self, command, name=None, priority=50):
+        """
+        Submits the command to the farm using deadlinecommand.exe and returns a
+        tuple containing the return code from the process.
+
+        Args:
+            command (str): the docker command to actually execute
+            name (str): optional name of the job, default: 'Docker-job'
+            priority (str): the priority of the job, default: 50
+
+        Returns (int):
+            deadlinecommand.exe return code
+        """
+        logger.debug('attempting deadline commandline submission')
+        if not name:
+            name = 'Docker-job'
+
+        if not priority:
+            priority = '50'
+
+        p = Popen([self.exe, '-SubmitCommandlineJob', '-executable', self._docker, '-arguments', command, '-priority',
+                   str(priority), '-name', name, '-pool', self._pool], stderr=PIPE, stdout=PIPE)
+
+        out, err = p.communicate()
+
+        if out:
+            logger.info(out)
+
+        if err:
+            logger.error(err)
+
+        return p.returncode
+
+
+def parse_args(args):
+    """
+
+    Args:
+        args(list): variable length list of arguments that must meet the requirements below.
+
+    Returns:
+        Namespace(command, name, priority, loglevel)
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--command', type=str, help='command to run with docker', required=True)
+    parser.add_argument('-n', '--name', type=str, help='name of job')
+    parser.add_argument('-p', '--priority', type=int, help='priority of the job', default=50)
+    parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG)
+    parser.add_argument('-v', '--verbose', action='store_const', dest='loglevel', const=logging.INFO)
+
+    return parser.parse_args(args)
+
+
+def main():
+    """
+    The main entry point of this particular program. Will attempt to submit a commandline job to
+    the deadline render farm manager that will run the 'command' argument via docker on the
+    worker nodes that exist in the 'docker' pool. It assumes there are nodes in the pool and that
+    the docker daemon is installed and running on the worker.
+
+    Returns:
+        The return code of the deadlinecommand.exe
+
+    """
+    args = parse_args(sys.argv[1:])
+    logging.basicConfig(level=args.loglevel)
+
+    return DeadlineDockerSubmitter()(args.command, args.name, args.priority)[0]
+
+
+if __name__ == '__main__':
+    sys.exit(main())
