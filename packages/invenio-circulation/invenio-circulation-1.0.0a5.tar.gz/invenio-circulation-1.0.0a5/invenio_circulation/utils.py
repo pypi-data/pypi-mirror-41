af52602bea7a4209d849c348f6a5df833fb0f83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:21 +0000

    Add package invenio-circulation/1.0.0a5/invenio-circulation-1.0.0a5.tar.gz

---
+++ packages/invenio-circulation/invenio-circulation-1.0.0a5.tar.gz/invenio-circulation-1.0.0a5/invenio_circulation/utils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+# Copyright (C) 2018 RERO.
+#
+# Invenio-Circulation is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Circulation API."""
+
+from datetime import timedelta
+
+import ciso8601
+
+
+def patron_exists(patron_pid):
+    """Return True if patron exists, False otherwise."""
+    return False
+
+
+def item_exists(item_pid):
+    """Return True if item exists, False otherwise."""
+    return False
+
+
+def is_item_available(item_pid):
+    """."""
+    return True
+
+
+# NOTE: Its on purpose `ref` and not `$ref` so it doesn't try to resolve
+def item_ref_builder(item_pid):
+    """Return the $ref for item_pid."""
+    return {"ref": "{}".format(item_pid)}
+
+
+def item_location_retriever(item_pid):
+    """Retrieve the location pid of the passed item pid."""
+    return ""
+
+
+def get_default_loan_duration(loan):
+    """Return a default loan duration in number of days."""
+    return 30
+
+
+def get_default_extension_duration(loan):
+    """Return a default extension duration in number of days."""
+    return 30
+
+
+def get_default_extension_max_count(loan):
+    """Return a default extensions max count."""
+    return float("inf")
+
+
+def is_loan_duration_valid(loan):
+    """Validate the loan duration."""
+    return loan["end_date"] > loan["start_date"] and loan["end_date"] - loan[
+        "start_date"
+    ] < timedelta(days=60)
+
+
+def parse_date(str_date):
+    """Parse string date with timezone and return a datetime object."""
+    return ciso8601.parse_datetime(str_date)
