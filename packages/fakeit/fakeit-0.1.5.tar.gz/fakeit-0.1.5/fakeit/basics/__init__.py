5b45c8f4e48aeac1215241d4bcde0ff87c30c94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package fakeit/0.1.5/fakeit-0.1.5.tar.gz

---
+++ packages/fakeit/fakeit-0.1.5.tar.gz/fakeit-0.1.5/fakeit/basics/__init__.py
@@ -0,0 +1,9 @@
+from . import bytes
+# from . import geo
+from . import hashes
+from . import numerics
+from . import personal
+from . import strings
+from . import boolean
+# from . import texts
+# from . import uuids
