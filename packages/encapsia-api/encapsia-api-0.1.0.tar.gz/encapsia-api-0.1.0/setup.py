10dda01cc4b179c42aabde3b10d0bce73914cf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:34 +0000

    Add package encapsia-api/0.1.0/encapsia-api-0.1.0.tar.gz

---
+++ packages/encapsia-api/encapsia-api-0.1.0.tar.gz/encapsia-api-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['encapsia_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests[security]>=2.21,<3.0']
+
+setup_kwargs = {
+    'name': 'encapsia-api',
+    'version': '0.1.0',
+    'description': 'Client API for talking to an Encapsia system.',
+    'long_description': None,
+    'author': 'Timothy Corbett-Clark',
+    'author_email': 'timothy.corbettclark@gmail.com',
+    'url': 'https://github.com/tcorbettclark/encapsia-api',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
