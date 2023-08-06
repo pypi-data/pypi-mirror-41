84515bd4f74aa8d26e977e9bdbc99f6159b3bcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package zippy-pipeline/2.1.3.0/zippy-pipeline-2.1.3.0.tar.gz

---
+++ packages/zippy-pipeline/zippy-pipeline-2.1.3.0.tar.gz/zippy-pipeline-2.1.3.0/setup.py
@@ -0,0 +1,44 @@
+"""
+Setup for Zippy (zippy)
+"""
+from setuptools import setup, find_packages
+
+package = __import__('zippy')
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+try:
+    long_desc = readme()
+except IOError as err:
+    long_desc = str(err)
+
+try:
+    version_str = open('version.txt').read()
+except IOError as err:
+    version_str = '2.0.0'
+
+setup(
+    name='zippy-pipeline',
+    long_description=long_desc,
+    url='https://github.com/Illumina/zippy',
+    download_url='https://github.com/Illumina/zippy/archive/2.1.3.tar.gz',
+    author='Aaron Wise',
+    author_email='quejebo@gmail.com',
+    license='Apache 2.0',
+    install_requires=[
+        'commentjson',
+        'pybloomfiltermmap',
+        'pyflow>=1.1.0'
+        ],
+    dependency_links=['https://github.com/Illumina/pyflow/releases/download/v1.1.17/pyflow-1.1.17.tar.gz'],
+    version=version_str,
+    description='NGS Pipeline Prototyping Tool for Python',
+    include_package_data=True,
+    packages=find_packages(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    scripts=[],
+    requires=[],
+    zip_safe=False)
