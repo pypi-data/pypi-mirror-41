aa5b9846499c355b1aa939d100c3c97cb0791d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:52 +0000

    Add package fdspy/0.0.10/fdspy-0.0.10-py3-none-any.whl

---
+++ packages/fdspy/fdspy-0.0.10-py3-none-any.whl/fdspy/fdsfront/__main__.py
@@ -0,0 +1,15 @@
+
+if __name__ == '__main__':
+
+    from fdspy.fds_cls import ClientAgentFront
+    import os
+
+    os. system("title "+"FDS FRONT")
+
+    CAB = ClientAgentFront(
+        path_fds_queue=os.path.join('C:', os.sep, 'APP', 'fdspy', 'fds_queue.json')
+    )
+
+    CAB.start()
+
+    print('Front agent is running...')
