57c68a51ea1ee38ba2d7f4c6a781bc87fa78eb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package gocli/0.8.12/gocli-0.8.12-py2.py3-none-any.whl

---
+++ packages/gocli/gocli-0.8.12-py2.py3-none-any.whl/genomoncology/pipeline/sinks/__init__.py
@@ -0,0 +1,21 @@
+from .base import Sink, JsonlFileSink, TextFileSink
+from .tsv import TsvFileSink
+from .excel import ExcelSink
+from .alterations import TempAlterationSink
+from .vcf import VcfFileSink
+from .annotations import LoadAnnotationSink
+from .warehouse import LoadWarehouseVariantsSink, LoadWarehouseFeaturesSink
+
+
+__all__ = (
+    "Sink",
+    "JsonlFileSink",
+    "TextFileSink",
+    "TsvFileSink",
+    "ExcelSink",
+    "TempAlterationSink",
+    "VcfFileSink",
+    "LoadAnnotationSink",
+    "LoadWarehouseVariantsSink",
+    "LoadWarehouseFeaturesSink",
+)
