9de03fad319b5f47d85917ad8e7756ab744f341e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:47 +0000

    Add package skycalc-ipy/0.1.1.dev0/skycalc_ipy-0.1.1.dev0.tar.gz

---
+++ packages/skycalc-ipy/skycalc_ipy-0.1.1.dev0.tar.gz/skycalc_ipy-0.1.1.dev0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    __readme__ = f.read()
+
+with open('LICENCE') as f:
+    __license__ = f.read()
+
+__version__ = "0.1.1dev"
+
+setup(
+    name='skycalc_ipy',
+    version=__version__,
+    description='Get atmospheric spectral information',
+    long_description=__readme__,
+    author='Kieran Leschinski',
+    author_email='kieran.leschinski@univie.ac.at',
+    url='https://github.com/astronomyk/skycalc_ipy',
+    license=__license__,
+    include_package_data=True,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['requests', 'pyyaml', 'numpy', 'astropy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        ],
+    )
---
+++ packages/skycalc-ipy/skycalc_ipy-0.1.1.dev0.tar.gz/skycalc_ipy-0.1.1.dev0/skycalc_ipy/__init__.py
@@ -0,0 +1,11 @@
+import warnings as _warnings
+from astropy.utils.exceptions import AstropyWarning as _AstropyWarning
+_warnings.simplefilter('ignore', category=_AstropyWarning)
+
+from . import ui
+from . import core
+
+from .ui import SkyCalc
+
+name = "skycalc_ipy"
+
