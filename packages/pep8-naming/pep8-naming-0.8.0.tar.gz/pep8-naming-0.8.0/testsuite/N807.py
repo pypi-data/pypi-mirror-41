7d50fe1897e7929dee2d8191f3c3c249b9d638ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package pep8-naming/0.8.0/pep8-naming-0.8.0.tar.gz

---
+++ packages/pep8-naming/pep8-naming-0.8.0.tar.gz/pep8-naming-0.8.0/testsuite/N807.py
@@ -0,0 +1,43 @@
+#: Okay
+class C1:
+    def __str__(self):
+        return ''
+#: Okay
+class C2:
+    if True:
+        def __str__(self):
+            return ''
+#: N807
+if True:
+    def __bad__():
+        pass
+#: Okay
+class C3:
+    try:
+        if True:
+            while True:
+                def __str__(self):
+                    return ''
+                break
+    except:
+        pass
+#: N807
+def __bad():
+    pass
+#: N807:1:5
+def bad__():
+    pass
+#: N807
+def __bad__():
+    pass
+#: N807
+class ClassName(object):
+    def method(self):
+        def __bad():
+            pass
+#: Okay
+def __dir__():
+    pass
+#: Okay
+def __getattr__(name):
+    pass
