276bf2396c4cd00e9fe61b2a3596fce594e1f770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:21 +0000

    Add package example-pkg-ParkNamsun5d2422/0.0.2/example-pkg-ParkNamsun5d2422-0.0.2.tar.gz

---
+++ packages/example-pkg-ParkNamsun5d2422/example-pkg-ParkNamsun5d2422-0.0.2.tar.gz/example-pkg-ParkNamsun5d2422-0.0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="example-pkg-ParkNamsun5d2422",
+    version='0.0.2',
+    description='A Small example package',
+    author='rakens2',
+    packages=setuptools.find_packages()
+)
