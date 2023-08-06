84e2104ec99fa0d365b1a36e0b1adea87c93504b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:52 +0000

    Add package django-multi-form-view/2.0.1/django-multi-form-view-2.0.1.tar.gz

---
+++ packages/django-multi-form-view/django-multi-form-view-2.0.1.tar.gz/django-multi-form-view-2.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='django-multi-form-view',
+    version='2.0.1',
+    author=u'Tim Best',
+    packages=find_packages(),
+    url='https://github.com/TimBest/django-multi-form-view',
+    license='GNU licence, see LICENCE',
+    description='Class based views for handling more than one form in a single view',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+)
