87e4cf8f29743f20965bc26e107d37a2b5516c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:55 +0000

    Add package revscoring/2.3.4/revscoring-2.3.4.tar.gz

---
+++ packages/revscoring/revscoring-2.3.4.tar.gz/revscoring-2.3.4/revscoring/languages/tests/util.py
@@ -0,0 +1,22 @@
+
+
+def compare_extraction(extractor, examples, counter_examples,
+                       lwrap="", rwrap=""):
+
+    for example in examples:
+        wrapped = lwrap + example + rwrap
+        # print(extractor.process(wrapped), example)
+        assert extractor.process(wrapped) == [example]
+        assert extractor.process(
+            "Sentence " +
+            wrapped +
+            " sandwich.") == [example]
+        assert extractor.process("Sentence end " + wrapped + ".") == [example]
+        assert extractor.process(wrapped + " start of sentence.") == [example]
+
+    for example in counter_examples:
+        wrapped = lwrap + example + rwrap
+        assert extractor.process(wrapped) == [], str(extractor.process(wrapped))
+        assert extractor.process("Sentence " + wrapped + " sandwich.") == []
+        assert extractor.process("Sentence end " + wrapped + ".") == []
+        assert extractor.process(wrapped + " start of sentence.") == []
