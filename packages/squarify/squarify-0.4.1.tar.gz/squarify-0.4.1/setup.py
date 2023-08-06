361a70cf483eabc547d0791a0ab635736cd472e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package squarify/0.4.1/squarify-0.4.1.tar.gz

---
+++ packages/squarify/squarify-0.4.1.tar.gz/squarify-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="squarify",
+    version="0.4.1",
+    packages=["squarify"],
+    author="Uri Laserson",
+    author_email="uri.laserson@gmail.com",
+    description="Pure Python implementation of the squarify treemap layout algorithm",
+    license="Apache license, version 2",
+    keywords="treemap visualization squarify layout graphics",
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    url="https://github.com/laserson/squarify",
+)
