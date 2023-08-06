b06ae0d178ed540886d15ba45b23b18c71f43bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:09 +0000

    Add package scary/0.1/scary-0.1.tar.gz

---
+++ packages/scary/scary-0.1.tar.gz/scary-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scary',
+      version='0.1',
+      description='What if something breaks',
+      url="http://github.com/gauravmhkd25/scary",
+      author='Flying Circus',
+      author_email='gauravmhkd25@gmail.com',
+      license='MIT',
+      packages=['scary'],
+      zip_safe=False)
