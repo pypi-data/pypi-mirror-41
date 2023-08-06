384e2e0480c3cadb0bd32630c5aa967151588641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:46 +0000

    Add package grpcalchemy/0.2.0.post0/grpcalchemy-0.2.0.post0-py2.py3-none-any.whl

---
+++ packages/grpcalchemy/grpcalchemy-0.2.0.post0-py2.py3-none-any.whl/grpcalchemy/__init__.py
@@ -0,0 +1,14 @@
+"""
+    gRPCAlchemy
+    ~~~~~
+
+    The Python micro framework for building gPRC application.
+"""
+
+__author__ = """GuangTian Li"""
+__email__ = 'guangtian_li@qq.com'
+__version__ = '0.2.0'
+
+from .blueprint import Blueprint, Context
+from .client import Client
+from .server import Server
