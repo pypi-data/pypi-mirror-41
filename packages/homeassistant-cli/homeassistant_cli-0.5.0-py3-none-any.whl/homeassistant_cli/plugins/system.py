a9bea39810d5d7df7348f2a80301d9f7a1f07772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package homeassistant-cli/0.5.0/homeassistant_cli-0.5.0-py3-none-any.whl

---
+++ packages/homeassistant-cli/homeassistant_cli-0.5.0-py3-none-any.whl/homeassistant_cli/plugins/system.py
@@ -0,0 +1,21 @@
+"""System plugin for Home Assistant CLI (hass-cli)."""
+import logging
+
+import click
+from homeassistant_cli.cli import pass_context
+import homeassistant_cli.remote as api
+
+_LOGGING = logging.getLogger(__name__)
+
+
+@click.group('system')
+@pass_context
+def cli(ctx):
+    """System details and operations for Home Assistant."""
+
+
+@cli.command()
+@pass_context
+def log(ctx):
+    """Get errors from Home Assistant."""
+    click.echo(api.get_raw_error_log(ctx))
