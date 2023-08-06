0b1dd35aedeb0681bafef5d1d221dfdc0bc2d6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:55 +0000

    Add package trex-stl-lib/0.3/trex_stl_lib-0.3.tar.gz

---
+++ packages/trex-stl-lib/trex_stl_lib-0.3.tar.gz/trex_stl_lib-0.3/README
@@ -0,0 +1,2 @@
+Python 3 only
+Tested on Python 3.6 and later
---
+++ packages/trex-stl-lib/trex_stl_lib-0.3.tar.gz/trex_stl_lib-0.3/src/trex_stl_lib/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import sys
+
+if sys.version_info < (2, 7):
+    print("\n**** TRex STL package requires Python version >= 2.7 ***\n")
+    exit(-1)
