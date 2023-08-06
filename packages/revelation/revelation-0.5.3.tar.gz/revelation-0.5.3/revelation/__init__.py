88209f81d28a2333fc5a0a7a003a1020a19b31c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:23 +0000

    Add package revelation/0.5.3/revelation-0.5.3.tar.gz

---
+++ packages/revelation/revelation-0.5.3.tar.gz/revelation-0.5.3/revelation/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Revelation root module with package info"""
+
+from .app import PresentationReloader, Revelation
+
+__author__ = "Humberto Rocha"
+__email__ = "humrochagf@gmail.com"
+__version__ = "0.5.3"
+__all__ = ["PresentationReloader", "Revelation"]
