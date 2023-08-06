80f9af056ce04d77c86ce67328061b9164a26883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package metalearn/0.5.1/metalearn-0.5.1.tar.gz

---
+++ packages/metalearn/metalearn-0.5.1.tar.gz/metalearn-0.5.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.5.1'
+
+setup(
+    name = 'metalearn',
+    packages = find_packages(),
+    version = __version__,
+    description = 'A package to aid in metalearning',
+    author = 'Roland Laboulaye, Brandon Schoenfeld, Casey Davis',
+    author_email = 'rlaboulaye@gmail.com, bjschoenfeld@gmail.com, caseykdavis@gmail.com',
+    url = 'https://github.com/byu-dml/metalearn',
+    download_url = 'https://github.com/byu-dml/metalearn/archive/{}.tar.gz'.format(__version__),
+    keywords = ['metalearning', 'machine learning', 'metalearn'],
+    install_requires = [
+        'numpy<=1.15.2',
+        'scipy<=1.1.0',
+        'scikit-learn<=0.20.2',
+        'pandas<=0.23.4,>=0.21.0'
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3.6'
+    ],
+    python_requires='~=3.6',
+    include_package_data=True,
+    tests_require=[
+        'arff2pandas<=1.0.1',
+        'jsonschema<=2.6.0'
+    ]
+)
