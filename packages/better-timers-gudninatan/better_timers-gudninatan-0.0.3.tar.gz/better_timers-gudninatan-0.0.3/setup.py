af52ce15d497ee25b23f9d98ac7b6e83e73c1900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:36 +0000

    Add package better-timers-gudninatan/0.0.3/better_timers-gudninatan-0.0.3.tar.gz

---
+++ packages/better-timers-gudninatan/better_timers-gudninatan-0.0.3.tar.gz/better_timers-gudninatan-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="better_timers-gudninatan",
+    version="0.0.3",
+    author="Guðni Natan Gunnarsson",
+    author_email="author@example.com",
+    description="Better timers for pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GudniNatan/better_timers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pygame',
+    ],
+)
