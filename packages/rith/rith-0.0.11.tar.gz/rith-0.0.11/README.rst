009cdd3964bf73cd30adc0038a5bfd45c96235fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:52 +0000

    Add package rith/0.0.11/rith-0.0.11.tar.gz

---
+++ packages/rith/rith-0.0.11.tar.gz/rith-0.0.11/README.rst
@@ -0,0 +1,27 @@
+.. image::https://travis-ci.org/rith-io/rith-core.svg?branch=master
+
+Arithmetic Framework
+====================
+Arithmetic helps you create production-ready APIs so that your team can build faster, smarter, and more secure.
+
+.. code::
+  
+  pip install rith
+
+Best Practices
+--------------
+When working with Python applications, it's always advisable to work within
+the constraints of a Virtual Environment.
+
+Arithemtic and the `rith` tools require Python 3+. To create a virtual environment
+that uses Python 3 execute the following command in your project directory.
+
+.. code:: 
+
+  virtualenv -p python3 venv
+      
+Start Your Virtual Environment
+
+.. code::
+  
+  source venv/bin/activate
---
+++ packages/rith/rith-0.0.11.tar.gz/rith-0.0.11/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""Arithmetic Geospatial Data API.
+"""
+
+
+import io
+from setuptools import setup
+from setuptools import find_packages
+
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+
+setup(
+    name="rith",
+    version="0.0.11",
+    description="Arithmetic is a simple framework for creating production-ready APIs so that your team can build faster, smarter, and more secure.",
+    long_description=readme,
+    url="https://github.com/rith-io/rith-core",
+    author="Joshua Powell",
+    maintainer="Arithmetic Framework Team",
+    license="MIT",
+    keywords="python geospatial location api",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "flask-security",
+        "flask-restless",
+        "flask-sqlalchemy",
+        "geoalchemy2",
+        "flask-oauthlib",
+        "requests",
+        "psycopg2-binary",
+        "Wand"
+    ],
+    extras_require={
+        'dev': [
+            'bandit',
+            'nose',
+            'pycodestyle',
+            'pydocstyle',
+            'pylint',
+            'tox',
+            'twine',
+        ],
+        'docs': []
+    },
+)
