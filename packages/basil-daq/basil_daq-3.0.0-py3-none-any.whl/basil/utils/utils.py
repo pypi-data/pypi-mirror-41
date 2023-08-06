dec91b4d4a71d6278e88da834651e80cc6de82b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:50 +0000

    Add package basil-daq/3.0.0/basil_daq-3.0.0-py3-none-any.whl

---
+++ packages/basil-daq/basil_daq-3.0.0-py3-none-any.whl/basil/utils/utils.py
@@ -0,0 +1,52 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+
+from __future__ import print_function
+from array import array
+
+import numpy as np
+from bitarray import bitarray
+
+
+def logging(fn):
+    def wrapped(*args, **kargs):
+        print(('loging:', locals()))
+#         if args:
+#             print("loging: arguments: " + str(args))
+#         if kargs:
+#             print("loging: kargs: " + str(kargs))
+        return fn(*args, **kargs)
+    return wrapped
+
+
+def bitvector_to_byte_array(bitvector):
+    bsize = len(bitvector)
+    size_bytes = int(((bsize - 1) / 8) + 1)
+    bs = tobytes(array('B', bitvector.vector))[0:size_bytes]
+    bitstream_swap = ''
+    lsbits = lambda b: (b * 0x0202020202 & 0x010884422010) % 1023
+    for b in bs:
+        bitstream_swap += chr(lsbits(b))
+    return array('B', bitstream_swap)
+
+
+def bitarray_to_byte_array(bitarr):
+    ba = bitarray(bitarr, endian=bitarr.endian())
+    ba.reverse()  # this flip the byte order and the bit order of each byte
+    bs = np.fromstring(ba.tobytes(), dtype=np.uint8)  # byte padding happens here, bitarray.tobytes()
+    bs = (bs * 0x0202020202 & 0x010884422010) % 1023
+    return array('B', bs.astype(np.uint8))
+
+# Python 2/3 compatibility function for array.tobytes function
+try:
+    array.tobytes
+except AttributeError:  # Python 2
+    def tobytes(v):
+        return v.tostring()
+else:  # Python 3
+    def tobytes(v):
+        return v.tobytes()
