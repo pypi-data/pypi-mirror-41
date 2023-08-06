06377ac8367e45e368791e392c8d99be4344af43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:18 +0000

    Add package pandoc-crossref/0.1.1/pandoc-crossref-0.1.1.tar.gz

---
+++ packages/pandoc-crossref/pandoc-crossref-0.1.1.tar.gz/pandoc-crossref-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pandoc-crossref',
+    version='0.1.1',
+
+    description='Stub that prevents name clash with https://github.com/conda-forge/pandoc-crossref-feedstock For pandoc-crossref see https://github.com/kiwi0fruit/py-pandoc-crossref',
+    url='https://github.com/kiwi0fruit/py-pandoc-crossref',
+    author='kiwi0fruit',
+    author_email='peter.zagubisalo@gmail.com',
+    license='MIT',
+)
