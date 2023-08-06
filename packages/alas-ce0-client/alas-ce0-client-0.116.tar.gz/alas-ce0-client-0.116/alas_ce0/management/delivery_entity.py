8b518efed374b45136b06358b46b37f905b02bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:28 +0000

    Add package alas-ce0-client/0.116/alas-ce0-client-0.116.tar.gz

---
+++ packages/alas-ce0-client/alas-ce0-client-0.116.tar.gz/alas-ce0-client-0.116/alas_ce0/management/delivery_entity.py
@@ -0,0 +1,25 @@
+from alas_ce0.common.client_base import EntityClientBase
+
+
+class B2BClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/b2bs/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(B2BClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
+
+
+class IntermediateCarrierClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/intermediate-carriers/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(IntermediateCarrierClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
+
+
+class RegionalPartnerClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/regional-partners/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(RegionalPartnerClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
