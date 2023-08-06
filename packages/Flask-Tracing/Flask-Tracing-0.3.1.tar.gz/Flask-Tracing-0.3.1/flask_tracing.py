3002ebdb7bb8ab56f0bf3cde277317638a8532a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:17 +0000

    Add package Flask-Tracing/0.3.1/Flask-Tracing-0.3.1.tar.gz

---
+++ packages/Flask-Tracing/Flask-Tracing-0.3.1.tar.gz/Flask-Tracing-0.3.1/flask_tracing.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import uuid
+import logging
+
+from flask import request, g, has_request_context
+
+
+def generate_request_id(original_id=''):
+    """Generate request id."""
+    new_id = uuid.uuid4()
+
+    if original_id:
+        new_id = '{}, {}'.format(original_id, new_id)
+
+    return new_id
+
+
+def get_request_id():
+    """Get current request id in session."""
+    if 'request_id' not in g:
+        original_id = request.headers.get('x-request-id')
+        g.request_id = generate_request_id(original_id)
+    return g.request_id
+
+
+class RequestIdFilter(logging.Filter):
+    def filter(self, record):
+        record.request_id = get_request_id() if has_request_context() else ''
+        return True
+
+
+class Tracing(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.after_request(self.hook)
+        pass
+
+    def hook(self, response):
+        response.headers['X-Request-ID'] = get_request_id()
+        return response
---
+++ packages/Flask-Tracing/Flask-Tracing-0.3.1.tar.gz/Flask-Tracing-0.3.1/setup.py
@@ -0,0 +1,34 @@
+"""
+Flask-Tracing
+-------------
+
+Tracing utilities for flask application.
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Tracing',
+    version='0.3.1',
+    url='https://github.com/Handsome2734/Flask-Tracing',
+    license='BSD',
+    author='Handsome2734',
+    author_email='songbowen1229@gmail.com',
+    description='Tracing utilities for your flask application.',
+    long_description=__doc__,
+    py_modules=['flask_tracing'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
