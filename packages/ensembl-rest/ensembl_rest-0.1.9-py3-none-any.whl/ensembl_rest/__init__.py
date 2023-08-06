e91bfc173697905be24898b8803a7d7a343a4cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:01 +0000

    Add package ensembl-rest/0.1.9/ensembl_rest-0.1.9-py3-none-any.whl

---
+++ packages/ensembl-rest/ensembl_rest-0.1.9-py3-none-any.whl/ensembl_rest/__init__.py
@@ -0,0 +1,4 @@
+from .ensemblclients import EnsemblClient, EnsemblGenomesClient
+from .core import HTTPError, InvalidResponseError
+from .assemblymapper import AssemblyMapper
+from .utils import region_str
