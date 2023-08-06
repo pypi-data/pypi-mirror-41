1186a72c6a9606a837ccc195e0df43d78160877d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:43 +0000

    Add package slack-cli-webhook/0.1.1/slack-cli-webhook-0.1.1.tar.gz

---
+++ packages/slack-cli-webhook/slack-cli-webhook-0.1.1.tar.gz/slack-cli-webhook-0.1.1/slack-cli-webhook/__main__.py
@@ -0,0 +1,23 @@
+# __main__.py
+from .slack_cli import SlackCLI
+from argparse import ArgumentParser
+import sys
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-m", "--message", dest="message", help="message to send", required=True)
+    parser.add_argument("-w", "--webhook", dest="webhook", help="webhook to send to", required=True)
+    args = parser.parse_args()
+    print(args.message)
+    slack_cli = SlackCLI(args.webhook)
+    if slack_cli.send_message(args.message):
+        print("Message successfully sent.")
+        sys.exit(0)
+    else:
+        print("Sending message failed, please try again later.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
