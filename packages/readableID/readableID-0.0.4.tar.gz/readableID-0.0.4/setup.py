6d8ba890b11b9237af01e0569e3f0f88b202dae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package readableID/0.0.4/readableID-0.0.4.tar.gz

---
+++ packages/readableID/readableID-0.0.4.tar.gz/readableID-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from os import path
+from setuptools import setup
+
+# read the contents of your README file
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='readableID',
+    version='0.0.4',
+    description='Transform integer ids into readable, nonconsecutive ids',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/nziehn/readableID',
+    author='Nils Ziehn',
+    author_email='nziehn@mail.com',
+    license='MIT',
+    packages=['readableID'],
+    zip_safe=False
+)
