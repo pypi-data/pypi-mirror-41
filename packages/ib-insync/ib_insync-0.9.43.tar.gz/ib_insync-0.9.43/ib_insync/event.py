8e94f7ecf44e883ccf0cc602c1967e716c319fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:05 +0000

    Add package ib-insync/0.9.43/ib_insync-0.9.43.tar.gz

---
+++ packages/ib-insync/ib_insync-0.9.43.tar.gz/ib_insync-0.9.43/ib_insync/event.py
@@ -0,0 +1,205 @@
+import types
+import weakref
+import asyncio
+
+__all__ = ['Event']
+
+
+class Event:
+    """
+    Enable event passing between loosely coupled components.
+
+    An event contains a list of callables (the listener slots) that are
+    called in order when the event is emitted.
+    """
+    __slots__ = ('name', 'slots')
+
+    def __init__(self, name=''):
+        self.name = name
+        self.slots = []  # list of [obj, weakref, func] sublists
+
+    def connect(self, c, weakRef: bool = True, hiPriority: bool = False):
+        """
+        Connect a callable to this event.
+        The ``+=`` operator can be used as a synonym for this method.
+
+        Args:
+            c: The callable to connect.
+            weakRef:
+                * True: The callable can be garbage collected
+                  upon which it will be automatically disconnected from this
+                  event
+                * False: A strong reference to the callable will be kept
+            hiPriority:
+                * True: The callable will be placed in the first slot
+                * False The callable will be placed last
+        """
+        obj, func = self._split(c)
+        if [(s[0] is obj or s[1] and s[1]() is obj) and s[2] is func
+                for s in self.slots]:
+            raise ValueError(f'Duplicate callback: {c}')
+
+        if weakRef and hasattr(obj, '__weakref__'):
+            ref = weakref.ref(obj, self._onFinalize)
+            obj = None
+        else:
+            ref = None
+        slot = [obj, ref, func]
+        if hiPriority:
+            self.slots.insert(0, slot)
+        else:
+            self.slots.append(slot)
+        return self
+
+    def disconnect(self, c):
+        """
+        Disconnect a callable from this event.
+        The ``-=`` operator can be used as a synonym for this method.
+
+        Args:
+            c: The callable to disconnect. It is valid if the callable is
+                already not connected.
+        """
+        obj, func = self._split(c)
+        for slot in self.slots:
+            if (slot[0] is obj or slot[1] and slot[1]() is obj) \
+                    and slot[2] is func:
+                slot[0] = slot[1] = slot[2] = None
+        self.slots = [s for s in self.slots if s != [None, None, None]]
+        return self
+
+    def emit(self, *args, **kwargs):
+        """
+        Call all slots in this event with the given arguments.
+        """
+        for obj, ref, func in self.slots:
+            if ref:
+                obj = ref()
+            if obj is None:
+                if func:
+                    func(*args, **kwargs)
+            else:
+                if func:
+                    func(obj, *args, **kwargs)
+                else:
+                    obj(*args, **kwargs)
+
+    def clear(self):
+        """
+        Clear all slots.
+        """
+        for slot in self.slots:
+            slot[0] = slot[1] = slot[2] = None
+        self.slots = []
+
+    @staticmethod
+    def init(obj, eventNames):
+        """
+        Convenience function for initializing events as members
+        of the given object.
+        """
+        for name in eventNames:
+            setattr(obj, name, Event(name))
+
+    async def wait(self):
+        """
+        Asynchronously await the next emit of this event and return
+        the emitted data.
+
+        The bare event can be used as synonym for this method.
+        """
+        def onEvent(*args):
+            if not fut.done():
+                fut.set_result(
+                    args[0] if len(args) == 1 else args if args else None)
+
+        fut = asyncio.Future()
+        self.connect(onEvent)
+        try:
+            return await fut
+        finally:
+            self.disconnect(onEvent)
+
+    async def aiter(self, skipToLast: bool = False):
+        """
+        Create an asynchronous iterator that yields the emitted data
+        from this event.
+
+        The bare event can be used as synonym for this method.
+
+        Args:
+            skipToLast:
+                * True: Backlogged events are skipped over to yield only
+                  the last event.
+                * False: All events are yielded.
+
+        Example usage:
+
+        .. code-block:: python
+
+            async for trade, fill in ib.execDetailsEvent:
+                print(fill)
+        """
+        def onEvent(*args):
+            q.put_nowait(args)
+
+        q = asyncio.Queue()
+        self.connect(onEvent)
+        try:
+            while True:
+                args = await q.get()
+                if skipToLast:
+                    while q.qsize():
+                        args = q.get_nowait()
+                yield args[0] if len(args) == 1 else args if args else None
+        finally:
+            self.disconnect(onEvent)
+
+    __iadd__ = connect
+    __isub__ = disconnect
+    __call__ = emit
+    __aiter__ = aiter
+
+    def __repr__(self):
+        return f'Event<{self.name}, {self.slots}>'
+
+    def __len__(self):
+        return len(self.slots)
+
+    def __await__(self):
+        return self.wait().__await__()
+
+    def __contains__(self, c):
+        """
+        See if callable is already connected.
+        """
+        obj, func = self._split(c)
+        return any(
+            (s[0] is obj or s[1] and s[1]() is obj) and s[2] is func
+            for s in self.slots)
+
+    def _split(self, c):
+        """
+        Split given callable in (object, function) tuple.
+        """
+        if isinstance(c, types.FunctionType):
+            return (None, c)
+        elif isinstance(c, types.MethodType):
+            return (c.__self__, c.__func__)
+        elif isinstance(c, types.BuiltinMethodType):
+            if type(c.__self__) is type:
+                # built-in method
+                return (c.__self__, c)
+            else:
+                # built-in function
+                return (None, c)
+        elif hasattr(c, '__call__'):
+            return (c, None)
+        else:
+            raise ValueError(f'Invalid callable: {c}')
+
+    def _onFinalize(self, ref):
+        for slot in self.slots:
+            if slot[1] is ref:
+                slot[0] = slot[1] = slot[2] = None
+        self.slots = [s for s in self.slots if s != [None, None, None]]
