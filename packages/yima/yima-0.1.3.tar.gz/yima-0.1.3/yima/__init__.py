118b07f86edcf7ea374ca615ea76000cb4ec2f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package yima/0.1.3/yima-0.1.3.tar.gz

---
+++ packages/yima/yima-0.1.3.tar.gz/yima-0.1.3/yima/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 14:53:41 2019
+
+@author: Simon Zhou
+"""
+import time
+import requests
+
+# import error_codes
+from .api import YMClient
+# from .error_codes import _codes
+
+
