9bed8b97c857758d7391f6e365716ba9335485ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:49 +0000

    Add package rcghci/2.0/rcghci-2.0.tar.gz

---
+++ packages/rcghci/rcghci-2.0.tar.gz/rcghci-2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rcghci",
+    description='A script that wraps a GHCI instance and make it available as a langauge server for editors.',
+    url='http://github.com/sras/ghci-remote',
+    author='Sandeep.C.R',
+    author_email='sandeepcr2@gmail.com',
+    license='MIT',
+    version = "2.0",
+    packages = ['rcghci'],
+    entry_points = {
+        "console_scripts":['rcghci=rcghci.rcghci:main', 'rcghci_nvim=rcghci.rcghci_nvim:main']
+    },
+    install_requires=[ 'pexpect' ]
+)
