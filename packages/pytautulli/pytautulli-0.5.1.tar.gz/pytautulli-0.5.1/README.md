679af823932a296dc858fd4439e8077cb196d90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:22 +0000

    Add package pytautulli/0.5.1/pytautulli-0.5.1.tar.gz

---
+++ packages/pytautulli/pytautulli-0.5.1.tar.gz/pytautulli-0.5.1/README.md
@@ -0,0 +1,15 @@
+# pytautulli [![Build Status](https://travis-ci.org/ludeeus/pytautulli.svg?branch=master)](https://travis-ci.org/ludeeus/pytautulli) [![PyPI version](https://badge.fury.io/py/pytautulli.svg)](https://badge.fury.io/py/pytautulli)
+
+_A python module to get information from Tautulli._
+
+## Notes
+
+This module require API key from Tautulli.  
+
+## Install
+
+```bash
+pip install pytautulli
+```
+
+Look at the file `example.py` for a usage example.
---
+++ packages/pytautulli/pytautulli-0.5.1.tar.gz/pytautulli-0.5.1/setup.py
@@ -0,0 +1,22 @@
+"""Setup configuration."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG = fh.read()
+setuptools.setup(
+    name="pytautulli",
+    version='0.5.1',
+    author="Joakim Sorensen",
+    author_email="ludeeus@gmail.com",
+    description="",
+    long_description=LONG,
+    install_requires=['aiohttp', 'async_timeout'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludeeus/pytautulli",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
