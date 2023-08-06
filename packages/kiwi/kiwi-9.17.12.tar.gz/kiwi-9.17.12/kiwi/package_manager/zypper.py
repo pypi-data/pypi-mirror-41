7386e51b40633ff8a3304808b9214abec810ef2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package kiwi/9.17.12/kiwi-9.17.12.tar.gz

---
+++ packages/kiwi/kiwi-9.17.12.tar.gz/kiwi-9.17.12/kiwi/package_manager/zypper.py
@@ -0,0 +1,339 @@
+# Copyright (c) 2015 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of kiwi.
+#
+# kiwi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kiwi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+#
+import re
+import os
+
+# project
+from kiwi.command import Command
+from kiwi.package_manager.base import PackageManagerBase
+from kiwi.path import Path
+from kiwi.logger import log
+from kiwi.exceptions import (
+    KiwiRpmDatabaseReloadError,
+    KiwiRequestError
+)
+
+
+class PackageManagerZypper(PackageManagerBase):
+    """
+    **Implements base class for installation/deletion of
+    packages and collections using zypper**
+
+    :param list zypper_args: zypper arguments from repository runtime
+        configuration
+    :param dict command_env: zypper command environment from repository
+        runtime configuration
+    """
+    def post_init(self, custom_args=None):
+        """
+        Post initialization method
+
+        Store custom zypper arguments
+
+        :param list custom_args: custom zypper arguments
+        """
+        self.custom_args = custom_args
+        if not custom_args:
+            self.custom_args = []
+
+        runtime_config = self.repository.runtime_config()
+
+        self.zypper_args = runtime_config['zypper_args']
+        self.chroot_zypper_args = self.root_bind.move_to_root(
+            self.zypper_args
+        )
+
+        self.command_env = runtime_config['command_env']
+        self.chroot_command_env = dict(self.command_env)
+        if 'ZYPP_CONF' in self.command_env:
+            self.chroot_command_env['ZYPP_CONF'] = self.root_bind.move_to_root(
+                [self.command_env['ZYPP_CONF']]
+            )[0]
+
+    def request_package(self, name):
+        """
+        Queue a package request
+
+        :param str name: package name
+        """
+        self.package_requests.append(name)
+
+    def request_collection(self, name):
+        """
+        Queue a collection request
+
+        :param str name: zypper pattern name
+        """
+        self.collection_requests.append('pattern:' + name)
+
+    def request_product(self, name):
+        """
+        Queue a product request
+
+        :param str name: zypper product name
+        """
+        self.product_requests.append('product:' + name)
+
+    def request_package_exclusion(self, name):
+        """
+        Queue a package exclusion(skip) request
+
+        :param str name: package name
+        """
+        self.exclude_requests.append(name)
+
+    def process_install_requests_bootstrap(self):
+        """
+        Process package install requests for bootstrap phase (no chroot)
+
+        :return: process results in command type
+
+        :rtype: namedtuple
+        """
+        command = ['zypper'] + self.zypper_args + [
+            '--root', self.root_dir,
+            'install', '--auto-agree-with-licenses'
+        ] + self.custom_args + self._install_items()
+        return Command.call(
+            command, self.command_env
+        )
+
+    def process_install_requests(self):
+        """
+        Process package install requests for image phase (chroot)
+
+        :return: process results in command type
+
+        :rtype: namedtuple
+        """
+        if self.exclude_requests:
+            # For zypper excluding a package means, removing it from
+            # the solver operation. This is done by adding a package
+            # lock. This means that if the package is hard required
+            # by another package, it will break the transaction.
+            metadata_dir = ''.join([self.root_dir, '/etc/zypp'])
+            if not os.path.exists(metadata_dir):
+                Path.create(metadata_dir)
+            for package in self.exclude_requests:
+                Command.run(
+                    [
+                        'chroot', self.root_dir, 'zypper'
+                    ] + self.chroot_zypper_args + ['al'] + [package],
+                    self.chroot_command_env
+                )
+        return Command.call(
+            ['chroot', self.root_dir, 'zypper'] + self.chroot_zypper_args + [
+                'install', '--auto-agree-with-licenses'
+            ] + self.custom_args + self._install_items(),
+            self.chroot_command_env
+        )
+
+    def process_delete_requests(self, force=False):
+        """
+        Process package delete requests (chroot)
+
+        :param bool force: force deletion: true|false
+
+        :raises KiwiRequestError: if none of the packages to delete is
+            installed
+        :return: process results in command type
+
+        :rtype: namedtuple
+        """
+        delete_items = []
+        for delete_item in self._delete_items():
+            try:
+                Command.run(['chroot', self.root_dir, 'rpm', '-q', delete_item])
+                delete_items.append(delete_item)
+            except Exception:
+                # ignore packages which are not installed
+                pass
+        if not delete_items:
+            raise KiwiRequestError(
+                'None of the requested packages to delete are installed'
+            )
+        if force:
+            force_options = ['--nodeps', '--allmatches', '--noscripts']
+            return Command.call(
+                [
+                    'chroot', self.root_dir, 'rpm', '-e'
+                ] + force_options + delete_items,
+                self.chroot_command_env
+            )
+        else:
+            return Command.call(
+                [
+                    'chroot', self.root_dir, 'zypper'
+                ] + self.chroot_zypper_args + [
+                    'remove', '-u', '--force-resolution'
+                ] + delete_items,
+                self.chroot_command_env
+            )
+
+    def update(self):
+        """
+        Process package update requests (chroot)
+
+        :return: process results in command type
+
+        :rtype: namedtuple
+        """
+        return Command.call(
+            ['chroot', self.root_dir, 'zypper'] + self.chroot_zypper_args + [
+                'update', '--auto-agree-with-licenses'
+            ] + self.custom_args,
+            self.chroot_command_env
+        )
+
+    def process_only_required(self):
+        """
+        Setup package processing only for required packages
+        """
+        if '--no-recommends' not in self.custom_args:
+            self.custom_args.append('--no-recommends')
+
+    def process_plus_recommended(self):
+        """
+        Setup package processing to also include recommended dependencies.
+        """
+        if '--no-recommends' in self.custom_args:
+            self.custom_args.remove('--no-recommends')
+
+    def match_package_installed(self, package_name, zypper_output):
+        """
+        Match expression to indicate a package has been installed
+
+        This match for the package to be installed in the output
+        of the zypper command is not 100% accurate. There might
+        be false positives due to sub package names starting with
+        the same base package name
+
+        :param list package_list: list of all packages
+        :param str log_line: zypper status line
+
+        :returns: match or None if there isn't any match
+
+        :rtype: match object, None
+        """
+        return re.match(
+            '.*Installing: ' + re.escape(package_name) + '.*', zypper_output
+        )
+
+    def match_package_deleted(self, package_name, zypper_output):
+        """
+        Match expression to indicate a package has been deleted
+
+        :param list package_list: list of all packages
+        :param str log_line: zypper status line
+
+        :returns: match or None if there isn't any match
+
+        :rtype: match object, None
+        """
+        return re.match(
+            '.*Removing: ' + re.escape(package_name) + '.*', zypper_output
+        )
+
+    def database_consistent(self):
+        """
+        Check if rpm package database is consistent
+
+        :return: True or False
+
+        :rtype: bool
+        """
+        try:
+            Command.run(['chroot', self.root_dir, 'rpmdb', '--initdb'])
+            return True
+        except Exception:
+            return False
+
+    def dump_reload_package_database(self, version=45):
+        """
+        Dump and reload the rpm database to match the desired rpm db version
+        Supported target rpm database db versions are
+
+        * 45 (db45_load)
+        * 48 (db48_load)
+
+        :param str version: target rpm db version
+        """
+        db_load_for_version = {
+            45: 'db45_load',
+            48: 'db48_load'
+        }
+
+        rpm_search_env = {
+            'PATH': os.sep.join([self.root_dir, 'usr', 'bin'])
+        }
+        rpm_bin = Path.which(
+            'rpm', custom_env=rpm_search_env, access_mode=os.X_OK
+        )
+        if not rpm_bin:
+            log.warning(
+                'RPM binary not found in image, RPM database is not reloaded'
+            )
+            return
+
+        rpmdb_path_request = Command.run(
+            ['chroot', self.root_dir, 'rpm', '-E', '%_dbpath']
+        )
+        rpmdb_path = os.path.normpath(os.sep.join(
+            [self.root_dir, rpmdb_path_request.output.strip()]
+        ))
+        if not os.path.exists(rpmdb_path):
+            raise KiwiRpmDatabaseReloadError(
+                'Unable to get the rpmdb location {0}'.format(rpmdb_path)
+            )
+
+        if version not in db_load_for_version:
+            raise KiwiRpmDatabaseReloadError(
+                'Dump reload for rpm DB version: %s not supported' % version
+            )
+        if not self.database_consistent():
+            reload_db_files = [
+                os.sep.join([rpmdb_path, 'Name']),
+                os.sep.join([rpmdb_path, 'Packages'])
+            ]
+            for db_file in reload_db_files:
+                db_file_backup = '{0}.bak'.format(db_file)
+                Command.run([
+                    'db_dump', '-f', db_file_backup, db_file
+                ])
+                Command.run(['rm', '-f', db_file])
+                Command.run([
+                    db_load_for_version[version],
+                    '-f', db_file_backup, db_file
+                ])
+                Command.run(['rm', '-f', db_file_backup])
+            Command.run([
+                'chroot', self.root_dir, 'rpm', '--rebuilddb'
+            ])
+
+    def _install_items(self):
+        items = self.package_requests + self.collection_requests \
+            + self.product_requests
+        self.cleanup_requests()
+        return items
+
+    def _delete_items(self):
+        # collections and products can't be deleted
+        items = []
+        items += self.package_requests
+        self.cleanup_requests()
+        return items
---
+++ packages/kiwi/kiwi-9.17.12.tar.gz/kiwi-9.17.12/test/unit/package_manager_zypper_test.py
@@ -0,0 +1,251 @@
+from mock import patch
+from mock import call
+from collections import namedtuple
+
+import mock
+
+from .test_helper import raises
+
+from kiwi.package_manager.zypper import PackageManagerZypper
+from kiwi.exceptions import (
+    KiwiRequestError,
+    KiwiRpmDatabaseReloadError
+)
+
+
+class TestPackageManagerZypper(object):
+    def setup(self):
+        repository = mock.Mock()
+        repository.root_dir = 'root-dir'
+
+        root_bind = mock.Mock()
+        root_bind.move_to_root = mock.Mock(
+            return_value=['root-moved-arguments']
+        )
+        repository.root_bind = root_bind
+
+        self.command_env = {
+            'HOME': '/home/ms', 'ZYPP_CONF': 'root-dir/my/zypp.conf'
+        }
+        repository.runtime_config = mock.MagicMock(
+            return_value={
+                'zypper_args': ['--reposd-dir', 'root-dir/my/repos'],
+                'command_env': self.command_env
+            }
+        )
+        self.manager = PackageManagerZypper(repository)
+
+        self.chroot_zypper_args = self.manager.root_bind.move_to_root(
+            self.manager.zypper_args
+        )
+        self.chroot_command_env = self.manager.command_env
+        zypp_conf = self.manager.command_env['ZYPP_CONF']
+        self.chroot_command_env['ZYPP_CONF'] = \
+            self.manager.root_bind.move_to_root(zypp_conf)[0]
+
+    def test_request_package(self):
+        self.manager.request_package('name')
+        assert self.manager.package_requests == ['name']
+
+    def test_request_collection(self):
+        self.manager.request_collection('name')
+        assert self.manager.collection_requests == ['pattern:name']
+
+    def test_request_product(self):
+        self.manager.request_product('name')
+        assert self.manager.product_requests == ['product:name']
+
+    def test_request_package_exclusion(self):
+        self.manager.request_package_exclusion('name')
+        assert self.manager.exclude_requests == ['name']
+
+    @patch('kiwi.command.Command.call')
+    def test_process_install_requests_bootstrap(self, mock_call):
+        self.manager.request_package('vim')
+        self.manager.process_install_requests_bootstrap()
+        mock_call.assert_called_once_with(
+            [
+                'zypper', '--reposd-dir', 'root-dir/my/repos',
+                '--root', 'root-dir',
+                'install', '--auto-agree-with-licenses'
+            ] + self.manager.custom_args + ['vim'], self.command_env
+        )
+
+    @patch('kiwi.command.Command.call')
+    @patch('kiwi.command.Command.run')
+    @patch('os.path.exists')
+    @patch('kiwi.package_manager.zypper.Path.create')
+    def test_process_install_requests(
+        self, mock_path, mock_exists, mock_run, mock_call
+    ):
+        mock_exists.return_value = False
+        self.manager.request_package('vim')
+        self.manager.request_package_exclusion('skipme')
+        self.manager.process_install_requests()
+        mock_path.assert_called_once_with('root-dir/etc/zypp')
+        mock_run.assert_called_once_with(
+            ['chroot', 'root-dir', 'zypper'] + self.chroot_zypper_args + [
+                'al'
+            ] + self.manager.custom_args + ['skipme'], self.chroot_command_env
+        )
+        mock_call.assert_called_once_with(
+            ['chroot', 'root-dir', 'zypper'] + self.chroot_zypper_args + [
+                'install', '--auto-agree-with-licenses'
+            ] + self.manager.custom_args + ['vim'], self.chroot_command_env
+        )
+
+    @patch('kiwi.command.Command.call')
+    @patch('kiwi.command.Command.run')
+    def test_process_delete_requests_all_installed(self, mock_run, mock_call):
+        self.manager.request_package('vim')
+        self.manager.process_delete_requests()
+        mock_call.assert_called_once_with(
+            ['chroot', 'root-dir', 'zypper'] + self.chroot_zypper_args + [
+                'remove', '-u', '--force-resolution'
+            ] + self.manager.custom_args + ['vim'], self.chroot_command_env
+        )
+
+    @patch('kiwi.command.Command.call')
+    @patch('kiwi.command.Command.run')
+    def test_process_delete_requests_force(self, mock_run, mock_call):
+        self.manager.request_package('vim')
+        self.manager.process_delete_requests(True)
+        mock_call.assert_called_once_with(
+            [
+                'chroot', 'root-dir', 'rpm', '-e',
+                '--nodeps', '--allmatches', '--noscripts', 'vim'
+            ], self.command_env
+        )
+
+    @patch('kiwi.command.Command.run')
+    @patch('kiwi.command.Command.call')
+    @raises(KiwiRequestError)
+    def test_process_delete_requests_package_missing(self, mock_call, mock_run):
+        mock_run.side_effect = Exception
+        self.manager.request_package('vim')
+        self.manager.process_delete_requests()
+        mock_run.assert_called_once_with(
+            ['chroot', 'root-dir', 'rpm', '-q', 'vim']
+        )
+
+    @patch('kiwi.command.Command.call')
+    def test_update(self, mock_call):
+        self.manager.update()
+        mock_call.assert_called_once_with(
+            ['chroot', 'root-dir', 'zypper'] + self.chroot_zypper_args + [
+                'update', '--auto-agree-with-licenses'
+            ] + self.manager.custom_args, self.chroot_command_env
+        )
+
+    def test_process_only_required(self):
+        self.manager.process_only_required()
+        assert self.manager.custom_args == ['--no-recommends']
+
+    def test_process_plus_recommended(self):
+        self.manager.process_only_required()
+        assert self.manager.custom_args == ['--no-recommends']
+        self.manager.process_plus_recommended()
+        assert '--no-recommends' not in self.manager.custom_args
+
+    def test_match_package_installed(self):
+        assert self.manager.match_package_installed('foo', 'Installing: foo')
+
+    def test_match_package_deleted(self):
+        assert self.manager.match_package_deleted('foo', 'Removing: foo')
+
+    @patch('kiwi.command.Command.run')
+    def test_database_consistent(self, mock_command):
+        assert self.manager.database_consistent() is True
+
+    @patch('kiwi.command.Command.run')
+    def test_database_not_consistent(self, mock_command):
+        mock_command.side_effect = Exception
+        assert self.manager.database_consistent() is False
+
+    @patch('os.path.exists')
+    @patch('kiwi.package_manager.zypper.Path.which')
+    @patch('kiwi.command.Command.run')
+    @patch('kiwi.package_manager.zypper.PackageManagerZypper.database_consistent')
+    def test_reload_package_database(
+        self, mock_consistent, mock_command, mock_which, mock_exists
+    ):
+        command_type = namedtuple('command', ['output'])
+        cmd = command_type(output='var/lib/rpm')
+        mock_command.return_value = cmd
+        mock_exists.return_value = True
+        mock_which.return_value = True
+        mock_consistent.return_value = False
+        self.manager.dump_reload_package_database()
+        assert mock_command.call_args_list == [
+            call([
+                'chroot', 'root-dir', 'rpm', '-E', '%_dbpath'
+            ]),
+            call([
+                'db_dump', '-f', 'root-dir/var/lib/rpm/Name.bak',
+                'root-dir/var/lib/rpm/Name'
+            ]),
+            call([
+                'rm', '-f', 'root-dir/var/lib/rpm/Name'
+            ]),
+            call([
+                'db45_load', '-f', 'root-dir/var/lib/rpm/Name.bak',
+                'root-dir/var/lib/rpm/Name'
+            ]),
+            call([
+                'rm', '-f', 'root-dir/var/lib/rpm/Name.bak'
+            ]),
+            call([
+                'db_dump', '-f', 'root-dir/var/lib/rpm/Packages.bak',
+                'root-dir/var/lib/rpm/Packages'
+            ]),
+            call([
+                'rm', '-f', 'root-dir/var/lib/rpm/Packages'
+            ]),
+            call([
+                'db45_load', '-f', 'root-dir/var/lib/rpm/Packages.bak',
+                'root-dir/var/lib/rpm/Packages'
+            ]),
+            call([
+                'rm', '-f', 'root-dir/var/lib/rpm/Packages.bak'
+            ]),
+            call([
+                'chroot', 'root-dir', 'rpm', '--rebuilddb'
+            ])
+        ]
+
+    @patch('os.path.exists')
+    @patch('kiwi.package_manager.zypper.Path.which')
+    @patch('kiwi.command.Command.run')
+    @raises(KiwiRpmDatabaseReloadError)
+    def test_reload_package_database_wrong_db_path(
+        self, mock_command, mock_which, mock_exists
+    ):
+        command_type = namedtuple('command', ['output'])
+        cmd = command_type(output='var/lib/rpm')
+        mock_command.return_value = cmd
+        mock_exists.return_value = False
+        mock_which.return_value = True
+        self.manager.dump_reload_package_database()
+
+    @patch('kiwi.logger.log.warning')
+    @patch('kiwi.package_manager.zypper.Path.which')
+    def test_reload_package_database_without_rpm_binary(
+        self, mock_which, mock_warning
+    ):
+        mock_which.return_value = False
+        self.manager.dump_reload_package_database()
+        assert mock_warning.called
+
+    @patch('os.path.exists')
+    @patch('kiwi.package_manager.zypper.Path.which')
+    @patch('kiwi.command.Command.run')
+    @raises(KiwiRpmDatabaseReloadError)
+    def test_reload_package_database_wrong_db_version(
+        self, mock_command, mock_which, mock_exists
+    ):
+        command_type = namedtuple('command', ['output'])
+        cmd = command_type(output='var/lib/rpm')
+        mock_command.return_value = cmd
+        mock_exists.return_value = True
+        mock_which.return_value = True
+        self.manager.dump_reload_package_database(42)
