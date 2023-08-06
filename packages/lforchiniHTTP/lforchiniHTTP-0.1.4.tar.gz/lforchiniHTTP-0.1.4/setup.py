e932ce832f31f5fa02246a53ae162935388cb590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:35 +0000

    Add package lforchiniHTTP/0.1.4/lforchiniHTTP-0.1.4.tar.gz

---
+++ packages/lforchiniHTTP/lforchiniHTTP-0.1.4.tar.gz/lforchiniHTTP-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lforchiniHTTP",
+    version="0.1.4",
+    author="LForchini",
+    author_email="leonardo.forchini@gmail.com",
+    description="A simple HTTP server with a custom handler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lforchini/HTTP-Server",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
