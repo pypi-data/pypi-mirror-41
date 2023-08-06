c09a90dbcc6c01eda73b1d8bf0a24bac90cc821e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:28 +0000

    Add package dmenutrello/0.0.6/dmenutrello-0.0.6-py2-none-any.whl

---
+++ packages/dmenutrello/dmenutrello-0.0.6-py2-none-any.whl/dmenutrello/dmenutrello.py
@@ -0,0 +1,79 @@
+import functools
+import dmenu
+import configparser
+from os.path import expanduser
+from trello import TrelloClient
+
+dmenu_show= functools.partial(dmenu.show, font='DejaVu Sans Mono for Powerline-14', background_selected='#2aa198',foreground_selected='#191919', foreground='#2aa198', background='#191919')
+
+def menu(key, token):
+    print(key)
+    print(token)
+    client = TrelloClient(
+        api_key = key,
+        api_secret = token
+        )
+    match = False
+
+    #client.list_boards()
+    #client.add_board()
+    #board.list_lists()
+    #board.add_list()
+    #list.list_cards()
+    #list.add_card()
+    #card.get_comments()
+    #names.append(board.name+ " #" + str(len(board.list_lists())))
+
+    data = {}
+    matchedData = None
+    ### BOARD
+    #fill data dict
+    for board in client.list_boards():
+        data[board.name] = board
+
+    out = dmenu_show(data.keys())
+    board = None
+    #check for match
+    if out in data:
+        match = True
+        temp = data[out]
+        board=temp
+        data[out] = {}
+        for d in temp.list_lists():
+            data[out][d.name] = d
+        matchedData = data[out]
+    elif out is not None:
+        #no match, add new
+        data[out] = client.add_board(out)
+        out=dmenu_show(data.keys, prompt="ok")
+
+    print(data)
+    data = matchedData
+    out=dmenu_show(matchedData.keys())
+
+    if out in data:
+        temp = data[out]
+        data[out] = {}
+        for d in temp.list_cards():
+            data[out][d.name] = d
+        matchedData = data[out]
+    elif out is not None:
+        #no match, add new
+        data[out] = board.add_list(out)
+        out=dmenu_show(data.keys(), prompt="ok")
+
+    out=dmenu_show(matchedData.keys())
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read(expanduser('~/.dmenutrello'))
+
+    key = config.get('TRELLO', 'key')
+    token = config.get('TRELLO', 'token')
+
+    menu(key, token)
+
+if __name__ == '__main__':
+    main()
+
