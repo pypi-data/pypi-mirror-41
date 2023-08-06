6e7b62e5e3b651a2e48a991cb42f11d3b7ff4ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package hera-py/0.5.2/hera_py-0.5.2-py3-none-any.whl

---
+++ packages/hera-py/hera_py-0.5.2-py3-none-any.whl/hera/main.py
@@ -0,0 +1,248 @@
+import sys
+import functools
+
+from .data import Settings, VOLUME_QUIET, VOLUME_VERBOSE
+from .debugger import debug
+from .loader import load_program_from_file
+from .utils import format_int
+from .vm import VirtualMachine
+
+
+def external_main(argv=None):
+    """A wrapper around main that ignores its return value, so it is not printed to the
+    console when the program exits.
+    """
+    main(argv)
+
+
+def main(argv=None):
+    """The main entry point into hera-py."""
+    arguments = parse_args(argv)
+    path = arguments["<path>"]
+
+    settings = Settings()
+    if arguments["--no-color"] or not sys.stderr.isatty():
+        settings.color = False
+
+    if arguments["--big-stack"]:
+        # Arbitrary value copied over from HERA-C.
+        settings.data_start = 0xC167
+
+    settings.no_debug = arguments["--no-debug"]
+    settings.warn_return_on = not arguments["--warn-return-off"]
+    settings.warn_octal_on = not arguments["--warn-octal-off"]
+
+    if arguments["--verbose"]:
+        settings.volume = VOLUME_VERBOSE
+    elif arguments["--quiet"]:
+        settings.volume = VOLUME_QUIET
+
+    if arguments["preprocess"]:
+        main_preprocess(path, settings)
+    elif arguments["debug"]:
+        main_debug(path, settings)
+    else:
+        return main_execute(path, settings)
+
+
+def main_debug(path, settings):
+    """Debug the program."""
+    program = load_program_from_file(path, settings)
+    debug(program, settings)
+
+
+def main_execute(path, settings):
+    """Execute the program."""
+    program = load_program_from_file(path, settings)
+
+    vm = VirtualMachine(settings)
+    vm.exec_many(program)
+    settings.warning_count += vm.warning_count
+
+    if settings.volume != VOLUME_QUIET:
+        dump_state(vm, settings)
+
+    return vm
+
+
+def main_preprocess(path, settings):
+    """Preprocess the program and print it to standard output."""
+    program = load_program_from_file(path, settings)
+    if program.data:
+        sys.stderr.write("[DATA]\n")
+        for data_op in program.data:
+            sys.stderr.write("  {}\n".format(data_op))
+
+        if program.code:
+            sys.stderr.write("\n[CODE]\n")
+
+    for i, op in enumerate(program.code):
+        sys.stderr.write("  {:0>4}  {}\n".format(i, op))
+
+
+def parse_args(argv):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    flags = {}
+    posargs = []
+    after_flags = False
+    for arg in argv:
+        longarg = short_to_long(arg)
+        if longarg == "--":
+            after_flags = True
+        elif longarg in FLAGS:
+            flags[longarg] = True
+        elif not after_flags and longarg.startswith("-") and len(longarg) > 1:
+            sys.stderr.write("Unrecognized flag: " + arg + "\n")
+            sys.exit(1)
+        else:
+            posargs.append(longarg)
+
+    if "--help" in flags:
+        if len(flags) == 1 and not posargs:
+            print(HELP)
+            sys.exit(0)
+        else:
+            sys.stderr.write(
+                "--help may not be combined with other flags or commands.\n"
+            )
+            sys.exit(1)
+
+    if "--version" in flags:
+        if len(flags) == 1 and not posargs:
+            print(VERSION)
+            sys.exit(0)
+        else:
+            sys.stderr.write(
+                "--version may not be combined with other flags or commands.\n"
+            )
+            sys.exit(1)
+
+    if len(posargs) == 0:
+        sys.stderr.write("No file path supplied.\n")
+        sys.exit(1)
+    elif len(posargs) > 1:
+        sys.stderr.write("Too many file paths supplied.\n")
+        sys.exit(1)
+
+    if "--big-stack" in flags:
+        if "preprocess" in flags:
+            sys.stderr.write("--big-stack cannot be used with preprocess subcommand.\n")
+            sys.exit(1)
+
+    if "--warn-return-off" in flags:
+        if "preprocess" in flags:
+            sys.stderr.write(
+                "--warn-return-off cannot be used with preprocess subcommand.\n"
+            )
+            sys.exit(1)
+
+    if "--quiet" in flags and "--verbose" in flags:
+        sys.stderr.write("--quiet and --verbose are incompatible.\n")
+        sys.exit(1)
+
+    flags["<path>"] = posargs[0]
+    for f in FLAGS:
+        if f not in flags:
+            flags[f] = False
+
+    return flags
+
+
+def short_to_long(arg):
+    if arg == "-h":
+        return "--help"
+    elif arg == "-v":
+        return "--version"
+    elif arg == "-q":
+        return "--quiet"
+    else:
+        return arg
+
+
+FLAGS = {
+    "--big-stack",
+    "--help",
+    "--no-color",
+    "--no-debug",
+    "--quiet",
+    "--verbose",
+    "--version",
+    "--warn-octal-off",
+    "--warn-return-off",
+    "preprocess",
+    "debug",
+}
+VERSION = "hera-py 0.5.2 for HERA version 2.4"
+HELP = """\
+hera: an interpreter for the Haverford Educational RISC Architecture.
+
+Usage:
+    hera <path>
+    hera preprocess <path>
+    hera debug <path>
+
+Common options:
+    -h, --help         Show this message and exit.
+    -v, --version      Show the version and exit.
+
+    --no-color         Do not print colored output.
+    --no-debug         Disallow debugging instructions.
+    -q --quiet         Set output level to quiet.
+    --verbose          Set output level to verbose.
+    --warn-octal-off   Do not print warnings for zero-prefixed integer literals.
+
+Interpreter and debugger options:
+    --big-stack        Reserve more space for the stack.
+    --warn-return-off  Do not print warnings for invalid RETURN addresses.
+"""
+
+
+def dump_state(vm, settings):
+    """Print the state of the virtual machine to standard output."""
+    # Make sure that all program output has been printed.
+    sys.stdout.flush()
+
+    # Redefine print in this function to use stderr.
+    nprint = functools.partial(print, file=sys.stderr)
+
+    verbose = settings.volume == VOLUME_VERBOSE
+    if verbose:
+        last_register = 15
+    else:
+        last_register = 10
+        while last_register > 0 and vm.registers[last_register] == 0:
+            last_register -= 1
+
+    nprint("\nVirtual machine state after execution:")
+    for i, value in enumerate(vm.registers[1 : last_register + 1], start=1):
+        rname = "    R" + str(i) + (" " if i < 10 else "")
+        nprint("{} = {}".format(rname, format_int(value)))
+
+    if last_register > 0:
+        nprint()
+    else:
+        nprint("    R1 through R10 are all zero.\n")
+
+    flags = [
+        vm.flag_carry_block,
+        vm.flag_carry,
+        vm.flag_overflow,
+        vm.flag_zero,
+        vm.flag_sign,
+    ]
+    if not verbose and all(flags):
+        nprint("    All flags are ON")
+    elif not verbose and all(not f for f in flags):
+        nprint("    All flags are OFF")
+    else:
+        nprint("    Carry-block flag is " + ("ON" if vm.flag_carry_block else "OFF"))
+        nprint("    Carry flag is " + ("ON" if vm.flag_carry else "OFF"))
+        nprint("    Overflow flag is " + ("ON" if vm.flag_overflow else "OFF"))
+        nprint("    Zero flag is " + ("ON" if vm.flag_zero else "OFF"))
+        nprint("    Sign flag is " + ("ON" if vm.flag_sign else "OFF"))
+
+    if settings.warning_count > 0:
+        c = settings.warning_count
+        nprint("\n{} warning{} emitted.".format(c, "" if c == 1 else "s"))
---
+++ packages/hera-py/hera_py-0.5.2-py3-none-any.whl/hera/vm.py
@@ -0,0 +1,137 @@
+"""The virtual HERA machine.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: January 2019
+"""
+import copy
+from typing import List
+
+from .data import Settings
+from .op import DataOperation, Operation
+from .utils import print_warning, register_to_index
+
+
+class VirtualMachine:
+    """An abstract representation of a HERA processor."""
+
+    def __init__(self, settings=Settings()):
+        self.settings = settings
+        self.reset()
+
+    def reset(self):
+        """Reset the machine to its initial state."""
+        # Sixteen 16-bit registers. The virtual machine stores integers in their
+        # unsigned representation, so the values of self.registers will always be
+        # non-negative, although values above 2**15 implicitly represent negative
+        # integers under a signed interpretation.
+        self.registers = [0] * 16
+        # 16-bit program counter
+        self.pc = 0
+        # Current memory cell for data instructions
+        self.dc = self.settings.data_start
+        # Status/control flags
+        self.flag_sign = False
+        self.flag_zero = False
+        self.flag_overflow = False
+        self.flag_carry = False
+        self.flag_carry_block = False
+        # A memory array of 16-bit words. The HERA specification requires 2**16 words
+        # to be addressable, but we start off with a considerably smaller array and
+        # expand it as necessary, to keep the start-up time fast.
+        self.memory = [0] * (2 ** 4)
+        # Stack of (call_address, return_address) pairs for CALL/RETURN instructions.
+        # Used for warning messages and debugging.
+        self.expected_returns = []
+        self.halted = False
+        # Location object for the current operation
+        self.location = None
+        # Have warnings been issued for use of SWI and RTI instructions?
+        self.warned_for_SWI = False
+        self.warned_for_RTI = False
+        self.warned_for_overflow = False
+        self.warning_count = 0
+
+    def copy(self):
+        ret = copy.copy(self)
+        ret.registers = self.registers.copy()
+        ret.memory = self.memory.copy()
+        return ret
+
+    def exec_many(self, program: List[Operation]) -> None:
+        """Execute a program (i.e., a list of operations), resetting the machine's
+        state beforehand.
+        """
+        self.reset()
+
+        for data_op in program.data:
+            self.exec_one(data_op)
+
+        while not self.halted and self.pc < len(program.code):
+            self.exec_one(program.code[self.pc])
+
+    def exec_one(self, op):
+        """Execute a single operation."""
+        self.location = op.loc
+
+        opc = self.pc
+        try:
+            op.execute(self)
+        except NotImplementedError:
+            self.handle_not_implemented(op.name)
+
+        if self.pc == opc and not isinstance(op, DataOperation):
+            self.halted = True
+
+    def handle_not_implemented(self, name):
+        if name == "SWI":
+            if not self.warned_for_SWI:
+                self.warn("SWI is a no-op in this simulator", loc=self.location)
+                self.warned_for_SWI = True
+            self.pc += 1
+        elif name == "RTI":
+            if not self.warned_for_RTI:
+                self.warn("RTI is a no-op in this simulator", loc=self.location)
+                self.warned_for_RTI = True
+            self.pc += 1
+        else:
+            raise RuntimeError("unsupported operation {}".format(name))
+
+    def load_register(self, name):
+        """Get the contents of the register with the given name."""
+        index = register_to_index(name)
+        return self.registers[index]
+
+    def store_register(self, target, value):
+        """Store the value in the target register (a string)."""
+        index = register_to_index(target)
+        if index != 0:
+            self.registers[index] = value
+            if index == 15 and value >= self.settings.data_start:
+                if not self.warned_for_overflow:
+                    self.warn(
+                        "stack has overflowed into data segment", loc=self.location
+                    )
+                    self.warned_for_overflow = True
+
+    def set_zero_and_sign(self, value):
+        """Set the zero and sign flags based on the value."""
+        self.flag_zero = value == 0
+        self.flag_sign = value & 0x8000
+
+    def load_memory(self, address):
+        """Get the value at the given memory address."""
+        if address >= len(self.memory):
+            return 0
+        else:
+            return self.memory[address]
+
+    def store_memory(self, address, value):
+        """Store a value to a location in memory."""
+        # Extend the size of the memory array if necessary.
+        if address >= len(self.memory):
+            self.memory.extend([0] * (address - len(self.memory) + 1))
+        self.memory[address] = value
+
+    def warn(self, msg, loc):
+        print_warning(self.settings, msg, loc=loc)
+        self.warning_count += 1
