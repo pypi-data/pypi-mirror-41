975e69b3773fe434ff508dee0f0cbfb082349cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package PrintAllwjl/1.0.2/PrintAllwjl-1.0.2.tar.gz

---
+++ packages/PrintAllwjl/PrintAllwjl-1.0.2.tar.gz/PrintAllwjl-1.0.2/PrintAllwjl.py
@@ -0,0 +1,16 @@
+
+###########################函数实现###########################################
+def list_all(the_list,level):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            list_all(each_item,level+1)
+        else:
+            for tab_stop in range(level):   #使用level的值控制使用制表符的个数
+                print("\t",end="")    #每一层缩进显示一个TAB制表符
+            print(each_item)
+            
+        
+
+
+
+
