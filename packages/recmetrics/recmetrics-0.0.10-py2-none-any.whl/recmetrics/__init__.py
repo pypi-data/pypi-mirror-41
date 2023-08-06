ebd0d421aa83fd32a4bb9a28b98437922da86a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:01 +0000

    Add package recmetrics/0.0.10/recmetrics-0.0.10-py2-none-any.whl

---
+++ packages/recmetrics/recmetrics-0.0.10-py2-none-any.whl/recmetrics/__init__.py
@@ -0,0 +1,2 @@
+from .plots import long_tail_plot, mark_plot, mapk_plot, coverage_plot, class_distribution_plot, roc_curve, precision_recall_curve
+from .metrics import mark, coverage, personalization, intra_list_similarity, rmse, mse
