3f446dbbf26e7575b4ba3624064209647af988e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:27 +0000

    Add package jblib/1.6.2.2/jblib-1.6.2.2.tar.gz

---
+++ packages/jblib/jblib-1.6.2.2.tar.gz/jblib-1.6.2.2/jblib/__init__.py
@@ -0,0 +1,7 @@
+######################################################################################
+#
+# 09/03/2014 jbard v1.2 Added hilight to green, yellow, blue, purple, teal, and white
+# 01/28/2018 jbard v1.6 Refactored Codebase for pypi upload
+#
+
+name = "jblib"
---
+++ packages/jblib/jblib-1.6.2.2.tar.gz/jblib-1.6.2.2/jblib/cd.py
@@ -0,0 +1,24 @@
+"""
+    Context manager for changing the current working directory 
+    
+    class cd()
+
+    Example: 
+        with cd(directory):
+            print (os.getcwd()) 
+
+        print (os.getcwd()) ## Moves you back to the originating directory on exit
+"""
+
+import os
+
+class cd:
+    def __init__(self, newPath):
+        self.newPath = newPath   
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
