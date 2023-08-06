195c0a807302100408bfa4445567fb27c81101a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package fasttrees/1.2.0/fasttrees-1.2.0.tar.gz

---
+++ packages/fasttrees/fasttrees-1.2.0.tar.gz/fasttrees-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='fasttrees',
+    version='1.2.0',
+    packages=['fasttrees'],
+    url='https://github.com/dominiczy/fasttrees',
+    license='MIT License',
+    author='dominiczijlstra',
+    author_email='dominiczijlstra@gmail.com',
+    description='A fast and frugal tree classifier for sklearn',
+    install_requires=[
+                'numpy',
+                'pandas',
+                'sklearn'
+          ]
+)
