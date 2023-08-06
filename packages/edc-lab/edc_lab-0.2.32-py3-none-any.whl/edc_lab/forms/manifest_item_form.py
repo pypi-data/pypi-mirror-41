87ba594020f70c8d7231025bee42b9bb4ed1399a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:19 +0000

    Add package edc-lab/0.2.32/edc_lab-0.2.32-py3-none-any.whl

---
+++ packages/edc-lab/edc_lab-0.2.32-py3-none-any.whl/edc_lab/forms/manifest_item_form.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from ..models import ManifestItem
+
+
+class ManifestItemForm(forms.ModelForm):
+
+    class Meta:
+        fields = '__all__'
+        model = ManifestItem
---
+++ packages/edc-lab/edc_lab-0.2.32-py3-none-any.whl/edc_lab/identifiers/__init__.py
@@ -0,0 +1,6 @@
+from .aliquot_identifier import AliquotIdentifier
+from .aliquot_identifier import AliquotIdentifierCountError, AliquotIdentifierLengthError
+from .box_identifier import BoxIdentifier
+from .manifest_identifier import ManifestIdentifier
+from .requisition_identifier import RequisitionIdentifier
+from .prefix import Prefix, PrefixLengthError, PrefixKeyError
