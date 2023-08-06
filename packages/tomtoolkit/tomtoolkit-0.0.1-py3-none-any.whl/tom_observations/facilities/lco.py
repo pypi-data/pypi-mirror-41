b523962a07e39700ac6d283ad095dcc9680746fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:31 +0000

    Add package tomtoolkit/0.0.1/tomtoolkit-0.0.1-py3-none-any.whl

---
+++ packages/tomtoolkit/tomtoolkit-0.0.1-py3-none-any.whl/tom_observations/facilities/lco.py
@@ -0,0 +1,396 @@
+import requests
+from django.conf import settings
+from django import forms
+from dateutil.parser import parse
+from crispy_forms.layout import Layout, Div
+from django.core.cache import cache
+
+from tom_observations.facility import GenericObservationForm
+from tom_common.exceptions import ImproperCredentialsException
+from tom_observations.facility import GenericObservationFacility
+from tom_targets.models import Target
+
+# Determine settings for this module.
+try:
+    LCO_SETTINGS = settings.FACILITIES['LCO']
+except (AttributeError, KeyError):
+    LCO_SETTINGS = {
+        'portal_url': 'https://observe.lco.global',
+        'api_key': '',
+    }
+
+# Module specific settings.
+PORTAL_URL = LCO_SETTINGS['portal_url']
+TERMINAL_OBSERVING_STATES = ['COMPLETED', 'CANCELED', 'WINDOW_EXPIRED']
+
+# The SITES dictionary is used to calculate visibility intervals in the
+# planning tool. All entries should contain latitude, longitude, elevation
+# and a code.
+SITES = {
+    'Siding Spring': {
+        'sitecode': 'coj',
+        'latitude': -31.272,
+        'longitude': 149.07,
+        'elevation': 1116
+    },
+    'Sutherland': {
+        'sitecode': 'cpt',
+        'latitude': -32.38,
+        'longitude': 20.81,
+        'elevation': 1804
+    },
+    'Teide': {
+        'sitecode': 'tfn',
+        'latitude': 20.3,
+        'longitude': -16.511,
+        'elevation': 2390
+    },
+    'Cerro Tololo': {
+        'sitecode': 'lsc',
+        'latitude': -30.167,
+        'longitude': -70.804,
+        'elevation': 2198
+    },
+    'McDonald': {
+        'sitecode': 'elp',
+        'latitude': 30.679,
+        'longitude': -104.015,
+        'elevation': 2027
+    },
+    'Haleakala': {
+        'sitecode': 'ogg',
+        'latitude': 20.706,
+        'longitude': -156.258,
+        'elevation': 3065
+    }
+}
+
+# Functions needed specifically for LCO
+
+
+def make_request(*args, **kwargs):
+    response = requests.request(*args, **kwargs)
+    if 400 <= response.status_code < 500:
+        raise ImproperCredentialsException('LCO')
+    response.raise_for_status()
+    return response
+
+
+def flatten_error_dict(form, error_dict):
+    non_field_errors = []
+    for k, v in error_dict.items():
+        if type(v) == list:
+            for i in v:
+                if type(i) == str:
+                    if k in form.fields:
+                        form.add_error(k, i)
+                    else:
+                        non_field_errors.append('{}: {}'.format(k, i))
+                if type(i) == dict:
+                    non_field_errors.append(flatten_error_dict(form, i))
+        elif type(v) == str:
+            if k in form.fields:
+                form.add_error(k, v)
+            else:
+                non_field_errors.append('{}: {}'.format(k, v))
+        elif type(v) == dict:
+            non_field_errors.append(flatten_error_dict(form, v))
+
+    return non_field_errors
+
+
+def get_instruments():
+    response = make_request(
+        'GET',
+        PORTAL_URL + '/api/instruments/',
+        headers={'Authorization': 'Token {0}'.format(LCO_SETTINGS['api_key'])}
+    )
+    return response.json()
+
+
+def instrument_choices():
+    return [(k, k) for k in get_instruments()]
+
+
+def filter_choices():
+    return set([(f, f) for ins in get_instruments().values() for f in ins['filters']])
+
+
+def proposal_choices():
+    response = make_request(
+        'GET',
+        PORTAL_URL + '/api/profile/',
+        headers={'Authorization': 'Token {0}'.format(LCO_SETTINGS['api_key'])}
+    )
+    choices = []
+    for p in response.json()['proposals']:
+        if p['current']:
+            choices.append((p['id'], '{} ({})'.format(p['title'], p['id'])))
+    return choices
+
+
+class LCOObservationForm(GenericObservationForm):
+    """
+    This is the class that is responsible for displaying the observation request form.
+    It inherits from tom_observations.facility.GenericObservationForm which provides
+    some base shared functionality. Extra fields are provided below.
+    The layout is handlded by Django crispy forms which allows customizability of the
+    form layout without needing to write html templates:
+    https://django-crispy-forms.readthedocs.io/en/d-0/layouts.html
+    See the documentation on Django forms for more information.
+    """
+    group_id = forms.CharField()
+    proposal = forms.ChoiceField(choices=proposal_choices)
+    ipp_value = forms.FloatField()
+    start = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
+    end = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
+    filter = forms.ChoiceField(choices=filter_choices)
+    instrument_name = forms.ChoiceField(choices=instrument_choices)
+    exposure_count = forms.IntegerField(min_value=1)
+    exposure_time = forms.FloatField(min_value=0.1)
+    max_airmass = forms.FloatField()
+    observation_type = forms.ChoiceField(
+        choices=(('NORMAL', 'Normal'), ('TARGET_OF_OPPORTUNITY', 'Rapid Response'))
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper.layout = Layout(
+            self.common_layout,
+            Div(
+                Div(
+                    'group_id', 'proposal', 'ipp_value', 'observation_type', 'start', 'end',
+                    css_class='col'
+                ),
+                Div(
+                    'filter', 'instrument_name', 'exposure_count', 'exposure_time', 'max_airmass',
+                    css_class='col'
+                ),
+                css_class='form-row'
+            )
+        )
+
+    def clean_start(self):
+        start = self.cleaned_data['start']
+        return parse(start).isoformat()
+
+    def clean_end(self):
+        end = self.cleaned_data['end']
+        return parse(end).isoformat()
+
+    def is_valid(self):
+        super().is_valid()
+        errors = LCOFacility.validate_observation(self.observation_payload)
+        if errors:
+            self.add_error(None, flatten_error_dict(self, errors))
+        return not errors
+
+    def instrument_to_type(self, instrument_name):
+        if any(x in instrument_name for x in ['FLOYDS', 'NRES']):
+            return 'SPECTRUM'
+        else:
+            return 'EXPOSE'
+
+    @property
+    def observation_payload(self):
+        """
+        This method is called to extract the data from the form into a form that
+        can be used by the rest of the module. In this example, a LCO request payload
+        is constructed by the form data and the associated target.
+        """
+        target = Target.objects.get(pk=self.cleaned_data['target_id'])
+        return {
+            "group_id": self.cleaned_data['group_id'],
+            "proposal": self.cleaned_data['proposal'],
+            "ipp_value": self.cleaned_data['ipp_value'],
+            "operator": "SINGLE",
+            "observation_type": self.cleaned_data['observation_type'],
+            "requests": [
+                {
+                    "target": {
+                        "name": target.name,
+                        "type": target.type,
+                        "ra": target.ra,
+                        "dec": target.dec,
+                        "proper_motion_ra": target.pm_ra,
+                        "proper_motion_dec": target.pm_dec,
+                        "epoch": target.epoch,
+                        "orbinc": target.inclination,
+                        "longascnode": target.lng_asc_node,
+                        "argofperih": target.arg_of_perihelion,
+                        "perihdist": target.distance,
+                        "meandist": target.semimajor_axis,
+                        "meananom": target.mean_anomaly,
+                        "dailymot": target.mean_daily_motion
+                    },
+                    "molecules": [
+                        {
+                            "type": self.instrument_to_type(self.cleaned_data['instrument_name']),
+                            "instrument_name": self.cleaned_data['instrument_name'],
+                            "filter": self.cleaned_data['filter'],
+                            "spectra_slit": self.cleaned_data['filter'],
+                            "exposure_count": self.cleaned_data['exposure_count'],
+                            "exposure_time": self.cleaned_data['exposure_time']
+                        }
+                    ],
+                    "windows": [
+                        {
+                            "start": self.cleaned_data['start'],
+                            "end": self.cleaned_data['end']
+                        }
+                    ],
+                    "location": {
+                        "telescope_class": self.cleaned_data['instrument_name'][:3].lower()
+                    },
+                    "constraints": {
+                        "max_airmass": self.cleaned_data['max_airmass'],
+                    }
+                }
+            ]
+        }
+
+
+class LCOFacility(GenericObservationFacility):
+    """
+    The facility class contains all the logic specific to the facility it is
+    written for. Some methods are used only internally (starting with an
+    uderscore) but some need to be implemented by all facility classes.
+    All facilities should inherit from GenericObservationFacility which
+    provides some base functionality.
+    In order to make use of a facility class, add the path to
+    TOM_FACILITY_CLASSES in your settings.py.
+    """
+    name = 'LCO'
+    form = LCOObservationForm
+
+    @classmethod
+    def submit_observation(clz, observation_payload):
+        """
+        This method takes in the serialized data from the form and actually
+        submits the observation to the remote api
+        """
+        response = make_request(
+            'POST',
+            PORTAL_URL + '/api/userrequests/',
+            json=observation_payload,
+            headers=clz._portal_headers()
+        )
+        return [r['id'] for r in response.json()['requests']]
+
+    @classmethod
+    def validate_observation(clz, observation_payload):
+        """
+        Same thing as submit_observation, but a dry run. You can
+        skip this in different modules by just using "pass"
+        """
+        response = make_request(
+            'POST',
+            PORTAL_URL + '/api/userrequests/validate/',
+            json=observation_payload,
+            headers=clz._portal_headers()
+        )
+        return response.json()['errors']
+
+    @classmethod
+    def get_observation_url(clz, observation_id):
+        """
+        Takes an observation id and return the url for which a user
+        can view the observation at an external location. In this case,
+        we return a URL to the LCO observation portal's observation
+        record page.
+        """
+        return PORTAL_URL + '/requests/' + observation_id
+
+    @classmethod
+    def get_terminal_observing_states(clz):
+        """
+        Returns the states for which an observation is not expected
+        to change.
+        """
+        return TERMINAL_OBSERVING_STATES
+
+    @classmethod
+    def get_observing_sites(clz):
+        return SITES
+
+    @classmethod
+    def get_observation_status(clz, observation_id):
+        """
+        Return the status for a single observation. observation_id should
+        be able to be used to retrieve the status from the external service.
+        """
+        response = make_request(
+            'GET',
+            PORTAL_URL + '/api/requests/{0}'.format(observation_id),
+            headers=clz._portal_headers()
+        )
+        return response.json()['state']
+
+    @classmethod
+    def data_products(clz, observation_id, product_id=None):
+        """
+        Using an observation_id, retrieve a list of the data
+        products that belong to this observation. In this case,
+        the LCO module retrieves a list of frames from the LCO
+        data archive.
+        """
+        products = []
+        for frame in clz._archive_frames(observation_id, product_id):
+            products.append({
+                'id': frame['id'],
+                'filename': frame['filename'],
+                'created': parse(frame['DATE_OBS']),
+                'url': frame['url']
+            })
+        return products
+
+    # The following methods are used internally by this module
+    # and should not be called directly from outside code.
+
+    @classmethod
+    def _portal_headers(clz):
+        if LCO_SETTINGS.get('api_key'):
+            return {'Authorization': 'Token {0}'.format(LCO_SETTINGS['api_key'])}
+        else:
+            return {}
+
+    @classmethod
+    def _archive_headers(clz):
+        if LCO_SETTINGS.get('api_key'):
+            archive_token = cache.get('LCO_ARCHIVE_TOKEN')
+            if not archive_token:
+                response = make_request(
+                    'GET',
+                    PORTAL_URL + '/api/profile/',
+                    headers={'Authorization': 'Token {0}'.format(LCO_SETTINGS['api_key'])}
+                )
+                archive_token = response.json().get('tokens', {}).get('archive')
+                if archive_token:
+                    cache.set('LCO_ARCHIVE_TOKEN', archive_token, 3600)
+                    return {'Authorization': 'Bearer {0}'.format(archive_token)}
+
+            else:
+                return {'Authorization': 'Bearer {0}'.format(archive_token)}
+        else:
+            return {}
+
+    @classmethod
+    def _archive_frames(clz, observation_id, product_id=None):
+        # todo save this key somewhere
+        frames = []
+        if product_id:
+            response = make_request(
+                'GET',
+                'https://archive-api.lco.global/frames/{0}/'.format(product_id),
+                headers=clz._archive_headers()
+            )
+            frames = [response.json()]
+        else:
+            response = make_request(
+                'GET',
+                'https://archive-api.lco.global/frames/?REQNUM={0}'.format(observation_id),
+                headers=clz._archive_headers()
+            )
+            frames = response.json()['results']
+
+        return frames
