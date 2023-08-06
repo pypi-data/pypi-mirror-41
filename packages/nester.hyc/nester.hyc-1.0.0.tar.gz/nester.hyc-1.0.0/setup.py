da74c1455064b52f29597fdf3db39a3ca57e3bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package nester.hyc/1.0.0/nester.hyc-1.0.0.tar.gz

---
+++ packages/nester.hyc/nester.hyc-1.0.0.tar.gz/nester.hyc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         ='nester.hyc',
+        version      ='1.0.0',
+        py_modules   =['nester'],
+        author       ='huiyuchen',
+        author_email ='m15251537482@163.com',
+        url          ='https://www.bilibili.com/',
+        description  ='A simple printer of nester list',
+      )
