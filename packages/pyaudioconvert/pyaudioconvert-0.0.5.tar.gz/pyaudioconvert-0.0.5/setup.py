67b820091059c36aa579122e6f5b5263eef0d6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:25 +0000

    Add package pyaudioconvert/0.0.5/pyaudioconvert-0.0.5.tar.gz

---
+++ packages/pyaudioconvert/pyaudioconvert-0.0.5.tar.gz/pyaudioconvert-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyaudioconvert",
+    version="0.0.5",
+    author="Rob Smith",
+    author_email="robmsmt@gmail.com",
+    description="A small package to convert audio files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robmsmt/pyaudioconvert",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+#python3 -m pip install --upgrade setuptools wheel
+#python3 setup.py sdist bdist_wheel
+#twine upload dist/*
