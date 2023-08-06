6196ad70cbc3162e2830f4b2c996e4713351150e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:20 +0000

    Add package pretalx-media-ccc-de/0.1.0/pretalx-media-ccc-de-0.1.0.tar.gz

---
+++ packages/pretalx-media-ccc-de/pretalx-media-ccc-de-0.1.0.tar.gz/pretalx-media-ccc-de-0.1.0/pretalx_media_ccc_de/__init__.py
@@ -0,0 +1,21 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_media_ccc_de'
+    verbose_name = 'media.ccc.de recordings'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('media.ccc.de recordings')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy('Pull recordings from media.ccc.de and embed them in talk pages')
+        visible = True
+        version = '0.1.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+        from . import tasks  # NOQA
+
+
+default_app_config = 'pretalx_media_ccc_de.PluginApp'
---
+++ packages/pretalx-media-ccc-de/pretalx-media-ccc-de-0.1.0.tar.gz/pretalx-media-ccc-de-0.1.0/pretalx_media_ccc_de/tasks.py
@@ -0,0 +1,35 @@
+import json
+from contextlib import suppress
+
+import requests
+from pretalx.celery_app import app
+from pretalx.event.models import Event
+from pretalx.submission.models import Submission
+
+
+@app.task()
+def task_refresh_recording_urls(event_slug):
+    try:
+        event = Event.objects.get(slug__iexact=event_slug)
+    except Event.DoesNotExist:
+        return
+
+    if not event.settings.media_ccc_de_id:
+        event.settings.media_ccc_de_id = event.slug
+
+    response = requests.get(
+        f'https://media.ccc.de/public/conferences/{event.settings.media_ccc_de_id}'
+    )
+    if not response.status_code == 200:
+        return None
+
+    structure = json.loads(response.content.decode())
+    for talk in structure.get('events', []):
+        if talk.get('frontend_link'):
+            with suppress(Submission.DoesNotExist):
+                submission = Submission.objects.get(
+                    event=event, code__iexact=talk['slug'].split('-')[1]
+                )
+                key = f'media_ccc_de_url_{submission.code}'
+                if not event.settings.get(key):
+                    event.settings.set(key, talk['frontend_link'])
