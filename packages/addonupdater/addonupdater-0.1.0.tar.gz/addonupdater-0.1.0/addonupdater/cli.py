1493d297ffaa4c63f56e18379305df3f0a73bc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:43 +0000

    Add package addonupdater/0.1.0/addonupdater-0.1.0.tar.gz

---
+++ packages/addonupdater/addonupdater-0.1.0.tar.gz/addonupdater-0.1.0/addonupdater/cli.py
@@ -0,0 +1,28 @@
+"""Enable CLI."""
+import click
+
+
+@click.command()
+@click.option('--token', '-T', help='GitHub access_token.')
+@click.option('--addon', '-A', help='Addon name.')
+@click.option('--repo', '-R', default=None, help='Addon repo.')
+@click.option('--test', is_flag=True, help="Test run, will not commit.")
+@click.option('--verbose', is_flag=True, help="Print more stuff.")
+@click.option('--org', default=None, help="Specify GitHub org.")
+@click.option('--skip_apk', is_flag=True, help="Skip apk updates.")
+@click.option('--skip_pip', is_flag=True, help="Skip pip updates.")
+@click.option('--skip_custom', is_flag=True, help="Skip custom updates.")
+@click.option('--pull_request', '-PR', is_flag=True, help="Create a PR instead"
+              "of commiting to master.")
+def cli(
+        token, addon, repo, test, verbose, skip_apk, skip_pip, skip_custom,
+        org, pull_request):
+    """CLI for this package."""
+    from addonupdater.updater import AddonUpdater
+    updater = AddonUpdater(
+        token, addon, repo, test, verbose, skip_apk, skip_pip, skip_custom,
+        org, pull_request)
+    updater.update_addon()
+
+
+cli()  # pylint: disable=E1120
