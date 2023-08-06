ee5b94d02f6d86fed8b5aed3d389ca620855fe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:48 +0000

    Add package sav.channels/0.4/sav.channels-0.4.tar.gz

---
+++ packages/sav.channels/sav.channels-0.4.tar.gz/sav.channels-0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sav.channels",
+    version="0.4",
+    author="Sander Voerman",
+    author_email="sander@savoerman.nl",
+    description="Iterable streams between coroutines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sandervoerman/channels",
+    packages=['sav.channels'],
+    package_data={
+        'sav.channels': ['py.typed'],
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
