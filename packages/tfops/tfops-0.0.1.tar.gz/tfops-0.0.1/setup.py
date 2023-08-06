9057b3fe0aa06878429e8a36f6551d1c9bc0205d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:14 +0000

    Add package tfops/0.0.1/tfops-0.0.1.tar.gz

---
+++ packages/tfops/tfops-0.0.1.tar.gz/tfops-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    version='0.0.1',
+    name='tfops',
+    packages=['tfops'],
+    description='Tensorflow extra operations',
+    author='Andreas Urbanski',
+    author_email='urbanski.andreas@gmail.com',
+    python_requires='>=3.6.5',
+    license='TBD',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
