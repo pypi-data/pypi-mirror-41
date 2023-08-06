e83bafb10f34afbce288c3508169c118fbd7714e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:34 +0000

    Add package alignmentrs/0.8.2/alignmentrs-0.8.2-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/alignmentrs/alignmentrs-0.8.2-cp36-cp36m-macosx_10_7_x86_64.whl/alignmentrs/__init__.py
@@ -0,0 +1,21 @@
+from libalignmentrs.record import Record
+from libalignmentrs.alignment import BaseAlignment
+from libalignmentrs.fasta import fasta_file_to_basealignments
+import libalignmentrs as librs
+
+from alignmentrs import aln, alnset
+from alignmentrs.aln import Alignment, fasta_file_to_alignment
+from alignmentrs.alnset import AlignmentSet, fasta_directory_to_alignmentset
+
+
+__author__ = 'Kent Kawashima'
+__version__ = '0.8.2'
+__all__ = [
+    # From dynamic library
+    'Record', 'BaseAlignment', 'librs',
+    # Modules
+    'aln', 'alnset',
+    # Classes
+    'Alignment', 'AlignmentSet',
+    # Functions
+    'fasta_file_to_alignment', 'fasta_directory_to_alignmentset']
