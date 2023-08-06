12dbbe93f3d755e1ef06cbedae3000af32d69e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:58 +0000

    Add package alone/0.1.6/alone-0.1.6.tar.gz

---
+++ packages/alone/alone-0.1.6.tar.gz/alone-0.1.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+version = '0.1.6'
+
+setuptools.setup(
+    name='alone',
+    version=version,
+    author='Adrien Horgnies',
+    author_email='adrien.pierre.horgnies@gmail.com',
+    description='Metaclass that defines a class as a singleton',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AdrienHorgnies/py-singleton',
+    download_url='https://github.com/AdrienHorgnies/py-singleton/archive/v{version}.tar.gz'.format(version=version),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    license='MIT License',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers'
+    ],
+    keywords=['singleton']
+)
