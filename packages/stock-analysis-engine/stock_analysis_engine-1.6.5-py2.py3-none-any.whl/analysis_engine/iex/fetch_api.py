cca98dfeb39e2d0afa78168ef6149d85e34dbf60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:48 +0000

    Add package stock-analysis-engine/1.6.5/stock_analysis_engine-1.6.5-py2.py3-none-any.whl

---
+++ packages/stock-analysis-engine/stock_analysis_engine-1.6.5-py2.py3-none-any.whl/analysis_engine/iex/fetch_api.py
@@ -0,0 +1,496 @@
+"""
+Fetch API calls wrapping pyEX
+
+Supported environment variables:
+
+::
+
+    # verbose logging in this module
+    export DEBUG_FETCH=1
+
+"""
+
+import pyEX.stocks as pyex_stocks
+import analysis_engine.iex.consts as iex_consts
+import analysis_engine.iex.utils as fetch_utils
+import analysis_engine.dataset_scrub_utils as scrub_utils
+import spylunking.log.setup_logging as log_utils
+
+log = log_utils.build_colorized_logger(name=__name__)
+
+
+def fetch_daily(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_daily
+
+    Fetch the IEX daily data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_DAILY
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - daily - scrub_mode={} args={} '
+        'ticker={}'.format(
+            label,
+            scrub_mode,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.chartDF(
+        symbol=ticker,
+        timeframe=work_dict.get(
+            'timeframe',
+            '1d'),
+        date=work_dict.get(
+            'date',
+            None))
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_daily
+
+
+def fetch_minute(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_minute
+
+    Fetch the IEX minute intraday data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_MINUTE
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    from_historical_date = None
+    last_close_to_use = None
+    dates = []
+
+    if 'from_historical_date' in work_dict:
+        from_historical_date = work_dict['from_historical_date']
+        last_close_to_use = work_dict['last_close_to_use']
+        dates = fetch_utils.get_days_between_dates(
+            from_historical_date=work_dict['from_historical_date'],
+            last_close_to_use=last_close_to_use)
+
+    log.info(
+        '{} - minute - args={} ticker={} '
+        'fhdate={} last_close={} dates={}'.format(
+            label,
+            work_dict,
+            ticker,
+            from_historical_date,
+            last_close_to_use,
+            dates))
+
+    res = pyex_stocks.chartDF(
+        symbol=ticker,
+        timeframe=work_dict.get(
+            'timeframe',
+            '1m'),
+        date=work_dict.get(
+            'date',
+            None))
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_minute
+
+
+def fetch_quote(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_quote
+
+    Fetch the IEX quote data for a ticker and
+    return as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_QUOTE
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - quote - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.quoteDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_quote
+
+
+def fetch_stats(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_stats
+
+    Fetch the IEX statistics data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_STATS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - stats - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.stockStatsDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_stats
+
+
+def fetch_peers(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_peers
+
+    Fetch the IEX peers data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_PEERS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - peers - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.peersDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_peers
+
+
+def fetch_news(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_news
+
+    Fetch the IEX news data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_NEWS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - news - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.newsDF(
+        symbol=ticker,
+        count=50)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_news
+
+
+def fetch_financials(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_financials
+
+    Fetch the IEX financial data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_FINANCIALS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - financials - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.financialsDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_financials
+
+
+def fetch_earnings(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_earnings
+
+    Fetch the IEX earnings data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_EARNINGS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - earnings - args={} ticker={}'.format(
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.earningsDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_earnings
+
+
+def fetch_dividends(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_dividends
+
+    Fetch the IEX dividends data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_DIVIDENDS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    timeframe = work_dict.get(
+        'timeframe',
+        '2y')
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - dividends - args={} ticker={} '
+        'timeframe={}'.format(
+            label,
+            work_dict,
+            ticker,
+            timeframe))
+
+    res = pyex_stocks.dividendsDF(
+        symbol=ticker,
+        timeframe=timeframe)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_dividends
+
+
+def fetch_company(
+        work_dict,
+        scrub_mode='NO_SORT'):
+    """fetch_company
+
+    Fetch the IEX company data for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = iex_consts.DATAFEED_COMPANY
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    use_date = work_dict.get(
+        'use_date',
+        None)
+
+    log.info(
+        '{} - company - args={} ticker={}'.format(
+            label,
+            label,
+            work_dict,
+            ticker))
+
+    res = pyex_stocks.companyDF(
+        symbol=ticker)
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=use_date,
+        df=res)
+
+    return scrubbed_df
+# end of fetch_company
---
+++ packages/stock-analysis-engine/stock_analysis_engine-1.6.5-py2.py3-none-any.whl/analysis_engine/send_to_slack.py
@@ -0,0 +1,288 @@
+"""
+Helper for extracting details from Celery task and
+sending it to a slack webhook.
+
+Supported environment variables:
+
+::
+
+    # slack webhook
+    export SLACK_WEBHOOK=https://hooks.slack.com/services/
+
+"""
+
+import os
+import json
+import requests
+import tabulate as tb
+import analysis_engine.consts as ae_consts
+import spylunking.log.setup_logging as log_utils
+
+log = log_utils.build_colorized_logger(name=__name__)
+
+
+def post_success(msg,
+                 jupyter=False,
+                 block=False,
+                 full_width=False):
+    """Post a SUCCESS message to slack
+
+    :param msg: A string, list, or dict to send to slack
+    """
+    result = {'status': ae_consts.FAILED}
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_success - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return result
+    if msg:
+        attachments = [{"attachments":
+                       [{"color": "good", "title": "SUCCESS"}]}]
+        fields = parse_msg(msg, block=block)
+        if fields:
+            if full_width:
+                attachments.append({"text": fields[0].pop("value")})
+            else:
+                attachments[0]["attachments"][0]["fields"] = fields
+            result = post(attachments, jupyter=jupyter)
+    return result
+
+
+def post_failure(msg,
+                 jupyter=False,
+                 block=False,
+                 full_width=False):
+    """Post a FAILURE message to slack
+
+    :param msg: A string, list, or dict to send to slack
+    """
+    result = {'status': ae_consts.FAILED}
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_failure - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return result
+    if msg:
+        attachments = [{"attachments":
+                       [{"color": "danger", "title": "FAILED"}]}]
+        fields = parse_msg(msg, block=block)
+        if fields:
+            if full_width:
+                attachments.append({"text": fields[0].pop("value")})
+            else:
+                attachments[0]["attachments"][0]["fields"] = fields
+            result = post(attachments, jupyter=jupyter)
+    return result
+
+
+def post_message(msg,
+                 jupyter=False,
+                 block=False,
+                 full_width=False):
+    """Post any message to slack
+
+    :param msg: A string, list, or dict to send to slack
+    """
+    result = {'status': ae_consts.FAILED}
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_message - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return result
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_message - please add a SLACK_WEBHOOK environment '
+            'variable for to work')
+        return result
+    if msg:
+        attachments = [{"attachments": [{"title": "MESSAGE"}]}]
+        fields = parse_msg(msg, block=block)
+        if fields:
+            if full_width:
+                attachments.append({"text": fields[0].pop("value")})
+            else:
+                attachments[0]["attachments"][0]["fields"] = fields
+            result = post(attachments, jupyter=jupyter)
+    return result
+
+
+def parse_msg(msg, block=False):
+    """Create an array of fields for slack from the msg type
+
+    :param msg: A string, list, or dict to massage for sending to slack
+    """
+    if type(msg) is str:
+        if block:
+            return [{"value": "```{}```".format(msg)}]
+        return [{"value": msg}]
+    elif type(msg) is list:
+        if block:
+            string_list = '\n'.join("{}".format(str(x)) for x in msg)
+            return [{"value": "```{}```".format(string_list)}]
+        return [{"value": str(x)} for x in msg]
+    elif type(msg) is dict:
+        if block:
+            string_dict = '\n'.join(
+                "{}: {}".format(str(k), str(v)) for k, v in list(msg.items()))
+            return [{"value": "```{}```".format(string_dict)}]
+        return [{"value": "{}: {}".format(
+            str(k), str(v))} for k, v in list(msg.items())]
+    return None
+
+
+def post(attachments, jupyter=False):
+    """Send created attachments to slack
+
+    :param attachments: Values to post to slack
+    """
+    SLACK_WEBHOOK = ae_consts.ev('SLACK_WEBHOOK', None)
+    result = {'status': ae_consts.FAILED}
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return result
+    if attachments and SLACK_WEBHOOK:
+        try:
+            # if not jupyter:
+            #     log.debug(('Attempting to post attachments={} '
+            #               'to slack_webhook exists').format(attachments))
+            for attachment in attachments:
+                r = requests.post(SLACK_WEBHOOK, data=json.dumps(attachment))
+                if str(r.status_code) == "200":
+                    # log.info((
+                    #   'Successful post of attachment={} '
+                    #   'to slack_webhook').format(
+                    #       attachment if not jupyter else
+                    #       True if attachment else False))
+                    result['status'] = ae_consts.SUCCESS
+                else:
+                    log.error(('Failed to post attachment={} '
+                               'with status_code={}').format(
+                                   attachment if not jupyter else
+                                   True if attachment else False,
+                                   r.status_code))
+                    result['status'] = ae_consts.FAILED
+                    break
+        except Exception as e:
+            log.error(('Failed to post attachments={} '
+                       'with ex={}').format(
+                           attachments if not jupyter else
+                           True if attachments else False,
+                           e))
+            result['status'] = ae_consts.ERR
+            result['err'] = e
+    else:
+        log.info(('Skipping post to slack due to missing '
+                  'attachments={} or SLACK_WEBHOOK missing={}').format(
+                      attachments if not jupyter else
+                      True if attachments else False,
+                      False if SLACK_WEBHOOK else True))
+    return result
+
+
+def post_df(
+        df,
+        columns=None,
+        block=True,
+        jupyter=True,
+        full_width=True,
+        tablefmt='github'):
+    """post_df
+
+    Post a ``pandas.DataFrame`` to Slack
+
+    :param df: ``pandas.DataFrame`` object
+    :param columns: ordered list of columns to for the table
+                    header row
+                    (``None`` by default)
+    :param block: bool for
+                  post as a Slack-formatted block ```like this```
+                  (``True`` by default)
+    :param jupyter: bool for
+                    jupyter attachment handling
+                    (``True`` by default)
+    :param full_width: bool to ensure the width is preserved
+                       the Slack message  (``True`` by default)
+    :param tablefmt: string for table format (``github`` by default).
+                     Additional format values can be found on:
+                     https://bitbucket.org/astanin/python-tabulate
+    """
+
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_df - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return
+    if not hasattr(df, 'index'):
+        log.debug('post_df - no df ')
+        return
+
+    log.debug('post_df - df.index={} columns={} fmt={}'.format(
+        len(df.index),
+        columns,
+        tablefmt))
+
+    msg = None
+    if columns:
+        msg = tb.tabulate(
+            df[columns],
+            headers=columns,
+            tablefmt=tablefmt)
+    else:
+        msg = tb.tabulate(
+            df,
+            tablefmt=tablefmt)
+    # end of if/else
+
+    post_success(
+        msg=msg,
+        block=block,
+        jupyter=jupyter,
+        full_width=full_width)
+# end of post_df
+
+
+def post_cb(
+        msg,
+        block=True,
+        jupyter=True,
+        full_width=True,
+        tablefmt='github'):
+    """post_cb
+
+    Post a text messsage as a code block to Slack
+
+    :param msg: text message (pre-formatting is not necessary)
+    :param block: bool for
+                  post as a Slack-formatted block ```like this```
+                  (``True`` by default)
+    :param jupyter: bool for
+                    jupyter attachment handling
+                    (``True`` by default)
+    :param full_width: bool to ensure the width is preserved
+                       the Slack message  (``True`` by default)
+    :param tablefmt: string for table format (``github`` by default).
+                     Additional format values can be found on:
+                     https://bitbucket.org/astanin/python-tabulate
+    """
+
+    if not os.getenv('SLACK_WEBHOOK', False):
+        log.info(
+            'post_cb - please add a SLACK_WEBHOOK environment '
+            'variable to publish messages')
+        return
+    if not msg:
+        log.debug('post_cb - no msg')
+        return
+
+    log.debug('post_cb - msg={} fmt={}'.format(
+        len(msg),
+        tablefmt))
+
+    post_success(
+        msg=msg,
+        block=block,
+        jupyter=jupyter,
+        full_width=full_width)
+# end of post_cb
