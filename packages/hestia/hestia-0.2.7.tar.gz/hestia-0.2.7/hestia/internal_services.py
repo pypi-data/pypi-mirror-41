cf000ebb9118d1e74931eb8bd27bb2646a1343ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:55 +0000

    Add package hestia/0.2.7/hestia-0.2.7.tar.gz

---
+++ packages/hestia/hestia-0.2.7.tar.gz/hestia-0.2.7/hestia/internal_services.py
@@ -0,0 +1,12 @@
+class InternalServices(object):  # noqa
+    DOCKERIZER = 'dockerizer'
+    SIDECAR = 'sidecar'
+    HELPER = 'helper'
+    RUNNER = 'runner'
+
+    VALUES = [
+        DOCKERIZER,
+        SIDECAR,
+        HELPER,
+        RUNNER
+    ]
