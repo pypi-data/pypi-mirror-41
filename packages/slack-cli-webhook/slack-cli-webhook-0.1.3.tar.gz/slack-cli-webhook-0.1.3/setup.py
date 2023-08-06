04730c4d66317f5f9189fd691291ad7ce40f6f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package slack-cli-webhook/0.1.3/slack-cli-webhook-0.1.3.tar.gz

---
+++ packages/slack-cli-webhook/slack-cli-webhook-0.1.3.tar.gz/slack-cli-webhook-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slack-cli-webhook",
+    version="0.1.3",
+    author="Matthijs Roelink",
+    author_email="matthijs@roelink.eu",
+    description="A Python package for sending messages to Slack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matthiti/slack-cli-webhook",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
