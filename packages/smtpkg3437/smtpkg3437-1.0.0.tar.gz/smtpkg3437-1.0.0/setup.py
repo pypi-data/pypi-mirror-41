a5d10ce0684d96b161da24343f186123bcf08aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:26 +0000

    Add package smtpkg3437/1.0.0/smtpkg3437-1.0.0.tar.gz

---
+++ packages/smtpkg3437/smtpkg3437-1.0.0.tar.gz/smtpkg3437-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    url='https://pypi.org/p/twine/',
+    name='smtpkg3437',
+    version='1.0.0',
+    description="about smartphone",
+    author='gop',
+    packages=['smtpkg777', 'smtpkg777/camera', 'smtpkg777/phone'],
+    py_modules=['hello']
+    )
---
+++ packages/smtpkg3437/smtpkg3437-1.0.0.tar.gz/smtpkg3437-1.0.0/smtpkg777/phone/phone.py
@@ -0,0 +1,5 @@
+def makeacall():
+    print("Make a Call")
+
+makeacall()
+print("phone.py's module name is", __name__)    
