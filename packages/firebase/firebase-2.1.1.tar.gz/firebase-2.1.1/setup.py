ad5c021de581165bff162a205381c11923198cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package firebase/2.1.1/firebase-2.1.1.tar.gz

---
+++ packages/firebase/firebase-2.1.1.tar.gz/firebase-2.1.1/setup.py
@@ -0,0 +1,38 @@
+"""
+jt_calc Python package is a python interface to the Google's Firebase REST APIs
+By Joe Tilsed
+---
+setup.py is the build script for setuptools.
+It tells setuptools about your package (such as the name and version) as well as which code files to include.
+"""
+
+import os
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='firebase',
+    version="2.1.1",
+    author="Joe Tilsed",
+    author_email="Joe@Tilsed.com",
+    description="Python interface to the Google's Firebase REST APIs",
+    long_description=long_description,
+    url="https://bitbucket.org/joetilsed/firebase/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords="firebase python",
+)
+
+
+# That's all folks...
