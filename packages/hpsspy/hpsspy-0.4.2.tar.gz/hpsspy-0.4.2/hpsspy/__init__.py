67da24cf8b7b500fafb6658ff637f592a56f0c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package hpsspy/0.4.2/hpsspy-0.4.2.tar.gz

---
+++ packages/hpsspy/hpsspy-0.4.2.tar.gz/hpsspy-0.4.2/hpsspy/__init__.py
@@ -0,0 +1,26 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+hpsspy
+~~~~~~
+
+Python interface to the HPSS system.
+"""
+#
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+class HpssError(Exception):
+    """Generic exception class for HPSS Errors.
+    """
+    pass
+
+
+class HpssOSError(HpssError):
+    """HPSS Errors that are similar to OSError.
+    """
+    pass
+
+
+__version__ = '0.4.2'
