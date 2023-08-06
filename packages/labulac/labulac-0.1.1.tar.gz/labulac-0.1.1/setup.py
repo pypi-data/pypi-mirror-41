01acd453c9c7413e115ed3468cfed2cc102c79ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package labulac/0.1.1/labulac-0.1.1.tar.gz

---
+++ packages/labulac/labulac-0.1.1.tar.gz/labulac-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labulac",
+    version="0.1.1",
+    keywords=("pip", "labulac", "labula"),
+    description="firs tool",
+    long_description="test",
+    license="MIT Licence",
+    url="https://github.com/labulac/labulac",
+    author="cms",
+    author_email="740162752@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[])
