d15e2d6dc28cb1734aee399fc48de1bb462369a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package online-judge-tools/0.1.53/online-judge-tools-0.1.53.tar.gz

---
+++ packages/online-judge-tools/online-judge-tools-0.1.53.tar.gz/online-judge-tools-0.1.53/onlinejudge/implementation/command/login.py
@@ -0,0 +1,43 @@
+# Python Version: 3.x
+import getpass
+import sys
+from typing import *
+
+import onlinejudge
+import onlinejudge.implementation.logging as log
+import onlinejudge.implementation.utils as utils
+
+if TYPE_CHECKING:
+    import argparse
+
+
+def login(args: 'argparse.Namespace') -> None:
+    # get service
+    service = onlinejudge.dispatch.service_from_url(args.url)
+    if service is None:
+        sys.exit(1)
+
+    # configure
+    kwargs = {}
+    if service.get_name() == 'yukicoder':
+        if not args.method:
+            args.method = 'github'
+        if args.method not in ['github', 'twitter']:
+            log.failure('login for yukicoder: invalid option: --method %s', args.method)
+            sys.exit(1)
+        kwargs['method'] = args.method
+    else:
+        if args.method:
+            log.failure('login for %s: invalid option: --method %s', service.get_name(), args.method)
+            sys.exit(1)
+
+    # login
+    def get_credentials() -> Tuple[str, str]:
+        if args.username is None:
+            args.username = input('Username: ')
+        if args.password is None:
+            args.password = getpass.getpass()
+        return args.username, args.password
+
+    with utils.with_cookiejar(utils.new_default_session(), path=args.cookie) as sess:
+        service.login(get_credentials, session=sess, **kwargs)  # type: ignore
