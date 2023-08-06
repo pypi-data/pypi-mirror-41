877ef8b15fe48843b3ef9d3740e8f09e6f2ed33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:18 +0000

    Add package signac/0.9.5/signac-0.9.5-py2.py3-none-any.whl

---
+++ packages/signac/signac-0.9.5-py2.py3-none-any.whl/signac/__init__.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2018 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""The signac framework aids in the management of large and
+heterogeneous data spaces.
+
+It provides a simple and robust data model to create a
+well-defined indexable storage layout for data and metadata.
+This makes it easier to operate on large data spaces,
+streamlines post-processing and analysis and makes data
+collectively accessible."""
+
+from __future__ import absolute_import
+from . import contrib
+from . import db
+from . import cite
+from . import errors
+from . import warnings
+from . import sync
+from .contrib import Project
+from .contrib import TemporaryProject
+from .contrib import get_project
+from .contrib import init_project
+from .contrib import fetch
+from .contrib import export_one
+from .contrib import export
+from .contrib import export_to_mirror
+from .contrib import export_pymongo
+from .contrib import fetch_one  # deprecated
+from .contrib import filesystems as fs
+from .contrib import Collection
+from .contrib import index_files
+from .contrib import index
+from .contrib import RegexFileCrawler
+from .contrib import MasterCrawler
+from .contrib import SignacProjectCrawler
+from .db import get_database
+from .core.jsondict import buffer_reads_writes as buffered
+from .core.jsondict import in_buffered_mode as is_buffered
+from .core.jsondict import flush_all as flush
+from .core.jsondict import get_buffer_size
+from .core.jsondict import get_buffer_load
+
+
+__version__ = '0.9.5'
+
+__all__ = ['__version__', 'contrib', 'db', 'errors', 'warnings', 'sync',
+           'cite',
+           'Project', 'TemporaryProject', 'get_project', 'init_project',
+           'get_database', 'fetch', 'fetch_one',
+           'export_one', 'export', 'export_to_mirror',
+           'Collection',
+           'export_pymongo', 'fs',
+           'index_files', 'index',
+           'RegexFileCrawler',
+           'MasterCrawler',
+           'SignacProjectCrawler',
+           'buffered', 'is_buffered', 'flush', 'get_buffer_size', 'get_buffer_load',
+           ]
---
+++ packages/signac/signac-0.9.5-py2.py3-none-any.whl/signac/contrib/hashing.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2017 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+import hashlib
+import json
+
+
+def calc_id(spec):
+    "Calculate and return a hash value for the given spec."
+    blob = json.dumps(spec, sort_keys=True)
+    m = hashlib.md5()
+    m.update(blob.encode())
+    return m.hexdigest()
