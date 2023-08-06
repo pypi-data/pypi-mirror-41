b3343822db771cd18d98af47bd72aa335c322b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package PatternTuring/0.1/PatternTuring-0.1.tar.gz

---
+++ packages/PatternTuring/PatternTuring-0.1.tar.gz/PatternTuring-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name='PatternTuring',
+    version='0.1',
+    description='simulation of Turing Pattern',
+    url='https://github.com/tsuchiura/kadai',
+    author='tsuchiura',
+    author_email='tsuchiurax@gmail.com',
+    license='MIT',
+    keywords='turing pattern simulation',
+    packages=[
+        "PatternTuring",
+    ],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
