0d0ba3ad8d142f8f0e9cba63c6eb20796f47f224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:45 +0000

    Add package nicexcel/0.1.7/nicexcel-0.1.7-py3-none-any.whl

---
+++ packages/nicexcel/nicexcel-0.1.7-py3-none-any.whl/nicexcel/__init__.py
@@ -0,0 +1,49 @@
+"""
+=====
+Formatting module for Excel files writing
+=====
+
+Provides
+
+  - Content 1: code for writing python files
+
+Available modules
+---------------------
+:nicexcel:
+    Module that takes Pandas DataFrame objects and writes them to excel
+    in a nicely formatted files:
+        - column width auto-adapted to fit characters contained in it
+        - filterable Excel columns
+        - header freezed by default
+        - no indexing by default
+        - easy possibility of number formatting of columns
+
+    Two main functions are available
+        - to_excel_nice() --> wrapper of pd.DataFrame.to_excel() function are
+            still available because of function wrapping
+        - list_to_excel_nice() --> function that writes a dictionary of
+            dataframes into one excel file, inserting each dataframe on a
+            single sheet
+
+
+References
+---------------------
+:Copyright: None
+:License: MIT
+"""
+# =============================================================================
+# Package attributes
+# =============================================================================
+# Documentation format
+__docformat__ = 'NumPy'
+# License type
+__license__ = 'MIT'
+# Status ('Prototype', 'Development' or 'Pre-production')
+__status__ = 'Development'
+# Version
+__version__ = '0.1.6'
+# Release date
+__releasedate__ = '04/02/2018'
+
+
+from nicexcel.wrapper import to_excel, to_excel_ms
