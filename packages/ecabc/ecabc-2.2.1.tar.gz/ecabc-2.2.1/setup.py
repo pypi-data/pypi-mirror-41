b19fbc6b8761b8bf1dea577036eafed052d1d965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:14 +0000

    Add package ecabc/2.2.1/ecabc-2.2.1.tar.gz

---
+++ packages/ecabc/ecabc-2.2.1.tar.gz/ecabc-2.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'ecabc',
+version = "2.2.1",
+description = 'Artificial bee colony for parameters tuning based on fitness scores',
+url = 'https://github.com/hgromer/ecabc',
+author = 'Hernan Gelaf-Romer',
+author_email = 'Hernan_Gelafromer@student.uml.edu',
+license = 'MIT',
+packages = ['ecabc'],
+install_requires = ["numpy", "ColorLogging"],
+zip_safe = False)
