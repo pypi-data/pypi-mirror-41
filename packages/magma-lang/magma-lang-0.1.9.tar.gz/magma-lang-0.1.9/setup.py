15f5b5b905c351e648fc0691958be1a1344eb326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:57 +0000

    Add package magma-lang/0.1.9/magma-lang-0.1.9.tar.gz

---
+++ packages/magma-lang/magma-lang-0.1.9.tar.gz/magma-lang-0.1.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='magma-lang',
+    version='0.1.9',
+    url='https://github.com/phanrahan/magma',
+    license='MIT',
+    maintainer='Lenny Truong',
+    maintainer_email='lenny@cs.stanford.edu',
+    description='An embedded DSL for constructing hardware circuits',
+    scripts=['bin/magma'],
+    packages=[
+        "magma",
+        "magma.frontend",
+        "magma.backend",
+        "magma.passes",
+        "magma.simulator",
+        "magma.testing"
+    ],
+    install_requires=[
+        "colorlog",
+        "astor",
+        "six",
+        "mako",
+        # "pyverilog",
+        "numpy",
+        "graphviz",
+        "coreir>=0.30a0, <= 0.31a0",
+        "bit_vector==0.39a0"
+    ],
+    python_requires='>=3.6'
+)
