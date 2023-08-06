b0dbc0fe8aebdd800d7123fe4087b98e6eda7a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package pyqt-tab-helper/0.2/pyqt-tab-helper-0.2.tar.gz

---
+++ packages/pyqt-tab-helper/pyqt-tab-helper-0.2.tar.gz/pyqt-tab-helper-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name='pyqt-tab-helper',    # This is the name of your PyPI-package.
+	version='0.2',                          # Update the version number for new releases
+	scripts=['pyqtTabHelper.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)
