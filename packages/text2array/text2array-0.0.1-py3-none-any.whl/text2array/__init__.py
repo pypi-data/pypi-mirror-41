a2b44b88bdb94594173301eead1d78ef61bfa0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:22 +0000

    Add package text2array/0.0.1/text2array-0.0.1-py3-none-any.whl

---
+++ packages/text2array/text2array-0.0.1-py3-none-any.whl/text2array/__init__.py
@@ -0,0 +1,12 @@
+from .batches import Batch
+from .datasets import Dataset, StreamDataset
+from .samples import Sample
+from .vocab import Vocab
+
+__all__ = [
+    Sample,
+    Batch,
+    Dataset,
+    StreamDataset,
+    Vocab,
+]
