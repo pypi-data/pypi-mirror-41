7fd377e070d2e2e29405419463351e1ba98b9fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package aws-cloudtrail-events-schema/0.0.1/aws-cloudtrail-events-schema-0.0.1.tar.gz

---
+++ packages/aws-cloudtrail-events-schema/aws-cloudtrail-events-schema-0.0.1.tar.gz/aws-cloudtrail-events-schema-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aws-cloudtrail-events-schema",
+    version="0.0.1",
+    author="Eamonn Faherty",
+    author_email="python-packages@designandsolve.co.uk",
+    description="Utility to discover AWS CloudTrail events pushed into S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eamonnfaherty/aws-cloudtrail-events-schema",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "botocore",
+        "Click",
+    ],
+)
