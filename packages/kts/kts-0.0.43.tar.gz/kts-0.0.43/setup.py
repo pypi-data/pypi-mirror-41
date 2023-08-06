44e6e1d3c7c9633bcc31d10d25ab4f77439caf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:36 +0000

    Add package kts/0.0.43/kts-0.0.43.tar.gz

---
+++ packages/kts/kts-0.0.43.tar.gz/kts-0.0.43/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('kts/VERSION', 'r') as f:
+    VERSION = int(f.read().strip())
+
+VERSION += 1
+
+with open('kts/VERSION', 'w') as f:
+    f.write(str(VERSION))
+
+setuptools.setup(
+    name="kts",
+    version=f"0.0.{VERSION}",
+    author="Nikita Konodyuk",
+    author_email="konodyuk@gmail.com",
+    description="Competition-oriented framework for interactive feature engineering and building pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konodyuk/kts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ['kts = kts.cli:run']
+        },
+    install_requires=[
+        "mprop",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "scikit-optimize",
+        "matplotlib",
+        "dill",
+        "feather-format",
+        "xgboost",
+        "lightgbm",
+        "catboost"
+    ],
+    package_data={
+        'kts': ['VERSION'],
+    }
+)
