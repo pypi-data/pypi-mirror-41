4aeb6bcd57e7215d90dd82ab3e5822c228e0a28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:05 +0000

    Add package rflearn/0.0.1/rflearn-0.0.1.tar.gz

---
+++ packages/rflearn/rflearn-0.0.1.tar.gz/rflearn-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='rflearn',
+      version='0.0.1',
+      description='Reinforcement learning for TensorFlow',
+      author='yogolab.com',
+      author_email='admin@yogolab.com')
+
