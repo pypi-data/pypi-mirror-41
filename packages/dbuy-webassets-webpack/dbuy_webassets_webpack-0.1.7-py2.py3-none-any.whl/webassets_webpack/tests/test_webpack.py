f9f19a16a0985f51fcf3952569808bed812f251e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:08 +0000

    Add package dbuy-webassets-webpack/0.1.7/dbuy_webassets_webpack-0.1.7-py2.py3-none-any.whl

---
+++ packages/dbuy-webassets-webpack/dbuy_webassets_webpack-0.1.7-py2.py3-none-any.whl/webassets_webpack/tests/test_webpack.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from webassets.filter import register_filter
+from webassets.test import TempEnvironmentHelper
+from webassets_webpack import Webpack
+
+register_filter(Webpack)
+
+
+class WebpackFilterTestCase(TempEnvironmentHelper, TestCase):
+
+    default_files = {
+        'main.js': """var odds = evens.map(v => v + 1)"""
+    }
+
+    def setUp(self):
+        super(WebpackFilterTestCase, self).setup()
+
+    def test_webpack_filter(self):
+        self.env.config['WEBPACK_BIN'] = './node_modules/.bin/webpack'
+        self.env.config['WEBPACK_CONFIG'] = './webpack.config.js'
+        self.env.config['WEBPACK_TEMP'] = 'temp.js'
+
+        # if not find_executable('webpack'):
+        #     raise SkipTest()
+
+        self.mkbundle('main.js', filters='webpack',
+                      output='bundle.js').build()
+        # print(self.get('bundle.js'))
+
+        with open("bundle-read.js","w+") as read_file:
+            read_file.write(self.get('bundle.js'))
