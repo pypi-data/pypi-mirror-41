7e9c1fcf732572a9f0f0edbe83fcbafa2d613697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:38 +0000

    Add package django-payu-latam/0.1.7/django-payu-latam-0.1.7.tar.gz

---
+++ packages/django-payu-latam/django-payu-latam-0.1.7.tar.gz/django-payu-latam-0.1.7/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='django-payu-latam',
+      version='0.1.7',
+      include_package_data=True,
+      license='MIT',
+      description='A django integration for PayU Latam.',
+      long_description=read('README.md'),
+      url='https://github.com/GearPlug/django-payu-latam',
+      author='GearPlug',
+      author_email='support@gearplug.io',
+      packages=['payulatam'],
+      install_requires=[
+          'django',
+          'payu-python==0.1.5',
+      ],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Framework :: Django :: 2.0',
+          'Framework :: Django :: 2.1',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ],
+      zip_safe=False)
