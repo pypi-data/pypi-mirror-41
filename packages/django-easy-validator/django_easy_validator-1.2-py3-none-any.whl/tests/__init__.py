1d68ae486f04a9d57fb71f017c78ac2e0151f268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:26 +0000

    Add package django-easy-validator/1.2/django_easy_validator-1.2-py3-none-any.whl

---
+++ packages/django-easy-validator/django_easy_validator-1.2-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,7 @@
+# PROJECT : django-easy-validator
+# TIME    : 18-1-2 上午9:44
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
+# CELL : 13811754531
+# WECHAT : 13811754531
+# WEB : https://youngershen.com
