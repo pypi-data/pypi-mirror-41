2102b0eb73fb977bc63a56f358a8fe5813d9877a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package autodora/0.1.3/autodora-0.1.3.tar.gz

---
+++ packages/autodora/autodora-0.1.3.tar.gz/autodora-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Distribute: python setup.py sdist upload
+
+setup(
+    name='autodora',
+    version='0.1.3',
+    description='Automate experiments and explore your data',
+    url='http://github.com/samuelkolb/pydora',
+    author='Samuel Kolb',
+    author_email='samuel.kolb@me.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['matplotlib', 'peewee', 'pebble', 'numpy']
+)
