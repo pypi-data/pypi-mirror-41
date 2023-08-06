c9b3864091572e5844bc710f1ea4c10769eb3b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:55 +0000

    Add package Mopidy-Audioteka/0.1.5/Mopidy_Audioteka-0.1.5-py2.py3-none-any.whl

---
+++ packages/Mopidy-Audioteka/Mopidy_Audioteka-0.1.5-py2.py3-none-any.whl/mopidy_audioteka/backend.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+import pykka
+
+from mopidy import backend
+
+
+from mopidy_audioteka import library, playback, audioteka
+
+
+logger = logging.getLogger(__name__)
+
+
+class AudiotekaBackend(pykka.ThreadingActor, backend.Backend):
+    uri_schemes = ['audioteka']
+
+    def __init__(self, config, audio):
+        super(AudiotekaBackend, self).__init__()
+
+        self.config = config
+        self.audio = audio
+
+        self.audioteka = audioteka.Audioteka(
+            config['proxy'], config['audioteka']['username'], config['audioteka']['password']
+        )
+        self.library = library.AudiotekaLibraryProvider(backend=self)
+        self.playback = playback.AudiotekaPlaybackProvider(
+            audio=audio, backend=self, cache_dir=config['core']['cache_dir'])
+        self.playlists = None
+
+    def on_start(self):
+        logger.info('Start refreshing Audioteka')
+        self.library.refresh()
