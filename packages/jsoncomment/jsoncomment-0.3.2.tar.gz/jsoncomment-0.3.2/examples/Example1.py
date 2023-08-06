ad1127a2336dc8b8cbce025e151f106af0127b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package jsoncomment/0.3.2/jsoncomment-0.3.2.tar.gz

---
+++ packages/jsoncomment/jsoncomment-0.3.2.tar.gz/jsoncomment-0.3.2/examples/Example1.py
@@ -0,0 +1,54 @@
+#!/bin/python
+# coding: utf-8
+
+##########################################################################################################################################
+
+try:
+	import ujson as json
+except ImportError:
+	import json
+
+from jsoncomment import JsonComment
+
+##########################################################################################################################################
+
+def main():
+	string = """
+/******************
+Comment 1
+Comment 2
+******************/
+[
+	# Objects
+	{
+		"key" : "value",
+		"another key" :
+		\"\"\"
+		\\n
+		A multiline string.\\n
+		It will wrap to single line, 
+		but a trailing space per line is kept.
+		\"\"\",
+	},
+	; Other Values
+	81,
+	// Allow a non standard trailing comma
+	true,
+]
+"""
+
+	parser = JsonComment(json)
+	parsed_object = parser.loads(string)
+
+	print("\n", "*"*80, "\n")
+
+	print(parsed_object[0]["another key"], "\n")
+
+	print(parser.dumps(parsed_object), "\n")
+
+	print("\n", "*"*80, "\n")
+
+if __name__ == "__main__":
+	main()
+
+##########################################################################################################################################
