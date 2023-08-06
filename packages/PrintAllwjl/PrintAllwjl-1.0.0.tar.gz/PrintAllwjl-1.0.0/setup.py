60c87d954e28aae7d7df004d7dad83675fb27e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:20 +0000

    Add package PrintAllwjl/1.0.0/PrintAllwjl-1.0.0.tar.gz

---
+++ packages/PrintAllwjl/PrintAllwjl-1.0.0.tar.gz/PrintAllwjl-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='PrintAllwjl',
+    version='1.0.0',
+    py_modules=['PrintAllwjl'],
+    author='lalalaWJL',
+    author_email='wjl254018382@163.com',
+    #url='https://blog.csdn.net/soulplayer4/'
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+     )
