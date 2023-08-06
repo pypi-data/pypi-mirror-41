5e2ba233f4de14b4bd8cd00b2bcce403dafcbcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package lactolyse/0.9.0a11/lactolyse-0.9.0a11.tar.gz

---
+++ packages/lactolyse/lactolyse-0.9.0a11.tar.gz/lactolyse-0.9.0a11/src/lactolyse/views/download.py
@@ -0,0 +1,26 @@
+"""Lactolyse download views."""
+import os
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views import View
+
+from lactolyse.models import LactateThresholdAnalyses
+from lactolyse.utils import deserialize_model_instance
+
+
+class ReportDownloadView(View):
+    """Download view."""
+
+    def get(self, request, ref):
+        """Serve the file specified in Django's session."""
+        analysis = deserialize_model_instance(ref)
+
+        if analysis.contributor != request.user and not request.user.is_superuser:
+            return HttpResponseBadRequest()
+
+        response = HttpResponse(analysis.report.read())
+        response['Content-Type'] = 'application/pdf'
+        response['Content-Length'] = os.path.getsize(file_path)
+        response['Content-Disposition'] = 'attachment; filename="Report.pdf"'
+
+        return response
