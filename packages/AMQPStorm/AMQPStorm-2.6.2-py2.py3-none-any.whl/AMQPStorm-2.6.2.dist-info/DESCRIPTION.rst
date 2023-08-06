eb9ba2bcd6a74ea287afbba90dd0be7231e107dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:12 +0000

    Add package AMQPStorm/2.6.2/AMQPStorm-2.6.2-py2.py3-none-any.whl

---
+++ packages/AMQPStorm/AMQPStorm-2.6.2-py2.py3-none-any.whl/AMQPStorm-2.6.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,78 @@
+AMQPStorm
+=========
+Thread-safe Python RabbitMQ Client & Management library.
+
+|Version| |CodeClimate| |Travis| |Coverage|
+
+Introduction
+============
+AMQPStorm is a library designed to be consistent, stable and thread-safe.
+
+- 100% Test Coverage!
+- Supports Python 2.7 and Python 3.3+.
+- Fully tested against Python Implementations; CPython, PyPy and Pyston.
+
+Documentation
+=============
+
+Additional documentation is available on `amqpstorm.io <https://www.amqpstorm.io>`_.
+
+Changelog
+=========
+
+Version 2.6.2
+-------------
+- Set default TCP Timeout to 10s on UriConnection to match Connection [#67] - Thanks josemonteiro.
+- Internal RPC Timeout for Opening and Closing Connections are now set to a fixed 30s [#67] - Thanks josemonteiro.
+
+Version 2.6.1
+-------------
+- Fixed minor issue with the last channel id not being available.
+
+Version 2.6.0
+-------------
+- Re-use closed channel ids [#55] - Thanks mikemrm.
+- Changed Poller Timeout to be a constant.
+- Improved Connection Close performance.
+- Channels is now a publicly available variable in Connections.
+
+Version 2.5.0
+-------------
+- Upgraded pamqp to v2.0.0.
+    - Python 3 keys will now always be of type str.
+    - For more information see https://pamqp.readthedocs.io/en/latest/history.html
+- Properly wait until the inbound queue is empty when break_on_empty is set [#63] - Thanks TomGudman.
+- Fixed issue with Management queue/exchange declare when the passive flag was set to True.
+
+Version 2.4.2
+-------------
+- Added support for External Authentication - Thanks Bernd Höhl.
+- Fixed typo in setup.py extra requirements - Thanks Bernd Höhl.
+- LICENSE file now included in package - Thanks Tomáš Chvátal.
+
+Version 2.4.1
+-------------
+- Added client/server negotiation to better determine the maximum supported channels and maximum allowed frame size [#52] - Thanks gastlich.
+- We now raise an exception if the maximum allowed channel count is reached.
+
+Version 2.4.0
+-------------
+- basic.consume now allows for multiple callbacks [#48].
+
+Credits
+=======
+Special thanks to gmr (Gavin M. Roy) for creating pamqp, and in addition amqpstorm is heavily influenced by his pika and rabbitpy libraries.
+
+.. |Version| image:: https://badge.fury.io/py/amqpstorm.svg?
+  :target: http://badge.fury.io/py/amqpstorm
+
+.. |CodeClimate| image:: https://codeclimate.com/github/eandersson/amqpstorm/badges/gpa.svg
+  :target: https://codeclimate.com/github/eandersson/amqpstorm
+
+.. |Travis| image:: https://travis-ci.org/eandersson/amqpstorm.svg
+  :target: https://travis-ci.org/eandersson/amqpstorm
+
+.. |Coverage| image:: https://codecov.io/gh/eandersson/amqpstorm/branch/master/graph/badge.svg
+  :target: https://codecov.io/gh/eandersson/amqpstorm
+
+
---
+++ packages/AMQPStorm/AMQPStorm-2.6.2-py2.py3-none-any.whl/amqpstorm/tests/unit/uri_connection/uri_connection_tests.py
@@ -0,0 +1,156 @@
+import ssl
+
+from amqpstorm import UriConnection
+from amqpstorm.connection import DEFAULT_HEARTBEAT_INTERVAL
+from amqpstorm.connection import DEFAULT_SOCKET_TIMEOUT
+from amqpstorm.connection import DEFAULT_VIRTUAL_HOST
+from amqpstorm.tests.utility import TestFramework
+
+
+class UriConnectionTests(TestFramework):
+    def test_uri_default(self):
+        connection = UriConnection(
+            'amqp://guest:guest@localhost:5672/%2F', True
+        )
+
+        self.assertEqual(connection.parameters['hostname'], 'localhost')
+        self.assertEqual(connection.parameters['username'], 'guest')
+        self.assertEqual(connection.parameters['password'], 'guest')
+        self.assertEqual(connection.parameters['virtual_host'],
+                         DEFAULT_VIRTUAL_HOST)
+        self.assertEqual(connection.parameters['port'], 5672)
+        self.assertEqual(connection.parameters['heartbeat'],
+                         DEFAULT_HEARTBEAT_INTERVAL)
+        self.assertEqual(connection.parameters['timeout'],
+                         DEFAULT_SOCKET_TIMEOUT)
+        self.assertFalse(connection.parameters['ssl'])
+
+    def test_uri_ssl(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F', True
+        )
+
+        self.assertTrue(connection.parameters['ssl'])
+
+    def test_uri_simple(self):
+        connection = UriConnection(
+            'amqps://localhost:5672/%2F', True
+        )
+
+        self.assertEqual(connection.parameters['hostname'], 'localhost')
+        self.assertEqual(connection.parameters['username'], 'guest')
+        self.assertEqual(connection.parameters['password'], 'guest')
+
+    def test_uri_set_hostname(self):
+        connection = UriConnection(
+            'amqps://guest:guest@my-server:5672/%2F?'
+            'heartbeat=360', True
+        )
+
+        self.assertIsInstance(connection.parameters['hostname'], str)
+        self.assertEqual(connection.parameters['hostname'], 'my-server')
+
+    def test_uri_set_username(self):
+        connection = UriConnection(
+            'amqps://username:guest@localhost:5672/%2F?'
+            'heartbeat=360', True
+        )
+
+        self.assertIsInstance(connection.parameters['username'], str)
+        self.assertEqual(connection.parameters['username'], 'username')
+
+    def test_uri_set_password(self):
+        connection = UriConnection(
+            'amqps://guest:password@localhost:5672/%2F?'
+            'heartbeat=360', True
+        )
+
+        self.assertIsInstance(connection.parameters['password'], str)
+        self.assertEqual(connection.parameters['password'], 'password')
+
+    def test_uri_set_port(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F', True
+        )
+
+        self.assertIsInstance(connection.parameters['port'], int)
+        self.assertEqual(connection.parameters['port'], 5672)
+
+    def test_uri_set_heartbeat(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F?'
+            'heartbeat=360', True
+        )
+
+        self.assertIsInstance(connection.parameters['heartbeat'], int)
+        self.assertEqual(connection.parameters['heartbeat'], 360)
+
+    def test_uri_set_timeout(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F?'
+            'timeout=360', True
+        )
+
+        self.assertIsInstance(connection.parameters['timeout'], int)
+        self.assertEqual(connection.parameters['timeout'], 360)
+
+    def test_uri_set_virtual_host(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/travis', True
+        )
+
+        self.assertIsInstance(connection.parameters['virtual_host'], str)
+        self.assertEqual(connection.parameters['virtual_host'], 'travis')
+
+    def test_uri_set_ssl(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5671/%2F?'
+            'ssl_version=protocol_tlsv1&cert_reqs=cert_required&'
+            'keyfile=file.key&certfile=file.crt&'
+            'ca_certs=travis-ci', True
+        )
+
+        self.assertTrue(connection.parameters['ssl'])
+        self.assertEqual(connection.parameters['ssl_options']['ssl_version'],
+                         ssl.PROTOCOL_TLSv1)
+        self.assertEqual(connection.parameters['ssl_options']['cert_reqs'],
+                         ssl.CERT_REQUIRED)
+        self.assertEqual(connection.parameters['ssl_options']['keyfile'],
+                         'file.key')
+        self.assertEqual(connection.parameters['ssl_options']['certfile'],
+                         'file.crt')
+        self.assertEqual(connection.parameters['ssl_options']['ca_certs'],
+                         'travis-ci')
+
+    def test_uri_get_ssl_version(self):
+        connection = UriConnection(
+            'amqp://guest:guest@localhost:5672/%2F', True
+        )
+
+        self.assertEqual(ssl.PROTOCOL_TLSv1,
+                         connection._get_ssl_version('protocol_tlsv1'))
+
+    def test_uri_get_ssl_validation(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F', True
+        )
+
+        self.assertEqual(ssl.CERT_REQUIRED,
+                         connection._get_ssl_validation('cert_required'))
+
+    def test_uri_get_ssl_options(self):
+        connection = UriConnection(
+            'amqps://guest:guest@localhost:5672/%2F', True
+        )
+        ssl_kwargs = {
+            'cert_reqs': ['cert_required'],
+            'ssl_version': ['protocol_tlsv1'],
+            'keyfile': ['file.key'],
+            'certfile': ['file.crt']
+        }
+        ssl_options = connection._parse_ssl_options(ssl_kwargs)
+
+        self.assertEqual(ssl_options['cert_reqs'], ssl.CERT_REQUIRED)
+        self.assertEqual(ssl_options['ssl_version'], ssl.PROTOCOL_TLSv1)
+        self.assertEqual(ssl_options['keyfile'], 'file.key')
+        self.assertEqual(ssl_options['certfile'], 'file.crt')
