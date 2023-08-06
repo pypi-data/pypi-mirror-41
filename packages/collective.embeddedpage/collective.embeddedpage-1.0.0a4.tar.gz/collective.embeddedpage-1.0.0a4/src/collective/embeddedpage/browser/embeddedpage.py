ebb36dba4dfc1d8ce3e23fdf1a2fef0b3a768e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package collective.embeddedpage/1.0.0a4/collective.embeddedpage-1.0.0a4.tar.gz

---
+++ packages/collective.embeddedpage/collective.embeddedpage-1.0.0a4.tar.gz/collective.embeddedpage-1.0.0a4/src/collective/embeddedpage/browser/embeddedpage.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+import lxml
+import requests
+
+
+class EmbeddedPageView(BrowserView):
+
+    template = ViewPageTemplateFile('embeddedpage.pt')
+
+    def __call__(self):
+        response = requests.get(self.context.url)
+        # we receive a utf-8 encoded string from requests
+        # lxml expect unicode though
+        content = safe_unicode(response.content)
+        content = lxml.html.fromstring(content)
+        if content.find('body'):
+            content = content.find('body')
+        self.embeddedpage = etree.tostring(content)
+        return self.template()
