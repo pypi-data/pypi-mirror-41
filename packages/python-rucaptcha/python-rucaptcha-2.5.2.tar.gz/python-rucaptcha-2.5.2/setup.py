5b06710d35b7751bf083ced689d879eefe4636f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package python-rucaptcha/2.5.2/python-rucaptcha-2.5.2.tar.gz

---
+++ packages/python-rucaptcha/python-rucaptcha-2.5.2.tar.gz/python-rucaptcha-2.5.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name = 'python-rucaptcha',
+    version = '2.5.2',
+
+    author = 'AndreiDrang, redV0ID',
+
+    packages = ['python_rucaptcha'],
+    install_requires = [
+        'requests==2.21.0',
+        'aiohttp==3.5.4',
+        'pika==0.13.0'
+        ],
+    description = 'Python 3 RuCaptcha library with AIO module.',
+    author_email = 'drang.andray@gmail.com',
+    package_dir={'python-rucaptcha': 'python_rucaptcha'},
+    include_package_data=True,
+    url = 'https://github.com/AndreiDrang/python-rucaptcha',
+    license = 'AGPL-3.0',
+    keywords = '''captcha 
+				rucaptcha 
+				python3
+				flask
+				recaptcha
+				captcha
+				security
+				api
+				python-library
+				python-rucaptcha
+				rucaptcha-client''',
+    python_requires = '>=3.6',
+    zip_safe=False
+    )
