de410f05a672db5494266b330d847622ebefa9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package FlowIO/0.9.3/FlowIO-0.9.3.tar.gz

---
+++ packages/FlowIO/FlowIO-0.9.3.tar.gz/FlowIO-0.9.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='FlowIO',
+    version='0.9.3',
+    packages=['flowio'],
+    package_data={'': []},
+    description='Flow Cytometry Standard I/O',
+    requires=[],
+)
