a41379ec2335c8e4c04363a7a392118da686058d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:44 +0000

    Add package dsndls-distributions/0.1/dsndls_distributions-0.1.tar.gz

---
+++ packages/dsndls-distributions/dsndls_distributions-0.1.tar.gz/dsndls_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsndls_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsndls_distributions'],
+      author = 'Liberto Siahaan',
+      author_mail = 'liberto.siahaan@gmail.com',
+      zip_safe=False)
