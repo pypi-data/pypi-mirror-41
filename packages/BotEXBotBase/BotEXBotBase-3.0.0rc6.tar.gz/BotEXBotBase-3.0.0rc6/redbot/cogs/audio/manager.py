7ca662b0393b3bfada58e2bad6eb6005660eaf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:31 +0000

    Add package BotEXBotBase/3.0.0rc6/BotEXBotBase-3.0.0rc6.tar.gz

---
+++ packages/BotEXBotBase/BotEXBotBase-3.0.0rc6.tar.gz/BotEXBotBase-3.0.0rc6/redbot/cogs/audio/manager.py
@@ -0,0 +1,127 @@
+import shlex
+import shutil
+import asyncio
+import asyncio.subprocess
+import os
+import logging
+import re
+from subprocess import Popen, DEVNULL
+from typing import Optional, Tuple
+
+_JavaVersion = Tuple[int, int]
+
+log = logging.getLogger("red.audio.manager")
+
+proc = None
+SHUTDOWN = asyncio.Event()
+
+
+def has_java_error(pid):
+    from . import LAVALINK_DOWNLOAD_DIR
+
+    poss_error_file = LAVALINK_DOWNLOAD_DIR / "hs_err_pid{}.log".format(pid)
+    return poss_error_file.exists()
+
+
+async def monitor_lavalink_server(loop):
+    while not SHUTDOWN.is_set():
+        if proc.poll() is not None:
+            break
+        await asyncio.sleep(0.5)
+
+    if not SHUTDOWN.is_set():
+        log.info("Lavalink jar shutdown.")
+        if not has_java_error(proc.pid):
+            log.info("Restarting Lavalink jar.")
+            await start_lavalink_server(loop)
+        else:
+            log.error(
+                "Your Java is borked. Please find the hs_err_pid{}.log file"
+                " in the Audio data folder and report this issue.".format(proc.pid)
+            )
+
+
+async def has_java(loop) -> Tuple[bool, Optional[_JavaVersion]]:
+    java_available = shutil.which("java") is not None
+    if not java_available:
+        return False, None
+
+    version = await get_java_version(loop)
+    return (2, 0) > version >= (1, 8) or version >= (8, 0), version
+
+
+async def get_java_version(loop) -> _JavaVersion:
+    """
+    This assumes we've already checked that java exists.
+    """
+    _proc: asyncio.subprocess.Process = await asyncio.create_subprocess_exec(
+        "java",
+        "-version",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        loop=loop,
+    )
+    # java -version outputs to stderr
+    _, err = await _proc.communicate()
+
+    version_info: str = err.decode("utf-8")
+    # We expect the output to look something like:
+    #     $ java -version
+    #     ...
+    #     ... version "MAJOR.MINOR.PATCH[_BUILD]" ...
+    #     ...
+    # We only care about the major and minor parts though.
+    version_line_re = re.compile(
+        r'version "(?P<major>\d+).(?P<minor>\d+).\d+(?:_\d+)?(?:-[A-Za-z0-9]+)?"'
+    )
+    short_version_re = re.compile(r'version "(?P<major>\d+)"')
+
+    lines = version_info.splitlines()
+    for line in lines:
+        match = version_line_re.search(line)
+        short_match = short_version_re.search(line)
+        if match:
+            return int(match["major"]), int(match["minor"])
+        elif short_match:
+            return int(short_match["major"]), 0
+
+    raise RuntimeError(
+        "The output of `java -version` was unexpected. Please report this issue on Red's "
+        "issue tracker."
+    )
+
+
+async def start_lavalink_server(loop):
+    java_available, java_version = await has_java(loop)
+    if not java_available:
+        raise RuntimeError("You must install Java 1.8+ for Lavalink to run.")
+
+    extra_flags = ""
+    if java_version == (1, 8):
+        extra_flags = "-Dsun.zip.disableMemoryMapping=true"
+
+    from . import LAVALINK_DOWNLOAD_DIR, LAVALINK_JAR_FILE
+
+    start_cmd = "java {} -jar {}".format(extra_flags, LAVALINK_JAR_FILE.resolve())
+
+    global proc
+    proc = Popen(
+        shlex.split(start_cmd, posix=os.name == "posix"),
+        cwd=str(LAVALINK_DOWNLOAD_DIR),
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+    )
+
+    log.info("Lavalink jar started. PID: {}".format(proc.pid))
+
+    loop.create_task(monitor_lavalink_server(loop))
+
+
+def shutdown_lavalink_server():
+    log.info("Shutting down lavalink server.")
+    SHUTDOWN.set()
+    global proc
+    if proc is not None:
+        proc.terminate()
+        proc.wait()
+        proc = None
---
+++ packages/BotEXBotBase/BotEXBotBase-3.0.0rc6.tar.gz/BotEXBotBase-3.0.0rc6/setup.py
@@ -0,0 +1,135 @@
+import distutils.ccompiler as ccompiler
+import os
+import re
+import tempfile
+from distutils.errors import CCompilerError, DistutilsPlatformError
+from setuptools import setup, find_packages
+
+install_requires = [
+    "aiohttp-json-rpc==0.11.2",
+    "aiohttp==3.4.4",
+    "appdirs==1.4.3",
+    "async-timeout==3.0.1",
+    "attrs==18.2.0",
+    "chardet==3.0.4",
+    "colorama==0.4.1",
+    "discord.py>=1.0.0a0",
+    "distro==1.3.0; sys_platform == 'linux'",
+    "fuzzywuzzy==0.17.0",
+    "idna-ssl==1.1.0",
+    "idna==2.8",
+    "multidict==4.5.2",
+    "python-levenshtein==0.12.0",
+    "pyyaml==3.13",
+    "raven==6.10.0",
+    "raven-aiohttp==0.7.0",
+    "schema==0.6.8",
+    "websockets==6.0",
+    "yarl==1.3.0",
+]
+
+extras_require = {
+    "test": [
+        "atomicwrites==1.2.1",
+        "more-itertools==5.0.0",
+        "pluggy==0.8.1",
+        "py==1.7.0",
+        "pytest==4.1.0",
+        "pytest-asyncio==0.10.0",
+        "six==1.12.0",
+    ],
+    "mongo": ["motor==2.0.0", "pymongo==3.7.2", "dnspython==1.16.0"],
+    "docs": [
+        "alabaster==0.7.12",
+        "babel==2.6.0",
+        "certifi==2018.11.29",
+        "docutils==0.14",
+        "imagesize==1.1.0",
+        "Jinja2==2.10",
+        "MarkupSafe==1.1.0",
+        "packaging==18.0",
+        "pyparsing==2.3.0",
+        "Pygments==2.3.1",
+        "pytz==2018.9",
+        "requests==2.21.0",
+        "six==1.12.0",
+        "snowballstemmer==1.2.1",
+        "sphinx==1.8.3",
+        "sphinx_rtd_theme==0.4.2",
+        "sphinxcontrib-asyncio==0.2.0",
+        "sphinxcontrib-websupport==1.1.0",
+        "urllib3==1.24.1",
+    ],
+    "voice": ["red-lavalink==0.1.2"],
+    "style": ["black==18.9b0", "click==7.0", "toml==0.10.0"],
+}
+
+python_requires = ">=3.7,<3.8"
+
+
+def get_dependency_links():
+    with open("dependency_links.txt") as file:
+        return file.read().splitlines()
+
+
+def check_compiler_available():
+    m = ccompiler.new_compiler()
+
+    with tempfile.TemporaryDirectory() as tdir:
+        with open(os.path.join(tdir, "dummy.c"), "w") as tfile:
+            tfile.write("int main(int argc, char** argv) {return 0;}")
+        try:
+            m.compile([tfile.name], output_dir=tdir)
+        except (CCompilerError, DistutilsPlatformError):
+            return False
+    return True
+
+
+def get_version():
+    with open("redbot/core/__init__.py") as f:
+        version = re.search(
+            r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+        ).group(1)
+    return version
+
+
+if __name__ == "__main__":
+    if not check_compiler_available():
+        install_requires.remove(
+            next(r for r in install_requires if r.lower().startswith("python-levenshtein"))
+        )
+
+
+    setup(
+        name="BotEXBotBase",
+        version=get_version(),
+        packages=find_packages(include=["redbot", "redbot.*"]),
+        package_data={"": ["locales/*.po", "data/*", "data/**/*"]},
+        url="https://github.com/BotEX-Developers/botbase",
+        license="GPLv3",
+        author="BotEX and Cog-Creators",
+        author_email="",
+        description="A highly customizable Discord bot",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Framework :: AsyncIO",
+            "Framework :: Pytest",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Communications :: Chat",
+        ],
+        entry_points={
+            "console_scripts": [
+                "redbot=redbot.__main__:main",
+                "redbot-setup=redbot.setup:main",
+                "redbot-launcher=redbot.launcher:main",
+            ],
+            "pytest11": ["red-discordbot = redbot.pytest"],
+        },
+        python_requires=python_requires,
+        install_requires=install_requires,
+        dependency_links=get_dependency_links(),
+        extras_require=extras_require,
+    )
