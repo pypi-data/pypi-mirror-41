3202fcf1c1e71e33ca471dfa638db6528043bf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:32 +0000

    Add package pyinsults/0.0.0.0.7/pyinsults-0.0.0.0.7-py3-none-any.whl

---
+++ packages/pyinsults/pyinsults-0.0.0.0.7-py3-none-any.whl/pyinsults/insults.py
@@ -0,0 +1,15 @@
+import random
+
+adjectives = ["stinking", "whining", "pathetic"]
+
+insults = ["asshole", "cumstain", "foolio", "dickwad", "Trump voter"]
+
+def random_insult():
+  return random.choice(insults)
+
+def long_insult():
+  return ' '.join([random.choice(adjectives), random.choice(insults)])
+
+
+if __name__ == "__main__":
+  random_insult()
