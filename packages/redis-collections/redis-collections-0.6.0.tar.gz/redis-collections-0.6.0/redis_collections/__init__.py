cb0661016a9c3b6b237b35f57b6fc32bad85a351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:49 +0000

    Add package redis-collections/0.6.0/redis-collections-0.6.0.tar.gz

---
+++ packages/redis-collections/redis-collections-0.6.0.tar.gz/redis-collections-0.6.0/redis_collections/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+__title__ = 'redis-collections'
+__version__ = '0.6.0'
+__author__ = 'Honza Javorek'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2013-? Honza Javorek'
+
+
+from .base import RedisCollection
+from .dicts import DefaultDict, Dict, Counter
+from .lists import Deque, List
+from .sets import Set
+from .sortedsets import GeoDB, SortedSetCounter
+from .syncable import (
+    LRUDict,
+    SyncableDict,
+    SyncableCounter,
+    SyncableDeque,
+    SyncableDefaultDict,
+    SyncableList,
+    SyncableSet,
+)
+
+__all__ = [
+    'Counter',
+    'DefaultDict',
+    'Deque',
+    'Dict',
+    'GeoDB',
+    'List',
+    'LRUDict',
+    'RedisCollection',
+    'Set',
+    'SortedSetCounter',
+    'SyncableDict',
+    'SyncableCounter',
+    'SyncableDefaultDict',
+    'SyncableDeque',
+    'SyncableList',
+    'SyncableSet',
+]
---
+++ packages/redis-collections/redis-collections-0.6.0.tar.gz/redis-collections-0.6.0/setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+import io
+import os
+import re
+import subprocess
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages  # NOQA
+
+# Hack to prevent stupid "TypeError: 'NoneType' object is not callable"
+# error in multiprocessing/util.py _exit_function when running `python
+# setup.py test`
+try:
+    import multiprocessing  # NOQA
+except ImportError:
+    pass
+
+
+base_path = os.path.dirname(__file__)
+
+
+# version
+meta_file = os.path.join(base_path, 'redis_collections/__init__.py')
+meta_file_contents = io.open(meta_file, encoding='utf-8').read()
+meta = dict(re.findall(r'__([^_]+)__ = \'([^\']*)\'', meta_file_contents))
+
+
+# release a version, publish to GitHub and PyPI
+if sys.argv[-1] == 'publish':
+    subprocess.check_call(['git', 'tag', 'v' + meta['version']])
+    subprocess.check_call(['git', 'push', '--tags', 'origin', 'master:master'])
+    # ...the rest happens on Travis CI
+    sys.exit()
+
+
+setup(
+    name=meta['title'],
+    version=meta['version'],
+    description='Set of basic Python collections backed by Redis.',
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+    author=meta['author'],
+    author_email='mail@honzajavorek.cz',
+    url='https://github.com/honzajavorek/redis-collections',
+    license='ISC',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=['redis>=3.1.0,<4.0.0', 'six>=1.0.0,<2.0.0'],
+    zip_safe=False,
+    keywords=['redis', 'persistence'],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+    )
+)
