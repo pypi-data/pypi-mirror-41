42ea1287b97b2066e2e0b11dc80aad13e6e31e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package cluster-funk/0.0.2a13/cluster_funk-0.0.2a13.tar.gz

---
+++ packages/cluster-funk/cluster_funk-0.0.2a13.tar.gz/cluster_funk-0.0.2a13/cluster_funk/templates/word_count/lib/word_count.py
@@ -0,0 +1,25 @@
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+# word count up some words
+
+
+def word_count(input_file):
+    spark = SparkSession\
+        .builder\
+        .appName("PythonSort")\
+        .getOrCreate()
+
+    lines = spark.read.text(input_file).rdd.map(lambda r: r[0])
+
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+        .map(lambda x: (x, 1)) \
+        .reduceByKey(add)
+
+    output = counts.collect()
+
+    spark.stop()
+
+    return output
