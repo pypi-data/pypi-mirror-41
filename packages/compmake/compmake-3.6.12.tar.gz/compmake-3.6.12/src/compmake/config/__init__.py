fb00817df1500bae10e46ebad5e99ea232aafe14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:51 +0000

    Add package compmake/3.6.12/compmake-3.6.12.tar.gz

---
+++ packages/compmake/compmake-3.6.12.tar.gz/compmake-3.6.12/src/compmake/config/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import sys
+from collections import namedtuple
+
+from .structure import *
+from .config_list import *
+from .config_html import *
+from .config_optparse import *
+
