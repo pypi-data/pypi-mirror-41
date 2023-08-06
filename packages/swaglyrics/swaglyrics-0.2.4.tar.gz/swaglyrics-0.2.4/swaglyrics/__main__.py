f3aaef7cba0fb4f70ee5ef0bfef093622aab1532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:16 +0000

    Add package swaglyrics/0.2.4/swaglyrics-0.2.4.tar.gz

---
+++ packages/swaglyrics/swaglyrics-0.2.4.tar.gz/swaglyrics-0.2.4/swaglyrics/__main__.py
@@ -0,0 +1,62 @@
+import argparse
+import time
+import os
+import webbrowser
+import threading
+import requests
+from swaglyrics.cli import lyrics, clear
+from swaglyrics import spotify
+from swaglyrics.tab import app
+
+
+def main():
+	print('Updating unsupported.txt from server.')
+	with open('unsupported.txt', 'w', encoding='utf-8') as f:
+		response = requests.get('http://aadibajpai.pythonanywhere.com/master_unsupported')
+		f.write(response.text)
+	print("Updated unsupported.txt successfully.")
+
+	parser = argparse.ArgumentParser(
+		description="Get lyrics for the currently playing song on Spotify. Either --tab or --cli is required.")
+
+	parser.add_argument('-t', '--tab', action='store_true', help='Display lyrics in a browser tab.')
+	parser.add_argument('-c', '--cli', action='store_true', help='Display lyrics in the command-line.')
+
+	args = parser.parse_args()
+
+	if args.tab:
+		print('Firing up a browser tab!')
+		app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+		app.static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+		port = 5042  # random
+		url = "http://127.0.0.1:{port}".format(port=port)
+		threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+		app.run(port=port)
+
+	elif args.cli:
+		song = spotify.song()  # get currently playing song
+		artist = spotify.artist()  # get currently playing artist
+		print(lyrics(song, artist))
+		print('\n(Press Ctrl+C to quit)')
+		while True:
+			# refresh every 5s to check whether song changed
+			# if changed, display the new lyrics
+			try:
+				if song == spotify.song() and artist == spotify.artist():
+					time.sleep(5)
+				else:
+					song = spotify.song()
+					artist = spotify.artist()
+					if song and artist is not None:
+						clear()
+						print(lyrics(song, artist))
+						print('\n(Press Ctrl+C to quit)')
+			except KeyboardInterrupt:
+				exit()
+
+	else:
+		parser.print_help()
+
+
+if __name__ == '__main__':
+	main()
