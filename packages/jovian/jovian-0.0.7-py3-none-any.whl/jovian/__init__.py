a4f490154b10143b7fe49a989ee64608381fc7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:31 +0000

    Add package jovian/0.0.7/jovian-0.0.7-py3-none-any.whl

---
+++ packages/jovian/jovian-0.0.7-py3-none-any.whl/jovian/__init__.py
@@ -0,0 +1,25 @@
+from time import sleep
+from jovian.utils.anaconda import upload_conda_env
+from jovian.utils.pip import upload_pip_env
+from jovian.utils.api import create_gist
+from jovian.utils.logger import log
+from jovian.utils.constants import WEBAPP_URL
+
+
+def commit(capture_env=True):
+    """Save the notebook, capture the environment, and upload to cloud for sharing"""
+    res = create_gist()
+    slug, owner = res['slug'], res['owner']
+
+    # Save & upload environment
+    if capture_env:
+        try:
+            upload_conda_env(slug)
+        except:
+            try:
+                upload_pip_env(slug)
+            except:
+                log('Failed to capture the Python environment', error=True)
+
+    log('Committed successfully! ' + WEBAPP_URL +
+        "/" + owner['username'] + "/" + slug)
