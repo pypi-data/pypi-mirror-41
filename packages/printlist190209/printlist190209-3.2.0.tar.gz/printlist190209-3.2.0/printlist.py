8257c137d7e30279737be36e417b3aec0debb834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:22 +0000

    Add package printlist190209/3.2.0/printlist190209-3.2.0.tar.gz

---
+++ packages/printlist190209/printlist190209-3.2.0.tar.gz/printlist190209-3.2.0/printlist.py
@@ -0,0 +1,12 @@
+﻿"""printlist模块，提供了一个名为printlist()的函数
+这个函数的作用是显示列表，其中有可能包含（也可能不包含）嵌套列表。"""
+def printlist(isinstance_list,level=0):
+    """这个函数有一个位置参数，名为isinstance_list，这可以是任何python列表，也可以是包含嵌套列表的列表。
+    所指定的列表中的每个数据项都会顺序显示在屏幕上，各数据项各占一行"""
+    for each_item in isinstance_list:
+        if isinstance(each_item,list):
+            printlist(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
