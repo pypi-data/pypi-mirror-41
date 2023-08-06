0be47c7ddfa0ee5089464243a0932a3db7ee62fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:14 +0000

    Add package cnab/0.1.4/cnab-0.1.4-py3-none-any.whl

---
+++ packages/cnab/cnab-0.1.4-py3-none-any.whl/cnab/__init__.py
@@ -0,0 +1,14 @@
+from cnab.types import (
+    Action,
+    Credential,
+    ImagePlatform,
+    Ref,
+    Image,
+    InvocationImage,
+    Maintainer,
+    Destination,
+    Metadata,
+    Parameter,
+    Bundle,
+)
+from cnab.cnab import CNAB
