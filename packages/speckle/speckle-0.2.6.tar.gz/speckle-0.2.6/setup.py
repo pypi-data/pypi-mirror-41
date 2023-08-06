0804e2797aa719c9928b0e61facf4cab372c48ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:44 +0000

    Add package speckle/0.2.6/speckle-0.2.6.tar.gz

---
+++ packages/speckle/speckle-0.2.6.tar.gz/speckle-0.2.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name="speckle", 
+	description="Python client for Speckle framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",	
+	url="https://github.com/speckleworks/",
+	author="Tom Svilans",
+	author_email="tom.svilans@gmail.com",
+	license="MIT",
+	version="0.2.6",
+	packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],	
+	)
