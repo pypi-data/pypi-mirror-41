b6f9adbdcf0f89bb87c0577768d047c0bd755ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:03 +0000

    Add package samuel/0.1.9/samuel-0.1.9.tar.gz

---
+++ packages/samuel/samuel-0.1.9.tar.gz/samuel-0.1.9/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+#
+# start samuel from within the source folder
+# you must run 'python setup.py build' to build the engine before this will run
+#
+
+import sys
+import os
+import sysconfig
+
+assert sys.version_info >= (3,0)
+
+build_lib = "lib.%s-%s" % (sysconfig.get_platform(), sysconfig.get_python_version())
+
+pypath = os.path.join("build", build_lib, "samuel")
+if not os.path.isdir(pypath):
+    print("build lib not found - did you remember to run 'python setup.py build'?")
+    sys.exit()
+
+sys.path.append(pypath)
+
+import samuel.samuel
+samuel.samuel.run()
---
+++ packages/samuel/samuel-0.1.9.tar.gz/samuel-0.1.9/setup.py
@@ -0,0 +1,47 @@
+from distutils.core import setup, Extension
+from glob import glob
+import os
+
+module1 = Extension('samuel.engine',
+     sources = ['engine/enginemodule.cpp',
+                'engine/ai.cpp',                
+                ])
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup (name = 'samuel',
+    version = '0.1.9',
+    description = 'A Draughts Program',
+    ext_modules = [module1],
+    author='John Cheetham',
+    author_email='developer@johncheetham.com',  
+    url='http://www.johncheetham.com/projects/samuel/', 
+    long_description=read("README.rst"),
+    platforms = ['Linux'],
+
+    license = "GPLv3+",
+
+    packages=['samuel'],    
+
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: X11 Applications :: GTK',
+          'Intended Audience :: End Users/Desktop',                
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX :: Linux',          
+          'Programming Language :: Python :: 3',
+          'Programming Language :: C++',
+          'Topic :: Games/Entertainment :: Board Games',
+          ],
+    data_files = [("share/samuel/images", glob('images/*.png')),
+            ("share/samuel/data", ['data/2pc.cdb', 'data/3pc.cdb', 'data/4pc.cdb', 'data/opening.gbk']),
+            ("share/doc/samuel-0.1.9", ["README.rst", "LICENSE"]),
+            ('share/applications',['samuel.desktop']),
+            ('share/pixmaps', ['samuel.png']),
+    ],    
+    scripts = [
+        'scripts/samuel'
+    ]
+               
+    )
