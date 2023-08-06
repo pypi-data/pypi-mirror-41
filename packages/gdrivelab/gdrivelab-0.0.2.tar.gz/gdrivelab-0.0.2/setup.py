371ce73eb5dc0b0d59db91be037cea2d3ef9d641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:33 +0000

    Add package gdrivelab/0.0.2/gdrivelab-0.0.2.tar.gz

---
+++ packages/gdrivelab/gdrivelab-0.0.2.tar.gz/gdrivelab-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gdrivelab",
+    version="0.0.2",
+    author="Mikolaj Ostrzolek",
+    author_email="mikolaj.ostrzolek@gmail.com",
+    description="Don't wast time",
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
