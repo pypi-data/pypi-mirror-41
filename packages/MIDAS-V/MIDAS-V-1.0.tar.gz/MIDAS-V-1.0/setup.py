88c7265a163210d0b1eb1cb9c60df6ceae1af11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:29 +0000

    Add package MIDAS-V/1.0/MIDAS-V-1.0.tar.gz

---
+++ packages/MIDAS-V/MIDAS-V-1.0.tar.gz/MIDAS-V-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='MIDAS-V',
+      version='1.0',
+      description='MIDAS is a system for generating Boolean Framework for Modeling and Inferring Drug Targets and Drug Resistance Mechanisms',
+      author='Vrushali Dipak Fangal, Amitabh Sharma, Channing Division of Network Medicine, Harvard Medical School',
+      author_email='vrushali@broadinstitute.org',
+      url='https://github.com/vrushali-broad/MIDAS/tree/master',
+      license='MIT',
+      packages=['MIDAS'],
+      package_dir={'MIDAS': 'MIDAS'}
+)
