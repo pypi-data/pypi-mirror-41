7008694623258480aaf9e0dbe8098f6f2c92e643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package django-idempotency-key/1.0.1/django-idempotency-key-1.0.1.tar.gz

---
+++ packages/django-idempotency-key/django-idempotency-key-1.0.1.tar.gz/django-idempotency-key-1.0.1/idempotency_key/encoders.py
@@ -0,0 +1,24 @@
+import abc
+import hashlib
+import json
+
+from idempotency_key.exceptions import MissingIdempotencyKeyError
+
+
+class IdempotencyKeyEncoder(object):
+    @abc.abstractmethod
+    def encode_key(self, request, key):
+        raise NotImplementedError
+
+
+class BasicKeyEncoder(IdempotencyKeyEncoder):
+    def encode_key(self, request, key):
+        if key is None:
+            raise MissingIdempotencyKeyError()
+        # Basic method for generating an encoded key
+        m = hashlib.sha256()
+        m.update(key.encode('UTF-8'))
+        m.update(request.path_info.encode('UTF-8'))
+        m.update(request.method.encode('UTF-8'))
+        m.update(json.dumps(request.POST).encode('UTF-8'))
+        return m.hexdigest()
