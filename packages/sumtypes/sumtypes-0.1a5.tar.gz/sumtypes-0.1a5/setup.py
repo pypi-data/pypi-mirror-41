2bba482808d5cb98f6bd17c734c0c05e00110d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:34 +0000

    Add package sumtypes/0.1a5/sumtypes-0.1a5.tar.gz

---
+++ packages/sumtypes/sumtypes-0.1a5.tar.gz/sumtypes-0.1a5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import setuptools
+
+setuptools.setup(
+    name="sumtypes",
+    version="0.1a5",
+    description="Algebraic types for Python (notably providing Sum Types, aka "
+                "Tagged Unions)",
+    long_description=open('README.rst').read(),
+    url="http://github.com/radix/sumtypes/",
+    author="Christopher Armstrong",
+    license="MIT",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['sumtypes'],
+    install_requires=['attrs'],
+)
