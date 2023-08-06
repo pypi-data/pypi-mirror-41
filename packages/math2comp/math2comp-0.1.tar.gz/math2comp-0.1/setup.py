7d2f87b4e4f9731c40dadf227ec4ea2c9cf3e4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:20 +0000

    Add package math2comp/0.1/math2comp-0.1.tar.gz

---
+++ packages/math2comp/math2comp-0.1.tar.gz/math2comp-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup (
+	name='math2comp',
+	version='0.1',
+	author='Jonathan Emery',
+	description='Converts mathematical functions to python functions.',
+	scripts=['math2comp']
+)
