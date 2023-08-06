3ea0edf79debfc08059e9ec02e868f8f7dc6ea30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:35 +0000

    Add package polyswarm-client/1.0.3/polyswarm-client-1.0.3.tar.gz

---
+++ packages/polyswarm-client/polyswarm-client-1.0.3.tar.gz/polyswarm-client-1.0.3/src/polyswarmclient/abstractscanner.py
@@ -0,0 +1,28 @@
+import logging
+from abc import ABC, abstractmethod
+
+logger = logging.getLogger(__name__)  # Initialize logger
+
+
+class AbstractScanner(ABC):
+    """
+    Base `Scanner` class. To be overwritten with other scanning logic.
+    """
+
+    @abstractmethod
+    async def scan(self, guid, content, chain):
+        """Override this to implement custom scanning logic
+
+        Args:
+            guid (str): GUID of the bounty under analysis, use to track artifacts in the same bounty
+            content (bytes): Content of the artifact to be scan
+        Returns:
+            Tuple(bool, bool, str): Tuple of bit, verdict, metadata
+
+        Note:
+            | The meaning of the return types are as follows:
+            |   - **bit** (*bool*): Whether to include this artifact in the assertion or not
+            |   - **verdict** (*bool*): Whether this artifact is malicious or not
+            |   - **metadata** (*str*): Optional metadata about this artifact
+        """
+        pass
