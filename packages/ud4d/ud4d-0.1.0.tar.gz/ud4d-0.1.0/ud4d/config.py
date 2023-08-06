fabf1917f91dfed593b691e0fd9857c12a48f2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:27 +0000

    Add package ud4d/0.1.0/ud4d-0.1.0.tar.gz

---
+++ packages/ud4d/ud4d-0.1.0.tar.gz/ud4d-0.1.0/ud4d/config.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+# check: udevadm is runnable?
+assert not subprocess.check_call(['udevadm', '--version']), 'udevadm started failed. Is your system linux?'
+
+CHARSET = 'utf-8'
+UDEVADM_PATH = subprocess.check_output(['which', 'udevadm']).decode(CHARSET).strip()
+DEVICE_IMAGE_NAME = 'williamfzc/adb_tester:0.1'
